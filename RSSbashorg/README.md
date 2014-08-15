36578ae2544e934a2cda100982ee022a86c80110
gil9red <gil9red@gmail.com>
2014-08-16 02:40:21 +0600

    Added script RSSbashorg.
    Signed-off-by: gil9red <gil9red@gmail.com>

---
+++ RSSbashorg/README.md
@@ -0,0 +1,8 @@
+RSSbashorg
+===========
+
+EN:
+Using the example on feedparser rss site bash.im
+
+RU:
+Использование модуля feedparser на примере rss сайта bash.im
---
+++ RSSbashorg/RSSbashorg.py
@@ -0,0 +1,11 @@
+__author__ = 'ipetrash'
+
+import feedparser
+import HTMLParser
+d = feedparser.parse('http://bash.im/rss/')
+print(d["feed"]["subtitle"])
+for entry in d["entries"]:
+    quote = entry["summary"]
+    h = HTMLParser.HTMLParser()
+    quote = h.unescape(quote).replace("<br />", "\n")
+    print(quote + '\n\n')
