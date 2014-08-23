3c01039c0be0f5e4943fc49289c4e47309aadc4b
Ilya Petrash <gil9red@users.noreply.github.com>
2014-08-23 18:32:39 +0600

    Update FOO_TEST_TEST.py

--- FOO_TEST_TEST.py
+++ FOO_TEST_TEST.py
@@ -237,62 +237,96 @@ __author__ = 'ipetrash'
 #     # image.save(path)
 
 
-# Sort / Сортировка
-# https://wiki.python.org/moin/HowTo/Sorting
-import random
-l = [x for x in range(20)]
-random.shuffle(l)
-print("List: %s" % l)
-l.sort()
-print("Sorted list: %s" % l)
-l.sort(reverse=True)
-print("Reversed Sorted list: %s" % l)
+# # Sort / Сортировка
+# # https://wiki.python.org/moin/HowTo/Sorting
+# import random
+# l = [x for x in range(20)]
+# random.shuffle(l)
+# print("List: %s" % l)
+# l.sort()
+# print("Sorted list: %s" % l)
+# l.sort(reverse=True)
+# print("Reversed Sorted list: %s" % l)
+#
+# print()
+# m = [x for x in range(20)]
+# random.shuffle(m)
+# print("Sorted list: %s" % sorted(m))
+# print("Reversed Sorted list: %s" % sorted(m, reverse=True))
+#
+#
+# class Student:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def __repr__(self):
+#         return "%s (%d)" % (self.name, self.age)
+#
+#
+# students = list()
+# students.append(Student("Вася", 15))
+# students.append(Student("Аня", 16))
+# students.append(Student("Петя", 14))
+# students.append(Student("Эдуарт", 25))
+# students.append(Student("Таня", 16))
+# students.append(Student("Саша", 15))
+# print(students)
+#
+# students.sort(key=lambda x: x.age)  # Sorted by 'age'
+# print(students)
+#
+# students.sort(key=lambda x: x.age, reverse=True)  # Sorted by 'age'
+# print(students)
+#
+#
+# students.sort(key=lambda x: x.name)  # Sorted by 'name'
+# print(students)
+#
+#
+# print()
+# words = ["he", "He", "Ab", "ab", "Cc", "cC"]
+# print("Words: {0}".format(words))
+# print("Sorting:")
+# print(sorted(words))
+# print(sorted(words, reverse=True))
+# print()
+#
+# # Sorting insensitive
+# print("Sorting insensitive:")
+# print(sorted(words, key=str.lower))  # or print(sorted(words, key=lambda x: x.lower()))
+# print(sorted(words, key=str.lower, reverse=True))  # or print(sorted(words, key=lambda x: x.lower(), reverse=True))
 
+
+import sqlite3
+# https://docs.python.org/3.4/library/sqlite3.html
+path = r"C:\Users\ipetrash\Desktop\teller\kmaksimov_NDC.sdb"
+conn = sqlite3.connect(path)
+# result = tuple(row for row in conn .cursor().execute("SELECT * FROM BNA_Demoninations"))
+# for s in result:
+#     print(s)
+
+# cursor = conn .cursor()
+# for row in cursor.execute("SELECT * FROM BNA_Demoninations"):
+#     print(row)
+
+c = conn.cursor()
+result = tuple(row for row in c.execute("SELECT * FROM BNA_Demoninations"))
+print(result)
+print(sorted(result, key=lambda x: x[1]))  # sorting by the second value
+
+# Way to get a list of column names
 print()
-m = [x for x in range(20)]
-random.shuffle(m)
-print("Sorted list: %s" % sorted(m))
-print("Reversed Sorted list: %s" % sorted(m, reverse=True))
+# 1
+print("Column names: {0}".format(c.description))
 
+# 2
+result = tuple(row for row in c.execute("PRAGMA table_info('BNA_Demoninations')"))
+print("Column names: {0}".format(result))
 
-class Student:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    def __repr__(self):
-        return "%s (%d)" % (self.name, self.age)
-
-
-students = list()
-students.append(Student("Вася", 15))
-students.append(Student("Аня", 16))
-students.append(Student("Петя", 14))
-students.append(Student("Эдуарт", 25))
-students.append(Student("Таня", 16))
-students.append(Student("Саша", 15))
-print(students)
-
-students.sort(key=lambda x: x.age)  # Sorted by 'age'
-print(students)
-
-students.sort(key=lambda x: x.age, reverse=True)  # Sorted by 'age'
-print(students)
-
-
-students.sort(key=lambda x: x.name)  # Sorted by 'name'
-print(students)
-
-
-print()
-words = ["he", "He", "Ab", "ab", "Cc", "cC"]
-print("Words: {0}".format(words))
-print("Sorting:")
-print(sorted(words))
-print(sorted(words, reverse=True))
-print()
-
-# Sorting insensitive
-print("Sorting insensitive:")
-print(sorted(words, key=str.lower))  # or print(sorted(words, key=lambda x: x.lower()))
-print(sorted(words, key=str.lower, reverse=True))  # or print(sorted(words, key=lambda x: x.lower(), reverse=True))
+# 3
+conn.row_factory = sqlite3.Row
+c = conn.execute('select * from BNA_Demoninations')
+row = c.fetchone()  # instead of cursor.description
+names = row.keys()
+print("Column names: {0}".format(names))
