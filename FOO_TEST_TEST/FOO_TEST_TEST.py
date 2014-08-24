b7a45179c9bef5c6e57bed73ac216fcef4906124
gil9red <gil9red@gmail.com>
2014-08-24 21:31:47 +0600

    Moved FOO_TEST_TEST
    Signed-off-by: gil9red <gil9red@gmail.com>

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -3,80 +3,6 @@
 __author__ = 'ipetrash'
 
 
-# def getprint(str="hello world!"):
-#     print(str)
-#
-# def decor(func):
-#     def wrapper(*args, **kwargs):
-#         print("1 begin: " + func.__name__)
-#         print("Args={} kwargs={}".format(args, kwargs))
-#         f = func(*args, **kwargs)
-#         print("2 end: " + func.__name__ + "\n")
-#         return f
-#     return wrapper
-#
-# def predecor(w="W"):
-#     print(w, end=': ')
-#     return decor
-#
-# # getprint = decor(getprint)
-# # getprint()
-#
-# getprint("Py!")
-#
-#
-# def rgb2hex(get_rgb_func):
-#     def wrapper(*args, **kwargs):
-#         r, g, b = get_rgb_func(*args, **kwargs)
-#         return '#{:02x}{:02x}{:02x}'.format(r, g, b)
-#     return wrapper
-#
-# class RGB:
-#     def __init__(self):
-#         self._r = 0xff
-#         self._g = 0xff
-#         self._b = 0xff
-#
-#     def getr(self):
-#         return self._r
-#     def setr(self, r):
-#         self._r = r
-#     r = property(getr, setr)
-#
-#     def getg(self):
-#         return self._g
-#     def setg(self, g):
-#         self._g = g
-#     g = property(getg, setg)
-#
-#     def getb(self):
-#         return self._b
-#     def setb(self, b):
-#         self._b = b
-#     b = property(getb, setb)
-#
-#     @predecor("Hello")
-#     def setrgb(self, r, g, b):
-#         self.r, self.g, self.b = r, g, b
-#
-#     @decor
-#     @rgb2hex
-#     def getrgb(self):
-#         return (self.r, self.g, self.b)
-#
-# rgb = RGB()
-# print(rgb.r)
-# rgb.setrgb(0xff, 0x1, 0xff)
-# print(rgb.getrgb())
-
-# @decor
-# def foo(a, b):
-#     print("{} ^ {} = {}".format(a, b, (a ** b)))
-#
-# foo(2, 3)
-#
-# foo(b=3, a=2)
-
 # print('{one} * {one} {two}'.format(one="45", two="Bugaga"))
 
 
@@ -298,35 +224,35 @@ __author__ = 'ipetrash'
 # print(sorted(words, key=str.lower, reverse=True))  # or print(sorted(words, key=lambda x: x.lower(), reverse=True))
 
 
-import sqlite3
-# https://docs.python.org/3.4/library/sqlite3.html
-path = r"C:\Users\ipetrash\Desktop\teller\kmaksimov_NDC.sdb"
-conn = sqlite3.connect(path)
-# result = tuple(row for row in conn .cursor().execute("SELECT * FROM BNA_Demoninations"))
-# for s in result:
-#     print(s)
-
-# cursor = conn .cursor()
-# for row in cursor.execute("SELECT * FROM BNA_Demoninations"):
-#     print(row)
-
-c = conn.cursor()
-result = tuple(row for row in c.execute("SELECT * FROM BNA_Demoninations"))
-print(result)
-print(sorted(result, key=lambda x: x[1]))  # sorting by the second value
-
-# Way to get a list of column names
-print()
-# 1
-print("Column names: {0}".format(c.description))
-
-# 2
-result = tuple(row for row in c.execute("PRAGMA table_info('BNA_Demoninations')"))
-print("Column names: {0}".format(result))
-
-# 3
-conn.row_factory = sqlite3.Row
-c = conn.execute('select * from BNA_Demoninations')
-row = c.fetchone()  # instead of cursor.description
-names = row.keys()
-print("Column names: {0}".format(names))
+# import sqlite3
+# # https://docs.python.org/3.4/library/sqlite3.html
+# path = r"C:\Users\ipetrash\Desktop\teller\kmaksimov_NDC.sdb"
+# conn = sqlite3.connect(path)
+# # result = tuple(row for row in conn .cursor().execute("SELECT * FROM BNA_Demoninations"))
+# # for s in result:
+# #     print(s)
+#
+# # cursor = conn .cursor()
+# # for row in cursor.execute("SELECT * FROM BNA_Demoninations"):
+# #     print(row)
+#
+# c = conn.cursor()
+# result = tuple(row for row in c.execute("SELECT * FROM BNA_Demoninations"))
+# print(result)
+# print(sorted(result, key=lambda x: x[1]))  # sorting by the second value
+#
+# # Way to get a list of column names
+# print()
+# # 1
+# print("Column names: {0}".format(c.description))
+#
+# # 2
+# result = tuple(row for row in c.execute("PRAGMA table_info('BNA_Demoninations')"))
+# print("Column names: {0}".format(result))
+#
+# # 3
+# conn.row_factory = sqlite3.Row
+# c = conn.execute('select * from BNA_Demoninations')
+# row = c.fetchone()  # instead of cursor.description
+# names = row.keys()
+# print("Column names: {0}".format(names))
