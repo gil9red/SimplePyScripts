f9041f8a03a15b4ffc5d6a9ba0f5d57cd054e2db
Ilya Petrash <gil9red@gmail.com>
2014-09-09 01:33:04 +0600

    Update FOO_TEST_TEST.
    Signed-off-by: gil9red <gil9red@gmail.com>

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -182,3 +182,12 @@ __author__ = 'ipetrash'
 #         pass
 #     else:
 #         print(pinfo)
+
+
+## Пример того как можно узнать свой ip, используя запрос на сайт.
+import urllib.request
+with urllib.request.urlopen("http://api.wipmania.com") as f:
+    context = f.read().decode()
+    data = context.split("<br>")
+    ip = data[0]
+    print("My IP: {}".format(ip))
