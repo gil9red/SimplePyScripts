4890d088f9fc3c4135e33a816b316ecf41fc4e6b
Ilya Petrash <gil9red@gmail.com>
2014-09-15 18:55:09 +0600

    Update FOO_FOO_TEST.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from idlelib.PyShell import extended_linecache_checkcache
 
 __author__ = 'ipetrash'
 
@@ -168,6 +169,25 @@ __author__ = 'ipetrash'
 # http://habrahabr.ru/post/151832/
 # https://ru.wikipedia.org/wiki/%D0%96%D0%B8%D0%B7%D0%BD%D1%8C_%28%D0%B8%D0%B3%D1%80%D0%B0%29#.D0.9F.D1.80.D0.B0.D0.B2.D0.B8.D0.BB.D0.B0
 # http://habrahabr.ru/post/140581/
+#
+# def print_arr(arr):
+#     size = len(arr)
+#     for i in range(size):
+#         for j in range(size):
+#             print(" {} ".format(arr[i][j]), end='')
+#         print()
+#
+# def create_arr(size):
+#     arr = []
+#     for i in range(size):
+#         arr.append([])
+#         for j in range(size):
+#             arr[i].append(0)
+#     return arr
+#
+# SIZE = 5
+# field = create_arr(SIZE)
+# print_arr(field)
 
 
 # ### Больше примеров по ссылкам: http://pythonhosted.org/psutil/ и https://github.com/giampaolo/psutil
