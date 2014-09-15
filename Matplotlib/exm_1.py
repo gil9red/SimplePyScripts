21609cdcd058eafe0a99df232e12e678b1e661dc
Ilya Petrash <gil9red@gmail.com>
2014-09-16 00:37:34 +0600

    Добавлены примеры работы с модулем Matplotlib.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

---
+++ Matplotlib/exm_1.py
@@ -0,0 +1,41 @@
+__author__ = 'ipetrash'
+
+
+# Следующий пример строит график функции f(x) = x / sin(x):
+
+import math
+
+# !!! Импортируем один из пакетов Matplotlib
+import pylab
+
+# !!! Импортируем пакет со вспомогательными функциями
+from matplotlib import mlab
+
+if __name__ == '__main__':
+    # Будем рисовать график этой функции
+    def func(x):
+        """
+        sinc (x)
+        """
+        if x == 0:
+            return 1.0
+        return math.sin (x) / x
+
+    # Интервал изменения переменной по оси X
+    xmin = -20.0
+    xmax = 20.0
+
+    # Шаг между точками
+    dx = 0.01
+
+    # !!! Создадим список координат по оси X на отрезке [-xmin; xmax], включая концы
+    xlist = mlab.frange (xmin, xmax, dx)
+
+    # Вычислим значение функции в заданных точках
+    ylist = [func (x) for x in xlist]
+
+    # !!! Нарисуем одномерный график
+    pylab.plot (xlist, ylist)
+
+    # !!! Покажем окно с нарисованным графиком
+    pylab.show()
---
+++ Matplotlib/exm_2.py
@@ -0,0 +1,44 @@
+__author__ = 'ipetrash'
+
+
+# Если вызывать функцию plot() несколько раз подряд, то на график
+# будут добавляться новые кривые. Следующий пример рисует два графика на одних осях:
+
+import math
+
+# !!! Импортируем один из пакетов Matplotlib
+import pylab
+
+# !!! Импортируем пакет со вспомогательными функциями
+from matplotlib import mlab
+
+if __name__ == '__main__':
+    # Будем рисовать график этой функции
+    def func (x):
+        """
+        sinc (x)
+        """
+        if x == 0:
+            return 1.0
+        return math.sin (x) / x
+
+    # Интервал изменения переменной по оси X
+    xmin = -20.0
+    xmax = 20.0
+
+    # Шаг между точками
+    dx = 0.01
+
+    # !!! Создадим список координат по оиси X на отрезке [-xmin; xmax], включая концы
+    xlist = mlab.frange (xmin, xmax, dx)
+
+    # Вычислим значение функции в заданных точках
+    ylist1 = [func (x) for x in xlist]
+    ylist2 = [func (x * 0.2) for x in xlist]
+
+    # !!! Нарисуем одномерные графики
+    pylab.plot (xlist, ylist1)
+    pylab.plot (xlist, ylist2)
+
+    # !!! Покажем окно с нарисованным графиком
+    pylab.show()
