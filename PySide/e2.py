00e2f5e141c4bf5b3d41f68183829b292c10e840
Ilya Petrash <gil9red@gmail.com>
2014-09-17 14:08:58 +0600

    Добавление нового примера работы с модулем PySide.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

---
+++ PySide/e2.py
@@ -0,0 +1,47 @@
+__author__ = 'ipetrash'
+
+
+# Пример показывает как можно создать простое окно с небольшим функционалом,
+# наследуюсь от класса QWidget
+
+
+from PySide import QtGui
+
+
+class SimpleWindow(QtGui.QWidget):
+    """Просто окно"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setWindowTitle("Простое окно")
+
+        self.lw_log = QtGui.QListWidget()
+        self.le_mess = QtGui.QLineEdit()
+        self.pb_send = QtGui.QPushButton("Отправить")
+        self.pb_send.clicked.connect(self.slot_add_log)
+
+        layout_cmd = QtGui.QHBoxLayout()
+        layout_cmd.addWidget(self.le_mess)
+        layout_cmd.addWidget(self.pb_send)
+
+        layout_main = QtGui.QVBoxLayout()
+        layout_main.addWidget(self.lw_log)
+        layout_main.addLayout(layout_cmd)
+
+        self.setLayout(layout_main)
+
+    def slot_add_log(self):
+        text = self.le_mess.text()
+        if text:
+            self.lw_log.addItem(text)
+
+
+import sys
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+
+    w = SimpleWindow()
+    w.show()
+
+    sys.exit(app.exec_())
