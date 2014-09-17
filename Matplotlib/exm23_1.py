a6805f8fddf1aedcd9c55d6df7a56d72f4766434
Ilya Petrash <gil9red@gmail.com>
2014-09-18 00:47:46 +0600

    Добавление новых примеров работы с модулем Matplotlib.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

---
+++ Matplotlib/exm23_1.py
@@ -0,0 +1,31 @@
+__author__ = 'ipetrash'
+
+
+## Использование библиотеки Matplotlib. Как изменять формат меток на осях
+## Введение
+#
+# Помимо настройки положения меток на осях Matplotlib позволяет настраивать
+# подписи под ними. Для этого используются специальные классы (форматеры),
+# производные от класса matplotlib.ticker.Formatter. Для того, чтобы установить
+# форматер для оси, нужно вызвать метод set_major_formatter() экземпляра класса
+# matplotlib.axis.Axis - класса для работы с одной осью.
+# Для начала выведем график, в котором используется форматирование подписей по
+# умолчанию.
+
+
+import numpy
+
+import pylab
+
+
+if __name__ == "__main__":
+    xvals = numpy.arange (-10.0, 10.1, 0.1)
+    yvals = numpy.sinc (xvals)
+
+    figure = pylab.figure()
+    axes = figure.add_subplot (1, 1, 1)
+
+    pylab.plot (xvals, yvals)
+
+    axes.grid()
+    pylab.show()
---
+++ Matplotlib/exm23_2.py
@@ -0,0 +1,39 @@
+__author__ = 'ipetrash'
+
+
+# Теперь рассмотрим, как можно менять форматирование с помощью метода
+# set_major_formatter(), а заодно посмотрим, какие форматеры предоставляет
+# библиотека Matplotlib. Все рассматриваемые здесь форматеры находятся в модуле
+# matplotlib.ticker. Кроме того некоторые другие, более специфические модули
+# предоставляют свои форматеры (например, модуль для работы с календарными данными),
+# но в данной статье мы их рассматривать не будем.
+# NullFormatter
+#
+# С помощью класса NullFormatter можно отключить вывод всех подписей у меток на
+# оси (при этом сами метки остаются, для их отключения нужно использовать локатор
+# NullLocator).
+
+
+import numpy
+
+import pylab
+import matplotlib.ticker
+
+
+if __name__ == "__main__":
+    xvals = numpy.arange (-10.0, 10.1, 0.1)
+    yvals = numpy.sinc (xvals)
+
+    figure = pylab.figure()
+    axes = figure.add_subplot (1, 1, 1)
+
+    # Создаем форматер
+    formatter = matplotlib.ticker.NullFormatter()
+
+    # Установка форматера для оси X
+    axes.xaxis.set_major_formatter (formatter)
+
+    pylab.plot (xvals, yvals)
+
+    axes.grid()
+    pylab.show()
