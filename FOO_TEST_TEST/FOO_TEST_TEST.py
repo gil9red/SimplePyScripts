86faee77a596835f9e5c1a2420b980776d7b273c
Ilya Petrash <gil9red@gmail.com>
2014-09-19 18:22:20 +0600

    Update FOO_TEST_TEST.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -286,3 +286,21 @@ __author__ = 'ipetrash'
 
 
 # TODO: придумать простое приложение и реализовтаь его с помощью TDD (используя unit-тесты)
+
+
+# Инвертирование цвета картинки
+from PIL import Image
+import PIL.ImageOps
+
+image_file = r"TowerOfGod_s2_ch100_p02_SIU.png_res.jpg"
+image = Image.open(image_file)
+if image.mode == 'RGBA':
+    r, g, b, a = image.split()
+    rgb_image = Image.merge('RGB', (r, g, b))
+    inverted_image = PIL.ImageOps.invert(rgb_image)
+    r2, g2, b2 = inverted_image.split()
+    final_transparent_image = Image.merge('RGBA', (r2, g2, b2, a))
+    final_transparent_image.save('new_file.png')
+else:
+    inverted_image = PIL.ImageOps.invert(image)
+    inverted_image.save('new_name.png')
