8e12c6826e84a5bb2f71774a3f5e357d0a86bc01
Ilya Petrash <gil9red@gmail.com>
2014-09-21 20:33:20 +0600

    Обновление скрипта ConfuciusQuotes.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- Confucius quotes/ConfuciusQuotes.py
+++ Confucius quotes/ConfuciusQuotes.py
@@ -4,14 +4,20 @@ __author__ = 'ipetrash'
 
 from grab import Grab
 
-g = Grab()
-# http://ru.wikiquote.org/wiki/Конфуций
-url = "http://ru.wikiquote.org/wiki/%D0%9A%D0%BE%D0%BD%D1%84%D1%83%D1%86%D0%B8%D0%B9"
-g.go(url)
 
-quotes = list()
-for el in g.doc.select('//h2/following-sibling::ul/li'):
-    quotes.append(el.text())
+def get_confucius_quotes():
+    g = Grab()
+    # http://ru.wikiquote.org/wiki/Конфуций
+    url = "http://ru.wikiquote.org/wiki/%D0%9A%D0%BE%D0%BD%D1%84%D1%83%D1%86%D0%B8%D0%B9"
+    g.go(url)
 
-for q in quotes:
-    print("'{}'".format(q))
+    quotes = list()
+    for el in g.doc.select('//h2/following-sibling::ul/li'):
+        quotes.append(el.text())
+
+    return quotes
+
+
+if __name__ == '__main__':
+    for q in get_confucius_quotes():
+        print("'{}'".format(q))
