b86a59d9ecfe675bc60254ecf210a37e46ffd68c
Ilya Petrash <gil9red@gmail.com>
2014-09-24 00:15:24 +0600

    Добавлен скрипт, который используя логин и пароль, авторизовывается в github, а после выводит список репозиториев и некоторую информацию о них.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

---
+++ Grab/get_repo_github_info.py
@@ -0,0 +1,50 @@
+"""
+Скрипт, используя логин и пароль, авторизовывается в github, после выводит список
+репозиториев и некоторую информацию о них.
+"""
+
+__author__ = 'ipetrash'
+
+
+from grab import Grab
+
+
+if __name__ == '__main__':
+    login = input("Логин: ")
+    password = input("Пароль: ")
+
+    g = Grab()
+
+    # Переходим на страницу входа
+    print("...Перехожу на страницу входа...")
+    g.go('https://github.com/login')
+
+    # Заполняем формы логина и пароля
+    print("...Заполняем формы логина и пароля...")
+    g.set_input('login', login)
+    g.set_input('password', password)
+
+    # Отсылаю данные формы
+    print("...Отсылаю данные формы...")
+    g.submit()
+
+    # Переход на страницу с репозиториями
+    print("...Перехожу на страницу с репозиториями...")
+    g.go("https://github.com/{}?tab=repositories".format(login))
+
+    # Получение списка репозиториев
+    print("...Получаю список репозиториев...")
+    # list_repo = g.doc.select('//ul[@class="repo-list js-repo-list"]/li/h3/a')
+    list_repo = g.doc.select('//h3[@class="repo-list-name"]/a')
+
+    print("\nРепозитории({}):".format(len(list_repo)))
+
+    # Перебор всех репозиториев
+    for elem in list_repo:
+        url = "https://github.com" + elem.attr('href')
+        grab_repo = Grab()
+        grab_repo.go(url)  # переход на страницу репозитория
+
+        # получение количества коммитов данного репозитория
+        count = grab_repo.doc.select('//span[@class="num text-emphasized"]').text()
+        print('"{}" ({} commits): {}'.format(elem.text(), count, url))
