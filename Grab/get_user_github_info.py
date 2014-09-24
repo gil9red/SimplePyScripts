3d7c3b64669d672da316e590b8e94afc59468b9b
Ilya Petrash <gil9red@gmail.com>
2014-09-25 00:23:42 +0600

    Добавлен скрипт, который используя логин и пароль, авторизовывается в github, а после выводит информацию о пользователе.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

---
+++ Grab/get_user_github_info.py
@@ -0,0 +1,52 @@
+"""
+Скрипт, используя логин и пароль, авторизовывается в github, после
+выводит информацию о пользователе.
+"""
+
+# TODO: добавить больше информации о пользователе.
+
+__author__ = 'ipetrash'
+
+
+from grab import Grab
+
+if __name__ == '__main__':
+    login = input("Логин: ")
+    password = input("Пароль: ")
+
+    g = Grab()
+
+    # Переходим на страницу входа
+    print("...Перехожу на страницу входа...")
+    g.go("https://github.com/login")
+
+    # Заполняем формы логина и пароля
+    print("...Заполняю формы логина и пароля...")
+    g.set_input("login", login)
+    g.set_input("password", password)
+
+    # Отсылаю данные формы
+    print("...Отсылаю данные формы...")
+    g.submit()
+
+    # Переход на страницу пользователя
+    print("...Перехожу на страницу пользователя...")
+    g.go("https://github.com/" + login)
+
+    # Получение информации с страницы пользователя
+    print("...Получаю информацию с страницы пользователя...")
+    fullname = g.doc.select('//span[@itemprop="name"]').text()
+    username = g.doc.select('//span[@itemprop="additionalName"]').text()
+    organization = g.doc.select('//li[@itemprop="worksFor"]').text()
+    homeLocation = g.doc.select('//li[@itemprop="homeLocation"]').text()
+    email = g.doc.select('//a[@class="email"]').text()
+    join_label = g.doc.select('//span[@class="join-label"]').text()
+    join_title_time = g.doc.select('//time[@class="join-date"]').text()
+    join_datetime = g.doc.select('//time[@class="join-date"]').attr("datetime")
+
+    print("\nfullname:", fullname)
+    print("username:", username)
+    print("organization:", organization)
+    print("homeLocation:", homeLocation)
+    print("email:", email)
+    print('{} {} ({})'.format(join_label, join_title_time, join_datetime))
