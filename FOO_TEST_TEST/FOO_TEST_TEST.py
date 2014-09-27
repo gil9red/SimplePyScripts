56200c370f49b6451738d1506a932b24c509873d
Ilya Petrash <gil9red@users.noreply.github.com>
2014-09-27 16:07:24 +0600

    Update FOO_TEST_TEST.py

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -169,6 +169,7 @@ __author__ = 'ipetrash'
 # http://habrahabr.ru/post/151832/
 # https://ru.wikipedia.org/wiki/%D0%96%D0%B8%D0%B7%D0%BD%D1%8C_%28%D0%B8%D0%B3%D1%80%D0%B0%29#.D0.9F.D1.80.D0.B0.D0.B2.D0.B8.D0.BB.D0.B0
 # http://habrahabr.ru/post/140581/
+# http://habrahabr.ru/company/mailru/blog/228379/
 #
 # def print_arr(arr):
 #     size = len(arr)
