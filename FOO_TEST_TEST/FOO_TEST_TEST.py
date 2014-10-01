49d5dd1dc8638377e4e69d7d425bde35194bb894
Ilya Petrash <gil9red@users.noreply.github.com>
2014-10-01 11:58:27 +0600

    Update FOO_TEST_TEST.py

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -375,5 +375,20 @@ __author__ = 'ipetrash'
 # сможет открыть книгочиталка.
 
 
+# TODO: https://docs.python.org/3/library/collections.html#collections.namedtuple
+# >>> # Basic example
+# >>> Point = namedtuple('Point', ['x', 'y'])
+# >>> p = Point(11, y=22)     # instantiate with positional or keyword arguments
+# >>> p[0] + p[1]             # indexable like the plain tuple (11, 22)
+# 33
+# >>> x, y = p                # unpack like a regular tuple
+# >>> x, y
+# (11, 22)
+# >>> p.x + p.y               # fields also accessible by name
+# 33
+# >>> p                       # readable __repr__ with a name=value style
+# Point(x=11, y=22)
+
+
 # TODO: brutforce Instagram 
 # http://habrahabr.ru/post/215829/
