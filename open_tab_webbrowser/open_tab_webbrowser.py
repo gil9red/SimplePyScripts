46fe1ca0749e032cd676b0460f043aca02be3f9b
Ilya Petrash <gil9red@gmail.com>
2014-10-04 01:34:16 +0600

    Добавлен скрипт open_tab_webbrowser.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

---
+++ open_tab_webbrowser/open_tab_webbrowser.py
@@ -0,0 +1,11 @@
+__author__ = 'ipetrash'
+
+
+"""Пример открытия вкладки в браузере."""
+
+
+if __name__ == '__main__':
+    import webbrowser
+
+    # Вернет True и откроет вкладку
+    webbrowser.open_new_tab("https://github.com/gil9red")
