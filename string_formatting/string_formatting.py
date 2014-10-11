e7ed9468a916b5164deca2a4d2c0341d6801d60a
Ilya Petrash <gil9red@gmail.com>
2014-10-12 01:08:03 +0600

    Добавлен string_formatting.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

---
+++ string_formatting/string_formatting.py
@@ -0,0 +1,7 @@
+__author__ = 'ipetrash'
+
+
+if __name__ == '__main__':
+    # TODO: побольше примеров форматирования строк.
+    # https://docs.python.org/3/library/string.html#string-formatting
+    print('{one} * {one} {two}'.format(one="45", two="Bugaga"))
