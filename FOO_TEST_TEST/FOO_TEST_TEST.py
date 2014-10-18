38f66e95d5d3e22ba969a812247665862ae7de55
Ilya Petrash <gil9red@gmail.com>
2014-10-19 03:03:26 +0600

    Обновлено.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -180,20 +180,6 @@ __author__ = 'ipetrash'
 # http://jenyay.net/Matplotlib/LogAxes
 
 
-# # TODO: datetime
-# # dates are easily constructed and formatted
-# from datetime import date
-# now = date.today()
-# # now
-# now.strftime("%m-%d-%y. %d %b %Y is a %A on the %d day of %B.")
-# #'12-02-03. 02 Dec 2003 is a Tuesday on the 02 day of December.'
-#
-# # dates support calendar arithmetic
-# birthday = date(1964, 7, 31)
-# age = now - birthday
-# print(age.days)
-
-
 # ## Quality Control
 # # doctest
 # def average(values):
