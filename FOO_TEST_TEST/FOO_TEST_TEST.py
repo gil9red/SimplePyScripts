555a86be584508a8b7f2601e5f7a7c714b5cc7f1
Ilya Petrash <gil9red@gmail.com>
2014-11-20 02:32:04 +0600

    Обновлено.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -4,6 +4,51 @@
 __author__ = 'ipetrash'
 
 
+__author__ = 'ipetrash'
+
+"""Модуль для возвращения информации о ранобе."""
+
+
+# def get_ranobe_info_from_ruranobe(url):
+#     """Функция возвращает словарь, содержащий информацию о ранобе."""
+#
+#     from grab import Grab
+#
+#     g = Grab()
+#     g.setup(hammer_mode=True)
+#     g.go(url)
+#
+#     # Если не удачно, выходим
+#     if g.response.code != 200:
+#         print("Не найден {}.".format(url))
+#         return
+#
+#     # Получение основного контекста, содержащий аннотацию и ссылки на тома
+#     content_text = g.doc.select('//div[@id="mw-content-text"]')
+#
+#     # Получение названия ранобе
+#     name_ranobe = g.doc.select('//h1[@id="firstHeading"]').text()
+#
+#     # Получение и объединение параграфов в единый текст
+#     annotation = '\n'.join(r.text() for r in content_text.select('p/i'))
+#
+#     # Получение списка томов
+#     list_volumes = content_text.select('ul/li/a')
+#
+#     info = {
+#         "name": name_ranobe,
+#         "annotation": annotation,
+#         "list_volumes": list_volumes,
+#         "url": url,
+#     }
+#     return info
+#
+# ranobe_info = get_ranobe_info_from_ruranobe('http://ruranobe.ru/r/sao')
+# print(ranobe_info['annotation'])
+# for i, volume in enumerate(ranobe_info['list_volumes'], 1):
+#     print('{}. "{}"'.format(i, volume.text()))
+
+
 # TODO: pretty xml
 # from xml.dom.minidom import parseString
 #
