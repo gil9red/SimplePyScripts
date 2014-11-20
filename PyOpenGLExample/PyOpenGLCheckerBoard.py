297fd214ba9fa337471f1104ebdc5df20c49a96a
Ilya Petrash <gil9red@gmail.com>
2014-11-20 23:13:57 +0600

    Обновлен модуль PyOpenGLCheckerBoard: пример теперь правильный.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- PyOpenGLExample/PyOpenGLCheckerBoard.py
+++ PyOpenGLExample/PyOpenGLCheckerBoard.py
@@ -2,28 +2,48 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 
-import random
+
+"""
+Generating a checkerboard
+
+This code generates a checkerboard.
+"""
+
 
 def initFun():
-    glClearColor(1.0,1.0,1.0,0.0)
-    glColor3f(0.0,0.0, 0.0)
+    glClearColor(1.0, 1.0, 1.0, 0.0)
+    glColor3f(0.0, 0.0, 0.0)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    gluOrtho2D(0.0,640.0,0.0,480.0)
-    
+    gluOrtho2D(0.0, 640.0, 0.0, 480.0)
+
 
 def displayFun():
+    CELL_SIZE = 40
+
     glClear(GL_COLOR_BUFFER_BIT)
-    for i in range(0,25):
-        gray = idx=random.randint(0,25)/25.0
-        glColor3f(gray,gray,gray)
-        glRecti(random.randint(0,640),random.randint(0,480),
-                random.randint(0,640),random.randint(0,480))
+
+    for i in range(0, 8):
+        x1 = i * CELL_SIZE
+        x2 = x1 + CELL_SIZE
+
+        for j in range(0, 8):
+            y1 = j * CELL_SIZE
+            y2 = y1 + CELL_SIZE
+
+            if (i % 2 and not j % 2) or (not i % 2 and j % 2):
+                glColor3f(0xff, 0xff, 0xff)  # white
+            else:
+                glColor3f(0x0, 0x0, 0x0)  # black
+
+            glRecti(x1, y1, x2, y2)
+
     glFlush()
+
 
 if __name__ == '__main__':
     glutInit()
-    glutInitWindowSize(640,480)
+    glutInitWindowSize(640, 480)
     glutCreateWindow(b"DrawSquares")
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
     glutDisplayFunc(displayFun)
