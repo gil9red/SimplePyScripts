47bd596e3c11bb2947836f20e0f7eb322730074c
Ilya Petrash <gil9red@gmail.com>
2014-11-20 14:35:11 +0600

    Обновлен модуль PyOpenGLHelloWorld:  * Добавление описания  * Рефакторинг
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- PyOpenGLExample/PyOpenGLHelloWorld.py
+++ PyOpenGLExample/PyOpenGLHelloWorld.py
@@ -2,27 +2,40 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 
+
+"""
+A hello world application
+
+This pages creates a simple window to which 2d orthogonal projection is applied.
+Next a couple of points are drawn. This will create a white window with three black
+dots.
+
+url = http://www.de-brauwer.be/wiki/wikka.php?wakka=PyOpenGLHelloWorld
+"""
+
+
 def initFun():
-    glClearColor(1.0,1.0,1.0,0.0)
-    glColor3f(0.0,0.0, 0.0)
+    glClearColor(1.0, 1.0, 1.0, 0.0)
+    glColor3f(0.0, 0.0, 0.0)
     glPointSize(4.0)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    gluOrtho2D(0.0,640.0,0.0,480.0)
-    
+    gluOrtho2D(0.0, 640.0, 0.0, 480.0)
+
 
 def displayFun():
     glClear(GL_COLOR_BUFFER_BIT)
     glBegin(GL_POINTS)
-    glVertex2i(100,50)
-    glVertex2i(100,130)
-    glVertex2i(150,130)
+    glVertex2i(100, 50)
+    glVertex2i(100, 130)
+    glVertex2i(150, 130)
     glEnd()
     glFlush()
 
+
 if __name__ == '__main__':
     glutInit()
-    glutInitWindowSize(640,480)
+    glutInitWindowSize(640, 480)
     glutCreateWindow(b"Drawdots")
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
     glutDisplayFunc(displayFun)
