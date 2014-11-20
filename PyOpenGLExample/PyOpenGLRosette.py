2fb1de247c9c7faf178b44a5959a014a957ac8a3
Ilya Petrash <gil9red@gmail.com>
2014-11-21 02:26:59 +0600

    Обновлен модуль PyOpenGLRosette.py:  * Добавление описания  * Рефакторинг
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- PyOpenGLExample/PyOpenGLRosette.py
+++ PyOpenGLExample/PyOpenGLRosette.py
@@ -1,49 +1,69 @@
+import math
+
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 
-import math 
 
-N=25
-RADIUS=95
+"""
+Rosette
+
+Introduction
+A rosette appears when the points of an n-gon (regular polygon with points
+P_i = (R cos(2*pi*i/n),R sin(2*pi*i/n)) ) are connected to all other points of
+the n-gon. E.g. when n=5, the result is an outer pentagon and an inner pentagram.
+In the code below, first all points of the n-gon are calculated, then a double for
+loop connect points i to points i+1,...,n-1,n.
 
 
-def initFun():
-    glClearColor(1.0,1.0,1.0,0.0)
-    glColor3f(0.0,0.0, 0.0)
+http://www.de-brauwer.be/wiki/wikka.php?wakka=PyOpenGLRosette
+"""
+
+N = 25
+RADIUS = 95
+
+
+def init_fun():
+    glClearColor(1.0, 1.0, 1.0, 0.0)
+    glColor3f(0.0, 0.0, 0.0)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    gluOrtho2D(-100,100,-100,100)
+    gluOrtho2D(-100, 100, -100, 100)
 
-def reshapeFun(w,h):
-    if w > h:
-        glViewport((w-h)/2,0,h,h)
-    else:
-        glViewport(0,(h-w)/2,w,w)
 
-def displayFun():
+def reshape_fun(w, h):
+    glViewport(0, 0, w, h)
+
+    # if w > h:
+    # glViewport((w - h) / 2, 0, h, h)
+    # else:
+    #     glViewport(0, (h - w) / 2, w, w)
+
+
+def display_fun():
     glClear(GL_COLOR_BUFFER_BIT)
-    glColor3f(0.0,0.0,1.0)
-    xpts=[]
-    ypts=[]
-    for i in range(0,N):
-        xpts.append(RADIUS*math.sin(2.0*math.pi*i/N))
-        ypts.append(RADIUS*math.cos(2.0*math.pi*i/N))
-    
+    glColor3f(0.0, 0.0, 1.0)
+    xpts = []
+    ypts = []
+    for i in range(0, N):
+        xpts.append(RADIUS * math.sin(2.0 * math.pi * i / N))
+        ypts.append(RADIUS * math.cos(2.0 * math.pi * i / N))
+
     glBegin(GL_LINE_STRIP)
-    for i in range(0,N):
-        for j in range(i,N):
-            glVertex2f(xpts[i],ypts[i])
-            glVertex2f(xpts[j],ypts[j])
+    for i in range(0, N):
+        for j in range(i, N):
+            glVertex2f(xpts[i], ypts[i])
+            glVertex2f(xpts[j], ypts[j])
     glEnd()
     glFlush()
 
+
 if __name__ == '__main__':
     glutInit()
-    glutInitWindowSize(400,400)
+    glutInitWindowSize(400, 400)
     glutCreateWindow(b"Rosette")
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
-    glutDisplayFunc(displayFun)
-    glutReshapeFunc(reshapeFun)
-    initFun()
+    glutDisplayFunc(display_fun)
+    glutReshapeFunc(reshape_fun)
+    init_fun()
     glutMainLoop()
