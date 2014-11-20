924f9e4f2a91e6b06a2895b39732f5a99db13754
Ilya Petrash <gil9red@gmail.com>
2014-11-20 12:10:19 +0600

    Добавлен модуль get_example_from_de_brauwer.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

---
+++ Grab/get_example_from_de_brauwer.py
@@ -0,0 +1,48 @@
+from grab import Grab
+import re
+
+__author__ = 'ipetrash'
+
+
+"""Пример получения исходного кода с сайта www.de-brauwer.be."""
+
+
+def get_example_from_de_brauwer(wakka):
+    g = Grab()
+    g.go('http://www.de-brauwer.be/wiki/wikka.php?wakka=' + wakka)
+    code_html = g.doc.select('//div[@class="code"]').html()
+
+    pattern = (r'(<div class="code".*?>)'
+               '|</div.*?>'
+               '|<span.*?>'
+               '|</span.*?>'
+               '|<br>')
+    source_code = re.sub(pattern, '', code_html)
+    source_code = re.sub('\xa0', ' ', source_code)  # remove &nbsp;
+    return source_code
+
+DIR_NAME = 'PyOpenGLExample'
+import os
+if not os.path.exists(DIR_NAME):
+    os.makedirs(DIR_NAME)
+
+examples = [
+    'PyOpenGLHelloWorld',
+    'PyOpenGLSierpinski',
+    'PyOpenGLSquares',
+    'PyOpenGLCheckerBoard',
+    'PyOpenGLMouse',
+    'PyOpenGLScatter',
+    'PyOpenGLGingerbread',
+    'PyOpenGLMaze',
+    'PyOpenGLReshape',
+    'PyOpenGLTurtle',
+    'PyOpenGLRosette',
+    'PyOpenGLWireframe',
+]
+
+for e in examples:
+    source_code = get_example_from_de_brauwer(e)
+
+    with open(os.path.join(DIR_NAME, e + '.py'), mode='w', encoding='utf-8') as f:
+        f.write(source_code)
