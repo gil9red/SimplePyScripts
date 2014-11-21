7f499ffe1b28ab2c540e7e643d4574231929ab37
Ilya Petrash <gil9red@gmail.com>
2014-11-22 03:16:44 +0600

    Добавлен модуль search_on_google.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

---
+++ search_on_google/README.md
@@ -0,0 +1,6 @@
+search_on_google
+===========
+
+RU: Содержит пример http-запроса для поиска в гугле.
+
+EN: Contains example http-request to search in Google.
---
+++ search_on_google/search_on_google.py
@@ -0,0 +1,25 @@
+__author__ = 'ipetrash'
+
+
+"""Пример поиск по гуглу."""
+
+
+if __name__ == '__main__':
+    from urllib.parse import quote_plus
+    from grab import Grab
+
+    url_pattern = 'https://www.google.ru/search?q='
+    search_text = 'манга читать онлайн'
+
+    url = url_pattern + quote_plus(search_text)
+
+    g = Grab()
+    g.go(url)
+
+    print(g.response.url)
+
+    print(g.doc.select('//div[@id="resultStats"]').text())
+
+    search_result = g.doc.select('//li[@class="g"]/*/h3/a')
+    for i, c in enumerate(search_result, 1):
+        print('{}. "{}": {}'.format(i, c.text(), c.attr('href')))
