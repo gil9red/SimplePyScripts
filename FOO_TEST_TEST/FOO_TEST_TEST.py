2a95718feac88a89fd5099fa315ac5b5dfd69745
Ilya Petrash <gil9red@gmail.com>
2014-11-30 00:38:34 +0600

    Обновлено.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -189,29 +189,3 @@ __author__ = 'ipetrash'
 
 # TODO: brutforce Instagram
 # http://habrahabr.ru/post/215829/
-
-
-# TODO: добавить
-# Комплексные числа (complex)
-# В Python встроены также и комплексные числа:
-
-x = complex(1, 2)
-print(x)
-y = complex(3, 4)
-print(y)
-z = x + y
-print(x)
-print(z)
-z = x * y
-print(z)
-z = x / y
-print(z)
-print(x.conjugate())  # Сопряжённое число
-print(x.imag)  # Мнимая часть
-print(x.real)  # Действительная часть
-# print ( x > y ) # Комплексные числа нельзя сравнить
-print(x == y)  # Но можно проверить на равенство
-# abs ( 3 + 4 j ) # Модуль комплексного числа
-# pow ( 3 + 4 j , 2 ) # Возведение в степень
-
-# Также для работы с комплексными числами используется также модуль cmath .
