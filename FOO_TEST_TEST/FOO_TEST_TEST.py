700ee5c6400d4924085c6ebf91a68342737f7c89
Ilya Petrash <gil9red@gmail.com>
2014-12-03 00:13:58 +0500

    Обновлено.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -117,35 +117,6 @@ __author__ = 'ipetrash'
 # print(sum(l) / len(l))
 
 
-# ## Quality Control
-# # doctest
-# def average(values):
-#     """Computes the arithmetic mean of a list of numbers.
-#
-#     >>> print(average([20, 30, 70]))
-#     40.0
-#     """
-#     return sum(values) / len(values)
-#
-# import doctest
-# doctest.testmod()   # automatically validate the embedded tests
-#
-# # unittest
-# import unittest
-#
-# class TestStatisticalFunctions(unittest.TestCase):
-#
-#     def test_average(self):
-#         self.assertEqual(average([20, 30, 70]), 40.0)
-#         self.assertEqual(round(average([1, 5, 7]), 1), 4.3)
-#         with self.assertRaises(ZeroDivisionError):
-#             average([])
-#         with self.assertRaises(TypeError):
-#             average(20, 30, 70)
-#
-# unittest.main() # Calling from the command line invokes all tests
-
-
 # TODO: https://docs.python.org/3/tutorial/stdlib2.html
 # import textwrap
 # text = 'Придумать простое приложение и реализовать его с помощью TDD (используя unit-тесты)'
