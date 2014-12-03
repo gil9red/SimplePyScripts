02e5fe609956efe8ed2c6567a0931a04381139da
Ilya Petrash <gil9red@gmail.com>
2014-12-04 00:53:50 +0500

    Обновлено.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -64,6 +64,27 @@ __author__ = 'ipetrash'
 #     # image.save(path)
 
 
+# # https://docs.python.org/3.4/library/sqlite3.html
+# import sqlite3
+# conn = sqlite3.connect('example.db')
+#
+# c = conn.cursor()
+#
+# # Create table
+# c.execute('''CREATE TABLE stocks
+#              (date text, trans text, symbol text, qty real, price real)''')
+#
+# # Insert a row of data
+# c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+#
+# # Save (commit) the changes
+# conn.commit()
+#
+# # We can also close the connection if we are done with it.
+# # Just be sure any changes have been committed or they will be lost.
+# conn.close()
+#
+#
 # import sqlite3
 # # https://docs.python.org/3.4/library/sqlite3.html
 # path = r"C:\Users\ipetrash\Desktop\teller\kmaksimov_NDC.sdb"
