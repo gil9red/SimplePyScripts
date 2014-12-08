95b3d2dd0cf5dd1c462fc9c1e1004f88e5cac59c
Ilya Petrash <gil9red@gmail.com>
2014-12-09 00:13:34 +0500

    Обновлено.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- email/simple.py
+++ email/simple.py
@@ -10,37 +10,41 @@ __author__ = 'ipetrash'
 # https://docs.python.org/3.4/library/email-examples.html
 
 if __name__ == '__main__':
-    mail_sender = 'USERNAME@DOMAIN'
-    mail_passwd = 'PASSWORD'
+    mail_sender = 'USERNAME@DOMAIN'  # Например: vasyapupkin@mail.ru
+    mail_passwd = 'PASSWORD'  # Пароль к почте
 
-    smtp_server = 'YOUR.MAIL.SERVER'
+    smtp_server = 'YOUR.MAIL.SERVER'  # Например: smtp.mail.ru
     port = 587
 
     mail_text = 'Example!\nFirst!\nSecond!\n\nРаз!\nДва!\nТри!'
     mail_subject = 'Здарова чувак! Hello!!!'
     mail_from = mail_sender
-    mail_to = [mail_sender]
-    # Несколько получателей указываются через запятую:
-    # mail_to = [mail_sender, '*****@mail.com']
+    mail_to = [
+        mail_sender
+        # , '*****@mail.com',
+        # ...
+    ]
+    # mail_cc = [
+    # # '*****@mail.com',
+    #     # '*****@gmail.com',
+    # ...
+    # ]
 
     # Create a text/plain message
     msg = MIMEText(mail_text)
-    msg['Subject'] = mail_subject
     msg['From'] = mail_from
     msg['To'] = ', '.join(mail_to)
+    # msg['Cc'] = ', '.join(mail_cc)  # Получатели копии письма
+    msg['Subject'] = mail_subject
 
     try:
         # Send the message on SMTP server.
-        s = smtplib.SMTP(smtp_server, port)
-        s.starttls()
-        s.login(mail_sender, mail_passwd)
-
-        try:
+        with smtplib.SMTP(smtp_server, port) as s:
+            s.starttls()
+            s.login(mail_sender, mail_passwd)
             s.send_message(msg)
-        finally:
-            s.quit()
 
-        print('Email sent')
+            print('Email sent')
 
     except Exception as e:
         print('Error sending mail: ' + str(e))
