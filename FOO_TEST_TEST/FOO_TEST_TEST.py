30d02a9f50ef9a01527a4acc39d8e029680e77c9
Ilya Petrash <gil9red@gmail.com>
2014-12-13 00:07:35 +0500

    Обновлено.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -4,29 +4,6 @@
 __author__ = 'ipetrash'
 
 
-# # TODO: добавить примеры чтения и записи в файл
-# # https://docs.python.org/3.4/tutorial/inputoutput.html#reading-and-writing-files
-# # http://pythonworld.ru/tipy-dannyx-v-python/fajly-rabota-s-fajlami.html
-#
-# # Открыть файл в режиме записи
-# with open('foo.txt', mode='w') as f:
-#     f.write('123\n')
-#     f.write('one two\n')
-#     f.write('one two\n')
-#     f.write('раз два')
-#
-# # Открыть файл в режиме чтения
-# with open('foo.txt', mode='r') as f:
-#     print(f.read())
-#
-# print()
-#
-# # Открыть файл в режиме чтения и построчно считать файл
-# with open('foo.txt', mode='r') as f:
-#     for r in f:
-#         print(r, end='')
-
-
 # TODO: воспроизведение музыкальных файлов
 # # Window only
 # # https://docs.python.org/3/library/winsound.html
