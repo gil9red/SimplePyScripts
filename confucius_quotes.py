f623dc427d53edc94c4f50e414933b0f7e244b42
Ilya Petrash <gil9red@gmail.com>
2014-12-15 00:12:31 +0500

    Обновлено.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- confucius_quotes.py
+++ confucius_quotes.py
@@ -1,14 +1,16 @@
+from urllib.parse import quote
+from grab import Grab
+
+
 __author__ = 'ipetrash'
 
 # Скрипт качает цитаты Конфуция из викицитатника.
 
-from grab import Grab
-
 
 def get_confucius_quotes():
+    url = quote('http://ru.wikiquote.org/wiki/Конфуций', safe='/:')
+
     g = Grab()
-    # http://ru.wikiquote.org/wiki/Конфуций
-    url = "http://ru.wikiquote.org/wiki/%D0%9A%D0%BE%D0%BD%D1%84%D1%83%D1%86%D0%B8%D0%B9"
     g.go(url)
 
     quotes = list()
