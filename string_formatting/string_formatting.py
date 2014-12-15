ea50b402246c92810e1bf217feddd85875c987a8
Ilya Petrash <gil9red@users.noreply.github.com>
2014-12-15 12:29:57 +0500

    Update string_formatting.py

--- string_formatting/string_formatting.py
+++ string_formatting/string_formatting.py
@@ -4,4 +4,6 @@ __author__ = 'ipetrash'
 if __name__ == '__main__':
     # TODO: побольше примеров форматирования строк.
     # https://docs.python.org/3/library/string.html#string-formatting
+    # http://pythonworld.ru/osnovy/formatirovanie-strok-metod-format.html
+    # http://pythonworld.ru/osnovy/formatirovanie-strok-operator.html
     print('{one} * {one} {two}'.format(one="45", two="Bugaga"))
