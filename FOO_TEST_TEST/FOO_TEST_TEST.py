c4b4e02320e63d3a963d120be86d47c19c62b9c3
Ilya Petrash <gil9red@gmail.com>
2015-01-03 01:46:53 +0500

    Обновлено.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -1,12 +1,16 @@
+import requests
+
 __author__ = 'ipetrash'
 
 
 
 # TODO: vk.com api
+# TODO: создать модуль с примерами работы
 # http://habrahabr.ru/sandbox/84639/
 # https://github.com/python273/vk_api
 # https://github.com/dimka665/vk
 # https://pypi.python.org/pypi/vk/1.5
+# https://vk.com/dev/methods
 
 import vk_api
 import sys
@@ -22,6 +26,20 @@ if __name__ == '__main__':
         print(error_msg)  # В случае ошибки выведем сообщение
         sys.exit()
 
+
+    ## Получение инфы о пользователе
+    # Получение инфы о текущем пользователе
+    rs = vk.method('users.get')
+
+    # # Получение больше инфы о текущем пользователе
+    # rs = vk.method('users.get', {'fields': 'sex, city, online, screen_name'})
+
+    # # Получение инфы о пользователе creeddevilmaycry
+    # rs = vk.method('users.get', {'user_ids': 'creeddevilmaycry'})
+    print(rs)
+
+
+    ## Печатаем текст последнего поста со стены
     # values = {
     #     'count': 1  # Получаем только один пост
     # }
@@ -31,15 +49,44 @@ if __name__ == '__main__':
     #     # Печатаем текст последнего поста со стены
     #     print(response['items'][0]['text'])
 
-    # Получение аудизаписей текущего пользователя (чей логин был введен)
-    audio = vk.method('audio.get')
 
-    # # Получение аудизаписей пользователя c id=10
-    # audio = vk.method('audio.get', {'owner_id': '10'})
+    # # Получение аудизаписей текущего пользователя (чей логин был введен)
+    # audio = vk.method('audio.get')
+    #
+    # # # Получение аудизаписей пользователя c id=170968205
+    # # audio = vk.method('audio.get', {'owner_id': '170968205'})
+    #
+    # # Первая аудизапись пользователя
+    # audio = audio['items'][0]
+    # # print(audio)
+    # artist = audio['artist']
+    # title = audio['title']
+    # url = audio['url']
+    #
+    # audio_file_name = '%s - %s.mp3' % (artist, title)
+    #
+    # # Попытаемся скачать аудизапись
+    # r = requests.get(url, stream=True)
+    # if r.status_code == 200:
+    #     with open(audio_file_name, 'wb') as f:
+    #         for chunk in r.iter_content(1024):
+    #             f.write(chunk)
 
-    print('Count: {}'.format(audio['count']))
-    for i, a in enumerate(audio['items'], 1):
-        print(i, a['title'])
+
+    # Вывод списка всех аудизаписей
+    # print('Count: {}'.format(audio['count']))
+    # for i, a in enumerate(audio['items'], 1):
+    #     print(i, a)
+
+
+    # ## Написание сообщения на стену
+    # # Написание сообщения себе на стену:
+    # response = vk.method('wall.post', {'message': 'Hello World! Привет Мир!'})
+    # print(response)
+    #
+    # # Написание сообщения пользователю с id=170968205 на стену:
+    # response = vk.method('wall.post', {'owner_id': '170968205', 'message': 'Даров, чувак!'})
+    # print(response)
 
 
 
