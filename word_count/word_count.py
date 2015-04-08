24739be15bbe008f690e41bd845dc6c112676971
Ilya Petrash <ip1992@inbox.ru>
2015-04-08 13:20:18 +0500

    Update word_count.py

--- word_count/word_count.py
+++ word_count/word_count.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
 __author__ = 'ipetrash'
 
 
@@ -8,10 +12,27 @@ if __name__ == '__main__':
     import re
     from collections import Counter
 
-    text = ("Предоставляет методы для управления дата и значения времени, "
-            "связанных с файлом 11 11.")
+    text = """Конечно, там еще много работы. Нужно корректно переводить слова с несколькими значениями, улучшать перевод
+    разных форм глаголов, сделать настраиваемый уровень перевода для разных уровней владения английским, готовить
+    упражнения для запоминания новых слов. Этим я буду заниматься на досуге в ближайшие месяцы. """
+
+    # Ищем слова в кирилице
+    for i, match in enumerate(re.finditer(r'[а-яА-Я]+', text), 1):
+        print(i, match)
+
+    print()
+
+    # Разделяем текст по любым символам, кроме буквенного или цифрового символа или знака подчёркивания
+    found = re.split(r'\W+', text)
+    found = [c for c in found if c]
+    print(len(found), found)
+
+    print()
+
+    # Ищем буквенные или цифровые символы или знаки подчёркивания, имеющие границу
     words = re.findall(r"\b\w+\b", text)
-    print("Words: {}\nCount: {}".format(words, len(words)))
+    print("Words: %s\nCount: %s" % (words, len(words)))
+
     word_count = Counter(words)
     for word, c in word_count.items():
-        print("'{}': {}".format(word, c))
+        print("'%s': %s" % (word, c))
