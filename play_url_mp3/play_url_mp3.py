a89c450b64902cc71763293cfa326d26cb67c8a9
Ilya Petrash <ip1992@inbox.ru>
2015-10-23 01:59:39 +0500

    Супер простой скрипт для воспроизведения url-аудифайлов

---
+++ play_url_mp3/play_url_mp3.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from urllib.request import urlretrieve
+from urllib.parse import urlparse
+
+import os
+import sys
+
+from PySide.QtGui import *
+from PySide.phonon import Phonon
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    audio = Phonon.MediaObject()
+    audio.setCurrentSource(Phonon.MediaSource(''))
+    output = Phonon.AudioOutput(Phonon.MusicCategory)
+    Phonon.createPath(audio, output)
+
+    widget = QWidget()
+    widget.setWindowTitle('Simple url audio player')
+
+    line_edit_url = QLineEdit()
+    line_edit_url.setText('https://www.dropbox.com/sh/a4bwzhn0s584u73/AACgDRii1rtCmoL3mXEwlXDga/CallYourName.mp3?dl=1')
+
+
+    def play():
+        url = line_edit_url.text()
+
+        # Выдираем из url имя файла
+        file_name = os.path.split(urlparse(url).path)[-1]
+        if not os.path.exists(file_name):
+            urlretrieve(url, file_name)
+
+        audio.setCurrentSource(Phonon.MediaSource(file_name))
+        audio.play()
+
+
+    play_url = QPushButton('Play')
+    play_url.clicked.connect(play)
+
+    layout = QHBoxLayout()
+    layout.addWidget(QLabel('Url audio:'))
+    layout.addWidget(line_edit_url)
+    layout.addWidget(play_url)
+
+    widget.setLayout(layout)
+    widget.show()
+
+
+    sys.exit(app.exec_())
---
+++ play_url_mp3/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# A very simple setup script to create a single executable
+#
+# Run the build process by running the command 'python setup.py build'
+#
+# If everything works well you should find a subdirectory in the build
+# subdirectory that contains the files needed to run the script without Python
+
+from cx_Freeze import setup, Executable
+
+executables = [
+    Executable('play_url_mp3.py')
+]
+
+setup(name='play_url_mp3',
+      version='0.1',
+      description='play_url_mp3.py',
+      executables=executables
+      )
