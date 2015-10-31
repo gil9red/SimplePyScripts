9a7ca61f5aeb6637199221d7ca69e25115e6b5f1
Ilya Petrash <ip1992@inbox.ru>
2015-11-01 01:52:28 +0500

    Добавлен скрипт, который раз в сутки постит на стену vk цитаты башорга

---
+++ vk_api/vk_spam_wall/vk_spam_wall.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from datetime import datetime
+import json
+import lxml.html
+import time
+from urllib.parse import urljoin
+from random import randint
+import sys
+
+import requests
+import vk_api
+
+
+def bash_quote(url='http://bash.im/', count=1):
+    rs = requests.get(url)
+    html = lxml.html.fromstring(rs.text)
+    quotes = html.xpath('//div[@class="quote"]')
+
+    hrefs = list()
+    while count:
+        quote = quotes.pop(0)
+        quote_href = quote.xpath('div/a[@class="id"]')[0].attrib['href']
+        quote_href = urljoin(rs.url, quote_href)
+        hrefs.append(quote_href)
+        count -= 1
+
+    return hrefs
+
+
+def vk_auth(login, password):
+    vk = vk_api.VkApi(login, password)
+
+    try:
+        # Авторизируемся
+        vk.authorization()
+    except vk_api.AuthorizationError as error_msg:
+        print(error_msg)  # В случае ошибки выведем сообщение
+        sys.exit()
+
+    return vk
+
+
+def wall_post(vk, owner_id, quote_href):
+    # Добавление сообщения на  стену пользователя (owner_id это id пользователя)
+    # Если не указывать owner_id, сообщения себе на стену поместится
+    rs = vk.method('wall.post', {
+        'owner_id': int(owner_id) if owner_id else None,
+        'attachments': quote_href,
+    })
+    print('{}: post_id: {}, quote href: {}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                                                   rs['post_id'], quote_href))
+
+
+if __name__ == '__main__':
+    config = json.load(open('config.json'))
+
+    # Логин, пароль к аккаунту и id человека, на стену которого будем постить сообщения
+    login = config['login']
+    password = config['password']
+    owner_id = config['owner_id']
+    quote_count = config['quote_count']
+
+    # Авторизируемся
+    vk = vk_auth(login, password)
+
+    while True:
+        # Получаем 3 последние цитаты
+        for href in bash_quote(count=quote_count):
+            wall_post(vk, owner_id, href)
+
+            # Ждем от 2 до 5 минут + 0 до 60 секунд
+            interval = randint(2, 5) * 60 + randint(0, 60)
+            print('До следующего постинга осталось {} секунд.'.format(interval))
+            time.sleep(interval)
+
+        # Ждем от 24 часов до 28 часов
+        interval = randint(24 * 3600, 28 * 3600)
+        print('\nСледующий раз пошлем через {} минут.\n'.format(interval))
+
+        # В следующий раз п пошлем
+        time.sleep(interval)
