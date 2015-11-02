49f7f06e5c0b4efb8b9e63496c107a0daf388b79
ipetrash <gil9red@gmail.com>
2015-11-02 15:20:47 +0500

    -

--- Flask module/example.py
+++ Flask module/example.py
@@ -4,12 +4,9 @@
 __author__ = 'ipetrash'
 
 
-# TODO: Flask
-# "Мега-Учебник Flask, Часть 1: Привет, Мир!": http://habrahabr.ru/post/193242/
-# http://ru.wikibooks.org/wiki/Flask
-
 from flask import Flask
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
