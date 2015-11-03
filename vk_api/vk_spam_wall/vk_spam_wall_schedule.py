754a5e07461c7d57c886e725c752c2e94a155973
ipetrash <gil9red@gmail.com>
2015-11-03 14:32:24 +0500

    Добавлен vk_spam_wall_schedule.py -- аналог vk_spam_wall.py с отличием что реализован с помощью планировщика и время начала его работы можно настраивать, но нет рандома в интервалах между запусками задач

---
+++ vk_api/vk_spam_wall/vk_spam_wall_schedule.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from datetime import datetime, timedelta
+import json
+import lxml.html
+import logging
+import time
+from urllib.parse import urljoin
+from random import randint
+import sys
+
+import requests
+import vk_api
+import schedule
+
+
+def get_logger(name, file='log.txt', encoding='utf8'):
+    log = logging.getLogger(name)
+    log.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('[%(asctime)s] %(filename)s[LINE:%(lineno)d] %(levelname)-8s %(message)s')
+
+    fh = logging.FileHandler(file, encoding=encoding)
+    fh.setLevel(logging.DEBUG)
+
+    ch = logging.StreamHandler(stream=sys.stdout)
+    ch.setLevel(logging.DEBUG)
+
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+
+    log.addHandler(fh)
+    log.addHandler(ch)
+
+    return log
+
+
+logger = get_logger('vk_spam_wall')
+
+
+def bash_quote(url='http://bash.im/', count=1):
+    rs = requests.get(url)
+    html = lxml.html.fromstring(rs.text)
+    quotes = html.xpath('//div[@class="quote"]//a[@class="id"]')
+
+    hrefs = list()
+
+    for quote_href in quotes[:count]:
+        quote_href = quote_href.attrib['href']
+        quote_href = urljoin(rs.url, quote_href)
+        hrefs.append(quote_href)
+
+    return hrefs
+
+
+def vk_auth(login, password):
+    vk = vk_api.VkApi(login, password)
+
+    try:
+        logger.debug('Авторизуюсь в vk.')
+        vk.authorization()
+    except vk_api.AuthorizationError as e:
+        logger.error('При авторизации произошла ошибка: %s.', e)
+        quit()
+
+    logger.debug('Удачная авторизация.')
+
+    return vk
+
+
+def wall_post(vk, owner_id, quote_href):
+    logger.debug('Размещаю сообщение на стену.')
+
+    # Добавление сообщения на  стену пользователя (owner_id это id пользователя)
+    # Если не указывать owner_id, сообщения себе на стену поместится
+    rs = vk.method('wall.post', {
+        'owner_id': int(owner_id) if owner_id else None,
+        'attachments': quote_href,
+    })
+
+    logger.debug('post_id: %s, quote href: %s.', rs['post_id'], quote_href)
+
+
+if __name__ == '__main__':
+    logger.debug('Начало работы.')
+    logger.debug('Читаю файл конфига.')
+
+    config = json.load(open('config.json'))
+
+    # Логин, пароль к аккаунту и id человека, на стену которого будем постить сообщения
+    login = config['login']
+    password = config['password']
+    owner_id = config['owner_id']
+    quote_count = config['quote_count']
+    at = config['at']
+
+    logger.debug('Закончено чтение файла конфига. Конфиг: %s.', config)
+
+    if not login or not password:
+        logger.error('Логин/пароль не указаны.')
+        quit()
+
+    # Авторизируемся
+    vk = vk_auth(login, password)
+
+    str_at_time = at
+
+    try:
+        for href in bash_quote(count=quote_count):
+            logger.debug('Задача запланирована на %s.', str_at_time)
+            schedule.every().day.at(str_at_time).do(wall_post, vk=vk, owner_id=owner_id, quote_href=owner_id)
+
+            # Следующая задача выполнится чуть позже
+            at_time = time.strptime(str_at_time, '%H:%M')
+            at_time = timedelta(hours=at_time.tm_hour, minutes=at_time.tm_min)
+            at_time += timedelta(minutes=3)
+            str_at_time = (datetime.min + at_time).strftime('%H:%M')
+
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+
+    except Exception as e:
+        print('Что-то пошло не так :( -- "{}"'.format(e))
