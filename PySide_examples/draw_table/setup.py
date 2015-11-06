9bed97830e882a57657c7dfe3fa383da9f06db1b
ipetrash <gil9red@gmail.com>
2015-11-06 11:12:24 +0500

    draw_table.py перенесен в папку и дополнен README.md с setup.py

---
+++ PySide_examples/draw_table/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# A very simple setup script to create a single executable
+#
+# Run the build process by running the command 'python setup.py build'
+#
+# If everything works well you should find a subdirectory in the build
+# subdirectory that contains the files needed to run the script without Python
+
+from cx_Freeze import setup, Executable
+
+executables = [
+    Executable('draw_table.py')
+]
+
+setup(name='draw_table',
+      version='0.1',
+      description='draw_table',
+      executables=executables
+      )
