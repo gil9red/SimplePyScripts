ca07270d8b88090d14a7a3f610f0882027e24205
Ilya Petrash <ip1992@inbox.ru>
2015-11-30 23:02:07 +0500

    Рефакторинг

---
+++ play_audio/play.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) > 1:
+        import play_audio
+        play_audio.play(sys.argv[1])
+
+    else:
+        import os
+        file_name = os.path.basename(sys.argv[0])
+        print('usage: {} [-h] audio_file_name'.format(file_name))
---
+++ play_audio/play_audio.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import sys
+
+from PySide.QtCore import QCoreApplication, QEventLoop
+from PySide.phonon import Phonon
+
+
+try:
+    app = QCoreApplication(sys.argv)
+except RuntimeError:
+    app = QCoreApplication.instance()
+
+
+audio = Phonon.MediaObject()
+output = Phonon.AudioOutput(Phonon.MusicCategory)
+Phonon.createPath(audio, output)
+
+
+def play(file_name):
+    audio.setCurrentSource(Phonon.MediaSource(file_name))
+    audio.play()
+
+    loop = QEventLoop()
+    audio.finished.connect(loop.quit)
+    loop.exec_()
