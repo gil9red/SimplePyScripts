56b486a8d005723eecef89674303a9db2c6f2711
Ilya Petrash <ip1992@inbox.ru>
2015-11-30 23:21:10 +0500

    Added

---
+++ play_mp3_with_mp3play_module/play.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# https://pypi.python.org/pypi/mp3play/
+
+
+def play(filename):
+    import mp3play
+    clip = mp3play.load(filename)
+    clip.play()
+
+    import time
+    time.sleep(min(30, clip.seconds()))
+    clip.stop()
