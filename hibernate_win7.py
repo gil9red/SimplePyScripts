2e5b4caba09f52f1d47817f1ed253d0787a1d884
ipetrash <gil9red@gmail.com>
2015-12-16 20:19:43 +0500

    Возможность указать задержку в секундах перед уходом в спящий режим

--- hibernate_win7.py
+++ hibernate_win7.py
@@ -7,5 +7,15 @@ __author__ = 'ipetrash'
 """Скрипт отправляет win7 в режим гибернации (спящий режим)"""
  
 if __name__ == '__main__':
-   import os
-   os.system(r'%windir%\system32\rundll32.exe powrprof.dll,SetSuspendState Hibernate')
+    import sys
+    if len(sys.argv) > 1:
+        seconds_delay = sys.argv[1]
+        print('Delay before hibernate: {} secs'.format(seconds_delay))
+
+        import time
+        time.sleep(float(seconds_delay))
+
+    print('Go to Hibernate')
+
+    import os
+    os.system(r'%windir%\system32\rundll32.exe powrprof.dll,SetSuspendState Hibernate')
