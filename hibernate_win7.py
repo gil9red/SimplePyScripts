4a24484bbfd17ddd0003a95d1e87d973460d9631
ipetrash <gil9red@gmail.com>
2015-12-18 12:13:07 +0500

    Показывается обратный отсчет перед переходом в спящий режим

--- hibernate_win7.py
+++ hibernate_win7.py
@@ -7,15 +7,22 @@ __author__ = 'ipetrash'
 """Скрипт отправляет win7 в режим гибернации (спящий режим)"""
  
 if __name__ == '__main__':
-    import sys
-    if len(sys.argv) > 1:
-        seconds_delay = sys.argv[1]
-        print('Delay before hibernate: {} secs'.format(seconds_delay))
+    try:
+        import sys
+        if len(sys.argv) > 1:
+            seconds_delay = int(sys.argv[1])
 
-        import time
-        time.sleep(float(seconds_delay))
+            import time
 
-    print('Go to Hibernate')
+            while seconds_delay > 0:
+                print('\rDelay before hibernate: {} secs'.format(seconds_delay), end='')
+                time.sleep(1)
+                seconds_delay -= 1
 
-    import os
-    os.system(r'%windir%\system32\rundll32.exe powrprof.dll,SetSuspendState Hibernate')
+        print('\nGo to Hibernate')
+
+        import os
+        os.system(r'%windir%\system32\rundll32.exe powrprof.dll,SetSuspendState Hibernate')
+
+    except KeyboardInterrupt:
+        print('\nInterrupt')
