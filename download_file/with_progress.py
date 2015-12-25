afa76fac3efd3bbeffd356a24e6c33e9373738e7
ipetrash <gil9red@gmail.com>
2015-12-25 11:01:08 +0500

    download_file/with_progress.py

---
+++ download_file/with_progress.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import sys
+from urllib.request import urlretrieve
+
+
+def reporthook(blocknum, blocksize, totalsize):
+    readsofar = blocknum * blocksize
+    if totalsize > 0:
+        percent = readsofar * 1e2 / totalsize
+        if percent > 100:
+            percent = 100
+            readsofar = totalsize
+
+        s = "\r%5.1f%% %*d / %d" % (percent, len(str(totalsize)), readsofar, totalsize)
+        sys.stderr.write(s)
+        if readsofar >= totalsize: # near the end
+            sys.stderr.write("\n")
+
+    # total size is unknown
+    else:
+        sys.stderr.write("read %d\n" % (readsofar,))
+
+
+if __name__ == '__main__':
+    URL = 'https://github.com/gil9red/SimplePyScripts/archive/master.zip'
+
+    local_filename, headers = urlretrieve(URL, reporthook=reporthook)
+    print(local_filename + '\n' + headers)
