2dc7b420606a4220c9e31035c6aa627f7e6f57f5
ipetrash <gil9red@gmail.com>
2015-12-30 15:00:23 +0500

    Added darker_picture

---
+++ pil_example/darker_picture/darker_picture.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+if __name__ == '__main__':
+    from PIL import Image
+
+    image_file = "Collapse all.png"
+    img = Image.open(image_file).convert('RGBA')
+
+    pixdata = img.load()
+
+    # Делаем темнее картинку, игнорируя фон
+    for y in range(img.size[1]):
+        for x in range(img.size[0]):
+            if pixdata[x, y][:3] != (255, 255, 255):
+                pixdata[x, y] = tuple(map(lambda x: x - 50, pixdata[x, y][:3])) + pixdata[x, y][3:]
+
+    img.save("Collapse all black.png")
