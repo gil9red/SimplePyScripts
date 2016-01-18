ab47af9311dc3d10e3f888bbeecf7ae46bade223
ipetrash <gil9red@gmail.com>
2016-01-18 09:25:04 +0500

    added encode_all_algo.py

---
+++ Hash/encode_all_algo.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import hashlib
+
+if __name__ == '__main__':
+    available = list(set(map(str.lower, hashlib.algorithms_available)))
+    available.sort()
+
+    text = 'hello world'
+
+    for algo_name in available:
+        try:
+            alg = hashlib.new(algo_name)  # create hash function from name
+            alg.update(text.encode())  # set data in hash-function
+            print("{}: {}".format(algo_name, alg.hexdigest().upper()))
+
+        except ValueError:
+            pass
