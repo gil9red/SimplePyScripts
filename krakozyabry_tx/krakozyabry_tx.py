83b10a821b30783d40df2deeec33264b636d1af9
ipetrash <gil9red@gmail.com>
2016-01-19 15:49:02 +0500

    added krakozyabry_tx.py

---
+++ krakozyabry_tx/krakozyabry_tx.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+"""Пример конвертирования кракозябры "РќРµ СѓРєР°Р·Р°РЅР° С‚РѕС‡РєР°" в человеческий вид."""
+
+
+import sys
+
+from PySide.QtGui import QApplication, QMessageBox
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    QMessageBox.information(None, None, open('t', encoding='utf-8').read())
