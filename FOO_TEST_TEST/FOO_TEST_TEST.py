2fa0ac345320062ab02a9837fec85687934a3721
ipetrash <gil9red@gmail.com>
2016-02-05 14:36:07 +0500

    #

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -18,6 +18,10 @@ class FileSystemModel(QFileSystemModel):
 
 app = QApplication(sys.argv)
 
+
+# print('\n'.join(QDir(r'C:\Users\ipetrash').entryList()))
+# quit()
+
 # model = FileSystemModel()
 # model.setRootPath("")
 # tree = QTreeView()
@@ -63,71 +67,36 @@ logger = get_logger('dir_sizes')
 import time
 t = time.clock()
 
-# it = QDirIterator(r"C:\Users\ipetrash\Projects\SimplePyScripts",
-#                   '*.*',
-#                   QDir.AllEntries | QDir.NoDotAndDotDot,
-#                   QDirIterator.Subdirectories)
+
+def get_bytes(text, units='BKMGTPE'):
+    """Возвращает числовое значение в байтах разбирая строки вида: 1 GB, 50 MB и т.п."""
+
+    text = text.strip().replace(' ', '').replace(',', '.')
+
+    # For '54,7GB' -> num='54,7' and unit='G'
+    num, unit = float(text[:-2]), text[-2:][0]
+    assert len(unit) == 1, 'Len unit should == 1, example G, M. Unit = {}.'.format(unit)
+    assert unit in units, 'Unknows unit {}, possible: {}.'.format(unit, ', '.join(tuple(units)))
+
+    unit_pow = units.find(unit)
+    assert unit_pow > 0, 'Unit pow should > 0, unit_pow={} unit={}.'.format(unit_pow, unit)
+
+    return int(num * 1024 ** unit_pow)
 
 
 def pretty_file_size(n_size):
     i = 0
     size = n_size
 
-    while size > 1023:
+    while size >= 1024:
         size /= 1024
         i += 1
 
-    # return n_size, int(size), "BKMGT"[i]
-    return str(int(size)) + "BKMGT"[i]
+    return n_size, '{:.2f}'.format(size) + ' ' + "BKMGTPE"[i] + 'B'
 
 
-# def dir_size_bytes(dir_path):
-#     # it = QDirIterator(dir_path, '*.*', QDir.AllEntries | QDir.NoDotAndDotDot, QDirIterator.Subdirectories)
-#     it = QDirIterator(dir_path, '*.*', QDir.AllDirs | QDir.NoDotAndDotDot)
-#
-#     sizes = 0
-#
-#     while it.hasNext():
-#         file = QFileInfo(it.next())
-#
-#         size = 0
-#
-#         if file.isDir():
-#             size = dir_size_bytes(file.absoluteFilePath())
-#             sizes += size
-#         elif file.isFile():
-#             size = file.size()
-#             sizes += size
-#         else:
-#             sys.stderr.write(file.absoluteFilePath())
-#
-#         print(("DIR" if file.isDir() else "FILE") + ': ' + file.filePath(), pretty_file_size(size))
-#
-#     return sizes
-
-# def dir_size_bytes(dir_path, level=1):
-#     # print(dir_path)
-#
-#     it = QDirIterator(dir_path, '*.*', QDir.AllEntries | QDir.NoDotAndDotDot)
-#
-#     sizes = 0
-#
-#     while it.hasNext():
-#         file_name = it.next()
-#
-#         file = QFileInfo(file_name)
-#         # print(('DIR ' if file.isDir() else 'FILE ') + (' ' * 4 * level) + file_name)
-#         # if file.isFile():
-#         print(' ' * 4 * level + file_name[len(dir_path) + 1:], pretty_file_size(file.size()))
-#
-#         if file.isDir():
-#             dir_size_bytes(file_name, level + 1)
-#             # print()
-#
-#     return sizes
-
-def dir_size_bytes(dir_path, level=0, do_indent=True, size_less=1024*1024*1024):
-    it = QDirIterator(dir_path, '*.*', QDir.AllEntries | QDir.NoDotAndDotDot)
+def dir_size_bytes(dir_path, files=0, dirs=0, level=0, do_indent=True, size_less=get_bytes('1 GB')):
+    it = QDirIterator(dir_path, '*.*', QDir.AllEntries | QDir.NoDotAndDotDot | QDir.Hidden | QDir.System)
 
     sizes = 0
 
@@ -136,41 +105,29 @@ def dir_size_bytes(dir_path, level=0, do_indent=True, size_less=1024*1024*1024):
         file = QFileInfo(file_name)
 
         if file.isDir():
-            size = dir_size_bytes(file_name, level + 1, do_indent)
+            dirs += 1
+            size, files, dirs = dir_size_bytes(file_name, files, dirs, level + 1, do_indent, size_less)
         else:
+            files += 1
             size = file.size()
 
         sizes += size
 
     if sizes > size_less:
         logger.debug(
-            ((' ' * 4 * level) if do_indent else '') + dir_path + ' ' + pretty_file_size(sizes)
+            ((' ' * 4 * level) if do_indent else '')
+            + dir_path + ' ' + '{1} ({0} bytes)'.format(*pretty_file_size(sizes))
         )
 
-    return sizes
+    return sizes, files, dirs
 
 
-dir_name = r"C:\\"
-dir_size_bytes(dir_name, do_indent=False)
+dir_name = r"C:\Users\ipetrash"
 
-# sizes = 0
-#
-# while it.hasNext():
-#     file = QFileInfo(it.next())
-#     print(("DIR" if file.isDir() else "FILE") + ': ' + file.filePath(), pretty_file_size(file.size()))
-#
-#     # if file.isFile():
-#     sizes += file.size()
-#
-#     # file = it.next()
-#     # print(file, os.path.isdir(file), os.path.isfile(file))
-#
-#     # file = it.next()
-#     # print(file)
+sizes, files, dirs = dir_size_bytes(dir_name, do_indent=False, size_less=get_bytes('2,3 GB'))
 
-
-# print('sizes={}'.format(pretty_file_size(sizes)))
-logger.debug('\n{:.2f} sec'.format(time.clock() - t))
+logger.debug('\nsizes = {}, files = {}, dirs = {}'.format('{1} ({0} bytes)'.format(*pretty_file_size(sizes)), files, dirs))
+logger.debug('{:.2f} sec'.format(time.clock() - t))
 
 # sys.exit(app.exec_())
 
