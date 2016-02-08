86805e41af47b2922fd4b3aa5de28e8ad657fe03
ipetrash <gil9red@gmail.com>
2016-02-08 16:34:35 +0500

    added info_from_bitkurs.ru.py

---
+++ info_from_bitkurs.ru.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import re
+import grab
+
+
+URL = 'http://bitkurs.ru/'
+
+# Copy page yandex
+# URL = 'http://hghltd.yandex.net/yandbtm?fmode=inject&url=http%3A%2F%2Fbitkurs.ru%2F&tld=ru&lang=ru&la=1453455360&tm=1454924280&text=bitkurs.ru&l10n=ru&mime=html&sign=2105e628b413520a0e087ae4ac0db180&keyno=0'
+
+g = grab.Grab()
+g.setup(proxy='proxy.compassplus.ru:3128', proxy_type='http')
+g.go(URL)
+
+# <div class='currency_block'>
+# <span class="btc_c currencies">1 BTC =</span>
+# <span class="usd_c currencies">&#36;397.16<img src="/template/images/down.png"></span>
+# <span class="rub_c currencies">19 862.57 руб.<img src="/template/images/down.png">
+# </span><span class="eur_c currencies">366.51 &euro
+
+usd = g.doc.select('//span[@class="usd_c currencies"]').text()
+rub = g.doc.select('//span[@class="rub_c currencies"]').text()
+eur = g.doc.select('//span[@class="eur_c currencies"]').text()
+
+
+def get_amt(text):
+    # Удаление всех символов, кроме цифр и точки
+    text = re.sub('[^\\d\.]+', '', text)
+
+    # Вытаскивание строки, описывающей число
+    text = re.search('\\d+(\.\\d*)?', text).group()
+    return text
+
+usd = get_amt(usd)
+rub = get_amt(rub)
+eur = get_amt(eur)
+
+print('1 BTC:\n  {} USD\n  {} RUB\n  {} EUR'.format(usd, rub, eur))
