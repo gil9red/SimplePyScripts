b6d343facf818c015fb37b33fffbd4fe470acd3c
Ilya Petrash <ilya.petrash@inbox.ru>
2016-02-12 17:47:49 +0600

    added try_repeat.py

---
+++ Decorators/try_repeat.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# http://ru.stackoverflow.com/a/491681/201445
+
+def try_repeat(func):
+    def wrapper(*args, **kwargs):
+        count = 10
+
+        while count:
+            try:
+                return func(*args, **kwargs)
+            except Exception as e:
+                print('Error:', e)
+                count -= 1
+
+    return wrapper
+
+
+@try_repeat
+def exception_func():
+    import random
+    if random.randint(0, 1):
+        raise Exception('!!!')
+
+
+exception_func()
