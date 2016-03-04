a14294d15077382578d19bcbf3f82d21ab268927
ipetrash <gil9red@gmail.com>
2016-03-04 11:04:38 +0500

    Added pyqode_example/main.py

---
+++ pyqode_example/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+if __name__ == '__main__':
+    import sys
+    from PySide.QtGui import QApplication, QMainWindow
+
+    app = QApplication(sys.argv)
+
+    from pyqode.core import api
+    from pyqode.core import modes
+
+    editor = api.CodeEdit()
+
+    # Добавление модов: подсветка кода, подсветка текущей строки и
+    # добавление/удаление отступов используя Tab/Shift+Tab
+    editor.modes.append(modes.PygmentsSyntaxHighlighter(editor.document()))
+    editor.modes.append(modes.CaretLineHighlighterMode())
+    editor.modes.append(modes.IndenterMode())
+
+    editor.setPlainText(open(__file__, encoding='utf-8').read(), None, None)
+
+    mw = QMainWindow()
+    mw.setWindowTitle('pyqode')
+    mw.setCentralWidget(editor)
+    mw.show()
+
+    sys.exit(app.exec_())
