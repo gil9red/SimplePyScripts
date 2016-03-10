277b65fb485bcfc830a5253aa9185b0b33089d42
ipetrash <gil9red@gmail.com>
2016-03-10 19:18:56 +0500

    #

--- Flask module/example.py
+++ Flask module/example.py
@@ -4,17 +4,27 @@
 __author__ = 'ipetrash'
 
 
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    d = {
+        'a': [1, 2, 3],
+        'b': {
+            'b1': [1],
+            'b2': '2',
+            'b3': 3,
+        },
+        'c': "CCC",
+    }
+    return jsonify(**d)
+    # return "Hello World!"
 
 if __name__ == "__main__":
-    # # Localhost
-    # app.run()
+    # Localhost
+    app.run()
 
-    # Public IP
-    app.run(host='0.0.0.0')
+    # # Public IP
+    # app.run(host='0.0.0.0')
