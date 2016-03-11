e29311be73d5ef208007c6e7de86174dd756185e
Ilya Petrash <ilya.petrash@inbox.ru>
2016-03-12 01:46:54 +0600

    #

--- caesar_code/caesar_code.py
+++ caesar_code/caesar_code.py
@@ -19,9 +19,33 @@ def caesar_code(text, shift):
     return new_t
 
 
+from string import ascii_uppercase
+
+
+def shift(text, num):
+    shift_text = ''
+
+    for c in text:
+        if c not in ascii_uppercase:
+            shift_text += c
+            continue
+
+        i = (ascii_uppercase.index(c) + num) % len(ascii_uppercase)
+        shift_text += ascii_uppercase[i]
+
+    return shift_text
+
+
 if __name__ == '__main__':
     print(caesar_code("Hello World!", shift=2))
     print(caesar_code("Hello World!", shift=-4))
     print(caesar_code("Hello World!", shift=26))
     print(caesar_code("Hello World!", shift=50))
     print(caesar_code("Hello World!", shift=78))
+
+    print()
+
+    TEXT = 'VWDQ LV QRW ZKDW KH VHHPV'
+
+    for i in range(len(ascii_uppercase)):
+        print(i, shift(TEXT, i))
