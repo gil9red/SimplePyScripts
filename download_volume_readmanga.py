9b0425e7072fd678518f6b80cacec573be9ff1b4
ipetrash <gil9red@gmail.com>
2016-03-28 16:29:05 +0500

    #

--- download_volume_readmanga.py
+++ download_volume_readmanga.py
@@ -4,61 +4,68 @@
 __author__ = 'ipetrash'
 
 
-# Скрипт качает главу по адресу
+"""Скрипт для скачивания главы по указанному url."""
 
 
-from grab import Grab
 import re
-import requests
-from zipfile import ZipFile
 import os
+from urllib.parse import urljoin
+from urllib.request import urlretrieve
+from zipfile import ZipFile
+import traceback
+
+import requests
 
 
-def download_file(url, file_name):
-    # Попытаемся скачать файл
-    r = requests.get(url, stream=True)
-    if r.status_code == 200:
-        # Создаем файл и в него записываем файл с сервера
-        with open(file_name, 'wb') as f:
-            for chunk in r.iter_content(1024):
-                f.write(chunk)
-    else:
-        raise Exception('Ошибка при скачивании "{}": {} - {}'.format(url, r.status_code, r.reason))
+def get_url_images(url):
+    print('Start get_url_images with url:', url)
+
+    rs = requests.get(url)
+
+    re_expr = '\.init\(.*(\[\[.+\]\]).*\)'
+    match = re.search(re_expr, rs.text)
+    if match:
+        match = match.group(1)
+        print('Match:', match)
+
+        urls = eval(match)
+        print('After eval:', urls)
+
+        return [urljoin(i[1], i[0] + i[2]) for i in urls]
+
+    raise Exception('Не получилось из страницы вытащить список картинок главы. '
+                    'Используемое регулярное выражение: ', re_expr)
 
 
 if __name__ == '__main__':
     url = 'http://readmanga.me/one__piece/vol60/591'
 
-    g = Grab()
-    g.go(url)
+    try:
+        url = 'http://readmanga.me/one__piece/vol60/591'
+        urls = get_url_images(url)
+        print('Urls:', urls)
 
-    # Список картинок описан в переменной pictures
-    # Выковыриваем инициализацию переменной
-    m = re.search(r'var pictures = (.+?);', g.response.body)
-    url_images_str = m.group(1)
+        # Если список изображений не пустой
+        if urls:
+            print('Всего картинок:', len(urls))
 
-    # Выковыриваем ссылки на изображения
-    url_images = re.findall(r'https?.+?png', url_images_str)
-    print('Всего изображений:', len(url_images))
+            # Создаем архив, у которого именем будет номер главы
+            with ZipFile(os.path.basename(url) + '.zip', mode='w') as myzip:
+                for img_url in urls:
+                    # Вытаскиваем имя файла
+                    file_name = os.path.basename(img_url)
 
-    # Если список изображений не пустой
-    if url_images:
-        # Создаем архив, у которого именем будет номер главы
-        with ZipFile(os.path.basename(url) + '.zip', mode='w') as myzip:
-            for im in url_images:
-                # Вытаскиваем имя файла
-                file_name = os.path.basename(im)
+                    # Имя файла будет {номер_главы}_{номер_страницы}.png
+                    print(img_url, file_name)
 
-                # Имя файла будет {номер_главы}_{номер_страницы}.png
-                # Нам же нужен только {номер_страницы}.png его и вытаскиваем
-                file_name = file_name.split('_')[-1]
-                print(im, file_name)
+                    # Скачиваем файл
+                    urlretrieve(img_url, file_name)
 
-                # Скачиваем файл
-                download_file(im, file_name)
+                    # Помещаем в архив
+                    myzip.write(file_name)
 
-                # Помещаем в архив
-                myzip.write(file_name)
+                    # Удаляем файл
+                    os.remove(file_name)
 
-                # Удаляем файл
-                os.remove(file_name)
+    except Exception as e:
+        print('Error: {}\n\n{}'.format(e, traceback.format_exc()))
