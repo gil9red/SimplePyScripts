dbbefc7433bff205671512f4385973ac31ca0668
ipetrash <gil9red@gmail.com>
2016-04-04 16:04:56 +0500

    Added show_my_ip

---
+++ web_servers/show_my_ip/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from flask import Flask, request
+app = Flask(__name__)
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+
+@app.route("/")
+def index():
+    return """Your IPv4 Address Is: {}""".format(request.remote_addr)
+
+
+if __name__ == '__main__':
+    # # Localhost
+    # app.run(port=5001)
+
+    # Public IP
+    app.run(host='0.0.0.0')
