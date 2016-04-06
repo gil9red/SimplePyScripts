bd9fff545495540ba967d526eea2eacc1deee7d5
ipetrash <gil9red@gmail.com>
2016-04-06 15:05:08 +0500

    #

--- OpenSSL_example/p12_to_pem.py
+++ OpenSSL_example/p12_to_pem.py
@@ -4,16 +4,29 @@
 __author__ = 'ipetrash'
 
 
+import os.path
 from OpenSSL import crypto
 
-p12_file_name = ''
-p12_password = ''
 
-# May require "" for empty password depending on version
-p12 = crypto.load_pkcs12(open(p12_file_name, 'rb').read(), p12_password)
+def save_pem(p12_file_name, p12_password, pem_file_name=None):
+    """Функция из p12 вытаскивает pem. Если pem_file_name не указан,
+    сохраняется в той же папке, что и p12_file_name."""
 
-# PEM formatted private key
-print(crypto.dump_privatekey(crypto.FILETYPE_PEM, p12.get_privatekey()))
+    if pem_file_name is None:
+        pem_file_name = os.path.splitext(p12_file_name)[0] + '.pem'
 
-# PEM formatted certificate
-print(crypto.dump_certificate(crypto.FILETYPE_PEM, p12.get_certificate()))
+    # May require "" for empty password depending on version
+    p12 = crypto.load_pkcs12(open(p12_file_name, 'rb').read(), p12_password)
+
+    with open(pem_file_name, mode='wb') as f:
+        # PEM formatted private key
+        f.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, p12.get_privatekey()))
+
+        # PEM formatted certificate
+        f.write(crypto.dump_certificate(crypto.FILETYPE_PEM, p12.get_certificate()))
+
+
+if __name__ == '__main__':
+    p12_file_name = ''
+    p12_password = ''
+    save_pem(p12_file_name, p12_password)
