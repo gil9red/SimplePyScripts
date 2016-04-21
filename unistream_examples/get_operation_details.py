a6719b2c5194f202f069715de2949addc79c4dd6
ipetrash <gil9red@gmail.com>
2016-04-21 16:17:21 +0500

    Added get_operation_details.py -- Получение деталей

---
+++ unistream_examples/get_operation_details.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+"""
+Получение деталей.
+
+GET https://test.api.unistream.com/v1/operations/{id}/details.
+"""
+
+
+if __name__ == '__main__':
+    from utils import get_today_RFC1123_date, get_authorization_header
+    from config import APPLICATION_ID, SECRET, UNISTREAM_BANK_ID
+
+    OPERATION_GUID = '<OPERATION_GUID>'
+    URL = 'https://test.api.unistream.com/v1/operations/{}/details'.format(OPERATION_GUID)
+    TODAY_DATE = get_today_RFC1123_date()
+
+    headers = dict()
+    headers['Date'] = TODAY_DATE
+    headers['X-Unistream-Security-PosId'] = UNISTREAM_BANK_ID
+    headers['Authorization'] = get_authorization_header(APPLICATION_ID, SECRET, TODAY_DATE, URL, headers)
+
+    import requests
+    rs = requests.get(URL, headers=headers)
+    print(rs)
+    print(rs.text)
