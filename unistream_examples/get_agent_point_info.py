a97db1c0a553b48d4b9152b3e2a7dbd634faa593
ipetrash <gil9red@gmail.com>
2016-04-22 14:07:27 +0500

    Added get_agent_point_info.py

---
+++ unistream_examples/get_agent_point_info.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+"""
+Получение информации о точке предоставления услуги.
+
+GET https://test.api.unistream.com/v1/agents/{agentId}/poses/{posId}
+"""
+
+
+if __name__ == '__main__':
+    from utils import get_today_RFC1123_date, get_authorization_header
+    from config import APPLICATION_ID, SECRET
+
+    # Идентификатор партнера
+    AGENT_ID = -1
+
+    # Идентификатор точки предоставления услуги
+    POS_ID = -1
+
+    params = {
+        'agentId': AGENT_ID,
+        'posId': POS_ID,
+    }
+    URL = 'https://test.api.unistream.com/v1/agents/{agentId}/poses/{posId}'.format(**params)
+
+    TODAY_DATE = get_today_RFC1123_date()
+
+    headers = dict()
+    headers['Date'] = TODAY_DATE
+    headers['Authorization'] = get_authorization_header(APPLICATION_ID, SECRET, TODAY_DATE, URL, headers)
+
+    import requests
+    rs = requests.get(URL, headers=headers)
+    print(rs)
+    print(rs.text)
