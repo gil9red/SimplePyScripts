594d44362b360d9098f7b64dd30fb04cd9e4ff7c
ipetrash <gil9red@gmail.com>
2016-04-25 13:53:46 +0500

    #

--- read_docx/read_docx.py
+++ read_docx/read_docx.py
@@ -7,9 +7,13 @@ __author__ = 'ipetrash'
 from docx import Document
 document = Document("Обеденное меню 777.docx")
 
+# Регулярка для поиска последовательностей пробелов: от двух подряд и более
+import re
+multi_space_pattern = re.compile(r'[ ]{2,}')
+
 for table in document.tables:
     for row in table.rows:
-        name, weight, price = [i.text.strip() for i in row.cells]
+        name, weight, price = [multi_space_pattern.sub(' ', i.text.strip()) for i in row.cells]
         print('name: {}, weight: {}, price: {}'.format(name, weight, price))
 
     # Таблицы в меню дублируются
