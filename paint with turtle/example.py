7769be8f7b02d3216bf095fc0f1b0a6690fcc753
ipetrash <gil9red@gmail.com>
2016-04-26 12:28:53 +0500

    Added "paint with turtle/example.py"

---
+++ paint with turtle/example.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import turtle
+turtle.color('red', 'yellow')
+turtle.begin_fill()
+
+while True:
+    turtle.forward(200)
+    turtle.left(170)
+    if abs(turtle.pos()) < 1:
+        break
+
+turtle.end_fill()
+turtle.done()
