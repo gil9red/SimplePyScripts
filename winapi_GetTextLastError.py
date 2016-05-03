87e4abef384048f1cef7154cf751bfb28d71edc3
Ilya Petrash <ilya.petrash@inbox.ru>
2016-05-04 00:18:23 +0600

    added winapi_GetTextLastError.py

---
+++ winapi_GetTextLastError.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+def GetTextLastError(error_code=None):
+    """Функция возвращает описание текстовое описание ошибки."""
+
+    import ctypes
+    GetLastError = ctypes.windll.kernel32.GetLastError
+    FormatMessage = ctypes.windll.kernel32.FormatMessageA
+    LocalFree = ctypes.windll.kernel32.LocalFree
+
+    from win32con import (
+        FORMAT_MESSAGE_FROM_SYSTEM,
+        FORMAT_MESSAGE_ALLOCATE_BUFFER,
+        FORMAT_MESSAGE_IGNORE_INSERTS)
+
+    if error_code is None:
+        error_code = GetLastError()
+
+    message_buffer = ctypes.c_char_p()
+    FormatMessage(
+        FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_IGNORE_INSERTS,
+        None,
+        error_code,
+        0,
+        ctypes.byref(message_buffer),
+        0,
+        None
+    )
+
+    error_message = message_buffer.value
+    LocalFree(message_buffer)
+
+    return '{} - {}'.format(error_code, error_message.decode('cp1251').strip())
+
+
+if __name__ == '__main__':
+    print(GetTextLastError())
+    print(GetTextLastError(128))
