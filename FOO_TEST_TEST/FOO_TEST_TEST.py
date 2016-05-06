53e608a90e91399e9b8d50d4f7f58b647c40d020
ipetrash <gil9red@gmail.com>
2016-05-06 14:03:59 +0500

    ListView_*

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -4,6 +4,320 @@
 __author__ = 'ipetrash'
 
 
+# NOTE: set position icon
+# SOURCE: http://webcache.googleusercontent.com/search?q=cache:GoDfFADI1_oJ:systools.losthost.org/%3Fcode%3D9+&cd=15&hl=ru&ct=clnk&client=firefox-b-ab
+# SendMessage(lv, LVM_SETITEMPOSITION, i, MAKELONG(pt.x, pt.y));
+
+
+
+import ctypes
+
+
+def GetDesktopListViewHandle():
+    """
+    Функция возвращает указатель на ListView рабочего стола.
+
+    Оригинал:
+    function GetDesktopListViewHandle: THandle;
+        var
+            S: string;
+        begin
+            Result := FindWindow('ProgMan', nil);
+            Result := GetWindow(Result, GW_CHILD);
+            Result := GetWindow(Result, GW_CHILD);
+            SetLength(S, 40);
+            GetClassName(Result, PChar(S), 39);
+            if PChar(S) <> 'SysListView32' then
+                Result := 0;
+        end;
+
+    """
+
+    import ctypes
+    FindWindow = ctypes.windll.user32.FindWindowW
+    GetWindow = ctypes.windll.user32.GetWindow
+
+    def GetClassName(hwnd):
+        buff = ctypes.create_unicode_buffer(100)
+        ctypes.windll.user32.GetClassNameW(hwnd, buff, 99)
+        return buff.value
+
+    from win32con import GW_CHILD
+
+    # Ищем окно с классом "Progman" ("Program Manager")
+    hwnd = FindWindow('Progman', None)
+    hwnd = GetWindow(hwnd, GW_CHILD)  # SHELLDLL_DefView
+    hwnd = GetWindow(hwnd, GW_CHILD)  # SysListView32
+
+    if GetClassName(hwnd) != 'SysListView32':
+        return 0
+
+    return hwnd
+
+
+def ListView_GetItemCount(hwnd):
+    """
+
+    Функция возвращает количество элементов указанного ListView.
+
+    Оригинал:
+    define ListView_GetItemCount(hwnd) (int)SNDMSG((hwnd),LVM_GETITEMCOUNT,(WPARAM)0,(LPARAM)0)
+
+    """
+
+    import commctrl
+    import ctypes
+    SendMessage = ctypes.windll.user32.SendMessageW
+
+    return SendMessage(hwnd, commctrl.LVM_GETITEMCOUNT, 0, 0)
+
+
+class LVITEMW(ctypes.Structure):
+    _fields_ = [
+        ('mask', ctypes.c_uint32),
+        ('iItem', ctypes.c_int32),
+        ('iSubItem', ctypes.c_int32),
+        ('state', ctypes.c_uint32),
+        ('stateMask', ctypes.c_uint32),
+        ('pszText', ctypes.c_uint64),
+        ('cchTextMax', ctypes.c_int32),
+        ('iImage', ctypes.c_int32),
+        ('lParam', ctypes.c_uint64),  # On 32 bit should be c_long
+        ('iIndent', ctypes.c_int32),
+        ('iGroupId', ctypes.c_int32),
+        ('cColumns', ctypes.c_uint32),
+        ('puColumns', ctypes.c_uint64),
+        ('piColFmt', ctypes.c_int64),
+        ('iGroup', ctypes.c_int32),
+    ]
+
+
+# def get_desktop_icons_list():
+#     import struct
+#     import ctypes
+#     from commctrl import LVIF_TEXT, LVIF_IMAGE, LVM_GETITEMTEXT, LVM_GETITEMPOSITION, LVIR_BOUNDS, LVM_GETITEMRECT, LVM_GETITEMW
+#     from win32con import PROCESS_ALL_ACCESS, MEM_RESERVE, MEM_COMMIT, PAGE_READWRITE, MEM_RELEASE
+#     from ctypes.wintypes import POINT, RECT
+#
+#     GetWindowThreadProcessId = ctypes.windll.user32.GetWindowThreadProcessId
+#     SendMessage = ctypes.windll.user32.SendMessageW
+#     OpenProcess = ctypes.windll.kernel32.OpenProcess
+#     VirtualAllocEx = ctypes.windll.kernel32.VirtualAllocEx
+#     WriteProcessMemory = ctypes.windll.kernel32.WriteProcessMemory
+#     ReadProcessMemory = ctypes.windll.kernel32.ReadProcessMemory
+#     VirtualFreeEx = ctypes.windll.kernel32.VirtualFreeEx
+#     CloseHandle = ctypes.windll.kernel32.CloseHandle
+#
+#     MAX_LEN = 4096
+#
+#     icons_list = list()
+#
+#     try:
+#         hwnd = GetDesktopListViewHandle()
+#         pid = ctypes.create_string_buffer(4)
+#         p_pid = ctypes.addressof(pid)
+#         GetWindowThreadProcessId(hwnd, p_pid)
+#
+#         h_process = OpenProcess(PROCESS_ALL_ACCESS, False, struct.unpack("i", pid)[0])
+#         buffer_txt = VirtualAllocEx(h_process, 0, MAX_LEN, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE)
+#
+#         copied = ctypes.create_string_buffer(4)
+#         p_copied = ctypes.addressof(copied)
+#
+#         lvitem = LVITEMW()
+#         lvitem.mask = ctypes.c_uint32(LVIF_TEXT | LVIF_IMAGE)
+#         lvitem.iItem = ctypes.c_int32(0)
+#         lvitem.iSubItem = ctypes.c_int32(0)
+#         lvitem.pszText = ctypes.c_uint64(buffer_txt)
+#         lvitem.cchTextMax = ctypes.c_int32(MAX_LEN)
+#
+#         p_buffer_lvi = VirtualAllocEx(h_process, 0, MAX_LEN, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE)
+#         WriteProcessMemory(h_process, p_buffer_lvi, ctypes.addressof(lvitem), ctypes.sizeof(LVITEMW), p_copied)
+#
+#         p_buffer_point = VirtualAllocEx(h_process, 0, ctypes.sizeof(POINT), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE)
+#         p_buffer_rect = VirtualAllocEx(h_process, 0, ctypes.sizeof(RECT), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE)
+#
+#         num_items = ListView_GetItemCount(hwnd)
+#
+#         for i in range(num_items):
+#             # Get icon text
+#             SendMessage(hwnd, LVM_GETITEMTEXT, i, p_buffer_lvi)
+#             target_bufftxt = ctypes.create_string_buffer(MAX_LEN)
+#             ReadProcessMemory(h_process, buffer_txt, ctypes.addressof(target_bufftxt), MAX_LEN, p_copied)
+#             name = target_bufftxt.value.decode('cp1251')
+#             print(name)
+#
+#             lvitem.iItem = ctypes.c_int32(i)
+#             p_buffer_lvi = VirtualAllocEx(h_process, 0, MAX_LEN, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE)
+#             WriteProcessMemory(h_process, p_buffer_lvi, ctypes.addressof(lvitem), ctypes.sizeof(LVITEMW), p_copied)
+#
+#             SendMessage(hwnd, LVM_GETITEMW, 0, p_buffer_lvi)
+#             ReadProcessMemory(h_process, p_buffer_lvi, ctypes.addressof(lvitem), ctypes.sizeof(LVITEMW), p_copied)
+#             print(lvitem.iImage)
+#             # quit()
+#
+#             # Get icon position
+#             p = POINT()
+#             SendMessage(hwnd, LVM_GETITEMPOSITION, i, p_buffer_point)
+#             ReadProcessMemory(h_process, p_buffer_point, ctypes.addressof(p), ctypes.sizeof(POINT), p_copied)
+#
+#             rect = RECT()
+#             rect.left = LVIR_BOUNDS
+#
+#             SendMessage(hwnd, LVM_GETITEMRECT, i, p_buffer_rect)
+#             ReadProcessMemory(h_process, p_buffer_rect, ctypes.addressof(rect), ctypes.sizeof(RECT), p_copied)
+#
+#             print(name)
+#             # icons_list.append((i, name, p, rect))
+#
+#     finally:
+#         try:
+#             VirtualFreeEx(h_process, p_buffer_lvi, 0, MEM_RELEASE)
+#         except:
+#             pass
+#
+#         try:
+#             VirtualFreeEx(h_process, buffer_txt, 0, MEM_RELEASE)
+#         except:
+#             pass
+#
+#         try:
+#             VirtualFreeEx(h_process, p_buffer_point, 0, MEM_RELEASE)
+#         except:
+#             pass
+#
+#         try:
+#             VirtualFreeEx(h_process, p_buffer_rect, 0, MEM_RELEASE)
+#         except:
+#             pass
+#
+#         try:
+#             CloseHandle(h_process)
+#         except:
+#             pass
+#
+#     return icons_list
+
+
+def get_desktop_image_icon(index):
+    import struct
+    import ctypes
+    from commctrl import LVIF_IMAGE, LVM_GETITEMW
+    from win32con import PROCESS_ALL_ACCESS, MEM_RESERVE, MEM_COMMIT, PAGE_READWRITE, MEM_RELEASE
+
+    GetWindowThreadProcessId = ctypes.windll.user32.GetWindowThreadProcessId
+    SendMessage = ctypes.windll.user32.SendMessageW
+    OpenProcess = ctypes.windll.kernel32.OpenProcess
+    VirtualAllocEx = ctypes.windll.kernel32.VirtualAllocEx
+    WriteProcessMemory = ctypes.windll.kernel32.WriteProcessMemory
+    ReadProcessMemory = ctypes.windll.kernel32.ReadProcessMemory
+    VirtualFreeEx = ctypes.windll.kernel32.VirtualFreeEx
+    CloseHandle = ctypes.windll.kernel32.CloseHandle
+
+    MAX_LEN = 4096
+
+    try:
+        hwnd = GetDesktopListViewHandle()
+        pid = ctypes.create_string_buffer(4)
+        p_pid = ctypes.addressof(pid)
+        GetWindowThreadProcessId(hwnd, p_pid)
+
+        h_process = OpenProcess(PROCESS_ALL_ACCESS, False, struct.unpack("i", pid)[0])
+
+        copied = ctypes.create_string_buffer(4)
+        p_copied = ctypes.addressof(copied)
+
+        lvitem = LVITEMW()
+        lvitem.mask = ctypes.c_uint32(LVIF_IMAGE)
+        lvitem.iItem = ctypes.c_int32(index)
+        lvitem.iSubItem = ctypes.c_int32(0)
+
+        p_buffer_lvi = VirtualAllocEx(h_process, 0, MAX_LEN, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE)
+        WriteProcessMemory(h_process, p_buffer_lvi, ctypes.addressof(lvitem), ctypes.sizeof(LVITEMW), p_copied)
+
+        SendMessage(hwnd, LVM_GETITEMW, 0, p_buffer_lvi)
+        ReadProcessMemory(h_process, p_buffer_lvi, ctypes.addressof(lvitem), ctypes.sizeof(LVITEMW), p_copied)
+        return lvitem.iImage
+
+    finally:
+        try:
+            VirtualFreeEx(h_process, p_buffer_lvi, 0, MEM_RELEASE)
+        except:
+            pass
+
+        try:
+            CloseHandle(h_process)
+        except:
+            pass
+
+
+def ListView_GetImageList(hwnd, i_image_list):
+    """
+
+    Gets the handle to an image list used for drawing list-view items. You can use this macro or send the LVM_GETIMAGELIST message explicitly.
+
+    Original:
+    #define ListView_GetImageList(w,i) (HIMAGELIST)SNDMSG((w),LVM_GETIMAGELIST,(i),0)
+
+    :param hwnd:
+    :param i_image_list:
+    :return:
+    """
+
+    import ctypes
+    SendMessage = ctypes.windll.user32.SendMessageW
+
+    from commctrl import LVM_GETIMAGELIST
+
+    return SendMessage(hwnd, LVM_GETIMAGELIST, i_image_list, 0)
+
+
+print(get_desktop_image_icon(0))  # 11
+print(get_desktop_image_icon(1))  # 12
+print(get_desktop_image_icon(2))  # 13
+
+hwnd = GetDesktopListViewHandle()
+print(hwnd)  # 65784
+
+from commctrl import LVSIL_NORMAL, ILD_IMAGE
+h_il = ListView_GetImageList(hwnd, LVSIL_NORMAL)
+print(h_il)  # 860139888
+
+from win32gui import ImageList_GetIcon
+print(ImageList_GetIcon(h_il, 0, ILD_IMAGE))  # 0
+print(ImageList_GetIcon(h_il, 11, ILD_IMAGE))  # 0
+
+
+# typedef struct _IMAGEINFO {
+#   HBITMAP hbmImage;
+#   HBITMAP hbmMask;
+#   int     Unused1;
+#   int     Unused2;
+#   RECT    rcImage;
+# } IMAGEINFO, *LPIMAGEINFO;
+
+import ctypes.wintypes
+class IMAGEINFO(ctypes.Structure):
+    _fields_ = [
+        ('hbmImage', ctypes.wintypes.HBITMAP),
+        ('hbmMask', ctypes.wintypes.HBITMAP),
+        ('Unused1', ctypes.c_int),
+        ('Unused2', ctypes.c_int),
+        ('rcImage', ctypes.wintypes.RECT),
+    ]
+
+ImageList_GetImageInfo = ctypes.windll.comctl32.ImageList_GetImageInfo
+# BOOL ImageList_GetImageInfo(
+#    HIMAGELIST himl,
+#    int        i,
+#    IMAGEINFO  *pImageInfo
+# );
+
+pImageInfo = IMAGEINFO()
+print(ImageList_GetImageInfo(h_il, 0, ctypes.byref(pImageInfo)))  # 0
+print(ImageList_GetImageInfo(h_il, 11, ctypes.byref(pImageInfo)))  # 0
+
+
 # import sys
 # import random
 #
