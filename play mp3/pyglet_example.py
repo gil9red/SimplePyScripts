5c5f496910151db415278c528f6dc4d7d153148d
Ilya Petrash <ilya.petrash@inbox.ru>
2016-05-22 01:27:13 +0600

    #

--- play mp3/pyglet_example.py
+++ play mp3/pyglet_example.py
@@ -5,8 +5,8 @@ __author__ = 'ipetrash'
 
 
 import pyglet
-pyglet.lib.load_library('avbin')
+# pyglet.lib.load_library('avbin')
 
-song = pyglet.media.load(r'C:\Users\ipetrash\Music\Эпоха – Ценой великих жертв.mp3')
+song = pyglet.media.load(r'speak.mp3')
 song.play()
 pyglet.app.run()
