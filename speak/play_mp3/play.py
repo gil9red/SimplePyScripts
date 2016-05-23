eaff5f1bac7c1f38033e023ab0260a0971bde440
ipetrash <gil9red@gmail.com>
2016-05-23 14:02:11 +0500

    Добавлен модуль speak/play_mp3 для воспроизведения mp3

---
+++ speak/play_mp3/play.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+def play(file_name):
+    import pyglet
+    import os
+    dll_file_name = os.path.join(os.path.dirname(__file__), 'avbin')
+    pyglet.lib.load_library(dll_file_name)
+
+    player = pyglet.media.Player()
+    source = pyglet.media.load(file_name)
+    player.queue(source)
+
+    player.play()
+
+    def update(dt):
+        if not player.playing:
+            pyglet.app.exit()
+
+    # Every 500 ms / 0.5 sec
+    pyglet.clock.schedule_interval(update, 0.5)
+    pyglet.app.run()
+
+
+if __name__ == '__main__':
+    play('speak_male.mp3')
