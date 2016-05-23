4512a53a8a237bb217fcc0c74fa5827723c58eac
ipetrash <gil9red@gmail.com>
2016-05-23 16:26:19 +0500

    Добавление примера сборки exe из питона, используя pyinstaller

---
+++ build_exe/pyinstaller_example/gui/qt_pyside/build_exe.bat
@@ -0,0 +1,1 @@
+pyinstaller.exe --onefile --windowed main.py
---
+++ build_exe/pyinstaller_example/gui/qt_pyside/main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+if __name__ == '__main__':
+    import sys
+
+    from PySide.QtGui import *
+
+    # https://mborgerson.com/creating-an-executable-from-a-python-script
+
+    app = QApplication(sys.argv)
+
+    line_edit = QLineEdit('New line')
+    button = QPushButton('Add')
+    text_edit = QTextEdit()
+
+    def add_to_text():
+        text = line_edit.text()
+        text_edit.append(text)
+
+    button.clicked.connect(add_to_text)
+
+    layout = QVBoxLayout()
+    layout.addWidget(line_edit)
+    layout.addWidget(button)
+    layout.addWidget(text_edit)
+
+    w = QWidget()
+    w.setWindowTitle('Example')
+    w.setLayout(layout)
+    w.show()
+
+    app.exec_()
