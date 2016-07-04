77e5080616964b9332b9f5fa2c3d554e76818e35
ipetrash <gil9red@gmail.com>
2016-07-04 11:42:48 +0500

    #

---
+++ total_time_playlist_youtube/gui.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import sys
+
+from PySide.QtGui import *
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('total_time_playlist_youtube')
+
+        self.url_line_edit = QLineEdit()
+        self.url_line_edit.returnPressed.connect(self.go)
+
+        self.go_button = QPushButton('Go!')
+        self.go_button.clicked.connect(self.go)
+
+        self.result_label = QLabel()
+
+        layout = QHBoxLayout()
+        layout.addWidget(self.url_line_edit)
+        layout.addWidget(self.go_button)
+
+        main_layout = QVBoxLayout()
+        main_layout.addLayout(layout)
+        main_layout.addWidget(self.result_label)
+
+        central_widget = QWidget()
+        central_widget.setLayout(main_layout)
+        self.setCentralWidget(central_widget)
+
+    def go(self):
+        try:
+            from total_time_playlist_youtube import count_total_playlist_time
+            import config
+
+            url = self.url_line_edit.text()
+            total_seconds = count_total_playlist_time(url, config.proxy, config.proxy_type)
+            self.result_label.setText(str(total_seconds))
+
+        except Exception as e:
+            import traceback
+            text = str(e) + '\n\n' + traceback.format_exc()
+
+            self.result_label.setText(text)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    mw = MainWindow()
+    mw.show()
+
+    # Пусть хоть что-то будет по умолчанию
+    mw.url_line_edit.setText('https://www.youtube.com/playlist?list=PLvX4-HTvsLu-j-K9n14cV5OvxwBl_8Won')
+
+    app.exec_()
--- total_time_playlist_youtube/total_time_playlist_youtube.py
+++ total_time_playlist_youtube/total_time_playlist_youtube.py
@@ -4,46 +4,49 @@
 __author__ = 'ipetrash'
 
 
-# TODO: оформить
+# TODO: возможно, если роликов будет слишком много, не все вернутся из запроса
+def count_total_playlist_time(url, proxy=None, proxy_type='http'):
+    """Функция парсит страницу плейлиста и подсчитывает сумму продолжительности роликов."""
+
+    import grab
+    g = grab.Grab()
+    if proxy:
+        g.setup(proxy=proxy, proxy_type=proxy_type)
+
+    g.go(url)
+
+    video_list = g.doc.select('//*[@class="pl-video yt-uix-tile "]')
+    time_list = g.doc.select('//*[@class="timestamp"]')
+
+    total_seconds = 0
+
+    print('Playlist:')
+    for i, (video, time) in enumerate(zip(video_list, time_list), 1):
+        time_str = time.text()
+        print('{}. {} ({})'.format(i, video.attr('data-title'), time_str))
+
+        time_split = time_str.split(':')
+        if len(time_split) == 3:
+            h, m, s = map(int, time_split)
+            total_seconds += h * 60 * 60 + m * 60 + s
+        elif len(time_split) == 2:
+            m, s = map(int, time_split)
+            total_seconds += m * 60 + s
+        else:
+            total_seconds += int(time_split[0])
+
+    return total_seconds
+
+
 # TODO: gui
 
-url = 'https://www.youtube.com/playlist?list=PLqf5JRBicHXnV4fUNPJtE2YFAjPMHRX4K'
-# url = 'https://www.youtube.com/playlist?list=PLKom48yw6lJpyYN2Q_zmss68ntjzxxpHd'
-url = 'https://www.youtube.com/playlist?list=PLvX4-HTvsLu-j-K9n14cV5OvxwBl_8Won'
+if __name__ == '__main__':
+    url = 'https://www.youtube.com/playlist?list=PLqf5JRBicHXnV4fUNPJtE2YFAjPMHRX4K'
+    # url = 'https://www.youtube.com/playlist?list=PLKom48yw6lJpyYN2Q_zmss68ntjzxxpHd'
+    url = 'https://www.youtube.com/playlist?list=PLvX4-HTvsLu-j-K9n14cV5OvxwBl_8Won'
 
-import grab
+    import config
+    total_seconds = count_total_playlist_time(url, config.proxy, config.proxy_type)
 
-g = grab.Grab()
-g.go(url)
-
-# match_list = g.doc.select('//*[@class="pl-video yt-uix-tile "]')
-# for i, item in enumerate(match_list, 1):
-#     # TODO: Не самый удачный вариант, лучше от текущего элемента искать, или создать два запроса xpath --
-#     # для списка видео и для списка продолжительности видео и соединить их
-#     print(i, item.attr('data-title'), item.select('(//*[@class="timestamp"])[{}]'.format(i)).text())
-
-video_list = g.doc.select('//*[@class="pl-video yt-uix-tile "]')
-time_list = g.doc.select('//*[@class="timestamp"]')
-
-total_seconds = 0
-
-print('Playlist:')
-for i, (video, time) in enumerate(zip(video_list, time_list), 1):
-    time_str = time.text()
-    print('{}. {} ({})'.format(i, video.attr('data-title'), time_str))
-
-    time_split = time_str.split(':')
-    if len(time_split) == 3:
-        h, m, s = map(int, time_split)
-        total_seconds += h * 60 * 60 + m * 60 + s
-    elif len(time_split) == 2:
-        m, s = map(int, time_split)
-        total_seconds += m * 60 + s
-    else:
-        total_seconds += int(time_split[0])
-
-# h, m, s = total_seconds // 3600, total_seconds // 60 % 60, total_seconds % 60
-# print('\nTotal time: {}:{}:{} ({} total seconds).'.format(h, m, s, total_seconds))
-
-from datetime import timedelta
-print('\nTotal time: {} ({} total seconds).'.format(timedelta(seconds=total_seconds), total_seconds))
+    from datetime import timedelta
+    print('\nTotal time: {} ({} total seconds).'.format(timedelta(seconds=total_seconds), total_seconds))
