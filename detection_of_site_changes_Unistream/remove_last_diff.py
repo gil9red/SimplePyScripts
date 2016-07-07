299fdb1e7b2f927d17bfa5c21a006cf35b76b418
ipetrash <gil9red@gmail.com>
2016-07-07 10:36:11 +0500

    Добавлен remove_last_diff.py

---
+++ detection_of_site_changes_Unistream/remove_last_diff.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+"""Скрипт удаляет последнюю ревизию."""
+
+
+if __name__ == '__main__':
+    from main import session, TextRevision
+
+    text_revision = session.query(TextRevision).order_by(TextRevision.id.desc()).first()
+    print(text_revision)
+    session.delete(text_revision)
+    session.commit()
