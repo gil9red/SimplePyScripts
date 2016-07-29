0f09b15d7eb2769af5c73dbbdd748a5a915daeed
ipetrash <gil9red@gmail.com>
2016-07-29 15:36:06 +0500

    Добавление примера replace_on_upper.py

---
+++ regexp_examples/replace_on_upper.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+"""Скрипт, использую регулярные выражения, находит текст по шаблону и
+заменяет его на аналогичный, но в верхнем регистре.
+"""
+
+
+text = """
+<xs:schema elementFormDefault="qualified" xmlns:xs="http://www.w3.org/2001/XMLSchema">
+    <xs:element name="Root">
+        <xs:complexType>
+            <xs:sequence>
+                <xs:element name="string_1">
+                    <xs:complexType>
+                        <xs:sequence>
+                            <xs:element name="string_1_1" type="xs:string"/>
+                            <xs:element name="string_1_2" type="xs:string"/>
+                            <xs:element name="string_1_3" type="xs:string"/>
+                        </xs:sequence>
+                    </xs:complexType>
+                </xs:element>
+
+                <xs:element name="string" type="xs:string"/>
+                <xs:element name="normalizedString" type="xs:normalizedString"/>
+                <xs:element name="token" type="xs:token"/>
+                <xs:element name="base64Binary" type="xs:base64Binary"/>
+                <xs:element name="hexBinary" type="xs:hexBinary"/>
+            </xs:sequence>
+        </xs:complexType>
+    </xs:element>
+</xs:schema>
+"""
+
+import re
+text = re.sub('name="(.+?)"', lambda x: 'name="{}"'.format(x.group(1).upper()), text)
+print(text)
