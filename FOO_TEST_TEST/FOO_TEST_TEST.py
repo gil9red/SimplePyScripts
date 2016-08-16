a3fbf22e4deb878d71bf98dfa9e464cc5e83f2b0
Ilya Petrash <ilya.petrash@inbox.ru>
2016-08-17 04:25:39 +0600

    #

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -4,6 +4,41 @@
 __author__ = 'ipetrash'
 
 
+"""Advanced time-outs -- прогрессивные тайм-ауты
+В случае неудачной попытки время следующей попытки откладывается на некоторую увеличивающуюся величину.
+
+"""
+
+
+def work():
+    import random
+    if random.randint(0, 5):
+        raise Exception()
+
+    print('Work!')
+
+
+timeout = 0.5
+
+# Для обеспечения повторных попыток выполнения используется цикл
+while True:
+    try:
+        work()
+
+        # Если не было исключений, прерываем цикл
+        break
+
+    except:
+        print('Неудача с таймаутом {}.'.format(timeout))
+
+        import time
+        time.sleep(timeout)
+        timeout += 0.1
+
+
+quit()
+
+
 """Пример реализации алгоритма выделения предыдущей вкладки.
 
 """
