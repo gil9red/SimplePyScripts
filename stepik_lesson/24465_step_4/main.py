e0654c305fc63cc94d9a3bcc5232731f00000237
ipetrash <gil9red@gmail.com>
2016-09-01 19:49:24 +0500

    Add stepik_lesson/24465_step_4

---
+++ stepik_lesson/24465_step_4/main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+"""
+Вам дается текстовый файл, содержащий некоторое количество непустых строк.
+На основе него сгенерируйте новый текстовый файл, содержащий те же строки в обратном порядке.
+
+Пример входного файла:
+ab
+c
+dde
+ff
+
+Пример выходного файла:
+ff
+dde
+c
+ab
+"""
+
+
+if __name__ == '__main__':
+    with open('dataset_24465_4.txt') as f:
+        lines = reversed(f.readlines())
+
+        with open('reverse_dataset_24465_4.txt', 'w') as f:
+            for line in lines:
+                print(line, file=f, end='')
