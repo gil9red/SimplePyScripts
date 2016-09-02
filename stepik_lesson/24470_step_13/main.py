edfa816156159d7bff29bdecaf551b061c57b5d4
ipetrash <gil9red@gmail.com>
2016-09-02 19:26:17 +0500

    Add 24470_step_13

---
+++ stepik_lesson/24470_step_13/main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+"""
+Вам дана последовательность строк.
+В каждой строке замените первое вхождение слова, состоящего только из латинских букв "a" (регистр не важен),
+на слово "argh".
+
+Примечание:
+Обратите внимание на параметр count у функции sub﻿.
+
+Sample Input:
+There’ll be no more "Aaaaaaaaaaaaaaa"
+AaAaAaA AaAaAaA
+
+Sample Output:
+There’ll be no more "argh"
+argh AaAaAaA
+
+"""
+
+
+# Пример использования. В консоли:
+# > python main.py < in
+# There’ll be no more "argh"
+# argh AaAaAaA
+
+
+if __name__ == '__main__':
+    import sys
+    import re
+
+    for line in sys.stdin:
+        line = line.rstrip()
+
+        print(re.sub(r'\ba+\b', 'argh', line, count=1, flags=re.IGNORECASE))
