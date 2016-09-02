65e3b0bb7b63c8e67c38b8228b32aadcd5525af0
ipetrash <gil9red@gmail.com>
2016-09-02 19:32:25 +0500

    Add 24470_step_14

---
+++ stepik_lesson/24470_step_14/main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+"""
+Вам дана последовательность строк.
+В каждой строке поменяйте местами две первых буквы в каждом слове, состоящем хотя бы из двух букв.
+Буквой считается символ из группы \w﻿.
+
+Sample Input:
+this is a text
+"this' !is. ?n1ce,
+
+Sample Output:
+htis si a etxt
+"htis' !si. ?1nce,
+
+"""
+
+
+# Пример использования. В консоли:
+# > python main.py < in
+# htis si a etxt
+# "htis' !si. ?1nce,
+
+
+if __name__ == '__main__':
+    import sys
+    import re
+
+    for line in sys.stdin:
+        line = line.rstrip()
+
+        # Ищем слова с хотя бы двумя символами, запоминаем первый символ, второй и все остальные
+        # При замене сначала вставляем второй символ, потом первый, и потом все остальные
+        print(re.sub(r'\b(\w)(\w)(\w*?)\b', r'\2\1\3', line))
