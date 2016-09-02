5c2e2dd000277b2a22952f3912b07ccddb76bd03
ipetrash <gil9red@gmail.com>
2016-09-02 19:18:53 +0500

    Add 24470_step_11

---
+++ stepik_lesson/24470_step_11/main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+"""
+Вам дана последовательность строк.
+Выведите строки, содержащие слово, состоящее из двух одинаковых частей (тандемный повтор).
+
+Sample Input:
+blabla is a tandem repetition
+123123 is good too
+go go
+aaa
+
+Sample Output:
+blabla is a tandem repetition
+123123 is good too
+
+"""
+
+
+# Пример использования. В консоли:
+# > python main.py < in
+# 3
+
+
+if __name__ == '__main__':
+    import sys
+    import re
+
+    for line in sys.stdin:
+        line = line.rstrip()
+
+        # Находим слова, в которых находим две одинаковые последовательности
+        if re.search(r'\b(.+)\1\b', line):
+            print(line)
