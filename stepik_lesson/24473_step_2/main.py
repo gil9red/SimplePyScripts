33a316ca56535e50ad308ae8062c894ef9047f15
ipetrash <gil9red@gmail.com>
2016-09-05 19:06:47 +0500

    Add 24473_step_2

---
+++ stepik_lesson/24473_step_2/main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+"""
+Вам дана частичная выборка из датасета зафиксированных преступлений, совершенных в городе Чикаго с 2001 года
+по настоящее время.
+
+Одним из атрибутов преступления является его тип – Primary Type.
+
+Вам необходимо узнать тип преступления, которое было зафиксировано максимальное число раз в 2015 году.
+
+Файл с данными:
+Crimes.csv
+
+"""
+
+
+if __name__ == '__main__':
+    import csv
+    with open('Crimes.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        # Фильтр по 2015 году
+        select_by_2015_year = filter(lambda x: '2015' in x['Date'], reader)
+
+        # Список словарей с преступлениями делаем списком с типом преступлений и подсчитываем их количество
+        from collections import Counter
+        counter = Counter(x['Primary Type'] for x in select_by_2015_year)
+
+        # Сортировка по количеству преступлений
+        primary_counter = sorted(counter.items(), key=lambda x: x[1], reverse=True)
+
+        # Вытаскиваем самое частое преступление
+        print(*primary_counter[0])
