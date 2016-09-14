62bbe5c4839dedc1d6bb255e99a41e1fb1e7abc8
ipetrash <gil9red@gmail.com>
2016-09-14 13:34:30 +0500

    Move to https://github.com/gil9red/web_2048_bot

--- 2048_bot/build_pyqt4_ui.bat
+++
@@ -1,1 +0,0 @@
-pyuic4 -o mainwindow_ui.py mainwindow.ui
--- 2048_bot/build_pyside_ui.bat
+++
@@ -1,7 +0,0 @@
-pyside-uic -o mainwindow_ui.py mainwindow.ui
-:: pyside-uic -o settings_ui.py settings.ui
-:: pyside-uic -o pluginmanager_ui.py pluginmanager.ui
-
-:: pyside-rcc -o resource_rc.py resource.qrc
-:: Problem with qRegisterResourceData:
-:: https://chr0n0m3t3r.wordpress.com/2014/04/03/build-pyside-with-python-3-4/
