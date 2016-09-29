ede653d9d95f276f52fdd56c30aaf3959136244f
ipetrash <gil9red@gmail.com>
2016-09-29 12:26:39 +0500

    #

--- export_from_sqlite/main.py
+++ export_from_sqlite/main.py
@@ -43,29 +43,47 @@ def get_table_database_info(file_name, table_name=None):
 
     return table_name, columns, rows
 
+# TODO:
+DIR = 'exports'
+import os
+
 
 def save_to_csv(table_name, columns, rows):
+    file_name = table_name + '.csv'
+    if not os.path.exists(DIR):
+        os.mkdir(DIR)
+
+    file_name = os.path.join(DIR, file_name)
+
     import csv
-    with open(table_name + '.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    with open(file_name, 'w', newline='', encoding='utf-8') as csvfile:
         csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         csv_writer.writerow(columns)
         for row in rows:
             csv_writer.writerow(row)
 
 
-def save_to_json(table_name, columns, rows):
+def save_to_json(table_name, columns, rows, pretty=True):
+    file_name = table_name + ('_mini' if not pretty else '') + '.json'
+    if not os.path.exists(DIR):
+        os.mkdir(DIR)
+
+    file_name = os.path.join(DIR, file_name)
+
     from collections import OrderedDict
     import json
-    with open(table_name + '.json', 'w', encoding='utf-8') as f:
+    with open(file_name, 'w', encoding='utf-8') as f:
         new_rows = [OrderedDict(zip(columns, row)) for row in rows]
-        json.dump(new_rows, f, indent=4)
-
-    with open(table_name + '_mini.json', 'w', encoding='utf-8') as f:
-        new_rows = [OrderedDict(zip(columns, row)) for row in rows]
-        json.dump(new_rows, f)
+        json.dump(new_rows, f, indent=4 if pretty else None)
 
 
-def save_to_xml(table_name, columns, rows):
+def save_to_xml(table_name, columns, rows, pretty=True):
+    file_name = table_name + ('_mini' if not pretty else '') + '.xml'
+    if not os.path.exists(DIR):
+        os.mkdir(DIR)
+
+    file_name = os.path.join(DIR, file_name)
+
     from xml.dom.minidom import getDOMImplementation
     impl = getDOMImplementation()
     doc = impl.createDocument(None, table_name, None)
@@ -82,16 +100,17 @@ def save_to_xml(table_name, columns, rows):
 
             employee_element.appendChild(field_element)
 
-    with open(table_name + '.xml', 'w', encoding='utf-8') as f:
-        f.write(doc.toprettyxml())
+    with open(file_name, 'w', encoding='utf-8') as f:
+        f.write(doc.toprettyxml() if pretty else doc.toxml())
 
-    with open(table_name + '_mini.xml', 'w', encoding='utf-8') as f:
-        f.write(doc.toxml())
-s
 
 if __name__ == '__main__':
     table_name, columns, rows = get_table_database_info('database')
 
     save_to_csv(table_name, columns, rows)
+
     save_to_json(table_name, columns, rows)
+    save_to_json(table_name, columns, rows, pretty=False)
+
     save_to_xml(table_name, columns, rows)
+    save_to_xml(table_name, columns, rows, pretty=False)
