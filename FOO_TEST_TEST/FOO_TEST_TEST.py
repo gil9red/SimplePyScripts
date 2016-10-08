499e8591b2f60e3fdeccfd2221d59b464ac65235
Ilya Petrash <ilya.petrash@inbox.ru>
2016-10-08 22:06:11 +0500

    #

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -48,6 +48,56 @@ print(rs.text)
 
 quit()
 
+
+
+quit()
+
+
+"""Пример получения и сохранения стандартной иконки Qt в base64"""
+
+import traceback
+
+
+# Для отлова всех исключений, которые в слотах Qt могут "затеряться" и привести к тихому падению
+def log_uncaught_exceptions(ex_cls, ex, tb):
+    text = '{}: {}:\n'.format(ex_cls.__name__, ex)
+    text += ''.join(traceback.format_tb(tb))
+
+    print('Error: ', text)
+    QMessageBox.critical(None, 'Error', text)
+    quit()
+
+
+import sys
+sys.excepthook = log_uncaught_exceptions
+
+
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+app = QApplication([])
+
+style = QWidget().style()
+clear_icon = style.standardIcon(QStyle.SP_LineEditClearButton)
+print(clear_icon.availableSizes())
+
+for size in clear_icon.availableSizes():
+    pixmap = clear_icon.pixmap(size)
+
+    bytes = QByteArray()
+    buffer = QBuffer(bytes)
+    buffer.open(QIODevice.WriteOnly)
+    pixmap.save(buffer, "PNG")
+
+    import base64
+    print(size, base64.b64encode(bytes))
+
+
+quit()
+
+
+
 """Скрипт для эмуляции запроса поиска видео в vk"""
 
 
