4e8bd89c5c85d744787e328959532223f6899f00
ipetrash <gil9red@gmail.com>
2016-10-21 14:34:46 +0500

    Append cbr_ru.py

--- exchange_rates/cbr_ru.py
+++ exchange_rates/cbr_ru.py
@@ -4,21 +4,32 @@
 __author__ = 'ipetrash'
 
 
-# http://www.cbr.ru/scripts/XML_daily.asp
-# http://www.cbr.ru/scripts/XML_daily.asp?date_req=21.10.2016
+def exchange_rate(currency, date_req=None):
+    """Return exchange rate currency for rub.
 
-if __name__ == '__main__':
-    # from datetime import date
-    # date_req = date.today().strftime('%d.%m.%Y')
-    # url = 'http://www.cbr.ru/scripts/XML_daily.asp?date_req=' + date_req
-    url = 'http://www.cbr.ru/scripts/XML_daily.asp'
+    currency: alpha3code, example "USD"
+    date_req: date, example 21.10.2016
+
+    """
+
+    # Example:
+    # http://www.cbr.ru/scripts/XML_daily.asp
+    # http://www.cbr.ru/scripts/XML_daily.asp?date_req=21.10.2016
+
+    if date_req is None:
+        from datetime import date, timedelta
+        date_req = date.today()
+        date_req -= timedelta(days=1)
+        date_req = date_req.strftime('%d.%m.%Y')
+
+    url = 'http://www.cbr.ru/scripts/XML_daily.asp?date_req=' + date_req
+    # url = 'http://www.cbr.ru/scripts/XML_daily.asp'
 
     from urllib.request import urlopen
     with urlopen(url) as f:
         from lxml import etree
         root = etree.XML(f.read())
 
-        # 840 / 978
         # <ValCurs Date="21.10.2016" name="Foreign Currency Market">
         #     <Valute ID="R01010">
         #         <NumCode>036</NumCode>
@@ -27,10 +38,16 @@ if __name__ == '__main__':
         #         <Name>Австралийский доллар</Name>
         #         <Value>47,8382</Value>
         #     </Valute>
-        print(root.attrib['Date'])
+        #     ...
+
         for valute in root:
-            currency = valute.xpath('child::CharCode/text()')[0]
+            ccy = valute.xpath('child::CharCode/text()')[0]
             value = valute.xpath('child::Value/text()')[0]
-            print(currency)
-            if currency in ['840', '978']:
-                print(currency, value)
+
+            if currency == ccy:
+                return float(value.replace(',', '.'))
+
+
+if __name__ == '__main__':
+    print('USD:', exchange_rate('USD'))
+    print('EUR:', exchange_rate('EUR'))
