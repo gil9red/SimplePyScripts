856661a72442502652d421ea3846409bb55ca5d6
ipetrash <gil9red@gmail.com>
2016-10-28 12:52:46 +0500

    Append plot

--- cbr_ru/xml_metall/main.py
+++ cbr_ru/xml_metall/main.py
@@ -4,8 +4,46 @@
 __author__ = 'ipetrash'
 
 
+def gen_plot(dates, numbers, label=None, file_name='plot.png', show_plot=True):
+    import matplotlib.pyplot as plt
+
+    from matplotlib import rcParams
+    rcParams['font.family'] = 'Times New Roman', 'Arial', 'Tahoma'
+    rcParams['font.fantasy'] = 'Times New Roman'
+    rcParams['axes.labelsize'] = 'large'
+    rcParams['savefig.dpi'] = 200
+    rcParams["axes.grid"] = True
+
+    from matplotlib.dates import DateFormatter
+    x_axis_date_formatter = DateFormatter('%d/%m/%y')
+
+    # ax = plt.subplot(211)
+    ax = plt.subplot(111)
+    plt.plot(dates, numbers)
+    # plt.plot(dates, numbers, 'ro-')
+    # plt.fill_between(dates, numbers, color='r', alpha=0.7)
+
+    plt.xlabel(label)
+    ax.xaxis.set_major_formatter(x_axis_date_formatter)
+
+    # ax = plt.subplot(212)
+    # plt.plot(dates_list, number_users, 'go-')
+    # plt.fill_between(dates_list, number_users, color='g', alpha=0.7)
+    # plt.xlabel('Дата')
+    # plt.ylabel('Количество пользователей')
+    # ax.xaxis.set_major_formatter(x_axis_date_formatter)
+
+    plt.savefig(file_name)
+
+    if show_plot:
+        plt.show()
+
+    return file_name
+
+
 if __name__ == '__main__':
-    from datetime import date
+    from datetime import date, datetime
+
     date_req1 = '01.01.2000'
     date_req2 = date.today().strftime('%d.%m.%Y')
 
@@ -31,8 +69,20 @@ if __name__ == '__main__':
         # Code="4" -- Палладий
         records = root.find_all('Record', attrs=dict(Code="1"))
 
-        # for i, record in enumerate((records[0], records[-1]), 1):
-        for i, record in enumerate(records, 1):
+        dates = list()
+        prices = list()
+
+        for record in records:
+            date = datetime.strptime(record["Date"], '%d.%m.%Y').date()
+            price = float(record.findChild("Buy").text.replace(',', '.'))
+
+            dates.append(date)
+            prices.append(price)
+
+        # for i, record in enumerate(records, 1):
+        for i, record in enumerate((records[0], records[-1]), 1):
             # b'<Record Date="06.01.2000" Code="1"><Buy>231,94</Buy><Sell>246,67</Sell></Record>\n\n'
             # record["Code"]
             print('{}. {}: {}'.format(i, record["Date"], record.findChild("Buy").text))
+
+        gen_plot(dates, prices, "Стоимость грамма золота в рублях")
