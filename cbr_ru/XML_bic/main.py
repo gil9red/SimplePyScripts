0dcb62fd68c9231137e8426ee7ead7f930ae2be2
ipetrash <gil9red@gmail.com>
2016-10-28 15:11:23 +0500

    Append cbr_ru/XML_bic/main.py

---
+++ cbr_ru/XML_bic/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from urllib.request import urlopen
+from urllib.parse import quote
+
+query = 'сбер'.encode('windows-1251')
+url = 'http://www.cbr.ru/scripts/XML_bic.asp?name=' + quote(query)
+with urlopen(url) as f:
+    from bs4 import BeautifulSoup
+    root = BeautifulSoup(f.read(), "xml")
+
+    for i, record in enumerate(root.find_all('Record'), 1):
+        name = record.find('ShortName').text
+        bic = record.find('Bic').text
+
+        print('{}. "{}", {}, {}'.format(i, name, bic, record['DU']))
+
