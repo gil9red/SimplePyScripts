668018b2b4899058385a25bb85946b80ab2a6d7c
ipetrash <gil9red@gmail.com>
2016-10-28 19:00:57 +0500

    #

--- jazzcinema_ru/print_today_films.py
+++ jazzcinema_ru/print_today_films.py
@@ -16,25 +16,36 @@ if __name__ == '__main__':
     with urlopen(url) as f:
         root = BeautifulSoup(f.read(), 'lxml')
 
-        # Список расписаний, первый в списке -- текущий день
+        # Список расписаний
         schedule_list = root.select('.schedule')
 
-        schedule = schedule_list[0]
+        from datetime import datetime, date
+        today = date.today()
 
-        from datetime import datetime
-        schedule_date = datetime.strptime(schedule['rel'], 'calendar-%Y-%m-%d-schedule').strftime('%d/%m/%Y')
-        print('Расписание фильмов за {}:'.format(schedule_date))
+        today_found = False
 
-        # Получение фильмов в текущей вкладке (по идеи, текущая вкладка -- текущий день)
-        for border in schedule.select('.border'):
-            a = border.select_one('.movie .title > a')
-            url = urljoin(url, a['href'])
-            print(a['title'], url)
-            print('   ', border.select_one('.genre').text)
+        # Проходим по списку и ищем расписание на текущую дату
+        for schedule in schedule_list:
+            schedule_date = datetime.strptime(schedule['rel'], 'calendar-%Y-%m-%d-schedule').date()
+            schedule_date_str = schedule_date.strftime('%d/%m/%Y')
 
-            for seanse in border.select('.seanses'):
-                time = seanse.select_one('a').text
-                price = seanse.select_one('.price').text
-                print('    {} : {}'.format(time, price))
+            if schedule_date == today:
+                today_found = True
+                print('Расписание фильмов на сегодня {}:'.format(schedule_date_str))
 
-            print()
+                # Получение фильмов в текущей вкладке (по идеи, текущая вкладка -- текущий день)
+                for border in schedule.select('.border'):
+                    a = border.select_one('.movie .title > a')
+                    url = urljoin(url, a['href'])
+                    print(a['title'], url)
+                    print('   ', border.select_one('.genre').text)
+
+                    for seanse in border.select('.seanses'):
+                        time = seanse.select_one('a').text
+                        price = seanse.select_one('.price').text
+                        print('    {} : {}'.format(time, price))
+
+                    print()
+
+        if not today_found:
+            print("Фильмов на сегодня ({}) нет".format(today.strftime('%d/%m/%Y')))
