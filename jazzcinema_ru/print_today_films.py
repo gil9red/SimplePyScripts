eaac6a76df15ac5051085487a239df81df2d53b1
ipetrash <gil9red@gmail.com>
2016-10-28 18:39:48 +0500

    #

--- jazzcinema_ru/print_today_films.py
+++ jazzcinema_ru/print_today_films.py
@@ -14,10 +14,19 @@ if __name__ == '__main__':
     url = 'http://www.jazzcinema.ru/'
 
     with urlopen(url) as f:
-        root = BeautifulSoup(f.read())
+        root = BeautifulSoup(f.read(), 'lxml')
+
+        # Список расписаний, первый в списке -- текущий день
+        schedule_list = root.select('.schedule')
+
+        schedule = schedule_list[0]
+
+        from datetime import datetime
+        schedule_date = datetime.strptime(schedule['rel'], 'calendar-%Y-%m-%d-schedule').strftime('%d/%m/%Y')
+        print('Расписание фильмов за {}:'.format(schedule_date))
 
         # Получение фильмов в текущей вкладке (по идеи, текущая вкладка -- текущий день)
-        for border in root.select('.schedule.active .border'):
+        for border in schedule.select('.border'):
             a = border.select_one('.movie .title > a')
             url = urljoin(url, a['href'])
             print(a['title'], url)
