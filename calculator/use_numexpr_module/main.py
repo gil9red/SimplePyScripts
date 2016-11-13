e7bb7460605e6a19441321a30ce62e97e1b3ec4a
Ilya Petrash <ilya.petrash@inbox.ru>
2016-11-14 04:19:10 +0500

    #

---
+++ calculator/use_numexpr_module/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import numexpr as ne
+print(ne.evaluate('2 + 2 * 2'))
+print(ne.evaluate('10 ** 3'))
+print(ne.evaluate('2 ** 10'))
