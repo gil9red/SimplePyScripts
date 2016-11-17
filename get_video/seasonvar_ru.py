59cebdd141ec38ed7b70afb8283acad87fc03112
Ilya Petrash <ilya.petrash@inbox.ru>
2016-11-17 21:48:30 +0500

    #

---
+++ get_video/seasonvar_ru.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import re
+
+# Регулярка для поиска последовательностей пробелов: от двух подряд и более
+multi_space_pattern = re.compile(r'[ ]{2,}')
+
+# Регулярка для вытаскивания id сериала
+serial_id_from_url_pattern = re.compile(r'https?://seasonvar\.ru/serial-(\d+?)-')
+
+# Регулярка для вытаскивания id сериала
+secure_mark_pattern = re.compile(r'var secureMark = "(.*)";')
+
+
+def get_video_list_url_from_seasonvar_ru(url):
+    # url выглядят так: http://seasonvar.ru/serial-14590-Sofiya_Prekrasnaya-3-season.html
+    # serial_id = serial_id_from_url_pattern.search(url)
+    match = serial_id_from_url_pattern.search(url)
+    if match is None:
+        return
+
+    serial_id = match.group(1)
+
+    import requests
+    session = requests.session()
+
+    rs = session.get(url)
+    if not rs.ok:
+        return
+
+    match = secure_mark_pattern.search(rs.text)
+    if match is None:
+        return
+
+    secure_mark = match.group(1)
+
+    url_list_of_series = 'http://seasonvar.ru/playls2/' + secure_mark + 'x/trans/' + serial_id + '/list.xml'
+    rs = session.get(url_list_of_series)
+    if not rs.ok:
+        return
+
+    list_of_series = list()
+
+    # TODO: а разве бывают в seasonvar вложенные плейлисты?
+    # TODO: Обработать название серии в comment -- удалить указания вариантов качества видео SD/HD и т.п.
+    # указание перевода можно оставить, но нужно его также оформить
+    # справа переводчик может быть и не указан (пример: сериал покемоны)
+    for row in rs.json()['playlist']:
+        if 'file' in row:
+            list_of_series.append((row['comment'], row['file']))
+
+        elif 'playlist' in row:
+            for row2 in row['playlist']:
+                list_of_series.append((row2['comment'], row2['file']))
+
+    return list_of_series
+
+
+if __name__ == '__main__':
+    url = 'http://seasonvar.ru/serial-14590-Sofiya_Prekrasnaya-3-season.html'
+    print(get_video_list_url_from_seasonvar_ru(url))
+
+    url = 'http://seasonvar.ru/serial-4574-Gravity_Falls.html'
+    print(get_video_list_url_from_seasonvar_ru(url))
+
