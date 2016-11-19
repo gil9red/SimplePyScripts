03bfca8993770189a9b967c15aabdf96bc86f9ac
Ilya Petrash <ilya.petrash@inbox.ru>
2016-11-19 23:55:27 +0500

    Append loop_check_url_with_many_ports.py

---
+++ using_tor/loop_check_url_with_many_ports.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# NOTE: Для смены ip нужно настроить tor
+# Для этого создается torrc файл
+# И в него добавляется содержимое:
+# NewCircuitPeriod 5
+# MaxCircuitDirtiness 10
+
+# pip install -U requests[socks]
+import requests
+
+url = 'http://httpbin.org/ip'
+
+import sys
+import logging
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(logging.Formatter('[%(asctime)s] %(message)s'))
+log.addHandler(handler)
+
+while True:
+    for port in [9050, 9051, 9052, 9053]:
+        proxies = {
+            'http': 'socks5://localhost:{}'.format(port),
+            'https': 'socks5://localhost:{}'.format(port),
+        }
+
+        try:
+            log.debug('port: %s, %s', port, requests.get(url, proxies=proxies).json())
+        except requests.exceptions.ConnectionError:
+            log.debug('port: %s, %s', port, 'WARN: Not found Tor')
+
+    import time
+    time.sleep(1)
