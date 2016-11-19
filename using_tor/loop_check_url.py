b921508147341c2a2857e3d00a00b5eb664beb72
Ilya Petrash <ilya.petrash@inbox.ru>
2016-11-19 23:50:36 +0500

    Append loop_check_url.py

---
+++ using_tor/loop_check_url.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install -U requests[socks]
+import requests
+proxies = {
+    'http': 'socks5://localhost:9050',
+    'https': 'socks5://localhost:9050',
+}
+
+# NOTE: Для смены ip нужно настроить tor
+# Для этого создается torrc файл
+# И в него добавляется содержимое:
+# NewCircuitPeriod 5
+# MaxCircuitDirtiness 10
+
+url = 'http://httpbin.org/ip'
+
+import sys
+import logging
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(logging.Formatter('[%(asctime)s] %(message)s'))
+log.addHandler(handler)
+
+while True:
+    try:
+        log.debug(requests.get(url, proxies=proxies).json())
+    except requests.exceptions.ConnectionError:
+        log.debug('WARN: Not found Tor')
+
+    import time
+    time.sleep(1)
