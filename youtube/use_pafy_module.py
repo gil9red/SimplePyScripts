18c8c2313d4bdf51b9c8d3b72dbe5a450cd292d5
Ilya Petrash <ilya.petrash@inbox.ru>
2016-11-26 21:43:52 +0500

    #

---
+++ youtube/use_pafy_module.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# http://np1.github.io/pafy/
+# https://github.com/mps-youtube/pafy
+import pafy
+video = pafy.new('https://www.youtube.com/watch?v=lz4r-TtIOjA')
+
+print(video.title)
+print(video.rating)
+print(video.viewcount, video.author, video.length)
+print(video.duration, video.likes, video.dislikes)
+
+streams = video.streams
+for s in streams:
+    print(s, s.resolution, s.extension, s.get_filesize(), s.url)
