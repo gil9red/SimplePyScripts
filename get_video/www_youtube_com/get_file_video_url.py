cbec3cfa260e93c6346a3254ad003a406f44745e
Ilya Petrash <ilya.petrash@inbox.ru>
2016-11-26 21:54:55 +0500

    Append get_video/www_youtube_com/get_file_video_url.py

---
+++ get_video/www_youtube_com/get_file_video_url.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+def get_video_file_urls(url):
+    # http://np1.github.io/pafy/
+    # https://github.com/mps-youtube/pafy
+    import pafy
+    video = pafy.new(url)
+
+    file_url_list = list()
+
+    for s in video.streams:
+        file_url_list.append({
+            'url': s.url,
+            'resolution': s.resolution,
+            'extension': s.extension,
+        })
+
+    return file_url_list
+
+
+if __name__ == '__main__':
+    url = 'https://www.youtube.com/watch?v=lz4r-TtIOjA'
+    print(get_video_file_urls(url))
+
+    url = 'https://www.youtube.com/watch?v=bMt47wvK6u0'
+    print(get_video_file_urls(url))
