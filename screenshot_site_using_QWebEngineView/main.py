d77fed1841416bc755ff0a35b42174aa1ad42c38
Ilya Petrash <ilya.petrash@inbox.ru>
2016-12-17 23:52:11 +0500

    Append screenshot_site_using_QWebEngineView

---
+++ screenshot_site_using_QWebEngineView/main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+
+
+app = QApplication([])
+
+view = QWebEngineView()
+view.show()
+
+url = 'http://gama-gama.ru/search/?searchField=titan'
+view.load(QUrl(url))
+
+view.loadFinished.connect(lambda x: view.grab().save('img.jpg'))
+
+app.exec()
