e8fa3410e846db5fb153601cae33c0b2b3fb0daf
ipetrash <gil9red@gmail.com>
2016-12-19 14:22:57 +0500

    Append Роскомнадзор запретил букву А.py

---
+++ Роскомнадзор запретил букву А.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+alp = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+result = "Роскомнадзор запретил букву "
+
+# Перебор всех символов алфавита
+for c in alp:
+    # Проверяем, что в строке result текущая буква не найдена
+    if c in result or c.upper() in result:
+        # Выводим надпись
+        print(result + c.upper())
+
+        # Удаляем букву из надписи
+        result = result.replace(c, '')
+        result = result.replace(c.upper(), '')
