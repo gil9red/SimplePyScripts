4924e22ac20416f891e159a78916cce54b3e51a5
ipetrash <gil9red@gmail.com>
2016-12-19 14:11:54 +0500

    Append standart_qt_icon_to_base64.py

---
+++ standart_qt_icon_to_base64.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+"""Пример получения и сохранения стандартной иконки Qt в base64.
+
+Немного изменив, можно и сохранять их.
+
+"""
+
+
+import traceback
+
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+
+# Для отлова всех исключений, которые в слотах Qt могут "затеряться" и привести к тихому падению
+def log_uncaught_exceptions(ex_cls, ex, tb):
+    text = '{}: {}:\n'.format(ex_cls.__name__, ex)
+    text += ''.join(traceback.format_tb(tb))
+
+    print('Error: ', text)
+    QMessageBox.critical(None, 'Error', text)
+    quit()
+
+
+import sys
+sys.excepthook = log_uncaught_exceptions
+
+
+app = QApplication([])
+
+style = QWidget().style()
+clear_icon = style.standardIcon(QStyle.SP_LineEditClearButton)
+
+print(clear_icon.availableSizes())
+
+for size in clear_icon.availableSizes():
+    pixmap = clear_icon.pixmap(size)
+
+    bytes = QByteArray()
+    buffer = QBuffer(bytes)
+    buffer.open(QIODevice.WriteOnly)
+    pixmap.save(buffer, "PNG")
+
+    import base64
+    print(size, base64.b64encode(bytes))
