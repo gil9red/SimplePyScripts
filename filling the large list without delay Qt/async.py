75130d26937dcc82337c91cd3be2e93cab7410e7
ipetrash <gil9red@gmail.com>
2016-12-20 15:37:39 +0500

    Append "filling the large list without delay Qt"

---
+++ filling the large list without delay Qt/async.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+"""
+Заполнение списка из функции без фриза окна. Вариант с QThread.
+
+"""
+
+
+from PyQt5.QtWidgets import QWidget, QListWidget, QApplication, QVBoxLayout
+from PyQt5.QtCore import QThread, pyqtSignal
+
+
+def log_uncaught_exceptions(ex_cls, ex, tb):
+    text = '{}: {}:\n'.format(ex_cls.__name__, ex)
+
+    import traceback
+    text += ''.join(traceback.format_tb(tb))
+
+    print('Error: ', text)
+
+import sys
+sys.excepthook = log_uncaught_exceptions
+
+
+class MyThread(QThread):
+    about_new_value = pyqtSignal(int)
+
+    def __init__(self):
+        super().__init__()
+
+        self.executed = False
+
+    def run(self):
+        print('start thread')
+
+        try:
+            for i in range(1000000):
+                if not self.executed:
+                    break
+
+                self.about_new_value.emit(i)
+                print(i)
+
+                # Задержка в 5 миллисекунд
+                import time
+                time.sleep(0.005)
+
+        finally:
+            print('finish thread')
+
+    def start(self, priority=QThread.InheritPriority):
+        self.executed = True
+
+        return super().start(priority)
+
+    def exit(self, returnCode=0):
+        self.executed = False
+
+        return super().exit(returnCode)
+
+
+class Widget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.lw = QListWidget()
+
+        layout = QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(self.lw)
+
+        self.setLayout(layout)
+
+        self.thread = MyThread()
+        self.thread.about_new_value.connect(lambda x: self.append_item(str(x)))
+
+    def append_item(self, item):
+        self.lw.addItem(item)
+        self.lw.scrollToBottom()
+
+    def fill(self):
+        self.thread.start()
+
+    def closeEvent(self, event):
+        # После закрытия окна приложение не завершится пока список работает
+        quit()
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    w = Widget()
+    w.show()
+    w.fill()
+
+    app.exec_()
+
---
+++ filling the large list without delay Qt/sync.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+"""
+Заполнение списка из функции без фриза окна. Вариант с QApplication.processEvents.
+
+"""
+
+
+from PyQt5.QtWidgets import QWidget, QListWidget, QApplication, QVBoxLayout
+
+
+def log_uncaught_exceptions(ex_cls, ex, tb):
+    text = '{}: {}:\n'.format(ex_cls.__name__, ex)
+
+    import traceback
+    text += ''.join(traceback.format_tb(tb))
+
+    print('Error: ', text)
+
+import sys
+sys.excepthook = log_uncaught_exceptions
+
+
+def generator_large_list():
+    for i in range(1000000):
+        QApplication.processEvents()
+
+        yield i
+
+
+class Widget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.lw = QListWidget()
+
+        layout = QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(self.lw)
+
+        self.setLayout(layout)
+
+    def fill(self):
+        for i in generator_large_list():
+            self.lw.addItem(str(i))
+            self.lw.scrollToBottom()
+
+    def closeEvent(self, event):
+        # После закрытия окна приложение не завершится пока список работает
+        quit()
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    w = Widget()
+    w.show()
+    w.fill()
+
+    app.exec_()
