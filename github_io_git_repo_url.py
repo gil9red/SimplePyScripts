00bf6350485731aa93429c4e7de28e82335d4b27
ipetrash <gil9red@gmail.com>
2016-12-20 15:59:01 +0500

    Append github_io_git_repo_url.py

---
+++ github_io_git_repo_url.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# Convertor githun pages url to github repo url
+# http://nemilya.github.io/coffeescript-game-life/html/game.html
+# https://github.com/nemilya/coffeescript-game-life
+
+def github_io_git_repo_url(github_io_url):
+    import re
+    match = re.search('https?://(.+)\.github.io/(.+)', github_io_url)
+    if match is not None:
+        user = match.group(1)
+        repo = match.group(2).split('/')[0]
+
+        return 'https://github.com/{}/{}'.format(user, repo)
+
+
+if __name__ == '__main__':
+    url = 'http://nemilya.github.io/coffeescript-game-life/html/game.html'
+    print(github_io_git_repo_url(url))
