86394c9765e16078f23a1ece353805c0825d6dfc
ipetrash <gil9red@gmail.com>
2016-12-20 19:47:35 +0500

    Append "draw watermark/main.py"

---
+++ pil_example/draw watermark/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+"""
+Overlay "watermark" image / Наложение "водяного знака" на изображение
+
+"""
+
+
+from PIL import Image, ImageDraw, ImageFont
+
+
+file_name = r"../TowerOfGod_s2_ch100_p02_SIU.png_res.jpg"
+image = Image.open(file_name)
+width, height = image.size
+
+drawer = ImageDraw.Draw(image)
+font = ImageFont.truetype("arial.ttf", 25)
+text = "Hello World!"
+width_text, height_text = font.getsize(text)
+
+for i in range(0, width, width_text * 2):
+    for j in range(0, height, height_text * 2):
+        drawer.text((i, j), text, font=font, fill=(0x00, 0xff, 0x00))
+
+image.show()
