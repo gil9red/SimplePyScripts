180670065e3166c2d86b30c9d9ae25b8ad95c63f
Ilya Petrash <ilya.petrash@inbox.ru>
2016-12-21 22:39:01 +0500

    Append download_and_show_async_image_on_QGraphicsScene_QNetworkAccessManager.py

---
+++ download_and_show_async_image_on_QGraphicsScene_QNetworkAccessManager.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import QUrl
+from PyQt5.QtNetwork import QNetworkAccessManager, QNetworkRequest
+from PyQt5.QtWidgets import (
+    QApplication, QWidget, QVBoxLayout, QGraphicsScene, QGraphicsView, QGraphicsPixmapItem, QGraphicsTextItem
+)
+
+
+class Widget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.scene = QGraphicsScene()
+        self.scene.addItem(QGraphicsTextItem('loading...'))
+
+        self.view = QGraphicsView()
+        self.view.setScene(self.scene)
+
+        self.nam = QNetworkAccessManager()
+        self.nam.finished.connect(self.finish_request)
+
+        url = "http://pp.vk.me/c627626/v627626428/be07/wbpWha0RqZ4.jpg"
+        self.nam.get(QNetworkRequest(QUrl(url)))
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.view)
+
+        self.setLayout(layout)
+
+    def finish_request(self, reply):
+        img = QPixmap()
+        img.loadFromData(reply.readAll())
+
+        item = QGraphicsPixmapItem(img)
+        self.scene.clear()
+        self.scene.addItem(item)
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    w = Widget()
+    w.show()
+
+    app.exec()
