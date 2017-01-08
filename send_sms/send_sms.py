7460214695b73b8ee158f40fb26b90f564e15e20
Ilya Petrash <ilya.petrash@inbox.ru>
2017-01-08 15:05:59 +0500

    #

--- send_sms/send_sms.py
+++ send_sms/send_sms.py
@@ -7,13 +7,17 @@ __author__ = 'ipetrash'
 """Скрипт, используя сервис http://sms.ru, отправляет смс."""
 
 
-api_id = "<api_id>"
-to = "<to>"
-text = "<text>"
+# Описание API: https://mts.sms.ru/?panel=api&subpanel=method&show=sms/send
 
-url_pattern = 'http://sms.ru/sms/send?api_id={api_id}&to={to}&text={text}'
 
-import requests
-rs = requests.get(url_pattern.format(api_id=api_id, to=to, text=text))
-print(rs)
-print(rs.text)
+if __name__ == '__main__':
+    api_id = "<api_id>"
+    to = "<to>"
+    text = "<text>"
+
+    url_pattern = 'http://sms.ru/sms/send?api_id={api_id}&to={to}&text={text}'
+
+    import requests
+    rs = requests.get(url_pattern.format(api_id=api_id, to=to, text=text))
+    print(rs)
+    print(rs.text)
