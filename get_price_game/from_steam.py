d9acb9f10e2f7e2eba6b1372cdbaf9356fb7b852
Ilya Petrash <ilya.petrash@inbox.ru>
2017-01-10 01:19:45 +0500

    #

--- get_price_game/from_steam.py
+++ get_price_game/from_steam.py
@@ -4,17 +4,39 @@
 __author__ = 'ipetrash'
 
 
-text = 'resident evil 6'
+def search_game_price_list(name):
+    # category1 = 998 (Game)
+    url = 'http://store.steampowered.com/search/?category1=998&os=win&supportedlang=english&term=' + name
 
-# category1 = 998 (Game)
-url = 'http://store.steampowered.com/search/?category1=998&os=win&supportedlang=english&term=' + text
+    game_price_list = list()
 
-import requests
-rs = requests.get(url)
-print(rs)
+    import requests
+    rs = requests.get(url)
+    if not rs.ok:
+        print('Что-то пошло не так: {}\n{}'.format(rs.status_code, rs.text))
+        return game_price_list
 
-from bs4 import BeautifulSoup
-root = BeautifulSoup(rs.content, 'lxml')
+    from bs4 import BeautifulSoup
+    root = BeautifulSoup(rs.content, 'lxml')
 
-for div in root.select('.search_result_row'):
-    print(div.select_one('.title').text.strip(), div.select_one('.search_price').text.strip())
+    for div in root.select('.search_result_row'):
+        name = div.select_one('.title').text.strip()
+        price = div.select_one('.search_price').text.strip()
+
+        # Если в цене нет цифры считаем что это "Free To Play" или что-то подобное
+        import re
+        match = re.search(r'\d', price)
+        if not match:
+            price = 0
+
+        game_price_list.append((name, price))
+
+    return game_price_list
+
+
+if __name__ == '__main__':
+    text = 'resident evil 6'
+
+    game_price_list = search_game_price_list(text)
+    for name, price in game_price_list:
+        print(name, price, sep=' / ')
