f21dfe2c0fa815745942807b669a57713437f82a
Ilya Petrash <ilya.petrash@inbox.ru>
2017-01-22 03:13:17 +0500

    #

--- import_all_github_public_repos/loop_run.py
+++ import_all_github_public_repos/loop_run.py
@@ -17,9 +17,17 @@ while True:
 
     # Every 12 hours
     from datetime import timedelta
-    while today <= today + timedelta(hours=12):
-        # Delay 5 minutes
+    timeout_date = today + timedelta(hours=12)
+
+    while today <= timeout_date:
+        print('\r' * 50, end='')
+        print('До следующего запуска осталось {}'.format(timeout_date - today), end='')
+
+        import sys
+        sys.stdout.flush()
+
+        # Delay 10 minutes
         import time
-        time.sleep(5 * 60)
+        time.sleep(10 * 60)
 
         today = datetime.today()
