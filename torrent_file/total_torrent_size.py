20f63d1093e4e965d323c5c8060c74690c2def8d
Ilya Petrash <ilya.petrash@inbox.ru>
2017-01-31 01:10:09 +0500

    Append total_torrent_size.py

---
+++ torrent_file/total_torrent_size.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+def sizeof_fmt(num):
+    for x in ['bytes', 'KB', 'MB', 'GB']:
+        if num < 1024.0:
+            return "%3.1f %s" % (num, x)
+        num /= 1024.0
+    return "%3.1f%s" % (num, 'TB')
+
+
+if __name__ == '__main__':
+    with open('_.torrent', 'rb') as f:
+        torrent_file_bytes = f.read()
+        torrent_file_text = torrent_file_bytes.decode('latin1')
+
+    import effbot_bencode
+    torrent = effbot_bencode.decode(torrent_file_text)
+
+    files = torrent["info"]["files"]
+    total_size = 0
+
+    print('Files:')
+    for file in files:
+        print("    %r - %d bytes" % ("/".join(file["path"]), file["length"]))
+
+        total_size += file["length"]
+
+    print()
+    print("Total size: {} ({} bytes)".format(sizeof_fmt(total_size), total_size))
