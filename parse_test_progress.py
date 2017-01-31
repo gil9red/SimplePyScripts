f4ed4edee0b3aae5e2074e5673bacd117604cbf0
ipetrash <gil9red@gmail.com>
2017-01-31 11:45:41 +0500

    Append parse_test_progress.py

---
+++ parse_test_progress.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+def parse(text):
+    total_value = 0
+    total_max_value = 0
+
+    for line in text.splitlines():
+        line = line.strip()
+        if not line:
+            continue
+
+        import re
+        match = re.search(r'(\d+) +/ +(\d+)', line)
+        value = int(match.group(1))
+        max_value = int(match.group(2))
+
+        total_value += value
+        total_max_value += max_value
+
+        print(line + " ({}%)".format(int(value / max_value * 100)))
+
+    print()
+    print("Total: {} / {} ({}%)".format(total_value, total_max_value, int(total_value / total_max_value * 100)))
+
+
+if __name__ == '__main__':
+    text = """
+ATM - VSDC Issuing:         4  / 48
+ATM - Magstripe Issuing:    0  / 4
+Retail - VSDC Issuing:      0  / 13
+Retail - Magstripe Issuing: 0  / 4
+UCAT - Mag or VSDC Issuing: 2  / 2
+T&E - Mag or VSDC Issuing:  2  / 2
+CNP - Key-Entered Issuing:  15 / 15
+Cash - Mag or VSDC Issuing: 5  / 5
+Payments - Issuing:         0  / 6
+    """
+
+    parse(text)
