3489186e572153f021dbb6f5dca2ec4d110085cc
ipetrash <gil9red@gmail.com>
2017-01-31 11:44:24 +0500

    #

--- parse_torrent_sites/rutor/get_info_from_torrent_page.py
+++ parse_torrent_sites/rutor/get_info_from_torrent_page.py
@@ -5,3 +5,9 @@ __author__ = 'ipetrash'
 
 
 # TODO: парсинг страницы торрент, вытаскивание характеристик, описания, скриншотов, торрент-файла и магнет-ссылки
+
+from bs4 import BeautifulSoup
+root = BeautifulSoup(open('test_pages/зеркало rutor.info   Darkwood (2014) PC _ RePack.htm', 'rb'), 'lxml')
+
+details = root.select_one('#details')
+print(details.text.strip())
