8af5bcddc42ca0b33440d2736ea9fd6d8700baeb
ipetrash <gil9red@gmail.com>
2017-02-17 13:17:00 +0500

    Append hook_exceptions_in_Qt.py

---
+++ hook_exceptions_in_Qt.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import traceback
+
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+
+# Для отлова всех исключений, которые в слотах Qt могут "затеряться" и привести к тихому падению
+def log_uncaught_exceptions(ex_cls, ex, tb):
+    text = '{}: {}:\n'.format(ex_cls.__name__, ex)
+    text += ''.join(traceback.format_tb(tb))
+
+    print('Error: ', text)
+    QMessageBox.critical(None, 'Error', text)
+    quit()
+
+
+import sys
+sys.excepthook = log_uncaught_exceptions
+
+
+app = QApplication([])
+
+w = QLineEdit()
+w.textChanged.conneect(lambda x: 1 + '1')
+
+# Emit textChanged signal
+w.setText('!!!')
