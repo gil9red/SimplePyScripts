b994e851c87c0e04b076b6ed3bb490214876fd86
Ilya Petrash <ilya.petrash@inbox.ru>
2017-03-02 22:08:36 +0500

    Добавлено "stackoverflow/spelling and grammar check by description tags.py"

---
+++ stackoverflow/spelling and grammar check by description tags.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# NOTE: function from languagetool/check.py
+def check_ru(text: str) -> list():
+    """
+    Функция делает запрос на https://languagetool.org/ чтобы проверить на правильность указанный текст.
+    Если возвращается пустой массив -- проблем не было найдено, иначе -- есть.
+    В возращаемом списке можно узнать что именно не понравилось languagetool и варианты исправления.
+
+    """
+
+    url = 'https://languagetool.org/api/v2/check'
+    post_data = {
+        'disabledRules': 'WHITESPACE_RULE',
+        'allowIncompleteResults': 'true',
+        'text': text,
+        'language': 'ru',
+    }
+
+    import requests
+    rs = requests.post(url, data=post_data)
+    if not rs.ok:
+        raise Exception('Проблема с {}, status_code = {}'.format(url, rs.status_code))
+
+    return rs.json()['matches']
+
+
+# NOTE: file tags.json from stackoverflow/download_tags.py
+import json
+tags = json.load(open('tags.json', 'r', encoding='utf-8'))
+
+import time
+
+for tag, value in sorted(tags.items(), key=lambda x: len(x[1]['description'])):
+    print(tag)
+
+    text = value['description']
+
+    while True:
+        try:
+            matches = check_ru(text)
+            if matches:
+                print(tag, value['url_info'], len(text))
+
+                print('Найденные проблемы:')
+                for match in matches:
+                    error = match['message']
+                    context = match['context']
+                    offset = context['offset']
+                    length = context['length']
+
+                    error_text = context['text'][offset: offset + length]
+                    print('"{}" [{}:{}]: "{}" -> {}'.format(error_text, offset, length, error,
+                                                            [i['value'] for i in match['replacements']]))
+
+                print('\n')
+                print('-' * 50)
+                print('\n')
+
+            break
+
+        except Exception as e:
+            import traceback
+            print("ERROR: '{}'\n\n{}".format(e, traceback.format_exc()))
+
+            time.sleep(10)
+
+    time.sleep(5)
