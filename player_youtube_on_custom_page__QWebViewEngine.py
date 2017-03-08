c326240d1823d62bd3b22321d648e8e75587ad81
Ilya Petrash <ilya.petrash@inbox.ru>
2017-03-08 23:16:22 +0500

    Append player_youtube_on_custom_page__QWebViewEngine.py

---
+++ player_youtube_on_custom_page__QWebViewEngine.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    view = QWebEngineView()
+    view.show()
+
+    view.setHtml("""\
+<html>
+<body>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/Cb-srOfRqNc" frameborder="0" allowfullscreen></iframe>
+</body>
+</html>
+    """)
+
+    app.exec()
