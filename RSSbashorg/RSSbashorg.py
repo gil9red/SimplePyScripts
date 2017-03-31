6514f942a5f24a3cd2dc0155af8465faef0092ce
ipetrash <ilya.petrash@inbox.ru>
2017-03-31 16:05:43 +0500

    #

--- RSSbashorg/RSSbashorg.py
+++ RSSbashorg/RSSbashorg.py
@@ -2,14 +2,22 @@ __author__ = 'ipetrash'
 
 
 import feedparser
-import HTMLParser
+# import HTMLParser
 
 if __name__ == '__main__':
     d = feedparser.parse('http://bash.im/rss/')
-    print(d["feed"]["subtitle"])
+    # print(d["feed"]["subtitle"])
+    #
+    # for entry in d["entries"]:
+    #     quote = entry["summary"]
+    #     h = HTMLParser.HTMLParser()
+    #     quote = h.unescape(quote).replace("<br />", "\n")
+    #     print(quote + '\n\n')
 
-    for entry in d["entries"]:
-        quote = entry["summary"]
-        h = HTMLParser.HTMLParser()
-        quote = h.unescape(quote).replace("<br />", "\n")
+    print(d.feed.subtitle)
+
+    for entry in d.entries:
+        quote = entry.summary
+        # h = HTMLParser.HTMLParser()
+        # quote = h.unescape(quote).replace("<br />", "\n")
         print(quote + '\n\n')
