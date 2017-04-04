6a9d1da01119aeebefd11b42135d8df8d80193c3
ipetrash <ilya.petrash@inbox.ru>
2017-04-04 17:06:45 +0500

    Append vk_auth.py

---
+++ requests_example/vk_auth.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import requests
+
+
+def auth_vk(login: str, password: str) -> (requests.sessions.Session, requests.models.Response):
+    """
+    Функция для авторизации в вк.
+
+    """
+
+    def get_form_action(html: str) -> str:
+        """
+        Функция вернет ссылку для запроса авторизации
+
+        """
+
+        import re
+        form_action = re.findall(r'<form(?= ).* action="(.+)"', html)
+        if form_action:
+            return form_action[0]
+
+    import requests
+    session = requests.Session()
+
+    # Без авторизации не получится воспользоваться страницей укорачивания ссылок
+    url = 'https://m.vk.com'
+    rs = session.get(url)
+    print(rs)
+
+    login_form_action = get_form_action(rs.text)
+    if not login_form_action:
+        raise Exception('Не получилось из формы авторизации вытащить ссылку на авторизацию')
+
+    login_form_data = {
+        'email': login,
+        'pass': password,
+    }
+    rs = session.post(login_form_action, login_form_data)
+    print(rs, type(rs))
+
+    return session, rs
+
+
+if __name__ == '__main__':
+    LOGIN = '<LOGIN>'
+    PASSWORD = '<PASSWORD>'
+
+    session, rs = auth_vk(LOGIN, PASSWORD)
+    print(session, session.cookies)
+    print(rs, rs.url)
