fdc8ba2a5a108914774c7fa08ea5e58445078541
ipetrash <ilya.petrash@inbox.ru>
2017-04-05 11:05:25 +0500

    #

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -4,6 +4,22 @@
 __author__ = 'ipetrash'
 
 
+def locate_usb():
+    import win32file
+    drive_list = []
+    drivebits=win32file.GetLogicalDrives()
+    for d in range(1,26):
+        mask=1 << d
+        if drivebits & mask:
+            # here if the drive is at least there
+            drname='%c:\\' % chr(ord('A')+d)
+            t=win32file.GetDriveType(drname)
+            if t == win32file.DRIVE_REMOVABLE:
+                drive_list.append(drname)
+    return drive_list
+
+
+print(locate_usb())
 
 quit()
 
