6e49a68def93ab732ac2b7407f4cede1f24840ad
Ilya Petrash <ilya.petrash@inbox.ru>
2017-04-05 23:27:32 +0500

    Append winapi__get_logical_drives.py Append winapi__get_usb_list.py

---
+++ winapi__get_usb_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+def locate_usb() -> list:
+    import win32file
+    from winapi__get_logical_drives import get_logical_drives
+
+    usb_list = list()
+
+    for drive_name in get_logical_drives():
+        drive_type = win32file.GetDriveType(drive_name)
+
+        if drive_type == win32file.DRIVE_REMOVABLE:
+            usb_list.append(drive_name)
+
+    return usb_list
+
+
+if __name__ == '__main__':
+    print(locate_usb())
