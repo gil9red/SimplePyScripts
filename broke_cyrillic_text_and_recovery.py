fb40dc6d3c7844acc5fb5332120164d9c4f9a9a4
ipetrash <ilya.petrash@inbox.ru>
2017-04-11 12:04:46 +0500

    Append broke_cyrillic_text_and_recovery.py

---
+++ broke_cyrillic_text_and_recovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+if __name__ == '__main__':
+    text = 'РЎРѕР·РґР°РЅРёРµ С‚Р°Р±Р»РёС†С‹'
+    print(text)
+    print(text.encode('cp1251').decode('utf-8'))
