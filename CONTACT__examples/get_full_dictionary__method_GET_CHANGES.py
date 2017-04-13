fa2ea625340464fa797de0b6bafc1bec783b05a2
ipetrash <ilya.petrash@inbox.ru>
2017-04-13 11:54:57 +0500

    #

--- CONTACT__examples/get_full_dictionary__method_GET_CHANGES.py
+++ CONTACT__examples/get_full_dictionary__method_GET_CHANGES.py
@@ -10,13 +10,8 @@ __author__ = 'ipetrash'
 """
 
 
-URL_NG_SERVER = '<URL_NG_SERVER>'
+from config import *
 
-# Идентификатор программного обеспечения интегратора
-INT_SOFT_ID = '<INT_SOFT_ID>'
-
-# Код точки обслуживания
-POINT_CODE = '<POINT_CODE>'
 
 FILE_NAME_FULL_DICT = 'full_dict__CONTACT.xml'
 
