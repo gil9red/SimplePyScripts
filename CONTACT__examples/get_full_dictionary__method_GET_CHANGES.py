91dacb58e00dfb334bf9994963441e47c40d726b
ipetrash <ilya.petrash@inbox.ru>
2017-04-13 10:46:39 +0500

    Append get_full_dictionary__method_GET_CHANGES.py

---
+++ CONTACT__examples/get_full_dictionary__method_GET_CHANGES.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+"""
+Скрипт выполняет запрос получения справочника CONTACT (метод GET_CHANGES).
+
+"""
+
+
+URL_NG_SERVER = '<URL_NG_SERVER>'
+
+# Идентификатор программного обеспечения интегратора
+INT_SOFT_ID = '<INT_SOFT_ID>'
+
+# Код точки обслуживания
+POINT_CODE = '<POINT_CODE>'
+
+FILE_NAME_FULL_DICT = 'full_dict__CONTACT.xml'
+
+
+if __name__ == '__main__':
+    post_data = """
+    <?xml version="1.0"?>
+    <REQUEST OBJECT_CLASS="TAbonentObject" ACTION="GET_CHANGES" VERSION="0" TYPE_VERSION="I" PACK="ZLIB"
+    INT_SOFT_ID="{INT_SOFT_ID}"
+    POINT_CODE="{POINT_CODE}"
+    SignOut="No"
+    ExpectSigned="No"
+    />
+    """.format(INT_SOFT_ID=INT_SOFT_ID, POINT_CODE=POINT_CODE)
+
+    import requests
+    rs = requests.post(URL_NG_SERVER, data=post_data)
+    print(rs)
+    print(len(rs.text))
+
+    with open(FILE_NAME_FULL_DICT, 'wb') as f:
+        f.write(rs.content)
+
+    print('Write to: {}'.format(FILE_NAME_FULL_DICT))
