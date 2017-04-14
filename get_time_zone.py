8e2d8ce29ff8b6320465dafdd9d8cc86c487db16
ipetrash <ilya.petrash@inbox.ru>
2017-04-14 11:39:21 +0500

    Append get_time_zone.py

---
+++ get_time_zone.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+if __name__ == '__main__':
+    import dateutil.tz as dtz
+    import pytz
+
+    for name in pytz.common_timezones:
+        print(dtz.gettz(name))
