14f54a2a4ffc8a4c3bf67287d16f131fb874db85
ipetrash <ilya.petrash@inbox.ru>
2017-04-18 18:06:06 +0500

    Append opencv__cv2__example.py

---
+++ opencv__cv2__example.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: http://docs.opencv.org/3.1.0/dc/da5/tutorial_py_drawing_functions.html
+
+import numpy as np
+import cv2
+
+# Create a black image
+img = np.zeros((512,512,3), np.uint8)
+
+# Draw a diagonal blue line with thickness of 5 px
+cv2.line(img,(0,0),(511,511),(255,0,0),5)
+
+cv2.rectangle(img,(384,0),(510,128),(0,255,0),3)
+cv2.circle(img,(447,63), 63, (0,0,255), -1)
+cv2.ellipse(img,(256,256),(100,50),0,0,180,255,-1)
+
+pts = np.array([[10,5],[20,30],[70,20],[50,10]], np.int32)
+pts = pts.reshape((-1,1,2))
+cv2.polylines(img,[pts],True,(0,255,255))
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(img,'OpenCV',(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
+
+cv2.imshow("Image", img)
+cv2.waitKey(10000)
