bbfd4ff20884d258f3da3c860d500cc8e6962a73
Ilya Petrash <ilya.petrash@inbox.ru>
2017-04-21 01:03:31 +0500

    Append Dark_Souls_II__print_locations_from_start_location__with_graph.py

---
+++ Dark_Souls_II__print_locations_from_start_location__with_graph.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+def get_transitions_location(url_location):
+    """
+    Функция для поиска переходов из локации
+
+    """
+
+    import requests
+    rs = requests.get(url_location)
+
+    from bs4 import BeautifulSoup
+    root = BeautifulSoup(rs.content, 'lxml')
+
+    transitions = list()
+
+    table_transitions = root.select_one('table.pi-horizontal-group')
+    if not table_transitions or 'Переходы:' not in table_transitions.text:
+        return transitions
+
+    for a in table_transitions.select('a'):
+        from urllib.parse import urljoin
+        url = urljoin(rs.url, a['href'])
+
+        transitions.append((url, a.text))
+
+    return transitions
+
+
+if __name__ == '__main__':
+    # TODO: pretty graph
+    import networkx as nx
+    G = nx.Graph()
+
+    visited_locations = list()
+
+    def print_transitions(url, title):
+        # if len(visited_locations) >= 5:
+        #     return
+
+        if title in visited_locations:
+            return
+
+        visited_locations.append(title)
+        print(title, url)
+
+        transitions = get_transitions_location(url)
+        if not transitions:
+            return transitions
+
+        # Сначала напечатаем все связанные локации
+        for url_trans, title_trans in transitions:
+            print('    {} -> {}'.format(title_trans, url_trans))
+
+        print('\n')
+
+        # Поищем у этих локаций связаные с ними локации
+        for url_trans, title_trans in transitions:
+            if title_trans not in visited_locations:
+                G.add_edge(title, title_trans)
+
+                print_transitions(url_trans, title_trans)
+
+
+    url_start_location = 'http://ru.darksouls.wikia.com/wiki/%D0%9C%D0%B5%D0%B6%D0%B4%D1%83%D0%BC%D0%B8%D1%80%D1%8C%D0%B5'
+    print_transitions(url_start_location, 'Междумирье')
+
+    print()
+    print(len(visited_locations), visited_locations)
+
+
+    pos = nx.spring_layout(G)  # positions for all nodes
+
+    # edges
+    nx.draw_networkx_edges(G, pos, edgelist=G.edges(), width=6)
+
+    # nodes
+    nx.draw_networkx_nodes(G, pos, node_size=70)
+
+    # labels
+    nx.draw_networkx_labels(G, pos, font_size=20, font_family='sans-serif')
+
+    import matplotlib.pyplot as plt
+    plt.figure(1, figsize=(80, 80))
+    plt.axis('off')
+    # plt.savefig("ds2_locations_graph.png")  # save as png
+    plt.show()  # display
+
