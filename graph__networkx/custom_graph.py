aa5b9923c7b6d8c54436510a98364447c637c39d
Ilya Petrash <ilya.petrash@inbox.ru>
2017-04-20 23:47:32 +0500

    Append custom_graph.py

---
+++ graph__networkx/custom_graph.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://networkx.readthedocs.io/en/stable/examples/drawing/weighted_graph.html
+
+import matplotlib.pyplot as plt
+import networkx as nx
+
+G = nx.Graph()
+
+G.add_edge('a', 'b')
+G.add_edge('a', 'c')
+G.add_edge('c', 'd')
+G.add_edge('c', 'e')
+G.add_edge('c', 'f')
+G.add_edge('a', 'd')
+
+pos = nx.spring_layout(G)  # positions for all nodes
+
+# nodes
+nx.draw_networkx_nodes(G, pos, node_size=700)
+
+# edges
+nx.draw_networkx_edges(G, pos, edgelist=G.edges(), width=6)
+
+# labels
+nx.draw_networkx_labels(G, pos, font_size=20, font_family='sans-serif')
+
+plt.axis('off')
+plt.savefig("custom_graph.png")  # save as png
+plt.show()  # display
