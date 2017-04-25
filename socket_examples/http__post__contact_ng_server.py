de10e39c729d19a6888330ad0f249f5ea8f5b5eb
ipetrash <ilya.petrash@inbox.ru>
2017-04-25 14:22:30 +0500

    Append http__post__contact_ng_server.py

---
+++ socket_examples/http__post__contact_ng_server.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+INT_SOFT_ID = '<INT_SOFT_ID>'
+POINT_CODE = '<POINT_CODE>'
+
+# NG Server
+HOST = "10.7.8.31"
+PORT = 12000
+
+
+post_data = """
+<?xml version="1.0"?>
+<REQUEST OBJECT_CLASS="TAbonentObject" ACTION="GET_CHANGES" VERSION="0" TYPE_VERSION="I" PACK="ZLIB"
+INT_SOFT_ID="{INT_SOFT_ID}"
+POINT_CODE="{POINT_CODE}"
+SignOut="No"
+ExpectSigned="No"
+/>
+""".format(INT_SOFT_ID=INT_SOFT_ID, POINT_CODE=POINT_CODE)
+
+
+http_request = (
+    'POST / HTTP/1.1\r\n',
+    'Host: {host}:{port}\r\n',
+    'Accept-Encoding: gzip, deflate\r\n',
+    'User-Agent: {user_agent}\r\n',
+    'Connection: keep-alive\r\n',
+    'Accept: */*\r\n',
+    'Content-Length: {content_length}\r\n',
+    '\r\n',
+    '\n',
+    '{body}\n'
+)
+http_request = ''.join(http_request)
+http_request = http_request.format(
+    host=HOST,
+    port=PORT,
+    user_agent='iHuman',
+    content_length=len(post_data),
+    body=post_data,
+)
+
+print(repr(http_request))
+
+
+import socket
+s = socket.socket()
+s.connect((HOST, PORT))
+s.send(http_request.encode())
+
+print('\nResponse:')
+
+while True:
+    result = s.recv(1024)
+    if not result:
+        break
+
+    print(repr(result))
