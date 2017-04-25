8862faff285c2256ea5b369414d672939fe9a347
ipetrash <ilya.petrash@inbox.ru>
2017-04-25 14:55:53 +0500

    #

--- socket_examples/http__get__via_proxy.py
+++ socket_examples/http__get__via_proxy.py
@@ -7,18 +7,24 @@ __author__ = 'ipetrash'
 # SOURCE: http://stackoverflow.com/a/42913875/5909792
 # Connect to proxy, in http request write full host address
 
+BUFFER_SIZE = 1024
+
 request = b"GET http://stackoverflow.com HTTP/1.1\nHost: stackoverflow.com\n\n"
-host = "<proxy_host>"
+host = "proxy.compassplus.ru"
 port = 3128
 
 import socket
-s = socket.socket()
-s.connect((host, port))
-s.send(request)
+sock = socket.socket()
+sock.settimeout(60)  # Если за 60 секунд данные не придут, соединение закрывается
+
+sock.connect((host, port))
+sock.send(request)
 
 while True:
-    result = s.recv(1024)
-    if not result:
+    data = sock.recv(BUFFER_SIZE)
+    if not data:
         break
 
-    print(result)
+    print(len(data), data)
+
+sock.close()
