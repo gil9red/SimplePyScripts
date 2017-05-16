2414a59177cde3df7c9730e4e888011c8b6ed91e
ipetrash <ilya.petrash@inbox.ru>
2017-05-16 20:48:06 +0500

    Append download_zip_and_extract_first_json_file_in_memory.py

---
+++ zip_file_example/download_zip_and_extract_first_json_file_in_memory.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import requests
+rs = requests.get('https://op.mos.ru/EHDWSREST/catalog/export/get?id=84505')
+
+import zipfile
+import io
+zip_data = io.BytesIO(rs.content)
+
+with zipfile.ZipFile(zip_data) as zip_file:
+    json_file_name = zip_file.namelist()[0]
+    print(json_file_name)
+
+    json_data = zip_file.read(json_file_name)
+    json_data = json_data.decode('cp1251')
+    print(json_data[:50])
+
+    import json
+    obj = json.loads(json_data)
+    print(obj[0]['AdmArea'])
