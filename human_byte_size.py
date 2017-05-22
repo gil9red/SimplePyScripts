a34b916752b09af4a52906b5bc6446fc8cbd69b3
ipetrash <ilya.petrash@inbox.ru>
2017-05-22 18:03:27 +0500

    Append human_byte_size.py

---
+++ human_byte_size.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+def sizeof_fmt(num):
+    for x in ['bytes', 'KB', 'MB', 'GB']:
+        if num < 1024.0:
+            return "%3.1f %s" % (num, x)
+        num /= 1024.0
+    return "%3.1f%s" % (num, 'TB')
+
+
+import shutil
+usage = shutil.disk_usage('C://')
+print('total: {:>8} ({} bytes)'.format(sizeof_fmt(usage.total), usage.total))
+print('used:  {:>8} ({} bytes)'.format(sizeof_fmt(usage.used), usage.used))
+print('free:  {:>8} ({} bytes)'.format(sizeof_fmt(usage.free), usage.free))
