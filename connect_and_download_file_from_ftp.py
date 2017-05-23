b9d29554f38b61550ba2e5240c49e86cb7ed8a51
Ilya Petrash <ilya.petrash@inbox.ru>
2017-05-23 22:30:34 +0500

    Append connect_and_download_file_from_ftp.py

---
+++ connect_and_download_file_from_ftp.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from ftplib import FTP
+
+# Connect to host, default port
+ftp = FTP('ftp.debian.org')
+print(ftp)
+
+# User anonymous, passwd anonymous@
+print(ftp.login())
+
+# Change into "debian" directory
+print(ftp.cwd('debian'))
+
+# List directory contents
+print(ftp.retrlines('LIST'))
+
+# Show file content
+print(ftp.retrbinary('RETR README', lambda data: print(repr(data))))
+
+# Download file
+print(ftp.retrbinary('RETR README', open('README', 'wb').write))
+
+# Send a QUIT command to the server and close the connection
+print(ftp.quit())
