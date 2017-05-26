3ea58a1fe0fb5f9584c022795dbe78d703488d28
Ilya Petrash <ilya.petrash@inbox.ru>
2017-05-26 23:09:52 +0500

    Append Excel

---
+++ Excel/xls example.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+columns = ['Name', 'Age', 'Course']
+rows = [
+    ['Vasya', '16', 1],
+    ['Anya', '17', 2],
+    ['Inna', '16', 1],
+]
+
+import xlwt
+wb = xlwt.Workbook()
+ws = wb.add_sheet('Students')
+
+for i, column in enumerate(columns):
+    ws.write(0, i, column)
+
+for i, row in enumerate(rows, 1):
+    for j, data in enumerate(row):
+        ws.write(i, j, data)
+
+wb.save('excel.xls')
---
+++ Excel/xlsx example.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+columns = ['Name', 'Age', 'Course']
+rows = [
+    ['Vasya', '16', 1],
+    ['Anya', '17', 2],
+    ['Inna', '16', 1],
+]
+
+import openpyxl
+wb = openpyxl.Workbook()
+
+# Удаление существующих листов (какой-то всегда создается по умолчанию)
+for sheet_name in wb.sheetnames:
+    sheet = wb.get_sheet_by_name(sheet_name)
+    wb.remove_sheet(sheet)
+
+ws = wb.create_sheet('Students')
+
+for i, value in enumerate(columns, 1):
+    ws.cell(row=1, column=i).value = value
+
+for i, row in enumerate(rows, 2):
+    for j, value in enumerate(row, 1):
+        ws.cell(row=i, column=j).value = value
+
+from openpyxl.writer.excel import save_workbook
+save_workbook(wb, 'excel.xlsx')
