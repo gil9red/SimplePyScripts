d9449e8e4a41575acc36649fabbd7cdffd7b4259
ipetrash <ilya.petrash@inbox.ru>
2017-05-30 13:39:49 +0500

    Append read_mp3_tags__id3

---
+++ read_mp3_tags__id3/main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from mutagen.easyid3 import EasyID3
+mp3info = EasyID3('test.mp3')
+
+print(mp3info)  # {'artist': ['Foo'], 'title': ['Test'], 'album': ['Bar'], 'organization': ['Hello World']}
+print()
+print(mp3info['title'])  # ['Test']
+print(mp3info['TITLE'])  # ['Test']
+print()
+
+# Print:
+# artist: ['Foo']
+# organization: ['Hello World']
+# album: ['Bar']
+# title: ['Test']
+for k, v in mp3info.items():
+    print('{}: {}'.format(k, v))
