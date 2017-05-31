32f9d17bd34d80e4eeeebfdcecd5161256579de9
ipetrash <ilya.petrash@inbox.ru>
2017-05-31 16:26:49 +0500

    Append "images_widget__api.vk photos.search requests qt.py"

---
+++ images_widget__api.vk photos.search requests qt.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+try:
+    from PyQt5.QtWidgets import *
+    from PyQt5.QtGui import *
+
+except:
+    try:
+        from PyQt4.QtGui import *
+
+    except:
+        try:
+            from PySide.QtGui import *
+        except:
+            pass
+
+
+def get_img_urls():
+    import requests
+    rs = requests.get("https://api.vk.com/method/photos.search?v=5.64")
+    img_urls = rs.json()["response"]["items"]
+    return [url['photo_130'] for url in img_urls]
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    mw = QScrollArea()
+    mw.setWindowTitle(__file__)
+    mw.show()
+
+    img_urls = get_img_urls()
+
+    layout = QGridLayout()
+
+    columns = 4
+    i, j = 0, 0
+
+    mw.setWindowTitle('Loading...')
+
+    print('Images:', len(img_urls))
+
+    url_by_label = dict()
+
+    # Append labels on form
+    for n, url in enumerate(img_urls):
+        label = QLabel()
+        label.setFixedSize(130, 130)
+        label.setFrameStyle(QFrame.Box)
+        layout.addWidget(label, i, j)
+
+        url_by_label[url] = label
+
+        j += 1
+        if j >= columns:
+            i += 1
+            j = 0
+
+    w = QWidget()
+    w.setLayout(layout)
+    mw.setWidget(w)
+
+    for n, (url, label) in enumerate(url_by_label.items()):
+        import requests
+        rs = requests.get(url)
+
+        image = QImage.fromData(rs.content)
+        pixmap = QPixmap.fromImage(image)
+
+        label.setPixmap(pixmap)
+
+        mw.setWindowTitle('{}/{} Loading...'.format(n, len(url_by_label)))
+        QApplication.processEvents()
+
+    mw.setWindowTitle('Ok!')
+
+    app.exec()
