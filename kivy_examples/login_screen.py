1359ad5f18256abf3fdb615460b04c9a32afdf29
ipetrash <ilya.petrash@inbox.ru>
2017-06-30 12:45:29 +0500

    Append button

--- kivy_examples/login_screen.py
+++ kivy_examples/login_screen.py
@@ -7,7 +7,9 @@ __author__ = 'ipetrash'
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+from kivy.uix.popup import Popup
 
 
 class LoginScreen(GridLayout):
@@ -15,12 +17,35 @@ class LoginScreen(GridLayout):
         super().__init__(**kwargs)
 
         self.cols = 2
+        self.spacing = 10
+        self.padding = 10
+
         self.add_widget(Label(text='User Name'))
         self.username = TextInput(multiline=False)
+        self.username.text = 'admin'
         self.add_widget(self.username)
+
         self.add_widget(Label(text='password'))
         self.password = TextInput(password=True, multiline=False)
+        self.password.text = 'admin'
         self.add_widget(self.password)
+
+        self.add_widget(Button(text="Ok", on_press=self.check))
+
+    def check(self, button):
+        print("auth called")
+
+        message = "Success!" if self.username.text == "admin" else "Need admin!"
+
+        popup = Popup(
+            title="Info",
+            content=Label(text=message),
+            size=(100, 100),
+            size_hint=(0.3, 0.3),
+            auto_dismiss=True,
+        )
+
+        popup.open()
 
 
 class MyLoginScreenApp(App):
