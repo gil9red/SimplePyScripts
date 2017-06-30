4bfc8c61ac66b3002c75dccd93344445e5617e0a
ipetrash <ilya.petrash@inbox.ru>
2017-06-30 11:28:48 +0500

    Append print_class_name.py

---
+++ print_class_name.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+class A:
+    pass
+
+a = A()
+print(A.__name__)  # A
+print(a.__class__.__name__)  # A
+
+
+class B:
+    def __init__(self):
+        print(self.__class__.__name__)  # B
+
+B()
