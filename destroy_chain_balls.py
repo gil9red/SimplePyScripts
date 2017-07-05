31fe1b3ec2e82f70753659dc42c692550ddc47cd
ipetrash <ilya.petrash@inbox.ru>
2017-07-05 18:16:04 +0500

    #

--- destroy_chain_balls.py
+++ destroy_chain_balls.py
@@ -8,29 +8,35 @@ def destroy_chain_balls(balls):
     # Будет работать с копией
     balls = list(balls)
 
-    while True:
-        repeat_index = [0]
+    while balls:
+        repeat_index_list = [0]
         last_ball = balls[0]
 
         for i in range(1, len(balls)):
             ball = balls[i]
 
+            # Наткнулись на новый тип шарика
             if last_ball != ball:
-                if len(repeat_index) < 3:
-                    repeat_index.clear()
+                # Если не удалось набрать последовательность, чистим список
+                if len(repeat_index_list) < 3:
+                    repeat_index_list.clear()
                 else:
+                    # Последовательность есть, прерываем цикл
                     break
 
-            repeat_index.append(i)
+            repeat_index_list.append(i)
             last_ball = ball
 
-        if len(repeat_index) < 3:
+        # Если перебор закончился, а последовательность не была найдена,
+        # заканчиваем уничтожение шариков
+        if len(repeat_index_list) < 3:
             break
 
-        for i in reversed(repeat_index):
+        # Удаляем шарики из найденной последовательности
+        for i in reversed(repeat_index_list):
             balls.pop(i)
 
-        repeat_index.clear()
+        repeat_index_list.clear()
 
     return balls
 
@@ -49,5 +55,13 @@ if __name__ == '__main__':
     print("{} -> {}, уничтожено: {}".format(balls, balls_2, len(balls) - len(balls_2)))
 
     balls = [1, 3, 3, 3, 2]
+    balls_2 = destroy_chain_balls(balls)
+    print("{} -> {}, уничтожено: {}".format(balls, balls_2, len(balls) - len(balls_2)))
+
+    balls = [1, 1, 1]
+    balls_2 = destroy_chain_balls(balls)
+    print("{} -> {}, уничтожено: {}".format(balls, balls_2, len(balls) - len(balls_2)))
+
+    balls = [1, 1]
     balls_2 = destroy_chain_balls(balls)
     print("{} -> {}, уничтожено: {}".format(balls, balls_2, len(balls) - len(balls_2)))
