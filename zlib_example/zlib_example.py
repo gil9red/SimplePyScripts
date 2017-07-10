83f8fb9c7c543aad45ba71b0ea114c9885887eb5
ipetrash <ilya.petrash@inbox.ru>
2017-07-10 14:55:13 +0500

    #

--- zlib_example/zlib_example.py
+++ zlib_example/zlib_example.py
@@ -1,13 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 __author__ = 'ipetrash'
 
 
-if __name__ == '__main__':
-    # Data Compression (модуль zlib_example)
-    import zlib
-    s = b'witch which has which witches wrist watch'
-    print(s)
-    print(len(s))
-    t = zlib.compress(s)
-    print(len(t))
-    print(zlib.decompress(t))
-    print(zlib.crc32(s))
+# Data Compression (модуль zlib_example)
+import zlib
+
+text = b'witch which has which witches wrist watch'
+print('[{}]: {} (crc32: {})'.format(len(text), text, zlib.crc32(text)))
+
+print()
+print('Compress...')
+compress_text = zlib.compress(text)
+print('[{}]: {} (crc32: {})'.format(len(compress_text), compress_text, zlib.crc32(compress_text)))
+
+print()
+print('Decompress...')
+
+decompress_text = zlib.decompress(compress_text)
+print('[{}]: {} (crc32: {})'.format(len(decompress_text), decompress_text, zlib.crc32(decompress_text)))
