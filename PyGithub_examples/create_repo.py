c2c838cd9cddfb71bb9ff1e26ff422a759359600
ipetrash <ilya.petrash@inbox.ru>
2017-07-10 13:51:47 +0500

    #

---
+++ PyGithub_examples/create_repo.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import github
+
+from config import LOGIN, PASSWORD
+
+# NOTE: delete repo: Settings -> <Danger Zone> -> <Delete this repository>
+NEW_REPO = 'Test-Repo'
+
+
+if __name__ == '__main__':
+    gh = github.Github(LOGIN, PASSWORD)
+    user = gh.get_user()
+
+    try:
+        repo = user.get_repo(NEW_REPO)
+
+    except:
+        repo = user.create_repo(NEW_REPO)
+
+    print(repo)
