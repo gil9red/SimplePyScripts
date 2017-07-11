bb156c88ed974d0d5d9fe0d5e493e0a525d29ebb
Ilya Petrash <ilya.petrash@inbox.ru>
2017-07-11 23:09:18 +0500

    Append debug_logging__requests.py

---
+++ requests_example/debug_logging__requests.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import logging
+
+# These two lines enable debugging at httplib level (requests->urllib3->http.client)
+# You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
+# The only thing missing will be the response.body which is not logged.
+try:
+    import http.client as http_client
+except ImportError:
+    # Python 2
+    import httplib as http_client
+http_client.HTTPConnection.debuglevel = 1
+
+# You must initialize logging, otherwise you'll not see debug output.
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+
+
+# Test
+import requests
+rs = requests.get('http://httpbin.org/ip')
+print(rs)
+print(rs.url)
+print(rs.history)
+print(rs.cookies)
+print(rs.text)
