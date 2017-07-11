233af48b9d87f2b39fc1767417b17fef39267b6f
Ilya Petrash <ilya.petrash@inbox.ru>
2017-07-11 23:32:51 +0500

    #

--- requests_example/auth__kingfashion.com.py
+++ requests_example/auth__kingfashion.com.py
@@ -25,26 +25,25 @@ __author__ = 'ipetrash'
 # requests_log.propagate = True
 
 
-data = {
-    'login[username]': 'yur4enko.vitya@yandex.ru',
-    'login[password]': 'YEvbpUZ1i5LanIFnxfVW',
-}
+if __name__ == '__main__':
+    data = {
+        'login[username]': 'yur4enko.vitya@yandex.ru',
+        'login[password]': 'YEvbpUZ1i5LanIFnxfVW',
+    }
 
+    import requests
+    session = requests.Session()
 
-import requests
-session = requests.Session()
+    rs = session.get('https://kingfashion.com/customer/account/')
 
-rs = session.get('https://kingfashion.com/customer/account/')
+    # Получение поля form_key из формы авторизации
+    from bs4 import BeautifulSoup
+    root = BeautifulSoup(rs.content, 'lxml')
+    form_key = root.select_one('#login-form > input[name=form_key]')
+    data['form_key'] = form_key['value']
 
-# Получение поля form_key из формы авторизации
-from bs4 import BeautifulSoup
-root = BeautifulSoup(rs.content, 'lxml')
-form_key = root.select_one('#login-form > input[name=form_key]')
-data['form_key'] = form_key['value']
+    rs = session.post("https://kingfashion.com/customer/account/loginPost/", data=data)
+    print(rs.url)
 
-rs = session.post("https://kingfashion.com/customer/account/loginPost/", data=data)
-print(rs.url)
-
-root = BeautifulSoup(rs.content, 'lxml')
-print(root.select_one('.quick-access.shop > .account'))
-
+    root = BeautifulSoup(rs.content, 'lxml')
+    print(root.select_one('.quick-access.shop > .account'))
