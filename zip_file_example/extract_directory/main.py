1bb05405df794ddc701886d347049456d887ef49
ipetrash <ilya.petrash@inbox.ru>
2017-07-13 10:59:43 +0500

    Append zip_file_example/extract_directory

---
+++ zip_file_example/extract_directory/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+DIR_NAME = 'dir_1'
+
+
+if __name__ == '__main__':
+    import zipfile
+    with zipfile.ZipFile(DIR_NAME + '.zip', mode='r') as zf:
+        zf.extractall()
