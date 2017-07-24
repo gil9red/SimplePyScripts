9fe3f3de85b9d68cbcd1f7d6f740d9e2e0572f2d
Ilya Petrash <ilya.petrash@inbox.ru>
2017-07-24 12:39:01 +0500

    #

--- yahoo_weather.py
+++ yahoo_weather.py
@@ -5,13 +5,21 @@ __author__ = 'ipetrash'
 
 
 if __name__ == '__main__':
-    city = 'Magnitogorsk'
-    query = "select item from weather.forecast where woeid in (select woeid from geo.places(1) where text='{}') and u='c'".format(city)
-    url = "https://query.yahooapis.com/v1/public/yql?q={}&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=".format(query)
+    city = "Магнитогорск"
+    # # OR:
+    # city = 'Magnitogorsk'
+    url = "https://query.yahooapis.com/v1/public/yql?q=select item from weather.forecast where woeid in " \
+          "(select woeid from geo.places(1) where text='{city}') and u='c'" \
+          "&format=json&diagnostics=true".format(city=city)
 
     import requests
     rs = requests.get(url)
     item = rs.json()['query']['results']['channel']['item']
-    for forecast in item['forecast']:
-        print('{date}: {low} - {high}. {day}: {text}'.format(**forecast))
 
+    condition = item['condition']
+    print('Current: {temp} °C, (text)'.format(**condition))
+    print()
+
+    print('Forecast:')
+    for forecast in item['forecast']:
+        print('{date}: {low} - {high} °C. {day}: {text}'.format(**forecast))
