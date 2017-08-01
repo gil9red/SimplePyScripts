f6b9e5e854e999c870cd4c03f8ae98f409d169c9
Ilya Petrash <ilya.petrash@inbox.ru>
2017-08-02 00:08:44 +0500

    Append "multithreading/flask__threaded"

---
+++ multithreading/flask__threaded/threaded__without.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    import threading
+    return "Current thread: {}".format(threading.current_thread())
+
+
+if __name__ == '__main__':
+    app.run(port=6001)
