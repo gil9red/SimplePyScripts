b624887452938f48a2588f7aa0d0b38a6d90ef55
ipetrash <ilya.petrash@inbox.ru>
2017-08-04 10:48:40 +0500

    #

--- Base64_examples/gui_base64.py
+++ Base64_examples/gui_base64.py
@@ -4,8 +4,6 @@
 __author__ = 'ipetrash'
 
 
-import base64
-from os.path import split as path_split
 import traceback
 import sys
 
@@ -17,6 +15,7 @@ except:
     try:
         from PyQt4.QtGui import *
         from PyQt4.QtCore import *
+
     except:
         from PySide.QtGui import *
         from PySide.QtCore import *
@@ -35,26 +34,125 @@ def log_uncaught_exceptions(ex_cls, ex, tb):
 sys.excepthook = log_uncaught_exceptions
 
 
+# from encodings.aliases import aliases
+# print(aliases)
+# OR:
+STANDART_ENCODINGS = [
+    "ascii",
+    "big5",
+    "big5hkscs",
+    "cp037",
+    "cp424",
+    "cp437",
+    "cp500",
+    "cp720",
+    "cp737",
+    "cp775",
+    "cp850",
+    "cp852",
+    "cp855",
+    "cp856",
+    "cp857",
+    "cp858",
+    "cp860",
+    "cp861",
+    "cp862",
+    "cp863",
+    "cp864",
+    "cp865",
+    "cp866",
+    "cp869",
+    "cp874",
+    "cp875",
+    "cp932",
+    "cp949",
+    "cp950",
+    "cp1006",
+    "cp1026",
+    "cp1140",
+    "cp1250",
+    "cp1251",
+    "cp1252",
+    "cp1253",
+    "cp1254",
+    "cp1255",
+    "cp1256",
+    "cp1257",
+    "cp1258",
+    "euc_jp",
+    "euc_jis_2004",
+    "euc_jisx0213",
+    "euc_kr",
+    "gb2312",
+    "gbk",
+    "gb18030",
+    "hz",
+    "iso2022_jp",
+    "iso2022_jp_1",
+    "iso2022_jp_2",
+    "iso2022_jp_2004",
+    "iso2022_jp_3",
+    "iso2022_jp_ext",
+    "iso2022_kr",
+    "latin_1",
+    "iso8859_2",
+    "iso8859_3",
+    "iso8859_4",
+    "iso8859_5",
+    "iso8859_6",
+    "iso8859_7",
+    "iso8859_8",
+    "iso8859_9",
+    "iso8859_10",
+    "iso8859_13",
+    "iso8859_14",
+    "iso8859_15",
+    "iso8859_16",
+    "johab",
+    "koi8_r",
+    "koi8_u",
+    "mac_cyrillic",
+    "mac_greek",
+    "mac_iceland",
+    "mac_latin2",
+    "mac_roman",
+    "mac_turkish",
+    "ptcp154",
+    "shift_jis",
+    "shift_jis_2004",
+    "shift_jisx0213",
+    "utf_32",
+    "utf_32_be",
+    "utf_32_le",
+    "utf_16",
+    "utf_16_be",
+    "utf_16_le",
+    "utf_7",
+    "utf_8",
+    "utf_8_sig"
+]
+
+
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
 
+        from os.path import split as path_split
         self.setWindowTitle(path_split(__file__)[1])
 
         self.button_direct = QPushButton()
 
-        # # TODO: добавить больше кодировок, и лучше не вручную
-        # cb_encoding = QComboBox()
-        # cb_encoding.addItem('UTF-8')
-        # cb_encoding.addItem('CP1251')
-        # cb_encoding.addItem('UTF-16LE')
-        # cb_encoding.addItem('UTF-16BE')
-        # cb_encoding.setCurrentIndex(0)
-        # cb_encoding.setFixedWidth(100)
+        # TODO: добавить больше кодировок, и лучше не вручную
+        self.cb_encoding = QComboBox()
+        self.cb_encoding.addItems(STANDART_ENCODINGS)
+        self.cb_encoding.setFixedWidth(100)
+
+        index = self.cb_encoding.findText('utf_8')
+        self.cb_encoding.setCurrentIndex(index)
 
         button_layout = QHBoxLayout()
         button_layout.addWidget(self.button_direct)
-        # button_layout.addWidget(cb_encoding)
+        button_layout.addWidget(self.cb_encoding)
 
         layout = QVBoxLayout()
         layout.addLayout(button_layout)
@@ -85,7 +183,7 @@ class MainWindow(QWidget):
 
         self.button_direct.clicked.connect(self.change_convert_direct)
         self.text_edit_input.textChanged.connect(self.input_text_changed)
-        # cb_encoding.currentIndexChanged.connect(input_text_changed)
+        self.cb_encoding.currentIndexChanged.connect(self.input_text_changed)
 
         splitter = QSplitter()
         splitter.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
@@ -122,22 +220,16 @@ class MainWindow(QWidget):
         self.last_detail_error_message = None
 
         try:
-            # codec_name = cb_encoding.currentText()
-            # text = text_edit_input.toPlainText().encode()
-            # text = base64.b64encode(text) if direct_encode_text else base64.b64decode(text)
-            # text_edit_output.setPlainText(
-            #     text.decode() if direct_encode_text else text.decode('utf-8').encode(codec_name).decode()
-            # )
+            codec_name = self.cb_encoding.currentText()
+            in_text = self.text_edit_input.toPlainText().encode(encoding=codec_name)
 
-            # text = text_edit_input.toPlainText().encode() if not direct_encode_text else \
-            #     text_edit_input.toPlainText().encode(codec_name)
-            # print(text)
-            # text = base64.b64encode(text) if direct_encode_text else base64.b64decode(text)
-            # text_edit_output.setPlainText(text.decode() if not direct_encode_text else text.decode(codec_name))
+            import base64
+            if self.direct_encode_text:
+                text = base64.b64encode(in_text)
+            else:
+                text = base64.b64decode(in_text)
 
-            text = self.text_edit_input.toPlainText().encode()
-            text = base64.b64encode(text) if self.direct_encode_text else base64.b64decode(text)
-            self.text_edit_output.setPlainText(text.decode())
+            self.text_edit_output.setPlainText(text.decode(encoding=codec_name))
 
         except Exception as e:
             # Выводим ошибку в консоль
@@ -154,7 +246,6 @@ class MainWindow(QWidget):
 
     def change_convert_direct(self):
         self.direct_encode_text = not self.direct_encode_text
-
         self.button_direct.setText('text -> base64' if self.direct_encode_text else 'base64 -> text')
 
         self.input_text_changed()
