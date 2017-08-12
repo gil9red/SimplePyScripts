0e4502882661cc06a1c45cd9ebec44d060e76a95
Ilya Petrash <ilya.petrash@inbox.ru>
2017-08-12 13:00:38 +0500

    Append bin2str

---
+++ bin2str/bin2str.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+def bin2str(bin_str):
+    bin_str = bin_str.replace(' ', '')
+    length = len(bin_str)
+
+    if length % 8 != 0:
+        raise Exception('Bad length')
+
+    text = ''
+
+    for i in range(length // 8):
+        end = i * 8 + 8
+
+        b = bin_str[i * 8: end]
+        c = chr(int(b, 2))
+        text += c
+
+    return text
+
+
+def str2bin(text):
+    bin_words = [bin(ord(c)).replace('0b', '').rjust(8, '0') for c in text]
+
+    return ' '.join(bin_words)
+
+
+if __name__ == '__main__':
+    assert bin2str('01011001 01101111 01110101') == 'You'
+    assert bin2str('010110010110111101110101') == 'You'
+    assert str2bin(bin2str('01011001 01101111 01110101')) == '01011001 01101111 01110101'
+    assert str2bin(bin2str('010110010110111101110101')) == '01011001 01101111 01110101'
+
+    assert str2bin('You') == '01011001 01101111 01110101'
+    assert bin2str(str2bin('You')) == 'You'
+
+    print('Bin to text:')
+    text = (
+        "01011001 01101111 01110101 00100000 01100001 01110010 01100101 00100000 01101101 01101111 "
+        "01110010 01100101 00100000 01110100 01101000 01100001 01101110 00100000 01101010 01110101 "
+        "01110011 01110100 00100000 01100001 01101110 00100000 01100001 01101110 01101001 01101101 "
+        "01100001 01101100 00101110"
+    )
+    print(bin2str(text))
+    print()
+
+    print('Text to bin:')
+    text = "You are more than just an animal."
+    print(str2bin(text))
---
+++ bin2str/gui.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+try:
+    from PyQt5.QtWidgets import *
+    from PyQt5.QtCore import *
+
+except:
+    try:
+        from PyQt4.QtGui import *
+        from PyQt4.QtCore import *
+
+    except:
+        from PySide.QtGui import *
+        from PySide.QtCore import *
+
+
+def log_uncaught_exceptions(ex_cls, ex, tb):
+    text = '{}: {}:\n'.format(ex_cls.__name__, ex)
+    import traceback
+    text += ''.join(traceback.format_tb(tb))
+
+    print(text)
+    QMessageBox.critical(None, 'Error', text)
+    quit()
+
+
+import sys
+sys.excepthook = log_uncaught_exceptions
+
+
+class Widget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('bin2str')
+
+        button_bin2str = QPushButton('bin -> str')
+        button_bin2str.clicked.connect(self._bin2str)
+
+        button_str2bin = QPushButton('str -> bin')
+        button_str2bin.clicked.connect(self._str2bin)
+
+        self.plain_text_bin = QPlainTextEdit()
+        self.plain_text_bin.setPlainText('01001000 01100101 01101100 01101100 01101111 00100000 '
+                                         '01010111 01101111 01110010 01101100 01100100 00100001')
+
+        self.plain_text_str = QPlainTextEdit()
+
+        h_layout = QHBoxLayout()
+        h_layout.addWidget(button_bin2str)
+        h_layout.addWidget(button_str2bin)
+
+        layout = QVBoxLayout()
+        layout.addLayout(h_layout)
+        layout.addWidget(QLabel('Bin:'))
+        layout.addWidget(self.plain_text_bin)
+        layout.addSpacing(10)
+        layout.addWidget(QLabel('Text:'))
+        layout.addWidget(self.plain_text_str)
+
+        self.setLayout(layout)
+
+    def _bin2str(self):
+        text = self.plain_text_bin.toPlainText()
+
+        from bin2str import bin2str
+        text = bin2str(text)
+
+        self.plain_text_str.setPlainText(text)
+
+    def _str2bin(self):
+        text = self.plain_text_str.toPlainText()
+
+        from bin2str import str2bin
+        text = str2bin(text)
+
+        self.plain_text_bin.setPlainText(text)
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    w = Widget()
+    w.show()
+
+    app.exec()
