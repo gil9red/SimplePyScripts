0d0189c5cdd4404caf1d1b1746fb90b2f62b4e38
Ilya Petrash <ilya.petrash@inbox.ru>
2017-08-14 01:58:33 +0500

    Append "download_file_with_progress/download_in_file__using_tqdm.py" Append "download_file_with_progress/download_in_memory__using_tqdm.py"

---
+++ download_file_with_progress/download_in_file__using_tqdm.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+def sizeof_fmt(num):
+    for x in ['bytes', 'KB', 'MB', 'GB']:
+        if num < 1024.0:
+            return "%3.1f %s" % (num, x)
+        num /= 1024.0
+    return "%3.1f%s" % (num, 'TB')
+
+
+from tqdm import tqdm
+import requests
+
+url = 'https://github.com/gil9red/NotesManager/raw/master/bin.rar'
+# Streaming, so we can iterate over the response.
+rs = requests.get(url, stream=True)
+
+# Total size in bytes.
+total_size = int(rs.headers.get('content-length', 0))
+print('From content-length:', sizeof_fmt(total_size))
+
+chunk_size = 1024
+num_bars = int(total_size / chunk_size)
+
+import os
+file_name = os.path.basename(url)
+
+import sys
+
+with open(file_name, mode='wb') as f:
+    for data in tqdm(rs.iter_content(chunk_size), total=num_bars, unit='KB', file=sys.stdout):
+        f.write(data)
+
+# Read from file
+file_data = open(file_name, mode='rb').read()
+print('File data size:', sizeof_fmt(len(file_data)))
