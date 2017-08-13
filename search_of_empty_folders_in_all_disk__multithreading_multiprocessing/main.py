78c4ceff5784ab7d900a2f46fb79aa9c9f903e4b
Ilya Petrash <ilya.petrash@inbox.ru>
2017-08-13 22:15:49 +0500

    Append "search_of_empty_folders_in_all_disk__multithreading_multiprocessing"

---
+++ search_of_empty_folders_in_all_disk__multithreading_multiprocessing/main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+def search_empty_folders(disk):
+    with open('log of ' + disk[0], mode='w') as f:
+        import os
+        for root, dirs, files in os.walk(disk):
+            if not dirs and not files:
+                f.write(root)
+                f.write('\n')
+
+
+if __name__ == '__main__':
+    import psutil
+    disk_list = [disk.device for disk in psutil.disk_partitions() if 'fixed' in disk.opts]
+
+    print('Start')
+    import time
+    t = time.clock()
+
+    # Number of thread or process
+    worker_number = len(disk_list)
+
+    # If need use threads
+    # from multiprocessing.dummy import Pool
+    # or:
+    from multiprocessing import Pool
+    pool = Pool(worker_number)
+
+    results = pool.map(search_empty_folders, disk_list)
+
+    # close the pool and wait for the work to finish
+    pool.close()
+    pool.join()
+
+    print('Finish! Elapsed time: {:.3} secs'.format(time.clock() - t))
