94835e1af4bb76054586403f93a61187a162c783
ipetrash <ilya.petrash@inbox.ru>
2017-08-17 10:42:32 +0500

    Append kivy_examples/generate_form_with_cycle.py

---
+++ kivy_examples/generate_form_with_cycle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+
+
+class MyWidget(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.cols = 1
+        self.spacing = 10
+        self.padding = 10
+
+        label_list = ['Собака', 'Сосед', 'Кот', 'Биткоин']
+        for i, title in enumerate(label_list):
+            label = Label(text=title)
+            label.id = 'id:' + str(i)
+
+            self.add_widget(label)
+
+
+class MyApp(App):
+    def build(self):
+        return MyWidget()
+
+
+if __name__ == '__main__':
+    MyApp().run()
