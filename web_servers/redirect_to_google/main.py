501916b16dce4404442a63ee683f7f646328f412
ipetrash <ilya.petrash@inbox.ru>
2017-08-20 23:59:33 +0500

    Append "redirect_to_google"

---
+++ web_servers/redirect_to_google/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from flask import Flask, redirect
+app = Flask(__name__)
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+
+@app.route("/")
+def index():
+    return redirect('https://google.ru')
+
+
+if __name__ == '__main__':
+    # Localhost
+    app.run(port=5001)
+
+    # # Public IP
+    # app.run(host='0.0.0.0')
