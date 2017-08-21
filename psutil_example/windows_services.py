3d0b2d1305aefa29820bd1634e26224ca662208d
ipetrash <ilya.petrash@inbox.ru>
2017-08-21 11:59:47 +0500

    Append psutil_example/windows_services.py

---
+++ psutil_example/windows_services.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install psutil
+import psutil
+
+# pip install tabulate
+from tabulate import tabulate
+
+win_service_list = list(psutil.win_service_iter())
+print('Win service list ({}):'.format(len(win_service_list)))
+
+win_service_list.sort(key=lambda x: x.status())
+
+# for win_service in win_service_list:
+#     print(win_service, win_service.as_dict())
+# #
+# # OR:
+# #
+if win_service_list:
+    headers = ['name', 'display_name', 'status', 'start_type', 'username', 'pid', 'binpath', 'description']
+
+    rows = []
+    for win_service in win_service_list:
+        info = win_service.as_dict()
+        row = [info[header] for header in headers]
+        rows.append(row)
+
+    headers = [header.upper() for header in headers]
+
+    print(tabulate(rows, headers=headers, tablefmt="grid", showindex=True))
+    print()
+
+print()
+name = win_service_list[0].name()
+print('Windows service info: {}'.format(name))
+win_service = psutil.win_service_get(name)
+print('  win_service:', win_service)
+print('  win_service info:', win_service.as_dict())
