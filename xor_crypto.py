7c3e004f8038cf7942226283fe0f5c184008b561
ipetrash <ilya.petrash@inbox.ru>
2017-08-22 19:33:36 +0500

    #

--- xor_crypto.py
+++ xor_crypto.py
@@ -5,10 +5,22 @@ __author__ = 'ipetrash'
 
 
 def crypto_xor_1(message, secret):
+    """
+    Каждый символ xor'ится одним числом.
+    Уязвимо для частотного анализа.
+
+    """
+
     return ''.join(chr(ord(c) ^ secret) for c in message)
 
 
 def crypto_xor_2(message, secret):
+    """
+    Каждый символ xor'ится последовательно каждым символом из secret.
+    Уязвимо для частотного анализа.
+
+    """
+
     new_chars = list()
 
     for num_chr in (ord(c) for c in message):
@@ -21,6 +33,16 @@ def crypto_xor_2(message, secret):
 
 
 def crypto_xor_3(message, secret):
+    """
+    Символы из message и secret xor'ятся последовательно: message[i] ^ secret[i] и когда
+    длина secret заканчивается, счетчик сбрасывается и xor продолжается: message[i] ^ secret[j]
+
+    Если secret меньше message, тогда частоты символов шифрованного сообщения распределяются,
+    т.к. в этом случае один и тот же символ из message может принять в шифрованном сообщение
+    разные значения (зависит от того на какой символ из secret попадет).
+
+    """
+
     new_chars = list()
     i = 0
 
