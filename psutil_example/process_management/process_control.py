a2d8eb73dcd0cc2cee200f9ad2dd6f8962ee082c
ipetrash <ilya.petrash@inbox.ru>
2017-08-22 12:48:42 +0500

    Append process_management/process_control.py

---
+++ psutil_example/process_management/process_control.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+def func():
+    import time
+    i = 1
+
+    import threading
+
+    while True:
+        print(threading.current_thread(), i)
+        i += 1
+
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    from multiprocessing import Process
+    p = Process(target=func)
+    p.start()
+    print(p.pid)
+
+    import psutil
+    process = psutil.Process(p.pid)
+    print('is_running:', process.is_running())
+
+    # from process_detail_info import print_info
+    # print_info(p.pid)
+
+    import time
+    time.sleep(4)
+
+    print('suspend on 3 seconds')
+    process.suspend()
+    time.sleep(3)
+
+    print('resume')
+    process.resume()
+
+    print('terminate after 5 seconds')
+    time.sleep(5)
+
+    process.terminate()
+    print('terminate')
+
+    process.wait(timeout=3)
