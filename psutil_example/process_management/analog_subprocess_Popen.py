4e7caa2d664de20bedfa4ff438c27b54d33ccb45
ipetrash <ilya.petrash@inbox.ru>
2017-08-22 13:11:40 +0500

    Append process_management/analog_subprocess_Popen.py

---
+++ psutil_example/process_management/analog_subprocess_Popen.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install psutil
+import psutil
+from subprocess import PIPE
+p = psutil.Popen(['python', "-c", "print('hello')"], stdout=PIPE)
+print(p.name())
+print(p.username())
+print(p.communicate())
+print(p.wait(timeout=2))
