d3ee62b48dd1277aa68244f7ec2966183517b931
ipetrash <ilya.petrash@inbox.ru>
2017-08-24 16:23:58 +0500

    #

--- generate_qrcode/main.py
+++ generate_qrcode/main.py
@@ -7,18 +7,18 @@ __author__ = 'ipetrash'
 text = 'Hello World!'
 
 # pip install qrcode
-from qrcode import make as make_qrcode
-im = make_qrcode(text)
+import qrcode
+img = qrcode.make(text)
 
 # In memory
 import io
 io_data = io.BytesIO()
-im.save(io_data, 'png')
+img.save(io_data, 'png')
 data = io_data.getvalue()
 print(data)
 
 # In file
-im.save('qr_code_1.png')
+img.save('qr_code_1.png')
 #
 # In file, version 2:
 with open('qr_code_2.png', 'wb') as f:
