e2342ddf4f9324d168b4522ac769dbf2c5eac613
ipetrash <ilya.petrash@inbox.ru>
2017-08-24 18:56:48 +0500

    #

--- python_object_to_json.py
+++ python_object_to_json.py
@@ -4,6 +4,20 @@
 __author__ = 'ipetrash'
 
 
+class SubField:
+    def __init__(self):
+        self.flag = True
+
+
+class Field:
+    def __init__(self, tag1, tag2, sub_field_flag=True):
+        self.tag1 = tag1
+        self.tag2 = tag2
+
+        self.sub_field = SubField()
+        self.sub_field.flag = sub_field_flag
+
+
 class Object:
     a = 0
     b = '123'
@@ -11,6 +25,13 @@ class Object:
     def __init__(self):
         self.c = 3
         self.items = [1, 2, 3, 4]
+        self.maps = {
+            'is': True,
+            'not': 0,
+        }
+
+        self.field = Field('abc', 'tag2')
+        self.field_2 = Field(777, False, sub_field_flag=False)
 
 
 def object_to_dict(object):
@@ -18,12 +39,23 @@ def object_to_dict(object):
     fields.update(object.__class__.__dict__)
     fields.update(object.__dict__)
 
-    return dict(filter(lambda x: not x[0].startswith('_'), fields.items()))
+    fields = dict(filter(lambda x: not x[0].startswith('_'), fields.items()))
 
-obj = Object()
-fields = object_to_dict(obj)
-print(fields)
-print()
+    new_fields = dict()
+    for k, v in fields.items():
+        if hasattr(v, '__dict__'):
+            v = object_to_dict(v)
 
-import json
-print(json.dumps(fields, ensure_ascii=False, indent=4))
+        new_fields[k] = v
+
+    return new_fields
+
+
+if __name__ == '__main__':
+    obj = Object()
+    fields = object_to_dict(obj)
+    print(fields)
+    print()
+
+    import json
+    print(json.dumps(fields, ensure_ascii=False, indent=4, sort_keys=True))
