4ee80d7fb46bc18470ae9891d839d09454553db3
ipetrash <ilya.petrash@inbox.ru>
2017-08-27 07:08:31 +0500

    #

--- web_servers/hello_world/main.py
+++ web_servers/hello_world/main.py
@@ -18,10 +18,15 @@ def index():
 
 if __name__ == '__main__':
     app.debug = True
+
+    # :param threaded: should the process handle each request in a separate
+    #                  thread?
+    # :param processes: if greater than 1 then handle each request in a new process
+    #                   up to this maximum number of concurrent processes.
     app.threaded = True
 
     # Localhost
-    app.run(port=5001)
+    app.run(port=5000)
 
     # # Public IP
     # app.run(host='0.0.0.0')
