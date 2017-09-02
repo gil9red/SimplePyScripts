19d4eb23392b792c02699c64738ccec5fac9b3b2
ipetrash <ilya.petrash@inbox.ru>
2017-09-02 20:30:45 +0500

    #

--- web_servers/get_upload_image_info/main.py
+++ web_servers/get_upload_image_info/main.py
@@ -107,6 +107,12 @@ def sizeof_fmt(num):
 
 from flask import Flask, jsonify, render_template_string, redirect, request
 app = Flask(__name__)
+
+# http://flask.pocoo.org/docs/0.12/config/#config
+# By default Flask will serialize JSON objects in a way that the keys are ordered. This is done in order to
+# ensure that independent of the hash seed of the dictionary the return value will be consistent to not trash external
+# HTTP caches. You can override the default behavior by changing this variable. This is not recommended but might give
+# you a performance improvement on the cost of cacheability.
 app.config['JSON_SORT_KEYS'] = False
 
 import logging
