297acfb3611252202e4004569cf78f0795a3f3df
ipetrash <ilya.petrash@inbox.ru>
2017-09-02 19:12:01 +0500

    #

--- web_servers/get_exif_info/main.py
+++ web_servers/get_exif_info/main.py
@@ -115,7 +115,10 @@ def index():
 
         <br><br>
         
-        <div class="exif show" style="display: none"></div>
+        <div class="exif show" style="display: none">
+            <pre></pre>
+            <img class="upload img preview"/>
+        </div>
 
         <script>
         $(document).ready(function() {
@@ -197,7 +200,7 @@ def index():
                         
                         json_str = syntaxHighlight(json_str);
 
-                        $('.exif.show').html('<pre>' + json_str + '</pre>');
+                        $('.exif.show > pre').html(json_str);
                         $('.exif.show').show();
                     },
                 });
@@ -209,6 +212,37 @@ def index():
     </body>
 </html>
 ''')
+
+
+# TODO: append preview from server and show size in bytes and pixels
+# def img_to_base64_html(file_name_or_bytes):
+#     if type(file_name_or_bytes) == str:
+#         with open(file_name, mode='rb') as f:
+#             img_bytes = f.read()
+#
+#     else:
+#         img_bytes = file_name_or_bytes
+#
+#     import io
+#     bytes_io = io.BytesIO(img_bytes)
+#
+#     from PIL import Image
+#     img = Image.open(bytes_io)
+#
+#     import base64
+#     img_base64 = base64.b64encode(img_bytes).decode('utf-8')
+#     # print(img_base64)
+#
+#     return 'data:image/{};base64,'.format(img.format.lower()) + img_base64
+#
+#
+# if __name__ == '__main__':
+#     file_name = 'img.jpg'
+#     img_base64 = img_to_base64_html(file_name)
+#     print('[len {}]: {}...'.format(len(img_base64), img_base64[:50]))
+#
+#     with open(file_name + '_base64.txt', mode='w', encoding='utf-8') as f:
+#         f.write(img_base64)
 
 
 @app.route("/get_exif", methods=['POST'])
