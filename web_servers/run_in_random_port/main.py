b32bc9da6b58a4a06d30005c1e311ad516d3da6e
ipetrash <ilya.petrash@inbox.ru>
2017-09-04 10:45:44 +0500

    Append web_servers/run_in_random_port

---
+++ web_servers/run_in_random_port/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+# SOURCE: https://stackoverflow.com/a/5089963/5909792
+
+
+from flask import Flask, request
+import socket
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, world! running on %s' % request.host
+
+
+if __name__ == '__main__':
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('localhost', 0))
+    port = sock.getsockname()[1]
+    print('sock.getsockname:', sock.getsockname())
+
+    sock.close()
+    app.run(port=port)
