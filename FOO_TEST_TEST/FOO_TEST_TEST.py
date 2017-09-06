0ec1e9015b6bda5862b4e8bd6c96d689a6878744
ipetrash <ilya.petrash@inbox.ru>
2017-09-07 00:51:33 +0500

    #

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -4,3 +4,18 @@
 __author__ = 'ipetrash'
 
 
+"""
+function underlineText(text) {
+    var result = '';
+
+    for (var i = 0, length = text.length; i < length; ++i) {
+        result += text.charAt(i)+'\u0332';
+    }
+
+    return result;
+}
+"""
+
+text = ' Вася, HelloWorld! '
+underline_text = '\u0332'.join(text)
+print(underline_text)
