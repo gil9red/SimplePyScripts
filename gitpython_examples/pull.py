2ec06bc3d3fdc8e7319a5053533bc42195e0597c
ipetrash <ilya.petrash@inbox.ru>
2017-09-09 14:47:16 +0500

    #

---
+++ gitpython_examples/pull.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+if __name__ == '__main__':
+    from common import get_repo
+    repo = get_repo()
+    repo.remotes.origin.pull()
