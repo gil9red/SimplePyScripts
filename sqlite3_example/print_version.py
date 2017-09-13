ead0750a03156b6d5784a2f9f80fcf16f9a5a132
ipetrash <ilya.petrash@inbox.ru>
2017-09-13 23:57:44 +0500

    Append print_version.py

---
+++ sqlite3_example/print_version.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import sqlite3
+with sqlite3.connect(':memory:') as connect:
+    version = connect.execute('SELECT SQLITE_VERSION()').fetchone()
+    print("SQLite version: %s" % version)
