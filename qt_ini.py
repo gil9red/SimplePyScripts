098205f3a58235e66905f051ee24e1377c3fb285
ipetrash <ilya.petrash@inbox.ru>
2017-09-14 11:52:51 +0500

    Append qt_ini.py

---
+++ qt_ini.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+try:
+    from PyQt4.QtCore import QSettings
+except:
+    from PyQt5.QtCore import QSettings
+
+
+if __name__ == '__main__':
+    config = QSettings('config.ini', QSettings.IniFormat)
+
+    counter = int(config.value('counter', 0))
+    config.setValue('counter', counter + 1)
+
+    config.setValue('key2', 'abc')
