1b2bf00ab3ec80047ee626814ed7375cf0607e7c
ipetrash <ilya.petrash@inbox.ru>
2017-09-16 23:38:12 +0500

    #

--- datetime_example/datetime_example.py
+++ datetime_example/datetime_example.py
@@ -7,12 +7,12 @@ __author__ = 'ipetrash'
 
 from datetime import date
 
+
 if __name__ == '__main__':
     # dates are easily constructed and formatted
     now = date.today()
     print(now)
     print(now.strftime("%m-%d-%y. %d %b %Y is a %A on the %d day of %B."))
-
 
     # dates support calendar arithmetic
     birthday = date(1964, 7, 31)
