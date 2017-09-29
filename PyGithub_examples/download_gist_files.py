1c5019fce6019c19f69250843ba3e695313a90f3
ipetrash <ilya.petrash@inbox.ru>
2017-09-30 02:57:56 +0500

    #

--- PyGithub_examples/download_gist_files.py
+++ PyGithub_examples/download_gist_files.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     # Filter by commit version
     history_list = [
         x for x in gist.history if x.version in [
-            'd4572ecd5b66ac06a64973cd35ef71936ebf84cd'
+            'd4572ecd5b66ac06a64973cd35ef71936ebf84cd',
             '8040c03f7ee008b0d3ce315a34e4c364f164d939',
             '798e7a89e02cac541e1866ac16278ce9a068cf68',
             '758e49da06e0bb5b6b455e9035a47047ece714d0',
