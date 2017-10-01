ac213988ca9e14e26c1a4c2c50417535d4649786
ipetrash <ilya.petrash@inbox.ru>
2017-10-01 23:23:42 +0500

    Append run_temp_bat_with_python.py

---
+++ run_temp_bat_with_python.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import sys
+import os
+
+
+FILE_NAME_RUN = '__temporary_run.bat'
+TEXT_RUN = '''\
+@echo off
+start {python} -c "print('Hello World');import time;time.sleep(5)"
+'''
+TEXT_RUN = TEXT_RUN.format(python=sys.executable)
+
+
+if __name__ == '__main__':
+    with open(FILE_NAME_RUN, mode='w') as f:
+        f.write(TEXT_RUN)
+
+    os.system(FILE_NAME_RUN)
+    os.remove(FILE_NAME_RUN)
