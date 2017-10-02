7c0b10feb6c1c415d90632fd7f67f2b23276db38
ipetrash <ilya.petrash@inbox.ru>
2017-10-02 16:52:50 +0500

    Append webserver__cherrypy__examples/all_exception_handler.py

---
+++ webserver__cherrypy__examples/all_exception_handler.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+# pip install cherrypy
+# https://github.com/cherrypy/cherrypy
+# SOURCE: https://github.com/cherrypy/cherrypy/blob/master/cherrypy/tutorial/tut01_helloworld.py
+
+import cherrypy
+
+
+def all_exception_handler(status, message, traceback, version):
+    response = cherrypy.response
+    response.headers['Content-Type'] = 'application/json'
+
+    import json
+    return json.dumps({
+        'about': 'Catch error!',
+        'status': status,
+        'message': message,
+        'traceback': traceback,
+        'version': version,
+    })
+
+
+class Root:
+    # Set a custom response for errors.
+    _cp_config = {'error_page.default': all_exception_handler}
+
+    # Expose the index method through the web. CherryPy will never
+    # publish methods that don't have the exposed attribute set to True.
+    @cherrypy.expose
+    def index(self):
+        # CherryPy will call this method for the root URI ("/") and send
+        # its return value to the client. Because this is tutorial
+        # lesson number 01, we'll just send something really simple.
+        # How about...
+        return 'Hello world!<br><a href="/error">Get error</a>'
+
+    @cherrypy.expose
+    def error(self):
+        _ = 1 / 0
+
+        return 'Bad!'
+
+
+if __name__ == '__main__':
+    # CherryPy always starts with app.root when trying to map request URIs
+    # to objects, so we need to mount a request handler root. A request
+    # to '/' will be mapped to HelloWorld().index().
+
+    # Set port
+    cherrypy.config.update({'server.socket_port': 9090})
+    cherrypy.quickstart(Root())
