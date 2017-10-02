c48e539800f435c8c81864c77f0c9c5503e71cd9
ipetrash <ilya.petrash@inbox.ru>
2017-10-02 13:42:17 +0500

    #

--- webserver__cherrypy__examples/hello_world.py
+++ webserver__cherrypy__examples/hello_world.py
@@ -28,7 +28,6 @@ if __name__ == '__main__':
     # to objects, so we need to mount a request handler root. A request
     # to '/' will be mapped to HelloWorld().index().
 
-    # # Set other port
-    # cherrypy.config.update({'server.socket_port': 9090})
-
+    # Set port
+    cherrypy.config.update({'server.socket_port': 9090})
     cherrypy.quickstart(HelloWorld())
