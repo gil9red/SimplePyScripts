930d5411b050f25ef7d0ac3927990d4e55bad0ee
ipetrash <ilya.petrash@inbox.ru>
2017-10-02 18:48:18 +0500

    Append webserver__cherrypy__examples: default_method__uri.py, expose_methods.py

---
+++ webserver__cherrypy__examples/expose_methods.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import cherrypy
+
+
+class HelloWorld:
+    @cherrypy.expose
+    def index(self):
+        # Let's link to another method here.
+        return 'We have an <a href="show_msg">important message</a> for you!'
+
+    @cherrypy.expose
+    def show_msg(self):
+        # Here's the important message!
+        return 'Hello world!'
+
+
+if __name__ == '__main__':
+    # CherryPy always starts with app.root when trying to map request URIs
+    # to objects, so we need to mount a request handler root. A request
+    # to '/' will be mapped to HelloWorld().index().
+    cherrypy.quickstart(HelloWorld())
