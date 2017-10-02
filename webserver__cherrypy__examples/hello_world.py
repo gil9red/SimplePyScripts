31f84db8e4ab39ec73483f840a6d1716767b712b
ipetrash <ilya.petrash@inbox.ru>
2017-10-02 12:55:13 +0500

    #

--- webserver__cherrypy__examples/hello_world.py
+++ webserver__cherrypy__examples/hello_world.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
-
 
 # pip install cherrypy
 # https://github.com/cherrypy/cherrypy
+# SOURCE: https://github.com/cherrypy/cherrypy/blob/master/cherrypy/tutorial/tut01_helloworld.py
 
 import cherrypy
 
 
-class HelloWorld(object):
+class HelloWorld:
+    """ Sample request handler class. """
+
+    # Expose the index method through the web. CherryPy will never
+    # publish methods that don't have the exposed attribute set to True.
     @cherrypy.expose
     def index(self):
-        return "Hello World!"
+        # CherryPy will call this method for the root URI ("/") and send
+        # its return value to the client. Because this is tutorial
+        # lesson number 01, we'll just send something really simple.
+        # How about...
+        return 'Hello world!'
 
 
 if __name__ == '__main__':
+    # CherryPy always starts with app.root when trying to map request URIs
+    # to objects, so we need to mount a request handler root. A request
+    # to '/' will be mapped to HelloWorld().index().
     cherrypy.quickstart(HelloWorld())
