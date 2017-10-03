fb5aa6cc32e3cef94418415ee7b7a53398dbf330
ipetrash <ilya.petrash@inbox.ru>
2017-10-04 03:43:55 +0500

    #

--- web_servers/upload_and_download_files/test_download_from_script.py
+++ web_servers/upload_and_download_files/test_download_from_script.py
@@ -9,7 +9,7 @@ def sizeof_fmt(num):
         if num < 1024.0:
             return "%3.1f%s" % (num, x)
         num /= 1024.0
-    return "%3.1f%s" % (num, 'TB')
+    return "%3.1f %s" % (num, 'TB')
 
 
 def progress(count, block_size, total_size):
