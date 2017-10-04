1da0287138e7e90ee4fe50c64d015e37909c8464
ipetrash <ilya.petrash@inbox.ru>
2017-10-04 19:03:25 +0500

    #

--- generate_uuid_guid.py
+++ generate_uuid_guid.py
@@ -13,7 +13,9 @@ print(uuid.uuid1())  # UUID('a8098c1a-f86e-11da-bd1a-00112444be1e')
 print(uuid.uuid3(uuid.NAMESPACE_DNS, 'python.org'))  # UUID('6fa459ea-ee8a-3ca4-894e-db77e160355e')
 
 # make a random UUID
-print(uuid.uuid4())  # UUID('16fd2706-8baf-433b-82eb-8c7fada847da')
+x = uuid.uuid4()
+print(x)  # 356f4f13-17b9-465d-b541-e40f34ec71a9
+print(x.hex.upper())  # 356F4F1317B9465DB541E40F34EC71A9
 
 # make a UUID using a SHA-1 hash of a namespace UUID and a name
 print(uuid.uuid5(uuid.NAMESPACE_DNS, 'python.org'))  # UUID('886313e1-3b8a-5372-9b90-0c9aee199e5d')
@@ -22,6 +24,8 @@ print()
 print('From UUID / GUID')
 # make a UUID from a string of hex digits (braces and hyphens ignored)
 x = uuid.UUID('{00010203-0405-0607-0809-0a0b0c0d0e0f}')
+print(x.hex)  # '000102030405060708090a0b0c0d0e0f'
+print(x.hex.upper())  # '000102030405060708090A0B0C0D0E0F'
 
 # convert a UUID to a string of hex digits in standard form
 print(str(x))  # '00010203-0405-0607-0809-0a0b0c0d0e0f'
