bb71a695ad35b8055b1850f1ef4208d1ce8920ee
ipetrash <ilya.petrash@inbox.ru>
2017-10-05 03:10:43 +0500

    Append webserver__cherrypy__examples\subscribe_to__on_start.py

---
+++ webserver__cherrypy__examples/subscribe_to__on_start.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import cherrypy
+
+
+class RootServer:
+    def __init__(self):
+        self.running = False
+
+        cherrypy.engine.subscribe('start', self.on_start)
+        cherrypy.engine.subscribe('stop', self.on_stop)
+        cherrypy.engine.subscribe('exit', self.on_exit)
+
+    def on_start(self):
+        print('start')
+        self.running = True
+
+        # Exit!
+        cherrypy.engine.exit()
+
+    def on_stop(self):
+        print('stop')
+        self.running = False
+
+    def on_exit(self):
+        print('exit')
+
+
+if __name__ == '__main__':
+    # Set port
+    cherrypy.config.update({'server.socket_port': 9090})
+
+    cherrypy.quickstart(RootServer())
