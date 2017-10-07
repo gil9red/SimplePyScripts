f66bc38eb86fd02671f217817ed32d172a142f26
ipetrash <ilya.petrash@inbox.ru>
2017-10-07 21:35:08 +0500

    Append python_interpreter_info.py

---
+++ python_interpreter_info.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import platform
+print('Implementation:', platform.python_implementation())
+print('Version:', platform.python_version())
+print('Compiler:', platform.python_compiler())
+
+import sys
+print('Executable:', sys.executable)
