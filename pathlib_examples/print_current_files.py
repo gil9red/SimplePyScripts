805f6633987d8475fda800006abb3c6682eef490
ipetrash <ilya.petrash@inbox.ru>
2017-10-11 10:45:57 +0500

    Append pathlib_examples/print_current_files.py

---
+++ pathlib_examples/print_current_files.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import pathlib
+current_dir = pathlib.Path(__file__).parent
+print(current_dir)
+
+for file_name in current_dir.glob('*.py'):
+    print('    {}:\n        {}\n'.format(file_name, file_name.read_bytes()))
