b995a224d9538212e501a9eb5bcbe0cb49e9ddc9
ipetrash <ilya.petrash@inbox.ru>
2017-10-23 12:05:57 +0500

    #

--- calculator/use_numexpr_module/main.py
+++ calculator/use_numexpr_module/main.py
@@ -8,3 +8,6 @@ import numexpr as ne
 print(ne.evaluate('2 + 2 * 2'))
 print(ne.evaluate('10 ** 3'))
 print(ne.evaluate('2 ** 10'))
+print(ne.evaluate('sin(2 ** 10)'))
+print(ne.evaluate('(0xFF + 255) / 0b1010'))
+print(ne.evaluate('(0xFF + 255) // 0b1010'))
