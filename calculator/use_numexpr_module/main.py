fd1ff8e9b866dceff90b064ccd67c8751956ebf8
ipetrash <ilya.petrash@inbox.ru>
2017-10-23 12:17:29 +0500

    #

--- calculator/use_numexpr_module/main.py
+++ calculator/use_numexpr_module/main.py
@@ -4,10 +4,10 @@
 __author__ = 'ipetrash'
 
 
-import numexpr as ne
-print(ne.evaluate('2 + 2 * 2'))
-print(ne.evaluate('10 ** 3'))
-print(ne.evaluate('2 ** 10'))
-print(ne.evaluate('sin(2 ** 10)'))
-print(ne.evaluate('(0xFF + 255) / 0b1010'))
-print(ne.evaluate('(0xFF + 255) // 0b1010'))
+import numexpr
+print(numexpr.evaluate('2 + 2 * 2'))
+print(numexpr.evaluate('10 ** 3'))
+print(numexpr.evaluate('2 ** 10'))
+print(numexpr.evaluate('sin(2 ** 10)'))
+print(numexpr.evaluate('(0xFF + 255) / 0b1010'))
+print(numexpr.evaluate('(0xFF + 255) // 0b1010'))
