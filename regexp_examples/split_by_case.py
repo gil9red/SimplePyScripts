75bbcae07190ab44dea99cf9674fb85bb55b2da1
ipetrash <ilya.petrash@inbox.ru>
2017-10-30 18:12:28 +0500

    Append regexp_examples/split_by_case.py

---
+++ regexp_examples/split_by_case.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+def split_by_case(text: str) -> list:
+    import re
+    return re.findall(r'[a-zA-Z][^A-Z]+', text)
+
+
+if __name__ == '__main__':
+    print(split_by_case('fadeInLeft'))
+    print(split_by_case('CharSequence'))
+    print(split_by_case('String'))
+    print(split_by_case('compareToIgnoreCase'))
+
+    assert split_by_case('fadeInLeft') == ['fade', 'In', 'Left']
+    assert split_by_case('CharSequence') == ['Char', 'Sequence']
+    assert split_by_case('String') == ['String']
+    assert split_by_case('compareToIgnoreCase') == ['compare', 'To', 'Ignore', 'Case']
