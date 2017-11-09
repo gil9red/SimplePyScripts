a6e240f47156d950abb664b5b120b2b9e83a24ce
ipetrash <ilya.petrash@inbox.ru>
2017-11-09 11:14:05 +0500

    #

--- Environment variables/EnvironmentVariables.py
+++ Environment variables/EnvironmentVariables.py
@@ -1,29 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 __author__ = 'ipetrash'
 
 
 import os
 
-if __name__ == '__main__':
-    # Вывести все переменные окружения (environment variables)
-    print("Environment variables:\n{}".format(os.environ))
 
-    print("\nEnvironment variables:")
-    for var, value in os.environ.items():
-        print("'{}': '{}'".format(var, value))
+# Вывести все переменные окружения (environment variables)
+print("Environment variables:\n{}".format(os.environ))
 
-    # Получение значения переменной окружения Path
-    env_path = os.environ["Path"]  # или os.environ.get("Path")
+print("\nEnvironment variables:")
+for var, value in os.environ.items():
+    print("'{}': '{}'".format(var, value))
 
-    # Вывести значение переменной окружения Path
-    print("\nPath: {}".format(env_path))
+#
+# Получение значения переменной окружения Path
+env_path = os.environ["Path"]  # или os.environ.get("Path")
 
-    # Разделение строки с путями на список
-    values_env_path = env_path.split(";")
-    print("Path: {}".format(values_env_path))  # Вывод списка
+# Вывести значение переменной окружения Path
+print("\nPath: {}".format(env_path))
 
-    # Вывод списка путей
-    print("Path:")
-    for i, val in enumerate(values_env_path):
-        # Если элемент не пустой
-        if val:
-            print("{}. '{}'".format(i + 1, val) + (": not exist" if not os.path.exists(val) else ""))
+# Разделение строки с путями на список
+values_env_path = env_path.split(";")
+print("Path: {}".format(values_env_path))  # Вывод списка
+
+# Вывод списка путей
+print("Path:")
+for i, val in enumerate(values_env_path, 1):
+    # Если элемент не пустой
+    if val:
+        print("{}. '{}'".format(i, val) + (": not exist" if not os.path.exists(val) else ""))
