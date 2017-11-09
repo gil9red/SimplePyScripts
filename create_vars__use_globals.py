cf8a22c247fecba117c5cfeb4aea4da80cb395cc
ipetrash <ilya.petrash@inbox.ru>
2017-11-09 10:42:01 +0500

    Append create_vars__use_globals.py

---
+++ create_vars__use_globals.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+globals()['abc'] = 123
+print(abc)  # 123
+
+for key, value in {"a": 1, "b": 2}.items():
+    globals()[key] = value
+
+print(a)  # 1
+print(b)  # 2
+print()
+
+
+number = 0
+
+
+def counter():
+    # If not exists global <number>
+    # if 'number' not in globals():
+    #     globals()['number'] = 0
+
+    globals()['number'] += 1
+
+
+counter()
+
+print(number)  # 1
+
+counter()
+counter()
+print(number)  # 3
