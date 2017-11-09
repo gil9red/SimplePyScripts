2a90a404a52dbc408e9e23f2a70f16866ca79caa
ipetrash <ilya.petrash@inbox.ru>
2017-11-09 11:06:16 +0500

    #

--- expand_env_AppData.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-__author__ = 'ipetrash'
-
-
-import os
-app_data = os.path.expandvars('%AppData%')
-print(app_data)
---
+++ expand_env__APPDATA.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# https://ru.wikipedia.org/wiki/Переменная_среды_Windows
+
+import os
+app_data = os.path.expandvars('%APPDATA%')
+print(app_data)
+
+print()
+print(os.path.expandvars('%OS%'))
+print(os.path.expandvars('%COMPUTERNAME%'))
+print(os.path.expandvars('%WINDIR%'))
+print()
+print(os.path.expandvars('%NUMBER_OF_PROCESSORS%'))
+print(os.path.expandvars('%PROCESSOR_ARCHITECTURE%'))
