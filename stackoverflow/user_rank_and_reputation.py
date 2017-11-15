ae8b728e9fa2a9094e99283a103151173e8eaf3a
ipetrash <ilya.petrash@inbox.ru>
2017-11-15 10:54:50 +0500

    #

--- stackoverflow/user_rank_and_reputation.py
+++ stackoverflow/user_rank_and_reputation.py
@@ -4,18 +4,26 @@
 __author__ = 'ipetrash'
 
 
-URL = 'https://stackexchange.com/leagues/filter-users/355/AllTime/2015-03-27/?filter=gil9red&sort=reputationchange'
+def get_user_rank_and_reputation() -> (str, str):
+    url = 'https://stackexchange.com/leagues/filter-users/355/AllTime/2015-03-27/?filter=gil9red&sort=reputationchange'
 
-import requests
-rs = requests.get(URL)
-text = rs.text
-# print(text)
+    import requests
+    rs = requests.get(url)
+    text = rs.text
+    # print(text)
 
-import re
-match = re.search('>#(.+)</span> all time rank', text)
-rank = match.group(1)
-print('rank:', rank)
+    import re
+    match = re.search('>#(.+)</span> all time rank', text)
+    rank = match.group(1)
 
-match = re.search('>(.+)</span> all time reputation', text)
-reputation = match.group(1).replace(',', '')
-print('reputation:', reputation)
+    match = re.search('>(.+)</span> all time reputation', text)
+    reputation = match.group(1).replace(',', '')
+
+    return rank, reputation
+
+
+if __name__ == '__main__':
+    rank, reputation = get_user_rank_and_reputation()
+
+    print('rank:', rank)
+    print('reputation:', reputation)
