2ab55c70b8e3e8f6604d0c3d12c8e20f4fce013e
ipetrash <ilya.petrash@inbox.ru>
2017-11-20 23:19:51 +0500

    Append qt_pixel_draw_image/main_with_fast_draw.py

---
+++ qt_pixel_draw_image/main_with_fast_draw.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+
+def log_uncaught_exceptions(ex_cls, ex, tb):
+    text = '{}: {}:\n'.format(ex_cls.__name__, ex)
+    import traceback
+    text += ''.join(traceback.format_tb(tb))
+
+    print(text)
+    QMessageBox.critical(None, 'Error', text)
+    quit()
+
+
+import sys
+sys.excepthook = log_uncaught_exceptions
+
+
+IMG_FILE_NAME = 'img.png'
+
+
+class Widget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('qt_pixel_draw_image')
+
+        self.img = QImage(IMG_FILE_NAME)
+        width = self.img.size().width()
+        height = self.img.size().height()
+        self.img = self.img.scaled(width * 3, height * 3)
+        self.img_width = self.img.size().width()
+        self.img_height = self.img.size().height()
+
+        self.resize(self.img_width * 2 + 20, self.img_height + 20)
+
+        # Сгенерируем список координат пикселей
+        self.pixel_list = [(y, x) for y in range(self.img_height) for x in range(self.img_width)]
+        # OR:
+        # self.pixel_list = []
+        # for y in range(self.img_height):
+        #     for x in range(self.img_width):
+        #         self.pixel_list.append((y, x))
+
+        # Перемешаем элементы списка случайным образом
+        import random
+        random.shuffle(self.pixel_list)
+
+        self.new_img = QImage(self.img_width, self.img_height, QImage.Format_RGB32)
+        self.new_img.fill(Qt.white)
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self._draw_pixel)
+        self.timer.start(1)  # 1 ms
+
+    def _draw_pixel(self):
+        # Количество пикселей за один шаг
+        pixels_by_step = 20
+
+        for _ in range(pixels_by_step):
+            y, x = self.pixel_list.pop()
+            pixel = self.img.pixel(x, y)
+
+            # Установка пикселя в новой картинке
+            self.new_img.setPixel(x, y, pixel)
+
+            # Если список пустой
+            if not self.pixel_list:
+                self.timer.stop()
+                break
+
+        # Перерисование виджета
+        self.update()
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+
+        # Рисуем старую картинку
+        painter.drawImage(0, 0, self.img)
+
+        # Рисуем новую картинку
+        painter.drawImage(0 + self.img.width() + 10, 0, self.new_img)
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    w = Widget()
+    # w.resize(200, 100)
+    w.show()
+
+    app.exec()
