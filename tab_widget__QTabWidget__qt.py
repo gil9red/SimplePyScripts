f47b1d07d4eee27ad8dc4b40e330558db63fc1e7
ipetrash <ilya.petrash@inbox.ru>
2017-11-29 14:00:39 +0500

    Append tab_widget__QTabWidget__qt.py

---
+++ tab_widget__QTabWidget__qt.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.QtWidgets import *
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.tab_widget = QTabWidget()
+        self.tab_widget.setTabsClosable(True)
+        self.tab_widget.tabCloseRequested.connect(self._on_close_tab)
+
+        tool_bar = self.addToolBar('General')
+        self.action_add_tab = tool_bar.addAction('Add tab')
+        self.action_add_tab.triggered.connect(self.add_tab)
+
+        self.action_remove_tab = tool_bar.addAction('Remove tab')
+        self.action_remove_tab.triggered.connect(self.remove_tab)
+
+        self.setCentralWidget(self.tab_widget)
+
+        self.update_states()
+
+    def update_states(self):
+        self.action_add_tab.setEnabled(self.tab_widget.count() < 3)
+        self.action_remove_tab.setEnabled(self.tab_widget.count() > 0)
+
+    def add_tab(self):
+        tab = QTextEdit()
+        self.tab_widget.addTab(tab, str(self.tab_widget.count() + 1))
+
+        self.update_states()
+
+    def remove_tab(self):
+        index = self.tab_widget.currentIndex()
+        self._on_close_tab(index)
+
+    def _on_close_tab(self, index):
+        if index == -1:
+            return
+
+        self.tab_widget.removeTab(index)
+
+        self.update_states()
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    mw = MainWindow()
+    mw.show()
+
+    app.exec()
