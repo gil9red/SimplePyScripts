46bb6b9f0b02cc8d717ebfc79e228a16f0831963
ipetrash <ilya.petrash@inbox.ru>
2017-12-07 12:48:36 +0500

    #

--- qt_dummy_calculator_window__with_autodown_button_on_key_press.py
+++ qt_dummy_calculator_window__with_autodown_button_on_key_press.py
@@ -3,3 +3,70 @@
 
 __author__ = 'ipetrash'
 
+from PyQt5.QtWidgets import *
+
+
+def log_uncaught_exceptions(ex_cls, ex, tb):
+    text = '{}: {}:\n'.format(ex_cls.__name__, ex)
+    import traceback
+    text += ''.join(traceback.format_tb(tb))
+
+    print(text)
+    QMessageBox.critical(None, 'Error', text)
+    quit()
+
+
+import sys
+
+sys.excepthook = log_uncaught_exceptions
+
+
+class Widget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('dummy_calculator_window')
+
+        self.key_by_button = {i: QPushButton(i) for i in '0123456789'}
+
+        layout = QGridLayout()
+        layout.addWidget(self.key_by_button['7'], 0, 0)
+        layout.addWidget(self.key_by_button['8'], 0, 1)
+        layout.addWidget(self.key_by_button['9'], 0, 2)
+
+        layout.addWidget(self.key_by_button['4'], 1, 0)
+        layout.addWidget(self.key_by_button['5'], 1, 1)
+        layout.addWidget(self.key_by_button['6'], 1, 2)
+
+        layout.addWidget(self.key_by_button['1'], 2, 0)
+        layout.addWidget(self.key_by_button['2'], 2, 1)
+        layout.addWidget(self.key_by_button['3'], 2, 2)
+
+        layout.addWidget(self.key_by_button['0'], 3, 0, 1, 3)
+
+        self.setLayout(layout)
+
+    def keyPressEvent(self, event):
+        key = chr(event.key())
+
+        if key in self.key_by_button:
+            self.key_by_button[key].setDown(True)
+
+        super().keyPressEvent(event)
+
+    def keyReleaseEvent(self, event):
+        key = chr(event.key())
+
+        if key in self.key_by_button:
+            self.key_by_button[key].setDown(False)
+
+        super().keyReleaseEvent(event)
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    w = Widget()
+    w.show()
+
+    app.exec()
