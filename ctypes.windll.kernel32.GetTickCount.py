a21508358cde86f90fa29a42973b3ba9141af905
ipetrash <ilya.petrash@inbox.ru>
2017-12-18 15:00:16 +0500

    Append ctypes.windll.kernel32.GetTickCount.py

---
+++ ctypes.windll.kernel32.GetTickCount.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import ctypes
+GetTickCount = ctypes.windll.kernel32.GetTickCount
+t = GetTickCount()
+
+import time
+time.sleep(1)
+
+print('Elapsed: {} ms'.format(GetTickCount() - t))
