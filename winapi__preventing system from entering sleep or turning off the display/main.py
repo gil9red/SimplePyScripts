fe1231c27108e4f1cd91778eaf401d77ca3f45fb
ipetrash <ilya.petrash@inbox.ru>
2017-12-19 14:18:58 +0500

    #

--- winapi__preventing system from entering sleep or turning off the display/main.py
+++ winapi__preventing system from entering sleep or turning off the display/main.py
@@ -11,24 +11,25 @@ ES_SYSTEM_REQUIRED = 0x00000001
 ES_AWAYMODE_REQUIRED = 0x00000040
 ES_DISPLAY_REQUIRED = 0x00000002
 
-
 import ctypes
 SetThreadExecutionState = ctypes.windll.kernel32.SetThreadExecutionState
 
-#
-# Television recording is beginning. Enable away mode and prevent the sleep idle time-out.
-#
-SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED | ES_AWAYMODE_REQUIRED | ES_DISPLAY_REQUIRED)
 
-#
-# Wait until recording is complete...
-#
+if __name__ == '__main__':
+    #
+    # Television recording is beginning. Enable away mode and prevent the sleep idle time-out.
+    #
+    SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED | ES_AWAYMODE_REQUIRED | ES_DISPLAY_REQUIRED)
 
-# Wait 1 hours
-import time
-time.sleep(60 * 60)
+    #
+    # Wait until recording is complete...
+    #
 
-#
-# Clear EXECUTION_STATE flags to disable away mode and allow the system to idle to sleep normally.
-#
-SetThreadExecutionState(ES_CONTINUOUS)
+    # Wait 1 hours
+    import time
+    time.sleep(60 * 60)
+
+    #
+    # Clear EXECUTION_STATE flags to disable away mode and allow the system to idle to sleep normally.
+    #
+    SetThreadExecutionState(ES_CONTINUOUS)
