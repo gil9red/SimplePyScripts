210f9854d1fdb8f299193c83cad3c2a33c29b35d
ipetrash <ilya.petrash@inbox.ru>
2017-12-26 10:20:57 +0500

    Append pil_example\img_to_grid

---
+++ pil_example/img_to_grid/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PIL import Image
+image = Image.open("img.jpg")
+width, height = image.size
+
+ROW_COUNT = 3
+COLUMN_COUNT = 5
+
+grid_image = Image.new("RGB", (width * COLUMN_COUNT, height * ROW_COUNT), (255, 255, 255))
+
+for row in range(ROW_COUNT):
+    y = row * width
+
+    for column in range(COLUMN_COUNT):
+        x = column * height
+        grid_image.paste(image, (x, y))
+
+# grid_image.show()
+grid_image.save('grid_img.jpg')
