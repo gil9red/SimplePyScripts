c921897039f6b51290bb1b755d0025aa15821efa
ipetrash <ilya.petrash@inbox.ru>
2017-12-26 14:32:34 +0500

    #

--- pyqt5_simple_check_password.py
+++ pyqt5_simple_check_password.py
@@ -7,24 +7,6 @@ __author__ = 'ipetrash'
 from PyQt5.QtWidgets import *
 
 
-class LoginWindow(QDialog):
-    def __init__(self):
-        super().__init__()
-
-        self.login = QLineEdit()
-        self.login.setPlaceholderText('Введите логин...')
-
-        self.password = QLineEdit()
-        self.password.setEchoMode(QLineEdit.Password)
-        self.password.setPlaceholderText('Введите пароль...')
-
-        layout = QFormLayout()
-        layout.addRow('Login:', self.login)
-        layout.addRow('Password:', self.password)
-
-        self.setLayout(layout)
-
-
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
