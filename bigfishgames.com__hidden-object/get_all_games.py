a98ff8edcda851b0630ec6392461ed085cc73edc
ipetrash <ilya.petrash@inbox.ru>
2018-01-05 10:54:03 +0500

    Append bigfishgames.com__hidden-object/get_all_games.py

---
+++ bigfishgames.com__hidden-object/get_all_games.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+def get_all_games():
+    url = 'https://www.bigfishgames.com/download-games/genres/15/hidden-object.html'
+
+    import requests
+    rs = requests.get(url)
+
+    from bs4 import BeautifulSoup
+    root = BeautifulSoup(rs.content, 'html.parser')
+
+    return [a.text.strip() for a in root.select('#genre_bottom a')]
+
+
+if __name__ == '__main__':
+    games = get_all_games()
+    print(len(games))
+    print(games)
