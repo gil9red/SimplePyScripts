641ccdca9df3ff36ced7c4549366bf7ab4a1a7ba
ipetrash <ilya.petrash@inbox.ru>
2018-01-07 10:15:57 +0500

    #

--- dis__bytecode__is_empty_function.py
+++ dis__bytecode__is_empty_function.py
@@ -5,7 +5,7 @@ __author__ = 'ipetrash'
 
 
 # https://ru.stackoverflow.com/a/767523/201445
-def check_is_empty_function__use_dis(function):
+def check_is_empty_function(function):
     import dis
     instructions = dis.get_instructions(function)
     instr = next(instructions, None)
@@ -20,10 +20,8 @@ if __name__ == '__main__':
     def foo():
         pass
 
-
     def foo2():
         return 1
 
-
-    print('Empty:', check_is_empty_function__use_dis(foo))
-    print('Empty:', check_is_empty_function__use_dis(foo2))
+    print('Empty:', check_is_empty_function(foo))
+    print('Empty:', check_is_empty_function(foo2))
