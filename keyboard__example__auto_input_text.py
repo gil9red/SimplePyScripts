c1f5d97f4d308231b71de48ca883546785e0832d
ipetrash <ilya.petrash@inbox.ru>
2018-01-07 12:21:00 +0500

    Append keyboard__example__auto_input_text.py

---
+++ keyboard__example__auto_input_text.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import keyboard
+keyboard.press_and_release('tab, tab, tab')
+
+keyboard.write('The quick brown fox jumps over the lazy dog.')
+
+# Press PAGE UP then PAGE DOWN to type "foobar".
+keyboard.add_hotkey('page up, page down', lambda: keyboard.write('foobar'))
+
+# Blocks until you press esc.
+keyboard.wait('esc')
+
+# Record events until 'esc' is pressed.
+recorded = keyboard.record(until='esc')
+
+# Then replay back at three times the speed.
+keyboard.play(recorded, speed_factor=3)
+
+# Type @@ then press space to replace with abbreviation.
+keyboard.add_abbreviation('@@', 'my.long.email@example.com')
+
+# Block forever.
+keyboard.wait()
