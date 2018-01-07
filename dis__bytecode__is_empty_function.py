50d62ea551a31fcb3da039cb4faaaa5a76272e06
ipetrash <ilya.petrash@inbox.ru>
2018-01-07 10:10:11 +0500

    Append dis__bytecode__is_empty_function.py

---
+++ dis__bytecode__is_empty_function.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# https://ru.stackoverflow.com/a/767523/201445
+def check_is_empty_function__use_dis(function):
+    import dis
+    instructions = dis.get_instructions(function)
+    instr = next(instructions, None)
+    if instr is None or instr.opname != 'LOAD_CONST' or instr.argrepr != 'None':
+        return False
+
+    instr = next(instructions, None)
+    return instr and (instr.opname == 'RETURN_VALUE')
+
+
+if __name__ == '__main__':
+    def foo():
+        pass
+
+
+    def foo2():
+        return 1
+
+
+    print('Empty:', check_is_empty_function__use_dis(foo))
+    print('Empty:', check_is_empty_function__use_dis(foo2))
