8e5f7b79edef5ea1e970c1b84f124f1318af9eea
ipetrash <ilya.petrash@inbox.ru>
2018-01-07 12:43:49 +0500

    Append keyboard__example__auto_replace_input_text.py

---
+++ keyboard__example__auto_replace_input_text.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import keyboard
+
+# Type fuck then press space to replace with abbreviation.
+keyboard.add_abbreviation('fuck', 'Furnification under consult of King')
+
+# Block forever.
+keyboard.wait()
