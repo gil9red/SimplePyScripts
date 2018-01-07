71d3da59bc6ec0a1b15f82713494583518cd9e38
ipetrash <ilya.petrash@inbox.ru>
2018-01-07 11:28:18 +0500

    Append zelle_graphics/pipette.py

---
+++ zelle_graphics/pipette.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://habrahabr.ru/post/346146/
+
+
+# pip install pyautogui
+import pyautogui
+from graphics import GraphWin, Circle, Point, Entry, color_rgb
+import time
+
+win = GraphWin("pipetka", 200, 200, autoflush=True)  # создаем графическую форму размером 200х200 и элементы на ней
+x, y = pyautogui.position()  # получаем в x, y координаты мыши
+r, g, b = pyautogui.pixel(x, y)  # получаем в r, g, b цвет
+
+ColorDot = Circle(Point(100, 100), 25)  # создаем точку, отображающую цвет
+ColorDot.setFill(color_rgb(r, g, b))  # устанавливает ей заливку из ранее полученных цветов
+ColorDot.draw(win)  # рисуем на форме win
+
+RGBtext = Entry(Point(win.getWidth() / 2, 25), 10)  # создаем RGB вывод
+RGBtext.draw(win)  # рисуем на форме win
+
+RGBstring = Entry(Point(win.getWidth() / 2, 45), 10)  # создаем вывод цвета в web стиле
+RGBstring.draw(win)  # рисуем на форме win
+
+Coordstring = Entry(Point(win.getWidth() / 2, 185), 10)  # создаем отображение координат
+Coordstring.draw(win)  # рисуем на форме win
+
+while True:  # цикл перереисовки формы
+    time.sleep(0.1)  # задержка в 0.1 с, чтобы питон не сходил с ума
+
+    x, y = pyautogui.position()  # получаем в x, y координаты мыши
+    r, g, b = pyautogui.pixel(x, y)  # получаем в r, g, b цвет
+    ColorDot.setFill(color_rgb(r, g, b))  # Обновляем цвет
+    RGBtext.setText(pyautogui.pixel(x, y))  # Обновляем RGB
+    RGBstring.setText(color_rgb(r, g, b))  # Обновляем web цвет
+    Coordstring.setText('{} x {}'.format(x, y))  # Обновляем координаты
+
+    if win.isClosed():
+        break
+
+    win.flush()  # Даем команду на перерисовку формы
