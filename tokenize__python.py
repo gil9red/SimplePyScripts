29dade80d6b26683c7cd41ec7a8518423115fe71
ipetrash <ilya.petrash@inbox.ru>
2018-01-08 20:26:22 +0500

    Append tokenize__python.py

---
+++ tokenize__python.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from io import StringIO
+import token
+import tokenize
+
+prog_example = """
+for i in range(100): # comment
+    if i % 1 == 0: 
+        print(":", t**2)
+"""
+
+rl = StringIO(prog_example).readline
+
+for t_type, t_str, (br, bc), (er, ec), logl in tokenize.generate_tokens(rl):
+    print("%3i %10s : %20r" % (t_type, token.tok_name[t_type], t_str))
