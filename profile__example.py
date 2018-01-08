f1b536e9e495945876be6ef70e0af40d0fbe614f
ipetrash <ilya.petrash@inbox.ru>
2018-01-09 04:19:20 +0500

    Append profile__example.py

---
+++ profile__example.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# LOOK: 1000    0.016    0.000    0.016    0.000 profile__example.py:14(_has)
+
+
+def foo(number=100):
+    def _inc_i():
+        nonlocal i
+        i += 1
+
+    def _has(value, items):
+        return value in items
+
+    items = ['1234567890' for _ in range(number)]
+    print(items)
+
+    i = 0
+
+    for i in range(number):
+        _inc_i()
+        _has('5', items)
+
+    print(i)
+
+
+import profile
+profile.run(r'foo(1000)')
