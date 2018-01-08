b79c83eed38a30b1a52e192c3f34705bea88e9d7
ipetrash <ilya.petrash@inbox.ru>
2018-01-09 04:28:45 +0500

    #

---
+++ timeit_example/timer_example.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# Performance Measurement (модуль timeit_example)
+from timeit import Timer
+print(Timer('t=a; a=b; b=t', 'a=1; b=2').timeit())
+print(Timer('a,b = b,a', 'a=1; b=2').timeit())
