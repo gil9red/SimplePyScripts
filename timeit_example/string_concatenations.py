fa62b741d89761e062ccd490faad568f9d025d4a
ipetrash <ilya.petrash@inbox.ru>
2018-01-09 04:35:34 +0500

    Append timeit_example/string_concatenations.py

---
+++ timeit_example/string_concatenations.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import timeit
+
+number = 10000
+
+print(timeit.timeit(
+    """
+text = ''
+for _ in range(1000):
+    text += 'abc'
+    """,
+    number=number
+))
+
+print(timeit.timeit(
+    """
+text = []
+for _ in range(1000):
+    text.append('abc')
+    
+text = ''.join(text)
+    """,
+    number=number
+))
+
+print(timeit.timeit(
+    "text = ''.join('abc' for _ in range(1000))",
+    number=number
+))
+
+print(timeit.timeit(
+    "text = ''.join(['abc' for _ in range(1000)])",
+    number=number
+))
