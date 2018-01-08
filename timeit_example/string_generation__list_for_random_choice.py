41d64ed9923457ef0b34aa25320fbd618aef9d59
ipetrash <ilya.petrash@inbox.ru>
2018-01-09 04:28:59 +0500

    Append timeit_example/string_generation__list_for_random_choice.py

---
+++ timeit_example/string_generation__list_for_random_choice.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+setup = '''
+from random import choices, choice
+from string import ascii_letters
+'''
+number = 100
+
+import timeit
+print(timeit.timeit(
+    "[''.join(choices(ascii_letters, k=10)) for _ in range(1000)]",
+    setup=setup,
+    number=number
+))
+
+print(timeit.timeit(
+    "[''.join([choice(ascii_letters) for _ in range(10)]) for _ in range(1000)]",
+    setup=setup,
+    number=number
+))
