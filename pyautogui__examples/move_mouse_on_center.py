c16a9c2c729f100d4b8f893a821320d63fd86366
ipetrash <ilya.petrash@inbox.ru>
2018-01-09 06:10:42 +0500

    Append pyautogui__examples/move_mouse_on_center.py

---
+++ pyautogui__examples/move_mouse_on_center.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import pyautogui
+screenWidth, screenHeight = pyautogui.size()
+pyautogui.moveTo(screenWidth / 2, screenHeight / 2)
