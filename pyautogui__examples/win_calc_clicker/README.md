46b4eebcad547aadeec2484d637062cb4ac7f79a
ipetrash <ilya.petrash@inbox.ru>
2018-01-09 07:17:40 +0500

    Append pyautogui__examples\win_calc_clicker\main__PI_clicker_monster.py

--- pyautogui__examples/win_calc_clicker/README.md
+++ pyautogui__examples/win_calc_clicker/README.md
@@ -4,3 +4,6 @@
 
 ## main__locateAllOnScreen.py
 ![](screenshot__locateAllOnScreen.gif)
+
+## main__PI_clicker_monster.py
+![](screenshot__PI_clicker_monster.gif)
---
+++ pyautogui__examples/win_calc_clicker/main__PI_clicker_monster.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+BUTTONS = {
+    '+': 'buttons/add.png',
+    '-': 'buttons/sub.png',
+    '/': 'buttons/div.png',
+    '*': 'buttons/mul.png',
+    '=': 'buttons/equal.png',
+}
+for i in range(10):
+    BUTTONS[str(i)] = 'buttons/{}.png'.format(i)
+
+import pyautogui
+
+cache_pos_button = dict()
+
+expression = '3,14159265358979323846264338327950288419716939937510582097494459230781640628620' \
+             '8998628034825342117067982148086513282306647093844609550582231725359408128481117'
+
+for x in expression:
+    if x not in BUTTONS:
+        print('Not found: "{}"'.format(x))
+        continue
+
+    if x in cache_pos_button:
+        pos = cache_pos_button[x]
+    else:
+        file_name = BUTTONS[x]
+        pos = pyautogui.locateCenterOnScreen(file_name)
+        cache_pos_button[x] = pos
+
+    print(x, pos)
+    pyautogui.click(pos)
