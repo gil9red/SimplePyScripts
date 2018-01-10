a1e08bf0dabb22b6a89726de25b05716fefb2d10
ipetrash <ilya.petrash@inbox.ru>
2018-01-10 12:18:06 +0500

    #

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -8,8 +8,10 @@ __author__ = 'ipetrash'
 # im1 = pyautogui.screenshot()
 # im2 = pyautogui.screenshot('my_screenshot.png')
 
-import cv2
 
+import numpy as np
+
+import cv2
 img = cv2.imread('img.png')
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -20,23 +22,41 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # ret, thresh = cv2.threshold(gray, 176, 176, 176)
 ret, thresh = cv2.threshold(gray, 176, 176, cv2.THRESH_BINARY)
 image, contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-cv2.imshow('image', image)
-cv2.waitKey()
-cv2.destroyAllWindows()
+# cv2.imshow('image', image)
+# cv2.waitKey()
+# cv2.destroyAllWindows()
 
 # print(sorted([cv2.contourArea(i) for i in contours], reverse=True)[:10])
 # contours = [i for i in contours if (cv2.contourArea(i) > 250000 and cv2.contourArea(i) < 255000) or (cv2.contourArea(i) > 11000 and cv2.contourArea(i) < 12000)]
 
 contours = [i for i in contours if 250000 < cv2.contourArea(i) < 255000]
 
+# board_frame = contours[0]
+# print(board_frame)
+# quit()
+
 # contours = [i for i in contours if cv2.contourArea(i) > 11000]
-print([cv2.contourArea(i) for i in contours])
+# print([cv2.contourArea(i) for i in contours])
 
 # print(contours)
 # print(hierarchy)
-img = cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
+# mask = np.zeros_like(img)
+# cv2.drawContours(mask, contours, 0, (0, 255, 0), 3)
+img_with_contour = img.copy()
+cv2.drawContours(img_with_contour, contours, 0, (0, 255, 0), 3)
+
+rect_board = cv2.boundingRect(contours[0])
+# rect_board = cv2.boundingRect(np.array(0,0, 0,0, 200,0, 200,0))
+# print(rect_board)
+x, y, h, w = rect_board
+
+# cv2.imshow('img_with_contour', img_with_contour)
+# cv2.imshow('image', img)
+# cv2.imshow('gray', gray)
+
+crop_img = img[y:y+h, x:x+w]
+cv2.imshow("cropped", crop_img)
 
 
-cv2.imshow('image', img)
 cv2.waitKey()
 cv2.destroyAllWindows()
