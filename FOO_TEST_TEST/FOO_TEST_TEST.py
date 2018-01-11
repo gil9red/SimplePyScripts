00673faa1db877a81aa4558e53df3de1a75dd657
ipetrash <ilya.petrash@inbox.ru>
2018-01-11 05:52:44 +0500

    #

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -59,63 +59,69 @@ ret, thresh = cv2.threshold(edges, 200, 255, cv2.THRESH_BINARY)
 image, contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 # cv2.imshow('image_', image)
 
-if contours:
-    print(len(contours))
-    print([cv2.contourArea(i) for i in contours if cv2.contourArea(i) > 10000])
-    contours = [i for i in contours if 249000 < cv2.contourArea(i) < 255000]
+if not contours:
+    print('Не получилсоь найти контуры')
+    quit()
 
-    img_with_contour = img.copy()
-    cv2.drawContours(img_with_contour, contours, -1, (0, 255, 0), 3)
-    cv2.imshow('img_with_contour', img_with_contour)
+print(len(contours))
+print([cv2.contourArea(i) for i in contours if cv2.contourArea(i) > 10000])
+contours = [i for i in contours if 249000 < cv2.contourArea(i) < 255000]
+if not contours:
+    print('Не получилсоь найти контур поля игры')
+    quit()
 
-cv2.waitKey()
-cv2.destroyAllWindows()
-quit()
-
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imshow('gray', gray)
-
-# ret, thresh = cv2.threshold(gray, 176, 176, 176)
-ret, thresh = cv2.threshold(gray, 176, 176, cv2.THRESH_BINARY)
-image, contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-# cv2.imshow('image_', image)
-# cv2.waitKey()
-# cv2.destroyAllWindows()
-
-# print(sorted([cv2.contourArea(i) for i in contours], reverse=True)[:10])
-# contours = [i for i in contours if (cv2.contourArea(i) > 250000 and cv2.contourArea(i) < 255000) or (cv2.contourArea(i) > 11000 and cv2.contourArea(i) < 12000)]
-
-contours = [i for i in contours if 250000 < cv2.contourArea(i) < 255000]
-print([cv2.contourArea(i) for i in contours])
-print([cv2.contourArea(i) for i in contours if 250000 < cv2.contourArea(i) < 255000])
-
-if contours:
-    img_with_contour = img.copy()
-    cv2.drawContours(img_with_contour, contours, -1, (0, 255, 0), 3)
-    cv2.imshow('img_with_contour', img_with_contour)
-
-cv2.waitKey()
-cv2.destroyAllWindows()
-
-quit()
-
-# board_frame = contours[0]
-# print(board_frame)
-# quit()
-
-# contours = [i for i in contours if cv2.contourArea(i) > 11000]
-# print([cv2.contourArea(i) for i in contours])
-
-# print(contours)
-# print(hierarchy)
-# mask = np.zeros_like(img)
-# cv2.drawContours(mask, contours, 0, (0, 255, 0), 3)
 img_with_contour = img.copy()
 cv2.drawContours(img_with_contour, contours, -1, (0, 255, 0), 3)
-
 cv2.imshow('img_with_contour', img_with_contour)
-# cv2.imshow('image', img)
+
+# cv2.waitKey()
+# cv2.destroyAllWindows()
+# quit()
+#
+# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # cv2.imshow('gray', gray)
+#
+# # ret, thresh = cv2.threshold(gray, 176, 176, 176)
+# ret, thresh = cv2.threshold(gray, 176, 176, cv2.THRESH_BINARY)
+# image, contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+# # cv2.imshow('image_', image)
+# # cv2.waitKey()
+# # cv2.destroyAllWindows()
+#
+# # print(sorted([cv2.contourArea(i) for i in contours], reverse=True)[:10])
+# # contours = [i for i in contours if (cv2.contourArea(i) > 250000 and cv2.contourArea(i) < 255000) or (cv2.contourArea(i) > 11000 and cv2.contourArea(i) < 12000)]
+#
+# contours = [i for i in contours if 250000 < cv2.contourArea(i) < 255000]
+# print([cv2.contourArea(i) for i in contours])
+# print([cv2.contourArea(i) for i in contours if 250000 < cv2.contourArea(i) < 255000])
+#
+# if contours:
+#     img_with_contour = img.copy()
+#     cv2.drawContours(img_with_contour, contours, -1, (0, 255, 0), 3)
+#     cv2.imshow('img_with_contour', img_with_contour)
+#
+# cv2.waitKey()
+# cv2.destroyAllWindows()
+#
+# quit()
+#
+# # board_frame = contours[0]
+# # print(board_frame)
+# # quit()
+#
+# # contours = [i for i in contours if cv2.contourArea(i) > 11000]
+# # print([cv2.contourArea(i) for i in contours])
+#
+# # print(contours)
+# # print(hierarchy)
+# # mask = np.zeros_like(img)
+# # cv2.drawContours(mask, contours, 0, (0, 255, 0), 3)
+# img_with_contour = img.copy()
+# cv2.drawContours(img_with_contour, contours, -1, (0, 255, 0), 3)
+#
+# cv2.imshow('img_with_contour', img_with_contour)
+# # cv2.imshow('image', img)
+# # cv2.imshow('gray', gray)
 
 
 rect_board = cv2.boundingRect(contours[-1])
@@ -123,8 +129,10 @@ rect_board = cv2.boundingRect(contours[-1])
 # print(rect_board)
 x, y, h, w = rect_board
 crop_img = img[y:y+h, x:x+w]
-# cv2.imshow("cropped", crop_img)
-
+cv2.imshow("cropped", crop_img)
+cv2.waitKey()
+cv2.destroyAllWindows()
+quit()
 
 # crop_img[crop_img == 255] = [0, 0, 255]
 gray_crop_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
