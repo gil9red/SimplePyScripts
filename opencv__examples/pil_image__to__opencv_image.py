5ac1a1442e83aebf0ae45711ba8cdc2caba036f3
ipetrash <ilya.petrash@inbox.ru>
2018-01-15 05:37:07 +0500

    #

---
+++ opencv__examples/pil_image__to__opencv_image.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# from PIL import Image
+
+import pyautogui
+import cv2
+import numpy
+
+pil_image = pyautogui.screenshot(region=(200, 200, 200, 200))
+print(pil_image.size)
+pil_image.show('pil_image')
+
+opencv_image = cv2.cvtColor(numpy.array(pil_image), cv2.COLOR_RGB2BGR)
+print(opencv_image.shape[:2])
+
+cv2.imshow('opencv_image', opencv_image)
+cv2.waitKey()
