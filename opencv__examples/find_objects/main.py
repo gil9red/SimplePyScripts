435aca6114a14da83e0f9a45734082261930ab74
ipetrash <ilya.petrash@inbox.ru>
2018-01-17 11:59:27 +0500

    Append opencv__examples\find_objects

---
+++ opencv__examples/find_objects/main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import cv2
+img = cv2.imread('image.png')
+cv2.imshow('img', img)
+
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+ret, thresh = cv2.threshold(gray_img, 0, 255, cv2.THRESH_BINARY)
+# cv2.imshow('thresh', thresh)
+
+# image, contours, _ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+image, contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# cv2.imshow('image_', image)
+
+# img_with_contour = img.copy()
+# cv2.drawContours(img_with_contour, contours, -1, (0, 255, 0), 3)
+# cv2.imshow('img_with_contour', img_with_contour)
+
+img_with_rect = img.copy()
+
+for contour in contours:
+    rect = cv2.boundingRect(contour)
+    x, y, w, h = rect
+
+    cv2.rectangle(img_with_rect, (x, y), (x + w, y + h), (0, 0, 255))
+
+cv2.imshow('img_with_rect', img_with_rect)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
