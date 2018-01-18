7c59583e06cbb804acdc79d6b6099facb14f5d09
ipetrash <ilya.petrash@inbox.ru>
2018-01-18 10:52:16 +0500

    Append pyautogui__examples/locate_center_on_screen.py

---
+++ pyautogui__examples/locate_center_on_screen.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import pyautogui
+
+
+def locate_center_on_screen(needle_image, screenshot_image=None):
+    if screenshot_image:
+        rect = pyautogui.locate(needle_image, screenshot_image)
+        return pyautogui.center(rect)
+
+    return pyautogui.locateCenterOnScreen(needle_image)
+
+
+if __name__ == '__main__':
+    needle_image = '<file_name>'
+
+    import time
+
+    t = time.clock()
+    screenshot = pyautogui.screenshot()
+    pos = locate_center_on_screen(needle_image, screenshot)
+    pos = locate_center_on_screen(needle_image, screenshot)
+    pos = locate_center_on_screen(needle_image, screenshot)
+    print(pos, time.clock() - t)
+
+    # Without buffered image
+    t = time.clock()
+    pos = pyautogui.locateCenterOnScreen(needle_image)
+    pos = pyautogui.locateCenterOnScreen(needle_image)
+    pos = pyautogui.locateCenterOnScreen(needle_image)
+    print(pos, time.clock() - t)
