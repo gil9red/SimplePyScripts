9f679c6b9c21a12c8c607f54d1e7ac6c8c920fc1
ipetrash <ilya.petrash@inbox.ru>
2018-01-22 14:53:10 +0500

    #

--- generate_random_list.py
+++ generate_random_list.py
@@ -5,6 +5,7 @@ __author__ = 'ipetrash'
 
 
 import random
+import numpy
 
 #
 # Non-Unique Numbers
@@ -12,7 +13,6 @@ import random
 items = [random.randint(0, 100000) for i in range(100000)]
 print(len(items), items[:5])  # 100000 [92560, 5867, 81098, 98309, 97995]
 
-import numpy
 items = numpy.random.random_integers(100000, size=100000)
 print(len(items), items[:5])  # 100000 [73846 49707 18846 73887 43349]
 
@@ -22,3 +22,6 @@ print(len(items), items[:5])  # 100000 [73846 49707 18846 73887 43349]
 items = list(range(100000))
 random.shuffle(items)
 print(len(items), items[:5])  # 100000 [36174, 38829, 93415, 74347, 40457]
+
+items = numpy.random.choice(100000, 100000, replace=False)
+print(len(items), items[:5])  # 100000 [94792 79537  9678 66784 92049]
