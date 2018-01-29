798845308d0c4b78fecbf74e470555f4af5aeba1
ipetrash <ilya.petrash@inbox.ru>
2018-01-29 19:26:10 +0500

    Append PySvn_examples/print_jira_by_date.py

---
+++ PySvn_examples/print_jira_by_date.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import re
+PATTERN_GET_JIRA = re.compile(r'\[.*?\] \((\w+?-\d+?)\)')
+
+
+def get_jira(msg):
+    """
+    '[.ii] (OPTT-113) . Refactoring' -> 'OPTT-113'
+
+    """
+
+    if not msg:
+        return
+
+    match = PATTERN_GET_JIRA.search(msg)
+    if match:
+        return match.group(1)
+
+
+import config
+import common
+
+log_list = common.get_log_list(config.SVN_FILE_NAME)
+# OR:
+# log_list = common.get_log_list(config.URL_SVN)
+
+print('Total commits:', len(log_list))
+
+
+from collections import OrderedDict
+month_by_jira_info = OrderedDict()
+
+
+for log in reversed(log_list):
+    jira = get_jira(log.msg)
+    if not jira:
+        continue
+
+    key = log.date.strftime('%Y/%m')
+
+    if key not in month_by_jira_info:
+        month_by_jira_info[key] = set()
+
+    month_by_jira_info[key].add(jira)
+
+for month, jira_items in month_by_jira_info.items():
+    print('{}: ({})\t{}'.format(month, len(jira_items), list(jira_items)))
+
