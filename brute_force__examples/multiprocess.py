6a9a4c835f2360de1ec26253657ac7c03cf24216
ipetrash <ilya.petrash@inbox.ru>
2018-01-29 11:48:12 +0500

    Append brute_force__examples

---
+++ brute_force__examples/multiprocess.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://ru.stackoverflow.com/a/776977/201445
+
+
+import binascii
+import hashlib
+import itertools
+import multiprocessing
+from functools import partial
+
+import string
+alphabet = (string.ascii_letters + string.digits).encode()
+
+
+def sha256(data):
+    return hashlib.sha256(data).digest()
+
+
+def check_sha256(repls_parent, bytes_format, n, target_sha256):
+    for repls in itertools.product(alphabet, repeat=n):
+        data = bytes_format % (repls_parent + repls)
+        if sha256(data) == target_sha256:
+            return data
+
+
+def brute_force(mask, target_sha256, n_cutoff=4):
+    """
+    n_cutoff -- number of `*` to process in a worker process
+    """
+
+    target_sha256 = binascii.unhexlify(target_sha256)
+
+    bytes_format = mask.replace(b'%', b'%%').replace(b'*', b'%c')
+    mp_check = partial(check_sha256,
+                       bytes_format=bytes_format,
+                       n=min(n_cutoff, mask.count(b'*')),
+                       target_sha256=target_sha256)
+    n = max(0, mask.count(b'*') - n_cutoff)
+    all_repls_parent = itertools.product(alphabet, repeat=n)
+    with multiprocessing.Pool() as pool:
+        for data in pool.imap_unordered(mp_check, all_repls_parent):
+            if data is not None:
+                return data
+
+
+if __name__ == '__main__':
+    # Password: qwe12y
+    sha256_hex = b'b8cc184b3f4aeb6adb6601ea3e39ef5ac098791acc2a505a66746f43d0c7ed85'
+
+    passw_bytes = brute_force(b'qw****', sha256_hex)
+    print(passw_bytes.decode())
---
+++ brute_force__examples/single.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://ru.stackoverflow.com/a/776780/201445
+
+
+import hashlib
+from itertools import product
+
+
+def brute_force(mask, hsh, alphabet):
+    # заменяем '*' на '{}' для последующей подстановки в 'str.format()'
+    pwd_pat = mask.replace('*', '{}')
+
+    # число звездочек - будем использовать в качестве `product(.., repeat)`
+    N = mask.count('*')
+    i = 0
+    for chars in product(alphabet, repeat=N):
+        i += 1
+        if i % 10000 == 0:
+            print('Iterations: {}'.format(i))
+
+        if hsh == hashlib.sha256(pwd_pat.format(*chars).encode()).hexdigest():
+            text = pwd_pat.format(*chars)
+            print('Found: ' + text)
+            return text
+
+    return None
+
+
+if __name__ == '__main__':
+    from string import ascii_letters, digits
+    alphabet = ascii_letters + digits
+
+    # Password: qwe12y
+    hsh = 'b8cc184b3f4aeb6adb6601ea3e39ef5ac098791acc2a505a66746f43d0c7ed85'
+    print(brute_force('qw****', hsh, alphabet))
