0d8b28cb7ca8221675ec1e6b009542eb157dbabe
ipetrash <ilya.petrash@inbox.ru>
2018-01-30 11:40:45 +0500

    Append "x == 1 and x == 2 and x == 3.py"

---
+++ x == 1 and x == 2 and x == 3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+class X:
+    def __eq__(self, other):
+        return True
+
+
+x = X()
+print(x == 1 and x == 2 and x == 3)
