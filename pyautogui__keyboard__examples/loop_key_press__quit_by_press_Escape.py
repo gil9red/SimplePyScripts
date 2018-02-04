d00bd6e33b35173006f33302dfe889e0bb8f53b3
ipetrash <ilya.petrash@inbox.ru>
2018-02-04 05:52:30 +0500

    Join keyboard__example and pyautoguid_examples

--- pyautogui__keyboard__examples/loop_key_press__quit_by_press_Escape.py
+++ pyautogui__keyboard__examples/loop_key_press__quit_by_press_Escape.py
@@ -12,9 +12,14 @@ import keyboard
 keyboard.add_hotkey(QUIT_COMBINATION, lambda: print('Quit by Escape') or os._exit(0))
 
 import time
+import pyautogui
+
+time.sleep(5)
 
 i = 0
 while True:
+    pyautogui.press('pgdn')
+
     i += 1
     print(i)
 
