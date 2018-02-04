781754a2cd72bfe6f9653b88f0bbfd70d6bc875a
ipetrash <ilya.petrash@inbox.ru>
2018-02-04 05:16:30 +0500

    Append quit_by_global_press_Escape.py

---
+++ keyboard__example/quit_by_global_press_Escape.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+QUIT_COMBINATION = 'Esc'
+print('Press "{}" for QUIT'.format(QUIT_COMBINATION))
+
+import os
+import keyboard
+keyboard.add_hotkey(QUIT_COMBINATION, lambda: print('Quit by Escape') or os._exit(0))
+
+import time
+
+i = 0
+while True:
+    i += 1
+    print(i)
+
+    time.sleep(1)
