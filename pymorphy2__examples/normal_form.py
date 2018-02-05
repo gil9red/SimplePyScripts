1622cd6cd32ddc5bc4c1e4becba40d6a00f53dcf
ipetrash <ilya.petrash@inbox.ru>
2018-02-05 16:55:06 +0500

    Append pymorphy2__examples/normal_form.py

---
+++ pymorphy2__examples/normal_form.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install pymorphy2
+import pymorphy2
+morph = pymorphy2.MorphAnalyzer()
+
+
+def get_normal_form(word):
+    return morph.parse(word)[0].normal_form
+
+
+print(get_normal_form('Иванова'))
+print(get_normal_form('Иванов'))
