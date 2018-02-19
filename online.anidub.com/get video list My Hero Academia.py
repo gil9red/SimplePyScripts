cf5093b245cf2385f17afd29559b0e6a638c47bf
ipetrash <ilya.petrash@inbox.ru>
2018-02-19 16:35:21 +0500

    Append "online.anidub.com/get video list My Hero Academia.py"

---
+++ online.anidub.com/get video list My Hero Academia.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+def search_video_list():
+    url = 'https://online.anidub.com/index.php?do=search'
+
+    data = {
+        'do': 'search',
+        'subaction': 'search',
+        'search_start': '1',
+        'full_search': '0',
+        'result_from': '1',
+        'story': 'Моя геройская академия',
+    }
+
+    import requests
+    rs = requests.post(url, data)
+
+    from bs4 import BeautifulSoup
+    root = BeautifulSoup(rs.content, 'html.parser')
+
+    return [a.text.strip() for a in root.select('.newstitle a')]
+
+
+if __name__ == '__main__':
+    items = search_video_list()
+    print('Items ({}): {}'.format(len(items), items))
+
+    import json
+    json.dump(items, open('video_list.json', 'w', encoding='utf-8'), ensure_ascii=False)
