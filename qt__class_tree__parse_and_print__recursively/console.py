d94c45bfd2cf4c3dea33f3d679ac09abfeff7107
ipetrash <ilya.petrash@inbox.ru>
2018-02-22 14:20:51 +0500

    Append qt__class_tree__parse_and_print__recursively

---
+++ qt__class_tree__parse_and_print__recursively/console.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import requests
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+import time
+
+
+def get_inherited_children(url, root):
+    td_inherited_by = None
+
+    for x in root.select('.alignedsummary td'):
+        if x.text.strip() == 'Inherited By:':
+            td_inherited_by = x
+            break
+
+    if not td_inherited_by:
+        # print('Not found "Inherited By:" td.')
+        return []
+
+    td_list = td_inherited_by.find_next_sibling('td').select('a')
+    return [(a.text.strip(), urljoin(url, a['href'])) for a in td_list]
+
+
+def print_children(url, total_class_list, global_number=-1, indent_level=0):
+    if global_number > 0 and len(total_class_list) >= global_number:
+        # print('GLOBAL_NUMBER!')
+        return
+
+    time.sleep(1)
+
+    indent = '  ' * indent_level
+
+    rs = requests.get(url)
+    root = BeautifulSoup(rs.content, 'html.parser')
+
+    name_class = root.select_one('.context > .title').text.split()[0]
+    total_class_list.append(name_class)
+
+    inherited_children = get_inherited_children(url, root)
+    number_inherited_children = len(inherited_children)
+    if number_inherited_children > 0:
+        print(indent + '{} ({}):'.format(name_class, number_inherited_children))
+    else:
+        print(indent + name_class)
+
+    for name, url in inherited_children:
+        print_children(url, total_class_list, global_number, indent_level + 1)
+
+
+ROOT_URL = 'http://doc.qt.io/qt-5/qobject.html'
+
+
+if __name__ == '__main__':
+    global_number = -1
+    total_class_list = []
+
+    print_children(ROOT_URL, total_class_list, global_number)
+
+    import json
+    json.dump(total_class_list, open('total_class.json', 'w', encoding='utf-8'), ensure_ascii=False)
---
+++ qt__class_tree__parse_and_print__recursively/gui.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5 import QtWidgets as qtw
+from PyQt5.QtTest import QTest
+
+import time
+import requests
+from bs4 import BeautifulSoup
+
+from console import get_inherited_children, ROOT_URL
+
+
+class MainWindow(qtw.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('qt__class_tree__parse_and_print__recursively')
+
+        self.tree = qtw.QTreeWidget()
+        self.tree.setAlternatingRowColors(True)
+        self.tree.setHeaderLabel('NAME')
+
+        self.setCentralWidget(self.tree)
+
+        self.number_total_class = 0
+
+    def _fill_root(self, node: qtw.QTreeWidgetItem, url: str, global_number: int, indent_level=0):
+        if global_number > 0 and self.number_total_class >= global_number:
+            return
+
+        QTest.qWait(1000)
+
+        indent = '  ' * indent_level
+
+        rs = requests.get(url)
+        root = BeautifulSoup(rs.content, 'html.parser')
+
+        name_class = root.select_one('.context > .title').text.split()[0]
+
+        inherited_children = get_inherited_children(url, root)
+        number_inherited_children = len(inherited_children)
+        if number_inherited_children > 0:
+            name_class = '{} ({})'.format(name_class, number_inherited_children)
+            print(indent + name_class + ':')
+        else:
+            print(indent + name_class)
+
+        item = qtw.QTreeWidgetItem([name_class])
+
+        if not node:
+            self.tree.addTopLevelItem(item)
+        else:
+            node.addChild(item)
+            node.setExpanded(True)
+
+        self.number_total_class += 1
+
+        for name, url in inherited_children:
+            self._fill_root(item, url, global_number, indent_level + 1)
+
+    def fill_tree(self, global_number=-1):
+        self.number_total_class = 0
+        self.tree.clear()
+
+        t = time.clock()
+
+        self._fill_root(None, ROOT_URL, global_number)
+
+        qtw.QMessageBox.information(
+            self,
+            'Complete!',
+            'Items: {}.\nElapsed: {:.3f} sec'.format(self.number_total_class, time.clock() - t)
+        )
+
+    def closeEvent(self, e):
+        quit()
+
+
+if __name__ == '__main__':
+    app = qtw.QApplication([])
+
+    w = MainWindow()
+    w.resize(500, 500)
+    w.show()
+
+    w.fill_tree()
+
+    app.exec()
