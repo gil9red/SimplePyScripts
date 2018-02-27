26345239eb2858b164d1e36da241b156496c8ed0
ipetrash <ilya.petrash@inbox.ru>
2018-02-27 13:04:04 +0500

    Append pyqt__redirect_print_stream_to_qtextedit.py

---
+++ pyqt__redirect_print_stream_to_qtextedit.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.QtWidgets import QMainWindow, QTextEdit, QMenuBar, QApplication
+from PyQt5.QtCore import QObject
+from PyQt5.QtCore import pyqtSignal as Signal
+
+
+class OutputLogger(QObject):
+    emit_write = Signal(str, int)
+
+    class Severity:
+        DEBUG = 0
+        ERROR = 1
+
+    def __init__(self, io_stream, severity):
+        super().__init__()
+
+        self.io_stream = io_stream
+        self.severity = severity
+
+    def write(self, text):
+        self.io_stream.write(text)
+        self.emit_write.emit(text, self.severity)
+
+    def flush(self):
+        self.io_stream.flush()
+
+
+import sys
+OUTPUT_LOGGER_STDOUT = OutputLogger(sys.stdout, OutputLogger.Severity.DEBUG)
+OUTPUT_LOGGER_STDERR = OutputLogger(sys.stderr, OutputLogger.Severity.ERROR)
+
+sys.stdout = OUTPUT_LOGGER_STDOUT
+sys.stderr = OUTPUT_LOGGER_STDERR
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.text_edit = QTextEdit()
+
+        OUTPUT_LOGGER_STDOUT.emit_write.connect(self.append_log)
+        OUTPUT_LOGGER_STDERR.emit_write.connect(self.append_log)
+
+        menu_bar = QMenuBar()
+        menu = menu_bar.addMenu('Say')
+        menu.addAction('hello', lambda: print('Hello!'))
+        menu.addAction('fail', lambda: print('Fail!', file=sys.stderr))
+        self.setMenuBar(menu_bar)
+
+        self.setCentralWidget(self.text_edit)
+
+    def append_log(self, text, severity):
+        text = repr(text)
+
+        if severity == OutputLogger.Severity.ERROR:
+            self.text_edit.append('<b>{}</b>'.format(text))
+        else:
+            self.text_edit.append(text)
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    mw = MainWindow()
+    mw.show()
+
+    print('Go!')
+
+    app.exec()
