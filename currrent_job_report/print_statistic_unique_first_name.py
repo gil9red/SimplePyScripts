b3c35628fe131fe1b8dc60b60c2f60b57bb20dc9
ipetrash <ilya.petrash@inbox.ru>
2018-02-28 14:16:57 +0500

    Append print_statistic_unique_first_name.py

---
+++ currrent_job_report/print_statistic_unique_first_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+"""
+Скрипт для вывода имен без повторений.
+
+"""
+
+
+from print_statistic_all_names import get_all_names
+first_name_list = [name[1] for name in get_all_names(split_name=True)]
+print('Total:', len(first_name_list))
+
+unique_first_name_list = list(set(first_name_list))
+print('Total unique ({}): {}'.format(len(unique_first_name_list), unique_first_name_list))
