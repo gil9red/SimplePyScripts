bdebcddc504f4189e73b700cf9ba9b8cdbd69241
ipetrash <ilya.petrash@inbox.ru>
2018-03-02 12:01:29 +0500

    #

--- pretty_xml/pretty_xml_gui.py
+++ pretty_xml/pretty_xml_gui.py
@@ -7,104 +7,125 @@ __author__ = 'ipetrash'
 from os.path import split as path_split
 import traceback
 
-# TODO: весь код перенести в виджет
-# TODO: избиваться от `import *`
+
+# TODO: избавиться от `import *`
 try:
     from PyQt5.QtWidgets import *
     from PyQt5.QtGui import *
-    from PyQt5.QtCore import *
+    from PyQt5.QtCore import Qt
 
 except:
     try:
         from PyQt4.QtGui import *
-        from PyQt4.QtCore import *
+        from PyQt4.QtCore import Qt
 
     except:
         from PySide.QtGui import *
-        from PySide.QtCore import *
+        from PySide.QtCore import Qt
+
+
+def log_uncaught_exceptions(ex_cls, ex, tb):
+    text = '{}: {}:\n'.format(ex_cls.__name__, ex)
+    import traceback
+    text += ''.join(traceback.format_tb(tb))
+
+    print(text)
+    QMessageBox.critical(None, 'Error', text)
+    quit()
+
+
+import sys
+sys.excepthook = log_uncaught_exceptions
 
 
 from pretty_xml import pretty_xml_minidom as to_pretty_xml
 
-app = QApplication([])
 
-mainWidget = QWidget()
-mainWidget.setWindowTitle(path_split(__file__)[1])
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
 
-layout = QVBoxLayout()
+        self.setWindowTitle(path_split(__file__)[1])
 
-text_edit_input = QPlainTextEdit()
-text_edit_output = QPlainTextEdit()
+        layout = QVBoxLayout()
 
-label_error = QLabel()
-label_error.setStyleSheet("QLabel { color : red; }")
-label_error.setTextInteractionFlags(Qt.TextSelectableByMouse)
-label_error.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
-button_detail_error = QPushButton('...')
-button_detail_error.setFixedSize(20, 20)
-button_detail_error.setToolTip('Detail error')
-last_error_message = None
-last_detail_error_message = None
+        self.text_edit_input = QPlainTextEdit()
+        self.text_edit_output = QPlainTextEdit()
+
+        self.label_error = QLabel()
+        self.label_error.setStyleSheet("QLabel { color : red; }")
+        self.label_error.setTextInteractionFlags(Qt.TextSelectableByMouse)
+        self.label_error.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+
+        self.button_detail_error = QPushButton('...')
+        self.button_detail_error.setFixedSize(20, 20)
+        self.button_detail_error.setToolTip('Detail error')
+        self.button_detail_error.hide()
+
+        self.last_error_message = None
+        self.last_detail_error_message = None
+
+        self.button_detail_error.clicked.connect(self.show_detail_error_message)
+        self.text_edit_input.textChanged.connect(self.input_text_changed)
+
+        splitter = QSplitter()
+        splitter.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        splitter.addWidget(self.text_edit_input)
+        splitter.addWidget(self.text_edit_output)
+
+        layout.addWidget(splitter)
+
+        layout_error = QHBoxLayout()
+        layout_error.addWidget(self.label_error)
+        layout_error.addWidget(self.button_detail_error)
+
+        layout.addLayout(layout_error)
+
+        self.setLayout(layout)
+
+    def input_text_changed(self):
+        self.label_error.clear()
+        self.button_detail_error.hide()
+
+        self.last_error_message = None
+        self.last_detail_error_message = None
+
+        try:
+            text = self.text_edit_input.toPlainText()
+            text = to_pretty_xml(text)
+            self.text_edit_output.setPlainText(text)
+
+        except Exception as e:
+            # Выводим ошибку в консоль
+            traceback.print_exc()
+
+            # Сохраняем в переменную
+            tb = traceback.format_exc()
+
+            self.last_error_message = str(e)
+            self.last_detail_error_message = str(tb)
+            self.button_detail_error.show()
+
+            self.label_error.setText('Error: ' + self.last_error_message)
+
+    def show_detail_error_message(self):
+        message = self.last_error_message + '\n\n' + self.last_detail_error_message
+
+        mb = QErrorMessage()
+        mb.setWindowTitle('Error')
+        # Сообщение ошибки содержит отступы, символы-переходы на следующую строку,
+        # которые поломаются при вставке через QErrorMessage.showMessage, и нет возможности
+        # выбрать тип текста, то делаем такой хак.
+        mb.findChild(QTextEdit).setPlainText(message)
+
+        mb.exec_()
 
 
-def show_detail_error_massage():
-    message = last_error_message + '\n\n' + last_detail_error_message
+if __name__ == '__main__':
+    app = QApplication([])
 
-    mb = QErrorMessage()
-    mb.setWindowTitle('Error')
-    # Сообщение ошибки содержит отступы, символы-переходы на следующую строку,
-    # которые поломаются при вставке через QErrorMessage.showMessage, и нет возможности
-    # выбрать тип текста, то делаем такой хак.
-    mb.findChild(QTextEdit).setPlainText(message)
+    mw = MainWindow()
+    mw.resize(650, 500)
+    mw.show()
 
-    mb.exec_()
-
-button_detail_error.clicked.connect(show_detail_error_massage)
-
-
-def input_text_changed():
-    label_error.clear()
-    button_detail_error.hide()
-
-    global last_error_message, last_detail_error_message
-    last_error_message = None
-    last_detail_error_message = None
-
-    try:
-        text = text_edit_input.toPlainText()
-        text = to_pretty_xml(text)
-        text_edit_output.setPlainText(text)
-    except Exception as e:
-        # Выводим ошибку в консоль
-        traceback.print_exc()
-
-        # Сохраняем в переменную
-        tb = traceback.format_exc()
-
-        last_error_message = str(e)
-        last_detail_error_message = str(tb)
-        button_detail_error.show()
-
-        label_error.setText('Error: ' + last_error_message)
-
-text_edit_input.textChanged.connect(input_text_changed)
-
-splitter = QSplitter()
-splitter.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-splitter.addWidget(text_edit_input)
-splitter.addWidget(text_edit_output)
-
-layout.addWidget(splitter)
-
-layout_error = QHBoxLayout()
-layout_error.addWidget(label_error)
-layout_error.addWidget(button_detail_error)
-
-layout.addLayout(layout_error)
-
-mainWidget.setLayout(layout)
-
-mainWidget.resize(650, 500)
-mainWidget.show()
-
-app.exec_()
+    app.exec_()
