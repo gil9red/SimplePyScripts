394c1fb4c28625482ac3ccc986e4a1f31d0765fa
ipetrash <ilya.petrash@inbox.ru>
2018-03-05 00:14:05 +0500

    Append:  - pathlib_examples/create_file_in_user_directory.py  - pathlib_examples/get_user_directory.py

---
+++ pathlib_examples/create_file_in_user_directory.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from pathlib import Path
+f = Path('~/test_file.txt').expanduser()
+print(f.exists())  # False
+
+print(f)  # C:\Users\ipetrash\test_file.txt
+print(f.exists())  # False
+
+print(f.write_text('Hello World', encoding='utf-8'))  # 11
+print(f.exists())  # True
+
+print(f.read_text(encoding='utf-8'))  # Hello World
+
+print(f.unlink())  # None
+print(f.exists())  # False
---
+++ pathlib_examples/get_user_directory.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from pathlib import Path
+print(Path('~').expanduser())
+print(str(Path('~').expanduser()))
