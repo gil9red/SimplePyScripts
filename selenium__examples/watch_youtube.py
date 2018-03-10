97573594fa8f06c21b68dd98f6c6ed727a885284
ipetrash <ilya.petrash@inbox.ru>
2018-03-10 07:14:19 +0500

    Append selenium__examples/watch_youtube.py

---
+++ selenium__examples/watch_youtube.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# https://pypi.python.org/pypi/selenium
+
+
+# pip install selenium
+from selenium import webdriver
+driver = webdriver.Firefox()
+driver.get('https://www.youtube.com/watch?v=i-AXImNxCAE')
+print('Title: "{}"'.format(driver.title))
