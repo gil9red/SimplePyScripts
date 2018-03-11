0cec0cd3a468b3de2ad62f7db23dcdef01ec1120
ipetrash <ilya.petrash@inbox.ru>
2018-03-11 05:14:48 +0500

    #

--- selenium__examples/hide_window__invisible__headless.py
+++ selenium__examples/hide_window__invisible__headless.py
@@ -10,6 +10,7 @@ from selenium.webdriver.firefox.options import Options
 
 options = Options()
 options.add_argument('--headless')
+
 driver = webdriver.Firefox(options=options)
 driver.get('https://www.google.com/doodles')
 
