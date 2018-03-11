e442d564f5d3c8a96a9ad8e4a6d63d946d2f087e
ipetrash <ilya.petrash@inbox.ru>
2018-03-11 21:43:21 +0500

    Append PyMsgBox__examples

---
+++ PyMsgBox__examples/alert.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from pymsgbox import alert
+button = alert(text='My Text', title='My Title', button='OK')
+print(button)
