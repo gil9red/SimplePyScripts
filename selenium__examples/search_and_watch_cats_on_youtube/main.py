4cbcff7bbfcb1348c2b61e4052620670a9a56305
ipetrash <ilya.petrash@inbox.ru>
2018-03-12 03:13:31 +0500

    #

--- selenium__examples/search_and_watch_cats_on_youtube/main.py
+++ selenium__examples/search_and_watch_cats_on_youtube/main.py
@@ -12,6 +12,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
+import time
+
 driver = webdriver.Firefox()
 driver.get('https://www.youtube.com/')
 print('Title: "{}"'.format(driver.title))
@@ -40,7 +42,21 @@ video_list = driver.find_elements_by_id('dismissable')
 import random
 random.choice(video_list).click()
 
-import time
-time.sleep(10)
+try:
+    video_title = WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.CLASS_NAME, 'title')))
+
+except TimeoutException:
+    print('Timeout!')
+    quit()
+
+while True:
+    if video_title.text:
+        print('Title: "{}"'.format(driver.title))
+        print('Video Title: "{}"'.format(video_title.text))
+        break
+
+    time.sleep(1)
 
 driver.save_screenshot('final.png')
+
+# driver.quit()
