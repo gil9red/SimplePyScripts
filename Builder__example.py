45a81a3ce6e789087e6e6723ff1fdefaff43c8d2
ipetrash <ilya.petrash@inbox.ru>
2018-03-14 01:53:53 +0500

    Append Builder__example.py

---
+++ Builder__example.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+class Foo:
+    def __init__(self):
+        self.items = []
+        self.key_by_value = dict()
+
+    def add_item(self, value):
+        self.items.append(value)
+        return self
+
+    def add_items(self, values):
+        self.items += values
+        return self
+
+    def put_value(self, key, value):
+        self.key_by_value[key] = value
+        return self
+
+    def __repr__(self):
+        return 'Foo<items={}, values={}>'.format(len(self.items), len(self.key_by_value))
+
+
+foo = Foo()\
+    .add_item(1)\
+    .add_items('abc')\
+    .put_value('x', 1)\
+    .put_value('x[]', [1, 2, 3])
+
+print(foo)
+print(foo.items)
+print(foo.key_by_value)
