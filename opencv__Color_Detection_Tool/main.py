51255109c1dbe4de951f365d8e74977f72adbea6
ipetrash <ilya.petrash@inbox.ru>
2018-03-15 20:27:18 +0500

    #

--- opencv__Color_Detection_Tool/main.py
+++ opencv__Color_Detection_Tool/main.py
@@ -41,15 +41,15 @@ class MainWindow(Qt.QWidget):
 
         self.ui.rbResult.setChecked(True)
 
-        self.m_settings = Qt.QSettings(CONFIG_FILE_NAME, Qt.QSettings.IniFormat)
-        self.m_lastLoadPath = "."
-        self.m_mat = None
+        self.settings = Qt.QSettings(CONFIG_FILE_NAME, Qt.QSettings.IniFormat)
+        self.last_load_path = "."
+        self.image_source = None
 
         for w in self.findChildren(Qt.QSlider):
             w.sliderMoved.connect(self.refresh_HSV)
 
             name = w.objectName()
-            w.setValue(int(self.m_settings.value(name, w.value())))
+            w.setValue(int(self.settings.value(name, w.value())))
             sp = self.findChild(Qt.QSpinBox, "sp" + name[2:])
             if sp:
                 sp.setMinimum(w.minimum())
@@ -66,14 +66,14 @@ class MainWindow(Qt.QWidget):
         self.ui.bnLoad.clicked.connect(self.on_load)
 
     def on_load(self):
-        file_name = Qt.QFileDialog.getOpenFileName(self, "Load image", self.m_lastLoadPath, "Images (*.jpg *.jpeg *.png *.bmp)")[0]
+        file_name = Qt.QFileDialog.getOpenFileName(self, "Load image", self.last_load_path, "Images (*.jpg *.jpeg *.png *.bmp)")[0]
         if not file_name:
             return
 
-        self.m_lastLoadPath = Qt.QFileInfo(file_name).absolutePath()
+        self.last_load_path = Qt.QFileInfo(file_name).absolutePath()
 
-        self.m_mat = cv2.imread(file_name)
-        self.m_mat = cv2.cvtColor(self.m_mat, cv2.COLOR_BGR2RGB)
+        self.image_source = cv2.imread(file_name)
+        self.image_source = cv2.cvtColor(self.image_source, cv2.COLOR_BGR2RGB)
 
         self.refresh_HSV()
     
@@ -89,11 +89,11 @@ class MainWindow(Qt.QWidget):
             return Qt.QImage(numpy_array.data, width, height, bytes_per_line, Qt.QImage.Format_Indexed8)
     
     def refresh_HSV(self):
-        if self.m_mat is None:
+        if self.image_source is None:
             return
 
         if self.ui.rbOriginal.isChecked():
-            result_img = self.numpy_array_to_QImage(self.m_mat)
+            result_img = self.numpy_array_to_QImage(self.image_source)
 
         else:
             hue_from = self.ui.slHueFrom.value()
@@ -105,50 +105,54 @@ class MainWindow(Qt.QWidget):
             value_from = self.ui.slValueFrom.value()
             value_to = max(value_from, self.ui.slValueTo.value())
 
-            thresholded_mat = cv2.cvtColor(self.m_mat, cv2.COLOR_RGB2HSV)
-            print(np.array((hue_from, saturation_from, value_from), np.uint8))
-            print(np.array((hue_to, saturation_to, value_to), np.uint8))
+            thresholded_image = cv2.cvtColor(self.image_source, cv2.COLOR_RGB2HSV)
+
+            hsv_min = hue_from, saturation_from, value_from
+            hsv_max = hue_to, saturation_to, value_to
+
+            # TODO: под слайдерами показывать пример цвета с hsv_min и hsv_max
+
             # Отфильтровываем только то, что нужно, по диапазону цветов
-            thresholded_mat = cv2.inRange(
-                thresholded_mat,
-                np.array((hue_from, saturation_from, value_from), np.uint8),
-                np.array((hue_to, saturation_to, value_to), np.uint8)
+            thresholded_image = cv2.inRange(
+                thresholded_image,
+                np.array(hsv_min, np.uint8),
+                np.array(hsv_max, np.uint8)
             )
 
             # Убираем шум
-            thresholded_mat = cv2.erode(
-                thresholded_mat,
+            thresholded_image = cv2.erode(
+                thresholded_image,
                 cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
             )
-            thresholded_mat = cv2.dilate(
-                thresholded_mat,
+            thresholded_image = cv2.dilate(
+                thresholded_image,
                 cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
             )
 
             # Замыкаем оставшиеся крупные объекты
-            thresholded_mat = cv2.dilate(
-                thresholded_mat,
+            thresholded_image = cv2.dilate(
+                thresholded_image,
                 cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
             )
-            thresholded_mat = cv2.erode(
-                thresholded_mat,
+            thresholded_image = cv2.erode(
+                thresholded_image,
                 cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
             )
 
             if self.ui.rbCanny.isChecked():
                 # Визуально выделяем границы
-                thresholded_mat = cv2.Canny(thresholded_mat, 100, 50, 5)
+                thresholded_image = cv2.Canny(thresholded_image, 100, 50, 5)
 
             if self.ui.rbResult.isChecked():
                 # Находим контуры
                 _, countours, hierarchy = cv2.findContours(
-                    thresholded_mat,
+                    thresholded_image,
                     cv2.RETR_TREE,
                     # cv2.RETR_EXTERNAL,
                     cv2.CHAIN_APPROX_SIMPLE
                 )
 
-                result_img = self.numpy_array_to_QImage(self.m_mat)
+                result_img = self.numpy_array_to_QImage(self.image_source)
 
                 p = Qt.QPainter(result_img)
                 p.setPen(Qt.QPen(Qt.Qt.green, 2))
@@ -160,7 +164,7 @@ class MainWindow(Qt.QWidget):
                 p.end()
 
             else:
-                result_img = self.numpy_array_to_QImage(thresholded_mat)
+                result_img = self.numpy_array_to_QImage(thresholded_image)
 
         size = self.ui.lbView.size()
         pixmap = Qt.QPixmap.fromImage(result_img).scaled(size, Qt.Qt.KeepAspectRatio, Qt.Qt.SmoothTransformation)
@@ -169,7 +173,7 @@ class MainWindow(Qt.QWidget):
     def closeEvent(self, e):
         for w in self.findChildren(Qt.QSlider):
             name = w.objectName()
-            self.m_settings.setValue(name, w.value())
+            self.settings.setValue(name, w.value())
 
 
 if __name__ == '__main__':
