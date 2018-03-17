8e81ca1e0998a278780a9680d160dd4d08dc6b76
ipetrash <ilya.petrash@inbox.ru>
2018-03-17 22:42:59 +0500

    Append opencv__examples:   * screenshot__full.py   * screenshot__resize.py

---
+++ opencv__examples/screenshot__full.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import cv2
+import numpy as np
+import pyautogui
+
+img = pyautogui.screenshot()
+img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+cv2.imshow('img', img)
+
+cv2.waitKey()
---
+++ opencv__examples/screenshot__resize.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import cv2
+import numpy as np
+import pyautogui
+
+img = pyautogui.screenshot()
+img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+img = cv2.resize(img, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
+cv2.imshow('img', img)
+
+cv2.waitKey()
