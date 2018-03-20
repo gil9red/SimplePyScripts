75cfcf22b72fc1de44290f91cd04d2033120f518
ipetrash <ilya.petrash@inbox.ru>
2018-03-20 19:25:10 +0500

    Append "opencv__examples\Find center of contour"

---
+++ opencv__examples/Find center of contour/main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://www.pyimagesearch.com/2016/02/01/opencv-center-of-contour/
+
+import cv2
+
+img = cv2.imread('example.jpg')
+cv2.imshow('img', img)
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+
+# Find contours in the thresholded image
+cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[1]
+
+# Loop over the contours
+for c in cnts:
+    try:
+        # Compute the center of the contour
+        M = cv2.moments(c)
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+
+        # Draw the contour and center of the shape on the image
+        cv2.drawContours(img, [c], -1, (0, 255, 0), 2)
+        cv2.circle(img, (cX, cY), 7, (255, 255, 255), -1)
+        cv2.putText(img, "center", (cX - 20, cY - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+
+    except ZeroDivisionError:
+        pass
+
+# Show the image
+cv2.imshow("result", img)
+cv2.imwrite('output.jpg', img)
+
+cv2.waitKey()
