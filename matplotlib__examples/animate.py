ded7c2273ad55306aecf05c6b29121bb66b4b653
ipetrash <ilya.petrash@inbox.ru>
2018-03-21 15:09:09 +0500

    #

---
+++ matplotlib__examples/animate.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import psutil
+from datetime import datetime as dt
+from collections import deque
+import matplotlib.pyplot as plt
+from matplotlib.dates import date2num
+import matplotlib.animation as animation
+
+N = 600
+x = deque([date2num(dt.now())], maxlen=N)
+y = deque([0], maxlen=N)
+
+fig, ax = plt.subplots(figsize=(8, 3))
+line, = ax.plot_date(x, y, marker="")
+
+ax.spines['left'].set_visible(False)
+ax.spines['bottom'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+
+
+def get_data():
+    return psutil.cpu_percent(.15)
+
+
+def animate(i):
+    x.append(date2num(dt.now()))
+    y.append(get_data())
+    ax.relim(visible_only=True)
+    ax.autoscale_view(True)
+    line.set_data(x, y)
+    ax.fill_between(x, -0.5, y, color='lightgrey')
+    return line,
+
+
+ani = animation.FuncAnimation(fig, animate, interval=300)
+# ani.save("test.gif", writer='imagemagick', fps=10)
+plt.show()
