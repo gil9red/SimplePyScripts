14625263ec54d7348dc66b1577d184788623f42b
ipetrash <ilya.petrash@inbox.ru>
2018-03-21 16:36:47 +0500

    #

---
+++ Bot Buff Knight Advanced/common.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from datetime import datetime
+import cv2
+import numpy as np
+
+
+def get_logger(name, file='log.txt', encoding='utf-8', log_stdout=True, log_file=True):
+    import logging
+    log = logging.getLogger(name)
+    log.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('[%(asctime)s] %(filename)s:%(lineno)d %(levelname)-8s %(message)s')
+
+    if log_file:
+        from logging.handlers import RotatingFileHandler
+        fh = RotatingFileHandler(file, maxBytes=10000000, backupCount=5, encoding=encoding)
+        fh.setFormatter(formatter)
+        log.addHandler(fh)
+
+    if log_stdout:
+        import sys
+        sh = logging.StreamHandler(stream=sys.stdout)
+        sh.setFormatter(formatter)
+        log.addHandler(sh)
+
+    return log
+
+
+def get_current_datetime_str():
+    return datetime.now().strftime('%d%m%y %H%M%S.%f')
+
+
+def find_contours(image_source_hsv, hsv_min, hsv_max):
+    thresholded_image = image_source_hsv
+
+    # Отфильтровываем только то, что нужно, по диапазону цветов
+    thresholded_image = cv2.inRange(
+        thresholded_image,
+        np.array(hsv_min, np.uint8),
+        np.array(hsv_max, np.uint8)
+    )
+
+    # Убираем шум
+    thresholded_image = cv2.erode(
+        thresholded_image,
+        cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    )
+    thresholded_image = cv2.dilate(
+        thresholded_image,
+        cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    )
+
+    # Замыкаем оставшиеся крупные объекты
+    thresholded_image = cv2.dilate(
+        thresholded_image,
+        cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    )
+    thresholded_image = cv2.erode(
+        thresholded_image,
+        cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    )
+
+    # Находим контуры
+    _, contours, hierarchy = cv2.findContours(
+        thresholded_image,
+        cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE
+    )
+
+    return contours
+
+
+def find_rect_contours(image_source_hsv, hsv_min, hsv_max):
+    return [cv2.boundingRect(c) for c in find_contours(image_source_hsv, hsv_min, hsv_max)]
--- Bot Buff Knight Advanced/main.py
+++ Bot Buff Knight Advanced/main.py
@@ -7,99 +7,17 @@ __author__ = 'ipetrash'
 
 import threading
 import os
+from timeit import default_timer as timer
+import time
+
 import cv2
 import numpy as np
 import pyautogui
-from timeit import default_timer as timer
-from datetime import datetime
-import time
 
-
-# TODO: можно вынести в common.py: get_logger, get_current_datetime_str, find_contours, find_rect_contours
-
-
-def get_logger(name, file='log.txt', encoding='utf-8', log_stdout=True, log_file=True):
-    import logging
-    log = logging.getLogger(name)
-    log.setLevel(logging.DEBUG)
-
-    formatter = logging.Formatter('[%(asctime)s] %(filename)s:%(lineno)d %(levelname)-8s %(message)s')
-
-    if log_file:
-        from logging.handlers import RotatingFileHandler
-        fh = RotatingFileHandler(file, maxBytes=10000000, backupCount=5, encoding=encoding)
-        fh.setFormatter(formatter)
-        log.addHandler(fh)
-
-    if log_stdout:
-        import sys
-        sh = logging.StreamHandler(stream=sys.stdout)
-        sh.setFormatter(formatter)
-        log.addHandler(sh)
-
-    return log
+from common import get_logger, get_current_datetime_str, find_contours, find_rect_contours
 
 
 log = get_logger('Bot Buff Knight Advanced')
-
-
-def get_current_datetime_str():
-    return datetime.now().strftime('%d%m%y %H%M%S.%f')
-
-
-def find_contours(image_source_hsv, hsv_min, hsv_max):
-    thresholded_image = image_source_hsv
-
-    # Отфильтровываем только то, что нужно, по диапазону цветов
-    thresholded_image = cv2.inRange(
-        thresholded_image,
-        np.array(hsv_min, np.uint8),
-        np.array(hsv_max, np.uint8)
-    )
-
-    # Убираем шум
-    thresholded_image = cv2.erode(
-        thresholded_image,
-        cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-    )
-    thresholded_image = cv2.dilate(
-        thresholded_image,
-        cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-    )
-
-    # Замыкаем оставшиеся крупные объекты
-    thresholded_image = cv2.dilate(
-        thresholded_image,
-        cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-    )
-    thresholded_image = cv2.erode(
-        thresholded_image,
-        cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-    )
-
-    # Находим контуры
-    _, contours, hierarchy = cv2.findContours(
-        thresholded_image,
-        cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE
-    )
-
-    return contours
-
-
-def find_rect_contours(image_source_hsv, hsv_min, hsv_max):
-    return [cv2.boundingRect(c) for c in find_contours(image_source_hsv, hsv_min, hsv_max)]
-
-
-# def draw_rects(img, contours_rects, color=(0, 255, 0)):
-#     for x, y, w, h in contours_rects:
-#         cv2.rectangle(img, (x, y), (x + w, y + h), color, thickness=5)
-#         # cv2.rectangle(img, (x, y), (x + w, y + h), color, thickness=-1)
-#
-#         for i in range(h // 10):
-#             cv2.line(img, (x, y + i * 10 + 5), (x + w, y + i * 10 + 5), color, thickness=2)
-#
-#         cv2.line(img, (0, y), (img.shape[1], y), color, thickness=1)
 
 
 def filter_button(rect):
@@ -109,12 +27,6 @@ def filter_button(rect):
 
     rule_1 = w > 30 and h > 30
     rule_2 = w > h
-
-    # if not rule_1:
-    #     print('filter_button. Fail rule_1')
-    #
-    # if not rule_2:
-    #     print('filter_button. Fail rule_2')
 
     # У кнопок ширина больше высоты
     return rule_1 and rule_2
@@ -133,15 +45,6 @@ def filter_fairy(rect):
 
     # Фея приблизительно по центру экрана летает
     rule_3 = y > 300 and y < 900
-
-    # if not rule_1:
-    #     print('filter_fairy. Fail rule_1')
-    #
-    # if not rule_2:
-    #     print('filter_fairy. Fail rule_2')
-    #
-    # if not rule_3:
-    #     print('filter_fairy. Fail rule_3')
 
     return rule_1 and rule_2 and rule_3
 
@@ -201,7 +104,6 @@ log.debug('Press "%s" for AUTO_ATTACK', AUTO_ATTACK_COMBINATION)
 if not os.path.exists(DIR):
     os.mkdir(DIR)
 
-# TODO: возможность автоматического использования хилок и восстановления маны
 
 import keyboard
 keyboard.add_hotkey(QUIT_COMBINATION, lambda: log.debug('Quit by Escape') or os._exit(0))
@@ -210,21 +112,16 @@ keyboard.add_hotkey(RUN_COMBINATION, change_start)
 
 
 def process_auto_attack():
-    # i = 1
-
     while True:
         if not BOT_DATA['START']:
             time.sleep(0.01)
             continue
-
-        # print(i, 'AUTO_ATTACK:', BOT_DATA['AUTO_ATTACK'])
 
         # Симуляция атаки
         if BOT_DATA['AUTO_ATTACK']:
             pyautogui.typewrite('C')
 
         time.sleep(0.01)
-        # i += 1
 
 # Запуск потока для автоатаки
 thread_auto_attack = threading.Thread(target=process_auto_attack)
@@ -266,18 +163,6 @@ def process_find_fairy(img_hsv):
     rects_blue = list(filter(lambda rect: filter_fairy_and_button(rect_fairy, rect), rects_blue))
     rects_orange = list(filter(lambda rect: filter_fairy_and_button(rect_fairy, rect), rects_orange))
 
-    # print('rects_blue({}): {}'.format(len(rects_blue), rects_blue))
-    # print('rects_orange({}): {}'.format(len(rects_orange), rects_orange))
-    # print('rects_fairy({}): {}'.format(len(rects_fairy), rects_fairy))
-    #
-    # draw_rects(img, rects_blue, (255, 0, 0))
-    # draw_rects(img, rects_orange, (0, 0, 255))
-    # draw_rects(img, rects_fairy, (255, 255, 255))
-    #
-    # cv2.putText(img, 'blue ' + str(rects_blue), (10, 500 + 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-    # cv2.putText(img, 'orange ' + str(rects_orange), (10, 500 + 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-    # cv2.putText(img, 'fairy ' + str(rects_fairy), (10, 500 + 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-
     # Если одновременно обе кнопки
     if rects_blue and rects_orange:
         save_screenshot('many_buttons', img_hsv)
@@ -316,6 +201,8 @@ while True:
 
         # Поиск феи
         process_find_fairy(img)
+
+        # TODO: возможность автоматического использования хилок и восстановления маны
 
     finally:
         log.debug('Elapsed: {} secs'.format(timer() - t))
