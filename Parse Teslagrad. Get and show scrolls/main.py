ad82be81719022078af34186140e54b7a39f895d
ipetrash <ilya.petrash@inbox.ru>
2018-03-22 05:28:43 +0500

    Append "Parse Teslagrad. Get and show scrolls"

---
+++ Parse Teslagrad. Get and show scrolls/main.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+__author__ = 'ipetrash'
+
+
+# Teslagrad. Прохождение игры на 100%. Карта расположения и изображения свитков (Сайт GamesisArt.ru)
+
+import os
+from urllib.parse import urljoin
+import requests
+
+# Cache
+if not os.path.exists('scrolls.html'):
+    rs = requests.get('http://gamesisart.ru/guide/Teslagrad_Prohozhdenie_4.html#Scrolls')
+    html = rs.content
+
+    with open('scrolls.html', 'wb') as f:
+        f.write(html)
+
+else:
+    html = open('scrolls.html', 'rb').read()
+
+
+URL = 'http://gamesisart.ru/guide/Teslagrad_Prohozhdenie_4.html#Scrolls'
+DIR_SCROLLS = 'scrolls'
+
+from bs4 import BeautifulSoup
+root = BeautifulSoup(html, 'html.parser')
+
+img_urls = [img['src'] for img in root.select('img[src]')]
+img_urls = [urljoin(URL, url_img) for url_img in img_urls if '/Teslagrad_Scroll_' in url_img]
+print(len(img_urls), img_urls)
+
+if not os.path.exists(DIR_SCROLLS):
+    os.mkdir(DIR_SCROLLS)
+
+# Save images
+for url in img_urls:
+    rs = requests.get(url)
+    img_data = rs.content
+
+    file_name = DIR_SCROLLS + '/' + os.path.basename(url)
+
+    with open(file_name, 'wb') as f:
+        f.write(img_data)
+
+# Merge all image into one
+IMAGE_WIDTH = 200
+IMAGE_HEIGHT = 376
+ROWS = 9
+COLS = 4
+
+from PIL import Image
+image = Image.new('RGB', (IMAGE_WIDTH * COLS, IMAGE_HEIGHT * ROWS))
+
+import glob
+file_names = sorted(glob.glob('scrolls/*.jpg'), key=lambda x: int(x.split('.')[0].split('_')[-1]))
+it = iter(file_names)
+
+for y in range(0, IMAGE_HEIGHT * ROWS, IMAGE_HEIGHT):
+    for x in range(0, IMAGE_WIDTH * COLS, IMAGE_WIDTH):
+        file_name = next(it)
+        img = Image.open(file_name)
+
+        image.paste(img, (x, y))
+
+image.save('scrolls.jpg')
+image.show()
