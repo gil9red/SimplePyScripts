bf69aad8bfb0617a554735ef814b35df899748bc
ipetrash <ilya.petrash@inbox.ru>
2018-03-30 16:39:02 +0500

    Append pil_example\pixelate_image

---
+++ pil_example/pixelate_image/main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://gist.github.com/danyshaanan/6754465
+
+"""
+A Python script to pixelate an image and add a thin black margin between the simulated pixels.
+
+"""
+
+from PIL import Image
+
+background_color = (0,) * 3
+pixel_size = 9
+
+image = Image.open('input.jpg')
+image = image.resize((image.size[0] // pixel_size, image.size[1] // pixel_size), Image.NEAREST)
+image = image.resize((image.size[0] * pixel_size, image.size[1] * pixel_size), Image.NEAREST)
+pixel = image.load()
+
+# Draw black margin between pixels
+for i in range(0, image.size[0], pixel_size):
+    for j in range(0, image.size[1], pixel_size):
+        for r in range(pixel_size):
+            pixel[i+r, j] = background_color
+            pixel[i, j+r] = background_color
+
+image.save('output.jpg')
+image.show()
