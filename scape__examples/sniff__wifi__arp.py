4452694172a6594616f0056f063326d9cbcd70ec
ipetrash <ilya.petrash@inbox.ru>
2018-03-30 20:54:04 +0500

    Append scape__examples/sniff__wifi__arp.py

---
+++ scape__examples/sniff__wifi__arp.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+# SOURCE: http://www.secdev.org/projects/scapy/build_your_own_tools.html
+
+
+from scapy.all import *
+
+
+"""
+Here is another tool that will constantly monitor all interfaces on a machine and print all ARP request 
+it sees, even on 802.11 frames from a Wi-Fi card in monitor mode. Note the store=0 parameter to sniff() 
+to avoid storing all packets in memory for nothing. 
+"""
+
+
+def arp_monitor_callback(pkt):
+    if ARP in pkt and pkt[ARP].op in (1,2):  # who-has or is-at
+        return pkt.sprintf("%ARP.hwsrc% %ARP.psrc%")
+
+sniff(prn=arp_monitor_callback, filter="arp", store=0)
