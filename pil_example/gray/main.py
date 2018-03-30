f12bced795d3cec866edda02b70f2fff1e066da3
ipetrash <ilya.petrash@inbox.ru>
2018-03-30 15:30:26 +0500

    Append pil_example/gray Append pil_example/invert_gray

---
+++ pil_example/gray/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PIL import Image, ImageOps
+
+image_file = "example.jpg"
+image = Image.open(image_file)
+
+image_gray = ImageOps.grayscale(image)
+image_gray.save('image_gray.png')
+image_gray.show()
---
+++ pil_example/invert_gray/main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PIL import Image, ImageOps
+
+
+def invert(image):
+    if image.mode == 'RGBA':
+        r, g, b, a = image.split()
+        rgb_image = Image.merge('RGB', (r, g, b))
+        inverted_image = ImageOps.invert(rgb_image)
+        r2, g2, b2 = inverted_image.split()
+        return Image.merge('RGBA', (r2, g2, b2, a))
+
+    else:
+        return ImageOps.invert(image)
+
+
+if __name__ == '__main__':
+    image_file = "example.jpg"
+    image = Image.open(image_file)
+
+    image_invert = invert(image)
+    image_invert_gray = ImageOps.grayscale(image_invert)
+    image_invert_gray.save('image_invert_gray.png')
+    image_invert_gray.show()
+
