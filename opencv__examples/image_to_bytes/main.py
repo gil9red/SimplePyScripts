5f57e3367152045261d09f9785a678ad06e56ad7
ipetrash <ilya.petrash@inbox.ru>
2018-03-30 17:02:21 +0500

    Append opencv__examples\image_to_bytes

---
+++ opencv__examples/image_to_bytes/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install opencv-python
+import cv2
+
+img = cv2.imread('example.jpg')
+# cv2.imshow('img', img)
+
+img_data = cv2.imencode('.jpg', img)[1].tostring()
+print(len(img_data), img_data)
