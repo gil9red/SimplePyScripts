e9ab6397e5dd2223d93579abf60b434cf2f6af0e
ipetrash <ilya.petrash@inbox.ru>
2018-03-30 15:11:09 +0500

    #

--- pil_example/invert/main.py
+++ pil_example/invert/main.py
@@ -7,19 +7,26 @@ __author__ = 'ipetrash'
 """Инвертирование цвета картинки"""
 
 
-if __name__ == '__main__':
-    from PIL import Image
-    import PIL.ImageOps
+from PIL import Image
+import PIL.ImageOps
 
-    image_file = "TowerOfGod_s2_ch100_p02_SIU.png_res.jpg"
-    image = Image.open(image_file)
+
+def invert(image):
     if image.mode == 'RGBA':
         r, g, b, a = image.split()
         rgb_image = Image.merge('RGB', (r, g, b))
         inverted_image = PIL.ImageOps.invert(rgb_image)
         r2, g2, b2 = inverted_image.split()
-        final_transparent_image = Image.merge('RGBA', (r2, g2, b2, a))
-        final_transparent_image.save('new_file.png')
+        return Image.merge('RGBA', (r2, g2, b2, a))
+
     else:
-        inverted_image = PIL.ImageOps.invert(image)
-        inverted_image.save('new_name.png')
+        return PIL.ImageOps.invert(image)
+
+
+if __name__ == '__main__':
+    image_file = "TowerOfGod_s2_ch100_p02_SIU.png_res.jpg"
+    image = Image.open(image_file)
+
+    inverted_image = invert(image)
+    inverted_image.save('new_name.png')
+    inverted_image.show()
