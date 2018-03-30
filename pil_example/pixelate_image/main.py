97ecd6282ebf8a54f461845ad1005c6fa0b8a6b4
ipetrash <ilya.petrash@inbox.ru>
2018-03-30 17:42:21 +0500

    #

--- pil_example/pixelate_image/main.py
+++ pil_example/pixelate_image/main.py
@@ -13,23 +13,32 @@ A Python script to pixelate an image and add a thin black margin between the sim
 
 from PIL import Image
 
-background_color = (0, 0, 0)  # black
-pixel_size = 9
 
-image = Image.open('input.jpg')
-image = image.resize((image.size[0] // pixel_size, image.size[1] // pixel_size), Image.NEAREST)
-image = image.resize((image.size[0] * pixel_size, image.size[1] * pixel_size), Image.NEAREST)
-pixel = image.load()
+def pixelate(image, pixel_size=9, draw_margin=True):
+    margin_color = (0, 0, 0)
 
-image.save('output_without_margin.jpg')
-image.show()
+    image = image.resize((image.size[0] // pixel_size, image.size[1] // pixel_size), Image.NEAREST)
+    image = image.resize((image.size[0] * pixel_size, image.size[1] * pixel_size), Image.NEAREST)
+    pixel = image.load()
 
-# Draw black margin between pixels
-for i in range(0, image.size[0], pixel_size):
-    for j in range(0, image.size[1], pixel_size):
-        for r in range(pixel_size):
-            pixel[i+r, j] = background_color
-            pixel[i, j+r] = background_color
+    # Draw black margin between pixels
+    if draw_margin:
+        for i in range(0, image.size[0], pixel_size):
+            for j in range(0, image.size[1], pixel_size):
+                for r in range(pixel_size):
+                    pixel[i+r, j] = margin_color
+                    pixel[i, j+r] = margin_color
 
-image.save('output.jpg')
-image.show()
+    return image
+
+
+if __name__ == '__main__':
+    image = Image.open('input.jpg')
+
+    image_pixelate = pixelate(image, draw_margin=False)
+    image_pixelate.save('output_without_margin.jpg')
+    image_pixelate.show()
+
+    image_pixelate = pixelate(image)
+    image_pixelate.save('output.jpg')
+    image_pixelate.show()
