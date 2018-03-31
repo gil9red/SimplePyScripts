e992a8bb1b5ea41f952e3a82c49346e73bb6b6c3
ipetrash <ilya.petrash@inbox.ru>
2018-03-31 05:28:28 +0500

    Append pil_example\pil_image__to__qimage_qpixmap__qt

---
+++ pil_example/pil_image__to__qimage_qpixmap__qt/main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install Pillow
+from PIL import Image
+from PyQt5 import Qt
+
+image_file = "input.jpg"
+image = Image.open(image_file)
+
+app = Qt.QApplication([])
+
+label_image = Qt.QLabel()
+pix_1 = Qt.QPixmap.fromImage(image.toqimage().copy())
+label_image.setPixmap(pix_1)
+
+label_pixmap = Qt.QLabel()
+pix_2 = image.toqpixmap()
+label_pixmap.setPixmap(pix_2)
+
+layout = Qt.QHBoxLayout()
+layout.addWidget(label_image)
+layout.addWidget(label_pixmap)
+
+w = Qt.QWidget()
+w.setLayout(layout)
+w.show()
+
+app.exec()
