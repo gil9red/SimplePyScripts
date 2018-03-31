9a4141a5522b316796e5eb38ef219bc1bf130c41
ipetrash <ilya.petrash@inbox.ru>
2018-03-31 16:12:20 +0500

    #

--- pil_example/jpeg_quality=1__шакалистые_фотки/main.py
+++ pil_example/jpeg_quality=1__шакалистые_фотки/main.py
@@ -5,5 +5,5 @@ __author__ = 'ipetrash'
 
 
 from PIL import Image
-im = Image.open('input.jpg')
-im.save('output.jpg', format='JPEG', quality=1)
+img = Image.open('input.jpg')
+img.save('output.jpg', format='JPEG', quality=1)
