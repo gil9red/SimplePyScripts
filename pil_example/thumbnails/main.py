ca3dd639c34bd477df06471632c82059425c3df2
ipetrash <ilya.petrash@inbox.ru>
2018-04-08 07:25:06 +0500

    Append pil_example\thumbnails

---
+++ pil_example/thumbnails/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install Pillow
+from PIL import Image
+
+image_file = "input.jpg"
+img = Image.open(image_file)
+
+size = 128, 128
+
+img.thumbnail(size)
+img.save('output_thumbnail.png')
+img.show()
