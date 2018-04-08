3d8ba15c400a87ab9919b9abef7d4f4dfb57b14a
ipetrash <ilya.petrash@inbox.ru>
2018-04-08 07:26:28 +0500

    Append pil_example\blur

---
+++ pil_example/blur/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install Pillow
+from PIL import Image, ImageFilter
+
+image_file = "input.jpg"
+img = Image.open(image_file)
+
+img = img.filter(ImageFilter.GaussianBlur(2))
+img.save('output.png')
+img.show()
