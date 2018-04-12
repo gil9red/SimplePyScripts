fe46f4558379df0d39dae43148560246b53788b8
ipetrash <ilya.petrash@inbox.ru>
2018-04-12 13:55:59 +0500

    #

--- qbittorrent_example/get_info_random_torrent.py
+++ qbittorrent_example/get_info_random_torrent.py
@@ -8,7 +8,9 @@ def sizeof_fmt(num):
     for x in ['bytes', 'KB', 'MB', 'GB']:
         if num < 1024.0:
             return "%3.1f %s" % (num, x)
+
         num /= 1024.0
+
     return "%3.1f %s" % (num, 'TB')
 
 
--- web_servers/upload_and_download_files/test_download_from_script.py
+++ web_servers/upload_and_download_files/test_download_from_script.py
@@ -7,8 +7,10 @@ __author__ = 'ipetrash'
 def sizeof_fmt(num):
     for x in ['bytes', 'KB', 'MB', 'GB']:
         if num < 1024.0:
-            return "%3.1f%s" % (num, x)
+            return "%3.1f %s" % (num, x)
+
         num /= 1024.0
+
     return "%3.1f %s" % (num, 'TB')
 
 
