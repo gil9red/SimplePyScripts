da39e96039fbc6ed0f7975889655068e98a4f2cf
ipetrash <ilya.petrash@inbox.ru>
2018-04-18 00:01:19 +0500

    Append tkinter_examples/super_hello_world.py

---
+++ tkinter_examples/super_hello_world.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# https://docs.python.org/3/library/tkinter.html
+
+
+import tkinter as tk
+from tkinter import messagebox
+
+
+def center_window(root, width=300, height=200):
+    # get screen width and height
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    # calculate position x and y coordinates
+    x = (screen_width / 2) - (width / 2)
+    y = (screen_height / 2) - (height / 2)
+    root.geometry('%dx%d+%d+%d' % (width, height, x, y))
+
+
+def on_btn_1():
+    print("Hello World!")
+
+
+def on_btn_2():
+    text = "Hello World!"
+
+    messagebox.showerror("Error", text)
+    messagebox.showwarning("Warning", text)
+    messagebox.showinfo("Information", text)
+
+
+root = tk.Tk()
+root.title("super_hello_world")
+center_window(root)
+
+label = tk.Label(root, text="Hello World!", font=("Helvetica", 16), fg='red')
+label.pack()
+
+btn_1 = tk.Button(root, text="Hello World!", command=on_btn_1)
+btn_1.pack()
+
+btn_2 = tk.Button(root, text="Click Me!", command=on_btn_2)
+btn_2.pack()
+
+root.mainloop()
