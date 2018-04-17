dfb1cd1201bbdf26849615a5f77275c428b7e5a2
ipetrash <ilya.petrash@inbox.ru>
2018-04-18 01:33:40 +0500

    Append play_mp3__pyqt5__qmediaplayer

---
+++ play_mp3__pyqt5__qmediaplayer/play.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5 import Qt
+
+
+def _on_media_status_changed(status):
+    if status == Qt.QMediaPlayer.EndOfMedia:
+        Qt.QCoreApplication.instance().quit()
+
+
+app = Qt.QCoreApplication([])
+
+player = Qt.QMediaPlayer()
+file_name = Qt.QUrl.fromLocalFile('example.mp3')
+player.setMedia(Qt.QMediaContent(file_name))
+player.mediaStatusChanged.connect(_on_media_status_changed)
+player.play()
+
+app.exec()
