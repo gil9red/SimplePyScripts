ed701f1dfb02f91934e0bbb1efff26c8e9be3e7e
ipetrash <ilya.petrash@inbox.ru>
2018-04-21 03:18:47 +0500

    Append tkinter_examples/center_window.py

---
+++ tkinter_examples/center_window.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import tkinter as tk
+
+
+def center_window(root, width=300, height=200):
+    # get screen width and height
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    # calculate position x and y coordinates
+    x = (screen_width / 2) - (width / 2)
+    y = (screen_height / 2) - (height / 2)
+    root.geometry('%dx%d+%d+%d' % (width, height, x, y))
+
+
+app = tk.Tk()
+app.title("center_window")
+center_window(app)
+
+label = tk.Label(app, text="Hello World!", font="Arial 16", fg='red')
+label.pack()
+
+app.mainloop()
