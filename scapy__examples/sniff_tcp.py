7a593c075a36aae176c05e89ad7b6b45ee9d0806
ipetrash <ilya.petrash@inbox.ru>
2018-04-20 11:42:03 +0500

    Append scapy__examples/sniff_tcp.py

---
+++ scapy__examples/sniff_tcp.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# https://github.com/secdev/scapy
+# pip install scapy
+from scapy.all import sniff
+
+
+def _packethandler(pkt):
+    data = pkt.summary()
+    print(data)
+
+
+sniff(filter="tcp", prn=_packethandler)
