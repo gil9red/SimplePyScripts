d535c8f2bd1802928baa1e47c166a7e2f9df35a3
ipetrash <ilya.petrash@inbox.ru>
2018-04-21 03:33:46 +0500

    Append tkinter_examples/entry_echo.py

---
+++ tkinter_examples/entry_echo.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import tkinter as tk
+from center_window import center_window
+
+
+app = tk.Tk()
+app.title("entry_echo")
+center_window(app)
+
+
+def _on_key_press(x):
+    text = entry.get()
+
+    label_1['text'] = text
+    label_2['text'] = text[::-1]
+
+
+entry = tk.Entry(app)
+entry.bind('<KeyPress>', _on_key_press)
+entry.pack()
+
+label_1 = tk.Label(app)
+label_1.pack()
+
+label_2 = tk.Label(app)
+label_2.pack()
+
+
+app.mainloop()
