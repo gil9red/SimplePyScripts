7ece93cd4e848c5ac55929d9f41bb865392253a0
ipetrash <ilya.petrash@inbox.ru>
2018-04-25 02:16:41 +0500

    #

--- closure__counter.py
+++ closure__counter.py
@@ -19,9 +19,29 @@ def make_counter(start=0, step=1):
     return counter
 
 
-if __name__ == '__main__':
-    c1 = make_counter()
-    print(c1(), c1(), c1())  # 1 2 3
+def make_counter_adder(start=0):
+    i = start
 
-    c2 = make_counter(step=0.1)
-    print(c2(), c2(), c2())  # 0.1 0.2 0.30000000000000004
+    def counter(step=1):  # counter() is a closure
+        nonlocal i
+        i += step
+
+        return i
+
+    return counter
+
+
+if __name__ == '__main__':
+    c = make_counter()
+    print(c(), c(), c())  # 1 2 3
+
+    c = make_counter(step=0.1)
+    print(c(), c(), c())  # 0.1 0.2 0.30000000000000004
+
+    print()
+
+    c = make_counter_adder()
+    print(c(1), c(1), c(1))  # 1 2 3
+
+    c = make_counter_adder(0.0)
+    print(c(0.1), c(0.1), c(0.1))  # 0.1 0.2 0.30000000000000004
