77856ffdf6d2bd9983d69d9c8fc43ca816e5a103
ipetrash <ilya.petrash@inbox.ru>
2018-04-26 17:18:13 +0500

    Append qt__pyqt__pyside__pyqode/pyqt5__winapi__get_color_window_header__use_screenshot.py

---
+++ qt__pyqt__pyside__pyqode/pyqt5__winapi__get_color_window_header__use_screenshot.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5 import Qt
+
+
+if __name__ == '__main__':
+    app = Qt.QApplication([])
+
+    # NOTE: для отладки
+    # label = Qt.QLabel()
+
+    def get_color():
+        rect = button.rect()
+        indent = 5
+
+        pixmap = app.primaryScreen().grabWindow(button.winId(), rect.x(), rect.y() - indent, indent, indent)
+        # label.setPixmap(pixmap)
+        # label.setScaledContents(True)
+        # label.resize(400, 400)
+        # label.show()
+
+        color = pixmap.toImage().pixelColor(0, 0)
+        button.setStyleSheet('background-color: ' + color.name())
+
+    button = Qt.QPushButton('Get')
+    button.clicked.connect(get_color)
+    button.resize(100, 100)
+    button.show()
+
+    app.exec()
