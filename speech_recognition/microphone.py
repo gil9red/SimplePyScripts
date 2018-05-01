c1217419e70cea961c0d3fca9dcdc4124aadfd32
ipetrash <ilya.petrash@inbox.ru>
2018-05-01 20:37:02 +0500

    Append speech_recognition/microphone.py

---
+++ speech_recognition/microphone.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: http://pythono.ru/speech-recognition-python/
+
+
+# pip install pyaudio
+# pip install SpeechRecognition
+import speech_recognition as sr
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Скажите что-нибудь")
+    audio = r.listen(source)
+
+try:
+    text = r.recognize_google(audio, language="ru-RU")
+    print('Фраза: "{}"'.format(text))
+
+except sr.UnknownValueError:
+    print("Робот не расслышал фразу")
+
+except sr.RequestError as e:
+    print("Ошибка сервиса; {0}".format(e))
