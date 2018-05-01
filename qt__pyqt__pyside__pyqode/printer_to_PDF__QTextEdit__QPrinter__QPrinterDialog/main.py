7529051c8c40c7fcad0e5f9f4d6e7de2e3471145
ipetrash <ilya.petrash@inbox.ru>
2018-05-01 19:04:46 +0500

    Append qt__pyqt__pyside__pyqode\printer_to_PDF__QTextEdit__QPrinter__QPrinterDialog

---
+++ qt__pyqt__pyside__pyqode/printer_to_PDF__QTextEdit__QPrinter__QPrinterDialog/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5 import Qt
+
+
+# https://stackoverflow.com/a/8499716/5909792
+html = """
+<p>Red dot:</p><br>
+<img width="100" src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==" alt="Red dot" />
+"""
+
+
+if __name__ == '__main__':
+    app = Qt.QApplication([])
+
+    printer = Qt.QPrinter()
+
+    te = Qt.QTextEdit()
+    te.setHtml(html)
+    te.show()
+
+    printer.setOutputFileName('result.pdf')
+    te.print(printer)
+
+    app.exec()
