2e82b16db26e38a3824ea116f1d1748a3242a4dc
ipetrash <ilya.petrash@inbox.ru>
2018-05-10 23:28:22 +0500

    Append openweathermap__pyowm__weather.py

---
+++ openweathermap__pyowm__weather.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+API_KEY = '87c7712a9b72646a269102230858837b'
+place = 'Магнитогорск'
+
+# pip install pyowm
+import pyowm
+owm = pyowm.OWM(API_KEY)
+observation = owm.weather_at_place(place)
+w = observation.get_weather()
+temperature = w.get_temperature('celsius')['temp']
+status = w.get_status()
+
+print('Температура: {} °C'.format(temperature))
+print('Небо: {}'.format(status))
