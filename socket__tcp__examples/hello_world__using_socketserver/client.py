c359adbb79c6a2dc9457214b428200da9cc37e54
ipetrash <ilya.petrash@inbox.ru>
2018-05-12 10:30:43 +0500

    Append socket__tcp__examples\hello_world__using_socketserver

---
+++ socket__tcp__examples/hello_world__using_socketserver/client.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://habrahabr.ru/post/149077/
+
+
+import socket
+
+import sys
+sys.path.append('..')
+
+from common import send_msg, recv_msg
+
+
+HOST, PORT = "localhost", 9090
+
+
+with socket.socket() as sock:
+    sock.connect((HOST, PORT))
+
+    # Send big data
+    # data = ','.join(str(i) for i in range(10000))
+    data = 'HelloWorld!' * 10000
+    print('Sending ({}): {}'.format(len(data), data))
+    print()
+
+    send_msg(sock, data.encode())
+
+    print('Receiving')
+
+    response_data = recv_msg(sock)
+    print('Response ({}): {}'.format(len(response_data), response_data))
+
+    print('Close\n')
---
+++ socket__tcp__examples/hello_world__using_socketserver/server.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://docs.python.org/3.6/library/socketserver.html#socketserver-tcpserver-example
+
+
+import socketserver
+
+import sys
+sys.path.append('..')
+
+from common import send_msg, recv_msg
+
+
+HOST, PORT = "localhost", 9090
+
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    """
+    The request handler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        print('Connected:', self.client_address)
+
+        data = recv_msg(self.request)
+        print('Receiving: {}: {}'.format(len(data), data))
+
+        print('Sending')
+        send_msg(self.request, data.upper())
+
+        print('Close\n')
+
+
+if __name__ == "__main__":
+    # Create the server, binding to localhost on port 9999
+    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
+        print('Server: {}'.format(server.server_address))
+
+        # Activate the server; this will keep running until you
+        # interrupt the program with Ctrl-C
+        server.serve_forever()
