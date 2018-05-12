3104e25fbc872279745c7adccc4ca504a96c5f19
ipetrash <ilya.petrash@inbox.ru>
2018-05-12 09:07:08 +0500

    #

--- socket_examples/hello_world/client.py
+++ socket_examples/hello_world/client.py
@@ -6,35 +6,41 @@ __author__ = 'ipetrash'
 
 # SOURCE: https://habrahabr.ru/post/149077/
 
+
+import socket
+
+
+HOST, PORT = "localhost", 9090
 BUFFER_SIZE = 4096
 
 
-import socket
-sock = socket.socket()
-sock.connect(('localhost', 9090))
+with socket.socket() as sock:
+    sock.connect((HOST, PORT))
 
-# Send big data
-# data = ','.join(str(i) for i in range(10000))
-data = 'HelloWorld!' * 10000
-print('Send len: {}'.format(len(data)))
-print()
+    # Send big data
+    # data = ','.join(str(i) for i in range(10000))
+    data = 'HelloWorld!' * 10000
+    print('Send len: {}'.format(len(data)))
+    print()
 
-sock.sendall(data.encode())
+    sock.sendall(data.encode())
 
-print('Response')
+    print('Response')
 
-response_data = bytearray()
+    response_data = bytearray()
 
-while True:
-    data = sock.recv(BUFFER_SIZE)
-    if not data:
-        break
+    while True:
+        data = sock.recv(BUFFER_SIZE)
+        if not data:
+            break
 
-    print(len(data), data)
-    response_data += data
+        print(len(data), data)
+        response_data += data
 
-print()
-print('Total len: {}'.format(len(response_data)))
+        if len(data) < BUFFER_SIZE:
+            break
 
-print('Close')
-sock.close()
+    print()
+    print('Total len: {}'.format(len(response_data)))
+
+    print('Close')
