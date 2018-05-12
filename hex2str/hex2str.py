6c0781de66aec4727616a06f536bcf052455cc88
ipetrash <ilya.petrash@inbox.ru>
2018-05-12 11:26:35 +0500

    Support utf-8

--- hex2str/hex2str.py
+++ hex2str/hex2str.py
@@ -4,33 +4,27 @@
 __author__ = 'ipetrash'
 
 
-def hex2str(hex_string):
-    items = list()
-
-    for i in range(0, len(hex_string), 2):
-        hex2 = hex_string[i] + hex_string[i + 1]
-        char = chr(int(hex2, 16))
-        items.append(char)
-
-    return ''.join(items)
+def hex2str(hex_string: str, encoding='utf-8') -> str:
+    data = bytes.fromhex(hex_string)
+    return str(data, encoding)
 
 
-def str2hex(text):
-    items = list()
+def str2hex(text: str, encoding='utf-8', upper=True) -> str:
+    hex_text = bytes(text, encoding).hex()
+    if upper:
+        hex_text = hex_text.upper()
 
-    for c in text:
-        # 'P' -> 0x50 -> 50
-        hex_char = hex(ord(c))[2:].upper()
-        items.append(hex_char)
-
-    return ''.join(items)
+    return hex_text
 
 
 if __name__ == '__main__':
     assert hex2str("504F53542068747470733A") == "POST https:"
     assert str2hex(hex2str("504F53542068747470733A")) == "504F53542068747470733A"
+    assert str2hex(hex2str("504F53542068747470733A"), upper=False) == "504f53542068747470733a"
     assert str2hex("POST https:") == "504F53542068747470733A"
     assert hex2str(str2hex("POST https:")) == "POST https:"
+    assert hex2str(str2hex("Привет мир!")) == "Привет мир!"
+    assert hex2str(str2hex("⌚⏰☀☁☔☺")) == "⌚⏰☀☁☔☺"
 
     hex_text = "504F53542068747470733A"
     text = hex2str(hex_text)
