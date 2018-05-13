c3f7ff47ee80870bf52618b1ebdb950c860dccea
ipetrash <ilya.petrash@inbox.ru>
2018-05-13 07:21:53 +0500

    #

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -5,4 +5,7 @@
 __author__ = 'ipetrash'
 
 
-
+# TODO: socket__tcp__examples + пример передачи картинки с клиента на сервер
+# TODO: socket__tcp__examples + пример передачи картинки с клиента на сервер + проверка crc32
+# TODO: socket__tcp__examples + пример передачи картинки с клиента на сервер и возврат tumbnails
+# TODO: socket__tcp__examples + пример передачи картинки с клиента на сервер и возврат tumbnails + проверка crc32
