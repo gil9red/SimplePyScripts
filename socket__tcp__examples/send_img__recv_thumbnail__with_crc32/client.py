b471addd7e6cb6ee992282578e228ad07f4d379e
ipetrash <ilya.petrash@inbox.ru>
2018-05-14 18:11:11 +0500

    Append socket__tcp__examples\send_img__recv_thumbnail__with_crc32

---
+++ socket__tcp__examples/send_img__recv_thumbnail__with_crc32/client.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://habrahabr.ru/post/149077/
+
+
+import socket
+
+import sys
+sys.path.append('..')
+
+from common import send_msg__with_crc32, recv_msg__with_crc32
+
+
+HOST, PORT = "localhost", 9090
+
+
+with socket.socket() as sock:
+    sock.connect((HOST, PORT))
+
+    # Send file
+    with open('img.jpg', 'rb') as f:
+        data = f.read()
+
+    print('Sending {} bytes'.format(len(data)))
+    print()
+
+    send_msg__with_crc32(sock, data)
+
+    print('Receiving')
+
+    response_data = recv_msg__with_crc32(sock)
+    print('Response {} bytes'.format(len(response_data)))
+
+    file_name = 'img_thumbnail.jpg'
+    print('Save in ' + file_name)
+
+    with open(file_name, 'wb') as f:
+        f.write(response_data)
+
+    print('Close\n')
---
+++ socket__tcp__examples/send_img__recv_thumbnail__with_crc32/server.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://habrahabr.ru/post/149077/
+
+
+from PIL import Image
+import io
+
+import socket
+
+import sys
+sys.path.append('..')
+
+from common import send_msg__with_crc32, recv_msg__with_crc32
+
+
+PORT = 9090
+
+
+with socket.socket() as sock:
+    sock.bind(('', 9090))
+    sock.listen(1)
+
+    print('Server: {}'.format(sock.getsockname()))
+
+    while True:
+        conn, addr = sock.accept()
+        print('Connected:', addr)
+
+        data = recv_msg__with_crc32(conn)
+        print('Receiving: {} bytes'.format(len(data), data))
+
+        img = Image.open(io.BytesIO(data))
+        print('Receiving image:', img)
+
+        print('Transform image in thumbnail')
+
+        # Transform in thumbnail
+        img.thumbnail((75, 75))
+
+        print('Img:', img)
+
+        # Write thumbnail in buffer
+        data_io = io.BytesIO()
+        img.save(data_io, 'jpeg')
+
+        response_data = data_io.getvalue()
+
+        print('Sending: {} bytes'.format(len(response_data)))
+
+        send_msg__with_crc32(conn, response_data)
+
+        print('Close\n')
