658efc91428c5bbc8e9db2be81b3adabb11269d8
ipetrash <ilya.petrash@inbox.ru>
2018-05-14 12:29:21 +0500

    Append socket__tcp__examples\send_img__recv_thumbnail

---
+++ socket__tcp__examples/send_img__recv_thumbnail/client.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://habrahabr.ru/post/149077/
+
+
+import socket
+
+import sys
+sys.path.append('..')
+
+from common import send_msg, recv_msg
+
+
+HOST, PORT = "localhost", 9090
+
+
+with socket.socket() as sock:
+    sock.connect((HOST, PORT))
+
+    # Send file
+    with open('img.jpg', 'rb') as f:
+        data = f.read()
+
+    print('Sending {} bytes'.format(len(data)))
+    print()
+
+    send_msg(sock, data)
+
+    print('Receiving')
+
+    response_data = recv_msg(sock)
+    print('Response {} bytes'.format(len(response_data)))
+
+    file_name = 'img_thumbnail.jpg'
+    print('Save in ' + file_name)
+
+    with open(file_name, 'wb') as f:
+        f.write(response_data)
+
+    print('Close\n')
