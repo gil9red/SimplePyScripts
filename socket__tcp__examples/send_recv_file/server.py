fac2c09bc080ff2b2025402ecf69a6443174ee05
ipetrash <ilya.petrash@inbox.ru>
2018-05-14 11:27:41 +0500

    Append socket__tcp__examples\send_recv_file

---
+++ socket__tcp__examples/send_recv_file/server.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://habrahabr.ru/post/149077/
+
+
+import socket
+
+import sys
+sys.path.append('..')
+
+from common import send_msg, recv_msg
+
+
+PORT = 9090
+
+
+with socket.socket() as sock:
+    sock.bind(('', 9090))
+    sock.listen(1)
+
+    print('Server: {}'.format(sock.getsockname()))
+
+    while True:
+        conn, addr = sock.accept()
+        print('Connected:', addr)
+
+        data = recv_msg(conn)
+        print('Receiving: {}: {}'.format(len(data), data))
+
+        text = 'Ok! Message size: {}'.format(len(data))
+        print('Sending: {}'.format(text))
+
+        rs = bytes(text, 'utf-8')
+        send_msg(conn, rs)
+
+        print('Close\n')
