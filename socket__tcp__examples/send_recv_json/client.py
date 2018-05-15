d884117f7d67cf046eb1f9e226cc8b82cd34a493
ipetrash <ilya.petrash@inbox.ru>
2018-05-15 08:19:39 +0500

    Append socket__tcp__examples\send_recv_json

---
+++ socket__tcp__examples/send_recv_json/client.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import socket
+import json
+
+import sys
+sys.path.append('..')
+
+from common import send_msg, recv_msg
+
+
+HOST, PORT = "localhost", 9090
+
+
+with socket.socket() as sock:
+    sock.connect((HOST, PORT))
+
+    data = {
+        'title': 'test',
+        'counter': 1,
+    }
+    data = json.dumps(data)
+    data = bytes(data, 'utf-8')
+    print('Sending ({}): {}'.format(len(data), data))
+    print()
+
+    send_msg(sock, data)
+
+    print('Receiving')
+
+    response_data = recv_msg(sock)
+    print('Response ({}): {}'.format(len(response_data), response_data))
+
+    print('Close\n')
---
+++ socket__tcp__examples/send_recv_json/server.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import socket
+import json
+
+import sys
+sys.path.append('..')
+
+from common import send_msg, recv_msg
+
+
+PORT = 9090
+
+
+with socket.socket() as sock:
+    sock.bind(('', 9090))
+    sock.listen(1)
+
+    print('Server: {}'.format(sock.getsockname()))
+
+    while True:
+        conn, addr = sock.accept()
+        print('Connected:', addr)
+
+        data = recv_msg(conn)
+        print('Receiving ({}): {}'.format(len(data), data))
+
+        json_data = json.loads(data, encoding='utf-8')
+        print('json_data:', json_data)
+
+        json_data['title'] = 'updates'
+        json_data['counter'] += 1
+
+        data = json.dumps(json_data)
+        print('Sending: {}'.format(data))
+
+        rs = bytes(data, 'utf-8')
+        send_msg(conn, rs)
+
+        print('Close\n')
