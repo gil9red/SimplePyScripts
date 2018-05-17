151c2957ebdde8f027b7d5b0294cf754394bf249
ipetrash <ilya.petrash@inbox.ru>
2018-05-17 19:36:57 +0500

    Append qt__pyqt__pyside__pyqode/table__cell__gif__setCellWidget

---
+++ qt__pyqt__pyside__pyqode/table__cell__gif__setCellWidget/main.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5 import Qt
+
+
+class Window(Qt.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        main_layout = Qt.QHBoxLayout()
+
+        self.table = Qt.QTableWidget()
+        self.table.setColumnCount(3)
+        self.table.setRowCount(3)
+
+        for i in range(self.table.rowCount()):
+            for j in range(self.table.columnCount()):
+                movie = Qt.QMovie("loading.gif")
+                item = Qt.QLabel()
+                item.setMovie(movie)
+                movie.start()
+
+                self.table.setCellWidget(i, j, item)
+
+        main_layout.addWidget(self.table)
+
+        self.setLayout(main_layout)
+
+
+if __name__ == '__main__':
+    app = Qt.QApplication([])
+
+    mw = Window()
+    mw.show()
+
+    app.exec()
