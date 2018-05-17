c3fba5e056312b35648117ad827da12d417c2bff
ipetrash <ilya.petrash@inbox.ru>
2018-05-17 16:27:28 +0500

    #

--- bigfishgames.com__hidden-object/find__Spirits of Mystery__of__Collector's Edition.py
+++ bigfishgames.com__hidden-object/find__Spirits of Mystery__of__Collector's Edition.py
@@ -10,5 +10,6 @@ POSTFIX = "Collector's Edition".upper()
 from get_all_games import get_all_games
 games = [game for game in get_all_games() if game.upper().startswith(PREFIX) and game.upper().endswith(POSTFIX)]
 print('Games ({}):'.format(len(games)))
+
 for game in games:
     print('    ' + game)
