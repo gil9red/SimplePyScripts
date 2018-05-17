f492320d33021c467a4e3e88a478dee4f70ca59e
ipetrash <ilya.petrash@inbox.ru>
2018-05-17 11:19:48 +0500

    Append excel__openpyxl__xlwt/load__or_create__append_new_sheets__xlsx.py

---
+++ excel__openpyxl__xlwt/load__or_create__append_new_sheets__xlsx.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import openpyxl
+
+
+columns = ['Name', 'Age', 'Course']
+rows = [
+    ['Vasya', '16', 1],
+    ['Anya', '17', 2],
+    ['Inna', '16', 1],
+]
+
+FILE_NAME = 'excel.xlsx'
+
+try:
+    wb = openpyxl.load_workbook(FILE_NAME)
+
+except:
+    wb = openpyxl.Workbook()
+
+    # Удаление листа, создаваемого по умолчанию, при создании документа
+    for sheet_name in wb.sheetnames:
+        sheet = wb.get_sheet_by_name(sheet_name)
+        wb.remove_sheet(sheet)
+
+# Создание нового листа, названия новых листов будут инкрементироваться: Students, Students1, Students2, и т.п.
+ws = wb.create_sheet('Students')
+
+for i, value in enumerate(columns, 1):
+    ws.cell(row=1, column=i).value = value
+
+for i, row in enumerate(rows, 2):
+    for j, value in enumerate(row, 1):
+        ws.cell(row=i, column=j).value = value
+
+from openpyxl.writer.excel import save_workbook
+save_workbook(wb, FILE_NAME)
