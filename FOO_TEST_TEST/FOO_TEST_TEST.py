44e0639ca0c1895fb8fbcc9d6349237bb33cdcdd
ipetrash <ilya.petrash@inbox.ru>
2018-05-18 15:57:07 +0500

    #

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -4,3 +4,29 @@
 __author__ = 'ipetrash'
 
 
+# # transliterate
+# # транслитерация
+#
+# # pip install transliterate
+# from transliterate import translit
+# text = "Тбилиси"
+# translit(text, 'en')
+# print(text)
+#
+#
+# # import transliterate
+# # name_input = 'Hello World Привет мир'
+# # try:
+# #     name = transliterate.translit(name_input, reversed=True)
+# # except transliterate.exceptions.LanguageDetectionError:
+# #     name = name_input
+# #
+# # print(name)
+#
+#
+# # print(translit(u"Тбилиси", 'ru', reversed=True))
+# # Tbilisi
+# #
+
+# TODO: excel__openpyxl__xlwt: set color cell
+# TODO: excel__openpyxl__xlwt: cell as pixel / draw image in sheet / cell is small size (1x1)
