c771788f8d860e1c3379643df282b3437f7747b8
ipetrash <ilya.petrash@inbox.ru>
2018-05-20 22:17:34 +0500

    Append excel__openpyxl__xlwt/random_fill_color.py

---
+++ excel__openpyxl__xlwt/random_fill_color.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import random
+
+import openpyxl
+from openpyxl.styles import PatternFill
+
+
+# SOURCE: https://github.com/gil9red/SimplePyScripts/blob/8b69f96d0bd87c10f3d0779ff46b02558c4cca84/excel__openpyxl__xlwt/xlsx__set_row_column_size.py
+def set_row_column_size(ws):
+    # SOURCE: excel__openpyxl__xlwt\excel\xl\worksheets\sheet1.xml
+    # <sheetFormatPr defaultColWidth="1.77734375" defaultRowHeight="6.6" customHeight="1" x14ac:dyDescent="0.3"/>
+    ws.sheet_format.defaultColWidth = 1.77734375
+    ws.sheet_format.defaultRowHeight = 9.0
+    ws.sheet_format.customHeight = 1
+
+
+# SOURCE: https://github.com/gil9red/SimplePyScripts/blob/1c2274804f52004b07601ece535ec74b3f82aa8d/get_random_hex_color.py
+def get_random_hex_color():
+    return ''.join(random.choices('0123456789ABCDEF', k=6))
+
+
+def set_fill_color(ws):
+    size = 250
+
+    for i in range(1, size):
+        for j in range(1, size):
+            cell = ws.cell(row=i, column=j)
+            cell.fill = PatternFill(fgColor=get_random_hex_color(), fill_type="solid")
+
+
+wb = openpyxl.Workbook()
+ws = wb.get_active_sheet()
+
+# Масштаб 10%
+ws.sheet_view.zoomScale = 10
+
+set_row_column_size(ws)
+set_fill_color(ws)
+
+from openpyxl.writer.excel import save_workbook
+save_workbook(wb, 'excel.xlsx')
