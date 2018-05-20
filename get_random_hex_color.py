1c2274804f52004b07601ece535ec74b3f82aa8d
ipetrash <ilya.petrash@inbox.ru>
2018-05-20 21:31:20 +0500

    Append get_random_hex_color.py

---
+++ get_random_hex_color.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import random
+
+
+def get_random_hex_color():
+    return ''.join(random.choices('0123456789ABCDEF', k=6))
+
+
+if __name__ == '__main__':
+    print(get_random_hex_color())
