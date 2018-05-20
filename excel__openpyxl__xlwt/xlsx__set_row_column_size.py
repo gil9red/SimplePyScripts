832b722f764910ed092a03e0d5f126c6783846a9
ipetrash <ilya.petrash@inbox.ru>
2018-05-20 21:05:00 +0500

    Append excel__openpyxl__xlwt/xlsx__set_row_column_size.py

---
+++ excel__openpyxl__xlwt/xlsx__set_row_column_size.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import openpyxl
+from openpyxl.utils.cell import _get_column_letter
+
+
+def set_row_column_size(worksheet):
+    for i in range(1, 500):
+        row = worksheet.row_dimensions[i]
+        row.height = 15
+
+        for j in range(1, 500):
+            j = _get_column_letter(j)
+
+            col = worksheet.column_dimensions[j]
+            col.width = 3.5
+
+
+wb = openpyxl.Workbook()
+ws = wb.get_active_sheet()
+
+# Sheet 2
+ws = wb.create_sheet()
+set_row_column_size(ws)
+
+from openpyxl.writer.excel import save_workbook
+save_workbook(wb, 'excel.xlsx')
