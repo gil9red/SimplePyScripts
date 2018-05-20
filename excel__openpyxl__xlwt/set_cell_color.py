09a34b6ed6ec40338835ea8ffc66e53d5c45ff01
ipetrash <ilya.petrash@inbox.ru>
2018-05-20 22:17:09 +0500

    Append excel__openpyxl__xlwt/set_cell_color.py

---
+++ excel__openpyxl__xlwt/set_cell_color.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import openpyxl
+from openpyxl.styles import PatternFill
+
+
+wb = openpyxl.Workbook()
+ws = wb.get_active_sheet()
+
+ws.cell(row=1, column=1).fill = PatternFill(fgColor='FF0000', fill_type="solid")
+ws.cell(row=2, column=1).fill = PatternFill(fgColor='00FF00', fill_type="solid")
+ws['A3'].fill = PatternFill(fgColor='0000FF', fill_type="solid")
+ws['A3:A6'].fill = PatternFill(fgColor='0000FF', fill_type="solid")
+
+from openpyxl.writer.excel import save_workbook
+save_workbook(wb, 'excel.xlsx')
