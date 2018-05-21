805680d8870e6d741c5ed8ff194b2b56eb150990
ipetrash <ilya.petrash@inbox.ru>
2018-05-21 11:04:24 +0500

    Append pil_pillow__examples\get_image_size

---
+++ pil_pillow__examples/get_image_size/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install Pillow
+from PIL import Image
+
+img = Image.open("input.jpg")
+print(img)
+print(img.size)
+# img.show()
