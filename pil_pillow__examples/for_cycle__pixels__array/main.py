c1be34a23f40e8f23cf26d91e2964a6425c0b5db
ipetrash <ilya.petrash@inbox.ru>
2018-05-21 12:08:18 +0500

    Append pil_pillow__examples\for_cycle__pixels__array

---
+++ pil_pillow__examples/for_cycle__pixels__array/main.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install Pillow
+from PIL import Image
+
+
+def rgb2hex(r: int, g: int, b: int) -> str:
+    return '{:02X}{:02X}{:02X}'.format(r, g, b)
+
+
+def get_pixel_array(img, rgb_hex=False):
+    width, height = img.size
+
+    pixels = []
+
+    for y in range(height):
+        row = []
+        pixels.append(row)
+
+        for x in range(width):
+            r, g, b = img.getpixel((x, y))
+
+            if rgb_hex:
+                row.append(rgb2hex(r, g, b))
+            else:
+                row.append((r, g, b))
+
+    return pixels
+
+
+if __name__ == '__main__':
+    img = Image.open("input.jpg")
+    print(img)
+    print()
+
+    pixels = get_pixel_array(img)
+    print('Rows: {}, cols: {}'.format(len(pixels), len(pixels[0])))
+    print([pixels[0][i] for i in range(2)])  # [(7, 7, 7), (23, 23, 23)]
+    print()
+
+    pixels = get_pixel_array(img, rgb_hex=True)
+    print('Rows: {}, cols: {}'.format(len(pixels), len(pixels[0])))
+    print([pixels[0][i] for i in range(2)])  # ['070707', '171717']
