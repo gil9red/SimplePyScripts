75889eedd8baff8e5c6271365615a89c2a4c76af
ipetrash <ilya.petrash@inbox.ru>
2018-05-22 09:30:29 +0500

    Append excel__openpyxl__xlwt/hidden_columns.py

---
+++ excel__openpyxl__xlwt/hidden_columns.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: http://openpyxl.readthedocs.io/en/stable/usage.html#fold-columns-outline
+
+
+import openpyxl
+
+
+wb = openpyxl.Workbook()
+
+ws = wb.get_active_sheet()
+ws.column_dimensions.group('B', 'D', hidden=True)
+
+wb.save('excel.xlsx')
