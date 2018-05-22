11892562cba6b0c6b5c2850f7cbf8586c4838cef
ipetrash <ilya.petrash@inbox.ru>
2018-05-22 09:22:29 +0500

    Append excel__openpyxl__xlwt\insert_image

---
+++ excel__openpyxl__xlwt/insert_image/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: http://openpyxl.readthedocs.io/en/stable/usage.html#inserting-an-image
+
+
+import openpyxl
+from openpyxl.drawing.image import Image
+
+
+wb = openpyxl.Workbook()
+ws = wb.get_active_sheet()
+
+ws['A1'] = 'You should see three logos below'
+
+# Create an image
+img = Image('logo.png')
+
+# Add to worksheet and anchor next to cells
+ws.add_image(img, 'A2')
+
+wb.save('excel.xlsx')
