0642c5f3a5010a844c305990c84a4bf898c215f0
ipetrash <ilya.petrash@inbox.ru>
2018-05-22 21:26:59 +0500

    #

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -4,27 +4,4 @@
 __author__ = 'ipetrash'
 
 
-# # transliterate
-# # транслитерация
-#
-# # pip install transliterate
-# from transliterate import translit
-# text = "Тбилиси"
-# translit(text, 'en')
-# print(text)
-#
-#
-# # import transliterate
-# # name_input = 'Hello World Привет мир'
-# # try:
-# #     name = transliterate.translit(name_input, reversed=True)
-# # except transliterate.exceptions.LanguageDetectionError:
-# #     name = name_input
-# #
-# # print(name)
-#
-#
-# # print(translit(u"Тбилиси", 'ru', reversed=True))
-# # Tbilisi
-# #
 
