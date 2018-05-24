456a44f8fd7a61550d4a3e3fcc68003905969f17
ipetrash <ilya.petrash@inbox.ru>
2018-05-24 18:53:30 +0500

    #

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -4,4 +4,27 @@
 __author__ = 'ipetrash'
 
 
+#  pip install Py-StackExchange
+import stackexchange
+from stackexchange.sites import __SEAPI
 
+so = stackexchange.Site(__SEAPI('ru.stackoverflow.com'), impose_throttling=True)
+
+from collections import OrderedDict
+tag_by_wiki = OrderedDict()
+
+page = 1
+
+while True:
+    for tag in so.all_tags(page=page):
+        wiki = tag.wiki.fetch()
+        print(tag.name, repr(wiki.excerpt))
+
+    page += 1
+    quit()
+
+# print(so.all_tags()[0].name)
+# print(so.all_tags(page=1)[0].name)
+# print(so.all_tags(page=20000))
+# quit()
+# print(so.all_tags(page=20000)[0].name)
