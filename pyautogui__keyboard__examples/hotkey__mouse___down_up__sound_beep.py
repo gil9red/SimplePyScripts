37a2e671d9ade90bb358ca5a84f6782b0f6af2a1
ipetrash <ilya.petrash@inbox.ru>
2018-05-25 03:11:39 +0500

    Append pyautogui__keyboard__examples/hotkey__mouse___down_up__sound_beep.py

---
+++ pyautogui__keyboard__examples/hotkey__mouse___down_up__sound_beep.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import pyautogui
+import winsound
+
+# pip install keyboard
+import keyboard
+keyboard.add_hotkey('Ctrl + 1', lambda: winsound.Beep(1000, duration=50) and pyautogui.mouseDown())
+keyboard.add_hotkey('Ctrl + 2', lambda: winsound.Beep(1000, duration=50) and pyautogui.mouseUp())
+
+keyboard.wait('Ctrl + Q')
+
