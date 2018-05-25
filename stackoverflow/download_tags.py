263d23bc4a53cb1e0ea2e6a7f03780191b47be33
ipetrash <ilya.petrash@inbox.ru>
2018-05-25 23:53:43 +0500

    #

--- stackoverflow/download_tags.py
+++ stackoverflow/download_tags.py
@@ -16,13 +16,14 @@ import time
 import itertools
 
 
-def get_all_tags(need_pages=None, on_exception_stop=False, repeat_request_tag_on_error=True) -> dict:
+def get_all_tags(need_pages=None, need_tags=None, on_exception_stop=False, repeat_request_tag_on_error=True) -> dict:
     """
     Функция парсит страницу тегов/меток и возвращает их.
 
     :param need_pages: Т.к. теги будут в порядке убывания популярности, то теги на первых десятках страниц будут
     гарантировано заполнены, из-за их популярности, остальные может не иметь смысла скачивать
     Пример получения первых 20 страниц: NEED_PAGES = 20
+    :param need_tags: ограничение на количество тегов, что нужно загрузить
     :param on_exception_stop: если True и при парсинге возникнет исключение, парсер будет остановлен
     :param repeat_request_tag_on_error: если True, запрос парсинга тега будет повторяться пока не будет удачен
     :return: dict
@@ -58,6 +59,9 @@ def get_all_tags(need_pages=None, on_exception_stop=False, repeat_request_tag_on
                                 'description': tag_text.text.strip(),
                             }
 
+                            if need_tags and len(tags) == need_tags:
+                                return tags
+
                         # Нам не нужно ДОСить сайт
                         time.sleep(2)
                         break
@@ -87,6 +91,11 @@ def get_all_tags(need_pages=None, on_exception_stop=False, repeat_request_tag_on
 if __name__ == '__main__':
     # # Parse all pages:
     # tags = get_all_tags()
+
+    # # Parse 2 tags
+    # tags = get_all_tags(need_tags=2)
+    # print(tags)
+
     tags = get_all_tags(need_pages=20)
 
     import json
