0ca27749684fccce1edbfe7ee1ae6c04749e0de3
ipetrash <ilya.petrash@inbox.ru>
2018-05-28 22:58:35 +0500

    Append tkinter_examples/table__label__on_click_cell_handler.py

---
+++ tkinter_examples/table__label__on_click_cell_handler.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from tkinter import Tk, Label
+
+
+def turn(event):
+    value = event.widget["text"]
+    value = 1 if value == '.' else int(value) + 1
+
+    event.widget["text"] = value
+
+
+root = Tk()
+
+for x in range(8):
+    for y in range(8):
+        cell = Label(root, width=3, height=1, text=".")
+        cell.grid(row=x, column=y)
+        cell.bind("<Button-1>", turn)
+
+root.mainloop()
