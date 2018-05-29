a14ce5d2fe489da4da911125acbe7e9903805128
ipetrash <ilya.petrash@inbox.ru>
2018-05-29 15:13:43 +0500

    Append tkinter_examples/excepthook__report_callback_exception__log_uncaught_exceptions.py

---
+++ tkinter_examples/excepthook__report_callback_exception__log_uncaught_exceptions.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from tkinter import Tk, messagebox
+import traceback
+
+
+def log_uncaught_exceptions(ex_cls, ex, tb):
+    text = '{}: {}:\n'.format(ex_cls.__name__, ex)
+    text += ''.join(traceback.format_tb(tb))
+    print(text)
+
+    # Если Yes, сохраняем ошибку в файл
+    if messagebox.askyesno("Неизвестная ошибка", "Сохранить лог с ошибкой?"):
+        with open('error.txt', 'w', encoding='utf-8') as f:
+            f.write(text)
+
+    quit()
+
+
+import sys
+sys.excepthook = log_uncaught_exceptions
+
+root = Tk()
+root.report_callback_exception = log_uncaught_exceptions
+
+
+def m_geometry(win):
+    # NOTE: this error -> root.report_callback_exception
+    # x = (win.winfo_screenwidth() / 2) - (295 / 2)
+    y = (win.winfo_screenheight() / 2) - (395 / 2)
+    root.wm_geometry("+%d+%d" % (x, y))
+
+# NOTE: this error -> sys.excepthook
+m_geometry(None)
+
+# Функция m_geometry(root) сработает через 2 секунды
+root.after(2000, lambda: m_geometry(root))
+
+root.mainloop()
