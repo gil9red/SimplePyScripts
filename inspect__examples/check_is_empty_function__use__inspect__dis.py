76b825bd1dfc75378e59b1bcc2acc6047dcd48f3
ipetrash <ilya.petrash@inbox.ru>
2018-05-29 13:33:25 +0500

    Move to inspect__examples

---
+++ inspect__examples/check_is_empty_function__use__inspect__dis.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from dis__bytecode__is_empty_function import check_is_empty_function as check_is_empty_function__use_dis
+from inspect__get_source__function import check_is_empty_function as check_is_empty_function__use_inspect
+
+
+def foo():
+    pass
+
+
+def foo2():
+    return 1
+
+
+print('check_is_empty_function__use_inspect:', check_is_empty_function__use_inspect(foo))
+print('check_is_empty_function__use_inspect:', check_is_empty_function__use_inspect(foo2))
+print()
+
+print('check_is_empty_function__use_dis:', check_is_empty_function__use_dis(foo))
+print('check_is_empty_function__use_dis:', check_is_empty_function__use_dis(foo2))
