c7a3e2470b8df4ba49e9be1f73915681ccb4bc87
ipetrash <ilya.petrash@inbox.ru>
2018-05-29 13:09:49 +0500

    Rename: get_script_dir.py -> get_current_script_dir.py

--- get_current_script_dir.py
+++ get_current_script_dir.py
@@ -10,18 +10,24 @@ import os
 import sys
 
 
-def get_script_dir(follow_symlinks=True) -> str:
+def get_current_script_dir(follow_symlinks=True, normcase=False) -> str:
     # py2exe, PyInstaller, cx_Freeze
     if getattr(sys, 'frozen', False):
         path = os.path.abspath(sys.executable)
     else:
-        path = inspect.getabsfile(get_script_dir)
+        # NOTE: analog inspect.getabsfile without os.path.normcase
+        path = inspect.getframeinfo(inspect.currentframe()).filename
+        path = os.path.abspath(path)
 
     if follow_symlinks:
         path = os.path.realpath(path)
+
+    if normcase:
+        path = os.path.normcase(path)
 
     return os.path.dirname(path)
 
 
 if __name__ == '__main__':
-    print(get_script_dir())
+    print(get_current_script_dir())
+    print(get_current_script_dir(normcase=True))
