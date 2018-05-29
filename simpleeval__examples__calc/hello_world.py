38765c6ef304fb5da233b8d44a9294b52b86343d
ipetrash <ilya.petrash@inbox.ru>
2018-05-29 17:28:32 +0500

    #

--- simpleeval__examples__calc/hello_world.py
+++ simpleeval__examples__calc/hello_world.py
@@ -12,4 +12,4 @@ print(simple_eval("21 + 21"))  # 42
 print(simple_eval("2 + 2 * 2"))  # 6
 print(simple_eval('10 ** 123'))  # 1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
 print(simple_eval("21 + 19 / 7 + (8 % 3) ** 9"))  # 535.7142857142857
-print(simple_eval("square(11)", functions={"square": lambda x: x*x}))  # 121
+print(simple_eval("square(11)", functions={"square": lambda x: x * x}))  # 121
