5c6b923fecb4c2ea218d63c10e6af250a7a2815c
ipetrash <ilya.petrash@inbox.ru>
2018-05-30 14:46:37 +0500

    Append simpleeval__examples__calc/operators__user_custom.py

---
+++ simpleeval__examples__calc/operators__user_custom.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# TODO: https://github.com/danthedeckie/simpleeval#operators
+# pip install simpleeval
+from simpleeval import SimpleEval
+
+import ast
+import operator
+
+s = SimpleEval()
+s.operators[ast.BitXor] = operator.xor
+# # OR:
+# s.operators[ast.BitXor] = lambda a, b: a ^ b
+
+print(s.eval("2 ^ 10"))
