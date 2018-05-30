6b4d6ed41123c2da762cda9f95c6a6a0e8c5b2ed
ipetrash <ilya.petrash@inbox.ru>
2018-05-30 15:07:13 +0500

    Append simpleeval__examples__calc/operators__user_custom__only__add_and_sub.py

---
+++ simpleeval__examples__calc/operators__user_custom__only__add_and_sub.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://github.com/danthedeckie/simpleeval#operators
+
+
+# pip install simpleeval
+from simpleeval import simple_eval, SimpleEval
+
+import ast
+import operator
+
+
+SUPPORTED_OPERATORS = {
+    ast.Add: operator.add,
+    ast.Sub: operator.sub,
+}
+
+
+print(simple_eval("2 + 2 - 1", operators=SUPPORTED_OPERATORS))  # 3
+
+try:
+    print(simple_eval("2 + 2 * 2", operators=SUPPORTED_OPERATORS))  # KeyError: <class '_ast.Mult'>
+except Exception as e:
+    print(repr(e))
+
+print()
+
+s = SimpleEval(operators=SUPPORTED_OPERATORS)
+# # OR:
+# s = SimpleEval()
+# s.operators = SUPPORTED_OPERATORS
+
+print(s.eval("2 + 2 - 1"))  # 3
+
+try:
+    print(s.eval("2 + 2 * 2"))  # KeyError: <class '_ast.Mult'>
+except Exception as e:
+    print(repr(e))
