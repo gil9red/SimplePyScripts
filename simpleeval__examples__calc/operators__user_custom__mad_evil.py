517f2ac352ffcb6f965723737daf9bc8c0e31a6d
ipetrash <ilya.petrash@inbox.ru>
2018-05-30 15:12:33 +0500

    #

--- simpleeval__examples__calc/operators__user_custom__mad_evil.py
+++ simpleeval__examples__calc/operators__user_custom__mad_evil.py
@@ -11,6 +11,7 @@ __author__ = 'ipetrash'
 from simpleeval import SimpleEval
 
 import ast
+import operator
 
 
 expr = "((2 + 2 * 2) / 3) ** 10 - 24"
@@ -19,9 +20,9 @@ s = SimpleEval()
 print(s.eval(expr))  # 1000.0
 
 # Mad
-s.operators[ast.Add] = lambda a, b: a * b
-s.operators[ast.Sub] = lambda a, b: a + b
-s.operators[ast.Mult] = lambda a, b: a ** b
-s.operators[ast.Pow] = lambda a, b: a - b
+s.operators[ast.Add] = operator.mul
+s.operators[ast.Sub] = operator.add
+s.operators[ast.Mult] = operator.pow
+s.operators[ast.Pow] = operator.sub
 
 print(s.eval(expr))  # 16.666666666666664
