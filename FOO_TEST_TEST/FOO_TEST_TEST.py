8ab560f151610fa62fa926337d89dc4e19385d41
ipetrash <ilya.petrash@inbox.ru>
2018-05-31 20:49:27 +0500

    #

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -5,3 +5,7 @@ __author__ = 'ipetrash'
 
 
 # TODO: append humanize example: https://github.com/jmoiron/humanize
+# TODO: qrcode reader: from file, from url
+#       append qrcode__reader
+#
+# TODO: rename generate_qrcode -> qrcode__generate
