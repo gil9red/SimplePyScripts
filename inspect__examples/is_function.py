6997d69302d4c068cc785cb623faee5c0c00ffca
ipetrash <ilya.petrash@inbox.ru>
2018-05-31 19:40:45 +0500

    #

--- inspect__examples/is_function.py
+++ inspect__examples/is_function.py
@@ -15,15 +15,16 @@ class Foo:
 
 import inspect
 print(inspect.isroutine(lambda: 1))  # True
-print(inspect.isroutine(foo))  # True
+print(inspect.isroutine(foo))        # True
 print()
 
 # Functor
 foo = Foo()
-print(inspect.isroutine(foo))  # False
+print(inspect.isroutine(foo))    # False
 print(hasattr(foo, '__call__'))  # True
+print(callable(foo))             # True
 print()
 
 import math
 print(inspect.isroutine(math.sin))  # True
-print(inspect.isroutine(print))  # True
+print(inspect.isroutine(print))     # True
