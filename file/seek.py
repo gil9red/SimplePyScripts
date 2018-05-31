4af2b4c4d0a51bacbc33eeca494799af4bf0e8cb
ipetrash <ilya.petrash@inbox.ru>
2018-05-31 13:06:46 +0500

    Append file/seek.py

---
+++ file/seek.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+with open('input__seek.txt') as f:
+    for i in range(3):
+        print('{}.'.format(i))
+
+        for line in f:
+            print(line.strip(), end=' ')
+
+        print('\n')
+        f.seek(0)
