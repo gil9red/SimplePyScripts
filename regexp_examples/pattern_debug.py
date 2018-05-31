9a4e16d6cf620f0328f8b1fc5cfad20bca61a4de
ipetrash <ilya.petrash@inbox.ru>
2018-05-31 16:33:21 +0500

    Append regexp_examples/pattern_debug.py

---
+++ regexp_examples/pattern_debug.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import re
+
+
+pattern = re.compile("[a-z]+|[0-9]+", re.DEBUG)
+# BRANCH
+#   MAX_REPEAT 1 MAXREPEAT
+#     IN
+#       RANGE (97, 122)
+# OR
+#   MAX_REPEAT 1 MAXREPEAT
+#     IN
+#       RANGE (48, 57)
+print()
+print(type(pattern), pattern)
+
+print('\n')
+
+pattern = re.compile("\d{2,6}", re.DEBUG)
+# MAX_REPEAT 2 6
+#   IN
+#     CATEGORY CATEGORY_DIGI
+print()
+print(type(pattern), pattern)
