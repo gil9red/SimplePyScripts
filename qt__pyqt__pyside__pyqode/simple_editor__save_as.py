44c1cea3013900a01333658520e634c3334f25f0
ipetrash <ilya.petrash@inbox.ru>
2018-06-01 18:42:30 +0500

    Append qt__pyqt__pyside__pyqode/simple_editor__save_as.py

---
+++ qt__pyqt__pyside__pyqode/simple_editor__save_as.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5 import Qt
+import sys
+
+
+class Example(Qt.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('Hi')
+
+        self.text_edit = Qt.QTextEdit()
+        self.setCentralWidget(self.text_edit)
+
+        menu = self.menuBar().addMenu('File')
+        save_file_action = menu.addAction('Save As ...')
+        save_file_action.triggered.connect(self.save_as)
+
+    def save_as(self):
+        file_name, ok = Qt.QFileDialog.getSaveFileName(self)
+        if not ok:
+            return
+
+        with open(file_name, 'w', encoding='utf-8') as f:
+            text = self.text_edit.toPlainText()
+            f.write(text)
+
+
+if __name__ == '__main__':
+    app = Qt.QApplication(sys.argv)
+
+    example = Example()
+    example.setGeometry(300, 300, 300, 300)
+    example.show()
+
+    app.exec()
