04bedad3b544525ad0cdcfe5faa19d397398e3f4
ipetrash <ilya.petrash@inbox.ru>
2018-06-01 13:40:03 +0500

    #

--- zip_file_example/append_directory/main.py
+++ zip_file_example/append_directory/main.py
@@ -7,13 +7,12 @@ __author__ = 'ipetrash'
 DIR_NAME = 'dir_1'
 
 
-if __name__ == '__main__':
-    import zipfile
-    with zipfile.ZipFile(DIR_NAME + '.zip', mode='w', compression=zipfile.ZIP_DEFLATED) as zf:
-        import os
-        for root, dirs, files in os.walk(DIR_NAME):
-            for file in files:
-                file_name = os.path.join(root, file)
-                zf.write(file_name)
+import zipfile
+with zipfile.ZipFile(DIR_NAME + '.zip', mode='w', compression=zipfile.ZIP_DEFLATED) as zf:
+    import os
+    for root, dirs, files in os.walk(DIR_NAME):
+        for file in files:
+            file_name = os.path.join(root, file)
+            zf.write(file_name)
 
-                print(file_name)
+            print(file_name)
