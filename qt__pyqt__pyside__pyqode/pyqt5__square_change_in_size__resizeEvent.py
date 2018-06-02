2ed92750d1f4403f92616d6f4b64e4e9742b5bd9
ipetrash <ilya.petrash@inbox.ru>
2018-06-02 17:06:06 +0500

    Append qt__pyqt__pyside__pyqode/pyqt5__square_change_in_size__resizeEvent.py

---
+++ qt__pyqt__pyside__pyqode/pyqt5__square_change_in_size__resizeEvent.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5 import Qt
+
+
+class Widget(Qt.QWidget):
+    def resizeEvent(self, event: Qt.QResizeEvent):
+        width, height = event.size().width(), event.size().height()
+        width = height = max(width, height)
+        self.resize(width, height)
+
+
+if __name__ == '__main__':
+    app = Qt.QApplication([])
+
+    w = Widget()
+    w.show()
+
+    app.exec()
