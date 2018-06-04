20213d0114c5360be2b2b85f028f1363c27efa73
ipetrash <ilya.petrash@inbox.ru>
2018-06-04 15:14:23 +0500

    Append simpleeval__examples__calc/extending__EvalNoMethods__disable_object_methods.py

--- simpleeval__examples__calc/extending__EvalNoMethods.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-__author__ = 'ipetrash'
-
-
-# TODO: https://github.com/danthedeckie/simpleeval/blob/master/README.rst#extending
---
+++ simpleeval__examples__calc/extending__EvalNoMethods__disable_object_methods.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://github.com/danthedeckie/simpleeval/blob/master/README.rst#extending
+
+
+import ast
+import simpleeval
+
+
+class EvalNoMethods(simpleeval.SimpleEval):
+    def _eval_call(self, node):
+        if isinstance(node.func, ast.Attribute):
+            raise simpleeval.FeatureNotAvailable("No methods please, we're British")
+
+        return super(EvalNoMethods, self)._eval_call(node)
+
+
+if __name__ == '__main__':
+    class Foo:
+        @classmethod
+        def get(cls):
+            return 1
+
+    my_eval = simpleeval.SimpleEval()
+    my_eval.names['foo'] = Foo
+
+    print(my_eval.eval('"hello".upper()'))  # HELLO
+    print(my_eval.eval('foo.get()'))        # 1
+    print()
+
+    # Disable object methods
+    my_eval_no_methods = EvalNoMethods()
+    my_eval_no_methods.names['foo'] = Foo
+
+    try:
+        print(my_eval_no_methods.eval('"foo".upper()'))
+    except Exception as e:
+        print(e)  # No methods please, we're British
+
+    try:
+        print(my_eval_no_methods.eval('foo.get()'))
+    except Exception as e:
+        print(e)  # No methods please, we're British
