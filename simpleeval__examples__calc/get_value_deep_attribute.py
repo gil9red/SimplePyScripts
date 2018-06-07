38056d9973f11a3ec0ac5574ff9e337393a1d4da
ipetrash <ilya.petrash@inbox.ru>
2018-06-07 17:43:48 +0500

    Append simpleeval__examples__calc/get_value_deep_attribute.py

---
+++ simpleeval__examples__calc/get_value_deep_attribute.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install simpleeval
+import simpleeval
+
+
+class Foo:
+    class A:
+        class B:
+            class C:
+                value = 3
+
+        value = 25
+
+
+my_eval = simpleeval.SimpleEval()
+my_eval.names['foo'] = Foo
+
+print(my_eval.eval('foo.A.B.C.value * 10 + foo.A.value'))  # 55
