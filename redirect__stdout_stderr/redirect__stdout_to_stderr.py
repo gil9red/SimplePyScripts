489d5109113d572c1b542db757ddf9e2317569f4
ipetrash <ilya.petrash@inbox.ru>
2018-06-07 11:14:02 +0500

    Append redirect__stdout_stderr/redirect__stdout_to_stderr.py

---
+++ redirect__stdout_stderr/redirect__stdout_to_stderr.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import sys
+from contextlib import redirect_stdout
+
+
+# How to send help() to stderr
+with redirect_stdout(sys.stderr):
+    help(dir)
