20f4226955ea008012cc27d7355495a4a2ca05a1
ipetrash <ilya.petrash@inbox.ru>
2018-06-07 12:35:47 +0500

    Append human_byte_size__using_humanize.py

---
+++ human_byte_size__using_humanize.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install humanize
+import humanize
+
+
+print(humanize.naturalsize(25000000000, binary=True))                   # '23.3 GiB'
+print(humanize.naturalsize(25000000000, binary=True).replace('i', ''))  # '23.3 GB',   Strange, misleading
+print()
+
+import shutil
+usage = shutil.disk_usage('C://')
+print('total: {:>8} ({} bytes)'.format(humanize.naturalsize(usage.total, binary=True), usage.total))
+print('used:  {:>8} ({} bytes)'.format(humanize.naturalsize(usage.used, binary=True), usage.used))
+print('free:  {:>8} ({} bytes)'.format(humanize.naturalsize(usage.free, binary=True), usage.free))
