4f7ed75bf3d9b2c4f4ffa04aceac2bb2a3f44ecc
ipetrash <ilya.petrash@inbox.ru>
2018-06-08 18:24:48 +0500

    Append qt__pyqt__pyside__pyqode/QWidget_with_QMenuBar.py

---
+++ qt__pyqt__pyside__pyqode/QWidget_with_QMenuBar.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5 import Qt
+
+
+class MainWindow(Qt.QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        menu_bar = Qt.QMenuBar()
+        menu_file = menu_bar.addMenu('File')
+        action_exit = menu_file.addAction('Exit')
+        action_exit.triggered.connect(self.close)
+
+        layout = Qt.QVBoxLayout()
+        layout.addWidget(Qt.QLabel('Test'))
+        layout.addWidget(Qt.QPushButton('Click!'))
+
+        layout.setMenuBar(menu_bar)
+
+        self.setLayout(layout)
+
+
+if __name__ == '__main__':
+    app = Qt.QApplication([])
+
+    mw = MainWindow()
+    mw.show()
+
+    app.exec()
