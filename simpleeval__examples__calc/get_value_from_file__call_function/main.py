0b77cd5bb75980c42d44f7c507a3cb860adfa17b
ipetrash <ilya.petrash@inbox.ru>
2018-06-08 14:00:20 +0500

    Append simpleeval__examples__calc\get_value_from_file__call_function

---
+++ simpleeval__examples__calc/get_value_from_file__call_function/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install simpleeval
+from simpleeval import SimpleEval
+
+
+def get_from_file(file_name):
+    with open(file_name) as f:
+        return f.read()
+
+
+my_eval = SimpleEval()
+my_eval.functions['get'] = get_from_file
+
+print(my_eval.eval("get('a_value.txt') + get('b_value.txt')"))            # '12345'
+print(my_eval.eval("int(get('a_value.txt')) + int(get('b_value.txt'))"))  # 168
