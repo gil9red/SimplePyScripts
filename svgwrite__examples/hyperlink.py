cb84470a3cc4c20942c902c3e7597356a410ffe1
ipetrash <ilya.petrash@inbox.ru>
2018-06-09 11:53:34 +0500

    Append svgwrite__examples/hyperlink.py

---
+++ svgwrite__examples/hyperlink.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://github.com/mozman/svgwrite/blob/master/examples/hyperlink.py
+
+
+# pip install svgwrite
+import svgwrite
+
+
+def hyperlink(name):
+    dwg = svgwrite.Drawing(name, (200, 200), debug=True)
+
+    # use the hyperlink element
+    link = dwg.add(dwg.a('http://www.w3.org'))
+    link.add(dwg.ellipse(center=(100, 50), r=(50, 25), fill='red'))
+
+    dwg.save(pretty=True)
+
+
+if __name__ == '__main__':
+    hyperlink("hyperlink.svg")
