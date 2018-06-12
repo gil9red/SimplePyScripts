b67bf3160d4b09b20fb37d003f974c338937d257
ipetrash <ilya.petrash@inbox.ru>
2018-06-12 16:49:32 +0500

    Append pyautogui__keyboard__examples/input_and_delete__typewrite_press_keyDown_keyUp.py

---
+++ pyautogui__keyboard__examples/input_and_delete__typewrite_press_keyDown_keyUp.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: http://pyautogui.readthedocs.io/en/latest/keyboard.html#the-press-keydown-and-keyup-functions
+
+
+# pip install pyautogui
+import pyautogui
+
+
+TEXT = 'Hello World!'
+
+
+# Input text
+pyautogui.typewrite(TEXT)
+
+# Select text
+pyautogui.keyDown('shift')   # Hold down the shift key
+
+for _ in TEXT:
+    pyautogui.press('left')  # Press the left arrow key
+
+pyautogui.keyUp('shift')     # Release the shift key
+
+pyautogui.press('delete')
