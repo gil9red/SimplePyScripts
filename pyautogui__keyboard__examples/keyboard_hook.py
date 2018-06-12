ac96556b6978ca28f04e7349c044fa94cb36096a
ipetrash <ilya.petrash@inbox.ru>
2018-06-12 16:37:40 +0500

    Append pyautogui__keyboard__examples/keyboard_hook.py

---
+++ pyautogui__keyboard__examples/keyboard_hook.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install keyboard
+import keyboard
+
+
+def print_pressed_keys(e):
+    print(e, e.event_type, e.name)
+
+
+keyboard.hook(print_pressed_keys)
+keyboard.wait()
