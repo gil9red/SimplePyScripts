c92db1a456eb4d6a3a7d8e9de8c6ebaa623eb14c
ipetrash <ilya.petrash@inbox.ru>
2018-06-12 16:55:41 +0500

    Append pyautogui__keyboard__examples/pyautogui_typewrite__hello_world.py

---
+++ pyautogui__keyboard__examples/pyautogui_typewrite__hello_world.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install pyautogui
+import pyautogui
+
+# Prints out "Hello world!" instantly
+pyautogui.typewrite('Hello world!')
+
+pyautogui.press('enter')
+
+# Prints out "Hello world!" with a quarter second delay after each character
+pyautogui.typewrite('Hello world!', interval=0.25)
