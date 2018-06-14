cd169e0a152acf013b2b27f99dcef3392d32cc73
ipetrash <ilya.petrash@inbox.ru>
2018-06-14 17:56:13 +0500

    Append qt__pyqt__pyside__pyqode\send_QKeyEvent__auto_click_button.py

---
+++ qt__pyqt__pyside__pyqode/send_QKeyEvent__auto_click_button.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.Qt import Qt, QKeyEvent, QApplication, QPushButton, QTimer
+
+
+def key_press_release(widget, key, modifier=Qt.NoModifier):
+    """
+    Функция для отправления события нажатия кнопки.
+    # Имитация нажатия на пробел:
+    key_press_release(widget, Qt.Key_Space)
+    """
+
+    key_press = QKeyEvent(QKeyEvent.KeyPress, key, modifier, None, False, 0)
+    QApplication.sendEvent(widget, key_press)
+
+    key_release = QKeyEvent(QKeyEvent.KeyRelease, key, modifier, None, False, 0)
+    QApplication.sendEvent(widget, key_release)
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    button = QPushButton('Click!')
+    button.clicked.connect(lambda: print('Hello World!'))
+    button.show()
+
+    timer = QTimer()
+    timer.timeout.connect(lambda: key_press_release(button, Qt.Key_Space))
+    timer.setInterval(1000)
+    timer.start()
+
+    app.exec()
