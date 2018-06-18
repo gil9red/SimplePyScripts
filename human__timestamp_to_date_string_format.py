8a13b11e1fb2032f49e64c8343e3d5615957bd99
ipetrash <ilya.petrash@inbox.ru>
2018-06-18 12:37:12 +0500

    Append human__timestamp_to_date_string_format.py

---
+++ human__timestamp_to_date_string_format.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://ru.stackoverflow.com/questions/843386/
+
+
+def timestamp_to_human(time_s):
+    items = [
+        (31536000, '{} г. '),
+        (2592000, '{} мес. '),
+        (604800, '{} нед. '),
+        (86400, '{} д. '),
+        (3600, '{} ч.'),
+    ]
+
+    if time_s == 0:
+        return None
+
+    if time_s < 3600:
+        return 'меньше часа'
+
+    result = ''
+
+    for value, fmt in items:
+        if time_s >= value:
+            result += fmt.format(int(time_s / value))
+            time_s %= value
+
+    return result
+
+
+if __name__ == '__main__':
+    print(timestamp_to_human(2827567.5759670734))  # 1 мес. 2 д. 17 ч.
+    assert timestamp_to_human(2827567.5759670734) == "1 мес. 2 д. 17 ч."
+
+    print(timestamp_to_human(269649.6857390404))   # 3 д. 2 ч.
+    assert timestamp_to_human(269649.6857390404) == "3 д. 2 ч."
