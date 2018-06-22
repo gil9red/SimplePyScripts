ea708ec1ea54468a507ce37d55e47ede5b636e4c
ipetrash <ilya.petrash@inbox.ru>
2018-06-22 11:44:25 +0500

    Append simple_console_progress_bar.py

---
+++ simple_console_progress_bar.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import itertools
+import time
+
+
+def loop():
+    for c in itertools.cycle('|/-\\'):
+        print(c + '\b', flush=True, end='')
+
+        time.sleep(0.3)
+
+
+if __name__ == '__main__':
+    loop()
