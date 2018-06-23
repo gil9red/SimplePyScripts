140808022c40c37f34d4b074c6c4a4feadbedb3d
ipetrash <ilya.petrash@inbox.ru>
2018-06-23 19:41:49 +0500

    #

---
+++ FOO_TEST_TEST/web_handler_key_click/main.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import pyautogui
+
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/key_click", methods=['POST'])
+def key_click():
+    print('key_click')
+
+    data = request.get_json()
+    print('data:', data)
+
+    key = data['key']
+    pyautogui.typewrite([key])
+
+    return jsonify({'status': 'ok'})
+
+
+if __name__ == "__main__":
+    # Localhost
+    app.debug = True
+    app.run(
+        # OR: host='127.0.0.1'
+        host='192.168.0.102',
+        port=9999,
+
+        # # Включение поддержки множества подключений
+        # threaded=True,
+    )
+
+    # # Public IP
+    # app.run(
+    #     host='0.0.0.0',
+    #     port=9999,
+    #
+    #     # # Включение поддержки множества подключений
+    #     # threaded=True,
+    # )
