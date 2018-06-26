699ae5aa34d2fda61b8e6587757bcc646e494a29
ipetrash <ilya.petrash@inbox.ru>
2018-06-26 18:05:46 +0500

    #

---
+++ FOO_TEST_TEST/web_handler_key_click/static/js/autoclick-while-pressed.js
@@ -0,0 +1,36 @@
+// Licensed under the Apache License, Version 2.0.
+// GitHub: https://github.com/silviubogan/jquery-autoclick-while-pressed
+!function (a) {
+    a.fn.autoclickWhilePressed = function (b) {
+        var c = null,
+        d = null,
+        e = {
+            intervalLength : 50,
+            initialDelay : 300,
+            eventToTrigger : "click"
+        };
+
+        if ("object" == typeof b)
+            a.extend(e, b);
+
+        var f = this;
+
+        return this.on({
+            mousedown : function () {
+                d = setTimeout(function () {
+                    c = setInterval(function () {
+                        f.trigger(e.eventToTrigger)
+                    }, e.intervalLength)
+                }, e.initialDelay)
+            },
+            "mouseup mouseleave" : function () {
+                if (null !== d)
+                    clearTimeout(d);
+
+                if (null !== c)
+                    clearInterval(c);
+            }
+        })
+    }
+
+}(jQuery);
