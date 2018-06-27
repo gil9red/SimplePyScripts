2c34bd90dc570e3c7803e1fb3f2a2f446c7bc74c
ipetrash <ilya.petrash@inbox.ru>
2018-06-27 17:54:10 +0500

    Append unshorten_url.py

---
+++ unshorten_url.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://ru.stackoverflow.com/a/847070/201445
+
+
+import requests
+
+
+def unshorten_url(url):
+    return requests.head(url, allow_redirects=True).url
+
+
+if __name__ == '__main__':
+    url = 'https://goo.gl/7RvxZh'
+    print(unshorten_url(url))  # https://ru.stackoverflow.com/questions/847061/
+
+    url = 'https://bit.ly/2tHp17X'
+    print(unshorten_url(url))  # https://ru.stackoverflow.com/questions/847061/
