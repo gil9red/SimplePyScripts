3f6237865d421885db1eef9d6986b03a73269d32
ipetrash <ilya.petrash@inbox.ru>
2018-06-27 14:42:21 +0500

    Append game__get_random_rarity_item.py

---
+++ game__get_random_rarity_item.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import random
+
+
+def get_random_rarity_item():
+    a = random.randrange(1, 1000)
+
+    if a <= 10:
+        return 'легендарный'
+    elif a <= 100:
+        return 'эпический'
+    elif a <= 250:
+        return 'редкий'
+    else:
+        return 'обычный'
+
+
+if __name__ == '__main__':
+    print(get_random_rarity_item())
+    print(get_random_rarity_item())
+    print(get_random_rarity_item())
