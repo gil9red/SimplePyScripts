167f3bda38f3c7b9e9f4033852efaad056c6e044
ipetrash <ilya.petrash@inbox.ru>
2018-06-27 11:28:55 +0500

    #

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -4,30 +4,3 @@
 __author__ = 'ipetrash'
 
 
-import time
-import os
-
-
-def shutdown(off_pc=19):
-    while time.localtime().tm_hour != off_pc:
-        time.sleep(60)  # Ожидание 1 минута
-        
-    ## os.system("shutdown -s")
-    # import sys
-    #
-    # if sys.platform == 'win32':
-    #
-    #     import ctypes
-    #     user32 = ctypes.WinDLL('user32')
-    #     user32.ExitWindowsEx(0x00000008, 0x00000000)
-    #
-    # else:
-    #
-    #     import os
-    #     os.system('sudo shutdown now')
-    #
-    print("shutdown -s")
-
-
-if __name__ == '__main__':
-    shutdown()
