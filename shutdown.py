3410970dd47c916603f3e67988820e7a5c3a7a69
ipetrash <ilya.petrash@inbox.ru>
2018-06-27 13:15:19 +0500

    #

--- shutdown.py
+++ shutdown.py
@@ -14,11 +14,30 @@ def shutdown(off_pc=19):
 
     if sys.platform == 'win32':
         import ctypes
+        import win32security
+        import win32api
+        import winerror
+
+        # Try to enable the required privileges to change the power state
+        privilege_flags = win32security.TOKEN_ADJUST_PRIVILEGES | win32security.TOKEN_QUERY
+        token_handle = win32security.OpenProcessToken(win32api.GetCurrentProcess(), privilege_flags)
+        privilege_id = win32security.LookupPrivilegeValue(None, win32security.SE_SHUTDOWN_NAME)
+
+        # Does the user have permission to change the power state?
+        win32security.AdjustTokenPrivileges(token_handle, 0, [(privilege_id, win32security.SE_PRIVILEGE_ENABLED)])
+
+        if ctypes.GetLastError() != winerror.ERROR_SUCCESS:
+            print(ctypes.WinError(), file=sys.stderr)
+            quit()
+
         user32 = ctypes.WinDLL('user32')
 
         # https://docs.microsoft.com/en-us/windows/desktop/api/winuser/nf-winuser-exitwindowsex
-        EWX_POWEROFF = 0x00000008
-        user32.ExitWindowsEx(EWX_POWEROFF, 0x00000000)
+        user32.ExitWindowsEx(0x00000001, 0x00000000)
+
+        if ctypes.GetLastError() != winerror.ERROR_SUCCESS:
+            print(ctypes.WinError(), file=sys.stderr)
+            quit()
 
     else:
         import os
