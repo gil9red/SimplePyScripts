e0b009939348617a0b17d4c6c51ed13884a1f5a6
ipetrash <ilya.petrash@inbox.ru>
2018-07-03 11:59:13 +0500

    Append qt__pyqt__pyside__pyqode/autowidth__QLineEdit.py

---
+++ qt__pyqt__pyside__pyqode/autowidth__QLineEdit.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5 import Qt
+
+
+class Widget(Qt.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.line_edit = Qt.QLineEdit()
+        self.line_edit.textChanged.connect(self.on_text_changed)
+
+        layout = Qt.QVBoxLayout()
+        layout.addWidget(self.line_edit)
+
+        self.setLayout(layout)
+
+    def on_text_changed(self, text):
+        # Рассчитываем ширину текст по шрифту
+        width = self.line_edit.fontMetrics().width(text)
+        self.line_edit.setMinimumWidth(width)
+
+
+if __name__ == '__main__':
+    app = Qt.QApplication([])
+
+    mw = Widget()
+    mw.show()
+
+    app.exec()
