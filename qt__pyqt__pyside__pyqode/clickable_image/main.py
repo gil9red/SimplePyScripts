a453d8106cf7818411b06e58ab34633c536b2e72
ipetrash <ilya.petrash@inbox.ru>
2018-07-05 14:07:52 +0500

    Append qt__pyqt__pyside__pyqode\clickable_image

---
+++ qt__pyqt__pyside__pyqode/clickable_image/main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5 import Qt
+
+
+app = Qt.QApplication([])
+
+button = Qt.QToolButton()
+button.setIcon(Qt.QIcon('img.png'))
+button.setAutoRaise(True)
+button.setMinimumSize(40, 50)
+button.setIconSize(button.minimumSize())
+
+layout = Qt.QVBoxLayout()
+layout.addWidget(Qt.QLabel('Example:'))
+layout.addWidget(button)
+
+central = Qt.QWidget()
+central.setStyleSheet('background-color: green;')
+central.setLayout(layout)
+
+mw = Qt.QMainWindow()
+mw.setCentralWidget(central)
+mw.show()
+
+app.exec()
