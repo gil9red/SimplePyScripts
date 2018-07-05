647b407481d4aa605a84b9ce40f445cdb92fa0ce
ipetrash <ilya.petrash@inbox.ru>
2018-07-06 00:20:32 +0500

    Append build_exe\pyinstaller_example\no_console__without_console

---
+++ build_exe/pyinstaller_example/no_console__without_console/build_exe.bat
@@ -0,0 +1,1 @@
+pyinstaller --onefile --noconsole main.py
---
+++ build_exe/pyinstaller_example/no_console__without_console/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+print('Start!')
+
+with open('hello.txt', 'w', encoding='utf-8') as f:
+    f.write('Hello World!')
