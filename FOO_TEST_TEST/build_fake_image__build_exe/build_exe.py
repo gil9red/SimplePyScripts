41d980a044a6c61e75bd1e2e5ca593a8860fd179
ipetrash <ilya.petrash@inbox.ru>
2018-07-06 01:42:18 +0500

    #

--- FOO_TEST_TEST/build_fake_image__build_exe/build_exe.py
+++ FOO_TEST_TEST/build_fake_image__build_exe/build_exe.py
@@ -8,6 +8,8 @@ __author__ = 'ipetrash'
 # SOURCE: http://www.fileformat.info/info/unicode/char/202E/index.htm
 
 
+# TODO: этот частный случай, сделать общий -- возможность засунуть любой файл
+
 # TODO: generate image_png.py: https://github.com/gil9red/SimplePyScripts/blob/aec64c1749d4f6f3176e3222c7e7c554f40c693f/generator_py_with_inner_image_with_open/main.py
 # TODO: convert image to ico
 
