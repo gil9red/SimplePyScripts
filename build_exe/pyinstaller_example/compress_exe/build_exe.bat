455d445299f12a2653d725e0383b5926c3c2c1dc
ipetrash <ilya.petrash@inbox.ru>
2018-07-06 00:08:26 +0500

    #

--- build_exe/pyinstaller_example/compress_exe/build_exe.bat
+++ build_exe/pyinstaller_example/compress_exe/build_exe.bat
@@ -1,2 +1,2 @@
 pyinstaller --onefile --windowed main.py
-pyinstaller --onefile --windowed --upx-dir upx391w -n main_compress main.py
+pyinstaller --onefile --windowed --upx-dir=upx391w --name=main_compress main.py
