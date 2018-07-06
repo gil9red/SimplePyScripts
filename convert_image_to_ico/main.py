7cebedb16a5ac81333ebf62af410fdb53a690d29
ipetrash <ilya.petrash@inbox.ru>
2018-07-06 15:41:20 +0500

    Append convert_image_to_ico

---
+++ convert_image_to_ico/main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+def convert_image_to_ico(file_name, file_name_ico, icon_sizes=None):
+    from PIL import Image
+    img = Image.open(file_name)
+
+    if icon_sizes:
+        img.save(file_name_ico, sizes=icon_sizes)
+    else:
+        img.save(file_name_ico)
+
+
+if __name__ == '__main__':
+    file_name = 'image.jpg'
+
+    convert_image_to_ico(file_name, 'logo.ico')
+    convert_image_to_ico(file_name, 'logo2.ico', icon_sizes=[(16, 16), (32, 32), (48, 48), (64, 64)])
+    convert_image_to_ico(file_name, 'logo3.ico', icon_sizes=[(16, 16), (32, 32)])
