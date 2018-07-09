21d375e4d5b76941ab112d53c23edd9375ee268b
ipetrash <ilya.petrash@inbox.ru>
2018-07-09 18:33:59 +0500

    Append qt__pyqt__pyside__pyqode\render_image_from_svg

---
+++ qt__pyqt__pyside__pyqode/render_image_from_svg/main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://stackoverflow.com/a/8551810/5909792
+
+
+from PyQt5 import Qt
+
+# A QApplication instance is necessary if fonts are used in the SVG
+app = Qt.QApplication([])
+
+# Load your SVG
+renderer = Qt.QSvgRenderer("input.svg")
+
+for width, height in [(32, 32), (64, 64), (512, 512), (4096, 4096)]:
+    # Prepare a QImage with desired characteritisc
+    image = Qt.QImage(width, height, Qt.QImage.Format_ARGB32)
+
+    # Partly transparent red-ish background
+    image.fill(Qt.Qt.transparent)
+
+    # Get QPainter that paints to the image
+    painter = Qt.QPainter(image)
+    renderer.render(painter)
+
+    # Save, image format based on file extension
+    image.save("output_{}x{}.png".format(width, height))
