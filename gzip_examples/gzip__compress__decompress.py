38231deb0f58734688f3dd1f533c28254afa3aeb
ipetrash <ilya.petrash@inbox.ru>
2018-07-10 17:19:04 +0500

    Append gzip_examples/gzip__compress__decompress.py

---
+++ gzip_examples/gzip__compress__decompress.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+s_in = "HelloWorld!" * 1000000
+s_in = bytes(s_in, encoding='utf-8')
+print(len(s_in))  # 11000000
+
+import gzip
+s_out = gzip.compress(s_in)
+assert gzip.decompress(s_out) == s_in
+print(len(s_out))  # 21396
