42a6c0470983cee322d9d95ac8b7e1d457e923ae
ipetrash <ilya.petrash@inbox.ru>
2018-07-10 20:16:33 +0500

    Append generate_fake_data__using_Faker/hello_world.py

---
+++ generate_fake_data__using_Faker/hello_world.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://github.com/joke2k/faker
+
+
+# pip install Faker
+from faker import Faker
+fake = Faker()
+
+for _ in range(10):
+    print(fake.name())
+
+print('\n')
+
+# With locale:
+fake = Faker('ru_RU')
+for _ in range(10):
+    print(fake.name())
