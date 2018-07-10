538c0a808b21fcba8b42e9f73166c8b7bf25ef1e
ipetrash <ilya.petrash@inbox.ru>
2018-07-10 17:17:21 +0500

    Append gzip_examples/create_file.py

---
+++ gzip_examples/create_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import gzip
+content = b"Lots of content here"
+with gzip.open('file.txt.gz', 'wb') as f:
+    f.write(content)
