3d8507a3c4e3c05db63d65b99375808d79efc612
ipetrash <ilya.petrash@inbox.ru>
2018-07-10 16:33:09 +0500

    Append XML_to_dict__xmltodict__examples/hello_world.py

---
+++ XML_to_dict__xmltodict__examples/hello_world.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://github.com/martinblech/xmltodict
+
+
+# pip install xmltodict
+import xmltodict
+
+
+doc = xmltodict.parse("""
+<mydocument has="an attribute">
+    <and>
+        <many>elements</many>
+        <many>more elements</many>
+    </and>
+    <plus a="complex">element as well</plus>
+</mydocument>
+""")
+
+print(doc['mydocument']['@has'])           # an attribute
+print(doc['mydocument']['and']["many"])    # ['elements', 'more elements']
+print(doc['mydocument']['plus'])           # OrderedDict([('@a', 'complex'), ('#text', 'element as well')])
+print(doc['mydocument']['plus']['@a'])     # complex
+print(doc['mydocument']['plus']['#text'])  # element as well
+print()
+
+import json
+print(json.dumps(doc, indent=4))
+# {
+#     "mydocument": {
+#         "@has": "an attribute",
+#         "and": {
+#             "many": [
+#                 "elements",
+#                 "more elements"
+#             ]
+#         },
+#         "plus": {
+#             "@a": "complex",
+#             "#text": "element as well"
+#         }
+#     }
+# }
