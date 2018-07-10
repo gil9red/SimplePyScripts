50c61173f29e964254fd9a512c43728b46263b5e
ipetrash <ilya.petrash@inbox.ru>
2018-07-10 17:22:48 +0500

    Append gzip_examples/compress_an_existing_file.py

---
+++ gzip_examples/compress_an_existing_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import gzip
+import shutil
+
+
+file_name = __file__
+
+with open(file_name, 'rb') as f_in:
+    with gzip.open(file_name + '.gz', 'wb') as f_out:
+        shutil.copyfileobj(f_in, f_out)
