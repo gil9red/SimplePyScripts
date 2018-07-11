c021764a034fdc1b6035d9e6835a214d8f2e68dd
ipetrash <ilya.petrash@inbox.ru>
2018-07-11 15:59:34 +0500

    Append XML_to_dict__xmltodict__examples/use_other__expat.py

---
+++ XML_to_dict__xmltodict__examples/use_other__expat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://github.com/martinblech/xmltodict
+
+
+# pip install xmltodict
+import xmltodict
+
+
+# You can pass an alternate version of `expat` (such as `defusedexpat`) by using the `expat` parameter. E.g:
+import defusedexpat
+doc = xmltodict.parse('<a>hello</a>', expat=defusedexpat.pyexpat)
+print(doc)  # OrderedDict([('a', 'hello')])
