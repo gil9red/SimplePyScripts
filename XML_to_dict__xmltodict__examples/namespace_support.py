513bab7f7cb57d612ed68f8a4ad98c0399993fa4
ipetrash <ilya.petrash@inbox.ru>
2018-07-11 14:23:10 +0500

    Append XML_to_dict__xmltodict__examples/namespace_support.py

---
+++ XML_to_dict__xmltodict__examples/namespace_support.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://github.com/martinblech/xmltodict#namespace-support
+
+
+# pip install xmltodict
+import xmltodict
+
+
+# By default, xmltodict does no XML namespace processing (it just treats namespace declarations as regular
+# node attributes), but passing process_namespaces=True will make it expand namespaces for you:
+xml = """
+<root xmlns="http://defaultns.com/"
+      xmlns:a="http://a.com/"
+      xmlns:b="http://b.com/">
+  <x>1</x>
+  <a:y>2</a:y>
+  <b:z>3</b:z>
+</root>
+"""
+doc = xmltodict.parse(xml, process_namespaces=True)
+
+import json
+print(json.dumps(doc, indent=4))
+# {
+#     "http://defaultns.com/:root": {
+#         "http://defaultns.com/:x": "1",
+#         "http://a.com/:y": "2",
+#         "http://b.com/:z": "3"
+#     }
+# }
+
+print('\n')
+
+
+# It also lets you collapse certain namespaces to shorthand prefixes, or skip them altogether:
+namespaces = {
+    'http://defaultns.com/': None,  # skip this namespace
+    'http://a.com/': 'ns_a',        # collapse "http://a.com/" -> "ns_a"
+}
+doc = xmltodict.parse(xml, process_namespaces=True, namespaces=namespaces)
+print(json.dumps(doc, indent=4))
+# {
+#     "root": {
+#         "x": "1",
+#         "ns_a:y": "2",
+#         "http://b.com/:z": "3"
+#     }
+# }
