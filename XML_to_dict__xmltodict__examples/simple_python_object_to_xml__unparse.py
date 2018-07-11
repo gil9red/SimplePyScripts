9704316b5b51cf4ea4dbd16158bdc9c6b5d53961
ipetrash <ilya.petrash@inbox.ru>
2018-07-11 13:39:45 +0500

    Append XML_to_dict__xmltodict__examples/simple_python_object_to_xml__unparse.py

---
+++ XML_to_dict__xmltodict__examples/simple_python_object_to_xml__unparse.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://github.com/martinblech/xmltodict#roundtripping
+
+
+# pip install xmltodict
+import xmltodict
+
+
+class Dog:
+    def __init__(self, name):
+        self.name = name
+        self.type = 'Animal'
+        self.paws = 4
+        self.has_tail = True
+
+    def __repr__(self):
+        return f'<Dog name={self.name} type="{self.type}" paws="{self.paws}" has_tail="{self.has_tail}">'
+
+
+dog = Dog('Ray')
+print(dog)  # <Dog name=Ray type="Animal" paws="4" has_tail="True">
+print()
+
+print(xmltodict.unparse({'Dog': dog.__dict__}, pretty=True))
+# <?xml version="1.0" encoding="utf-8"?>
+# <Dog>
+# 	<name>Ray</name>
+# 	<type>Animal</type>
+# 	<paws>4</paws>
+# 	<has_tail>True</has_tail>
+# </Dog>
