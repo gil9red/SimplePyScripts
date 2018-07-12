ee12deb081e19764a1d9e7066faf3292fa8247dd
ipetrash <ilya.petrash@inbox.ru>
2018-07-12 14:05:00 +0500

    Append shutil__make_archive/print_supported_formats.py

---
+++ shutil__make_archive/print_supported_formats.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://docs.python.org/3/library/shutil.html#archiving-operations
+
+
+import shutil
+
+
+print('Archive formats:')
+for i, (name, description) in enumerate(shutil.get_archive_formats(), 1):
+    print(f'  {i}. {name}: "{description}"')
+
+print()
+
+print('Unpack formats:')
+for i, (name, extensions, description) in enumerate(shutil.get_unpack_formats(), 1):
+    print(f'  {i}. {name}, {extensions}: "{description}"')
