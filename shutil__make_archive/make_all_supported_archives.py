f54264a1e7db74c52ea74bf68f6c414eff6053cc
ipetrash <ilya.petrash@inbox.ru>
2018-07-12 15:02:09 +0500

    Append shutil__make_archive/make_all_supported_archives.py

---
+++ shutil__make_archive/make_all_supported_archives.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://docs.python.org/3/library/shutil.html#archiving-operations
+
+
+import shutil
+
+
+DIR_NAME = 'archive'
+
+import os
+if not os.path.exists(DIR_NAME):
+    os.mkdir(DIR_NAME)
+
+
+# Make archives
+for name, _ in shutil.get_archive_formats():
+    file_name = shutil.make_archive(
+        base_name=DIR_NAME + '/input.txt',
+        format=name,
+        base_dir='input_data/input.txt',
+    )
+    print(file_name)
+
