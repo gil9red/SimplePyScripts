c28fc5f2d92fcfc116d482438674786339c82d02
ipetrash <ilya.petrash@inbox.ru>
2018-07-13 17:14:39 +0500

    Append bcrypt__hash_password__kdf_key_derivation_function

---
+++ bcrypt__hash_password__kdf_key_derivation_function/hash_password.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://github.com/pyca/bcrypt
+
+
+# pip install bcrypt
+import bcrypt
+
+# If "AttributeError: module 'bcrypt._bcrypt' has no attribute 'ffi'":
+# pip uninstall py-bcrypt
+# pip uninstall bcrypt
+# pip install bcrypt
+
+
+password = b"super secret password"
+
+# Hash a password for the first time, with a randomly-generated salt
+hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+print(hashed)
+
+# Check that an unhashed password matches one that has previously been hashed
+if bcrypt.checkpw(password, hashed):
+    print("It Matches!")
+else:
+    print("It Does not Match :(")
---
+++ bcrypt__hash_password__kdf_key_derivation_function/kdf__key_derivation_function.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://github.com/pyca/bcrypt
+# SOURCE: https://en.wikipedia.org/wiki/Key_derivation_function
+
+
+# pip install bcrypt
+import bcrypt
+
+# If "AttributeError: module 'bcrypt._bcrypt' has no attribute 'ffi'":
+# pip uninstall py-bcrypt
+# pip uninstall bcrypt
+# pip install bcrypt
+
+
+key = bcrypt.kdf(
+    password=b'password',
+    salt=b'salt',
+    desired_key_bytes=32,
+    rounds=100
+)
+print(key)  # b'W\x1cq\xbd\xf25W\xf9\xe7\x99\x0fH\xfb\x1a-:n\xbd\x03\xd0\x1a>\x12\xa7\xf7\x0b\x85\x03\xc9\xf9\xbe8'
