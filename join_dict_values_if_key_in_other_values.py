97d2cba23c3f9ff5e2664d256de0b7798322e8a9
ipetrash <ilya.petrash@inbox.ru>
2018-07-13 12:13:07 +0500

    Append join_dict_values_if_key_in_other_values.py

---
+++ join_dict_values_if_key_in_other_values.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# https://ru.stackoverflow.com/questions/854015/
+"""
+Пишу метод, который бы брал на вход словарь, проверял, хранятся ли ключи в значениях других ключей, 
+если да - объединял бы оба списка значений. 
+"""
+
+
+total = {'a': ['b', 'd'], 'b': ['c', 'd'], 'c': ['d']}
+
+for k in total:
+    for k1, v1 in total.items():
+        if k == k1 or v1 is None or k not in v1:
+            continue
+
+        # Объединение значений
+        total[k1] += total[k]
+
+        # Обнуление значения у поглощенного ключа
+        total[k] = None
+
+
+print(total)  # {'a': ['b', 'd', 'c', 'd', 'd'], 'b': None, 'c': None}
+
+total = {k: v for k, v in total.items() if v is not None}
+print(total)  # {'a': ['b', 'd', 'c', 'd', 'd']}
