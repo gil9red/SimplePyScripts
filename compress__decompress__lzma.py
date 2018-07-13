5e5fd9be94b2a1dc244f2a0773c2971106c100be
ipetrash <ilya.petrash@inbox.ru>
2018-07-13 12:55:15 +0500

    #

--- compress__decompress__lzma.py
+++ compress__decompress__lzma.py
@@ -4,6 +4,9 @@
 __author__ = 'ipetrash'
 
 
+# SOURCE: https://docs.python.org/3/library/lzma.html#lzma.compress
+
+
 s_in = "HelloWorld!" * 1000000
 s_in = bytes(s_in, encoding='utf-8')
 print(len(s_in))  # 11000000
