ae78fa4c4e9be08040103d900a9a842dfa4d8f3a
ipetrash <ilya.petrash@inbox.ru>
2018-07-13 17:20:43 +0500

    Append cryptography__examples/hello_world.py

---
+++ cryptography__examples/hello_world.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://github.com/pyca/cryptography
+
+
+# pip install cryptography
+from cryptography.fernet import Fernet
+
+# Put this somewhere safe!
+key = Fernet.generate_key()
+
+message = b"A really secret message. Not for prying eyes."
+
+f = Fernet(key)
+token = f.encrypt(message)
+print(token)
+
+de_message = f.decrypt(token)
+print(de_message)  # b'A really secret message. Not for prying eyes.'
+assert message == de_message
