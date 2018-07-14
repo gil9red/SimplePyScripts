17a3b13d2132ce6687ee5fde33fb443259a08c80
ipetrash <ilya.petrash@inbox.ru>
2018-07-14 21:56:24 +0500

    Append pandas__examples/table_from__html_by_url.py

---
+++ pandas__examples/table_from__html_by_url.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install pandas
+import pandas as pd
+
+url = 'http://www.gks.ru/bgd/regl/b12_14p/IssWWW.exe/Stg/d01/05-02.htm'
+df = pd.read_html(url, header=0, index_col=0)[0]
+print(df)
+
+df.to_excel('table.xlsx')
