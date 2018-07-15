1a86289ed646bffbf114fd0c96b0448c4b53d08c
ipetrash <ilya.petrash@inbox.ru>
2018-07-15 22:19:26 +0500

    #

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -4,3 +4,17 @@
 __author__ = 'ipetrash'
 
 
+url = 'https://ru.wikipedia.org/wiki/Список_серий_«Наруто»_(сезоны_1—3)'
+
+import requests
+rs = requests.get(url)
+
+from bs4 import BeautifulSoup
+root = BeautifulSoup(rs.content, 'html.parser')
+
+td_list = []
+
+for td in root.select('td'):
+    if td.has_attr('id') and td['id'].startswith('ep'):
+        td_list.append(td)
+        print(td)
