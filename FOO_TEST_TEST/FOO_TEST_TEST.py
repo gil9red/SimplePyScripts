855e6561620b068bb88d7ea99379f103105f9d19
ipetrash <ilya.petrash@inbox.ru>
2018-07-16 11:51:43 +0500

    #

--- FOO_TEST_TEST/FOO_TEST_TEST.py
+++ FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -4,13 +4,35 @@
 __author__ = 'ipetrash'
 
 
-url = 'https://ru.wikipedia.org/wiki/Список_серий_«Наруто»_(сезоны_1—3)'
+def get_html_by_url__from_cache(url, cache_dir='cache'):
+    import os
+    if not os.path.exists(cache_dir):
+        os.makedirs(cache_dir)
 
-import requests
-rs = requests.get(url)
+    file_name = os.path.basename(url).replace(':', '_')
+    file_name = cache_dir + '/' + file_name + '.html'
+
+    if os.path.exists(file_name):
+        with open(file_name, 'rb') as f:
+            return f.read()
+
+    with open(file_name, 'wb') as f:
+        import requests
+        rs = requests.get(url)
+        f.write(rs.content)
+
+        return rs.content
+
+
+url = 'https://ru.wikipedia.org/wiki/Список_серий_«Наруто»_(сезоны_1—3)'
+# url = 'https://ru.wikipedia.org/wiki/Список_серий_«Наруто»_(сезоны_4—6)'
+# url = 'https://ru.wikipedia.org/wiki/Список_серий_«Наруто»_(сезоны_7—9)'
+# url = 'https://ru.wikipedia.org/wiki/Список_серий_«Наруто:_Ураганные_хроники»_(сезоны_1—4)'
+html_content = get_html_by_url__from_cache(url)
+
 
 from bs4 import BeautifulSoup
-root = BeautifulSoup(rs.content, 'html.parser')
+root = BeautifulSoup(html_content, 'html.parser')
 
 td_list = []
 
