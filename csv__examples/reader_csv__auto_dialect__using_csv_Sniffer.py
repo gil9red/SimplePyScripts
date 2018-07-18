ccac86022c56bbaa2ad4cb141b011ad0e5e87fca
ipetrash <ilya.petrash@inbox.ru>
2018-07-18 12:32:34 +0500

    Append csv__examples

---
+++ csv__examples/reader_csv__auto_dialect__using_csv_Sniffer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import csv
+
+with open('input.csv') as f:
+    dialect = csv.Sniffer().sniff(f.readline(), delimiters=[',', ';'])
+    f.seek(0)
+
+    csv_reader = csv.reader(f, dialect=dialect)
+    for row in csv_reader:
+        print(row)
