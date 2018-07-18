5b5f4d0f70beda9364aa476a017cb49247d0dc9a
ipetrash <ilya.petrash@inbox.ru>
2018-07-18 14:42:42 +0500

    Append enum__examples/hello_world__auto_value.py

---
+++ enum__examples/hello_world__auto_value.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from enum import IntEnum, auto
+
+
+class Direction(IntEnum):
+    UP = auto()
+    DOWN = auto()
+    LEFT = auto()
+    RIGHT = auto()
+
+
+print(Direction)           # <enum 'Direction'>
+print(Direction.UP)        # Direction.UP
+print(Direction.DOWN)      # Direction.DOWN
+print()
+
+print(Direction.UP.value)  # 1
+print(int(Direction.UP))   # 1
+print()
+
+print(Direction.UP == 1)   # True
+print(Direction.UP == 2)   # False
+print()
+
+direction = Direction.UP
+print(direction == Direction.UP)    # True
+print(direction == Direction.DOWN)  # False
+print()
+
+print(Direction(1))  # Direction.UP
+print(Direction(3))  # Direction.LEFT
+print()
+
+print(list(Direction))         # [<Direction.UP: 1>, <Direction.DOWN: 2>, <Direction.LEFT: 3>, <Direction.RIGHT: 4>]
+print([x for x in Direction])  # [<Direction.UP: 1>, <Direction.DOWN: 2>, <Direction.LEFT: 3>, <Direction.RIGHT: 4>]
