c7b1360ed90060f8bc83d0da229bcdba76259e1d
ipetrash <ilya.petrash@inbox.ru>
2018-07-20 14:30:53 +0500

    Append pathlib__examples/syntax_path.py

---
+++ pathlib__examples/syntax_path.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# C:\Windows\System32\Boot
+
+import pathlib
+path = pathlib.Path('C:/') / 'Windows' / 'System32' / 'Boot'
+print(path)  # C:\Windows\System32\Boot
