f2fcde75c4937e0ef30f8988b3fe3de07c6b271f
ipetrash <ilya.petrash@inbox.ru>
2018-07-20 18:29:36 +0500

    Append qt__pyqt__pyside__pyqode/window_as_glass__win7.py

---
+++ qt__pyqt__pyside__pyqode/window_as_glass__win7.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout, QPushButton
+from PyQt5.QtCore import Qt
+
+
+class Widget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setAttribute(Qt.WA_TranslucentBackground)
+
+        layout = QVBoxLayout()
+        layout.addStretch()
+        layout.addWidget(QPushButton("Закрыть окно", clicked=self.close))
+
+        self.setLayout(layout)
+
+        from ctypes import windll, c_int, byref
+        windll.dwmapi.DwmExtendFrameIntoClientArea(c_int(self.winId()), byref(c_int(-1)))
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    w = Widget()
+    w.resize(400, 300)
+    w.show()
+
+    app.exec()
