e716ea12466e4998e0f9f545a21ab6262f27fc05
ipetrash <ilya.petrash@inbox.ru>
2018-07-21 00:18:03 +0500

    Append winapi__windows__ctypes/ctypes__FindWindow.py

---
+++ winapi__windows__ctypes/ctypes__FindWindow.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import ctypes
+FindWindow = ctypes.windll.user32.FindWindowW
+hwnd = FindWindow('Shell_TrayWnd', None)
+print(hwnd)
