94e35a4b2fc6778288b1d3e81a989deff0f67e7e
ipetrash <ilya.petrash@inbox.ru>
2018-07-21 03:29:23 +0500

    Append qt__pyqt__pyside__pyqode/pyqt_QTextEdit_setHtml__hello_world.py

---
+++ qt__pyqt__pyside__pyqode/pyqt_QTextEdit_setHtml__hello_world.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5 import Qt
+
+app = Qt.QApplication([])
+
+te = Qt.QTextEdit()
+te.setHtml("""
+<b>Hello</b> <i>Qt!</i><br>
+<font color="red"><b>Привет<b></font> <font size="20">мир!</font>
+""")
+te.show()
+
+app.exec()
