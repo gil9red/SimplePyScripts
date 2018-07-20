dc98e0411832d23350e4655e75be97ed09c1d7a1
ipetrash <ilya.petrash@inbox.ru>
2018-07-21 00:45:31 +0500

    Append winapi__windows__ctypes/ctypes__MessageBeep.py

---
+++ winapi__windows__ctypes/ctypes__MessageBeep.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import ctypes
+
+# SOURCE: https://docs.microsoft.com/ru-ru/windows/desktop/api/winuser/nf-winuser-messagebeep
+#
+# BOOL MessageBeep(
+#   UINT uType
+# );
+MessageBeep = ctypes.windll.user32.MessageBeep
+
+
+ok = MessageBeep(0xFFFFFFFF)
+print(ok)
