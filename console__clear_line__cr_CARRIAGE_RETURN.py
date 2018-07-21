c6b7e47e88f4538d5073bd0b1484cbb2b816f6f1
ipetrash <ilya.petrash@inbox.ru>
2018-07-21 05:57:18 +0500

    #

--- console__clear_line__cr_CARRIAGE_RETURN.py
+++ console__clear_line__cr_CARRIAGE_RETURN.py
@@ -5,8 +5,10 @@ __author__ = 'ipetrash'
 
 
 import time
+import sys
 
 for ips in (10254, 1, 10, 1, 10254, 999):
-    print(f'Current value [{ips}]', end='\r\n')
+    sys.stdout.write('\r' + f'Current value [{ips}]' + ' ' * 100)
+    sys.stdout.flush()
 
     time.sleep(0.5)
