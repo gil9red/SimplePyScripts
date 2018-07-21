ec4c457660628deffdb58c68d7ab55ae4cea7d15
ipetrash <ilya.petrash@inbox.ru>
2018-07-22 02:05:05 +0500

    Append qt__pyqt__pyside__pyqode/pyqt5__move_child_on_center__parent_widget.py

---
+++ qt__pyqt__pyside__pyqode/pyqt5__move_child_on_center__parent_widget.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5 import Qt
+
+
+class MainWindow(Qt.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.widget = Qt.QWidget()
+        self.widget.resize(100, 100)
+
+        button = Qt.QPushButton('Click!', clicked=self.show_and_move)
+
+        self.setCentralWidget(button)
+
+    def show_and_move(self):
+        self.widget.hide()
+
+        self.widget.move(self.geometry().center() - self.widget.rect().center())
+
+        self.widget.show()
+
+
+if __name__ == '__main__':
+    app = Qt.QApplication([])
+
+    mw = MainWindow()
+    mw.resize(500, 500)
+    mw.show()
+
+    app.exec()
