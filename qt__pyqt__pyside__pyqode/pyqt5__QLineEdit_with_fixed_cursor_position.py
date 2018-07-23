94ddf38dfc4084b26d82686191360a1de2b52bc7
ipetrash <ilya.petrash@inbox.ru>
2018-07-23 14:37:25 +0500

    Append qt__pyqt__pyside__pyqode/pyqt5__QLineEdit_with_fixed_cursor_position.py

---
+++ qt__pyqt__pyside__pyqode/pyqt5__QLineEdit_with_fixed_cursor_position.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5 import Qt
+
+
+app = Qt.QApplication([])
+
+le = Qt.QLineEdit()
+le.textEdited.connect(lambda _: le.setCursorPosition(1))
+le.show()
+
+app.exec()
