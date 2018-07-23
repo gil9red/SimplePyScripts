1136c8019403dcd019139f73845cb7b18586c217
ipetrash <ilya.petrash@inbox.ru>
2018-07-23 14:45:36 +0500

    Append threading__examples/timeout_run_function.py

---
+++ threading__examples/timeout_run_function.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import time
+from threading import Thread, current_thread
+
+
+class User:
+    def __init__(self, name):
+        self.name = name
+
+    def post_msg(self):
+        time.sleep(3)
+
+        # Написать пользователю
+        print('Hi, {}! current_thread: {}'.format(self.name, current_thread()))
+
+
+user_1 = User('Vasya')
+user_1.post_msg()
+
+user_2 = User('Petya')
+Thread(target=user_2.post_msg).start()
+
+
+def foo(name):
+    time.sleep(5)
+
+    # Написать пользователю
+    print('Hi, {}! current_thread: {}'.format(name, current_thread()))
+
+
+Thread(target=lambda: foo('Thread')).start()
