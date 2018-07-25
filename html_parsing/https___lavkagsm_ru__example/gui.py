c116e64e23bafd285edfcfe728af978f7c0931f6
ipetrash <ilya.petrash@inbox.ru>
2018-07-26 00:47:07 +0500

    Append html_parsing\https___lavkagsm_ru__example

---
+++ html_parsing/https___lavkagsm_ru__example/gui.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.Qt import *
+
+from main import get_html_by_url__from_cache, get_parsed_items
+from bs4 import BeautifulSoup
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        headers = ['NAME', 'PRICE', 'ARTICLE', 'URL', 'PHOTO_URL']
+
+        self.table_widget = QTableWidget()
+        self.table_widget.setColumnCount(len(headers))
+        self.table_widget.setHorizontalHeaderLabels(headers)
+        self.table_widget.horizontalHeader().setStretchLastSection(True)
+        self.table_widget.setSelectionBehavior(QTableWidget.SelectRows)
+        self.table_widget.setSelectionMode(QTableWidget.SingleSelection)
+        self.table_widget.itemDoubleClicked.connect(self._on_item_double_click)
+
+        self.setCentralWidget(self.table_widget)
+
+    def fill(self):
+        url = 'https://lavkagsm.ru/catalog/mikroskhemy/?view=blocks&page_count=48&sort=name&by=asc'
+        html_content = get_html_by_url__from_cache(url)
+        root = BeautifulSoup(html_content, 'html.parser')
+
+        for item_data in get_parsed_items(url, root):
+            row = self.table_widget.rowCount()
+            self.table_widget.setRowCount(row + 1)
+
+            self.table_widget.setItem(row, 0, QTableWidgetItem(item_data['name']))
+            self.table_widget.setItem(row, 1, QTableWidgetItem(item_data['price']))
+            self.table_widget.setItem(row, 2, QTableWidgetItem(item_data['art']))
+            self.table_widget.setItem(row, 3, QTableWidgetItem(item_data['url']))
+            self.table_widget.setItem(row, 4, QTableWidgetItem(item_data['photo_url']))
+
+        self.table_widget.resizeColumnToContents(0)
+        self.table_widget.resizeColumnToContents(1)
+
+    def _on_item_double_click(self, item):
+        row = item.row()
+
+        item_url = self.table_widget.item(row, 3)
+        url = item_url.text()
+
+        QDesktopServices.openUrl(QUrl(url))
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    mw = MainWindow()
+    mw.show()
+    mw.resize(1500, 800)
+    mw.fill()
+
+    app.exec()
---
+++ html_parsing/https___lavkagsm_ru__example/main.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from urllib.parse import urljoin
+
+
+def get_html_by_url__from_cache(url, cache_dir='cache'):
+    import os
+    if not os.path.exists(cache_dir):
+        os.makedirs(cache_dir)
+
+    file_name = os.path.basename(url)
+
+    import re
+    file_name = re.sub(r'[^a-zA-Z\d]', '_', file_name)
+
+    file_name = cache_dir + '/' + file_name + '.html'
+
+    if os.path.exists(file_name):
+        with open(file_name, 'rb') as f:
+            return f.read()
+
+    with open(file_name, 'wb') as f:
+        import requests
+        rs = requests.get(url)
+        f.write(rs.content)
+
+        return rs.content
+
+
+def parse_item(url, node_item) -> dict:
+    data = {
+        'name': node_item.select_one('.link').text.strip(),
+        'url': urljoin(url, node_item.select_one('.link')['href']),
+        'photo_url': urljoin(url, node_item.select_one('.photo img')['data-original']),
+        'art': node_item.select_one('.art > strong').text.strip(),
+        'price': node_item.select_one('.price > .value').text.strip(),
+    }
+
+    return data
+
+
+def get_parsed_items(url, root) -> [dict]:
+    return [parse_item(url, item) for item in root.select('#catalog_section .catalog-item')]
+
+
+if __name__ == '__main__':
+    url = 'https://lavkagsm.ru/catalog/mikroskhemy/?view=blocks&page_count=48&sort=name&by=asc'
+
+    from bs4 import BeautifulSoup
+    html_content = get_html_by_url__from_cache(url)
+    root = BeautifulSoup(html_content, 'html.parser')
+
+    items = get_parsed_items(url, root)
+
+    for i, item in enumerate(items, 1):
+        print(i, item)
