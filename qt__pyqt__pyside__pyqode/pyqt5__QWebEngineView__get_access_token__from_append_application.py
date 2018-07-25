7f659fefc2d37522932d3c4dff9282b7a35a6a5c
ipetrash <ilya.petrash@inbox.ru>
2018-07-25 18:10:39 +0500

    Append qt__pyqt__pyside__pyqode/pyqt5__QWebEngineView__get_access_token__from_append_application.py

---
+++ qt__pyqt__pyside__pyqode/pyqt5__QWebEngineView__get_access_token__from_append_application.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import re
+
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWidgets import QApplication
+
+
+def authorize(token):
+    print(f'token: {token}')
+
+
+def url_changed(url):
+    url = url.toString()
+    print(f'url_changed: {url}')
+
+    match = re.search('access_token=([a-fA-F0-9]+)&?', url)
+    if match:
+        access_token = match.group(1)
+        authorize(access_token)
+
+    else:
+        print(url, 'doesn\'t match')
+
+
+app_id = 5356487  # mini_vk_bot (никаких требований не запрашивает)
+app_id = 3088991  # Игра "Проклятье часовщика" (хочет "Доступ к общей информации")
+
+params = {
+    'client_id': str(app_id),
+    'display': 'page',
+    'redirect_uri': 'https://oauth.vk.com/blank.html',
+    'response_type': 'token',
+    # 'scope': 'messages',
+    'v': '5.80'
+}
+link = 'https://oauth.vk.com/authorize?' + '&'.join(f'{k}={v}' for k, v in params.items())
+# link = 'https://vk.com/'
+
+app = QApplication([])
+
+web = QWebEngineView()
+web.load(QUrl(link))
+web.urlChanged.connect(url_changed)
+web.show()
+
+app.exec()
