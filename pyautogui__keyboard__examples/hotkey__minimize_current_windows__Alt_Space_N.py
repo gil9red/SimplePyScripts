ffd33ffac91cdde201100783b92586184b0b7945
ipetrash <ilya.petrash@inbox.ru>
2018-07-25 11:58:48 +0500

    Append pyautogui__keyboard__examples/hotkey__minimize_current_windows__Alt_Space_N.py

---
+++ pyautogui__keyboard__examples/hotkey__minimize_current_windows__Alt_Space_N.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install keyboard
+import keyboard
+keyboard.add_hotkey('Win + -', lambda: keyboard.send('Alt + Space + N'))
+
+# Block forever.
+keyboard.wait()
