430b0c62c1d5a90a0c685078129d2c449eb8ac5c
ipetrash <ilya.petrash@inbox.ru>
2018-07-27 13:52:04 +0500

    Append qt__pyqt__pyside__pyqode/pyqt5__QTreeView_QFileSystemModel.py

---
+++ qt__pyqt__pyside__pyqode/pyqt5__QTreeView_QFileSystemModel.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.QtWidgets import QTreeView, QFileSystemModel, QApplication
+from PyQt5.QtCore import QDir
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    model = QFileSystemModel()
+    model.setRootPath(QDir.currentPath())
+    model.setReadOnly(False)
+
+    mw = QTreeView()
+    mw.setModel(model)
+    mw.setRootIndex(model.index(QDir.currentPath()))
+    mw.show()
+
+    app.exec()
