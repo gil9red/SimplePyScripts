5a0455661969c7c9df2a272bdbb036e5c43d2b7d
ipetrash <ilya.petrash@inbox.ru>
2018-07-30 17:44:37 +0500

    Append regexp_examples/sub__with_callback_on_match.py

---
+++ regexp_examples/sub__with_callback_on_match.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import re
+
+
+text = """\
+<item id="111">
+<title>Заголовок</title>
+<description>Описание</description>
+<images>
+<image id="111">фото1</image>
+<image id="111">фото2</image>
+</images>
+</item>
+<item id="111">
+<title>Заголовок</title>
+<description>Описание</description>
+<images>
+<image id="111">фото1</image>
+<image id="111">фото2</image>
+</images>
+</item>
+<item id="111">
+<title>Заголовок</title>
+<description>Описание</description>
+<images>
+<image id="111">фото1</image>
+<image id="111">фото2</image>
+</images>
+</item>
+"""
+
+counter = 3000 - 1
+
+
+def on_sub(_):
+    global counter
+    counter += 1
+
+    return 'id="{}"'.format(counter)
+
+
+new_text = re.sub(r'id="\d+"', on_sub, text)
+print(new_text)
