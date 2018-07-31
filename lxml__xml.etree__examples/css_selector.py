a604b9e6d4fc9295b5b6bab20167f2e6f7523691
ipetrash <ilya.petrash@inbox.ru>
2018-07-31 14:47:38 +0500

    Append lxml__xml.etree__examples/css_selector.py

---
+++ lxml__xml.etree__examples/css_selector.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from lxml import html
+root = html.fromstring('<p>Hello<br><b>world</b>!</p><br>')
+
+word = root.cssselect('b')[0]
+print(html.tostring(word, encoding='unicode', with_tail=False))  # <b>world</b>
