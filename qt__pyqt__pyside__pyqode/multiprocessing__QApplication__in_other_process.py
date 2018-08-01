c0f8c2bd098dba53f694831a8ddaef225a23ad7d
ipetrash <ilya.petrash@inbox.ru>
2018-08-01 23:38:37 +0500

    #

--- qt__pyqt__pyside__pyqode/multiprocessing__QApplication__in_other_process.py
+++ qt__pyqt__pyside__pyqode/multiprocessing__QApplication__in_other_process.py
@@ -3,3 +3,22 @@
 
 __author__ = 'ipetrash'
 
+
+from multiprocessing import Process
+from PyQt5 import Qt
+
+
+def go(name):
+    app = Qt.QApplication([])
+
+    mw = Qt.QLabel()
+    mw.setText('Hello, ' + name)
+    mw.show()
+
+    app.exec()
+
+
+if __name__ == '__main__':
+    p = Process(target=go, args=('bob',))
+    p.start()
+    p.join()
