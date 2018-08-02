6a20749870846ec4e06410ba7076c4531f368b75
ipetrash <ilya.petrash@inbox.ru>
2018-08-02 15:03:45 +0500

    #

--- qt__pyqt__pyside__pyqode/multiprocessing__QApplication__in_other_process_list__Pool_map.py
+++ qt__pyqt__pyside__pyqode/multiprocessing__QApplication__in_other_process_list__Pool_map.py
@@ -4,10 +4,9 @@
 __author__ = 'ipetrash'
 
 
-from multiprocessing import Pool
-from multiprocessing__QApplication__in_other_process import go
-
-
 if __name__ == '__main__':
+    from multiprocessing import Pool
+    from multiprocessing__QApplication__in_other_process import go
+
     with Pool() as p:
         p.map(go, ['Alice', 'Bob', 'World'])
