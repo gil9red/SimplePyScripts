37f2b3bd5fbc2132fafc26a8af3ea07855f33750
ipetrash <ilya.petrash@inbox.ru>
2018-08-02 13:27:56 +0500

    Append multiprocessing__examples/create_process_get_result.py

---
+++ multiprocessing__examples/create_process_get_result.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from multiprocessing import Process, Queue
+
+
+def f(q, name):
+    q.put('Hello, ' + name)
+
+
+if __name__ == '__main__':
+    q = Queue()
+    p = Process(target=f, args=(q, 'bob'))
+    p.start()
+    print(q.get())  # Hello, bob
+    p.join()
