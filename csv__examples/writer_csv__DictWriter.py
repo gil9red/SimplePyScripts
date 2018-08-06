ae436098872b007de445b6563e81e5cb96ba506f
ipetrash <ilya.petrash@inbox.ru>
2018-08-06 13:00:49 +0500

    Append csv__examples/writer_csv__DictWriter.py

---
+++ csv__examples/writer_csv__DictWriter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://docs.python.org/3/library/csv.html#csv.DictWriter
+
+
+import csv
+
+with open('names.csv', 'w', encoding='utf-8', newline='') as csvfile:
+    fieldnames = ['first_name', 'last_name']
+
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+
+    writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
+    writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
+    writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
