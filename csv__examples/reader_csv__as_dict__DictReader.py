bfb1a04aafc62bc02296805d8cd56c2cdb71a716
ipetrash <ilya.petrash@inbox.ru>
2018-08-06 12:48:37 +0500

    Append csv__examples/reader_csv__as_dict__DictReader.py

---
+++ csv__examples/reader_csv__as_dict__DictReader.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import csv
+
+with open('input.csv') as f:
+    csv_reader = csv.DictReader(f, delimiter=";")
+    for row in csv_reader:
+        print(row['name'], row['address'])
