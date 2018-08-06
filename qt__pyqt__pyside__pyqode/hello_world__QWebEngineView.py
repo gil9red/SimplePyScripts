2bf0fc955d14bbe5b9c826371e0a2f8cc5531cc2
ipetrash <ilya.petrash@inbox.ru>
2018-08-06 14:37:42 +0500

    Append qt__pyqt__pyside__pyqode/hello_world__QWebEngineView.py

---
+++ qt__pyqt__pyside__pyqode/hello_world__QWebEngineView.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+
+
+app = QApplication([])
+
+view = QWebEngineView()
+view.show()
+
+url = 'https://www.google.com/search?q=hello world'
+view.load(QUrl(url))
+
+app.exec()
