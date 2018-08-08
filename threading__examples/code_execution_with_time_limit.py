ec3a89b37d54f70cc4865401b966bee20d6f7389
ipetrash <ilya.petrash@inbox.ru>
2018-08-08 14:03:48 +0500

    Append threading__examples/code_execution_with_time_limit.py

---
+++ threading__examples/code_execution_with_time_limit.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import threading
+
+
+def run():
+    import time
+
+    i = 1
+
+    # Бесконечный цикл
+    while True:
+        print(i)
+        i += 1
+
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    thread = threading.Thread(target=run, daemon=True)
+    thread.start()
+    thread.join(5)
+
+    print('Quit!')
