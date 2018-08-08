12d2de377046ae00e43d7ed7e6f33b33fbe34544
ipetrash <ilya.petrash@inbox.ru>
2018-08-08 21:20:28 +0500

    Append seconds_to_str.py

---
+++ seconds_to_str.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+def seconds_to_str(seconds: int) -> str:
+    mm, ss = divmod(seconds, 60)
+    hh, mm = divmod(mm, 60)
+
+    return "%02d:%02d:%02d" % (hh, mm, ss)
+
+
+if __name__ == '__main__':
+    print(seconds_to_str(1))     # 00:00:01
+    assert seconds_to_str(1) == '00:00:01'
+
+    print(seconds_to_str(60))    # 00:01:00
+    assert seconds_to_str(60) == '00:01:00'
+
+    print(seconds_to_str(3600))  # 01:00:00
+    assert seconds_to_str(3600) == '01:00:00'
+
+    print(seconds_to_str(3678))  # 01:01:18
+    assert seconds_to_str(3678) == '01:01:18'
+
+    print(seconds_to_str(8888))  # 02:28:08
+    assert seconds_to_str(8888) == '02:28:08'
