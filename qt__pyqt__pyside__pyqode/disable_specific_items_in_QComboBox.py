4e0136371d6d09070b6e28446fa80de2980da4d3
ipetrash <ilya.petrash@inbox.ru>
2018-08-08 17:33:15 +0500

    Append qt__pyqt__pyside__pyqode/disable_specific_items_in_QComboBox.py

---
+++ qt__pyqt__pyside__pyqode/disable_specific_items_in_QComboBox.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.Qt import QApplication, QWidget, QVBoxLayout, QComboBox
+
+
+app = QApplication([])
+
+combo_box = QComboBox()
+
+for i in range(10):
+    combo_box.addItem("item_" + str(i + 1))
+
+model = combo_box.model()
+
+# Указываем какие элементы сделать невыбираемыми
+model.item(0).setEnabled(False)
+model.item(2).setEnabled(False)
+model.item(4).setEnabled(False)
+
+combo_box.setCurrentIndex(-1)
+
+layout = QVBoxLayout()
+layout.addWidget(combo_box)
+
+mw = QWidget()
+mw.setLayout(layout)
+mw.show()
+
+app.exec()
