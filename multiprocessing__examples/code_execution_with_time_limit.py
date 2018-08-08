3c7df302072d001f2d991590b41f93984749829c
ipetrash <ilya.petrash@inbox.ru>
2018-08-08 14:06:54 +0500

    Append multiprocessing__examples/code_execution_with_time_limit.py

---
+++ multiprocessing__examples/code_execution_with_time_limit.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import multiprocessing
+
+
+def run():
+    import time
+
+    i = 1
+
+    # Бесконечный цикл
+    while True:
+        print(i)
+        i += 1
+
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    p = multiprocessing.Process(target=run)
+    p.start()
+
+    # Wait
+    p.join(5)
+
+    # Если процесс живой,то убиваем его
+    if p.is_alive():
+        print("Kill it.")
+
+        # Terminate
+        p.terminate()
