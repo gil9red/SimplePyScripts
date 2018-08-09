8370ab35095d26588736bd59cbef4b067ab3e77a
ipetrash <ilya.petrash@inbox.ru>
2018-08-09 18:34:04 +0500

    Append qt__pyqt__pyside__pyqode/pyqt5__clicked_label.py

---
+++ qt__pyqt__pyside__pyqode/pyqt5__clicked_label.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.Qt import QLabel, pyqtSignal, QApplication, QVBoxLayout, QWidget
+
+
+class ClickedLabel(QLabel):
+    clicked = pyqtSignal()
+
+    def mouseReleaseEvent(self, e):
+        super().mouseReleaseEvent(e)
+
+        self.clicked.emit()
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    label_1 = ClickedLabel('Label 1')
+    label_1.clicked.connect(lambda: print('label_1'))
+
+    label_2 = ClickedLabel('Label 2')
+    label_2.clicked.connect(lambda: print('label_2'))
+
+    layout = QVBoxLayout()
+    layout.addWidget(label_1)
+    layout.addWidget(label_2)
+
+    mw = QWidget()
+    mw.setLayout(layout)
+    mw.show()
+
+    app.exec()
