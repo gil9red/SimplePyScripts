e196c8df49ff74a0a70fced3502fa3bce344d246
ipetrash <ilya.petrash@inbox.ru>
2018-08-09 12:11:11 +0500

    #

--- list_split_by_pairs.py
+++ list_split_by_pairs.py
@@ -5,5 +5,9 @@ __author__ = 'ipetrash'
 
 
 items = [5, 80, 3, 83, 1, 77, 1, 77, 2, 82, 1, 77, 5, 81, 2, 78, 1, 81, 5, 85, 5, 85, 4, 84, 2, 78, 1, 81, 3, 83]
+
 new_items = [(items[i], items[i + 1]) for i in range(0, len(items), 2)]
+print(new_items)  # [(5, 80), (3, 83), (1, 77), (1, 77), (2, 82), ...
+
+new_items = [x for x in zip(*[iter(items)] * 2)]
 print(new_items)  # [(5, 80), (3, 83), (1, 77), (1, 77), (2, 82), ...
