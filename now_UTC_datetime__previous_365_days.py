2ebd9bba9011a598ed4ec2c7ea26d0ef9d9ef502
ipetrash <ilya.petrash@inbox.ru>
2018-08-10 15:47:57 +0500

    Append now_UTC_datetime__previous_365_days.py

---
+++ now_UTC_datetime__previous_365_days.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import datetime as DT
+utc_datetime = DT.datetime.utcnow()
+
+for i in range(365 + 1):
+    date = utc_datetime - DT.timedelta(days=i)
+    print(date.strftime("%d/%m/%Y %H:%M:%S"))
