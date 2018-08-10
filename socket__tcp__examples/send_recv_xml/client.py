74d02ceb9c5ece7bd2681d7f866f20647873df68
ipetrash <ilya.petrash@inbox.ru>
2018-08-10 17:00:34 +0500

    Append socket__tcp__examples\send_recv_xml

---
+++ socket__tcp__examples/send_recv_xml/client.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import socket
+import json
+
+import sys
+sys.path.append('..')
+
+from common import send_msg, recv_msg
+
+
+HOST, PORT = "localhost", 9090
+
+
+with socket.socket() as sock:
+    sock.connect((HOST, PORT))
+
+    data = """\
+<root>
+    <title>test</title>
+    <counter>1</counter>
+</root>    
+    """
+    data = bytes(data, 'utf-8')
+    print('Sending ({}): {}'.format(len(data), data))
+    print()
+
+    send_msg(sock, data)
+
+    print('Receiving')
+
+    response_data = recv_msg(sock)
+    print('Response ({}): {}'.format(len(response_data), response_data))
+
+    print('Close\n')
---
+++ socket__tcp__examples/send_recv_xml/server.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import socket
+from bs4 import BeautifulSoup
+
+import sys
+sys.path.append('..')
+
+from common import send_msg, recv_msg
+
+
+PORT = 9090
+
+
+with socket.socket() as sock:
+    sock.bind(('', 9090))
+    sock.listen(1)
+
+    print('Server: {}'.format(sock.getsockname()))
+
+    while True:
+        conn, addr = sock.accept()
+        print('Connected:', addr)
+
+        data = recv_msg(conn)
+        print('Receiving ({}): {}'.format(len(data), data))
+
+        xml_data = BeautifulSoup(data, 'html.parser')
+        print('xml_data:', xml_data)
+
+        xml_data.title.string = 'updates'
+        xml_data.counter.string = str(int(xml_data.counter.text) + 1)
+
+        data = str(xml_data)
+        print('Sending: {}'.format(data))
+
+        rs = bytes(data, 'utf-8')
+        send_msg(conn, rs)
+
+        print('Close\n')
