c6ba93797339b5b9e3cc5a920d5958394aa40af6
ipetrash <ilya.petrash@inbox.ru>
2018-08-13 14:57:47 +0500

    #

--- stackoverflow_site__parsing/print__datetime_utc_and_reputation_change.py
+++ stackoverflow_site__parsing/print__datetime_utc_and_reputation_change.py
@@ -6,6 +6,7 @@ __author__ = 'ipetrash'
 
 import requests
 from bs4 import BeautifulSoup
+import datetime as DT
 
 
 url = 'https://ru.stackoverflow.com/users/201445/gil9red?tab=reputation'
@@ -13,7 +14,16 @@ url = 'https://ru.stackoverflow.com/users/201445/gil9red?tab=reputation'
 rs = requests.get(url)
 root = BeautifulSoup(rs.content, 'html.parser')
 
+day_by_rep = dict()
+
 for row in root.select('.rep-table-row'):
     day = row.select_one('.rep-day')['title']
     rep = row.select_one('.rep-cell').text.strip()
-    print(day, rep)
+
+    day = DT.datetime.strptime(day, '%Y-%m-%d')
+
+    day_by_rep[day] = rep
+
+# Print
+for day, rep in sorted(day_by_rep.items(), key=lambda x: x[0]):
+    print('{:%d/%m/%Y} : {}'.format(day, rep))
