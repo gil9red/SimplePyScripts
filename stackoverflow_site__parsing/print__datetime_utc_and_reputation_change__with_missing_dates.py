891cab4834e763c0e05bd4af6735a0e9b287ad08
ipetrash <ilya.petrash@inbox.ru>
2018-08-13 15:05:44 +0500

    Append stackoverflow_site__parsing/print__datetime_utc_and_reputation_change__with_missing_dates.py

---
+++ stackoverflow_site__parsing/print__datetime_utc_and_reputation_change__with_missing_dates.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import datetime as DT
+
+
+def generate_range_dates(start_date, end_date) -> list:
+    date_1 = min(start_date, end_date)
+    date_2 = max(start_date, end_date)
+
+    # Сразу добавляем стартовую дату
+    items = [date_1]
+
+    i = 1
+
+    while date_1 < date_2:
+        date_1 += DT.timedelta(days=i)
+        items.append(date_1)
+
+    return items
+
+from print__datetime_utc_and_reputation_change import get_day_by_rep
+
+
+if __name__ == '__main__':
+    url = 'https://ru.stackoverflow.com/users/201445/gil9red?tab=reputation'
+    day_by_rep = get_day_by_rep(url)
+
+    start_date, end_date = min(day_by_rep), max(day_by_rep)
+    print('Start: {}, end: {}'.format(start_date, end_date))
+    print()
+
+    # Сгенерируем диапазон дат
+    dates = generate_range_dates(start_date, end_date)
+
+    # Print
+    for day in dates:
+        print('{:%d/%m/%Y} : {}'.format(day, day_by_rep.get(day, 0)))
