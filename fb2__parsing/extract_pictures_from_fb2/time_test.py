7ad1b04192b21ca2cbcb2f92b06f898b7755f4f1
ipetrash <ilya.petrash@inbox.ru>
2018-08-15 17:32:03 +0500

    Append fb2__parsing\extract_pictures_from_fb2\fb2_pictures__using_xml_sax.py

--- fb2__parsing/extract_pictures_from_fb2/time_test.py
+++ fb2__parsing/extract_pictures_from_fb2/time_test.py
@@ -5,9 +5,10 @@ __author__ = 'ipetrash'
 
 
 from fb2_pictures__using_lxml import do as do_lxml
-from fb2_pictures__using_re import do as do_using_re
 from fb2_pictures__using_xml_expat import do as do_xml_expat
 from fb2_pictures__using_xml_etree import do as do_xml_etree
+from fb2_pictures__using_xml_sax import do as do_xml_sax
+from fb2_pictures__using_re import do as do_using_re
 
 
 file_name = '../input/Непутевый ученик в школе магии 1. Зачисление в школу (Часть 1).fb2'
@@ -17,4 +18,5 @@ from timeit import timeit
 print('LXML:      {:.3f} secs'.format(timeit('do_lxml(file_name, False)', globals=globals(), number=count)))
 print('XML EXPAT: {:.3f} secs'.format(timeit('do_xml_expat(file_name, False)', globals=globals(), number=count)))
 print('XML.ETREE: {:.3f} secs'.format(timeit('do_xml_etree(file_name, False)', globals=globals(), number=count)))
+print('XML SAX:   {:.3f} secs'.format(timeit('do_xml_sax(file_name, False)', globals=globals(), number=count)))
 print('RE:        {:.3f} secs'.format(timeit('do_using_re(file_name, False)', globals=globals(), number=count)))
