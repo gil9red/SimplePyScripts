094928d51b51dbac1b56efb314f5de845bd13783
ipetrash <ilya.petrash@inbox.ru>
2018-08-15 21:32:39 +0500

    #

--- fb2__parsing/extract_pictures_from_fb2/time_test.py
+++ fb2__parsing/extract_pictures_from_fb2/time_test.py
@@ -5,6 +5,7 @@ __author__ = 'ipetrash'
 
 
 from fb2_pictures__using_lxml import do as do_lxml
+from fb2_pictures__using_bs4 import do as do_bs4
 from fb2_pictures__using_xml_expat import do as do_xml_expat
 from fb2_pictures__using_xml_etree import do as do_xml_etree
 from fb2_pictures__using_xml_sax import do as do_xml_sax
@@ -17,13 +18,15 @@ count = 10
 from timeit import timeit
 
 runs = [
-    ('LXML',      'do_lxml(file_name, debug=False)'),
-    ('XML EXPAT', 'do_lxml(file_name, debug=False)'),
-    ('XML.ETREE', 'do_lxml(file_name, debug=False)'),
-    ('XML SAX',   'do_xml_sax(file_name, debug=False)'),
-    ('REGEXP',    'do_using_re(file_name, debug=False)'),
+    ('LXML',      'do_lxml'),
+    ('XML EXPAT', 'do_xml_expat'),
+    ('XML.ETREE', 'do_xml_etree'),
+    ('XML SAX',   'do_xml_sax'),
+    ('REGEXP',    'do_using_re'),
+    ('BS4',       'do_bs4'),
 ]
 runs_format = '{:%s} | {:.3f} secs' % (max(len(x[0]) for x in runs),)
 
 for name, stmt in runs:
-    print(runs_format.format(name, timeit(stmt, globals=globals(), number=count)))
+    timing = timeit(stmt + '(file_name, debug=False)', globals=globals(), number=count)
+    print(runs_format.format(name, timing))
