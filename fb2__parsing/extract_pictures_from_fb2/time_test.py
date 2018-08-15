d3bb67f255d200404af846fbf8f2c4df6a3edded
ipetrash <ilya.petrash@inbox.ru>
2018-08-15 16:32:22 +0500

    #

--- fb2__parsing/extract_pictures_from_fb2/time_test.py
+++ fb2__parsing/extract_pictures_from_fb2/time_test.py
@@ -4,15 +4,15 @@
 __author__ = 'ipetrash'
 
 
-from fb2_pictures__using_lxml import do as do1
-from fb2_pictures__using_re import do as do2
-from fb2_pictures__using_xml_expat import do as do3
+from fb2_pictures__using_lxml import do as do_lxml
+from fb2_pictures__using_re import do as do_using_re
+from fb2_pictures__using_xml_expat import do as do_xml_expat
 
 
 file_name = '../input/Непутевый ученик в школе магии 1. Зачисление в школу (Часть 1).fb2'
 count = 10
 
 from timeit import timeit
-print('LXML:      {:.3f} secs'.format(timeit('do1(file_name, False)', globals=globals(), number=count)))
-print('XML EXPAT: {:.3f} secs'.format(timeit('do3(file_name, False)', globals=globals(), number=count)))
-print('RE:        {:.3f} secs'.format(timeit('do2(file_name, False)', globals=globals(), number=count)))
+print('LXML:      {:.3f} secs'.format(timeit('do_lxml(file_name, False)', globals=globals(), number=count)))
+print('XML EXPAT: {:.3f} secs'.format(timeit('do_xml_expat(file_name, False)', globals=globals(), number=count)))
+print('RE:        {:.3f} secs'.format(timeit('do_using_re(file_name, False)', globals=globals(), number=count)))
