f097166943d09326d6d0119b29cf233aa93f351f
ipetrash <ilya.petrash@inbox.ru>
2018-08-15 15:47:18 +0500

    #

--- xml.parsers.expat__examples__like_sax/hello_world.py
+++ xml.parsers.expat__examples__like_sax/hello_world.py
@@ -11,22 +11,22 @@ import xml.parsers.expat
 
 
 # 3 handler functions
-def start_element(name, attrs):
+def on_start_element(name, attrs):
     print('Start element:', name, attrs)
 
 
-def end_element(name):
+def on_end_element(name):
     print('End element:', name)
 
 
-def char_data(data):
+def on_char_data(data):
     print('Character data:', repr(data))
 
 
 p = xml.parsers.expat.ParserCreate()
-p.StartElementHandler = start_element
-p.EndElementHandler = end_element
-p.CharacterDataHandler = char_data
+p.StartElementHandler = on_start_element
+p.EndElementHandler = on_end_element
+p.CharacterDataHandler = on_char_data
 
 p.Parse("""\
 <?xml version="1.0"?>
