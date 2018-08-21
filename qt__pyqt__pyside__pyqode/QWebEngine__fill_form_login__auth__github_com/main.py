4a701708d71a1cb653f463a67c4d0b742c7c2bf3
ipetrash <ilya.petrash@inbox.ru>
2018-08-22 01:32:51 +0500

    Append qt__pyqt__pyside__pyqode\QWebEngine__fill_form_login__auth__github_com

---
+++ qt__pyqt__pyside__pyqode/QWebEngine__fill_form_login__auth__github_com/main.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+
+
+LOGIN = '<LOGIN>'
+PASSWORD = '<PASSWORD>'
+
+
+with open('../QWebEngine__append_custom_javascript__jQuery/js/jquery-3.1.1.min.js') as f:
+    jquery_text = f.read()
+    jquery_text += "\nvar qt = { 'jQuery': jQuery.noConflict(true) };"
+
+
+app = QApplication([])
+
+view = QWebEngineView()
+view.load(QUrl('https://github.com/login'))
+
+
+def _on_load_finished(ok: bool):
+    page = view.page()
+
+    url = page.url().toString()
+    print(url)
+
+    if not url.endswith('login'):
+        return
+
+    page.runJavaScript(jquery_text)
+
+    page.runJavaScript(f"""
+    qt.jQuery('#login_field').val('{LOGIN}');
+    qt.jQuery('#password').val('{PASSWORD}');
+    
+    qt.jQuery('input[name="commit"]').click();
+    """)
+
+    print()
+
+
+view.loadProgress.connect(lambda value: view.setWindowTitle('{} ({}%)'.format(view.url(), value)))
+view.loadFinished.connect(_on_load_finished)
+
+mw = QMainWindow()
+mw.setCentralWidget(view)
+mw.resize(500, 500)
+mw.show()
+
+app.exec()
