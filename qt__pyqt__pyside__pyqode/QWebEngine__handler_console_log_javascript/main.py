79af9e3e64b1b1467a6286c26cdd74e1ff9f97ba
ipetrash <ilya.petrash@inbox.ru>
2018-08-21 21:58:57 +0500

    Append qt__pyqt__pyside__pyqode\QWebEngine__handler_console_log_javascript

---
+++ qt__pyqt__pyside__pyqode/QWebEngine__handler_console_log_javascript/main.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage
+
+import sys
+
+
+class MyWebEnginePage(QWebEnginePage):
+    def javaScriptConsoleMessage(self, level: 'JavaScriptConsoleMessage', message: str, lineNumber: int, sourceID: str):
+        print(f'javascript_console_message: {level}, {message}, {lineNumber}, {sourceID}', file=sys.stderr)
+
+
+app = QApplication([])
+
+page = MyWebEnginePage()
+page.load(QUrl('https://ya.ru'))
+
+view = QWebEngineView()
+view.setPage(page)
+
+
+def _on_load_finished(ok: bool):
+    page = view.page()
+    print(page.url())
+
+    page.runJavaScript('console.log(this)')
+    page.runJavaScript('console.log("Hello World!")')
+    page.runJavaScript('console.log(2 + 2 * 2)')
+
+
+view.loadProgress.connect(lambda value: view.setWindowTitle('{} ({}%)'.format(view.url(), value)))
+view.loadFinished.connect(_on_load_finished)
+
+mw = QMainWindow()
+mw.setCentralWidget(view)
+mw.resize(500, 500)
+mw.show()
+
+app.exec()
