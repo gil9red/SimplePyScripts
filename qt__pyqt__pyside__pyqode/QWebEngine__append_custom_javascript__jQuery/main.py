cb4b571e2cb2639ca572c2a599ebf008bfe6cec7
ipetrash <ilya.petrash@inbox.ru>
2018-08-21 20:52:40 +0500

    Append qt__pyqt__pyside__pyqode\QWebEngine__append_custom_javascript__jQuery

---
+++ qt__pyqt__pyside__pyqode/QWebEngine__append_custom_javascript__jQuery/main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+
+
+app = QApplication([])
+
+
+with open('js/jquery-3.1.1.min.js') as f:
+    jquery_text = f.read()
+    jquery_text += "\nvar qt = { 'jQuery': jQuery.noConflict(true) };"
+
+
+view = QWebEngineView()
+
+
+def _on_load_finished(ok: bool):
+    view.page().runJavaScript(jquery_text)
+
+    # Test jQuery
+    view.page().runJavaScript("qt.jQuery('a').css('background-color', 'yellow');")
+
+
+view.loadFinished.connect(_on_load_finished)
+
+view.load(QUrl('https://store.steampowered.com/'))
+view.show()
+
+
+app.exec()
