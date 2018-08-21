8d68e644352f523d17233cc3f89baaf0bef655e7
ipetrash <ilya.petrash@inbox.ru>
2018-08-22 01:51:12 +0500

    #

--- qt__pyqt__pyside__pyqode/render_image_from_svg/main.py
+++ qt__pyqt__pyside__pyqode/render_image_from_svg/main.py
@@ -7,24 +7,28 @@ __author__ = 'ipetrash'
 # SOURCE: https://stackoverflow.com/a/8551810/5909792
 
 
-from PyQt5 import Qt
+from PyQt5.Qt import QApplication, QSvgRenderer, QImage, Qt, QPainter
+
 
 # A QApplication instance is necessary if fonts are used in the SVG
-app = Qt.QApplication([])
+app = QApplication([])
 
 # Load your SVG
-renderer = Qt.QSvgRenderer("input.svg")
+renderer = QSvgRenderer("input.svg")
 
 for width, height in [(32, 32), (64, 64), (512, 512), (4096, 4096)]:
     # Prepare a QImage with desired characteritisc
-    image = Qt.QImage(width, height, Qt.QImage.Format_ARGB32)
+    image = QImage(width, height, QImage.Format_ARGB32)
 
     # Partly transparent red-ish background
-    image.fill(Qt.Qt.transparent)
+    image.fill(Qt.transparent)
 
     # Get QPainter that paints to the image
-    painter = Qt.QPainter(image)
+    painter = QPainter(image)
     renderer.render(painter)
 
     # Save, image format based on file extension
     image.save("output_{}x{}.png".format(width, height))
+
+    # FIX error: "Process finished with exit code -1073741819 (0xC0000005)"
+    painter.end()
