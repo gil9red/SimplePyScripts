12dfbad3eb3d9e0186db327f09dd38d41ab9a079
ipetrash <ilya.petrash@inbox.ru>
2018-08-21 20:30:20 +0500

    Append qt__pyqt__pyside__pyqode/pyqt5__one_signal_slot_for_many_buttons__QButtonGroup.py

---
+++ qt__pyqt__pyside__pyqode/pyqt5__one_signal_slot_for_many_buttons__QButtonGroup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.Qt import QApplication, QGroupBox, QCheckBox, QButtonGroup, QGridLayout
+
+
+def _on_button_clicked(button: QCheckBox):
+    print(button, button.text(), button.isChecked())
+
+
+app = QApplication([])
+
+button_group = QButtonGroup()
+button_group.setExclusive(False)
+button_group.buttonClicked.connect(_on_button_clicked)
+
+buttons_array = [
+    [QCheckBox('a1'), QCheckBox('a2'), QCheckBox('a3')],
+    [QCheckBox('b1'), QCheckBox('b2'), QCheckBox('b3')],
+    [QCheckBox('c1'), QCheckBox('c2'), QCheckBox('c3')],
+]
+
+main_layout = QGridLayout()
+
+for i, row in enumerate(buttons_array):
+    for j, button in enumerate(row):
+        button_group.addButton(button)
+        main_layout.addWidget(button, i, j)
+
+mw = QGroupBox()
+mw.setLayout(main_layout)
+mw.show()
+
+app.exec()
