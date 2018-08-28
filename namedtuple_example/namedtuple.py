232e2985e36a61408d3bf5b06667c3c038a57664
ipetrash <ilya.petrash@inbox.ru>
2018-08-28 16:51:42 +0500

    #

--- namedtuple_example/namedtuple.py
+++ namedtuple_example/namedtuple.py
@@ -1,21 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 __author__ = 'ipetrash'
 
 
-if __name__ == '__main__':
-    # TODO: https://docs.python.org/3/library/collections.html#collections.namedtuple_example
-    # Basic example
-    from collections import namedtuple
-    Point = namedtuple('Point', ['x', 'y'])
-    p = Point(11, y=22)  # instantiate with positional or keyword arguments
-    print(p[0] + p[1])   # indexable like the plain tuple (11, 22)
-    x, y = p             # unpack like a regular tuple
-    print(x, y)
-    print(p.x + p.y)     # fields also accessible by name
-    print(p)             # readable __repr__ with a name=value style
+# TODO: https://docs.python.org/3/library/collections.html#collections.namedtuple_example
+# Basic example
+from collections import namedtuple
+Point = namedtuple('Point', ['x', 'y'])
+p = Point(11, y=22)  # instantiate with positional or keyword arguments
+print(p[0] + p[1])   # indexable like the plain tuple (11, 22)
+x, y = p             # unpack like a regular tuple
+print(x, y)
+print(p.x + p.y)     # fields also accessible by name
+print(p)             # readable __repr__ with a name=value style
 
-    print()
-    XYZPoint = namedtuple('XYZPoint', ['x', 'y', 'z'])
-    xyz = XYZPoint(2, 2, z=5)
-    print(xyz)
-    print("z:", xyz.z)
-    print("Points: x: {x}, y: {y}, z: {z}".format(**xyz.__dict__))
+print()
+XYZPoint = namedtuple('XYZPoint', ['x', 'y', 'z'])
+xyz = XYZPoint(2, 2, z=5)
+print(xyz)
+print("z:", xyz.z)
+print("Points: x: {x}, y: {y}, z: {z}".format(**xyz._asdict()))
