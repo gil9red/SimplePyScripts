4a4d403e3d7d2ad14d38feb615a5d24968b51105
ipetrash <ilya.petrash@inbox.ru>
2018-08-28 16:55:34 +0500

    Append namedtuple__typing__example

---
+++ namedtuple__typing__example/main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from collections import namedtuple
+
+
+Point = namedtuple('Point', ['x', 'y'])
+
+# # TypeError: __new__() missing 2 required positional arguments: 'x' and 'y'
+# p = Point()
+# print(p)
+
+p = Point(1, 2)
+print(p)
+print()
+
+
+from typing import NamedTuple
+
+
+class Point(NamedTuple):
+    x: int = 0
+    y: int = 0
+
+
+p = Point()
+print(p)
+
+p = Point(1, 2)
+print(p)
