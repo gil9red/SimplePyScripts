a58c09207d90ae2652896b6a49dfb78da637cc4e
ipetrash <ilya.petrash@inbox.ru>
2018-08-29 06:52:49 +0500

    Append photon__crawler_spider__examples

---
+++ photon__crawler_spider__examples/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://github.com/s0md3v/Photon/wiki/Photon-Library
+# pip install photon
+import photon
+photon.crawl('https://github.com/s0md3v/Photon/wiki/Photon-Library')
+result = photon.result()
+print(result)
+
+with open('result.json', 'w', encoding='utf-8') as f:
+    import json
+    json.dump(result, f, indent=4, ensure_ascii=False)
