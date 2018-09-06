40979071c504d1d24ef1338b09d1817cbff9e918
ipetrash <ilya.petrash@inbox.ru>
2018-09-07 01:58:06 +0500

    Append datetime__current_week_number.py

---
+++ datetime__current_week_number.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import datetime as DT
+
+week_number = DT.datetime.now().isocalendar()[1]
+print('week_number:', week_number)
