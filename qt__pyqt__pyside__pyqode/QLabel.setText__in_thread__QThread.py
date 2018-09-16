d2a6b2f8f2d13f1d38c140f8bd42a1911275a74a
ipetrash <ilya.petrash@inbox.ru>
2018-09-16 20:28:13 +0500

    Append qt__pyqt__pyside__pyqode/QLabel.setText__in_thread__QThread.py

---
+++ qt__pyqt__pyside__pyqode/QLabel.setText__in_thread__QThread.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import time
+
+from PyQt5.Qt import QMainWindow, QLabel, QFont, QApplication, Qt, QThread
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.label = QLabel()
+        self.label.setAlignment(Qt.AlignCenter)
+        self.label.setFont(QFont('Courier', 25))
+
+        def loop():
+            i = 0
+
+            while True:
+                self.label.setText(str(i))
+
+                i += 1
+                time.sleep(1)
+
+        class Thread(QThread):
+            def run(self):
+                loop()
+
+        self.thread = Thread()
+        self.thread.start()
+
+        self.setCentralWidget(self.label)
+
+
+app = QApplication([])
+
+mw = MainWindow()
+mw.show()
+
+app.exec()
