dedade06c050716f3e97ca16e5189b02080abe71
gil9red <ilya.petrash@inbox.ru>
2018-09-28 16:33:52 +0500

    Append parse_WSDL/print__operation.py

---
+++ parse_WSDL/print__operation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from lxml import etree
+root = etree.parse(open('smev-message-exchange-service-1.3.wsdl', 'rb'))
+
+for operation_node in root.xpath('//*[local-name()="portType"]/*[local-name()="operation"]'):
+    print(operation_node.attrib['name'])
+
+    text = operation_node.xpath('./*[local-name()="documentation"]/text()')[0]
+    text = ' '.join(filter(None, map(str.strip, text.split('\n'))))
+    print(f'    "{text}"')
+    print()
