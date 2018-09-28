67662f04c4f06bf9accd4489b9fd0ef22fafb4ff
gil9red <ilya.petrash@inbox.ru>
2018-09-28 17:13:24 +0500

    Append zeep_examples__WSDL/hello_world.py

---
+++ zeep_examples__WSDL/hello_world.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://github.com/mvantellingen/python-zeep
+
+
+# pip install zeep
+from zeep import Client
+
+# client = Client('http://www.webservicex.net/ConvertSpeed.asmx?WSDL')
+# result = client.service.ConvertSpeed(100, 'kilometersPerhour', 'milesPerhour')
+#
+# print(result)
+# assert result == 62.137
+
+
+client = Client("http://regexlib.com/WebServices.asmx?WSDL")
+
+result = client.service.ListAllAsXml(maxrows=4)
+print(result)
