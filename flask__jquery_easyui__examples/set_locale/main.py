6feafe9d9c0aeb370c792d17d78715147db8109d
gil9red <ilya.petrash@inbox.ru>
2018-10-01 16:15:05 +0500

    Append flask__jquery_easyui__examples\set_locale

---
+++ flask__jquery_easyui__examples/set_locale/main.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: http://www.jeasyui.com/tutorial/index.php
+# SOURCE: http://www.jeasyui.com/tutorial/app/crud.php
+
+
+from flask import Flask, render_template_string
+app = Flask(__name__, static_folder='../static')
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+
+@app.route("/")
+def index():
+    return render_template_string("""\
+<html>
+<head>
+    <meta content='text/html; charset=UTF-8' http-equiv='Content-Type'/>
+    <title>set_locale</title>
+
+    <link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='js/jquery-easyui-1.6.3/themes/default/easyui.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='js/jquery-easyui-1.6.3/themes/icon.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='js/jquery-easyui-1.6.3/themes/color.css') }}">
+    
+    <script type="text/javascript" src="{{ url_for('static', filename='js/jquery-easyui-1.6.3/jquery.min.js') }}"></script>
+    <script type="text/javascript" src="{{ url_for('static', filename='js/jquery-easyui-1.6.3/jquery.easyui.min.js') }}"></script>
+    
+    <!-- VARIANT 1 -->
+    <script type="text/javascript" src="{{ url_for('static', filename='js/jquery-easyui-1.6.3/locale/easyui-lang-ru.js') }}"></script>
+</head>
+<body>
+    <!-- VARIANT 2 -->
+    <script type="text/javascript" src="{{ url_for('static', filename='js/jquery-easyui-1.6.3/easyloader.js') }}"></script>
+    <script type="text/javascript">
+        $('document').ready(function() {
+            easyloader.locale = 'ru';
+        });
+    </script>
+
+    <div class="easyui-calendar" style="width:250px;height:250px;"></div>
+</body>
+</html>
+    """)
+
+
+if __name__ == '__main__':
+    # app.debug = True
+
+    # Localhost
+    # port=0 -- random free port
+    # app.run(port=0)
+    app.run(
+        port=5000,
+
+        # :param threaded: should the process handle each request in a separate
+        #                  thread?
+        # :param processes: if greater than 1 then handle each request in a new process
+        #                   up to this maximum number of concurrent processes.
+        threaded=True,
+    )
+
+    # # Public IP
+    # app.run(host='0.0.0.0')
