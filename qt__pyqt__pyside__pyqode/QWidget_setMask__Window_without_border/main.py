a7e1b7ce56a48375e55f28ad13dfd93db1004817
gil9red <ilya.petrash@inbox.ru>
2018-10-02 16:08:36 +0500

    Append qt__pyqt__pyside__pyqode\QWidget_setMask__Window_without_border

---
+++ qt__pyqt__pyside__pyqode/QWidget_setMask__Window_without_border/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.Qt import QApplication, QPixmap, QLabel
+
+
+app = QApplication([])
+
+pix = QPixmap("pipe.png")
+
+win = QLabel()
+win.setPixmap(pix)
+win.setMask(pix.mask())
+win.show()
+
+app.exec()
