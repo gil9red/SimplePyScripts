4544f6cf0a06cebad3db7fc2c8836a8a45babe4a
ipetrash <ilya.petrash@inbox.ru>
2018-10-04 23:49:29 +0500

    #

--- https_гибдд_рф__crash_statistics/main.py
+++ https_гибдд_рф__crash_statistics/main.py
@@ -5,6 +5,7 @@ __author__ = 'ipetrash'
 
 
 import time
+import traceback
 from common import init_db, append_crash_statistics_db
 
 
@@ -12,7 +13,14 @@ init_db()
 
 
 while True:
-    append_crash_statistics_db()
+    try:
+        append_crash_statistics_db()
 
-    # Every 12 hours
-    time.sleep(12 * 60 * 60)
+        # Every 12 hours
+        time.sleep(12 * 60 * 60)
+
+    except Exception as e:
+        print('ERROR: {}:\n\n{}'.format(e, traceback.format_exc()))
+        print('Wait 5 minutes.')
+
+        time.sleep(60 * 5)
