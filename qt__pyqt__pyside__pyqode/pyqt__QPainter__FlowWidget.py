b548889bc26c19f620f937c09265cb29c83fad6d
gil9red <ilya.petrash@inbox.ru>
2018-10-08 18:24:18 +0500

    Append qt__pyqt__pyside__pyqode/pyqt__QPainter__FlowWidget.py

---
+++ qt__pyqt__pyside__pyqode/pyqt__QPainter__FlowWidget.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.Qt import *
+
+
+def log_uncaught_exceptions(ex_cls, ex, tb):
+    text = '{}: {}:\n'.format(ex_cls.__name__, ex)
+    import traceback
+    text += ''.join(traceback.format_tb(tb))
+
+    print(text)
+    QMessageBox.critical(None, 'Error', text)
+    quit()
+
+
+import sys
+sys.excepthook = log_uncaught_exceptions
+
+
+class FlowWidget(QWidget):
+    def __init__(self, length=25):
+        super().__init__()
+
+        self.cell_size = 30
+        self.column_count = 10
+        self.items = list(range(1, length + 1))
+
+    def minimumSizeHint(self):
+        try:
+            height = (len(self.items) // self.column_count + 1) * self.cell_size
+            return QSize(self.minimumWidth(), height)
+
+        except ZeroDivisionError:
+            return super().minimumSizeHint()
+
+    def minimumSize(self):
+        return self.minimumSizeHint()
+
+    def showEvent(self, event):
+        super().showEvent(event)
+
+        self.updateGeometry()
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+
+        self.column_count = self.width() // self.cell_size
+
+    def paintEvent(self, event: QPaintEvent):
+        painter = QPainter(self)
+        painter.setPen(Qt.black)
+        painter.setBrush(Qt.white)
+
+        for index, num in enumerate(self.items):
+            num = str(num)
+
+            # Превращаем индекс в координаты
+            i = index % self.column_count
+            j = index // self.column_count
+
+            x = i * self.cell_size
+            y = j * self.cell_size
+            w, h = self.cell_size, self.cell_size
+
+            painter.drawRect(x, y, w, h)
+            painter.drawText(x, y, w, h, Qt.AlignCenter, num)
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    simple = FlowWidget()
+    simple.setWindowTitle('Simple')
+    simple.resize(300, 300)
+    simple.show()
+
+    def get_QScrollArea_with_FlowWidget(length):
+        scroll = QScrollArea()
+        scroll.setWidget(FlowWidget(length))
+        scroll.setWidgetResizable(True)
+        return scroll
+
+    mw = QTabWidget()
+    mw.setWindowTitle('More examples')
+    mw.addTab(get_QScrollArea_with_FlowWidget(25), '25')
+    mw.addTab(get_QScrollArea_with_FlowWidget(100), '100')
+    mw.addTab(get_QScrollArea_with_FlowWidget(250), '250')
+    mw.addTab(get_QScrollArea_with_FlowWidget(500), '500')
+    mw.addTab(get_QScrollArea_with_FlowWidget(1000), '1000')
+    mw.resize(800, 500)
+    mw.show()
+
+    app.exec()
