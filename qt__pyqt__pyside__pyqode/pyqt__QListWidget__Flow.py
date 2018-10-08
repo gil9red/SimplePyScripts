83d3d9f3cfb3f7af46af7c8e3d2d3fe6a17342b6
gil9red <ilya.petrash@inbox.ru>
2018-10-08 18:39:19 +0500

    Append qt__pyqt__pyside__pyqode/pyqt__QListWidget__Flow.py

---
+++ qt__pyqt__pyside__pyqode/pyqt__QListWidget__Flow.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.Qt import *
+
+
+app = QApplication([])
+
+list_widget = QListWidget()
+list_widget.addItems(map(str, range(100)))
+list_widget.setFlow(QListView.LeftToRight)
+list_widget.setWrapping(True)
+list_widget.setUniformItemSizes(True)
+list_widget.show()
+
+app.exec()
