11fd9f14fe04ec2e1fb12b2658511072f5c930dd
gil9red <ilya.petrash@inbox.ru>
2018-10-09 18:47:32 +0500

    Если вместо версии указана программа, то считаем что нам нужен trunk

--- tool_for_run_project/go.py
+++ tool_for_run_project/go.py
@@ -27,40 +27,8 @@ WHAT_BY_FILE = {
     's': '!!server.cmd',
 }
 
-argc = len(sys.argv)
 
-if argc == 1:
-    print('''\
-RUN:
-  <name> <version> <what>  -- Run tool
-  <name> <version>         -- Open dir version
-  <name> open              -- Open dir
-  <name>                   -- Print versions
-
-SUPPORTED NAMES:
-{}
-
-EXAMPLES:
-  > optt trunk designer
-    Run: "C:/DEV__OPTT/trunk_optt/!!designer.cmd"
-    
-  > tx 3.2.6.10 server
-    Run: "C:/DEV__TX/3.2.6.10/!!server.cmd"
-  
-  > optt trunk
-    Open: "C:/DEV__OPTT/trunk_optt"
-   
-  > optt open
-    Open: "C:/DEV__OPTT"
-    
-  > optt
-    Version: ['2.1.7.1', 'trunk_optt']
-'''.format('\n'.join('  {:<10} {}'.format(k, v) for k, v in sorted(NAME_BY_PATH.items()))))
-    quit()
-
-if argc == 4:
-    name, version, what = map(str.lower, sys.argv[1:])
-
+def go_run(name: str, version: str, what: str):
     if version == 'trunk':
         version += '_' + name
 
@@ -75,9 +43,8 @@ if argc == 4:
     # Run
     os.startfile(file_name)
 
-elif argc == 3:
-    name, version = map(str.lower, (sys.argv[1], sys.argv[2]))
 
+def go_open(name: str, version: str):
     dir_file_name = NAME_BY_PATH[name]
 
     # Для команды open версия не нужна
@@ -92,9 +59,8 @@ elif argc == 3:
     # Open
     os.startfile(dir_file_name)
 
-elif argc == 2:
-    name = sys.argv[1].lower()
 
+def go_print_versions(name: str):
     if name == 'manager':
         dir_file_name = NAME_BY_PATH[name]
         file_name = dir_file_name + '/manager/bin/manager64.exe'
@@ -122,3 +88,57 @@ elif argc == 2:
                 dirs.append(path)
 
         print('Version:', dirs)
+
+
+argc = len(sys.argv)
+
+if argc == 1:
+    print('''\
+RUN:
+  <name> <version> <what>  -- Run tool
+  <name> <what>            -- Run tool (trunk version)
+  <name> <version>         -- Open dir version
+  <name> open              -- Open dir
+  <name>                   -- Print versions
+
+SUPPORTED NAMES:
+{}
+
+EXAMPLES:
+  > optt trunk designer
+    Run: "C:/DEV__OPTT/trunk_optt/!!designer.cmd"
+    
+  > tx 3.2.6.10 server
+    Run: "C:/DEV__TX/3.2.6.10/!!server.cmd"
+  
+  > go tx designer
+    Run: "C:/DEV__TX/trunk_tx/!!designer.cmd"
+  
+  > optt trunk
+    Open: "C:/DEV__OPTT/trunk_optt"
+   
+  > optt open
+    Open: "C:/DEV__OPTT"
+    
+  > optt
+    Version: ['2.1.7.1', 'trunk_optt']
+'''.format('\n'.join('  {:<10} {}'.format(k, v) for k, v in sorted(NAME_BY_PATH.items()))))
+    quit()
+
+
+if argc == 4:
+    name, version, what = map(str.lower, sys.argv[1:])
+    go_run(name, version, what)
+
+elif argc == 3:
+    name, version = map(str.lower, (sys.argv[1], sys.argv[2]))
+
+    # Если вместо версии указана программа, то считаем что нам нужен trunk
+    if version in WHAT_BY_FILE:
+        go_run(name, 'trunk', version)
+    else:
+        go_open(name, version)
+
+elif argc == 2:
+    name = sys.argv[1].lower()
+    go_print_versions(name)
