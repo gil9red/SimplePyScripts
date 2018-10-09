8a34766c21779fa43e05581e7463571f28322287
gil9red <ilya.petrash@inbox.ru>
2018-10-09 18:38:37 +0500

    Append regexp_examples/sub__with_callback_on_match__random_pattern_text.py

---
+++ regexp_examples/sub__with_callback_on_match__random_pattern_text.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import re
+import random
+
+
+def random_text(text: str) -> str:
+    def _on_sub_process(match) -> str:
+        # "Дима,Вася,Игорь" -> ["Дима", "Вася", "Игорь"]
+        variants = match.group(1).split(',')
+        return random.choice(variants)
+
+    # В регулярке выполняется замена выражений внутри круглых скобок
+    return re.sub(r'\((.+?)\)', _on_sub_process, text)
+
+
+text = "Привет, (Дима,Вася,Игорь)! (Как дела?,Как настроение?,Как жизнь?) Пойдешь (завтра,сегодня,на следующей неделе) на тренировку?"
+new_text = random_text(text)
+print(new_text)  # Привет, Игорь! Как настроение? Пойдешь сегодня на тренировку?
+
+new_text = random_text(text)
+print(new_text)  # Привет, Дима! Как жизнь? Пойдешь сегодня на тренировку?
