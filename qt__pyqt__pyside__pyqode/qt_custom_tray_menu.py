31954eaa1ea624d2585c43f20ec9f93c3a1ec284
ipetrash <ilya.petrash@inbox.ru>
2018-10-13 19:43:21 +0500

    #

--- qt__pyqt__pyside__pyqode/qt_custom_tray_menu.py
+++ qt__pyqt__pyside__pyqode/qt_custom_tray_menu.py
@@ -4,30 +4,27 @@
 __author__ = 'ipetrash'
 
 
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
+from PyQt5.Qt import QApplication, QSystemTrayIcon, QStyle, QLabel, QWidgetAction, QMenu
 
 
-if __name__ == '__main__':
-    app = QApplication([])
-    app.setQuitOnLastWindowClosed(False)
+app = QApplication([])
+app.setQuitOnLastWindowClosed(False)
 
-    icon = app.style().standardIcon(QStyle.SP_DirOpenIcon)
-    # icon = QIcon('favicon.ico')
+icon = app.style().standardIcon(QStyle.SP_DirOpenIcon)
+# icon = QIcon('favicon.ico')
 
-    tray = QSystemTrayIcon(icon)
+tray = QSystemTrayIcon(icon)
 
-    menu_widget = QLabel('<b>Hello</b>, World!')
-    menu_widget_action = QWidgetAction(menu_widget)
-    menu_widget_action.setDefaultWidget(menu_widget)
+menu_widget = QLabel('<b>Hello</b>, World!')
+menu_widget_action = QWidgetAction(menu_widget)
+menu_widget_action.setDefaultWidget(menu_widget)
 
-    menu = QMenu()
-    menu.addAction(menu_widget_action)
+menu = QMenu()
+menu.addAction(menu_widget_action)
 
-    tray.setContextMenu(menu)
-    tray.activated.connect(lambda x: menu.exec(tray.geometry().center()))
+tray.setContextMenu(menu)
+tray.activated.connect(lambda x: menu.exec(tray.geometry().center()))
 
-    tray.show()
+tray.show()
 
-    app.exec()
+app.exec()
