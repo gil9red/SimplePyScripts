08247f9986afddd1c188e70256aa7cebfab595c1
ipetrash <ilya.petrash@inbox.ru>
2018-10-16 23:22:32 +0500

    Append using_tor/load_onion.py

---
+++ using_tor/load_onion.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# About socks5h: https://stackoverflow.com/a/43823166/5909792
+
+# NOTE: tor должен быть запущен
+# pip install -U requests[socks]
+import requests
+
+# Порт может быть и 9050, и 9150. Нужно смотреть настройки tor. Tor можно через Tor Browser запускать,
+# в настройках сети Tor браузера будет указан порт tor
+proxies = {
+    'http': 'socks5h://127.0.0.1:9150',
+    'https': 'socks5h://127.0.0.1:9150'
+}
+
+data = requests.get("http://altaddresswcxlld.onion/", proxies=proxies).content
+print(data)
+
+data = requests.get("https://www.facebookcorewwwi.onion/", proxies=proxies).content
+print(data)
+
+data = requests.get("http://sblib3fk2gryb46d.onion/", proxies=proxies).content
+print(data)
