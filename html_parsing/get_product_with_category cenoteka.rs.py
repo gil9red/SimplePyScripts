861ff4785eb714b873b7f842b9756336ff78064e
gil9red <ilya.petrash@inbox.ru>
2018-10-17 17:50:26 +0500

    Append html_parsing/get_product_with_category cenoteka.rs.py

---
+++ html_parsing/get_product_with_category cenoteka.rs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://ru.stackoverflow.com/questions/894302/
+
+
+from bs4 import BeautifulSoup
+import requests
+
+
+url = 'https://cenoteka.rs/proizvodi/mlecni-proizvodi/jogurt?page=3'
+rs = requests.get(url)
+root = BeautifulSoup(rs.content, 'lxml')
+
+for category in root.select('#products > .row.section'):
+    category_title = category.select_one('.section-title').get_text(strip=True)
+    print(category_title)
+
+    for product in category.select('[data-product-id]'):
+        try:
+            title = product.select_one('.article-name').get_text(strip=True)
+            price_list = [price.get_text(strip=True) for price in product.select('.article-price')]
+            print('    {}: {}'.format(title, price_list))
+
+        except:
+            pass
+
+    print()
