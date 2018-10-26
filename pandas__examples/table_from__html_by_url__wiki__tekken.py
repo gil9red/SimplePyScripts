4251fe31b680bc7a6370d6b0b5165f03f2a916ec
ipetrash <ilya.petrash@inbox.ru>
2018-10-26 22:37:16 +0500

    Append pandas__examples/table_from__html_by_url__wiki__tekken.py

---
+++ pandas__examples/table_from__html_by_url__wiki__tekken.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install pandas
+import pandas as pd
+
+# pip install requests
+import requests
+
+# pip install bs4
+from bs4 import BeautifulSoup
+
+url = 'https://ru.wikipedia.org/wiki/Список_персонажей_Tekken'
+rs = requests.get(url)
+
+# Удаление сносок -- из-за них имена персонажей и значения присутствия в конкретной серии показывается
+# невалидно, например "Акума14", а не "Акума"
+root = BeautifulSoup(rs.content, 'html.parser')
+for sub in root.select('sup'):
+    sub.decompose()
+
+df_list = pd.read_html(str(root), header=0, index_col=0)
+df = df_list[0]
+
+print(df)
+
+df.to_excel('tekken_table.xlsx')
