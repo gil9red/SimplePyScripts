9250e213407eef595c5f92236565473570cd0d5a
gil9red <ilya.petrash@inbox.ru>
2018-10-26 16:33:55 +0500

    Append tkinter_examples\button__change_button_icon_on_click\main__next_image_generator.py

---
+++ tkinter_examples/button__change_button_icon_on_click/main__next_image_generator.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import tkinter as tk
+
+
+def _on_button_click(button):
+    button.config(image=next(NEXT_IMAGE))
+
+
+def next_image_generator():
+    photo_list = [
+        tk.PhotoImage(file="icons/ok.png"),
+        tk.PhotoImage(file="icons/no.png"),
+        tk.PhotoImage(file="icons/help.png"),
+    ]
+
+    while True:
+        for photo in photo_list:
+            yield photo
+
+
+NEXT_IMAGE = next_image_generator()
+
+
+root = tk.Tk()
+root.geometry('200x200')
+
+button = tk.Button(root, text="ClickMe!", image=next(NEXT_IMAGE))
+button.config(command=lambda: _on_button_click(button))
+button.pack()
+
+root.mainloop()
