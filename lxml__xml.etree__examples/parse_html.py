d487a3d3099825c645f5536c86e92a9f1a41503e
gil9red <ilya.petrash@inbox.ru>
2018-10-27 15:51:23 +0500

    #

--- lxml__xml.etree__examples/parse_html.py
+++ lxml__xml.etree__examples/parse_html.py
@@ -9,3 +9,9 @@ root = html.fromstring('<p>Hello<br>world!</p><br>')
 
 print(html.tostring(root))                      # b'<div><p>Hello<br>world!</p><br></div>'
 print(html.tostring(root, encoding='unicode'))  # <div><p>Hello<br>world!</p><br></div>
+print(html.tostring(root, pretty_print=True))   # b'<div>\n<p>Hello<br>world!</p>\n<br>\n</div>\n'
+print(html.tostring(root, encoding='unicode', pretty_print=True))
+# <div>
+# <p>Hello<br>world!</p>
+# <br>
+# </div>
