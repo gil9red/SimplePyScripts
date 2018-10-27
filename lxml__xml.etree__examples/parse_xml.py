2c6ef2a184dab4495c28c48b8fa0036f844fa251
gil9red <ilya.petrash@inbox.ru>
2018-10-27 15:53:15 +0500

    #

--- lxml__xml.etree__examples/parse_xml.py
+++ lxml__xml.etree__examples/parse_xml.py
@@ -9,3 +9,9 @@ root = etree.fromstring('<a><b>Hello</b><c>world!</c></a>')
 
 print(etree.tostring(root))                      # b'<a><b>Hello</b><c>world!</c></a>'
 print(etree.tostring(root, encoding='unicode'))  # <a><b>Hello</b><c>world!</c></a>
+print(etree.tostring(root, pretty_print=True))   # b'<a>\n  <b>Hello</b>\n  <c>world!</c>\n</a>\n'
+print(etree.tostring(root, encoding='unicode', pretty_print=True))
+# <a>
+#   <b>Hello</b>
+#   <c>world!</c>
+# </a>
