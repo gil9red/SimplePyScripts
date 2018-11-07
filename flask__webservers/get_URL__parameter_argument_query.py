df2af5b02eb0e4bb8b007bf1bc7376e2c5706837
gil9red <ilya.petrash@inbox.ru>
2018-11-07 18:51:42 +0500

    Append flask__webservers/get_URL__parameter_argument_query.py

---
+++ flask__webservers/get_URL__parameter_argument_query.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from flask import Flask, request
+app = Flask(__name__)
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+
+@app.route("/")
+def index():
+    return """
+<div id="result"></div>
+    
+<script type="text/javascript">
+function callAjax(url, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function(){
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+            callback(xmlhttp.responseText);
+        }
+    }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+callAjax('/print_args?a=1&text_en=Hello World!&text_ru=Привет Мир!&ok=true', function(responseText) {
+    document.getElementById("result").innerHTML = responseText;
+});
+</script>
+
+<br>
+<div>
+    <a href="/print_args?a=1&b=2&c=abc">/print_args?a=1&b=2&c=abc</a>
+</div>
+"""
+
+
+@app.route("/print_args")
+def print_args():
+    args = request.args
+    print(args)
+
+    text = '<table border="1px" width="300px">'
+    text += '<caption>URL ARGUMENTS:<caption>'
+    for k, v in args.items():
+        text += f'<tr><td>{k}</td><td>{v}</td></tr>'
+    text += '</table>'
+
+    return text
+
+
+if __name__ == '__main__':
+    app.run()
