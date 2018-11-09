d6e81408d049aac2a945b1f5010127a55d6802a8
gil9red <ilya.petrash@inbox.ru>
2018-11-09 11:44:56 +0500

    Append flask__webservers/post_data__as_form.py

---
+++ flask__webservers/post_data__as_form.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+
+@app.route("/")
+def index():
+    return """
+<div id="result"></div>
+<br>
+
+<form method="POST" action="/print_data" 
+    onsubmit="return submitForm(this, function(responseText) { document.getElementById('result').innerHTML = responseText; });">
+    <input name="name" value="Виктор">
+    <input name="surname" value="Цой">
+    <p><input type="submit" value="Отправить"></p>
+</form>
+
+<script type="text/javascript">
+function submitForm(form, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action, true);
+            
+    // Создать объект для формы
+    var formData = new FormData(form);
+    
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200){
+            callback(xhr.responseText);
+        }
+    }
+    
+    xhr.send(formData);
+    return false;
+}
+
+</script>
+"""
+
+
+@app.route("/print_data", methods=['POST'])
+def print_data():
+    data = request.data
+    print('data:', data)
+
+    form = request.form
+    print('form:', form)
+
+    return form['name'] + " " + form['surname']
+
+
+if __name__ == '__main__':
+    app.run()
