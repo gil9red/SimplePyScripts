03f25dbbc88a7bc302e89f2fc9e4abc598b45f56
ipetrash <ilya.petrash@inbox.ru>
2018-11-13 00:48:15 +0500

    Append bs4__BeautifulSoup__examples/about_builders.py

---
+++ bs4__BeautifulSoup__examples/about_builders.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install lxml
+# pip install html5lib
+# pip install bs4
+
+from bs4 import BeautifulSoup
+
+
+text = "<xml/>"
+
+root = BeautifulSoup(text, 'html.parser')
+print(root.builder)  # bs4.builder._htmlparser.HTMLParserTreeBuilder
+
+root = BeautifulSoup(text, 'html5lib')
+print(root.builder)  # bs4.builder._html5lib.HTML5TreeBuilder
+
+root = BeautifulSoup(text, 'xml')
+print(root.builder)  # bs4.builder._lxml.LXMLTreeBuilderForXML
+
+root = BeautifulSoup(text, 'lxml-xml')
+print(root.builder)  # bs4.builder._lxml.LXMLTreeBuilderForXML
+
+root = BeautifulSoup(text, 'lxml')
+print(root.builder)  # bs4.builder._lxml.LXMLTreeBuilder
+
+root = BeautifulSoup(text, 'lxml-html')
+print(root.builder)  # bs4.builder._lxml.LXMLTreeBuilder
