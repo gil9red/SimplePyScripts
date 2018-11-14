a4033aa445f3495d48d4e03836f28675438dda70
gil9red <ilya.petrash@inbox.ru>
2018-11-14 16:14:45 +0500

    #

---
+++ design_patterns__examples/Factory/example_2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://ru.wikipedia.org/wiki/Фабричный_метод_(шаблон_проектирования)
+
+
+from abc import ABCMeta, abstractmethod
+
+
+class Animal(metaclass=ABCMeta):
+    @staticmethod
+    def initial(animal: str) -> 'Animal':
+        if animal == 'Lion':
+            return Lion()
+        elif animal == 'Cat':
+            return Cat()
+
+        raise Exception(f'Unsupported animal "{animal}"')
+
+    @abstractmethod
+    def voice(self):
+        pass
+
+
+class Lion(Animal):
+    def voice(self):
+        print("Rrrrrrrr i'm the lion")
+
+
+class Cat(Animal):
+    def voice(self):
+        print("Meow, meow i'm the kitty")
+
+
+if __name__ == '__main__':
+    animal_1 = Animal.initial('Lion')
+    animal_2 = Animal.initial('Cat')
+
+    animal_1.voice()
+    animal_2.voice()
