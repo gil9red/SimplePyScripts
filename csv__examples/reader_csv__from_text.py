9ada6c83d4c34615a7ed236864285057f71ef794
gil9red <ilya.petrash@inbox.ru>
2018-11-14 15:42:50 +0500

    Append csv__examples/reader_csv__from_text.py

---
+++ csv__examples/reader_csv__from_text.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import csv
+
+text = """\
+id;name;address;zip
+1;vasya;moscow;11111
+2;oleg;sochi;22222
+"""
+
+# Variant 1
+csv_reader = csv.reader(text.splitlines(), delimiter=";")
+for row in csv_reader:
+    print(row)
+
+print()
+
+# Variant 2
+from io import StringIO
+
+csv_reader = csv.reader(StringIO(text), delimiter=";")
+for row in csv_reader:
+    print(row)
+
+print()
+
