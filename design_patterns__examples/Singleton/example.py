a45bf63f0514da37d764441d12ec0b9dd7a3e310
gil9red <ilya.petrash@inbox.ru>
2018-11-14 12:28:19 +0500

    Append design_patterns__examples\Singleton

---
+++ design_patterns__examples/Singleton/example.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://www.python.org/dev/peps/pep-0318/#examples
+
+
+def singleton(cls):
+    instances = dict()
+
+    def get_instance():
+        if cls not in instances:
+            instances[cls] = cls()
+        return instances[cls]
+    return get_instance
+
+
+if __name__ == '__main__':
+    @singleton
+    class MyClass:
+        pass
+
+    x1 = MyClass()
+    x2 = MyClass()
+    x3 = MyClass()
+
+    print(id(x1), id(x2), id(x3))
