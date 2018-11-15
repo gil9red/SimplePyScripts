ff5a9123467ab92ff0be223f732505aaff5922bf
gil9red <ilya.petrash@inbox.ru>
2018-11-15 11:19:45 +0500

    Append currying__example.py

---
+++ currying__example.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://ru.wikipedia.org/wiki/Каррирование
+
+
+def my_sum(x, y, z):
+    return x + y + z
+
+
+def foo(x):
+    def a(y):
+        def b(z):
+            return my_sum(x, y, z)
+        return b
+    return a
+
+
+print(foo(1)(2)(3))        # 6
+print(foo("1")("2")("3"))  # 123
