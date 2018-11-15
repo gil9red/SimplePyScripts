70f24756c88dcc61136d5bb98ab593eef3684676
gil9red <ilya.petrash@inbox.ru>
2018-11-15 11:21:36 +0500

    #

---
+++ closure__examples/hello_world.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://ru.wikipedia.org/wiki/Замыкание_(программирование)
+
+
+# multiplier возвращает функцию умножения на n
+def multiplier(n):
+    def mul(k):
+        return n * k
+
+    return mul
+
+
+# mul3 - функция, умножающая на 3
+mul3 = multiplier(3)
+print(mul3(3), mul3(5))  # 9 15
