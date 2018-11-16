c48ebcf542572feaa2a6d905316d0b134d090555
ipetrash <ilya.petrash@inbox.ru>
2018-11-16 23:28:09 +0500

    Append qbittorrent_example/print_torrent_comment.py

---
+++ qbittorrent_example/print_torrent_comment.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from common import get_client
+
+
+def get_comment(torrent):
+    return qb.get_torrent(torrent['hash'])['comment']
+
+
+qb = get_client()
+
+comment_list = []
+
+for torrent in qb.torrents():
+    comment = get_comment(torrent)
+    if comment:
+        comment_list.append(comment)
+
+comment_list.sort()
+print('\n'.join(comment_list))
