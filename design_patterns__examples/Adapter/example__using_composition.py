3e016199cf555104cb55893da8303e5b7365c18d
gil9red <ilya.petrash@inbox.ru>
2018-11-16 19:01:44 +0500

    Added design_patterns__examples\Adapter

---
+++ design_patterns__examples/Adapter/example__using_composition.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://ru.wikipedia.org/wiki/Адаптер_(шаблон_проектирования)#Python
+
+
+from abc import ABCMeta, abstractmethod
+
+
+class SourceAdapter(metaclass=ABCMeta):
+    @abstractmethod
+    def get_picture(self):
+        pass
+
+
+class GameConsole:
+    def create_game_picture(self):
+        return 'picture from console'
+
+
+class Antenna:
+    def create_wave_picture(self):
+        return 'picture from wave'
+
+
+class SourceGameConsoleAdapter(SourceAdapter):
+    def __init__(self, game_console: GameConsole):
+        self.game_console = game_console
+
+    def get_picture(self):
+        return self.game_console.create_game_picture()
+
+
+class SourceAntennaAdapter(SourceAdapter):
+    def __init__(self, antenna: Antenna):
+        self.antenna = antenna
+
+    def get_picture(self):
+        return self.antenna.create_wave_picture()
+
+
+class TV:
+    def __init__(self, source: SourceAdapter):
+        self.source = source
+
+    def show_picture(self):
+        return self.source.get_picture()
+
+
+if __name__ == '__main__':
+    g = SourceGameConsoleAdapter(GameConsole())
+    game_tv = TV(g)
+    print(game_tv.show_picture())
+
+    a = SourceAntennaAdapter(Antenna())
+    cabel_tv = TV(a)
+    print(cabel_tv.show_picture())
---
+++ design_patterns__examples/Adapter/example__using_inheritance.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://ru.wikipedia.org/wiki/Адаптер_(шаблон_проектирования)#Python
+
+
+from abc import ABCMeta, abstractmethod
+
+
+class SourceAdapter(metaclass=ABCMeta):
+    @abstractmethod
+    def get_picture(self):
+        pass
+
+
+class GameConsole:
+    def create_game_picture(self):
+        return 'picture from console'
+
+
+class Antenna:
+    def create_wave_picture(self):
+        return 'picture from wave'
+
+
+class SourceGameConsoleAdapter(SourceAdapter, GameConsole):
+    def get_picture(self):
+        return self.create_game_picture()
+
+
+class SourceAntennaAdapter(SourceAdapter, Antenna):
+    def get_picture(self):
+        return self.create_wave_picture()
+
+
+class TV:
+    def __init__(self, source: SourceAdapter):
+        self.source = source
+
+    def show_picture(self):
+        return self.source.get_picture()
+
+
+if __name__ == '__main__':
+    g = SourceGameConsoleAdapter()
+    game_tv = TV(g)
+    print(game_tv.show_picture())
+
+    a = SourceAntennaAdapter()
+    cabel_tv = TV(a)
+    print(cabel_tv.show_picture())
