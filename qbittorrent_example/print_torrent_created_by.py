5aa4c440897ec50dd024a7a51e6b11e76e662757
ipetrash <ilya.petrash@inbox.ru>
2018-11-16 23:19:57 +0500

    Append qbittorrent_example/print_torrent_created_by.py

---
+++ qbittorrent_example/print_torrent_created_by.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from common import get_client
+
+
+qb = get_client()
+
+created_by_list = set()
+
+for torrent in qb.torrents():
+    created_by = qb.get_torrent(torrent['hash'])['created_by']
+    if created_by:
+        created_by_list.add(created_by)
+
+print('\n'.join(created_by_list))
