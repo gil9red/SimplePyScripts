1c78135fe9372e49e6e3aa23c01913770ee3f5a4
ipetrash <ilya.petrash@inbox.ru>
2018-11-17 05:34:22 +0500

    #

--- robobrowser_examples/github__login_and_print_email_from_settings.py
+++ robobrowser_examples/github__login_and_print_email_from_settings.py
@@ -4,27 +4,27 @@
 __author__ = 'ipetrash'
 
 
+from robobrowser import RoboBrowser
+
+
 LOGIN = '<LOGIN>'
 PASSWORD = '<PASSWORD>'
 
 
-if __name__ == '__main__':
-    from robobrowser import RoboBrowser
+browser = RoboBrowser(
+    user_agent='Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0',
+    parser='lxml'
+)
+browser.open('https://github.com/login')
 
-    browser = RoboBrowser(
-        user_agent='Mozilla/5.0 (Windows NT 6.1; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0',
-        parser='lxml'
-    )
-    browser.open('https://github.com/login')
+signup_form = browser.get_form()
+signup_form['login'].value = LOGIN
+signup_form['password'].value = PASSWORD
 
-    signup_form = browser.get_form()
-    signup_form['login'].value = LOGIN
-    signup_form['password'].value = PASSWORD
+# Submit the form
+browser.submit_form(signup_form)
 
-    # Submit the form
-    browser.submit_form(signup_form)
+browser.open('https://github.com/settings/emails')
 
-    browser.open('https://github.com/settings/emails')
-
-    for tag in browser.select('#settings-emails > li > span.css-truncate-target'):
-        print(tag.text)
+for tag in browser.select('#settings-emails > li > span.css-truncate-target'):
+    print(tag.text)
