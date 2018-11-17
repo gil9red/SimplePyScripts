51cbe974cf850207a7cad16f74d0c1cdb134ca87
ipetrash <ilya.petrash@inbox.ru>
2018-11-17 06:57:01 +0500

    Append qbittorrent_examples/print_total_size.py

---
+++ qbittorrent_examples/print_total_size.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from common import get_client, sizeof_fmt
+
+
+qb = get_client()
+
+torrents = qb.torrents()
+total_size = sum(torrent['total_size'] for torrent in torrents)
+
+print(f'Total torrents: {len(torrents)}')
+print(f'Total size:     {sizeof_fmt(total_size)} ({total_size} bytes)')
