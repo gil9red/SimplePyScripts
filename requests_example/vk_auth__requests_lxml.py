07c29be8483e67f82bce7403a3988bdf30b3e3a9
ipetrash <ilya.petrash@inbox.ru>
2018-11-17 05:48:28 +0500

    #

--- requests_example/vk_auth__requests_lxml.py
+++ requests_example/vk_auth__requests_lxml.py
@@ -13,22 +13,12 @@ def get_text(node):
 
 
 URL = 'https://m.vk.com/'
-
-HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; rv:14.0) Gecko/20100101 Firefox/14.0.1',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Language': 'ru-ru,ru;q=0.8,en-us;q=0.5,en;q=0.3',
-    'Accept-Encoding': 'gzip, deflate',
-    'Connection': 'keep-alive',
-    'DNT': '1'
-}
-
 LOGIN = '<LOGIN>'
 PASSWORD = '<PASSWORD>'
 
 
 session = requests.session()
-rs = session.get(URL, headers=HEADERS)
+rs = session.get(URL)
 root = html.fromstring(rs.content)
 
 form = root.forms[0]
