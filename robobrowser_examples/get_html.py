912afcfb3eeb6ebf17e8225db2b2bd41e13d321a
ipetrash <ilya.petrash@inbox.ru>
2018-11-17 07:15:05 +0500

    Append robobrowser_examples/get_html.py

--- robobrowser_examples/get_html.py
+++ robobrowser_examples/get_html.py
@@ -9,8 +9,11 @@ from robobrowser import RoboBrowser
 
 browser = RoboBrowser(
     user_agent='Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0',
-    parser='lxml'
+    parser='html.parser'
 )
 browser.open('https://github.com')
-html = browser.parsed
+root = browser.parsed
+print(type(root))  # <class 'bs4.BeautifulSoup'>
+
+html = str(root)
 print(html)
