cb1202a2590a6f28e23d295ee6ffc632f64c8057
ipetrash <ilya.petrash@inbox.ru>
2018-11-19 01:36:56 +0500

    Append qbittorrent_examples/torrent_list__table_gui_pyqt.py

---
+++ qbittorrent_examples/torrent_list__table_gui_pyqt.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://github.com/qbittorrent/qbittorrent/wiki/WebUI-API-Documentation#get-torrent-list
+
+
+from common import get_client
+
+from PyQt5.Qt import *
+
+
+class TorrentInfoWidget(QTableWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setEditTriggers(QTableWidget.NoEditTriggers)
+        self.setSelectionBehavior(QTableWidget.SelectRows)
+        self.setSelectionMode(QTableWidget.SingleSelection)
+
+        headers = ['KEY', 'VALUE']
+        self.setColumnCount(len(headers))
+        self.setHorizontalHeaderLabels(headers)
+
+        self.horizontalHeader().setStretchLastSection(True)
+        self.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
+
+    def fill(self, torrent: dict):
+        while self.rowCount():
+            self.removeRow(0)
+
+        for k, v in torrent.items():
+            row = self.rowCount()
+            self.setRowCount(row + 1)
+
+            self.setItem(row, 0, QTableWidgetItem(str(k)))
+            self.setItem(row, 1, QTableWidgetItem(str(v)))
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.table_torrent = QTableWidget()
+        self.table_torrent.setEditTriggers(QTableWidget.NoEditTriggers)
+        self.table_torrent.setSelectionBehavior(QTableWidget.SelectRows)
+        self.table_torrent.setSelectionMode(QTableWidget.SingleSelection)
+        self.table_torrent.itemClicked.connect(self.fill_torrent_info)
+
+        self.torrent_info_widget = TorrentInfoWidget()
+
+        splitter = QSplitter(Qt.Vertical)
+        splitter.addWidget(self.table_torrent)
+        splitter.addWidget(self.torrent_info_widget)
+
+        self.setCentralWidget(splitter)
+
+    def fill_table(self):
+        qb = get_client()
+        torrents = qb.torrents()
+        if not torrents:
+            return
+
+        self.table_torrent.clear()
+
+        headers = torrents[0].keys()
+        self.table_torrent.setColumnCount(len(headers))
+        self.table_torrent.setHorizontalHeaderLabels(headers)
+
+        self.table_torrent.setRowCount(len(torrents))
+
+        for row, torrent in enumerate(torrents):
+            for column, key in enumerate(headers):
+                value = str(torrent[key])
+
+                item = QTableWidgetItem(value)
+                item.setData(Qt.UserRole, torrent)
+
+                self.table_torrent.setItem(row, column, item)
+
+    def fill_torrent_info(self, item: QTableWidgetItem):
+        torrent = item.data(Qt.UserRole)
+
+        qb = get_client()
+        torrent = qb.get_torrent(torrent['hash'])
+
+        self.torrent_info_widget.fill(torrent)
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    mw = MainWindow()
+    mw.resize(900, 600)
+    mw.show()
+    mw.fill_table()
+
+    app.exec()
