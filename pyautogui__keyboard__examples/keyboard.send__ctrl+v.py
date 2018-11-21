c53d6edc9eced96f9ae76d8c842cc1e3267005c8
gil9red <ilya.petrash@inbox.ru>
2018-11-21 16:39:43 +0500

    Append pyautogui__keyboard__examples/keyboard.send__ctrl+v.py

---
+++ pyautogui__keyboard__examples/keyboard.send__ctrl+v.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://github.com/boppreh/keyboard#keyboard.send
+
+# pip install keyboard
+import keyboard
+
+keyboard.send("Ctrl+V")
