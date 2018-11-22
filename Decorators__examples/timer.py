a10f2594eda904689c40dc9f690c12db2161d8c5
gil9red <ilya.petrash@inbox.ru>
2018-11-22 11:57:46 +0500

    Append Decorators__examples/timer.py

---
+++ Decorators__examples/timer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import time
+
+
+def timer(f):
+    def wrapper(*args, **kwargs):
+        t = time.time()
+        r = f(*args, **kwargs)
+        print("Время выполнения функции: %f сек." % (time.time() - t))
+        return r
+    return wrapper
+
+
+if __name__ == '__main__':
+    @timer
+    def my_sleep():
+        print(123)
+        time.sleep(0.3)
+        print(456)
+
+    my_sleep()
+
+    print()
+
+    @timer
+    def my_foo():
+        return [i for i in range(10 ** 7) if i % 2 == 0]
+
+    print(my_foo()[:20])
