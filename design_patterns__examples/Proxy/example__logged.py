d2d8aef9fd675a10ea435df851d3673f35dd4aba
gil9red <ilya.petrash@inbox.ru>
2018-11-22 13:05:56 +0500

    Append design_patterns__examples/Proxy/example__logged.py

---
+++ design_patterns__examples/Proxy/example__logged.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from example__cached import IGoUrl, GoUrl, GoUrlCachedProxy, requests
+
+
+def get_logger(name, file='log.txt', encoding='utf-8'):
+    import logging
+    log = logging.getLogger(name)
+    log.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('[%(asctime)s] %(filename)s:%(lineno)d %(levelname)-8s %(message)s')
+
+    # Simple file handler
+    # fh = logging.FileHandler(file, encoding=encoding)
+    # or:
+    from logging.handlers import RotatingFileHandler
+    fh = RotatingFileHandler(file, maxBytes=10000000, backupCount=5, encoding=encoding)
+    fh.setFormatter(formatter)
+    log.addHandler(fh)
+
+    import sys
+    sh = logging.StreamHandler(stream=sys.stdout)
+    sh.setFormatter(formatter)
+    log.addHandler(sh)
+
+    return log
+
+
+class GoUrlLoggedProxy(IGoUrl):
+    """Прокси"""
+
+    __LOGGER = get_logger('GoUrlLoggedProxy')
+
+    def __init__(self, go_url: IGoUrl = None):
+        if go_url is None:
+            go_url = GoUrl()
+
+        self.__url = go_url
+
+    def get(self, url: str) -> requests.Response:
+        self.__log(f'Start get(url="{url}")')
+
+        rs = self.__url.get(url)
+        self.__log(f'Finish get(url="{url}") -> {rs}')
+
+        return rs
+
+    def get_status_code(self, url: str) -> int:
+        self.__log(f'Start get_status_code(url="{url}")')
+
+        code = self.__url.get_status_code(url)
+        self.__log(f'Finish get_status_code(url="{url}") -> {code}')
+
+        return code
+
+    def __log(self, text: str):
+        GoUrlLoggedProxy.__LOGGER.debug(text)
+
+
+if __name__ == '__main__':
+    import time
+
+    class TimeThis:
+        def __enter__(self):
+            self.start_time = time.clock()
+            return self
+
+        def __exit__(self, exc_type, exc_value, exc_traceback):
+            print('Elapsed time: {:.6f} sec'.format(time.clock() - self.start_time))
+
+    url = 'https://github.com/gil9red'
+
+    go_url = GoUrl()
+
+    with TimeThis():
+        rs = go_url.get(url)
+        code = go_url.get_status_code(url)
+        print(rs, rs.status_code, code, rs.content)
+
+    print()
+    print('Logged proxy:')
+
+    go_url = GoUrlLoggedProxy()
+
+    with TimeThis():
+        rs = go_url.get(url)
+        code = go_url.get_status_code(url)
+        print(rs, rs.status_code, code, rs.content)
+
+    print()
+    print('Cached proxy with Logged proxy:')
+
+    go_url = GoUrlLoggedProxy(go_url=GoUrlCachedProxy())
+
+    with TimeThis():
+        rs = go_url.get(url)
+        code = go_url.get_status_code(url)
+        print(rs, rs.status_code, code, rs.content)
+
+    print()
+
+    with TimeThis():
+        rs = go_url.get(url)
+        code = go_url.get_status_code(url)
+        print(rs, rs.status_code, code, rs.content)
