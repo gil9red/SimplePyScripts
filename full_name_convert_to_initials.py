5f26b6a27b6000b449af528ff6770d7f8f9f64b6
gil9red <ilya.petrash@inbox.ru>
2018-11-23 11:57:12 +0500

    Append full_name_convert_to_initials.py

---
+++ full_name_convert_to_initials.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://ru.stackoverflow.com/a/910435/201445
+
+
+def convert(name: str) -> str:
+    parts = name.split()
+    return parts[0] + " " + ''.join(x[0] + '.' for x in parts[1:])
+
+
+if __name__ == '__main__':
+    authors = ["Иванов Иван", "Петров Петр Петрович"]
+    items = [convert(x) for x in authors]
+    print(items)  # ["Иванов И.", "Петров П.П."]
