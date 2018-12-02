7c5bc077a92136cf797d182453dba04aed96e685
ipetrash <ilya.petrash@inbox.ru>
2018-12-02 21:45:28 +0500

    Append "Dark Souls/print_bosses_of_location"

---
+++ Dark Souls/print_bosses_of_location/print_bosses_of_location__Dark_Souls.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import sys
+sys.path.append('..')
+
+from common import parse_locations_ds1
+from collections import defaultdict
+
+
+visited_locations, links, bosses = parse_locations_ds1(log=False)
+
+location_by_bosses = defaultdict(list)
+for location, boss in bosses:
+    location_by_bosses[location].append(boss)
+
+for location in visited_locations:
+    print(f'{location}:')
+    for boss in location_by_bosses[location]:
+        print(f'   {boss}')
+
+    print()
