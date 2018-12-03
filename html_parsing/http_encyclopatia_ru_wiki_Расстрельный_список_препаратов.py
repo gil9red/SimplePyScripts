bd0687bb48799878a9b25abb090b95cc17b933ba
gil9red <ilya.petrash@inbox.ru>
2018-12-03 17:30:35 +0500

    Append html_parsing/http_encyclopatia_ru_wiki_Расстрельный_список_препаратов.py

---
+++ html_parsing/http_encyclopatia_ru_wiki_Расстрельный_список_препаратов.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import requests
+from bs4 import BeautifulSoup
+import re
+
+
+def is_has(letter: str) -> bool:
+    letter = letter.strip().lower()
+
+    return bool(re.match('^[а-яё]$', letter))
+
+
+rs = requests.get('http://encyclopatia.ru/wiki/Расстрельный_список_препаратов')
+
+root = BeautifulSoup(rs.content, 'html.parser')
+for span in root.select('h2 > .mw-headline'):
+    if not is_has(span.text):
+        continue
+
+    print(span.text)
+
+    # Нам нужен ul -- в нем описаны препараты, но чтобы его найти,
+    # мы сначала найдем элемент с буквой, а после сам элемент
+    # <h2><span>А</span></h2>
+    # <ul>
+    ul = span.parent.find_next_sibling('ul')
+    for li in ul.select('li'):
+        print(li.text.strip())
+
+    print()
