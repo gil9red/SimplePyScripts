797464a41e90cf56a00aedb699e2633ec5fdbfd9
ipetrash <ilya.petrash@inbox.ru>
2018-12-08 03:57:09 +0500

    Append aiohttp__asyncio__examples

---
+++ aiohttp__asyncio__examples/hello_world.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://github.com/aio-libs/aiohttp
+
+
+# pip install aiohttp
+import aiohttp
+import asyncio
+
+
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.text()
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        html = await fetch(session, 'http://python.org')
+        print(html)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
---
+++ aiohttp__asyncio__examples/hello_world__parse__lxml_etree.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://github.com/aio-libs/aiohttp
+
+
+# pip install aiohttp
+import aiohttp
+import asyncio
+
+from lxml import etree
+
+
+async def fetch(session, url):
+    async with session.get(url) as response:
+        # Возвращаем ответ в байтах
+        return await response.content.read()
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        xml_str = await fetch(session, 'https://sdvk-oboi.ru/sitemap.xml')
+        root = etree.fromstring(xml_str)
+
+        for url in root.xpath('//*[local-name()="loc"]/text()'):
+            print(url)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
