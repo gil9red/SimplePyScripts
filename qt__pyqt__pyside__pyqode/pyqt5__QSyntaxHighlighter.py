d9960685f10e083e643e81ea3a06d81ea168609b
gil9red <ilya.petrash@inbox.ru>
2018-12-10 14:55:20 +0500

    Append qt__pyqt__pyside__pyqode/pyqt5__QSyntaxHighlighter.py

---
+++ qt__pyqt__pyside__pyqode/pyqt5__QSyntaxHighlighter.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.Qt import *
+
+
+class MyHighlighter(QSyntaxHighlighter):
+    def highlightBlock(self, text):
+        char_format = QTextCharFormat()
+        char_format.setFontWeight(QFont.Bold)
+        char_format.setForeground(Qt.darkMagenta)
+
+        expression = QRegularExpression(r"\b(cos|sin)\b")
+        i = expression.globalMatch(text)
+        while i.hasNext():
+            match = i.next()
+            self.setFormat(match.capturedStart(), match.capturedLength(), char_format)
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    mw = QTextEdit()
+    mw.setText("x = sin(1)\nb = cos(sin(PI));")
+
+    highlighter = MyHighlighter(mw.document())
+
+    mw.show()
+
+    app.exec()
