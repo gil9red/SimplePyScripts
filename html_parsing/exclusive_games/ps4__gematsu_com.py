60380f7c37ceee43ebef3d487e22e1a93b331b7a
gil9red <ilya.petrash@inbox.ru>
2018-12-11 19:02:43 +0500

    #

--- html_parsing/exclusive_games/ps4__gematsu_com.py
+++ html_parsing/exclusive_games/ps4__gematsu_com.py
@@ -7,10 +7,9 @@ __author__ = 'ipetrash'
 from common import get_game_list_from
 
 
-if __name__ == '__main__':
-    url = 'https://gematsu.com/exclusives/ps4'
-    exclusive_games = get_game_list_from(url, 'fullexclusive', 'platformexclusive', 'consoleexclusive')
+url = 'https://gematsu.com/exclusives/ps4'
+exclusive_games = get_game_list_from(url, 'fullexclusive', 'platformexclusive', 'consoleexclusive')
 
-    print(f'Games ({len(exclusive_games)}):')
-    for i, game in enumerate(exclusive_games, 1):
-        print(f'  {i:3}. {game}')
+print(f'Games ({len(exclusive_games)}):')
+for i, game in enumerate(exclusive_games, 1):
+    print(f'  {i:3}. {game}')
