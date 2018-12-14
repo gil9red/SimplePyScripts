03e002ec8cb23a656f77e20c2d021e329442b6e6
ipetrash <ilya.petrash@inbox.ru>
2018-12-15 00:45:57 +0500

    #

--- tkinter_examples/tab_Notebook__with_widgets_as_modules/main.py
+++ tkinter_examples/tab_Notebook__with_widgets_as_modules/main.py
@@ -12,34 +12,6 @@ from tab_b import Example as TabB
 from tab_c import Example as TabC
 
 
-# class MainInterface:
-#     def __init__(self):
-#         self.window = tk.Tk()
-#         self.window.title('version')
-#         self.window.geometry("1024x768")
-#         self.create_widgets()
-#
-#     def create_widgets(self):
-#         self.window['padx'] = 10
-#         self.window['pady'] = 10
-#
-#         self.notebook = ttk.Notebook(self.window, width=1000, height=700)
-#
-#         a_tab = TabA(self.notebook)
-#         b_tab = TabB(self.notebook)
-#         c_tab = TabC(self.notebook)
-#
-#         self.notebook.add(a_tab, text="Notebook A")
-#         self.notebook.add(b_tab, text="Notebook B")
-#         self.notebook.add(c_tab, text="Notebook C")
-#
-#         self.notebook.grid(row=1, column=1)
-#
-#
-# if __name__ == '__main__':
-#     program = MainInterface()
-#     program.window.mainloop()
-
 class MainWindow(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
