58e68e66bff480e9687ae1d3677efe6f09a1f3a1
ipetrash <ilya.petrash@inbox.ru>
2018-12-14 23:54:33 +0500

    Append tkinter_examples/class_Example_with_Frame_Text.py

---
+++ tkinter_examples/class_Example_with_Frame_Text.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from tkinter import Tk, Frame, Text
+import tkinter as tk
+
+
+class Example(Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.parent = parent
+        self.init_ui()
+
+    def init_ui(self):
+        self.text = Text(width=20, height=10)
+        self.text.pack()
+        self.text.insert(1.0, 'Hello World!\nFoo\nBar\n\n123\n')
+
+        self.button = tk.Button(text='Append', command=self.on_append)
+        self.button.pack()
+
+    def on_append(self):
+        self.text.insert(tk.END, 'Go-go-go!\n')
+
+
+if __name__ == '__main__':
+    root = Tk()
+    root.title('Example')
+    ex = Example(root)
+    root.geometry("300x250+300+300")
+    root.mainloop()
