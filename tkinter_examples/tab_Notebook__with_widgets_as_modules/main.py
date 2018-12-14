c634e65a855fbf2775f945e50df01c4138e08940
ipetrash <ilya.petrash@inbox.ru>
2018-12-15 00:23:03 +0500

    Append tkinter_examples/tab_Notebook__with_widgets_as_modules

---
+++ tkinter_examples/tab_Notebook__with_widgets_as_modules/main.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from tkinter import Tk, ttk
+import tkinter as tk
+
+from tab_a import Example as TabA
+from tab_b import Example as TabB
+from tab_c import Example as TabC
+
+
+class MainWindow(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.parent = parent
+        self.init_ui()
+
+    def init_ui(self):
+        self.parent['padx'] = 10
+        self.parent['pady'] = 10
+
+        self.notebook = ttk.Notebook(self, width=1000, height=700)
+
+        a_tab = TabA(self.notebook)
+        b_tab = TabB(self.notebook)
+        c_tab = TabC(self.notebook)
+
+        self.notebook.add(a_tab, text="Notebook A")
+        self.notebook.add(b_tab, text="Notebook B")
+        self.notebook.add(c_tab, text="Notebook C")
+
+        self.notebook.pack()
+
+        self.pack()
+
+
+if __name__ == '__main__':
+    root = Tk()
+    root.title('version')
+    ex = MainWindow(root)
+    root.geometry("600x500")
+    root.mainloop()
---
+++ tkinter_examples/tab_Notebook__with_widgets_as_modules/tab_a.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import tkinter as tk
+
+
+class Example(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.parent = parent
+        self.init_ui()
+
+    def init_ui(self):
+        self.pack(fill=tk.BOTH, expand=1)
+
+        acts = ['Scarlett Johansson', 'Rachel Weiss', 'Natalie Portman', 'Jessica Alba']
+
+        self.lb = tk.Listbox(self)
+        for i in acts:
+            self.lb.insert(tk.END, i)
+            self.lb.bind("<<ListboxSelect>>", self.on_select)
+            self.lb.pack(pady=15)
+
+        self.var = tk.StringVar()
+        self.label = tk.Label(self, text=0, textvariable=self.var)
+        self.label.pack()
+
+        self.pack()
+
+    def on_select(self, val):
+        sender = val.widget
+        idx = sender.curselection()
+        value = sender.get(idx)
+
+        self.var.set(value)
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title('Example')
+    ex = Example(root)
+    root.geometry("300x250+300+300")
+    root.mainloop()
