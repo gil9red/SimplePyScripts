f890464a7b10b173b2d5e815a687a561b5a9d815
ipetrash <ilya.petrash@inbox.ru>
2018-12-15 00:44:06 +0500

    #

--- tkinter_examples/tab_Notebook__with_widgets_as_modules/main.py
+++ tkinter_examples/tab_Notebook__with_widgets_as_modules/main.py
@@ -12,11 +12,41 @@ from tab_b import Example as TabB
 from tab_c import Example as TabC
 
 
+# class MainInterface:
+#     def __init__(self):
+#         self.window = tk.Tk()
+#         self.window.title('version')
+#         self.window.geometry("1024x768")
+#         self.create_widgets()
+#
+#     def create_widgets(self):
+#         self.window['padx'] = 10
+#         self.window['pady'] = 10
+#
+#         self.notebook = ttk.Notebook(self.window, width=1000, height=700)
+#
+#         a_tab = TabA(self.notebook)
+#         b_tab = TabB(self.notebook)
+#         c_tab = TabC(self.notebook)
+#
+#         self.notebook.add(a_tab, text="Notebook A")
+#         self.notebook.add(b_tab, text="Notebook B")
+#         self.notebook.add(c_tab, text="Notebook C")
+#
+#         self.notebook.grid(row=1, column=1)
+#
+#
+# if __name__ == '__main__':
+#     program = MainInterface()
+#     program.window.mainloop()
+
 class MainWindow(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
 
         self.parent = parent
+        self.parent.title('version')
+
         self.init_ui()
 
     def init_ui(self):
@@ -40,7 +70,6 @@ class MainWindow(tk.Frame):
 
 if __name__ == '__main__':
     root = Tk()
-    root.title('version')
     ex = MainWindow(root)
     root.geometry("300x250")
     root.mainloop()
---
+++ tkinter_examples/tab_Notebook__with_widgets_as_modules/main_v2.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from tkinter import Tk, ttk
+import tkinter as tk
+
+from tab_a import Example as TabA
+from tab_b import Example as TabB
+from tab_c import Example as TabC
+
+
+class MainInterface:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title('version')
+        self.window.geometry("300x250")
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.window['padx'] = 10
+        self.window['pady'] = 10
+
+        self.notebook = ttk.Notebook(self.window, width=1000, height=700)
+
+        a_tab = TabA(self.notebook)
+        b_tab = TabB(self.notebook)
+        c_tab = TabC(self.notebook)
+
+        self.notebook.add(a_tab, text="Notebook A")
+        self.notebook.add(b_tab, text="Notebook B")
+        self.notebook.add(c_tab, text="Notebook C")
+
+        self.notebook.pack()
+
+
+if __name__ == '__main__':
+    program = MainInterface()
+    program.window.mainloop()
