120f4c952f9128b7be301bcee89d507e033e8ca4
ipetrash <ilya.petrash@inbox.ru>
2018-12-14 23:57:53 +0500

    Append tkinter_examples/class_Example_with_Frame_Tab_Notebook.py

---
+++ tkinter_examples/class_Example_with_Frame_Tab_Notebook.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from tkinter import Tk, Frame, ttk
+
+
+class Example(Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.parent = parent
+        self.init_ui()
+
+    def init_ui(self):
+        self.notebook = ttk.Notebook(width=1000, height=700)
+
+        a_tab = ttk.Frame(self.notebook)
+        b_tab = ttk.Frame(self.notebook)
+        c_tab = ttk.Frame(self.notebook)
+
+        self.notebook.add(a_tab, text="Notebook A")
+        self.notebook.add(b_tab, text="Notebook B")
+        self.notebook.add(c_tab, text="Notebook C")
+
+        self.notebook.pack()
+
+
+if __name__ == '__main__':
+    root = Tk()
+    root.title('Example')
+    ex = Example(root)
+    root.geometry("300x250+300+300")
+    root.mainloop()
