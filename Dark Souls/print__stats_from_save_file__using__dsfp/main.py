b56c8266280d759d4ec622bbfc4388dbfec4abee
ipetrash <ilya.petrash@inbox.ru>
2018-12-18 00:00:38 +0500

    Append "Dark Souls/print__stats_from_save_file__using__dsfp"

---
+++ Dark Souls/print__stats_from_save_file__using__dsfp/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://github.com/tarvitz/dsfp
+
+
+# C:\Users\<CURRENT_USER>\Documents\NBGI\DarkSouls\<PLAYER_NAME>\DRAKS0005.sl2
+file_name = '../print__deaths_from_save_file/DRAKS0005.sl2'
+
+import dsfp
+ds = dsfp.DSSaveFileParser(file_name)
+
+stats_list = ds.get_stats()
+print(f'Stats list ({len(stats_list)}): {stats_list}')
+print()
+
+print(f'Character ({len(stats_list)}):')
+
+for stats in stats_list:
+    name, deaths = stats['name'], stats['deaths']
+    print('    {}, deaths: {}'.format(name, deaths))
