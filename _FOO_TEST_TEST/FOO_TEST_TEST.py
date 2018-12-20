179a45a04ce8d3d66c10d3eee9b9fd404f8e1219
ipetrash <ilya.petrash@inbox.ru>
2018-12-20 21:26:25 +0500

    #

--- _FOO_TEST_TEST/FOO_TEST_TEST.py
+++ _FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -5,19 +5,3 @@ __author__ = 'ipetrash'
 
 
 
-from bs4 import BeautifulSoup
-root = BeautifulSoup(open('3.html', encoding='utf-8'), 'html.parser')
-
-table = root.select_one('table')
-
-for tr in table.select('tr'):
-    tds = tr.select('td')
-    if len(tds) != 3:
-        continue
-
-    title_node, description_node = tds[1:]
-
-    title = title_node.text.strip()
-    description = description_node.text.strip()
-
-    print('{:20}: {}'.format(title, repr(description)))
