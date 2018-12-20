ad33a95c3f0487555e9d8a0a0c5aef00613c9bbf
ipetrash <ilya.petrash@inbox.ru>
2018-12-20 21:25:57 +0500

    Append html_parsing/https_newyear_mail_ru

---
+++ html_parsing/https_newyear_mail_ru/download__m3u8.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import requests
+rs = requests.get('https://newyear.mail.ru/video/man/kolya/sweets/manifest.m3u8')
+print(rs.content)
+
+rs = requests.get('https://newyear.mail.ru/video/man/kolya/games/manifest.m3u8')
+print(rs.content)
