57fa98bb88f104d3db39f1013ac559c1157d5276
gil9red <ilya.petrash@inbox.ru>
2018-12-20 18:06:25 +0500

    Append Dark Souls\print__stats

---
+++ Dark Souls/print__stats/common.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import requests
+
+from bs4 import BeautifulSoup
+
+
+def get_parsed_two_column_table_stats(url: str) -> [(str, str)]:
+    rs = requests.get(url)
+    root = BeautifulSoup(rs.content, 'html.parser')
+
+    table = root.select_one('table')
+
+    items = []
+
+    for tr in table.select('tr'):
+        tds = tr.select('td')
+        if len(tds) != 3:
+            continue
+
+        title_node, description_node = tds[1:]
+
+        title = title_node.text.strip()
+        description = description_node.text.strip()
+
+        items.append((title, description))
+
+    return items
---
+++ Dark Souls/print__stats/print__stats_ds3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from common import get_parsed_two_column_table_stats
+
+
+url = 'http://ru.darksouls.wikia.com/wiki/Характеристики_(Dark_Souls_III)'
+items = get_parsed_two_column_table_stats(url)
+print(f'items ({len(items)}): {items}')
+print()
+
+for title, description in items:
+    print('{:20}: {}'.format(title, repr(description)))
