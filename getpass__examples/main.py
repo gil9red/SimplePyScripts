051289b34288c3f176864727e60dad058de69880
gil9red <ilya.petrash@inbox.ru>
2018-12-21 12:13:33 +0500

    Append getpass__examples

---
+++ getpass__examples/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import getpass
+
+# Return the “login name” of the user.
+print('Hello,', getpass.getuser())
+
+# NOTE: not work in PyCharm console
+password = getpass.getpass()
+print(password)
+
+password = getpass.getpass('Input pass: ')
+print(password)
