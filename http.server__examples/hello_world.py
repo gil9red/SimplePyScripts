10bd88f40ecf8296bb4140f7b4e42e1ee7bb5e44
gil9red <ilya.petrash@inbox.ru>
2018-12-25 11:23:55 +0500

    #

--- http.server__examples/hello_world.py
+++ http.server__examples/hello_world.py
@@ -14,6 +14,15 @@ class HttpProcessor(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(bytes("Hello!", 'utf-8'))
 
+    def do_POST(self):
+        length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(length).decode('utf-8')
+        print('post_data:', post_data)
+
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(bytes("Ok!", 'utf-8'))
+
 
 if __name__ == '__main__':
     server = HTTPServer(("localhost", 80), HttpProcessor)
