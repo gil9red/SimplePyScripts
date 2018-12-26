a6e817609d78bcd632c8b01fe353b0dd87e68c93
gil9red <ilya.petrash@inbox.ru>
2018-12-26 17:29:35 +0500

    Append contextlib__redirect__stdout_stderr/catch_stdout_and_stderr__Exception.py

---
+++ contextlib__redirect__stdout_stderr/catch_stdout_and_stderr__Exception.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from contextlib import redirect_stdout, redirect_stderr
+import io
+import sys
+import traceback
+
+
+f = io.StringIO()
+
+with redirect_stdout(f), redirect_stderr(f):
+    try:
+        print('123')
+        print('abc')
+        1 / 0
+    except:
+        print(traceback.format_exc(), file=sys.stderr)
+
+s = f.getvalue()
+print(s)
+# 123
+# abc
+# Traceback (most recent call last):
+#   File ..., line 18, in <module>
+#     1 / 0
+# ZeroDivisionError: division by zero
