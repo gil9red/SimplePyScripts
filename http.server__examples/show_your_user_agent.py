fc58d17b983471839cd5fdedf29c33465579482d
gil9red <ilya.petrash@inbox.ru>
2018-12-26 11:40:18 +0500

    Append http.server__examples/show_your_user_agent.py

---
+++ http.server__examples/show_your_user_agent.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class HttpProcessor(BaseHTTPRequestHandler):
+    def do_GET(self):
+        user_agent = self.headers['User-Agent']
+        print(user_agent)
+
+        self.send_response(200)
+        self.send_header('content-type', 'text/html')
+        self.end_headers()
+
+        html = f"""\
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <div>{user_agent}</div>
+    </body>
+</html>
+"""
+
+        self.wfile.write(bytes(html, 'utf-8'))
+
+
+if __name__ == '__main__':
+    host, port = "localhost", 80
+    print(f'Running on http://{host}:{port}/')
+
+    server = HTTPServer((host, port), HttpProcessor)
+    server.serve_forever()
