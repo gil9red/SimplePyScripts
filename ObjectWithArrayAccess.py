1aba0f442496e0fd7a47ae27e158cf972ace591a
ipetrash <ilya.petrash@inbox.ru>
2019-01-03 13:23:03 +0500

    Append ObjectWithArrayAccess.py

---
+++ ObjectWithArrayAccess.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+class ObjectWithArrayAccess:
+    def __init__(self):
+        self._fields = dict()
+
+    def __getitem__(self, item):
+        return self._fields.get(item)
+
+    def __setitem__(self, key, value):
+        self._fields[key] = value
+
+    def __len__(self):
+        return len(self._fields)
+
+
+if __name__ == '__main__':
+    obj = ObjectWithArrayAccess()
+    print(bool(obj))       # False
+    obj[0] = 'Hello'
+    obj[1] = 'world'
+    print(obj._fields)     # {0: 'Hello', 1: 'world'}
+    print(len(obj))        # 2
+    print(bool(obj))       # True
+    print(obj[0], obj[1])  # Hello world
