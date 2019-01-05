8576072e28443e5ea5dcd5e8f4a4d80762624a6a
ipetrash <ilya.petrash@inbox.ru>
2019-01-05 16:45:07 +0500

    Append html_parsing/www_dns_shop_ru/print__payment_variants.py

---
+++ html_parsing/www_dns_shop_ru/print__payment_variants.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import requests
+from bs4 import BeautifulSoup
+
+url = 'http://www.dns-shop.ru/'
+
+rs = requests.get(url)
+root = BeautifulSoup(rs.content, 'html.parser')
+
+for item in root.select('#payment-variants > span'):
+    print(item['title'])
