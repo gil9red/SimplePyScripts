7f70e9d6963585e04231adf4ac470e4093102cc8
ipetrash <ilya.petrash@inbox.ru>
2019-01-06 05:59:42 +0500

    #

--- bitstruct__examples__parse_binary_files/parse_ts__print_packet_transport_stream_header/main.py
+++ bitstruct__examples__parse_binary_files/parse_ts__print_packet_transport_stream_header/main.py
@@ -18,8 +18,6 @@ import io
 # SOURCE: https://nypromo2019.hb.bizmrg.com/video/man/ny2019__000.ts
 #         from m3u8 https://github.com/gil9red/SimplePyScripts/blob/ad33a95c3f0487555e9d8a0a0c5aef00613c9bbf/html_parsing/https_newyear_mail_ru/download__m3u8.py
 with open('ny2019__000.ts', 'rb') as file:
-    print(len(file.read()) // 188)
-
     while True:
         data = file.read(188)
         if not data:
@@ -32,20 +30,20 @@ with open('ny2019__000.ts', 'rb') as file:
         sync_byte = f.read(1)
         print('sync_byte:', sync_byte)
 
-        n = f.read(2)
+        data = f.read(2)
         transport_error_indicator, \
         payload_unit_start_indicator, \
         transport_priority, \
-        pid = bitstruct.unpack('>b1b1b1u13', n)
+        pid = bitstruct.unpack('>b1b1b1u13', data)
         print('transport_error_indicator:', transport_error_indicator)
         print('payload_unit_start_indicator:', payload_unit_start_indicator)
         print('transport_priority:', transport_priority)
         print('pid:', pid)
 
-        n = f.read(1)
+        data = f.read(1)
         transport_scrambling_control, \
         adaptation_field_control, \
-        continuity_counter = bitstruct.unpack('>u2u2u4', n)
+        continuity_counter = bitstruct.unpack('>u2u2u4', data)
         print('transport_scrambling_control:', transport_scrambling_control)
         print('adaptation_field_control:', adaptation_field_control)
         print('continuity_counter:', continuity_counter)
