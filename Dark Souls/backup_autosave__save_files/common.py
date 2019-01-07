74cd2e0ce5897772d2e7215c944548297bfa99b4
ipetrash <ilya.petrash@inbox.ru>
2019-01-08 00:31:27 +0500

    #

--- Dark Souls/backup_autosave__save_files/common.py
+++ Dark Souls/backup_autosave__save_files/common.py
@@ -29,36 +29,48 @@ def get_logger(name):
 log = get_logger(__file__)
 
 
-def backup_saves(path_ds_save, forced=False):
-    for path_file_name in glob(path_ds_save):
-        log.debug(f"Check: {path_file_name}")
-
-        # Get timestamps
-        save_timestamp = os.path.getmtime(path_file_name)
+def backup(path_file_name: str, now_timestamp=None):
+    if now_timestamp is None:
         now_timestamp = time.time()
 
-        # Save backup. If less than 600 secs have passed since the last modification of the file
-        is_modified = (now_timestamp - save_timestamp) < 600
-        ok = forced or is_modified
-        log.debug(f"{'Need backup' if ok else 'Not need backup'}. "
-                  f"Reason: Forced={forced}, Is modified file save={is_modified}")
-        if not ok:
-            continue
+    path_dir, file_name = os.path.split(path_file_name)
 
-        path_dir, file_name = os.path.split(path_file_name)
+    path_dir_backup = os.path.join(path_dir, "BACKUP")
 
-        path_dir_backup = os.path.join(path_dir, "BACKUP")
+    # Create backup dir
+    os.makedirs(path_dir_backup, exist_ok=True)
 
-        # Create backup dir
-        os.makedirs(path_dir_backup, exist_ok=True)
+    time_backup = time.strftime("%y-%m-%d_%H%M%S", time.localtime(now_timestamp))
+    file_name_backup = file_name + '.backup_' + time_backup
+    file_name_backup = os.path.join(path_dir_backup, file_name_backup)
 
-        time_backup = time.strftime("%y-%m-%d_%H%M%S", time.localtime(now_timestamp))
-        file_name_backup = file_name + '.backup_' + time_backup
-        file_name_backup = os.path.join(path_dir_backup, file_name_backup)
+    shutil.copyfile(path_file_name, file_name_backup)
 
-        shutil.copyfile(path_file_name, file_name_backup)
 
-        log.debug(f"Saving backup: {file_name_backup}")
+def backup_saves(path_ds_save, forced=False):
+    try:
+        for path_file_name in glob(path_ds_save):
+            log.debug(f"Check: {path_file_name}")
+
+            # Get timestamps
+            save_timestamp = os.path.getmtime(path_file_name)
+            now_timestamp = time.time()
+
+            # Save backup. If less than 600 secs have passed since the last modification of the file
+            is_modified = (now_timestamp - save_timestamp) < 600
+            ok = forced or is_modified
+            log.debug(f"{'Need backup' if ok else 'Not need backup'}. "
+                      f"Reason: Forced={forced}, Is modified file save={is_modified}")
+            if not ok:
+                continue
+
+            backup(path_file_name, now_timestamp)
+
+            log.debug(f"Saving backup: {file_name_backup}")
+
+    except:
+        print("ERROR:\n" + traceback.format_exc())
+        time.sleep(5 * 60)
 
 
 def run(path_ds_save, timeout_minutes=5):
@@ -68,12 +80,7 @@ def run(path_ds_save, timeout_minutes=5):
     backup_saves(path_ds_save, forced=True)
 
     while True:
-        try:
-            print(f"\nSleeping for {timeout_minutes} minutes\n")
-            time.sleep(timeout_minutes * 60)
+        print(f"\nSleeping for {timeout_minutes} minutes\n")
+        time.sleep(timeout_minutes * 60)
 
-            backup_saves(path_ds_save, False)
-
-        except:
-            print("ERROR:\n" + traceback.format_exc())
-            time.sleep(5 * 60)
+        backup_saves(path_ds_save, False)
