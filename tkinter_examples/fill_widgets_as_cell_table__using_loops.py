0984b74aafaabf17b11f507546d0d863b636fcf3
ipetrash <ilya.petrash@inbox.ru>
2019-01-08 02:32:20 +0500

    Append tkinter_examples/fill_widgets_as_cell_table__using_loops.py

---
+++ tkinter_examples/fill_widgets_as_cell_table__using_loops.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://ru.stackoverflow.com/questions/929442/
+
+
+import tkinter as tk
+
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+
+def on_selecting_file():
+    pass
+
+
+def on_change_symbols():
+    pass
+
+
+def on_window_deleted():
+    print('Window closed')
+    root.quit()
+
+
+root = tk.Tk()
+root.title("Example")
+root.geometry("400x550+300+300")
+root.protocol('WM_DELETE_WINDOW', on_window_deleted)
+
+select_file_button = tk.Button(text="Select file", command=on_selecting_file)
+select_file_button.place(relx=0.5, y=10)
+
+change_letters_button = tk.Button(text="Change letters", command=on_change_symbols)
+change_letters_button.place(relx=0.5, y=500)
+
+pos = 0
+x = 50
+y = 50
+
+try:
+    for i in range(3):
+        for j in range(9):
+            letter = alphabet[pos]
+
+            label = tk.Label(text=letter)
+            label.place(x=x, y=y)
+
+            entry = tk.Entry()
+            entry.place(x=x+30, y=y, width=30)
+
+            globals()['label_' + letter] = label
+            globals()['letter_' + letter] = entry
+
+            pos += 1
+            y += 50
+
+        y = 50
+        x += 100
+
+# For quit from loops
+except IndexError:
+    pass
+
+root.mainloop()
