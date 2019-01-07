925eb85f4eabdbd07176637c0f8f176196d9d82f
ipetrash <ilya.petrash@inbox.ru>
2019-01-08 01:24:51 +0500

    Append "Dark Souls/backup__save_files/by_hotkey"

---
+++ Dark Souls/backup__save_files/by_hotkey/common.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import os
+import time
+import traceback
+from glob import glob
+
+# For import utils.py
+import sys
+sys.path.append('..')
+
+from utils import get_logger, backup
+
+# pip install keyboard
+import keyboard
+
+
+def beep():
+    try:
+        import winsound
+        winsound.Beep(1000, duration=50)
+    except:
+        # ignore
+        pass
+
+
+log = get_logger(__file__)
+
+# Default hotkey
+HOTKEY = "CTRL + F1"
+
+
+def backup_saves(path_ds_save: str):
+    try:
+        for path_file_name in glob(path_ds_save):
+            now_timestamp = time.time()
+
+            file_name_backup = backup(path_file_name, now_timestamp)
+            log.debug(f"Saving backup: {file_name_backup}")
+
+        beep()
+
+    except:
+        print("ERROR:\n" + traceback.format_exc())
+        time.sleep(5 * 60)
+
+
+def run(path_ds_save: str, hotkey=HOTKEY):
+    # Example: r'~\Documents\NBGI\DarkSouls\*\DRAKS0005.sl2'
+    path_ds_save = os.path.expanduser(path_ds_save)
+
+    print(f'{path_ds_save}\n')
+    print(f'Using hotkey: {hotkey}\n')
+
+    # Run hotkey
+    keyboard.add_hotkey(HOTKEY, lambda: backup_saves(path_ds_save))
+    keyboard.wait()
