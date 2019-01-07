4c043de195a9bc2aa7acc377729fc9efcf25015c
ipetrash <ilya.petrash@inbox.ru>
2019-01-07 17:17:53 +0500

    Append tkinter_examples/create_new_window__on_click_button__v2.py

---
+++ tkinter_examples/create_new_window__on_click_button__v2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from tkinter import Tk, Label, Button
+
+
+class MainApp(Tk):
+    def __init__(self, *arg, **kwarg):
+        super().__init__(*arg, **kwarg)
+
+        label = Label(self, text='First Window')
+        button = Button(self, text='Open Window', command=self.new_window)
+
+        label.pack()
+        button.pack()
+
+    def new_window(self):
+        Window().mainloop()
+
+
+class Window(Tk):
+    def __init__(self, *arg, **kwarg):
+        super().__init__(*arg, **kwarg)
+
+        label = Label(self, text='Second Window')
+        label.pack()
+
+
+if __name__ == '__main__':
+    MainApp().mainloop()
