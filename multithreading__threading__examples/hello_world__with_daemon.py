dee38a960d0561ce0d284237647848fc567b6250
ipetrash <ilya.petrash@inbox.ru>
2019-01-10 19:17:22 +0500

    Append multithreading__threading__examples:     hello_world__with_daemon.py     hello_world__without_daemon.py

---
+++ multithreading__threading__examples/hello_world__with_daemon.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import threading
+import time
+
+
+def run():
+    print(threading.current_thread())
+    for i in 'Hello World!':
+        print(i)
+        time.sleep(0.2)
+
+
+if __name__ == '__main__':
+    thread = threading.Thread(target=run, daemon=True)
+    thread.start()
+
+    # Main process NOT WAIT thread
