91b73773213485fda2dd07828de6c232201e2bbe
ipetrash <ilya.petrash@inbox.ru>
2019-01-13 03:35:24 +0500

    Append Reading_the_target_of_a_lnk_file/using__winshell.py

---
+++ Reading_the_target_of_a_lnk_file/using__winshell.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from glob import glob
+import os
+import winshell
+
+path_desktop_lnk = os.path.expanduser(r'~\Desktop\**\*.lnk')
+
+file_name = glob(path_desktop_lnk, recursive=True)[0]
+shortcut = winshell.shortcut(file_name)
+print(shortcut)
+print(shortcut.lnk_filepath)
+print(shortcut.path)
+print(shortcut.working_directory)
