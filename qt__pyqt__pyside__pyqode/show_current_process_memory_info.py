a4ee70e46eb4ee3ebf2ffbd083d823c62169fbbe
gil9red <ilya.petrash@inbox.ru>
2019-01-18 17:28:34 +0500

    Append qt__pyqt__pyside__pyqode/show_current_process_memory_info.py

---
+++ qt__pyqt__pyside__pyqode/show_current_process_memory_info.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from multiprocessing import current_process
+
+# pip install psutil
+import psutil
+
+from PyQt5.Qt import QApplication, QWidget, QLabel, QTimer, QVBoxLayout
+
+
+class MainWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self._label_info = QLabel()
+
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(self._label_info)
+        self.setLayout(main_layout)
+
+        pid = current_process().pid
+        self._process = psutil.Process(pid)
+
+        self._timer = QTimer()
+        self._timer.timeout.connect(self.update_memory_info)
+        self._timer.start(500)
+
+        self.update_memory_info()
+
+    def update_memory_info(self):
+        memory_bytes = self._process.memory_info().rss
+
+        self._label_info.setText(
+            f'Current memory: {memory_bytes} bytes\n'
+            f'Current memory: {memory_bytes // 1024} KB\n'
+            f'Current memory: {memory_bytes // 1024 // 1024} MB'
+        )
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    mw = MainWidget()
+    mw.show()
+
+    app.exec()
