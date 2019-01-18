00ed77bac5f512ac352b4bbb252c5c6e6d116450
gil9red <ilya.petrash@inbox.ru>
2019-01-18 17:00:56 +0500

    Append psutil_example/process_management/current_process_detail_info.py

---
+++ psutil_example/process_management/current_process_detail_info.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from multiprocessing import current_process
+from process_detail_info import print_info
+
+pid = current_process().pid
+print(pid)
+print()
+
+print_info(pid)
