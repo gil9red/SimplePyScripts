60cad367e92760d3a29a1ea566916ca197df1047
gil9red <ilya.petrash@inbox.ru>
2019-01-25 16:35:00 +0500

    #

--- fnmatch_examples__wildcard/hello_world__fnmatch_wildcard.py
+++ fnmatch_examples__wildcard/hello_world__fnmatch_wildcard.py
@@ -8,22 +8,26 @@ import fnmatch
 
 
 file_name = 'README.md'
-patterns = ['*.md', 'READ*', 'RE*md', 'RE??ME.*']
-template_string = '{} is {:<%s} -> {}' % len(max(patterns))
+patterns = ['*.md', 'READ*', 'RE*md', 'RE??ME.*', 'README.*', 'README.md']
+template_string = '{} is {:<%s} -> {}' % len(max(patterns, key=len))
 
 for p in patterns:
     print(template_string.format(file_name, p, fnmatch.fnmatch(file_name, p)))
-# README.md is *.md     -> True
-# README.md is READ*    -> True
-# README.md is RE*md    -> True
-# README.md is RE??ME.* -> True
+# README.md is *.md      -> True
+# README.md is READ*     -> True
+# README.md is RE*md     -> True
+# README.md is RE??ME.*  -> True
+# README.md is README.*  -> True
+# README.md is README.md -> True
 
 print()
 
 file_name = 'README.txt'
 for p in patterns:
     print(template_string.format(file_name, p, fnmatch.fnmatch(file_name, p)))
-# README.txt is *.md     -> False
-# README.txt is READ*    -> True
-# README.txt is RE*md    -> False
-# README.txt is RE??ME.* -> True
+# README.md is *.md      -> True
+# README.md is READ*     -> True
+# README.md is RE*md     -> True
+# README.md is RE??ME.*  -> True
+# README.md is README.*  -> True
+# README.md is README.md -> True
