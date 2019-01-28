2b00379cd66811466084c53f4c3750f7d40a4e16
gil9red <ilya.petrash@inbox.ru>
2019-01-28 18:28:22 +0500

    Append get_list_all_the_available_drive_letters/use__fsutil_fsinfo_drives__with__re.py

---
+++ get_list_all_the_available_drive_letters/use__fsutil_fsinfo_drives__with__re.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import os
+import re
+
+
+def get_drivers() -> list:
+    text = os.popen("fsutil fsinfo drives").read()
+    return re.findall(r'([A-Z]):\\', text)
+
+
+if __name__ == '__main__':
+    drives = get_drivers()
+    print(drives)
