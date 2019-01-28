7087ad055dac79dcca38bd6dd52e06852bf6a671
gil9red <ilya.petrash@inbox.ru>
2019-01-28 18:34:40 +0500

    Append get_list_all_the_available_drive_letters/use__psutil.py

---
+++ get_list_all_the_available_drive_letters/use__psutil.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install psutil
+import psutil
+
+
+def get_drivers() -> list:
+    return [x.device[0] for x in psutil.disk_partitions(all=True)]
+
+
+if __name__ == '__main__':
+    drives = get_drivers()
+    print(drives)
