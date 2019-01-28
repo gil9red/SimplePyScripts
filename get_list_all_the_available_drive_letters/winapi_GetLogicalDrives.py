f0910eb64e8d8e960ff47ce3ed4e97b8649f3b2f
gil9red <ilya.petrash@inbox.ru>
2019-01-28 18:11:58 +0500

    Append get_list_all_the_available_drive_letters/winapi_GetLogicalDrives.py

---
+++ get_list_all_the_available_drive_letters/winapi_GetLogicalDrives.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://stackoverflow.com/a/827398/5909792
+
+
+import string
+from ctypes import windll
+
+
+def get_drivers() -> list:
+    drives = []
+    bitmask = windll.kernel32.GetLogicalDrives()
+
+    for letter in string.ascii_uppercase:
+        if bitmask & 1:
+            drives.append(letter)
+        bitmask >>= 1
+
+    return drives
+
+
+if __name__ == '__main__':
+    drives = get_drivers()
+    print(drives)
