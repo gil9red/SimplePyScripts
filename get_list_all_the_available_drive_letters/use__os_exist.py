c8fb9cb7fa3363e57fe91245d83e283deda2532c
gil9red <ilya.petrash@inbox.ru>
2019-01-28 18:19:27 +0500

    Append get_list_all_the_available_drive_letters/use__os_exist.py

---
+++ get_list_all_the_available_drive_letters/use__os_exist.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://stackoverflow.com/a/34187346/5909792
+
+import os
+import string
+
+
+def get_drivers() -> list:
+    return [d for d in string.ascii_uppercase if os.path.exists('%s:' % d)]
+
+
+if __name__ == '__main__':
+    drives = get_drivers()
+    print(drives)
