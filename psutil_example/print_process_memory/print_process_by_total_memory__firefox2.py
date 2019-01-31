e9b313a55b0164718d47fadd616007685494d162
ipetrash <ilya.petrash@inbox.ru>
2019-01-31 19:24:51 +0500

    Append psutil_example/print_process_memory/print_process_by_total_memory__firefox2.py

---
+++ psutil_example/print_process_memory/print_process_by_total_memory__firefox2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install psutil
+import psutil
+
+
+total = 0
+
+for p in psutil.process_iter():
+    name = p.name()
+    if name.lower() != 'firefox.exe':
+        continue
+
+    memory = p.memory_info().rss
+    total += memory
+
+    print(name, memory)
+
+print(f'''
+Total: {total} bytes
+       {total // 1024} KB
+       {total // 1024 // 1024} MB
+       {total // 1024 // 1024 // 1024} GB
+''')
