3117abc448c6303dea8ba2ea2913400c67de0dc4
ipetrash <ilya.petrash@inbox.ru>
2019-01-31 19:13:58 +0500

    Append psutil_example/print_process_by_memory__simple.py

---
+++ psutil_example/print_process_by_memory__simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install psutil
+import psutil
+
+for p in psutil.process_iter():
+    print(p.name(), p.memory_info().rss)
