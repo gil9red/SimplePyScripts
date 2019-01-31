9d3e2e640e2666bebcea82de82814199f32fc2ab
ipetrash <ilya.petrash@inbox.ru>
2019-01-31 19:21:03 +0500

    #

--- psutil_example/print_process_memory/print_process_by_memory.py
+++ psutil_example/print_process_memory/print_process_by_memory.py
@@ -10,17 +10,31 @@ from collections import defaultdict
 import psutil
 
 
+# SOURCE: https://github.com/gil9red/SimplePyScripts/blob/9007628099adb5964fdbf827f14cc872ba35f8ad/human_byte_size.py
+def sizeof_fmt(num):
+    for x in ['bytes', 'KB', 'MB', 'GB']:
+        if num < 1024.0:
+            return "%3.1f %s" % (num, x)
+
+        num /= 1024.0
+
+    return "%3.1f %s" % (num, 'TB')
+
+
 column_width = defaultdict(int)
 process = []
 
 for p in psutil.process_iter():
-    cols = p.name(), str(p.memory_info().rss) + ' bytes'
+    memory = p.memory_info().rss
+    cols = p.name(), str(memory) + ' bytes', sizeof_fmt(memory)
     process.append(cols)
 
     for i, x in enumerate(cols):
         column_width[i] = max(column_width[i], len(x))
 
 # Sort by memory size
-for p in sorted(process, key=lambda x: int(x[1].split(' ')[0]), reverse=True):
+process.sort(key=lambda x: int(x[1].split(' ')[0]), reverse=True)
+
+for p in process:
     row = [x.rjust(column_width[i]) for i, x in enumerate(p)]
     print(' | '.join(row))
