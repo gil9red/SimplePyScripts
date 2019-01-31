b8e91b69d6a5e3fb39a530d8571083e9f927e8a7
ipetrash <ilya.petrash@inbox.ru>
2019-01-31 19:15:17 +0500

    Append psutil_example/print_process_by_memory__simple__only_firefox.py

---
+++ psutil_example/print_process_by_memory__simple__only_firefox.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install psutil
+import psutil
+
+for p in psutil.process_iter():
+    name = p.name()
+    if name.lower() != 'firefox.exe':
+        continue
+
+    print(name, p.memory_info().rss)
