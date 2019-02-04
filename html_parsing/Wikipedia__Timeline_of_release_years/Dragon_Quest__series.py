bcdecdacc2f0e7a63923db7ce8cefc40fcf6763a
ipetrash <ilya.petrash@inbox.ru>
2019-02-05 04:30:21 +0500

    Append html_parsing/Wikipedia__Timeline_of_release_years/Dragon_Quest__series.py

---
+++ html_parsing/Wikipedia__Timeline_of_release_years/Dragon_Quest__series.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# Хронология выхода игр
+
+
+from common import get_parsed_two_column_wikitable
+
+
+def is_match_func(table) -> bool:
+    return 'TIMELINE OF RELEASE YEARS' in table.caption.text.strip().upper()
+
+
+url = 'https://en.wikipedia.org/wiki/Dragon_Quest'
+for year, name in get_parsed_two_column_wikitable(url, is_match_func):
+    print(f'{year}: {name}')
+
+# 1986: Dragon Quest
+# 1987: Dragon Quest II
+# 1988: Dragon Quest III
+# 1990: Dragon Quest IV
+# 1992: Dragon Quest V
+# 1995: Dragon Quest VI
+# 2000: Dragon Quest VII
+# 2004: Dragon Quest VIII
+# 2009: Dragon Quest IX
+# 2012: Dragon Quest X
+# 2017: Dragon Quest XI
