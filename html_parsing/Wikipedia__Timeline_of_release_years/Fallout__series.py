46ce81e4e4bd84a53eed8096b5ece47233a1098f
ipetrash <ilya.petrash@inbox.ru>
2019-02-07 02:55:01 +0500

    Append html_parsing/Wikipedia__Timeline_of_release_years/Fallout__series.py

---
+++ html_parsing/Wikipedia__Timeline_of_release_years/Fallout__series.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# Хронология выхода игр
+
+
+from common import get_parsed_two_column_wikitable
+
+
+def is_match_func(table) -> bool:
+    return 'TIMELINE OF RELEASE YEARS' in table.caption.text.strip().upper()
+
+
+url = 'https://en.wikipedia.org/wiki/Fallout_(series)'
+for year, name in get_parsed_two_column_wikitable(url, is_match_func):
+    print(f'{year}: {name}')
+
+# 1997: Fallout
+# 1998: Fallout 2
+# 2001: Fallout Tactics: Brotherhood of Steel
+# 2004: Fallout: Brotherhood of Steel
+# 2008: Fallout 3
+# 2010: Fallout: New Vegas
+# 2015: Fallout Shelter
+# 2015: Fallout 4
+# 2018: Fallout 76
