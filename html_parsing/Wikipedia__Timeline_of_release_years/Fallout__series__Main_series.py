b29fc5fa952d02dc88586a8ce7651c653d8a52fa
ipetrash <ilya.petrash@inbox.ru>
2019-02-07 03:59:30 +0500

    Append html_parsing/Wikipedia__Timeline_of_release_years/Fallout__series__Main_series.py

---
+++ html_parsing/Wikipedia__Timeline_of_release_years/Fallout__series__Main_series.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# Хронология выхода игр
+
+
+from common import get_parsed_two_column_wikitable
+
+
+def is_match_func(table) -> bool:
+    return 'TIMELINE OF RELEASE YEARS' in table.caption.text.strip().upper()
+
+
+def is_match_row_func(tr) -> bool:
+    td_items = tr.select('td')
+    td = td_items[0] if len(td_items) == 1 else td_items[1]
+
+    # Main series in bold
+    return td.i.b is not None
+
+
+url = 'https://en.wikipedia.org/wiki/Fallout_(series)'
+for year, name in get_parsed_two_column_wikitable(url, is_match_func, is_match_row_func):
+    print(f'{year}: {name}')
+
+# 1997: Fallout
+# 1998: Fallout 2
+# 2008: Fallout 3
+# 2015: Fallout 4
