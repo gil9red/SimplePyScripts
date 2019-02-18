6067c316b0fe7d77e8b756e572474553ccc361e8
gil9red <ilya.petrash@inbox.ru>
2019-02-18 17:58:03 +0500

    Append graphviz__examples/generate__dynamic_change_format.py

---
+++ graphviz__examples/generate__dynamic_change_format.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install graphviz
+from graphviz import Digraph
+
+
+g = Digraph('G', format='svg')
+g.edge('Hello', 'World')
+
+# Get bytes
+print(g.pipe())
+
+g.format = 'png'
+print(g.pipe())
+
+g.format = 'pdf'
+print(g.pipe())
