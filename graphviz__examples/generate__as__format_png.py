1c6069fe033a527f5c4019c3d3b903d8c81b7e44
gil9red <ilya.petrash@inbox.ru>
2019-02-18 17:32:38 +0500

    Append graphviz__examples/generate__as__format_png.py

---
+++ graphviz__examples/generate__as__format_png.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install graphviz
+from graphviz import Digraph
+
+g = Digraph('G', filename='test-output/hello_world.gv', format='png')
+g.edge('Hello', 'World')
+
+out_file_name = g.render()
+print(out_file_name)
