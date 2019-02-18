6904524628b1f87d7aadad2b33c891b960d04e20
gil9red <ilya.petrash@inbox.ru>
2019-02-18 17:38:33 +0500

    Append graphviz__examples/about_piped_output.py

---
+++ graphviz__examples/about_piped_output.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://graphviz.readthedocs.io/en/stable/manual.html#piped-output
+
+
+# pip install graphviz
+from graphviz import Digraph
+
+
+g = Digraph('hello', format='svg')
+g.edge('Hello', 'World')
+print(g.pipe().decode('utf-8'))
+# <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+# <!DOCTYPE svg
+# ...
+# </svg>
