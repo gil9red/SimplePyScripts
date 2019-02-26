786d4e56fc9f13e65a304c4e8b848ff012e33c6e
gil9red <ilya.petrash@inbox.ru>
2019-02-26 17:06:36 +0500

    Append design_patterns__examples/Flyweight

---
+++ design_patterns__examples/Flyweight/example_2.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: Design Patterns: Flyweight — Приспособленец
+# SOURCE: https://ru.wikipedia.org/wiki/Приспособленец_(шаблон_проектирования)
+
+
+from abc import ABC
+from typing import Dict
+
+
+# "Flyweight"
+class Character(ABC):
+    symbol: str
+    width: int
+    height: int
+    ascent: int
+    descent: int
+    point_size: int
+
+    def display(self, point_size: int):
+        self.point_size = point_size
+        print('{} (point_size {})'.format(self.symbol, self.point_size))
+
+
+# "FlyweightFactory"
+class CharacterFactory:
+    def __init__(self):
+        self._characters: Dict[str, Character] = dict()
+
+    def get_character(self, key: str) -> Character:
+        # Uses "lazy initialization"
+        character = self._characters.get(key)
+        if character is None:
+            if key == 'A':
+                character = CharacterA()
+
+            elif key == 'B':
+                character = CharacterB()
+
+            # ...
+
+            elif key == 'Z':
+                character = CharacterZ()
+
+            self._characters[key] = character
+
+        return character
+    
+  
+# "ConcreteFlyweight"
+class CharacterA(Character):
+    def __init__(self):
+        self.symbol = 'A'
+        self.height = 100
+        self.width = 120
+        self.ascent = 70
+        self.descent = 0
+
+
+# "ConcreteFlyweight"
+class CharacterB(Character):
+    def __init__(self):
+        self.symbol = 'B'
+        self.height = 100
+        self.width = 140
+        self.ascent = 72
+        self.descent = 0
+
+
+# ... C, D, E, etc.
+
+
+# "ConcreteFlyweight"
+class CharacterZ(Character):
+    def __init__(self):
+        self.symbol = 'Z'
+        self.height = 100
+        self.width = 100
+        self.ascent = 68
+        self.descent = 0
+
+
+if __name__ == '__main__':
+    # Build a document with text
+    chars = "AAZZBBZB"
+
+    f = CharacterFactory()
+
+    # Extrinsic state
+    point_size = 10
+
+    # For each character use a flyweight object
+    for c in chars:
+        point_size += 1
+        character = f.get_character(c)
+        character.display(point_size)
