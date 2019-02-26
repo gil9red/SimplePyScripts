2b509163e6f58459379a98fa850c8885b276fe9a
gil9red <ilya.petrash@inbox.ru>
2019-02-26 12:12:39 +0500

    Append glob__examples/search_with_prefix.py

---
+++ glob__examples/search_with_prefix.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://docs.python.org/3.6/library/glob.html#module-glob
+
+
+import glob
+import os
+
+
+items = glob.glob('../**/reader_*.py', recursive=True)
+print(items)
+print(list(map(os.path.abspath, items)))
+print()
+
+items = glob.glob('../**/opencv_*/*.py', recursive=True)
+print(items)
+print(list(map(os.path.abspath, items)))
