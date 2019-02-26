bdea3f97e64da1d77ef47b7a1e96ba44221e48ed
gil9red <ilya.petrash@inbox.ru>
2019-02-26 12:11:31 +0500

    Append glob__examples/recursive_search.py

---
+++ glob__examples/recursive_search.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://docs.python.org/3.6/library/glob.html#module-glob
+
+
+import glob
+import os
+
+
+items = glob.glob('../*.md')
+print(items)
+print(list(map(os.path.abspath, items)))
+print()
+
+items = glob.glob('../**/*.md', recursive=True)
+print(items)
+print(list(map(os.path.abspath, items)))
+print()
+
+items = glob.glob('../**/*.txt', recursive=True)
+print(items)
+print(list(map(os.path.abspath, items)))
+print()
+
+items = glob.glob('../**/*.png', recursive=True)
+print(items)
+print(list(map(os.path.abspath, items)))
+print()
