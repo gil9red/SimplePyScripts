2bec607cb7460d7aae2bed32f75e75ab6525024e
gil9red <ilya.petrash@inbox.ru>
2019-02-26 12:16:06 +0500

    Append glob__examples/glob__vs__iglob.py

---
+++ glob__examples/glob__vs__iglob.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://docs.python.org/3.6/library/glob.html#module-glob
+# SOURCE: https://docs.python.org/3.6/library/glob.html#glob.iglob
+
+
+import glob
+import os
+
+
+pattern = '../**/*.py'
+
+# First `glob` will collect all the results, and then return the list
+for file_name in glob.glob(pattern, recursive=True):
+    print(os.path.abspath(file_name))
+
+print('\n' + '-' * 50 + '\n')
+
+# iglob returns an iterator that will return the next pattern found by the path at each iteration
+for file_name in glob.iglob(pattern, recursive=True):
+    print(os.path.abspath(file_name))
