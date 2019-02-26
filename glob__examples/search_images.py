bbe13ddcf422c6e4ed58e7fbe771563b41612d9c
gil9red <ilya.petrash@inbox.ru>
2019-02-26 12:24:32 +0500

    Append glob__examples/search_images.py

---
+++ glob__examples/search_images.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://docs.python.org/3.6/library/glob.html#module-glob
+
+
+import glob
+import os
+
+
+pattern_template = '../**/*.{}'
+patterns = [pattern_template.format(fmt) for fmt in ['jpg', 'jpeg', 'png', 'gif']]
+items = []
+
+for pattern in patterns:
+    items += glob.glob(pattern, recursive=True)
+
+items.sort()
+
+print(items)
+print(list(map(os.path.abspath, items)))
