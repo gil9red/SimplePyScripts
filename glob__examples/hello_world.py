ae1155413ef30223da01ceb733df541a0c38edd6
gil9red <ilya.petrash@inbox.ru>
2019-02-26 12:09:56 +0500

    Append glob__examples/hello_world.py

---
+++ glob__examples/hello_world.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://docs.python.org/3.6/library/glob.html#module-glob
+
+
+import glob
+
+
+items = glob.glob('*.py')
+print(items)
+
+items = list(glob.iglob('*.py'))
+print(items)
+
+print()
+
+for file_name in glob.iglob('*.py'):
+    print(file_name)
