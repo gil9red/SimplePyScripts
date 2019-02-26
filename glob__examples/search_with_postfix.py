906bc8e244774fb5aba05d6e9b33454ad0b07e07
gil9red <ilya.petrash@inbox.ru>
2019-02-26 12:13:24 +0500

    Append glob__examples/search_with_postfix.py

---
+++ glob__examples/search_with_postfix.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://docs.python.org/3.6/library/glob.html#module-glob
+
+
+import glob
+import os
+
+
+items = glob.glob('../**/*_re.py', recursive=True)
+print(items)
+print(list(map(os.path.abspath, items)))
+print()
+
+# Для папки
+items = glob.glob('../**/*_examples/*.py', recursive=True)
+print(items)
+print(list(map(os.path.abspath, items)))
