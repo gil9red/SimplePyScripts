d2b916de7f670e7bb1004ad7d31007203704db13
gil9red <ilya.petrash@inbox.ru>
2019-02-27 18:23:45 +0500

    #

--- functor.py
+++ functor.py
@@ -22,9 +22,28 @@ class MyPow:
         return a ** b
 
 
+class Bar:
+    def __init__(self, start_value=0):
+        self.value = start_value
+
+    def __call__(self, *args, **kwargs):
+        self.value += 1
+        return self
+
+    def __str__(self):
+        return '<Foo(value={self.value})>'.format(self=self)
+
+
 foo = Foo()
 print(foo())  # 1
 
 print(MySum()(1, 2, 3, 4))  # 10
 
 print(MyPow()(2, 10))  # 1024
+
+print(Bar())            # 0
+print(Bar()())          # 1
+print(Bar()()())        # 2
+print(Bar()()()())      # 3
+print(Bar()()()()())    # 4
+print(Bar()()()()()())  # 5
