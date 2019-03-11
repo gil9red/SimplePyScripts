7c6270836c338434671d4eb0bee14d38a8c8501e
gil9red <ilya.petrash@inbox.ru>
2019-03-11 13:25:33 +0500

    #

--- pycryptodome__examples__AES_DES/hello_world__DES.py
+++ pycryptodome__examples__AES_DES/hello_world__DES.py
@@ -14,14 +14,18 @@ __author__ = 'ipetrash'
 
 from Crypto.Cipher import DES
 
+
+# SOURCE: https://github.com/gil9red/SimplePyScripts/blob/99ec5746cc93c8f1c143af0a04a42716d3dc5a16/pycryptodome__examples__AES_DES/info_security.py
+def pad(s: bytes) -> bytes:
+    bs = 8
+    return s + bytes((bs - len(s) % bs) for _ in range(bs - len(s) % bs))
+
+
+def unpad(s: bytes) -> bytes:
+    return s[:-ord(s[len(s) - 1:])]
+
+
 key = b'abcdefgh'
-
-
-def pad(text):
-    while len(text) % 8 != 0:
-        text += b' '
-
-    return text
 
 
 des = DES.new(key, DES.MODE_ECB)
@@ -32,7 +36,7 @@ encrypted_text = des.encrypt(padded_text)
 print(encrypted_text)  # b'\x95\x123.Y\x8dN\x17\xcc\xf1\xc7\\\xc7\xac\xbc\x7f'
 
 data = des.decrypt(encrypted_text)
-print(data)  # b'Hello World!    '
+print(data)  # b'Hello World!\x04\x04\x04\x04'
 
-data = data.rstrip(b' ')
+data = unpad(data)
 print(data)  # b'Hello World!'
