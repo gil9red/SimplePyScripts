b49f843fbc6a7b567c844e4702c64aaae433eae9
gil9red <ilya.petrash@inbox.ru>
2019-03-12 17:56:31 +0500

    Append requests_example/post_file.py

---
+++ requests_example/post_file.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: http://docs.python-requests.org/en/master/user/quickstart/#post-a-multipart-encoded-file
+
+
+import requests
+from os.path import basename
+
+
+url = 'https://httpbin.org/post'
+abs_file_name = __file__
+file_name = basename(abs_file_name)
+
+
+files = {'file': open(abs_file_name, 'rb')}
+rs = requests.post(url, files=files)
+print(rs)
+print(rs.text)
+print()
+
+
+# With file name
+files = {'file': (abs_file_name, open(abs_file_name, 'rb'))}
+rs = requests.post(url, files=files)
+print(rs)
+print(rs.text)
+print()
+
+
+# Send string as file
+files = {'file': ('report.csv', 'some,data,to,send\nanother,row,to,send\n')}
+rs = requests.post(url, files=files)
+print(rs)
+print(rs.text)
