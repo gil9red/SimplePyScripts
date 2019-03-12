7f46654ce9b19a5a7d7ca15cceddd438866ec376
gil9red <ilya.petrash@inbox.ru>
2019-03-12 16:10:12 +0500

    Append pycryptodome__examples__AES_DES_RSA/RSA__examples/read_private_key_and_get_public

---
+++ pycryptodome__examples__AES_DES_RSA/RSA__examples/read_private_key_and_get_public/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install pycryptodome
+# OR:
+# pip install pycryptodomex
+
+
+from Crypto.PublicKey import RSA
+
+
+SECRET_CODE = "Unguessable"
+
+key = RSA.generate(2048)
+encrypted_key = key.export_key(passphrase=SECRET_CODE)
+print(key.publickey().export_key())
+
+# READ
+key = RSA.import_key(encrypted_key, passphrase=SECRET_CODE)
+print(key.publickey().export_key())
