e4599134c4adad19f8a936667107f0067d900e58
gil9red <ilya.petrash@inbox.ru>
2019-03-12 16:01:34 +0500

    Append pycryptodome__examples__AES_DES_RSA/RSA__examples/geпenerate_public_key_and_private_key

---
+++ pycryptodome__examples__AES_DES_RSA/RSA__examples/geпenerate_public_key_and_private_key/main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://github.com/Legrandin/pycryptodome/blob/29810b96cefc900138523203f027a179a15ceffe/Doc/src/examples.rst#generate-public-key-and-private-key
+
+
+# pip install pycryptodome
+# OR:
+# pip install pycryptodomex
+
+
+from Crypto.PublicKey import RSA
+
+
+key = RSA.generate(2048)
+print(key)  # Private RSA key at...
+
+private_key = key.export_key()
+print(private_key)  # b'-----BEGIN RSA PRIVATE KEY-----\n...
+
+with open("private.pem", "wb") as f:
+    f.write(private_key)
+
+public_key = key.publickey().export_key()
+print(public_key)  # b'-----BEGIN PUBLIC KEY-----\nMIIBIj...
+
+with open("public.pem", "wb") as f:
+    f.write(public_key)
