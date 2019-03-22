3dc3c48b22db1dfc4754d426bb651bee1a76a21f
gil9red <ilya.petrash@inbox.ru>
2019-03-22 11:06:35 +0500

    Append psutil_example/loop_kill_process_by_name.py

---
+++ psutil_example/loop_kill_process_by_name.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import time
+
+# pip install psutil
+import psutil
+
+
+while True:
+    for process in psutil.process_iter():
+        if process.name() == 'calc.exe':
+            process.kill()
+
+    # Wait 5 secs
+    time.sleep(5)
