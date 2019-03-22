4cfe625993a50b84b885b7660e55863fe37e9f4b
gil9red <ilya.petrash@inbox.ru>
2019-03-22 11:09:32 +0500

    #

--- psutil_example/process_management/random_process_info.py
+++ psutil_example/process_management/random_process_info.py
@@ -4,14 +4,14 @@
 __author__ = 'ipetrash'
 
 
+import random
+
 # pip install psutil
 import psutil
 
 print('Random process info')
 
 process_pid_list = psutil.pids()
-
-import random
 pid = random.choice(process_pid_list)
 print('Pid:', pid)
 
