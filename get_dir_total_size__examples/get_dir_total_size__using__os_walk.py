57517e33f6873f4489eab57922b9588af999b6d4
gil9red <ilya.petrash@inbox.ru>
2019-03-26 11:10:08 +0500

    Append get_dir_total_size__examples/get_dir_total_size__using__os_walk.py

---
+++ get_dir_total_size__examples/get_dir_total_size__using__os_walk.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://docs.python.org/3/library/os.html#os.walk
+
+
+from os.path import join, getsize
+
+from common import sizeof_fmt
+
+
+def get_dir_total_size(dir_name: str) -> (int, str):
+    total_size = 0
+
+    for root, dirs, files in os.walk(dir_name):
+        total_size += sum(getsize(join(root, name)) for name in files)
+
+    return total_size, sizeof_fmt(total_size)
+
+
+if __name__ == '__main__':
+    import os
+
+    # paths = [r"C:\Users\Default", r"C:\Program Files (x86)", os.path.expanduser(r'~\Desktop')]
+    paths = ['..']
+
+    for path in paths:
+        path = os.path.abspath(path)
+
+        size, size_str = get_dir_total_size(path)
+        print(f'"{path}": {size} bytes / {size_str}')
