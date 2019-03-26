5669711fac60f76d6b82f36282fb1467a31a6561
gil9red <ilya.petrash@inbox.ru>
2019-03-26 10:53:26 +0500

    Append pathlib__examples/search_with_glob.py

---
+++ pathlib__examples/search_with_glob.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import pathlib
+
+
+# Print current folders and files
+for file_name in pathlib.Path().glob('*'):
+    # print abs path
+    print(file_name.resolve())
+
+print()
+
+# Print current folders and files with filter
+for file_name in pathlib.Path().glob('get_*'):
+    # print abs path
+    print(file_name.resolve())
+
+print()
+
+# for file_name in pathlib.Path('../').rglob('*'):
+# OR:
+for file_name in pathlib.Path('../').glob('**/*'):
+    # print abs path
+    print(file_name.resolve())
