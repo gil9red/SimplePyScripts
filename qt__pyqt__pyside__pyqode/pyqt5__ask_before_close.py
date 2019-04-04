25901bb0010a84893c34c2a2c11dfdc348885af3
gil9red <ilya.petrash@inbox.ru>
2019-04-04 11:48:35 +0500

    Append qt__pyqt__pyside__pyqode/pyqt5__ask_before_close.py

---
+++ qt__pyqt__pyside__pyqode/pyqt5__ask_before_close.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.Qt import QWidget, QApplication, QPushButton, QVBoxLayout, QMessageBox
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        button = QPushButton('Close')
+        button.clicked.connect(self.close)
+
+        layout = QVBoxLayout()
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+
+    def closeEvent(self, event):
+        reply = QMessageBox.question(
+            self, "Quit", "Are you sure you want to quit?",
+            QMessageBox.Yes, QMessageBox.No
+        )
+
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    mw = MainWindow()
+    mw.show()
+
+    app.exec()
