2ef0bf0b4e5b2b419d0fcfaf5aae53e13c8c6a58
ipetrash <ilya.petrash@inbox.ru>
2019-04-06 17:29:51 +0500

    #

--- html_parsing/get_set_sushi_list__sushivkusno.com.py
+++ html_parsing/get_set_sushi_list__sushivkusno.com.py
@@ -4,22 +4,22 @@
 __author__ = 'ipetrash'
 
 
+import re
+from bs4 import BeautifulSoup
 import requests
+
+
 rs = requests.get('http://sushivkusno.com/category/nabory-siety')
 print(rs)
 
-from bs4 import BeautifulSoup
 root = BeautifulSoup(rs.content, 'lxml')
-
 items = root.select('.CardContent')
-
-import re
 
 for i, product in enumerate(items, 1):
     title = product.select_one('.CardText__title').text
     weight = product.select_one('.CardText__subtitle > span > b').text
 
-    price = product.select_one('[itemprop="price"] > span').text
+    price = product.select_one('.ProductParams__price').text
     price = int(re.sub('\D', '', price))
 
     print('{}. "{}": {}, {}'.format(i, title, weight, price))
