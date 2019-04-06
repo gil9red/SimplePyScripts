aaf4518cbe25a1ffc9ab4d0965760bc100a6e65a
ipetrash <ilya.petrash@inbox.ru>
2019-04-07 01:21:43 +0500

    Append play_sound__pyqt5/main.py

---
+++ play_sound__pyqt5/main.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.QtCore import QCoreApplication, QUrl, QTimer
+from PyQt5.QtMultimedia import QMediaPlaylist, QMediaContent, QMediaPlayer
+import os
+
+
+def play(file_name: str):
+    if not os.path.exists(file_name):
+        raise FileNotFoundError(file_name)
+
+    app = QCoreApplication([])
+
+    url = QUrl.fromLocalFile(file_name)
+
+    playlist = QMediaPlaylist()
+    playlist.addMedia(QMediaContent(url))
+
+    player = QMediaPlayer()
+    player.setPlaylist(playlist)
+    player.play()
+
+    def tick():
+        if player.state() == QMediaPlayer.StoppedState:
+            app.quit()
+
+    timer = QTimer()
+    timer.timeout.connect(tick)
+    timer.start(1000)
+
+    app.exec()
+
+
+def play_async(file_name: str):
+    # from threading import Thread
+    # thread = Thread(target=play, args=(file_name,))
+    # thread.start()
+
+    from multiprocessing import Process
+    process = Process(target=play, args=(file_name,))
+    process.start()
+
+
+if __name__ == '__main__':
+    file_name = r'..\speak\play_mp3\speak_male.mp3'
+
+    play(file_name)
+
+    import time
+    play_async(file_name)
+    time.sleep(0.2)
+    play_async(file_name)
+    time.sleep(0.2)
+    play_async(file_name)
