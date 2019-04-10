f08a5c668228d3135b6410327df41fa00e27f634
gil9red <ilya.petrash@inbox.ru>
2019-04-10 15:32:34 +0500

    Append "Printing all instances of a class"

---
+++ Printing all instances of a class/use__garbage_collector__gc.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://stackoverflow.com/a/328882/5909792
+
+
+import gc
+
+
+def get_instances(class_: type) -> [type]:
+    return [obj for obj in gc.get_objects() if isinstance(obj, class_)]
+
+
+class X:
+    def __init__(self, name):
+        self.name = name
+
+
+x = X("x")
+y = X("y")
+print([r.name for r in get_instances(X)])  # ['x', 'y']
+
+del y
+
+print([r.name for r in get_instances(X)])  # ['x']
---
+++ Printing all instances of a class/use__mixin_and_weakrefs.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://stackoverflow.com/a/328882/5909792
+
+
+from collections import defaultdict
+import weakref
+
+
+class KeepRefs:
+    __refs__ = defaultdict(list)
+
+    def __init__(self):
+        self.__refs__[self.__class__].append(weakref.ref(self))
+
+    @classmethod
+    def get_instances(cls):
+        for inst_ref in cls.__refs__[cls]:
+            inst = inst_ref()
+            if inst is not None:
+                yield inst
+
+
+class X(KeepRefs):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+
+
+x = X("x")
+y = X("y")
+print([r.name for r in X.get_instances()])  # ['x', 'y']
+
+del y
+
+print([r.name for r in X.get_instances()])  # ['x']
