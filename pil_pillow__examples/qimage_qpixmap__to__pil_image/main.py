964c5dd11f799092eb07917c3b866dac0cc3527e
gil9red <ilya.petrash@inbox.ru>
2019-04-12 15:33:47 +0500

    Append pil_pillow__examples/qimage_qpixmap__to__pil_image

---
+++ pil_pillow__examples/qimage_qpixmap__to__pil_image/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install Pillow
+from PIL import ImageQt
+
+# pip install pyqt5
+from PyQt5.Qt import QImage, QPixmap, QApplication
+
+image_file = "input.jpg"
+
+app = QApplication([])
+
+img_image = QImage(image_file)
+pix_image = QPixmap(image_file)
+
+pil_img_image = ImageQt.fromqimage(img_image)
+pil_img_image.show()
+
+pil_pix_image = ImageQt.fromqimage(pix_image)
+pil_pix_image.show()
