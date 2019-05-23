3fd0097e75afe1a2f914b46cfc433e52d9e256b1
gil9red <ilya.petrash@inbox.ru>
2019-05-23 12:40:12 +0500

    Append enum__examples/random_value.py

---
+++ enum__examples/random_value.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from enum import Enum, auto
+from random import choice, choices
+
+
+class MoveEnum(Enum):
+    UP = auto()
+    RIGHT = auto()
+    DOWN = auto()
+    LEFT = auto()
+
+
+for _ in range(5):
+    button = choice(list(MoveEnum))
+    print(f'{button:<14} {button.name:<5} {button.value}')
+
+print()
+print(choices(list(MoveEnum), k=5))
