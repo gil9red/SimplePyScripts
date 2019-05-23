dadead16470c853d0e56d81c7f9e9c7f13b61223
gil9red <ilya.petrash@inbox.ru>
2019-05-23 12:34:30 +0500

    Append enum__examples/value_as_title.py

---
+++ enum__examples/value_as_title.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from enum import Enum, auto
+
+
+class KeyboardEnum(Enum):
+    ALL = auto()
+    YES = auto()
+    NO = auto()
+    CANCEL = auto()
+    GET_ALL = auto()
+    SEND_ALL = auto()
+
+    def title(self):
+        return self.name.replace("_", " ")
+
+
+print(KeyboardEnum.GET_ALL)          # KeyboardEnum.GET_ALL
+print(KeyboardEnum.GET_ALL.title())  # GET ALL
+print()
+
+from random import choice
+button = choice(list(KeyboardEnum))
+print(f'{button}: "{button.title()}"')
+
+print(KeyboardEnum.SEND_ALL)
