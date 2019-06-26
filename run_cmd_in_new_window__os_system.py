66e15ed4c88524a7635ce552690b1732e7f8772c
gil9red <ilya.petrash@inbox.ru>
2019-06-26 14:23:15 +0500

    Append:     run_cmd_in_new_window__os_system.py     run_cmd_in_new_window__subprocess.py

---
+++ run_cmd_in_new_window__os_system.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import os
+os.system("start cmd.exe @cmd /k ipconfig")
---
+++ run_cmd_in_new_window__subprocess.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import subprocess
+subprocess.run(["start", "cmd.exe", "@cmd", "/k", "ipconfig"], shell=True)
