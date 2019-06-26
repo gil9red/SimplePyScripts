2ba260415891dd4fcb1a11235c9dffad3e2d5267
gil9red <ilya.petrash@inbox.ru>
2019-06-26 14:28:48 +0500

    Append qt__pyqt__pyside__pyqode/run_cmd_in_new_window.py

---
+++ qt__pyqt__pyside__pyqode/run_cmd_in_new_window.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import sys
+from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QVBoxLayout, QMessageBox
+import subprocess
+import os
+
+
+def log_uncaught_exceptions(ex_cls, ex, tb):
+    text = '{}: {}:\n'.format(ex_cls.__name__, ex)
+    import traceback
+    text += ''.join(traceback.format_tb(tb))
+
+    print(text)
+    QMessageBox.critical(None, 'Error', text)
+    quit()
+
+sys.excepthook = log_uncaught_exceptions
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.cmd_command = "start cmd.exe @cmd /k ipconfig"
+
+        button_run_subprocess = QPushButton('Run subprocess')
+        button_run_subprocess.clicked.connect(self.btn_clicked_subprocess)
+
+        button_os_system = QPushButton('Run os.system')
+        button_os_system.clicked.connect(self.btn_clicked_subprocess)
+
+        layout = QVBoxLayout()
+        layout.addWidget(button_run_subprocess)
+        layout.addWidget(button_os_system)
+
+        self.setLayout(layout)
+
+    def btn_clicked_subprocess(self):
+        subprocess.run(self.cmd_command.split(), shell=True)
+
+    def btn_clicked_os_system(self):
+        os.system(self.cmd_command)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    mw = MainWindow()
+    mw.show()
+
+    sys.exit(app.exec_())
