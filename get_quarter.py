4e4efd7467795aa5881c8b22a5829314ba71f409
gil9red <ilya.petrash@inbox.ru>
2019-06-27 11:15:10 +0500

    #

--- get_quarter.py
+++ get_quarter.py
@@ -4,14 +4,18 @@
 __author__ = 'ipetrash'
 
 
-def get_quarter(date__or__datetime=None) -> int:
-    dt = date__or__datetime
+import datetime as DT
 
+
+def get_quarter(month_or_date=None) -> int:
+    dt = month_or_date
     if dt is None:
-        import datetime as DT
         dt = DT.date.today()
 
-    month = dt.month
+    if isinstance(dt, int):
+        month = dt
+    else:
+        month = dt.month
 
     if month in (1, 2, 3):
         return 1
@@ -29,20 +33,25 @@ def get_quarter(date__or__datetime=None) -> int:
         raise Exception('Invalid "month": {}'.format(month))
 
 
-def get_current_quarter() -> int:
-    return get_quarter()
-
-
 if __name__ == '__main__':
     print(get_quarter())
-    print(get_current_quarter())
     print()
-
-    import datetime as DT
-
     print(get_quarter(DT.datetime.today()))
     print(get_quarter(DT.date.today()))
     print()
 
     for dt in [DT.date(2018, month=i + 1, day=1) for i in range(12)]:
         print(dt, get_quarter(dt))
+
+    assert get_quarter(1) == 1
+    assert get_quarter(2) == 1
+    assert get_quarter(3) == 1
+    assert get_quarter(4) == 2
+    assert get_quarter(5) == 2
+    assert get_quarter(6) == 2
+    assert get_quarter(7) == 3
+    assert get_quarter(8) == 3
+    assert get_quarter(9) == 3
+    assert get_quarter(10) == 4
+    assert get_quarter(11) == 4
+    assert get_quarter(12) == 4
