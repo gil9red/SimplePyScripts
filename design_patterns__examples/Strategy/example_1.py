95f676f67f6285c1f60801084e6c6e829e7bf675
gil9red <ilya.petrash@inbox.ru>
2019-06-27 17:01:48 +0500

    Append design_patterns__examples/Strategy/example_1.py

---
+++ design_patterns__examples/Strategy/example_1.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: Design Patterns: Strategy — Стратегия
+# SOURCE: https://ru.wikipedia.org/wiki/Стратегия_(шаблон_проектирования)
+
+
+from abc import ABC, abstractmethod
+
+
+class Strategy(ABC):
+    @abstractmethod
+    def action(self, a, b):
+        pass
+
+
+class AdditionStrategy(Strategy):
+    def action(self, a, b):
+        return a + b
+
+
+class SubtractionStrategy(Strategy):
+    def action(self, a, b):
+        return a - b
+
+
+class Context:
+    def __init__(self, strategy: Strategy):
+        self._strategy = strategy
+
+    def set_strategy(self, strategy: Strategy):
+        self._strategy = strategy
+
+    def action(self, a, b):
+        return self._strategy.action(a, b)
+
+
+if __name__ == '__main__':
+    context = Context(AdditionStrategy())
+    print(context.action(10, 5))  # 15
+    print()
+
+    context = Context(SubtractionStrategy())
+    print(context.action(10, 5))  # 5
+
+    context.set_strategy(AdditionStrategy())
+    print(context.action(10, 5))  # 15
