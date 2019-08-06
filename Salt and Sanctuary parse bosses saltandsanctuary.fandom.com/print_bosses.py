05014056bacab35a7aa24b587e26c7017aef2922
ipetrash <ilya.petrash@inbox.ru>
2019-08-06 22:49:15 +0500

    #

--- Salt and Sanctuary parse bosses saltandsanctuary.fandom.com/print_bosses.py
+++ Salt and Sanctuary parse bosses saltandsanctuary.fandom.com/print_bosses.py
@@ -56,7 +56,7 @@ if __name__ == '__main__':
     #         10. "Карсджоу Жестокий": https://saltandsanctuary.fandom.com/ru/wiki/%D0%9A%D0%B0%D1%80%D1%81%D0%B4%D0%B6%D0%BE%D1%83_%D0%96%D0%B5%D1%81%D1%82%D0%BE%D0%BA%D0%B8%D0%B9
     #         11. "Забытый король": https://saltandsanctuary.fandom.com/ru/wiki/%D0%97%D0%B0%D0%B1%D1%8B%D1%82%D1%8B%D0%B9_%D0%BA%D0%BE%D1%80%D0%BE%D0%BB%D1%8C
 
-    print_bosses(bosses_by_category, only_name=True)
+    print_bosses(bosses_by_category, only_names=True)
     # Salt and Sanctuary (23):
     #     Обязательные боссы (12):
     #         1. "Обезумевший рыцарь"
