0aa2cc3ac742d703ce3ddbc2a1f571a0b8c4c6e3
gil9red <ilya.petrash@inbox.ru>
2019-08-16 14:57:20 +0500

    Append Decorators__examples/memoize_class.py

---
+++ Decorators__examples/memoize_class.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# Using memoization as decorator (decorator-class)
+class MemoizeClass:
+    def __init__(self, fn):
+        self.fn = fn
+        self.memo = dict()
+
+    def __call__(self, arg):
+        if arg not in self.memo:
+            self.memo[arg] = self.fn(arg)
+            return self.memo[arg]
+
+
+@MemoizeClass
+def fib(n):
+    a, b = 1, 1
+    for i in range(n - 1):
+        a, b = b, a + b
+    return a
+
+
+print(fib(1000))
