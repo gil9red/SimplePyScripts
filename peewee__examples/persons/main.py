198188e834d2037c60dd5cf4d9b31b805b1cfa33
gil9red <ilya.petrash@inbox.ru>
2019-08-19 17:29:57 +0500

    Append peewee__examples\persons

---
+++ peewee__examples/persons/main.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import datetime as DT
+import json
+
+# pip install peewee
+from peewee import *
+
+
+db = SqliteDatabase('persons.sqlite')
+
+
+class Person(Model):
+    name = CharField()
+    birthday = DateField()
+
+    class Meta:
+        database = db
+
+
+class Pet(Model):
+    owner = ForeignKeyField(Person, backref='pets')
+    name = CharField()
+    animal_type = CharField()
+
+    class Meta:
+        database = db
+
+
+db.connect()
+db.create_tables([Person, Pet])
+
+for person_data in json.load(open('persons.json', encoding='utf-8')):
+    birthday = DT.datetime.strptime(person_data['birthday'], '%Y-%M-%d')
+    person, _ = Person.get_or_create(name=person_data['name'], birthday=birthday)
+
+    for pet in person_data['pets']:
+        Pet.get_or_create(owner=person, name=pet['name'], animal_type=pet['animal_type'])
+
+
+for person in Person.select():
+    print(f'{person.name} ({person.birthday}). Pets: {person.pets.count()}')
+
+    for pet in person.pets:
+        print(f'    {pet.name} ({pet.animal_type}). Owner: {pet.owner.name}')
+
+    print()
+
+# Bob (1960-01-15). Pets: 1
+#     Kitty (cat). Owner: Bob
+#
+# Grandma (1935-01-01). Pets: 0
+#
+# Herb (1950-01-05). Pets: 3
+#     Fido (dog). Owner: Herb
+#     Mittens (cat). Owner: Herb
+#     Mittens Jr (cat). Owner: Herb
