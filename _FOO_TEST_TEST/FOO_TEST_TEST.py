777137a41b5230a2b4736efe71636269973d2cc5
ipetrash <ilya.petrash@inbox.ru>
2019-08-19 23:29:38 +0500

    #

--- _FOO_TEST_TEST/FOO_TEST_TEST.py
+++ _FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -14,6 +14,42 @@ from bs4 import BeautifulSoup
 import requests
 
 
+def wait(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0):
+    from datetime import timedelta, datetime
+    today = datetime.today()
+    timeout_date = today + timedelta(
+        days=days, seconds=seconds, microseconds=microseconds,
+        milliseconds=milliseconds, minutes=minutes, hours=hours, weeks=weeks
+    )
+
+    while today <= timeout_date:
+        def str_timedelta(td):
+            # Remove ms
+            td = str(td)
+            if '.' in td:
+                td = td[:td.index('.')]
+
+            return td
+
+        left = timeout_date - today
+        left = str_timedelta(left)
+
+        print('\r' * 100, end='')
+        print('До следующего запуска осталось {}'.format(left), end='')
+
+        import sys
+        sys.stdout.flush()
+
+        # Delay 1 seconds
+        import time
+        time.sleep(1)
+
+        today = datetime.today()
+
+    print('\r' * 100, end='')
+    print('\n')
+
+
 def get_price(url: str) -> Optional[int]:
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0',
@@ -104,7 +140,7 @@ while True:
             # print(list(product.prices), last_price.value if last_price else None)
             # print()
 
-        time.sleep(24 * 3600)  # 24 hours
+        wait(days=1)
 
     except Exception as e:
         # Выводим ошибку в консоль
@@ -113,6 +149,3 @@ while True:
         print(tb)
 
         time.sleep(15 * 60)  # 60 minutes
-
-
-
