e02278bbad6fb822af424e0866687068e31b3cba
gil9red <ilya.petrash@inbox.ru>
2019-08-19 17:39:28 +0500

    Append peewee__examples/hello_world.py

---
+++ peewee__examples/hello_world.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install peewee
+from peewee import *
+
+
+db = SqliteDatabase('my_database.sqlite')
+
+
+class Info(Model):
+    first_name = TextField()
+    second_name = TextField()
+    state = IntegerField()
+
+    class Meta:
+        database = db
+
+    def __str__(self):
+        return f'Info<#{self.id} first_name={self.first_name} second_name={self.second_name} state={self.state}>'
+
+
+db.connect()
+db.create_tables([Info])
+
+
+# Вызываем впервый раз, чтобы заполнить таблицу
+if not Info.select().count():
+    for i in range(5):
+        info = Info.create(first_name="first_name_" + str(i), second_name="second_name_" + str(i), state=i)
+        info.save()
+
+for info in Info.select():
+    print(info)
+#
+# Info<#1 first_name=first_name_0 second_name=second_name_0 state=0>
+# Info<#2 first_name=first_name_1 second_name=second_name_1 state=1>
+# Info<#3 first_name=first_name_2 second_name=second_name_2 state=2>
+# Info<#4 first_name=first_name_3 second_name=second_name_3 state=3>
+# Info<#5 first_name=first_name_4 second_name=second_name_4 state=4>
+
+for info in Info.select():
+    if info.state % 2 == 0:
+        info.first_name = "Четный state!"
+        info.save()
+
+print()
+
+for info in Info.select():
+    print(info)
+#
+# Info<#1 first_name=Четный state! second_name=second_name_0 state=0>
+# Info<#2 first_name=first_name_1 second_name=second_name_1 state=1>
+# Info<#3 first_name=Четный state! second_name=second_name_2 state=2>
+# Info<#4 first_name=first_name_3 second_name=second_name_3 state=3>
+# Info<#5 first_name=Четный state! second_name=second_name_4 state=4>
