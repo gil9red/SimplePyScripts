cab886b15071ee7d3b9887864683bec2a0d677fb
gil9red <ilya.petrash@inbox.ru>
2019-08-27 14:36:12 +0500

    Append secrets__examples/generate_token_urlsafe.py

---
+++ secrets__examples/generate_token_urlsafe.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://docs.python.org/3/library/secrets.html#secrets.token_urlsafe
+
+
+from secrets import token_urlsafe
+
+
+if __name__ == '__main__':
+    # Example:
+    url = 'https://mydomain.com/reset=' + token_urlsafe()
+    print(url)
+    print()
+
+    for i in (None, 8, 8, 16, 32):
+        token = token_urlsafe() if i is None else token_urlsafe(i)
+        print(f'[{len(token)}]: {token}')
