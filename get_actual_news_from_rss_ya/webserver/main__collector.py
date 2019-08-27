378403d6a187102507ef44aaae7d5930969ebe52
ipetrash <ilya.petrash@inbox.ru>
2019-08-28 00:46:52 +0500

    #

--- get_actual_news_from_rss_ya/webserver/main__collector.py
+++ get_actual_news_from_rss_ya/webserver/main__collector.py
@@ -12,6 +12,12 @@ def get_news_from_rss(url_rss) -> list:
 
 def wait(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0):
     from datetime import timedelta, datetime
+    from itertools import cycle
+    import sys
+    import time
+
+    progress_bar = cycle('|/-\\|/-\\')
+
     today = datetime.today()
     timeout_date = today + timedelta(
         days=days, seconds=seconds, microseconds=microseconds,
@@ -19,25 +25,28 @@ def wait(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, 
     )
 
     while today <= timeout_date:
-        def str_timedelta(td):
-            # Remove ms
+        def str_timedelta(td: timedelta) -> str:
             td = str(td)
+
+            # Remove ms
+            # 0:01:40.123000 -> 0:01:40
             if '.' in td:
-                td = td[:td.index('.')]
+                td = td[:td.rindex('.')]
+
+            # 0:01:40 -> 00:01:40
+            if td.startswith('0:'):
+                td = '00:' + td[2:]
 
             return td
 
         left = timeout_date - today
         left = str_timedelta(left)
 
-        print('\r' * 100, end='')
-        print('До следующего запуска осталось {}'.format(left), end='')
-
-        import sys
+        print('\r' + ' ' * 100 + '\r', end='')
+        print('[{}] До следующего запуска осталось {}'.format(next(progress_bar), left), end='')
         sys.stdout.flush()
 
         # Delay 1 seconds
-        import time
         time.sleep(1)
 
         today = datetime.today()
