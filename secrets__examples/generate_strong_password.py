91df0cfcd904fd7e6c3bc4b7da91c044b4883d52
gil9red <ilya.petrash@inbox.ru>
2019-08-27 14:25:08 +0500

    Append secrets__examples/generate_strong_password.py

---
+++ secrets__examples/generate_strong_password.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://docs.python.org/3/library/secrets.html
+
+
+from secrets import choice
+import string
+
+
+def generate_password(length=8) -> str:
+    # Generate a ten-character alphanumeric password with at least one lowercase character,
+    # at least one uppercase character, and at least three digits:
+    alphabet = string.ascii_letters + string.digits
+
+    while True:
+        password = ''.join(choice(alphabet) for _ in range(length))
+        if (any(c.islower() for c in password)
+                and any(c.isupper() for c in password)
+                and sum(c.isdigit() for c in password) >= 3):
+            break
+
+    return password
+
+
+if __name__ == '__main__':
+    print(generate_password())
+    print(generate_password())
+    print()
+
+    for i in (8, 8, 16, 32):
+        password = generate_password(length=i)
+        print(f'[{len(password)}]: {password}')
