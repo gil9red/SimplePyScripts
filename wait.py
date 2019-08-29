cd87ff0c4c6bbd63c84bd859cf467da3d0e6e443
gil9red <ilya.petrash@inbox.ru>
2019-08-29 15:42:48 +0500

    Append wait.py

---
+++ wait.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+def wait(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0):
+    from datetime import timedelta, datetime
+    from itertools import cycle
+    import sys
+    import time
+
+    try:
+        progress_bar = cycle('|/-\\|/-\\')
+
+        today = datetime.today()
+        timeout_date = today + timedelta(
+            days=days, seconds=seconds, microseconds=microseconds,
+            milliseconds=milliseconds, minutes=minutes, hours=hours, weeks=weeks
+        )
+
+        while today <= timeout_date:
+            def str_timedelta(td: timedelta) -> str:
+                td = str(td)
+
+                # Remove ms
+                # 0:01:40.123000 -> 0:01:40
+                if '.' in td:
+                    td = td[:td.rindex('.')]
+
+                # 0:01:40 -> 00:01:40
+                if td.startswith('0:'):
+                    td = '00:' + td[2:]
+
+                return td
+
+            left = timeout_date - today
+            left = str_timedelta(left)
+
+            print('\r' + ' ' * 100 + '\r', end='')
+            print('[{}] Time left: {}'.format(next(progress_bar), left), end='')
+            sys.stdout.flush()
+
+            # Delay 1 seconds
+            time.sleep(1)
+
+            today = datetime.today()
+
+        print('\r' + ' ' * 100 + '\r', end='')
+
+    except KeyboardInterrupt:
+        print()
+        print('Waiting canceled')
+
+
+if __name__ == '__main__':
+    wait(seconds=1)
+    wait(seconds=3)
+    wait(seconds=5)
+
+    print('Start wait')
+    wait(seconds=1)
+    print('Finish wait')
+
+    import datetime as DT
+    while True:
+        print()
+        print('Current datetime:', DT.datetime.now())
+        print()
+        wait(minutes=1, seconds=30)
