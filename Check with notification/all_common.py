8fa9b9c23d10b5ee7ff0161da997b463f7a861bf
gil9red <ilya.petrash@inbox.ru>
2019-08-30 18:04:14 +0500

    Moved: wait

--- Check with notification/all_common.py
+++ Check with notification/all_common.py
@@ -24,53 +24,11 @@ def make_backslashreplace_console():
             pass
 
 
-def wait(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0):
-    from datetime import timedelta, datetime
-    from itertools import cycle
-    import sys
-    import time
+# Import https://github.com/gil9red/SimplePyScripts/blob/f75070a1d2066167a46e2f5b1e98e804101fdc14/wait/wait.py
+import sys
+sys.path.append('../wait')
 
-    try:
-        progress_bar = cycle('|/-\\|/-\\')
-
-        today = datetime.today()
-        timeout_date = today + timedelta(
-            days=days, seconds=seconds, microseconds=microseconds,
-            milliseconds=milliseconds, minutes=minutes, hours=hours, weeks=weeks
-        )
-
-        def str_timedelta(td: timedelta) -> str:
-            td = str(td)
-
-            # Remove ms
-            # 0:01:40.123000 -> 0:01:40
-            if '.' in td:
-                td = td[:td.rindex('.')]
-
-            # 0:01:40 -> 00:01:40
-            if td.startswith('0:'):
-                td = '00:' + td[2:]
-
-            return td
-
-        while today <= timeout_date:
-            left = timeout_date - today
-            left = str_timedelta(left)
-
-            print('\r' + ' ' * 100 + '\r', end='')
-            print('[{}] Time left to wait: {}'.format(next(progress_bar), left), end='')
-            sys.stdout.flush()
-
-            # Delay 1 seconds
-            time.sleep(1)
-
-            today = datetime.today()
-
-        print('\r' + ' ' * 100 + '\r', end='')
-
-    except KeyboardInterrupt:
-        print()
-        print('Waiting canceled')
+from wait import wait
 
 
 def get_logger(name, file='log.txt', encoding='utf-8', log_stdout=True, log_file=True) -> 'logging.Logger':
