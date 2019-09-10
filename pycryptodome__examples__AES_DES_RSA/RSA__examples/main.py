9850fc1be1579ab7815c2a3c05f397824120e04a
gil9red <ilya.petrash@inbox.ru>
2019-09-10 19:32:23 +0500

    #

--- pycryptodome__examples__AES_DES_RSA/RSA__examples/main.py
+++ pycryptodome__examples__AES_DES_RSA/RSA__examples/main.py
@@ -16,10 +16,11 @@ msg1 = "Hello Tony, I am Jarvis!".encode('utf-8')
 msg2 = "Hello Toni, I am Jarvis!".encode('utf-8')
 
 public, private = rsa.new_keys(key_size=2048)
-print(private.exportKey('PEM'))
-print(public.exportKey('PEM'))
+print('private:', private.exportKey('PEM'))
+print('public:', public.exportKey('PEM'))
 print()
 
+# Encrypt-Decrypt: private -> private
 encrypted = b64encode(rsa.encrypt(msg1, private))
 print("Encrypted:", encrypted)
 
@@ -31,8 +32,31 @@ signature = b64encode(rsa.sign(msg1, private))
 print("Signature:", signature)
 print()
 
+verify = rsa.verify(msg1, b64decode(signature), private)
+print("Verify:", verify)  # True
+
+verify = rsa.verify(msg2, b64decode(signature), private)
+print("Verify:", verify)  # False
+# Encrypt-Decrypt: private -> private
+
+print('\n')
+
+# Encrypt-Decrypt: public -> private
+encrypted = b64encode(rsa.encrypt(msg1, public))
+print("Encrypted:", encrypted)
+
+decrypted = rsa.decrypt(b64decode(encrypted), private)
+print("Decrypted:", decrypted)
+print()
+
+# TODO: signature with public don't working
+# signature = b64encode(rsa.sign(msg1, public))
+# print("Signature:", signature)
+# print()
+
 verify = rsa.verify(msg1, b64decode(signature), public)
 print("Verify:", verify)  # True
 
 verify = rsa.verify(msg2, b64decode(signature), public)
 print("Verify:", verify)  # False
+# Encrypt-Decrypt: public -> private
