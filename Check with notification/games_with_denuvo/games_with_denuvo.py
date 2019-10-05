b5c6a4424b0ecf4b06b37c893e04bdf31955f7a1
ipetrash <ilya.petrash@inbox.ru>
2019-10-06 00:46:32 +0500

    #

--- Check with notification/games_with_denuvo/games_with_denuvo.py
+++ Check with notification/games_with_denuvo/games_with_denuvo.py
@@ -4,6 +4,10 @@
 __author__ = 'ipetrash'
 
 
+import requests
+from bs4 import BeautifulSoup
+
+
 def get_games_with_denuvo() -> [(str, bool)]:
     """
     Функция со страницы википедии вытаскивает список игр с Denuvo и их статус "Взломан".
@@ -12,11 +16,8 @@ def get_games_with_denuvo() -> [(str, bool)]:
 
     url = 'https://ru.wikipedia.org/wiki/Список_игр,_защищённых_Denuvo'
 
-    import requests
     rs = requests.get(url)
-
-    from bs4 import BeautifulSoup
-    root = BeautifulSoup(rs.content, 'lxml')
+    root = BeautifulSoup(rs.content, 'html.parser')
 
     # Таблица "Список защищённых игр"
     table = root.select('.wikitable')[0]
@@ -44,12 +45,52 @@ def get_games_with_denuvo() -> [(str, bool)]:
     return games
 
 
+def get_games_which_denuvo_is_removed() -> [str]:
+    """
+    Функция со страницы википедии вытаскивает список игр в которых убрана защита Denuvo.
+
+    """
+
+    url = 'https://ru.wikipedia.org/wiki/Список_игр,_защищённых_Denuvo'
+
+    rs = requests.get(url)
+    root = BeautifulSoup(rs.content, 'html.parser')
+
+    # Таблица "Список игр, в которых убрана защита"
+    table = root.select('.wikitable')[1]
+
+    games = list()
+
+    for tr in table.select('tr'):
+        td_list = tr.select('td')
+
+        # Например, текущий tr -- заголовок таблицы и не содержит td
+        if not td_list:
+            continue
+
+        name = td_list[0].text.strip()
+
+        # Удаление сносок в названии: "Mad Max[а 1]", "Deus Ex: Mankind Divided[а 3]"
+        if '[' in name and ']' in name:
+            name = name[:name.index('[')]
+
+        games.append(name)
+
+    return games
+
+
 if __name__ == '__main__':
     games_with_denuvo = get_games_with_denuvo()
-
-    print('Всего игр с Denuvo {}: {}\n'.format(len(games_with_denuvo), games_with_denuvo))
+    print(f'Всего игр с Denuvo {len(games_with_denuvo)}: {games_with_denuvo}\n')
 
     cracked_games = list(filter(lambda x: x[1], games_with_denuvo))
-    print('Взломанные ({}):'.format(len(cracked_games)))
+    print(f'Взломанные ({len(cracked_games)}):')
     for game in sorted(game for game, is_cracked in cracked_games):
         print(game)
+
+    print('\n')
+
+    games_without_denuvo = get_games_which_denuvo_is_removed()
+    print(f'Всего игр в которых убрана защита Denuvo ({len(games_without_denuvo)}):')
+    for name in games_without_denuvo:
+        print(name)
