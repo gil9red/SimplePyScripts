00a313e82bcbd9f41c4c604052597c07873ad0a0
gil9red <ilya.petrash@inbox.ru>
2019-10-11 12:02:30 +0500

    Added qt__pyqt__pyside__pyqode/RunFuncThread.py

---
+++ qt__pyqt__pyside__pyqode/RunFuncThread.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.QtCore import QThread, pyqtSignal
+
+
+class RunFuncThread(QThread):
+    run_finished = pyqtSignal(object)
+
+    def __init__(self, func):
+        super().__init__()
+
+        self.func = func
+
+    def run(self):
+        self.run_finished.emit(self.func())
+
+
+if __name__ == '__main__':
+    from PyQt5.QtCore import QCoreApplication
+    app = QCoreApplication([])
+
+    thread = RunFuncThread(func=lambda: 2 + 2 * 2)
+    thread.run_finished.connect(print)
+    thread.finished.connect(app.exit)  # Без app.exec() слоты не сработают
+    thread.start()
+
+    app.exec()
