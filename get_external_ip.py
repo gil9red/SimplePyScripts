1d251974489c0671d99067eaefb03048098af828
gil9red <ilya.petrash@inbox.ru>
2019-10-11 11:34:07 +0500

    Added get_external_ip.py

---
+++ get_external_ip.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import requests
+
+
+# SOURCE: https://stackoverflow.com/a/54585203/5909792
+def get_external_ip() -> str:
+    return requests.get('https://www.wikipedia.org').headers['X-Client-IP']
+
+
+if __name__ == '__main__':
+    print(get_external_ip())
