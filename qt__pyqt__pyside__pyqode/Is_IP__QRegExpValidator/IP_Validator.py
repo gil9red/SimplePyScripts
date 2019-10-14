0d88af83136ce7e7c1e48cce20a81e17e170a833
gil9red <ilya.petrash@inbox.ru>
2019-10-14 11:13:38 +0500

    #

---
+++ qt__pyqt__pyside__pyqode/Is_IP__QRegExpValidator/IP_Validator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.QtGui import QRegularExpressionValidator
+from PyQt5.QtCore import QRegularExpression
+
+from is_ip import IP_REGEXP
+
+
+def get_ip_validator() -> QRegularExpressionValidator:
+    return QRegularExpressionValidator(QRegularExpression(IP_REGEXP))
--- qt__pyqt__pyside__pyqode/Is_IP__QRegExpValidator/main.py
+++ qt__pyqt__pyside__pyqode/Is_IP__QRegExpValidator/main.py
@@ -5,17 +5,15 @@ __author__ = 'ipetrash'
 
 
 from PyQt5.QtWidgets import QLineEdit, QApplication
-from PyQt5.QtGui import QRegularExpressionValidator
-from PyQt5.QtCore import QRegularExpression
 
-from is_ip import IP_REGEXP
+from IP_Validator import get_ip_validator
 
 
 if __name__ == '__main__':
     app = QApplication([])
 
     mw = QLineEdit()
-    mw.setValidator(QRegularExpressionValidator(QRegularExpression(IP_REGEXP)))
+    mw.setValidator(get_ip_validator())
     mw.setText("0.0.0.0")
     mw.show()
 
