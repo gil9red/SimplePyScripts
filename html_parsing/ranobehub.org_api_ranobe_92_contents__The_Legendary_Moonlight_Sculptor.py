6b383c498b541328aee789ed48e6f6286b3b825f
ipetrash <ilya.petrash@inbox.ru>
2019-11-08 01:08:28 +0500

    Added html_parsing/ranobehub.org_api_ranobe_92_contents__The_Legendary_Moonlight_Sculptor.py

---
+++ html_parsing/ranobehub.org_api_ranobe_92_contents__The_Legendary_Moonlight_Sculptor.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from typing import List
+
+import requests
+
+
+def get_chapters() -> List[str]:
+    rs = requests.get('https://ranobehub.org/api/ranobe/92/contents')
+    rs.raise_for_status()
+
+    items = []
+
+    for volume in rs.json()['data']:
+        for chapter in volume['chapters']:
+            items.append(f'{volume["name"]} - {chapter["name"]}')
+
+    return items
+
+
+if __name__ == '__main__':
+    items = get_chapters()
+
+    print(f'Items ({len(items)}):')
+    for x in items:
+        print(f'    {x}')
