fa0f2b3858ac6b2cafcea7d68d430ab2f08c59c6
ipetrash <ilya.petrash@inbox.ru>
2019-11-23 00:20:35 +0500

    Added html_parsing/get_game_genres/stopgame_ru.py

---
+++ html_parsing/get_game_genres/stopgame_ru.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from typing import List, Tuple
+
+from bs4 import BeautifulSoup
+import requests
+
+from common import smart_comparing_names
+
+
+def search_game_genres(game_name: str) -> List[Tuple[str, List[str]]]:
+    rs = requests.get(f'https://stopgame.ru/search/?s={game_name}&where=games&sort=name')
+    root = BeautifulSoup(rs.content, 'html.parser')
+
+    items = []
+
+    for game_block in root.select('.game-block'):
+        title = game_block.select_one('.title').get_text(strip=True)
+        genres = [a.get_text(strip=True) for a in game_block.select('.game-genre-value > a') if '?genre[]' in a['href']]
+        items.append(
+            (title, genres)
+        )
+
+    return items
+
+
+def get_game_genres(game_name: str) -> List[str]:
+    for game, genres in search_game_genres(game_name):
+        if smart_comparing_names(game, game_name):
+            return genres
+
+    return []
+
+
+if __name__ == '__main__':
+    from common import TEST_GAMES
+    for name in TEST_GAMES:
+        items = search_game_genres(name)
+        print(f'Items ({len(items)}:')
+        for game, genres in items:
+            print(f'    {game!r}: {genres}')
+        print()
+        print(f'Genres of {name!r}: {get_game_genres(name)}')
+
+        print('\n' + '-' * 20 + '\n')
