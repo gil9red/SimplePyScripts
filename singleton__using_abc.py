38a0541f98f8c0f341390da4199f99deb67bc2c2
gil9red <ilya.petrash@inbox.ru>
2019-11-25 16:15:03 +0500

    Added singleton__using_abc.py

---
+++ singleton__using_abc.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://stackoverflow.com/a/39186313/5909792
+
+
+from abc import ABC
+
+
+def singleton(real_cls):
+    class SingletonFactory(ABC):
+        instance = None
+
+        def __new__(cls, *args, **kwargs):
+            if not cls.instance:
+                cls.instance = real_cls(*args, **kwargs)
+            return cls.instance
+
+    SingletonFactory.register(real_cls)
+    return SingletonFactory
+
+
+if __name__ == '__main__':
+    @singleton
+    class MyClass:
+        pass
+
+    x1 = MyClass()
+    x2 = MyClass()
+    x3 = MyClass()
+
+    print(id(x1), id(x2), id(x3), id(MyClass.instance))
+    assert id(x1) == id(x2) == id(x3) == id(MyClass.instance)
