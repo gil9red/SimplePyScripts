028a1400565b7f6ae1714afc0475d6a4bcccaf16
ipetrash <ilya.petrash@inbox.ru>
2019-11-26 22:09:24 +0500

    Added result

--- opencv__examples/invert/main.py
+++ opencv__examples/invert/main.py
@@ -13,9 +13,11 @@ cv2.imshow('img', img)
 # Invert
 img_invert = cv2.bitwise_not(img)
 cv2.imshow('img_invert', img_invert)
+cv2.imwrite('img_invert.jpg', img_invert)
 
 # Gray
 img_invert_gray = cv2.cvtColor(img_invert, cv2.COLOR_BGR2GRAY)
 cv2.imshow('img_invert_gray', img_invert_gray)
+cv2.imwrite('img_invert_gray.jpg', img_invert_gray)
 
 cv2.waitKey()
