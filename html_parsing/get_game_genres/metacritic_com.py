74c97ac7bb2ccd235b8bc0ef74763a1463ba538f
ipetrash <ilya.petrash@inbox.ru>
2019-12-19 22:47:35 +0500

    Added html_parsing/get_game_genres/metacritic_com.py

---
+++ html_parsing/get_game_genres/metacritic_com.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from urllib.parse import urljoin
+from typing import List
+
+from bs4 import BeautifulSoup
+import requests
+
+from common import smart_comparing_names, USER_AGENT, get_norm_text
+
+
+def get_game_genres(game_name: str, need_logs=False) -> List[str]:
+    need_logs and print(f'[+] Search {game_name!r}...')
+
+    headers = {
+        'User-Agent': USER_AGENT,
+    }
+
+    session = requests.session()
+    session.headers.update(headers)
+
+    url = f'https://www.metacritic.com/search/game/{game_name}/results'
+    rs = session.get(url)
+    if not rs.ok:
+        need_logs and print(f'  [-] Something went wrong...: status_code: {rs.status_code}\n{rs.text}')
+        return []
+
+    root = BeautifulSoup(rs.content, 'html.parser')
+
+    for game_block_preview in root.select('.result'):
+        a = game_block_preview.select_one('.product_title > a')
+        title = get_norm_text(a)
+        if not smart_comparing_names(title, game_name):
+            continue
+
+        url_game = urljoin(rs.url, a['href'])
+        need_logs and print(f'  [+] Load {url_game!r}')
+
+        rs = session.get(url_game)
+        if not rs.ok:
+            need_logs and print(f'    [-] Something went wrong...: status_code: {rs.status_code}\n{rs.text}')
+            continue
+
+        game_block = BeautifulSoup(rs.content, 'html.parser')
+        # <li class="summary_detail product_genre">
+        #     <span class="label">Genre(s): </span>
+        #     <span class="data">Role-Playing</span>,
+        #     <span class="data">Action RPG</span>
+        # </li>
+        genres = [
+            get_norm_text(a) for a in game_block.select('.summary_detail.product_genre > .data')
+        ]
+
+        # Сойдет первый, совпадающий по имени, вариант
+        return genres
+
+    return []
+
+
+if __name__ == '__main__':
+    from common import _common_test
+    _common_test(get_game_genres)
+
+    # Search 'Hellgate: London'...
+    #     Genres: ['Role-Playing', 'First-Person', 'First-Person', 'Western-Style']
+    #
+    # Search 'The Incredible Adventures of Van Helsing'...
+    #     Genres: ['Role-Playing', 'Action RPG', 'Action RPG']
+    #
+    # Search 'Dark Souls: Prepare to Die Edition'...
+    #     Genres: ['Role-Playing', 'Action RPG', 'Action RPG']
+    #
+    # Search 'Twin Sector'...
+    #     Genres: ['Action Adventure', 'Modern', 'General', 'Modern', 'Linear']
+    #
+    # Search 'Call of Cthulhu: Dark Corners of the Earth'...
+    #     Genres: ['Action Adventure', 'Horror', 'Horror', 'Survival']
