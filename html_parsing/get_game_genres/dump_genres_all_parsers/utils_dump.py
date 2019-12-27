196886e815b7c1f3c607cfdbb3e7abeb533811ea
gil9red <ilya.petrash@inbox.ru>
2019-12-27 14:13:52 +0500

    #

--- html_parsing/get_game_genres/dump_genres_all_parsers/utils_dump.py
+++ html_parsing/get_game_genres/dump_genres_all_parsers/utils_dump.py
@@ -34,7 +34,7 @@ def get_funcs_parsers() -> Dict[str, Callable]:
         if 'get_game_genres' not in dir(module):
             continue
 
-        data[module.__name__.replace('_', '.')] = module.get_game_genres
+        data[module.__name__] = module.get_game_genres
 
     return data
 
@@ -98,17 +98,17 @@ if __name__ == "__main__":
     #
     # for site, get_game_genres in get_funcs_parsers().items():
     #     print(f"{site:<25}: {get_game_genres('Dead Space')}")
-    # # ag.ru                    : ['Шутеры', 'Экшены']
-    # # gamebomb.ru              : ['Боевик-приключения', 'Шутер']
-    # # gamefaqs.gamespot.com    : ['Arcade', 'Shooter', 'Action', 'Third-Person']
-    # # gameguru.ru              : ['Экшен', 'Шутер']
-    # # gamer.info.com           : ['ужасы', 'action']
-    # # gamespot.com             : ['3D', 'Shooter', 'Action', 'Third-Person']
-    # # igromania.ru             : ['Боевик', 'Ужасы', 'Боевик от третьего лица']
-    # # iwantgames.ru            : []
-    # # metacritic.com           : ['Arcade', 'Third-Person', 'Sci-Fi', 'Action', 'Shooter']
-    # # mobygames.com            : ['Action']
-    # # playground.ru            : ['Ужасы', 'От третьего лица', 'Космос', 'Экшен']
-    # # spong.com                : ['Adventure: Survival Horror']
-    # # stopgame.ru              : []
-    # # store.steampowered.com   : ['Action']
+    # # ag_ru                    : ['Шутеры', 'Экшены']
+    # # gamebomb_ru              : ['Боевик-приключения', 'Шутер']
+    # # gamefaqs_gamespot_com    : ['Arcade', 'Shooter', 'Action', 'Third-Person']
+    # # gameguru_ru              : ['Экшен', 'Шутер']
+    # # gamer_info_com           : ['ужасы', 'action']
+    # # gamespot_com             : ['3D', 'Shooter', 'Action', 'Third-Person']
+    # # igromania_ru             : ['Боевик', 'Ужасы', 'Боевик от третьего лица']
+    # # iwantgames_ru            : []
+    # # metacritic_com           : ['Arcade', 'Third-Person', 'Sci-Fi', 'Action', 'Shooter']
+    # # mobygames_com            : ['Action']
+    # # playground_ru            : ['Ужасы', 'От третьего лица', 'Космос', 'Экшен']
+    # # spong_com                : ['Adventure: Survival Horror']
+    # # stopgame_ru              : []
+    # # store_steampowered_com   : ['Action']
