524930808baf4c265b008a47eddd92cd11a299a9
gil9red <ilya.petrash@inbox.ru>
2019-12-31 16:00:47 +0500

    Added html_parsing/get_game_genres/dump_genres_all_parsers/_load_genre_translate_file.py

---
+++ html_parsing/get_game_genres/dump_genres_all_parsers/_load_genre_translate_file.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import json
+
+
+def dict_clean(items, default):
+    return {
+        k: default if v is None else v
+        for k, v in items
+    }
+
+
+genre_translate = json.load(
+    open('genre_translate.json', encoding='utf-8'),
+    object_pairs_hook=lambda items: dict_clean(items, default=[])
+)
+print(genre_translate)
+
+genre_translate = json.load(
+    open('genre_translate.json', encoding='utf-8'),
+    object_pairs_hook=lambda items: dict_clean(items, default='<null>')
+)
+print(genre_translate)
