7630ba5a4514c436b2037b533a072c2fe22fe3e6
gil9red <ilya.petrash@inbox.ru>
2019-12-31 12:31:39 +0500

    #

--- html_parsing/get_game_genres/dump_genres_all_parsers/main.py
+++ html_parsing/get_game_genres/dump_genres_all_parsers/main.py
@@ -12,13 +12,29 @@ from db import db_create_backup, Game, db
 from utils_dump import get_parsers, get_games_list, wait, get_logger, AtomicCounter, seconds_to_str
 
 
+IGNORE_SITE_NAMES = ['gamefaqs_gamespot_com']
+
+# Test
+USE_FAKE_PARSER = False
+if USE_FAKE_PARSER:
+    class FakeParser:
+        @classmethod
+        def get_site_name(cls): return "<test>"
+        def get_game_genres(self): raise Exception('Error')
+
+    # Monkey Patch
+    def get_parsers():
+        return [FakeParser]
+
+
 log = get_logger()
 counter = AtomicCounter()
 
 
 def run_parser(parser, games: list):
+    site_name = parser.get_site_name()
+
     for game_name in games:
-        site_name = parser.get_site_name()
         if Game.exists(site_name, game_name):
             continue
 
@@ -29,14 +45,14 @@ def run_parser(parser, games: list):
                 genres = parser.get_game_genres(game_name)
                 log.info(f'Found genres {game_name!r} ({site_name}): {genres}')
 
-                Game.add(parser.get_site_name(), game_name, genres)
+                Game.add(site_name, game_name, genres)
                 counter.inc()
 
                 time.sleep(1)
                 break
 
             except:
-                log.exception("Error")
+                log.exception('')
                 wait(minutes=5)
 
 
@@ -53,13 +69,14 @@ if __name__ == "__main__":
 
             threads = []
             for parser in get_parsers():
-                if parser.get_site_name() == 'gamefaqs_gamespot_com':
+                if parser.get_site_name() in IGNORE_SITE_NAMES:
                     continue
 
                 threads.append(
                     Thread(target=run_parser, args=[parser, games])
                 )
             log.info(f'Total parsers/threads: {len(threads)}')
+            log.info(f'Ignore parsers ({len(IGNORE_SITE_NAMES)}): {", ".join(IGNORE_SITE_NAMES)}')
 
             counter.value = 0
 
@@ -75,5 +92,5 @@ if __name__ == "__main__":
             wait(days=1)
 
         except:
-            log.exception('Error')
+            log.exception('')
             wait(minutes=15)
