88110d4d7713049b60ce35a6ebd5efab59134e2d
ipetrash <ilya.petrash@inbox.ru>
2020-01-05 01:50:20 +0500

    Release create genre translator

--- html_parsing/get_game_genres/dump_genres_all_parsers/_create_genre_translate_file.py
+++ html_parsing/get_game_genres/dump_genres_all_parsers/_create_genre_translate_file.py
@@ -7,10 +7,31 @@ __author__ = 'ipetrash'
 import json
 
 from db import Dump
+from _load_genre_translate_file import FILE_NAME_GENRE_TRANSLATE, load
+from utils_dump import get_logger
+
+
+log = get_logger('genre_translate')
+
+log.info('Start.')
+log.info('Load genres')
+
+genre_translate = load()
+
+log.info(f'Current genres: {len(genre_translate)}')
+
+for genre in Dump.get_all_genres():
+    if genre not in genre_translate:
+        log.info(f'Added new genre: {genre!r}')
+        genre_translate[genre] = None
+
+log.info('Save genres')
 
 json.dump(
-    dict.fromkeys(Dump.get_all_genres()),
-    open('genre_translate.json', 'w', encoding='utf-8'),
+    genre_translate,
+    open(FILE_NAME_GENRE_TRANSLATE, 'w', encoding='utf-8'),
     ensure_ascii=False,
     indent=4
 )
+
+log.info('Finish!')
--- html_parsing/get_game_genres/dump_genres_all_parsers/_load_genre_translate_file.py
+++ html_parsing/get_game_genres/dump_genres_all_parsers/_load_genre_translate_file.py
@@ -14,8 +14,21 @@ def dict_clean(items, default):
     }
 
 
-genre_translate = json.load(
-    open('genre_translate.json', encoding='utf-8'),
-    object_pairs_hook=lambda items: dict_clean(items, default=[])
-)
-print(genre_translate)
+FILE_NAME_GENRE_TRANSLATE = 'genre_translate.json'
+
+
+def load() -> dict:
+    try:
+        genre_translate = json.load(
+            open(FILE_NAME_GENRE_TRANSLATE, encoding='utf-8'),
+            object_pairs_hook=lambda items: dict_clean(items, default=[])
+        )
+    except:
+        genre_translate = dict()
+
+    return genre_translate
+
+
+if __name__ == '__main__':
+    genre_translate = load()
+    print(genre_translate)
