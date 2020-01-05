0e4528e857646b340a2a106634111d4c43348959
ipetrash <ilya.petrash@inbox.ru>
2020-01-05 23:35:45 +0500

    Print undefined genres

--- html_parsing/get_game_genres/dump_genres_all_parsers/_load_genre_translate_file.py
+++ html_parsing/get_game_genres/dump_genres_all_parsers/_load_genre_translate_file.py
@@ -24,3 +24,16 @@ def load(file_name: str = FILE_NAME_GENRE_TRANSLATE) -> dict:
 if __name__ == '__main__':
     genre_translate = load()
     print(genre_translate)
+
+    # Print all undefined genres without '{' / '}' and indent
+    genre_null_translate = {
+        k: v
+        for k, v in genre_translate.items()
+        if v is None
+    }
+    json_text = json.dumps(genre_null_translate, ensure_ascii=False, indent=4)
+    lines = json_text.splitlines()[1:-1]
+    for i, line in enumerate(lines):
+        print(line.strip())
+        if i > 0 and i % 40 == 0:
+            print()
