3dda5c40af5a462c0caff6e41330eda6ee0dd16f
ipetrash <ilya.petrash@inbox.ru>
2020-01-05 23:28:22 +0500

    Added html_parsing/get_game_genres/dump_genres_all_parsers/_merge_genre_translate_file.py

---
+++ html_parsing/get_game_genres/dump_genres_all_parsers/_merge_genre_translate_file.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import datetime as DT
+import json
+import shutil
+
+from _load_genre_translate_file import load, FILE_NAME_GENRE_TRANSLATE
+from utils_dump import get_logger
+
+
+FILE_NAME_MERGE_GENRE_TRANSLATE = 'merge_genre_translate.json'
+
+
+log = get_logger('merge_genre_translate.txt')
+
+log.info('Start.')
+
+backup_file_name = 'backup/' + DT.datetime.today().strftime('%d%m%y-%H%M%S_') + FILE_NAME_GENRE_TRANSLATE
+log.info(f'Save backup to: {backup_file_name!r}')
+shutil.copy(
+    FILE_NAME_GENRE_TRANSLATE,
+    backup_file_name
+)
+
+log.info('Load genres')
+
+genre_translate = load()
+log.info(f'Current genres: {len(genre_translate)}')
+
+log.info('Load merge')
+
+merge_genre_translate = load(FILE_NAME_MERGE_GENRE_TRANSLATE)
+log.info(f'Current merged genres: {len(merge_genre_translate)}')
+
+for k, v in merge_genre_translate.items():
+    if v is not None and k in genre_translate and genre_translate.get(k) is None:
+        log.info(f'Merge: {k!r} -> {v!r}')
+        genre_translate[k] = v
+
+log.info('Save merged genres')
+
+json.dump(
+    genre_translate,
+    open(FILE_NAME_GENRE_TRANSLATE, 'w', encoding='utf-8'),
+    ensure_ascii=False,
+    indent=4
+)
+
+log.info('Finish!')
