af81832acacffc5cda198b90a0cd9a1ff6854cb7
gil9red <ilya.petrash@inbox.ru>
2020-01-09 12:13:40 +0500

    Added html_parsing/get_game_genres/dump_genres_all_parsers/export_import

---
+++ html_parsing/get_game_genres/dump_genres_all_parsers/export_import/import.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import json
+
+import sys
+sys.path.append('..')
+
+from db import Dump
+
+import peewee
+from playhouse.shortcuts import dict_to_model
+
+from export import FILE_NAME_EXPORT_JSON
+
+
+items = json.load(
+    open(FILE_NAME_EXPORT_JSON, encoding='utf-8')
+)
+print('items:', len(items))
+
+for x in items:
+    try:
+        dump = dict_to_model(Dump, x)
+        dump.save(force_insert=True)
+        print(f'Import {x}')
+
+    except peewee.IntegrityError as e:
+        # Ignore error "UNIQUE constraint failed: dump.id"
+        pass
+
+print('Dump count:', Dump.select().count())
