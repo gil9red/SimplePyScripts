9594fda7c4b5e0343365e749a9dab86dc6be1324
gil9red <ilya.petrash@inbox.ru>
2020-01-09 13:11:17 +0500

    Added html_parsing/get_game_genres/dump_genres_all_parsers/_view_db_site_by_genres.py

---
+++ html_parsing/get_game_genres/dump_genres_all_parsers/_view_db_site_by_genres.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from collections import defaultdict
+from db import Dump
+
+
+genres = Dump.get_all_genres()
+print(f'Genres ({len(genres)}): {genres}')
+
+sites = Dump.get_all_sites()
+print(f'Sites ({len(sites)}): {sites}')
+
+print()
+
+sites = [x.site for x in Dump.select(Dump.site).distinct()]
+max_width = len(max(sites, key=len))
+fmt_str = '{:<%d} : ({}) {}' % max_width
+
+site_by_genres = defaultdict(list)
+for x in Dump.get():
+    site_by_genres[x.site] += x.genres
+
+for k, v in site_by_genres.items():
+    site_by_genres[k] = sorted(set(v))
+
+print(f'Total {len(site_by_genres)}:')
+
+for site, genres in site_by_genres.items():
+    print(fmt_str.format(site, len(genres), genres))
