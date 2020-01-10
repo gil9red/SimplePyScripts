d398d065505c669be3abacb5a3f3c2b30196483f
gil9red <ilya.petrash@inbox.ru>
2020-01-10 11:59:20 +0500

    #

--- html_parsing/get_game_genres/dump_genres_all_parsers/_view_db_Dump.py
+++ html_parsing/get_game_genres/dump_genres_all_parsers/_view_db_Dump.py
@@ -21,8 +21,7 @@ print(f'Sites ({len(sites)}): {sites}')
 
 print()
 
-sites = [x.site for x in Dump.select(Dump.site).distinct()]
-max_width = len(max(sites, key=len))
+max_width = max(len(x.site) for x in Dump.select(Dump.site).distinct())
 fmt_str = '    {:<%d} : {}' % max_width
 
 game_by_dump = defaultdict(list)
