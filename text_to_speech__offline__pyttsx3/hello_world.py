a7b4a43b9f57a3ffd7e5a755ca5b4c5fb480b07a
gil9red <ilya.petrash@inbox.ru>
2020-01-23 17:49:36 +0500

    Added text_to_speech__offline__pyttsx3/

---
+++ text_to_speech__offline__pyttsx3/hello_world.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://github.com/nateshmbhat/pyttsx3
+
+
+# pip install pyttsx3
+import pyttsx3
+
+engine = pyttsx3.init()
+engine.say("I will speak this text")
+engine.runAndWait()
