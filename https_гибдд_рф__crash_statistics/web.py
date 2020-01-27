5dc95f694dab3ea54b840e8eccff420500b23933
ipetrash <ilya.petrash@inbox.ru>
2020-01-27 21:19:29 +0500

    Added line chart Added using: bootstrap, jquery, chart.js

--- https_гибдд_рф__crash_statistics/web.py
+++ https_гибдд_рф__crash_statistics/web.py
@@ -4,6 +4,8 @@
 __author__ = 'ipetrash'
 
 
+import datetime as DT
+
 from flask import Flask, render_template_string
 from common import get_crash_statistics_list_db
 
@@ -13,8 +15,45 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    headers = ["№", "Дата", "ДТП", "Погибли", "Погибло детей", "Ранены", "Ранено детей"]
+    headers = ["Дата", "ДТП", "Погибли", "Погибло детей", "Ранены", "Ранено детей"]
     rows = get_crash_statistics_list_db()
+    rows.reverse()
+
+    data_labels = []
+    data_dtp = []
+    data_died = []
+    data_children_died = []
+    data_wounded = []
+    data_wounded_children = []
+
+    for date, dtp, died, children_died, wounded, wounded_children in rows:
+        x = DT.datetime.strptime(date, '%d.%m.%Y').date().isoformat()
+        data_labels.append(x)
+
+        data_dtp.append({
+            "x": x,
+            "y": int(dtp)
+        })
+
+        data_died.append({
+            "x": x,
+            "y": int(died)
+        })
+
+        data_children_died.append({
+            "x": x,
+            "y": int(children_died)
+        })
+
+        data_wounded.append({
+            "x": x,
+            "y": int(wounded)
+        })
+
+        data_wounded_children.append({
+            "x": x,
+            "y": int(wounded_children)
+        })
 
     return render_template_string("""
 <!DOCTYPE html>
@@ -23,62 +62,116 @@ def index():
     <meta content='text/html; charset=UTF-8' http-equiv='Content-Type'/>
     <title>{{ title }}</title>
 
-    <style type="text/css">
-        /* Для размещения таблицы по центру. SOURCE: https://stackoverflow.com/a/9402889/5909792 */
-        html, body {
-            width: 100%;
-        }
-    
-        table {
-            border-collapse: collapse; /* Убираем двойные линии между ячейками */
-            width: 70%; /* Ширина таблицы */
-            
-            /* Для размещения таблицы по центру. SOURCE: https://stackoverflow.com/a/9402889/5909792 */
-            margin: 0 auto;
-        }
-            /* Увеличим заголовок таблиц */
-            table > caption {
-                font-size: 150%;
-            }
+    <link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='bootstrap-4.3.1/bootstrap.min.css') }}">
 
-            th {
-                font-size: 120%;
-            }
-            td, th {
-                border: 1px double #333; /* Рамка таблицы */
-                padding: 5px;
-            }
-    </style>
+    <script src="{{ url_for('static', filename='js/jquery-3.1.1.min.js') }}"></script>
+    <script src="{{ url_for('static', filename='bootstrap-4.3.1/bootstrap.min.js') }}"></script>
+    <script src="{{ url_for('static', filename='chart_js_2.8.0/Chart.bundle.min.js') }}"></script>
 </head>
 <body>
-    <table>
-        <caption><a href="https://гибдд.рф/">{{ title }}</a></caption>
+<div class="container-fluid">
+    <h2 class="text-center"><a href="https://гибдд.рф/">{{ title }}</a></h2>
 
-        <colgroup>
-            <col span="1" style="width: 5%;">
-        </colgroup>
-
-        <tbody>
-            <tr>
-            {% for header in headers %}
-                <th>{{ header }}</th>
-            {% endfor %}
-            </tr>
-
-        {% for row in rows %}
-            <tr>
-                <td>{{ loop.index }}</td>
-                
-                {% for value in row %}
-                <td>{{ value }}</td>
+    <div class="row">
+        <div class="col-5">
+            <table class="table table-hover">
+                <thead class="thead-dark">
+                    <tr>
+                    {% for header in headers %}
+                        <th>{{ header }}</th>
+                    {% endfor %}
+                    </tr>
+                </thead>
+                <tbody>
+                {% for row in rows %}
+                    <tr>
+                        {% for value in row %}
+                        <td>{{ value }}</td>
+                        {% endfor %}
+                    </tr>
                 {% endfor %}
-            </tr>
-        {% endfor %}
-        </tbody>
-    </table>
+                </tbody>
+            </table>
+        </div>
+        <div class="col">
+            <canvas id="lineChart"></canvas>
+        </div>
+    </div>
+</div>
+<script>
+    $(document).ready(function() {
+        let data_dtp = {{ data_dtp | safe }}
+        let data_died = {{ data_died | safe }};
+        let data_children_died = {{ data_children_died | safe }};
+        let data_wounded = {{ data_wounded | safe }};
+        let data_wounded_children = {{ data_wounded_children | safe }};
+        
+        var ctx = document.getElementById("lineChart").getContext("2d");
+        var lineChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: {{ data_labels | safe }},
+                datasets: [
+                    {
+                        label: 'ДТП',
+                        lineTension: 0,
+                        borderColor: "red",
+                        data: data_dtp,
+                    },
+                    {
+                        label: 'Погибли',
+                        lineTension: 0,
+                        borderColor: "rgb(246, 139, 31)",
+                        data: data_died,
+                    },
+                    {
+                        label: 'Погибло детей',
+                        lineTension: 0,
+                        borderColor: "rgb(68, 44, 110)",
+                        data: data_children_died,
+                    },
+                    {
+                        label: 'Ранены',
+                        lineTension: 0,
+                        borderColor: "blue",
+                        data: data_wounded,
+                    },
+                    {
+                        label: 'Ранено детей',
+                        lineTension: 0,
+                        borderColor: "green",
+                        data: data_wounded_children,
+                    },
+                ],
+            },
+            options: {
+                scales: {
+                    xAxes: [{
+                        type: 'time',
+                        time: {
+                            unit: 'day',
+                            tooltipFormat: 'DD/MM/YYYY',
+                            displayFormats: {
+                               day: 'DD/MM/YY'
+                            }
+                        },
+                        distribution: 'linear'
+                    }]
+                }
+            }
+        });
+    });
+</script>
 </body>
 </html>
-    """, title="АВАРИЙНОСТЬ НА ДОРОГАХ РОССИИ", headers=headers, rows=rows)
+    """, title="АВАРИЙНОСТЬ НА ДОРОГАХ РОССИИ", headers=headers, rows=rows,
+    data_labels=data_labels,
+    data_dtp=data_dtp,
+    data_died=data_died,
+    data_children_died=data_children_died,
+    data_wounded=data_wounded,
+    data_wounded_children=data_wounded_children
+)
 
 
 if __name__ == "__main__":
