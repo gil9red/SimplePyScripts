3a179039e4b3df7f07a3dbf3a40382c71624b37a
gil9red <ilya.petrash@inbox.ru>
2020-02-21 13:55:31 +0500

    Added flask__webservers/bootstrap_4__toggle_switch__examples

---
+++ flask__webservers/bootstrap_4__toggle_switch__examples/main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://github.com/twbs/bootstrap
+# SOURCE: https://github.com/gitbrent/bootstrap4-toggle
+# SOURCE: https://gitbrent.github.io/bootstrap4-toggle/
+
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+
+    # Localhost
+    # port=0 -- random free port
+    # app.run(port=0)
+    app.run(
+        port=5000,
+
+        # :param threaded: should the process handle each request in a separate
+        #                  thread?
+        # :param processes: if greater than 1 then handle each request in a new process
+        #                   up to this maximum number of concurrent processes.
+        threaded=True,
+    )
+
+    # # Public IP
+    # app.run(host='0.0.0.0')
