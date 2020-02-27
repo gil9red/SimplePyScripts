992607376ce1f591ca44f8ef43bc1b9eef0efaf9
gil9red <ilya.petrash@inbox.ru>
2020-02-27 15:21:04 +0500

    Added "emulating input().py"

---
+++ emulating input().py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from io import StringIO
+buffer = StringIO(
+    "dfdfdfdf\n12\n34",
+    newline='\n'
+)
+
+def input(*args, **kwargs):
+    return next(buffer).rstrip('\n')
+
+
+text = input()
+print(text)
+# dfdfdfdf
+
+a, b = int(input()), int(input())
+print(a, b)
+# 12 34
