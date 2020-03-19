561e457a8253713a211badfa9e40800fd7124676
gil9red <ilya.petrash@inbox.ru>
2020-03-19 18:11:27 +0500

    Added qt__pyqt__pyside__pyqode/simple_key_press_handler__keyPressEvent.py

---
+++ qt__pyqt__pyside__pyqode/simple_key_press_handler__keyPressEvent.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.QtWidgets import QApplication, QLabel
+from PyQt5.QtGui import QKeyEvent
+from PyQt5.QtCore import Qt
+
+
+class MainWindow(QLabel):
+    def keyPressEvent(self, event: QKeyEvent):
+        super().keyPressEvent(event)
+
+        text = f'{event.key()} : {event.text()!r}'
+        print(text)
+
+        self.setText(text)
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    mw = MainWindow()
+    mw.setAlignment(Qt.AlignCenter)
+    mw.resize(200, 200)
+    mw.show()
+
+    app.exec()
