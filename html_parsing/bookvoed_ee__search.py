9f7cebf05d00785728ce648a6d3a3bc72b5c4d47
ipetrash <ilya.petrash@inbox.ru>
2020-03-20 20:54:07 +0500

    Added html_parsing/bookvoed_ee__search.py

---
+++ html_parsing/bookvoed_ee__search.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from urllib.parse import urljoin
+
+from bs4 import BeautifulSoup
+import requests
+
+
+rs = requests.get('https://bookvoed.ee/search?q=white+fang')
+root = BeautifulSoup(rs.content, 'html.parser')
+
+for a in root.select('.o-row > a.title'):
+    href = urljoin(rs.url, a.get('href'))
+    title = a.get_text(strip=True)
+    print(title, href)
+
+# White Fang https://bookvoed.ee/goods/4496177
+# Белый клык = White Fang https://bookvoed.ee/goods/6807168
+# The Call of the Wild and White Fang https://bookvoed.ee/goods/8654867
+# White Fang [= Белый Клык] https://bookvoed.ee/goods/451827
+# Call of the Wild & White Fang https://bookvoed.ee/goods/4914246
