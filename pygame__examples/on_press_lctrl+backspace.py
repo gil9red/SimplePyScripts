6cb401cb54e7d8749845e2b6320b635ee74cccab
ipetrash <ilya.petrash@inbox.ru>
2020-03-22 00:31:56 +0500

    Added pygame__examples/on_press_lctrl+backspace.py

---
+++ pygame__examples/on_press_lctrl+backspace.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import pygame
+import pygame.locals
+
+pygame.init()
+
+screen = pygame.display.set_mode((300,200))
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+            elif event.key == pygame.K_BACKSPACE and pygame.key.get_mods() & pygame.KMOD_LCTRL:
+                print("pressed: LCTRL + BACKSPACE")
+
+pygame.quit()
