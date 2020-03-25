585517d75752fccf184c329889c1a96c0f1923c2
gil9red <ilya.petrash@inbox.ru>
2020-03-25 11:19:46 +0500

    Added qt__pyqt__pyside__pyqode/RotatedButton__QGraphicsScene.py

---
+++ qt__pyqt__pyside__pyqode/RotatedButton__QGraphicsScene.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+__author__ = 'ipetrash'
+
+
+import sys
+
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGraphicsScene, \
+    QGraphicsView, QGraphicsLinearLayout, QGraphicsWidget, QHBoxLayout
+from PyQt5.QtCore import Qt, QSize
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.scene = QGraphicsScene()
+        self.scene.setSceneRect(0, 0, 400, 400)
+
+        self.view = QGraphicsView()
+        self.view.setScene(self.scene)
+
+        scene_layout = QGraphicsLinearLayout(Qt.Horizontal)
+
+        form = QGraphicsWidget()
+        form.setLayout(scene_layout)
+
+        self.scene.addItem(form)
+
+        self.main_layout = QHBoxLayout()
+        self.main_layout.addWidget(self.view)
+        self.setLayout(self.main_layout)
+
+        self.rb0 = QPushButton("Hello")
+        proxy_rb0 = self.scene.addWidget(self.rb0)
+        scene_layout.addItem(proxy_rb0)
+
+        self.rb1 = QPushButton("Hello")
+        proxy_rb1 = self.scene.addWidget(self.rb1)
+        proxy_rb1.setRotation(90)
+        scene_layout.addItem(proxy_rb1)
+
+        self.rb2 = QPushButton("Hello")
+        proxy_rb2 = self.scene.addWidget(self.rb2)
+        proxy_rb2.setRotation(180)
+        scene_layout.addItem(proxy_rb2)
+
+        self.rb3 = QPushButton("Hello")
+        proxy_rb3 = self.scene.addWidget(self.rb3)
+        proxy_rb3.setRotation(-166)
+        scene_layout.addItem(proxy_rb3)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    w = Window()
+    w.setMinimumSize(QSize(0, 400))
+    w.show()
+
+    sys.exit(app.exec_())
