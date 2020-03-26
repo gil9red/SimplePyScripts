d3a9a036aafbcfa2a3d48092adf0a4a160dd3e49
ipetrash <ilya.petrash@inbox.ru>
2020-03-27 01:54:53 +0500

    Added selenium__examples/google_search__get_first_image.py

---
+++ selenium__examples/google_search__get_first_image.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from base64 import b64decode
+
+# pip install selenium
+from selenium import webdriver
+
+
+key = 'яблоко'
+url = f'https://www.google.ru/search?q={key}&newwindow=1&espv=2&source=lnms&tbm=isch&sa=X'
+
+driver = webdriver.Firefox()
+driver.implicitly_wait(10)  # seconds
+driver.get(url)
+
+# Поиск первой картинки
+img = driver.find_element_by_xpath('//img[starts-with(@src, "data:image/jpeg;base64,")]')
+
+src = img.get_attribute('src')
+src = src.split('data:image/jpeg;base64,')[1]
+
+img_data = b64decode(src)
+
+with open('img.jpg', 'wb') as f:
+    f.write(img_data)
+
+# Делаем скриншот результата
+driver.save_screenshot('screenshot.png')
+
+driver.quit()
