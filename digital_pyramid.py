f9a164bfb19873887a6f865e6c602f9af6d0b010
gil9red <ilya.petrash@inbox.ru>
2020-03-27 15:56:04 +0500

    Added digital_pyramid.py

---
+++ digital_pyramid.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+n = 9
+
+for i in range(1, n + 1):
+    left = ''.join(map(str, range(1, i)))
+    spaces = ' ' * (n - len(left) - 1)
+    print(f'{spaces}{left}{i}{left[::-1]}')
+
+#         1
+#        121
+#       12321
+#      1234321
+#     123454321
+#    12345654321
+#   1234567654321
+#  123456787654321
+# 12345678987654321
