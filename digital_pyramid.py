576c519f634eebeff75fc23e9e94a4f73f2cc344
gil9red <ilya.petrash@inbox.ru>
2020-03-27 16:01:24 +0500

    #

--- digital_pyramid.py
+++ digital_pyramid.py
@@ -8,9 +8,25 @@ n = 9
 
 for i in range(1, n + 1):
     left = ''.join(map(str, range(1, i)))
-    spaces = ' ' * (n - len(left) - 1)
+    spaces = ' ' * (n - i)
     print(f'{spaces}{left}{i}{left[::-1]}')
 
+#         1
+#        121
+#       12321
+#      1234321
+#     123454321
+#    12345654321
+#   1234567654321
+#  123456787654321
+# 12345678987654321
+
+print()
+
+x = ""
+for i in range(1, n + 1):
+    x += str(i)
+    print(' ' * (n-i) + x + x[-2::-1])
 #         1
 #        121
 #       12321
