5280046aefa801c9b233aebb14bf3a21990129ad
ipetrash <ilya.petrash@inbox.ru>
2020-04-22 19:33:45 +0500

    Added googletrans__examples/hello_world.py

---
+++ googletrans__examples/hello_world.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install googletrans
+from googletrans import Translator
+
+
+text = """
+At this Time, two great empires struggled for dominion over Ivalice: Archadia in the East, Rozarria, the West.
+The Invasion of the Kingdom of Nabradia was Archadia's first Step in its westward March.
+"""
+from_lang = 'en'
+to_lang = 'ru'
+
+translator = Translator()
+translation = translator.translate(
+    text,
+    src=from_lang,
+    dest=to_lang
+).text
+print(translation)
+# В это время две великие империи боролись за власть над Ivalice: Archadia на Востоке, Rozarria, Запада.
+# Вторжение Королевства Nabradia был первый шаг Archadia в ее западном направлении марта.
