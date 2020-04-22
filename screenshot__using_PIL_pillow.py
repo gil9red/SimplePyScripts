3971a086c3494d9d2f61efc3dec4a58b4066b79c
ipetrash <ilya.petrash@inbox.ru>
2020-04-22 19:44:13 +0500

    #

--- screenshot__using_PIL_pillow.py
+++ screenshot__using_PIL_pillow.py
@@ -5,7 +5,21 @@ __author__ = 'ipetrash'
 
 
 # Fullscreen
+
+
+import datetime as DT
+
+# pip install pillow
 from PIL import ImageGrab
+
+
 im = ImageGrab.grab()
 im.save('screenshot.png')
+im.save('screenshot.jpg')
 im.show()
+
+# Filename with datetime
+file_name = f'screenshot_{DT.datetime.now():%Y-%m-%d_%H%M%S}.jpg'
+print(file_name)
+
+im.save(file_name)
