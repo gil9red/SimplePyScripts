df5649a4de9c4ea7aca1a6dbd30c661874f9f212
ipetrash <ilya.petrash@inbox.ru>
2020-04-26 21:40:18 +0500

    #

--- html_parsing/www_techcult_ru__technology.py
+++ html_parsing/www_techcult_ru__technology.py
@@ -7,6 +7,7 @@ __author__ = 'ipetrash'
 from bs4 import BeautifulSoup
 import requests
 
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0',
 }
