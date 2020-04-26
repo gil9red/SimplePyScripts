32ce8575b5ae2d9dc494a43f3bae687df4cb048d
ipetrash <ilya.petrash@inbox.ru>
2020-04-26 06:51:04 +0500

    Added requests_html__examples/save_before_after_js.py

---
+++ requests_html__examples/save_before_after_js.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install requests-html
+from requests_html import HTMLSession
+
+
+session = HTMLSession()
+rs = session.get('https://coronavirus-monitor.ru/statistika/')
+with open('rs_before_js.html', 'w') as f:
+    f.write(rs.html.html)
+
+rs.html.render()  # Без этого не будет выполнения js кода
+
+with open('rs_after_js.html', 'w') as f:
+    f.write(rs.html.html)
