3b46bf2235a72fa6dbf2b92ed7cfc6afedc28940
ipetrash <ilya.petrash@inbox.ru>
2020-04-26 06:53:50 +0500

    #

--- requests_html__examples/save_before_after_js/main.py
+++ requests_html__examples/save_before_after_js/main.py
@@ -10,10 +10,10 @@ from requests_html import HTMLSession
 
 session = HTMLSession()
 rs = session.get('https://coronavirus-monitor.ru/statistika/')
-with open('rs_before_js.html', 'w') as f:
+with open('rs_before_js.html', 'w', encoding='utf-8') as f:
     f.write(rs.html.html)
 
 rs.html.render()  # Без этого не будет выполнения js кода
 
-with open('rs_after_js.html', 'w') as f:
+with open('rs_after_js.html', 'w', encoding='utf-8') as f:
     f.write(rs.html.html)
