4692cbb36b5addb0f2bd5e93e69eb8c7c257bdf8
ipetrash <ilya.petrash@inbox.ru>
2020-05-02 18:33:32 +0500

    #

--- download_file/with_progress.py
+++ download_file/with_progress.py
@@ -6,6 +6,7 @@ __author__ = 'ipetrash'
 
 import sys
 from urllib.request import urlretrieve
+from threading import Thread
 
 
 def reporthook(blocknum, blocksize, totalsize):
@@ -17,17 +18,41 @@ def reporthook(blocknum, blocksize, totalsize):
             readsofar = totalsize
 
         s = "\r%5.1f%% %*d / %d" % (percent, len(str(totalsize)), readsofar, totalsize)
-        sys.stderr.write(s)
-        if readsofar >= totalsize: # near the end
-            sys.stderr.write("\n")
+        sys.stdout.write(s)
+        if readsofar >= totalsize:  # Near the end
+            sys.stdout.write("\n")
 
-    # total size is unknown
+    # Total size is unknown
     else:
-        sys.stderr.write("read %d\n" % (readsofar,))
+        sys.stdout.write(f"read {readsofar}\n")
+
+
+def download(url: str, file_name: str = None, as_thread=False, callback_func=None) -> str:
+    if as_thread:
+        def run(url, file_name, reporthook, callback_func):
+            local_file_name, _ = urlretrieve(url, file_name, reporthook=reporthook)
+            if callable(callback_func):
+                callback_func(local_file_name)
+            return local_file_name
+
+        thread = Thread(target=run, args=(url, file_name, reporthook, callback_func))
+        thread.start()
+
+    else:
+        return urlretrieve(url, file_name, reporthook=reporthook)[0]
 
 
 if __name__ == '__main__':
     URL = 'https://github.com/gil9red/SimplePyScripts/archive/master.zip'
+    print(download(URL))
+    print()
+    print(download(URL, 'SimplePyScripts.zip'))
 
-    local_filename, headers = urlretrieve(URL, reporthook=reporthook)
-    print(local_filename + '\n' + headers)
+    print('\n')
+    sys.stderr.write('Threading...\n')
+
+    print(download(URL, 'SimplePyScripts.zip', as_thread=True))
+
+    def callback_func(file_name: str):
+        print('File name:', file_name)
+    print(download(URL, 'SimplePyScripts.zip', as_thread=True, callback_func=callback_func))
