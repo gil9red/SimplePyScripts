ca14073f6380fa44d3d484815d254d3ddcd12c7b
ipetrash <ilya.petrash@inbox.ru>
2020-05-02 22:48:11 +0500

    Added qt__pyqt__pyside__pyqode/download_file/simple.py

---
+++ qt__pyqt__pyside__pyqode/download_file/simple.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from threading import Thread
+import traceback
+from urllib.request import urlretrieve
+
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QFormLayout
+
+
+def download_file(url: str, file_name: str):
+    try:
+        local_file_name, _ = urlretrieve(url, file_name)
+        print(local_file_name)
+    except:
+        print(traceback.format_exc())
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.line_edit_url = QLineEdit('https://github.com/gil9red/SimplePyScripts/archive/master.zip')
+        self.line_edit_file_name = QLineEdit('SimplePyScripts.zip')
+
+        self.button_download = QPushButton('Download')
+        self.button_download.clicked.connect(self.download)
+
+        layout = QFormLayout()
+        layout.addRow('URL:', self.line_edit_url)
+        layout.addRow('File Name:', self.line_edit_file_name)
+        layout.addWidget(self.button_download)
+
+        self.setLayout(layout)
+
+    def download(self):
+        url = self.line_edit_url.text()
+        file_name = self.line_edit_file_name.text()
+
+        thread = Thread(target=download_file, args=(url, file_name))
+        thread.start()
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    mw = MainWindow()
+    mw.show()
+
+    app.exec()
