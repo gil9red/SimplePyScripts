42fc02cce76b5119d3238cd28954f35701ce8fb5
ipetrash <ilya.petrash@inbox.ru>
2020-05-06 20:18:10 +0500

    Added selenium__examples/pub_fsa_gov_ru__rds__declaration.py

---
+++ selenium__examples/pub_fsa_gov_ru__rds__declaration.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install selenium
+from selenium import webdriver
+
+
+driver = webdriver.Firefox()
+driver.implicitly_wait(10)
+driver.get('https://pub.fsa.gov.ru/rds/declaration')
+
+content = driver.find_element_by_css_selector('input[_ngcontent-c27]')
+content.send_keys('ЕАЭС N RU Д-TR.РА01.А.44855/19')
