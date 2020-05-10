f87a0266c2ae7920e2be6215fb3fd306ba2b5d09
ipetrash <ilya.petrash@inbox.ru>
2020-05-10 05:06:36 +0500

    Added pytube__examples/main.py

---
+++ pytube__examples/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import sys
+
+# pip install pytube3 --upgrade
+from pytube import YouTube
+
+
+url = 'https://www.youtube.com/watch?v=90KZnwrVMgY'
+
+yt = YouTube(url)
+print(f'Download video {yt.title!r}: {url}')
+
+# Атрибут progressive=True нужен, чтобы получить поток с видео и аудио в одном файле
+streams = yt.streams\
+    .filter(progressive=True, file_extension='mp4', resolution='720p')\
+    .order_by('resolution')
+
+if not streams:
+    print('Not found mp4 with 720p!')
+    sys.exit()
+
+video = streams[-1]
+print('Stream url:', video.url)
+video.download()
