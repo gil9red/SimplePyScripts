b0eaefb1266331faa5799a2c46e99336465099a9
ipetrash <ilya.petrash@inbox.ru>
2020-05-11 10:46:39 +0500

    Added get_path_Documents.py

---
+++ get_path_Documents.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import ctypes
+from ctypes.wintypes import MAX_PATH
+import sys
+
+
+# SOURCE: https://github.com/mhammond/pywin32/blob/d64fac8d7bda2cb1d81e2c9366daf99e802e327f/com/win32comext/shell/shellcon.py#L284
+CSIDL_PERSONAL = 5
+
+
+shell32 = ctypes.windll.shell32
+buf = ctypes.create_unicode_buffer(MAX_PATH + 1)
+if not shell32.SHGetSpecialFolderPathW(None, buf, CSIDL_PERSONAL, False):
+    # This buf.value is empty
+    print("Failure!")
+    sys.exit()
+
+print(buf.value)
+# C:\Users\ipetrash\Documents
