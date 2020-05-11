e8b067451e5c1d2692eeebbb771d7306381bd84c
ipetrash <ilya.petrash@inbox.ru>
2020-05-11 11:47:36 +0500

    Added get_path_Documents__winreg.py

---
+++ get_path_Documents__winreg.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from winreg import *
+
+aReg = ConnectRegistry(None, HKEY_CURRENT_USER)
+aKey = OpenKey(aReg, r"Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders")
+name = QueryValueEx(aKey, 'Personal')[0]
+print(name)
+# C:\Users\ipetrash\Documents
