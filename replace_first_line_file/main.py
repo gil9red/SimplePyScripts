dfec348a28bd426a026c12d9f7bb02dad88b27be
ipetrash <ilya.petrash@inbox.ru>
2020-05-13 12:10:21 +0500

    Added replace_first_line_file/main.py

---
+++ replace_first_line_file/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import shutil
+
+
+FILE_NAME = "file.txt"
+
+# Create file
+with open(FILE_NAME, 'w', encoding='utf-8') as f:
+    f.write('1\n2\n3\n4\n5')
+
+# Replace first line
+from_file = open(FILE_NAME, encoding='utf-8')
+line = from_file.readline()
+line = 'NEW FIRST LINE\n'
+
+to_file = open(FILE_NAME, mode="w", encoding='utf-8')
+to_file.write(line)
+shutil.copyfileobj(from_file, to_file)
