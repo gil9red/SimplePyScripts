d807a9e56f54a766c51a810b60bce6a3e2290575
ipetrash <ilya.petrash@inbox.ru>
2020-05-14 11:50:03 +0500

    Added qbittorrent_examples/print_torrents_by_specific_Disc.py

---
+++ qbittorrent_examples/print_torrents_by_specific_Disc.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from common import get_client, print_torrents
+
+
+qb = get_client()
+need_disc = "E:"
+torrents = [x for x in qb.torrents() if x['save_path'].startswith(need_disc)]
+print_torrents(torrents)
