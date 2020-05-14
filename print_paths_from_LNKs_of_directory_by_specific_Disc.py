d0c4561b6e8aaadffa475b40c2baf2564cd79b78
ipetrash <ilya.petrash@inbox.ru>
2020-05-14 16:33:04 +0500

    Added print_paths_from_LNKs_of_directory_by_specific_Disc.py

---
+++ print_paths_from_LNKs_of_directory_by_specific_Disc.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from glob import iglob
+import os
+
+# pip install winshell
+import winshell
+
+
+path_desktop_lnk = os.path.expanduser(r'~\Desktop\Пройти\*.lnk')
+need_disc = "E:"
+
+paths = []
+
+for file_name in iglob(path_desktop_lnk):
+    shortcut = winshell.shortcut(file_name)
+    path = shortcut.path
+    if path.startswith(need_disc):
+        paths.append(path)
+
+paths.sort()
+
+print(f"Total lnk's on {need_disc!r} ({len(paths)}):")
+for i, path in enumerate(paths, 1):
+    print(f"{i:3}. {path}")
