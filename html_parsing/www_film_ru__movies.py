71802f0d0cf22c1a81ca34d0dfe03377ba2ca458
ipetrash <ilya.petrash@inbox.ru>
2020-05-26 11:19:52 +0500

    Added html_parsing/www_film_ru__movies.py

---
+++ html_parsing/www_film_ru__movies.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from urllib.parse import urljoin
+
+from bs4 import BeautifulSoup
+import requests
+
+
+URL = 'https://www.film.ru/movies'
+rs = requests.get(URL)
+root = BeautifulSoup(rs.content, 'html.parser')
+
+for a in root.select('div.rating.infinite_scroll > a'):
+    title = a.strong.get_text(strip=True)
+    url = urljoin(rs.url, a['href'])
+    print(title, url)
+
+# Проклятый путь https://www.film.ru/articles/semeynaya-drama-s-tommi-ganom-v-ruke
+# Унесенные призраками https://www.film.ru/articles/klassika-anime-unesennye-prizrakami
+# Адаптация https://www.film.ru/articles/lukavoe-naglyadnoe-posobie-po-ekranizacii
+# ...
+# Первый мститель: Другая война https://www.film.ru/articles/so-schitom-ili-na-schite
