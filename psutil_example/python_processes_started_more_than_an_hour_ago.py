d7d85fe0d2d370e8e0b0b06163615cc80695ded3
ipetrash <ilya.petrash@inbox.ru>
2020-05-27 23:40:11 +0500

    Added psutil_example/python_processes_started_more_than_an_hour_ago.py

---
+++ psutil_example/python_processes_started_more_than_an_hour_ago.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import datetime as DT
+
+# pip install psutil
+import psutil
+
+
+for proc in psutil.process_iter():
+    if not proc.name().startswith('python'):
+        continue
+
+    secs = proc.create_time()
+    started = DT.datetime.fromtimestamp(secs)
+    if (DT.datetime.now() - started) >= DT.timedelta(hours=1):
+        print(proc)
