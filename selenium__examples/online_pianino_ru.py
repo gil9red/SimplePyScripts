aee31882ff8e84737f1d64a9c57fac6518017a71
ipetrash <ilya.petrash@inbox.ru>
2020-05-31 14:43:06 +0500

    Added selenium__examples/online_pianino_ru.py

---
+++ selenium__examples/online_pianino_ru.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import time
+
+# pip install selenium
+from selenium import webdriver
+
+
+driver = webdriver.Firefox()
+driver.implicitly_wait(20)  # seconds
+driver.get('https://online-pianino.ru/')
+print(f'Title: {driver.title!r}')
+
+for button in driver.find_elements_by_css_selector('#keyboardspot button[id]'):
+    print(button.get_attribute('id'))
+    button.click()
+
+    time.sleep(0.3)
