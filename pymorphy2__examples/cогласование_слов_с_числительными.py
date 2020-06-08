6ad10e58255363f3ffc4ef0394467699995c5cb3
ipetrash <ilya.petrash@inbox.ru>
2020-06-09 02:06:41 +0500

    Added pymorphy2__examples/cогласование_слов_с_числительными.py

---
+++ pymorphy2__examples/cогласование_слов_с_числительными.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# Для импортирования ascii_table__simple_pretty__format.py
+import sys
+sys.path.append('..')
+
+from ascii_table__simple_pretty__format import print_pretty_table
+
+# pip install pymorphy2
+import pymorphy2
+
+
+morph = pymorphy2.MorphAnalyzer()
+
+
+words = [
+    'программист',
+    'ёлка',
+    'фрукт',
+    'игр',
+    'барсук'
+]
+parsed_words = [morph.parse(word)[0] for word in words]
+
+rows = [words]
+for i in range(5 + 1):
+    row = []
+    for parsed_word in parsed_words:
+        word = parsed_word.make_agree_with_number(i).word
+        row.append(f'{i} {word}')
+    rows.append(row)
+
+print_pretty_table(rows, align='<')
+# программист     | ёлка   | фрукт     | игр    | барсук
+# ----------------+--------+-----------+--------+-----------
+# 0 программистов | 0 ёлок | 0 фруктов | 0 игр  | 0 барсуков
+# 1 программист   | 1 ёлка | 1 фрукт   | 1 игры | 1 барсук
+# 2 программиста  | 2 ёлки | 2 фрукта  | 2 игр  | 2 барсука
+# 3 программиста  | 3 ёлки | 3 фрукта  | 3 игр  | 3 барсука
+# 4 программиста  | 4 ёлки | 4 фрукта  | 4 игр  | 4 барсука
+# 5 программистов | 5 ёлок | 5 фруктов | 5 игр  | 5 барсуков
