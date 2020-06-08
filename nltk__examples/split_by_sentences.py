e2e2e44c03339c600ab4cc9c080315eab343ebc5
ipetrash <ilya.petrash@inbox.ru>
2020-06-09 00:16:52 +0500

    Added nltk__examples/split_by_sentences.py

---
+++ nltk__examples/split_by_sentences.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install nltk
+import nltk
+
+
+text = "Ай да А.С. Пушкин! Ай да сукин сын!"
+
+sentences = nltk.sent_tokenize(text)
+print(f"Before ({len(sentences)}): {sentences}")
+# Before (3): ['Ай да А.С.', 'Пушкин!', 'Ай да сукин сын!']
+
+sentences = nltk.sent_tokenize(text, language="russian")
+print(f"After ({len(sentences)}): {sentences}")
+# After (2): ['Ай да А.С. Пушкин!', 'Ай да сукин сын!']
