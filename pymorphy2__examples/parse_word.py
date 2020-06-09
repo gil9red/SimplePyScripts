0185c17b234eda6d38cbffc20008b78524d3aac3
ipetrash <ilya.petrash@inbox.ru>
2020-06-09 07:10:23 +0500

    Added pymorphy2__examples/parse_word.py

---
+++ pymorphy2__examples/parse_word.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install pymorphy2
+import pymorphy2
+
+
+morph = pymorphy2.MorphAnalyzer()
+
+words = [
+    "Думала", "Подумала", "Согласен"
+]
+
+for word in words:
+    print(repr(word))
+
+    parsed_word = morph.parse(word)[0]
+    print(parsed_word.tag)
+    print()
+
+# 'Думала'
+# VERB,impf,intr femn,sing,past,indc
+#
+# 'Подумала'
+# VERB,perf,intr femn,sing,past,indc
+#
+# 'Согласен'
+# ADJS,Qual masc,sing
