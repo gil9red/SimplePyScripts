e0824aef58ba267d88e1cbbd74c4ec68f35f96de
ipetrash <ilya.petrash@inbox.ru>
2020-06-11 23:43:20 +0500

    Added nltk__examples/nltk_sent_tokenize__with_new_lines.py

---
+++ nltk__examples/nltk_sent_tokenize__with_new_lines.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import re
+
+# pip install nltk
+import nltk
+
+
+text = """\
+Согласен.
+В первое ПОЕ
+Да вся вторая часть
+А читеры — подлецы.
+"""
+
+sentences = []
+for line in text.splitlines():
+    sentences += nltk.sent_tokenize(line, language='russian')
+
+print(*map(repr, sentences))
+# 'Согласен.' 'В первое ПОЕ' 'Да вся вторая часть' 'А читеры — подлецы.'
+
+print('-' * 100)
+
+text = text.replace('\n', '. ')
+text = re.sub('[.]{2,}', '.', text)
+
+sentences = nltk.sent_tokenize(text, language='russian')
+print(*map(repr, sentences))
+# 'Согласен.' 'В первое ПОЕ.' 'Да вся вторая часть.' 'А читеры — подлецы.'
