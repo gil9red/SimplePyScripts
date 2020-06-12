a87a6eb7e4f60f7f1cae2274b19ae989bbbe1bcf
ipetrash <ilya.petrash@inbox.ru>
2020-06-13 03:09:17 +0500

    Added sort_imports.py

---
+++ sort_imports.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+text_import = """
+import datetime as DT
+import sqlite3
+from typing import List, Union
+from pathlib import Path
+import os
+"""
+
+print('\n'.join(
+    sorted(text_import.strip().splitlines(), key=lambda x: x.split()[1])
+))
+# import datetime as DT
+# import os
+# from pathlib import Path
+# import sqlite3
+# from typing import List, Union
