4a7af092eb01b7f0174ba76a7301476788785c20
ipetrash <ilya.petrash@inbox.ru>
2020-06-23 15:17:08 +0500

    #

--- search_for_similar_images__perceptual_hash__phash/ui/AboutDialog.py
+++ search_for_similar_images__perceptual_hash__phash/ui/AboutDialog.py
@@ -22,7 +22,8 @@ class AboutDialog(QDialog):
         """)
         text_browser.setHtml("""
             <div>
-                Icons made by <a href="https://icon54.com/" title="Pixel perfect">Pixel perfect</a>,  
+                Icons made by 
+                <a href="https://icon54.com/" title="Pixel perfect">Pixel perfect</a>,  
                 <a href="https://www.flaticon.com/authors/those-icons" title="Those Icons">Those Icons</a>, 
                 <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a>
                 from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>, 
