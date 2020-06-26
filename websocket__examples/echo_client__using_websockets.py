22f0ae17565d4a72e73f3a01abb60cea74285eeb
ipetrash <ilya.petrash@inbox.ru>
2020-06-26 21:49:18 +0500

    Added websocket__examples/echo_client__using_websockets.py

---
+++ websocket__examples/echo_client__using_websockets.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://github.com/aaugustin/websockets
+
+
+import asyncio
+
+# pip install websockets
+import websockets
+
+
+async def hello(url: str):
+    async with websockets.connect(url) as websocket:
+        await websocket.send("Hello World!")
+
+        print(await websocket.recv())
+        # Hello World!
+
+
+if __name__ == '__main__':
+    # From http://websocket.org/echo.html
+    url = 'wss://echo.websocket.org'
+
+    asyncio.get_event_loop().run_until_complete(
+        hello(url)
+    )
