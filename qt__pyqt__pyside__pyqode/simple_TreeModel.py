6dab5f6b4eee57b400187565560e99cfbba8571d
ipetrash <ilya.petrash@inbox.ru>
2020-06-30 12:00:27 +0500

    Added qt__pyqt__pyside__pyqode/simple_TreeModel.py

---
+++ qt__pyqt__pyside__pyqode/simple_TreeModel.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from typing import List, Optional, Union
+
+from PyQt5.QtWidgets import QApplication, QTreeView, QMessageBox
+from PyQt5.QtCore import Qt, QAbstractItemModel, QModelIndex
+
+
+def log_uncaught_exceptions(ex_cls, ex, tb):
+    text = '{}: {}:\n'.format(ex_cls.__name__, ex)
+    import traceback
+    text += ''.join(traceback.format_tb(tb))
+
+    print(text)
+    QMessageBox.critical(None, 'Error', text)
+    sys.exit(1)
+
+
+import sys
+sys.excepthook = log_uncaught_exceptions
+
+
+class TreeItem:
+    def __init__(self, data: list):
+        self._childItems: List[TreeItem] = []
+        self._itemData: list = data
+        self._parentItem: Optional[TreeItem] = None
+
+    def appendChild(self, child: Union['TreeItem', str]) -> 'TreeItem':
+        if isinstance(child, str):
+            child = TreeItem([child])
+
+        child._parentItem = self
+        self._childItems.append(child)
+
+        return child
+
+    def appendChilds(self, childs: List['TreeItem']):
+        for x in childs:
+            self.appendChild(x)
+
+    def clearChilren(self):
+        self._childItems.clear()
+
+    def child(self, row: int) -> Optional['TreeItem']:
+        if row < 0 or row >= len(self._childItems):
+            return
+
+        return self._childItems[row]
+
+    def childCount(self) -> int:
+        return len(self._childItems)
+
+    def data(self, column: int):
+        return self._itemData[column]
+
+    def columnCount(self) -> int:
+        return len(self._itemData)
+
+    def row(self) -> int:
+        if self._parentItem:
+            return self._parentItem._childItems.index(self)
+        return 0
+
+    def parentItem(self) -> Optional['TreeItem']:
+        return self._parentItem
+
+
+class TreeModel(QAbstractItemModel):
+    column_names = ['File name']
+
+    def __init__(self):
+        super().__init__()
+
+        self._root_item = TreeItem(["ROOT"])
+
+    def setModelData(self, items: List[TreeItem]):
+        self.beginResetModel()
+
+        self._root_item.clearChilren()
+        self._root_item.appendChilds(items)
+
+        self.endResetModel()
+
+    def columnCount(self, parent: QModelIndex = ...) -> int:
+        return len(self.column_names)
+
+    def rowCount(self, parent_index: QModelIndex = ...) -> int:
+        if parent_index.isValid():
+            item = parent_index.internalPointer()
+        else:
+            item = self._root_item
+
+        return item.childCount()
+
+    def index(self, row: int, column: int, parent: QModelIndex = ...) -> QModelIndex:
+        if not self.hasIndex(row, column, parent):
+            return QModelIndex()
+
+        if not parent.isValid():
+            parentItem = self._root_item
+        else:
+            parentItem = parent.internalPointer()
+
+        childItem = parentItem.child(row)
+        if childItem:
+            return self.createIndex(row, column, childItem)
+
+        return QModelIndex()
+
+    def parent(self, child_index: QModelIndex) -> QModelIndex:
+        if not child_index.isValid():
+            return QModelIndex()
+
+        childItem = child_index.internalPointer()
+        parentItem = childItem.parentItem()
+        if parentItem == self._root_item:
+            return QModelIndex()
+
+        return self.createIndex(parentItem.row(), 0, parentItem)
+
+    def headerData(self, section: int, orientation=Qt.Horizontal, role=Qt.DisplayRole):
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            return self.column_names[section]
+        return
+
+    def data(self, index: QModelIndex, role=Qt.DisplayRole):
+        if not index.isValid():
+            return
+
+        item = index.internalPointer()
+
+        if role == Qt.DisplayRole:
+            return item.data(index.column())
+
+        return
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    items = []
+    for i in range(9999):
+        item = TreeItem([f'root_{i:04}'])
+        items.append(item)
+
+        for j in range(10):
+            child = item.appendChild(f'item__{i:04}/{j}')
+
+            for k in range(10):
+                child.appendChild(f'item__{i:04}/{j}/{k}')
+
+    model = TreeModel()
+    model.setModelData(items)
+
+    view = QTreeView()
+    view.setWindowTitle("Simple Tree Model")
+    view.setAlternatingRowColors(True)
+    view.setUniformRowHeights(True)  # Allows for scrolling optimizations.
+    view.setModel(model)
+    view.show()
+
+    sys.exit(app.exec_())
