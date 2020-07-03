33f8ad80474e4c290c6beb8883fb66de67ab90d4
ipetrash <ilya.petrash@inbox.ru>
2020-07-03 20:11:12 +0500

    Added datetime__get_prev_month.py

---
+++ datetime__get_prev_month.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://stackoverflow.com/a/7153449/5909792
+
+
+import datetime as DT
+
+
+prev_month = DT.date.today().replace(day=1) - DT.timedelta(days=1)
+print(prev_month)
