eef8ae1f21ec374a089b5d9a75aa3d89b57af52a
ipetrash <ilya.petrash@inbox.ru>
2020-07-05 14:09:26 +0500

    Added selenium__examples/mute.py

---
+++ selenium__examples/mute.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://stackoverflow.com/q/39392479/5909792
+
+
+# pip install selenium
+from selenium import webdriver
+
+
+# Firefox
+profile = webdriver.FirefoxProfile()
+profile.set_preference("media.volume_scale", "0.0")
+driver = webdriver.Firefox(firefox_profile=profile)
+
+# Chrome
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--mute-audio")
+driver = webdriver.Chrome(chrome_options=chrome_options)
