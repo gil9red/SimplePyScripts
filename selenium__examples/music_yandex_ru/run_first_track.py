7be1d8d4edbb8309d2ff0c0c8b5661bcf5000b20
ipetrash <ilya.petrash@inbox.ru>
2020-07-05 12:58:33 +0500

    #

--- selenium__examples/music_yandex_ru/run_first_track.py
+++ selenium__examples/music_yandex_ru/run_first_track.py
@@ -4,7 +4,8 @@
 __author__ = 'ipetrash'
 
 
-import os.path
+import time
+import sys
 
 # pip install selenium
 from selenium import webdriver
@@ -14,37 +15,52 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 
+sys.path.append('get_all_tracks_playlist')
+from config import profile, url
 
-profile_directory = os.path.expandvars(r'%AppData%\Mozilla\Firefox\Profiles\p75l82q1.for_mail__selenium')
-profile = webdriver.FirefoxProfile(profile_directory)
 
-url = 'https://music.yandex.ru/users/ilyapetrash/playlists/3'
-
-driver = webdriver.Firefox(profile)
-driver.implicitly_wait(2)
-driver.get(url)
-print(f'Title: {driver.title!r}')
-
-SEARCHING_TRACK = 'Шишки-телепаты'
-
-try:
-    tracks_el = WebDriverWait(driver, timeout=5).until(
-        EC.visibility_of_any_elements_located(
-            (By.CSS_SELECTOR, '.page-playlist__tracks-list .d-track')
+def play_track(driver, track_title: str):
+    try:
+        tracks_el = WebDriverWait(driver, timeout=5).until(
+            EC.visibility_of_any_elements_located(
+                (By.CSS_SELECTOR, '.page-playlist__tracks-list .d-track')
+            )
         )
-    )
-except TimeoutException:
-    tracks_el = []
+    except TimeoutException:
+        tracks_el = []
 
-track_el = None
-for x in tracks_el:
-    title = x.find_element_by_css_selector('.d-track__title').text
-    if title == SEARCHING_TRACK:
-        track_el = x
-        break
+    track_el = None
+    for x in tracks_el:
+        title = x.find_element_by_css_selector('.d-track__title').text
+        if title == track_title:
+            track_el = x
+            break
 
-if track_el:
-    el = track_el.find_element_by_css_selector('.d-track__start-column')
-    ActionChains(driver).move_to_element(el).click().perform()
-else:
-    print(f'Not result for: {SEARCHING_TRACK!r}')
+    if track_el:
+        el = track_el.find_element_by_css_selector('.d-track__start-column')
+        ActionChains(driver).move_to_element(el).click().perform()
+    else:
+        print(f'Not result for: {track_title!r}')
+
+
+if __name__ == '__main__':
+    driver = None
+    try:
+        driver = webdriver.Firefox(profile)
+        driver.implicitly_wait(2)
+        driver.get(url)
+        print(f'Title: {driver.title!r}')
+
+        time.sleep(2)
+
+        SEARCHING_TRACK = 'Шишки-телепаты'
+        play_track(driver, SEARCHING_TRACK)
+
+        # Чтобы скрипт завершился вместо с инстанцией селениума
+        while True:
+            _ = driver.title
+            time.sleep(0.1)
+
+    finally:
+        if driver:
+            driver.quit()
