19e238d27f5f77cae865e242684d85f7455b22f2
gil9red <ilya.petrash@inbox.ru>
2020-07-06 18:29:01 +0500

    The launch is now always at 20:00

--- parse_jira_Assigned_Open_Issues_per_Project/main.py
+++ parse_jira_Assigned_Open_Issues_per_Project/main.py
@@ -14,24 +14,41 @@ from get_assigned_open_issues_per_project import get_and_prints
 import db
 
 
-while True:
-    try:
-        print(f'Начало в {DT.date.today()}\n')
+def run():
+    while True:
+        try:
+            print(f'Начало в {DT.date.today()}\n')
 
-        assigned_open_issues_per_project = get_and_prints()
-        print()
+            assigned_open_issues_per_project = get_and_prints()
+            print()
 
-        ok = db.add(assigned_open_issues_per_project)
-        if ok is None:
-            print("Количество открытых задач в проектах не поменялось. Пропускаю...")
+            ok = db.add(assigned_open_issues_per_project)
+            if ok is None:
+                print("Количество открытых задач в проектах не поменялось. Пропускаю...")
+            elif ok:
+                print("Добавляю запись")
+            else:
+                print("Сегодня запись уже была добавлена. Пропускаю...")
 
-        print('\n' + '-' * 100 + '\n')
-        wait(days=1)
+            print('\n' + '-' * 100 + '\n')
+            break
 
-    except Exception:
-        import traceback
-        print('Ошибка:')
-        print(traceback.format_exc())
+        except Exception:
+            import traceback
+            print('Ошибка:')
+            print(traceback.format_exc())
 
-        print('Через 15 минут попробую снова...')
-        wait(minutes=15)
+            print('Через 15 минут попробую снова...')
+            wait(minutes=15)
+
+
+if __name__ == '__main__':
+    # pip install schedule
+    import schedule
+    import time
+
+    schedule.every().day.at("20:00").do(run)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
