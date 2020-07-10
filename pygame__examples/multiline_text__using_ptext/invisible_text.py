b994eabc273a2574b972906f5c6a8c78cb5bc409
ipetrash <ilya.petrash@inbox.ru>
2020-07-10 12:35:46 +0500

    Refactoring

--- pygame__examples/multiline_text__using_ptext/invisible_text.py
+++ pygame__examples/multiline_text__using_ptext/invisible_text.py
@@ -26,9 +26,6 @@ surf.fill(BLACK)
 rect = surf.get_rect()
 
 
-fnt = pygame.font.Font(None, 40)
-
-
 text = """\
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
 sed do eiusmod tempor incididunt ut labore et dolore 
