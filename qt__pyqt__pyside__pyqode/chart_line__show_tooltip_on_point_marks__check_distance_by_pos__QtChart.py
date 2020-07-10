f293d3ab0b4901f9fd7548e6a8e33255d1759a8f
ipetrash <ilya.petrash@inbox.ru>
2020-07-10 19:44:36 +0500

    Added qt__pyqt__pyside__pyqode/chart_line__show_tooltip_on_point_marks__check_distance_by_pos__QtChart.py

---
+++ qt__pyqt__pyside__pyqode/chart_line__show_tooltip_on_point_marks__check_distance_by_pos__QtChart.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import math
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QPainter
+from PyQt5.QtCore import QRectF, QPoint, QSizeF
+from PyQt5.QtChart import QChart, QChartView, QLineSeries
+
+from chart_line__show_tooltip_on_series__QtChart import Callout
+
+
+class MainWindow(QChartView):
+    def __init__(self):
+        super().__init__()
+
+        self.setRenderHint(QPainter.Antialiasing)
+
+        series = QLineSeries()
+        series.setPointsVisible(True)
+        series.setPointLabelsVisible(True)
+        series.setPointLabelsFormat("(@xPoint, @yPoint)")
+        series.hovered.connect(self.show_series_tooltip)
+        series.clicked.connect(self.keepCallout)
+
+        series.append(0, 6)
+        series.append(2, 4)
+        series.append(3, 8)
+        series.append(7, 4)
+        series.append(10, 5)
+
+        self._chart = QChart()
+        self._chart.setMinimumSize(640, 480)
+        self._chart.setTitle("Line Chart Example")
+        self._chart.setAnimationOptions(QChart.SeriesAnimations)
+        self._chart.legend().hide()
+        self._chart.addSeries(series)
+        self._chart.createDefaultAxes()
+
+        self.setChart(self._chart)
+
+        self._tooltip = None
+        self._callouts = []
+
+    def show_series_tooltip(self, point, state: bool):
+        # value -> pos
+        point = self._chart.mapToPosition(point)
+
+        if not self._tooltip:
+            self._tooltip = Callout(self._chart)
+
+        if state:
+            distance = 20
+            for series in self._chart.series():
+                for p_value in series.pointsVector():
+                    p = self._chart.mapToPosition(p_value)
+
+                    current_distance = math.sqrt(
+                        (p.x() - point.x()) * (p.x() - point.x())
+                        + (p.y() - point.y()) * (p.y() - point.y())
+                    )
+                    if current_distance < distance:
+                        self._tooltip.setText("X: {}\nY: {}".format(p.x(), p.y()))
+                        self._tooltip.setAnchor(p_value)
+                        self._tooltip.setZValue(11)
+                        self._tooltip.updateGeometry()
+                        self._tooltip.show()
+        else:
+            self._tooltip.hide()
+
+    def keepCallout(self):
+        self._callouts.append(self._tooltip)
+        self._tooltip = Callout(self._chart)
+
+    def resizeEvent(self, event):
+        if self.scene():
+            size = QSizeF(event.size())
+
+            self.scene().setSceneRect(QRectF(QPoint(0, 0), size))
+            self._chart.resize(size)
+
+            for callout in self._callouts:
+                callout.updateGeometry()
+
+        super().resizeEvent(event)
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    mw = MainWindow()
+    mw.setWindowTitle("Chart Line")
+    mw.resize(800, 600)
+    mw.show()
+
+    app.exec()
