7f32e32f955a3846ddf14a4651e54011c531f256
ipetrash <ilya.petrash@inbox.ru>
2020-07-14 16:31:43 +0500

    Added proxy/proxy_requests__examples.py

---
+++ proxy/proxy_requests__examples.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install proxy-requests
+from proxy_requests import ProxyRequests
+
+
+rs = ProxyRequests('https://api.ipify.org')
+rs.get()
+print(rs.get_raw())
+# b'128.199.214.87'
