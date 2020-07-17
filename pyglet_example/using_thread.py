26e12fb675079f1f91d96240f6d991ea8230b1d5
ipetrash <ilya.petrash@inbox.ru>
2020-07-17 23:39:58 +0500

    Added pyglet_example/using_thread.py

---
+++ pyglet_example/using_thread.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from threading import Thread
+
+# pip install pyglet
+import pyglet
+
+
+def play_song():
+    song = pyglet.media.load('speak.mp3')
+    song.play()
+    pyglet.app.run()
+
+
+thread = Thread(target=play_song)
+thread.start()
+
+print("Song...")
