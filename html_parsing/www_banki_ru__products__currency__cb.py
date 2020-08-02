f9e76ef146318e9dfe46a3329383263b6b0c21b3
ipetrash <ilya.petrash@inbox.ru>
2020-08-02 22:06:15 +0500

    Added html_parsing/www_banki_ru__products__currency__cb.py

---
+++ html_parsing/www_banki_ru__products__currency__cb.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from bs4 import BeautifulSoup
+import requests
+
+
+url = 'https://www.banki.ru/products/currency/cb/'
+
+rs = requests.get(url)
+root = BeautifulSoup(rs.content, 'html.parser')
+
+for tr in root.select('tbody > tr[data-currency-code]'):
+    td_items = tr.select('td')
+    td_code, td_num, td_name, td_value, td_change = \
+        map(lambda x: x.get_text(strip=True), td_items)
+    print(td_code, td_num, td_name, td_value, td_change)
+
+# USD 1 Доллар США 73.4261 +0,0628
+# EUR 1 Евро 87.2889 +1,0357
+# AUD 1 Австралийский доллар 52.8154 +0,4633
+# ...
+# ZAR 10 Южноафриканский рэнд 43.4922 -0,2144
+# JPY 100 Японская иена 70.1870 +0,3803
