823aa554badb1f0c1b2cc79f49e8a1758140a3e8
ipetrash <ilya.petrash@inbox.ru>
2020-08-02 16:29:37 +0500

    Added telegram_bot_examples/send_contact_or_location.py

---
+++ telegram_bot_examples/send_contact_or_location.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import os
+import time
+
+# pip install python-telegram-bot
+from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
+from telegram.ext import Updater, MessageHandler, CommandHandler, Filters, CallbackContext
+from telegram.ext.dispatcher import run_async
+
+import config
+from common import get_logger, log_func
+
+
+location_keyboard = KeyboardButton("Send location",  request_location=True)
+contact_keyboard = KeyboardButton('Share contact', request_contact=True)
+custom_keyboard = [[location_keyboard, contact_keyboard]]
+REPLY_KEYBOARD_MARKUP = ReplyKeyboardMarkup(custom_keyboard)
+
+
+log = get_logger(__file__)
+
+
+@run_async
+@log_func(log)
+def on_start(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        f'Введите что-нибудь',
+        reply_markup=REPLY_KEYBOARD_MARKUP
+    )
+
+
+@run_async
+@log_func(log)
+def on_request(update: Update, context: CallbackContext):
+    message = update.message
+
+    message.reply_text(
+        'Echo: ' + message.text,
+        reply_markup=REPLY_KEYBOARD_MARKUP
+    )
+
+
+@run_async
+@log_func(log)
+def on_contact_or_location(update: Update, context: CallbackContext):
+    message = update.message
+
+    text = ''
+    if message.contact:
+        text += str(message.contact)
+
+    if message.location:
+        text += str(message.location)
+
+    message.reply_text(
+        text,
+        reply_markup=REPLY_KEYBOARD_MARKUP
+    )
+
+
+def error_callback(update: Update, context: CallbackContext):
+    log.exception('Error: %s\nUpdate: %s', context.error, update)
+    update.message.reply_text(config.ERROR_TEXT)
+
+
+def main():
+    cpu_count = os.cpu_count()
+    workers = cpu_count
+    log.debug('System: CPU_COUNT=%s, WORKERS=%s', cpu_count, workers)
+
+    log.debug('Start')
+
+    # Create the EventHandler and pass it your bot's token.
+    updater = Updater(
+        config.TOKEN,
+        workers=workers,
+        use_context=True
+    )
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler('start', on_start))
+    dp.add_handler(MessageHandler(Filters.text, on_request))
+    dp.add_handler(MessageHandler(Filters.contact | Filters.location, on_contact_or_location))
+
+    # log all errors
+    dp.add_error_handler(error_callback)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until the you presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+    log.debug('Finish')
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            main()
+        except:
+            log.exception('')
+
+            timeout = 15
+            log.info(f'Restarting the bot after {timeout} seconds')
+            time.sleep(timeout)
