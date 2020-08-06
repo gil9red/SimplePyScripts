499f695886b41f883e97fe2c654b37cc95f6a65f
ipetrash <ilya.petrash@inbox.ru>
2020-08-07 02:59:00 +0500

    Added selenium__examples/set_user_agent.py

---
+++ selenium__examples/set_user_agent.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://stackoverflow.com/a/29966769/5909792
+
+
+# pip install selenium
+from selenium import webdriver
+
+
+url = 'https://www.whatismybrowser.com/detect/what-is-my-user-agent'
+
+driver = webdriver.Firefox()
+try:
+    driver.get(url)
+    print(f'Title: {driver.title!r}\n')
+
+    print('From js. User-Agent:', driver.execute_script("return navigator.userAgent"))
+    print('From site. User-Agent:', driver.find_element_by_id('detected_value').text)
+
+finally:
+    driver.quit()
+
+
+print('\n' + '-' * 100 + '\n')
+
+print('Change User-Agent on mobile')
+
+profile = webdriver.FirefoxProfile()
+profile.set_preference("general.useragent.override", "Mozilla/5.0 (Android 10; Mobile; rv:78.0) Gecko/78.0 Firefox/78.0")
+
+driver = webdriver.Firefox(profile)
+try:
+    driver.get(url)
+    print(f'Title: {driver.title!r}\n')
+
+    print('From js. User-Agent:', driver.execute_script("return navigator.userAgent"))
+    print('From site. User-Agent:', driver.find_element_by_id('detected_value').text)
+
+finally:
+    driver.quit()
