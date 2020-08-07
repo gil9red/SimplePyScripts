573aa2c730f68cf3b8030dd166d81d6348efb34c
ipetrash <ilya.petrash@inbox.ru>
2020-08-07 21:07:33 +0500

    Added pil_pillow__examples/crop.py

---
+++ pil_pillow__examples/crop.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install Pillow
+from PIL import Image
+
+
+image_file = "blur/input.jpg"
+img = Image.open(image_file)
+
+cropped_img = img.crop((175, 42, 336, 170))
+cropped_img.show()
