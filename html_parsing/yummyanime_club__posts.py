2103c0d85642e9d0510d2d363a3858a0edf50421
ipetrash <ilya.petrash@inbox.ru>
2020-08-08 19:42:44 +0500

    Refactoring

--- html_parsing/yummyanime_club__posts.py
+++ html_parsing/yummyanime_club__posts.py
@@ -38,10 +38,6 @@ URL = 'https://yummyanime.club/posts'
 headers = {
     'Host': 'yummyanime.club',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
-    'Accept-Encoding': 'gzip, deflate',
-    'Connection': 'close',
 }
 
 session = requests.session()
