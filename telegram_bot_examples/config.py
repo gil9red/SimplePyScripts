d2b8aa50bd48b810b13bb86018a03b02e2e92ade
ipetrash <ilya.petrash@inbox.ru>
2020-08-08 06:31:30 +0500

    Refactoring

--- telegram_bot_examples/config.py
+++ telegram_bot_examples/config.py
@@ -5,8 +5,12 @@ __author__ = 'ipetrash'
 
 
 import os
+from pathlib import Path
 
 
-TOKEN = os.environ.get('TOKEN') or open('TOKEN.txt', encoding='utf-8').read().strip()
+DIR = Path(__file__).resolve().parent
+TOKEN_FILE_NAME = DIR / 'TOKEN.txt'
+
+TOKEN = os.environ.get('TOKEN') or TOKEN_FILE_NAME.read_text('utf-8').strip()
 
 ERROR_TEXT = '⚠ Возникла какая-то проблема. Попробуйте повторить запрос или попробовать чуть позже...'
