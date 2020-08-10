12158467d08032431c321533a9794c4f48546097
ipetrash <ilya.petrash@inbox.ru>
2020-08-10 18:53:53 +0500

    Added telegram__telethon__examples/print_dialogs.py

---
+++ telegram__telethon__examples/print_dialogs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://github.com/LonamiWebs/Telethon
+# SOURCE: https://my.telegram.org/apps
+
+
+# pip install telethon
+from telethon.sync import TelegramClient
+
+from config import API_ID, API_HASH
+
+
+with TelegramClient('name', API_ID, API_HASH) as client:
+    for i, dialog in enumerate(client.iter_dialogs(), 1):
+        print(f'{i:3}. {dialog.name!r}')
