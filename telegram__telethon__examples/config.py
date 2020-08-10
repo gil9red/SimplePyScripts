8031826206b88443520a2df77d5bc91122f49792
ipetrash <ilya.petrash@inbox.ru>
2020-08-10 18:27:09 +0500

    Added telegram__telethon__examples

---
+++ telegram__telethon__examples/config.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import os
+from pathlib import Path
+
+
+DIR = Path(__file__).resolve().parent
+
+
+API_HASH_FILE_NAME = DIR / 'API_HASH.txt'
+API_ID_FILE_NAME   = DIR / 'API_ID.txt'
+
+API_HASH = os.environ.get('API_HASH') or API_HASH_FILE_NAME.read_text('utf-8').strip()
+API_ID   = os.environ.get('API_ID') or API_ID_FILE_NAME.read_text('utf-8').strip()
---
+++ telegram__telethon__examples/hello_world.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://github.com/LonamiWebs/Telethon
+# SOURCE: https://my.telegram.org/apps
+
+
+# pip install telethon
+from telethon.sync import TelegramClient, events
+
+from config import API_ID, API_HASH
+
+
+with TelegramClient('name', API_ID, API_HASH) as client:
+   client.send_message('me', 'Hello, myself!')
+   print(client.download_profile_photo('me'))
+
+   @client.on(events.NewMessage(pattern='(?i).*Hello'))
+   async def handler(event):
+      await event.reply('Hey!')
+
+   client.run_until_disconnected()
