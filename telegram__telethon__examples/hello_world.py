b4d2d1c86cb10a9c095859aaad4e04f883c5709b
ipetrash <ilya.petrash@inbox.ru>
2020-08-10 18:31:06 +0500

    Refactoring

--- telegram__telethon__examples/hello_world.py
+++ telegram__telethon__examples/hello_world.py
@@ -15,11 +15,11 @@ from config import API_ID, API_HASH
 
 
 with TelegramClient('name', API_ID, API_HASH) as client:
-   client.send_message('me', 'Hello, myself!')
-   print(client.download_profile_photo('me'))
+    client.send_message('me', 'Hello, myself!')
+    print(client.download_profile_photo('me'))
 
-   @client.on(events.NewMessage(pattern='(?i).*Hello'))
-   async def handler(event):
-      await event.reply('Hey!')
+    @client.on(events.NewMessage(pattern='(?i).*Hello'))
+    async def handler(event):
+        await event.reply('Hey!')
 
-   client.run_until_disconnected()
+    client.run_until_disconnected()
