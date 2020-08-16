d3e60115df285e30b6ce5b4b59b6846ac18359d7
ipetrash <ilya.petrash@inbox.ru>
2020-08-16 11:27:50 +0500

    Added html_parsing/anekdotme_ru__random.py

---
+++ html_parsing/anekdotme_ru__random.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from bs4 import BeautifulSoup
+import requests
+
+
+def get_anekdot() -> str:
+    rs = requests.get('http://anekdotme.ru/random')
+    root = BeautifulSoup(rs.content, "html.parser")
+
+    el = root.select_one('.anekdot_text')
+    return el.get_text(strip=True, separator='\n')
+
+
+if __name__ == '__main__':
+    print(get_anekdot())
+    """
+    Урок языковедения в школе. Училка распинается:
+    — Есть языки, в которых двойное отрицание означает отрицание, утверждение
+    и отрицание все-равно означает отрицание. Но нет ни одного языка, где
+    бы двойное утверждение означало отрицание!
+    Голос с последней парты:
+    — Ну да, конечно!
+    """
