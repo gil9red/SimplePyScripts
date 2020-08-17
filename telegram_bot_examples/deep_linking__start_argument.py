a18f26d78eeccc3355bfcbf36db4fa87c9e9cb0e
ipetrash <ilya.petrash@inbox.ru>
2020-08-17 09:08:29 +0500

    Added telegram_bot_examples/deep_linking__start_argument.py

---
+++ telegram_bot_examples/deep_linking__start_argument.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://core.telegram.org/bots#deep-linking
+# SOURCE: https://github.com/python-telegram-bot/python-telegram-bot/wiki/Types-Of-Handlers#deep-linking-start-parameters
+
+
+import os
+import time
+
+# pip install python-telegram-bot
+from telegram import Update
+from telegram.ext import Updater, MessageHandler, CommandHandler, Filters, CallbackContext
+from telegram.ext.dispatcher import run_async
+
+import config
+from common import get_logger, log_func
+
+
+log = get_logger(__file__)
+
+
+@run_async
+@log_func(log)
+def on_start(update: Update, context: CallbackContext):
+    start_argument = ''
+    if context.args:
+        # https://t.me/<bot>?start=<start_argument>
+        start_argument = context.args[0]
+
+    update.message.reply_text(
+        'Start argument: ' + start_argument
+    )
+
+
+@run_async
+@log_func(log)
+def on_request(update: Update, context: CallbackContext):
+    message = update.message
+
+    message.reply_text(
+        message.text
+    )
+
+
+def on_error(update: Update, context: CallbackContext):
+    log.exception('Error: %s\nUpdate: %s', context.error, update)
+    if update and update.message:
+        update.message.reply_text(config.ERROR_TEXT)
+
+
+def main():
+    cpu_count = os.cpu_count()
+    workers = cpu_count
+    log.debug('System: CPU_COUNT=%s, WORKERS=%s', cpu_count, workers)
+
+    log.debug('Start')
+
+    # Create the EventHandler and pass it your bot's token.
+    updater = Updater(
+        config.TOKEN,
+        workers=workers,
+        use_context=True
+    )
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler('start', on_start))
+    dp.add_handler(MessageHandler(Filters.text, on_request))
+
+    # Handle all errors
+    dp.add_error_handler(on_error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until the you presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+    log.debug('Finish')
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            main()
+        except:
+            log.exception('')
+
+            timeout = 15
+            log.info(f'Restarting the bot after {timeout} seconds')
+            time.sleep(timeout)
