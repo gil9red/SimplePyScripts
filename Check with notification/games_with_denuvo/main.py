0aa46cc17f5f833382dae0c58a05118c7eff5f77
ipetrash <ilya.petrash@inbox.ru>
2020-08-19 11:58:00 +0500

    Refactoring

--- Check with notification/games_with_denuvo/main.py
+++ Check with notification/games_with_denuvo/main.py
@@ -10,42 +10,42 @@ __author__ = 'ipetrash'
 """
 
 
+import time
+
 from common import *
 from games_with_denuvo import get_games_with_denuvo, get_games_which_denuvo_is_removed
 
 
-if __name__ == '__main__':
-    # connect = create_connect()
-    # connect.execute("DROP TABLE IF EXISTS Game")
-    # connect.commit()
+# connect = create_connect()
+# connect.execute("DROP TABLE IF EXISTS Game")
+# connect.commit()
 
-    init_db()
+init_db()
 
-    # NOTE: С этим флагом нужно быть осторожным при первом запуске, когда база пуста,
-    # ведь на каждую добавленную взломанную игру отправится уведомление по смс
-    notified_by_sms = True
+# NOTE: С этим флагом нужно быть осторожным при первом запуске, когда база пуста,
+# ведь на каждую добавленную взломанную игру отправится уведомление по смс
+notified_by_sms = True
 
-    while True:
-        try:
-            log.debug('get_games_with_denuvo')
+while True:
+    try:
+        log.debug('get_games_with_denuvo')
 
-            games = get_games_with_denuvo()
-            log.debug('games (%s): %s', len(games), games)
+        games = get_games_with_denuvo()
+        log.debug('games (%s): %s', len(games), games)
 
-            games_without_denuvo = get_games_which_denuvo_is_removed()
-            log.debug('games_without_denuvo (%s): %s', len(games_without_denuvo), games_without_denuvo)
+        games_without_denuvo = get_games_which_denuvo_is_removed()
+        log.debug('games_without_denuvo (%s): %s', len(games_without_denuvo), games_without_denuvo)
 
-            append_list_games_which_denuvo_is_removed(games_without_denuvo, notified_by_sms)
-            append_list_games(games, notified_by_sms)
+        append_list_games_which_denuvo_is_removed(games_without_denuvo, notified_by_sms)
+        append_list_games(games, notified_by_sms)
 
-            db_create_backup()
+        db_create_backup()
 
-            wait(days=3)
+        wait(days=3)
 
-        except Exception:
-            log.exception('Ошибка:')
-            log.debug('Через 5 минут попробую снова...')
+    except Exception:
+        log.exception('Ошибка:')
+        log.debug('Через 5 минут попробую снова...')
 
-            # Wait 5 minutes before next attempt
-            import time
-            time.sleep(5 * 60)
+        # Wait 5 minutes before next attempt
+        time.sleep(5 * 60)
