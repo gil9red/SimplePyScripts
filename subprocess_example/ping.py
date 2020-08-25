7fa1bbba57726d3cde374740f73d78dc25cb6358
ipetrash <ilya.petrash@inbox.ru>
2020-08-25 09:25:06 +0500

    Refactoring

--- subprocess_example/ping.py
+++ subprocess_example/ping.py
@@ -4,19 +4,14 @@
 __author__ = 'ipetrash'
 
 
-from subprocess import Popen, PIPE
-
-if __name__ == '__main__':
-    ipconfig_res = Popen("ping ya.ru", universal_newlines=True, stdout=PIPE)
-    for line in ipconfig_res.stdout:
-        print(line, end='')
+from subprocess import Popen, PIPE, STDOUT
 
 
-# import subprocess
-#
-# if __name__ == '__main__':
-#     ping_res = subprocess.Popen("ping ya.ru", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-#     for line in ping_res.stdout.readlines():
-#         line = line.rstrip()
-#         if line:
-#             print(line.decode('cp866'))
+ping_res = Popen("ping ya.ru", stdout=PIPE, stderr=STDOUT)
+
+text = ''
+for line in ping_res.stdout.readlines():
+    line = line.decode('cp866')
+    text += line
+
+print(text)
