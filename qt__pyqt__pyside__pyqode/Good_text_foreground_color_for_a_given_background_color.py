4ee1ac9d756ee977addc71bd53105bc456bb2bc3
ipetrash <ilya.petrash@inbox.ru>
2020-08-27 23:58:53 +0500

    Added qt__pyqt__pyside__pyqode/Good_text_foreground_color_for_a_given_background_color.py

---
+++ qt__pyqt__pyside__pyqode/Good_text_foreground_color_for_a_given_background_color.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.QtGui import QColor
+from PyQt5.QtCore import Qt
+
+
+# SOURCE: https://stackoverflow.com/a/3943023/5909792
+def get_good_text_foreground_color(color: QColor) -> QColor:
+    _, r, g, b = color.getRgb()
+
+    if (r * 0.299 + g * 0.587 + b * 0.114) > 186:
+        return QColor(Qt.white)
+
+    return QColor(Qt.black)
+
