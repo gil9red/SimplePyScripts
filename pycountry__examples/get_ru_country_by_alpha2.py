89204c5437bffd0710c0a2e9a87acb2e7d46a1d9
ipetrash <ilya.petrash@inbox.ru>
2020-08-28 19:18:17 +0500

    Added pycountry__examples/get_ru_country_by_alpha2.py

---
+++ pycountry__examples/get_ru_country_by_alpha2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import gettext
+
+# pip install pycountry
+import pycountry
+
+russian = gettext.translation("iso3166", pycountry.LOCALES_DIR, languages=["ru"])
+russian.install()
+
+
+def get_country(code):
+    ru = pycountry.countries.get(alpha_2=code)
+    return _(ru.name)
+
+
+if __name__ == '__main__':
+    print(get_country("RU"))
+    # Российская Федерация
+
+    print(get_country("US"))
+    # Соединённые штаты
+
+    print(get_country("DE"))
+    # Германия
+
+    print(get_country("CA"))
+    # Канада
+
+    print(get_country("FR"))
+    # Франция
