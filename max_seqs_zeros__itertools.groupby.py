3812449246ce8ed7a7d6b780ad275d9a7659d1d5
ipetrash <ilya.petrash@inbox.ru>
2020-09-08 20:00:49 +0500

    Added max_seqs_zeros__itertools.groupby.py

---
+++ max_seqs_zeros__itertools.groupby.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import itertools
+
+
+def end_zeros(num: int) -> int:
+    size = 0
+
+    for x, items in itertools.groupby(str(num)):
+        if x == '0':
+            size = max(size, len(list(items)))
+
+    return size
+
+
+if __name__ == '__main__':
+    assert end_zeros(10979000000) == 6
+    assert end_zeros(1000000) == 6
+    assert end_zeros(10009) == 3
