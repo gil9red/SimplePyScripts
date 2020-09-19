ad2c9b6d7bb782e7178c82fd26e975339e92b739
ipetrash <ilya.petrash@inbox.ru>
2020-09-19 07:06:10 +0500

    Refactoring

--- PyGithub_examples/config.py
+++ PyGithub_examples/config.py
@@ -4,7 +4,14 @@
 __author__ = 'ipetrash'
 
 
-TOKEN = None
+import os
+from pathlib import Path
+
+
+DIR = Path(__file__).resolve().parent
+TOKEN_FILE_NAME = DIR / 'TOKEN.txt'
+
+TOKEN = os.environ.get('TOKEN') or TOKEN_FILE_NAME.read_text('utf-8').strip()
 
 # http://user:password@proxy_host:proxy_port
 PROXY = None
