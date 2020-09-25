e7ba83046ec63dd8a38d77c96e5aa88d909f6898
ipetrash <ilya.petrash@inbox.ru>
2020-09-26 04:43:50 +0500

    Added bleach__examples__remove_tags__clear_html/linkify__callbacks_for_removing_links.py

---
+++ bleach__examples__remove_tags__clear_html/linkify__callbacks_for_removing_links.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://github.com/mozilla/bleach
+# SOURCE: https://bleach.readthedocs.io/en/latest/linkify.html#removing-links
+
+
+# pip install bleach
+from bleach.linkifier import Linker
+
+
+# Removing Links
+
+def remove_mailto(attrs, new=False):
+    if attrs[(None, 'href')].startswith('mailto:'):
+        return None
+    return attrs
+
+
+linker = Linker(callbacks=[remove_mailto])
+
+
+html = '''
+<a href="mailto:janet@example.com">mail janet!</a>
+abc <a href="http://example.com">http://example.com</a> def
+'''.strip()
+print(linker.linkify(html))
+# mail janet!
+# abc <a href="http://example.com">http://example.com</a> def
