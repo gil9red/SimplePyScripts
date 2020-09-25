8b7ec5a15a46b62db36ee9a91e2b288badb0f6c0
ipetrash <ilya.petrash@inbox.ru>
2020-09-26 02:03:16 +0500

    Added bleach__examples__remove_tags__clear_html/class_Cleaner.py

---
+++ bleach__examples__remove_tags__clear_html/class_Cleaner.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://github.com/mozilla/bleach
+# SOURCE: https://bleach.readthedocs.io/en/latest/clean.html#using-bleach-sanitizer-cleaner
+
+
+# pip install bleach
+from bleach.sanitizer import Cleaner
+
+
+html = '''
+<p><span>is not <b><span>allowed</span></b></span></p>
+<p><b>Hello World!</b></p>
+'''.strip()
+
+cleaner = Cleaner()
+
+for text in html.splitlines():
+    print(cleaner.clean(text))
+# &lt;p&gt;&lt;span&gt;is not <b>&lt;span&gt;allowed&lt;/span&gt;</b>&lt;/span&gt;&lt;/p&gt;
+# &lt;p&gt;<b>Hello World!</b>&lt;/p&gt;
+
+print()
+
+cleaner = Cleaner(tags=['b', 'span'], strip=True)
+for text in html.splitlines():
+    print(cleaner.clean(text))
+# <span>is not <b><span>allowed</span></b></span>
+# <b>Hello World!</b>
