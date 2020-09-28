c1ede5a1087d0b7bd2c12ef9c716068a4c25a236
ipetrash <ilya.petrash@inbox.ru>
2020-09-29 03:20:43 +0500

    Added multithreading__threading__examples/ThreadPoolExecutor__examples

---
+++ multithreading__threading__examples/ThreadPoolExecutor__examples/hello_world.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://docs.python.org/dev/library/concurrent.futures.html#threadpoolexecutor-example
+
+
+import concurrent.futures
+import urllib.request
+
+URLS = [
+    'http://www.foxnews.com/',
+    'http://www.cnn.com/',
+    'http://europe.wsj.com/',
+    'http://www.bbc.co.uk/',
+    'http://some-made-up-domain.com/'
+]
+
+MAX_WORKERS = 5
+
+
+# Retrieve a single page and report the URL and contents
+def load_url(url, timeout):
+    with urllib.request.urlopen(url, timeout=timeout) as conn:
+        return conn.read()
+
+
+# We can use a with statement to ensure threads are cleaned up promptly
+with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+    # Start the load operations and mark each future with its URL
+    future_to_url = {executor.submit(load_url, url, 60): url for url in URLS}
+    for future in concurrent.futures.as_completed(future_to_url):
+        url = future_to_url[future]
+        try:
+            data = future.result()
+        except Exception as e:
+            print(f'{url!r} generated an exception: {e}')
+        else:
+            print(f'{url!r} page is {len(data)} bytes')
