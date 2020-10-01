c3e7e9936d8c2cc6cd316b5932307e5fa3ac70b3
ipetrash <ilya.petrash@inbox.ru>
2020-10-01 20:19:10 +0500

    Bug fix

--- html_parsing/youtube_com__download_previews_playlist/main.py
+++ html_parsing/youtube_com__download_previews_playlist/main.py
@@ -4,42 +4,29 @@
 __author__ = 'ipetrash'
 
 
-import json
-import re
 import sys
 from pathlib import Path
 
 # pip install dpath
 import dpath.util
 
-import requests
-
+sys.path.append('..')
 sys.path.append('../..')
 from get_valid_filename import get_valid_filename
+from youtube_com__results_search_query import get_ytInitialData, session
 
-
-USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0'
-PATTERN = re.compile(r'window\["ytInitialData"\] = (\{.+?\});')
 
 PATTERN_URL_PLAYLIST = 'https://www.youtube.com/playlist?list={playlist_id}'
 PATTERN_URL_VIDEO_PREVIEW = 'https://i.ytimg.com/vi/{video_id}/maxresdefault.jpg'
 
 
-session = requests.Session()
-session.headers['User-Agent'] = USER_AGENT
-
-
 def download_playlist_video_previews(playlist_id: str):
     url = PATTERN_URL_PLAYLIST.format(playlist_id=playlist_id)
 
-    rs = session.get(url)
-    m = PATTERN.search(rs.text)
-    if not m:
+    data = get_ytInitialData(url)
+    if not data:
         print('Not found "ytInitialData"!')
         return
-
-    data_str = m.group(1)
-    data = json.loads(data_str)
 
     playlist_title = dpath.util.get(data, '**/playlistMetadataRenderer/title')
     safe_playlist_title = get_valid_filename(playlist_title)
@@ -52,7 +39,7 @@ def download_playlist_video_previews(playlist_id: str):
     for video in videos:
         video_id = video['videoId']
         seq = video['index']['simpleText']
-        title = video['title']['simpleText']
+        title = dpath.util.get(video, 'title/runs/0/text')
 
         safe_title = get_valid_filename(title)
         file_name = dir_name / f'{seq}. {safe_title}. {video_id}.jpg'
