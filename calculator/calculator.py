a7ba6b502c8a9a6f2268c49aa1511518ad2b5293
Ilya Petrash <ilya.petrash@inbox.ru>
2020-10-01 17:38:54 +0500

    Update calculator.py

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,28 +1,39 @@
-while True:
-    options = input("Enter 'add', 'substract','multiply',divide','quit',power")
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-    if options == 'quit':
+__author__ = 'ipetrash'
+
+
+while True:
+    oper = input("Enter 'add', 'substract', 'multiply', 'divide', 'quit', 'power'")
+    if oper == 'quit':
         break
-    elif options == 'add':
-        x = int(input("Enter 1st num"))
-        y = int(input("Enter 2nd num"))
-        print(x + y)
-    elif options == 'substract':
-        x = int(input("Enter 1st num"))
-        y = int(input("Enter 2nd num"))
-        print(x - y)
-    elif options == 'multiply':
-        x = int(input("Enter 1st num"))
-        y = int(input("Enter 2nd num"))
-        print(x * y)
-    elif options == 'divide':
-        x = int(input("Enter 1st num"))
-        y = int(input("Enter 2nd num"))
-        print(x / y)
-    elif options == 'power':
-        x = int(input("Enter Enter the base"))
-        y = int(input("Enter the exponent"))
-        print(x ** y)
-    else:
-        print("ma error")
-print("look i am out the loop")
+    
+    try:
+        if oper == 'add':
+            x = int(input("Enter 1st num"))
+            y = int(input("Enter 2nd num"))
+            print(x + y)
+        elif oper == 'substract':
+            x = int(input("Enter 1st num"))
+            y = int(input("Enter 2nd num"))
+            print(x - y)
+        elif oper == 'multiply':
+            x = int(input("Enter 1st num"))
+            y = int(input("Enter 2nd num"))
+            print(x * y)
+        elif oper == 'divide':
+            x = int(input("Enter 1st num"))
+            y = int(input("Enter 2nd num"))
+            print(x / y)
+        elif oper == 'power':
+            x = int(input("Enter Enter the base"))
+            y = int(input("Enter the exponent"))
+            print(x ** y)
+        else:
+            print("Unknown command!")
+    
+    except ValueError:
+        print("Invalid value!")
+
+print("Finish")
