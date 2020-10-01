d6dad75a1ad474cc2de93cff32245f5fdccf0d31
ipetrash <ilya.petrash@inbox.ru>
2020-10-01 19:16:06 +0500

    Refactoring

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -5,30 +5,30 @@ __author__ = 'ipetrash'
 
 
 while True:
-    oper = input("Enter 'add', 'substract', 'multiply', 'divide', 'quit', 'power'")
+    oper = input("Enter 'add', 'substract', 'multiply', 'divide', 'quit', 'power': ")
     if oper == 'quit':
         break
     
     try:
         if oper == 'add':
-            x = int(input("Enter 1st num"))
-            y = int(input("Enter 2nd num"))
+            x = int(input("Enter 1st num: "))
+            y = int(input("Enter 2nd num: "))
             print(x + y)
         elif oper == 'substract':
-            x = int(input("Enter 1st num"))
-            y = int(input("Enter 2nd num"))
+            x = int(input("Enter 1st num: "))
+            y = int(input("Enter 2nd num: "))
             print(x - y)
         elif oper == 'multiply':
-            x = int(input("Enter 1st num"))
-            y = int(input("Enter 2nd num"))
+            x = int(input("Enter 1st num: "))
+            y = int(input("Enter 2nd num: "))
             print(x * y)
         elif oper == 'divide':
-            x = int(input("Enter 1st num"))
-            y = int(input("Enter 2nd num"))
+            x = int(input("Enter 1st num: "))
+            y = int(input("Enter 2nd num: "))
             print(x / y)
         elif oper == 'power':
-            x = int(input("Enter Enter the base"))
-            y = int(input("Enter the exponent"))
+            x = int(input("Enter the base: "))
+            y = int(input("Enter the exponent: "))
             print(x ** y)
         else:
             print("Unknown command!")
