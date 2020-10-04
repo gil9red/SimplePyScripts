969fec972b4e289d23e9a78922163c7ce4d0f718
ipetrash <ilya.petrash@inbox.ru>
2020-10-04 17:06:43 +0500

    Added html_parsing/pravicon_com__s/collect_main_icon_images__v2.py

---
+++ html_parsing/pravicon_com__s/collect_main_icon_images__v2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import re
+import shutil
+
+from main import DIR_DUMP
+
+
+DIR_DUMP_IMAGES = DIR_DUMP.parent / 'DUMP_images__v2'
+DIR_DUMP_IMAGES.mkdir(parents=True, exist_ok=True)
+
+pattern = re.compile(r'icon-\d+__(.+)')
+
+
+for file_name in DIR_DUMP.glob('*/Главное изображение.jpg'):
+    m = pattern.search(file_name.parent.stem)
+    new_file_name = DIR_DUMP_IMAGES / f'{m.group(1)}.jpg'
+
+    shutil.copy(file_name, new_file_name)
