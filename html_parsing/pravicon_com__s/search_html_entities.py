37ef798961869bc0a6d2821c62a77fea88d355fc
ipetrash <ilya.petrash@inbox.ru>
2020-10-06 21:56:46 +0500

    Added html_parsing/pravicon_com__s/search_html_entities.py

---
+++ html_parsing/pravicon_com__s/search_html_entities.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from html import unescape
+from config import DIR_DUMP
+
+
+for file_name in DIR_DUMP.glob('*/Описания иконы.txt'):
+    text = file_name.read_text('utf-8')
+    new_text = unescape(text)
+    if text != new_text:
+        print(file_name)
