5ecf92aed126fe046eec58c736e9b28c2fe943b3
ipetrash <ilya.petrash@inbox.ru>
2020-10-14 20:35:18 +0500

    Refactoring

--- zip_file_example/extract_file_in_memory/main.py
+++ zip_file_example/extract_file_in_memory/main.py
@@ -4,19 +4,22 @@
 __author__ = 'ipetrash'
 
 
-def sizeof_fmt(num):
-    for x in ['bytes', 'KB', 'MB', 'GB']:
-        if num < 1024.0:
-            return "%3.1f %s" % (num, x)
-
-        num /= 1024.0
-
-    return "%3.1f %s" % (num, 'TB')
-
-
 import zipfile
+import sys
+
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
+from human_byte_size import sizeof_fmt
+
+
+FILE_NAME = Path('Doc_df7c89c378c04e8daf69257ea95d9a2e.zip')
+
+print('Zip size:', sizeof_fmt(len(FILE_NAME.read_bytes())))
+
+
 with zipfile.ZipFile('Doc_df7c89c378c04e8daf69257ea95d9a2e.zip') as f:
     data_file = f.read('Doc_df7c89c378c04e8daf69257ea95d9a2e.html')
     size = sizeof_fmt(len(data_file))
-    print('Total: {}'.format(size))
-    print('data_file[:100]: {}'.format(data_file[:100]))
+    print(f'File size: {size}')
+    print(f'data_file[:100]: {data_file[:100]}')
