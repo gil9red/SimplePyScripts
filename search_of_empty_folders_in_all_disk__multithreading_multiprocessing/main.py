c046c2dcd318cd68875b7815c0b5b733a62e606b
ipetrash <ilya.petrash@inbox.ru>
2020-10-14 22:45:45 +0500

    Refactoring

--- search_of_empty_folders_in_all_disk__multithreading_multiprocessing/main.py
+++ search_of_empty_folders_in_all_disk__multithreading_multiprocessing/main.py
@@ -4,62 +4,59 @@
 __author__ = 'ipetrash'
 
 
-def sizeof_fmt(num):
-    for x in ['bytes', 'KB', 'MB', 'GB']:
-        if num < 1024.0:
-            return "%3.1f %s" % (num, x)
+import time
+import os
+import shutil
+import sys
 
-        num /= 1024.0
+# If need use threads
+from multiprocessing.dummy import Pool
+# Or:
+# from multiprocessing import Pool
 
-    return "%3.1f %s" % (num, 'TB')
+from pathlib import Path
+
+import psutil
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+from human_byte_size import sizeof_fmt
 
 
 def search_empty_folders(disk):
     disk_letter = disk[0]
-    file_name = 'log of {}.txt'.format(disk_letter)
+    file_name = f'log of {disk_letter}.txt'
 
-    import time
-    t = time.clock()
+    t = time.perf_counter()
 
-    import shutil
     usage = shutil.disk_usage(disk)
-    print('  Start of {} ({} free of {})'.format(disk_letter, sizeof_fmt(usage.free), sizeof_fmt(usage.total)))
+    print(f'  Start of {disk_letter} ({sizeof_fmt(usage.free)} free of {sizeof_fmt(usage.total)})')
 
     with open(file_name, mode='w', encoding='utf-8') as f:
-        import os
         for root, dirs, files in os.walk(disk):
             # If dir is empty
             if not dirs and not files:
                 f.write(root + '\n')
 
-    print('  Finish "{}"! Elapsed time: {:.3f} secs'.format(disk_letter, time.clock() - t))
+    print(f'  Finish "{disk_letter}"! Elapsed time: {time.perf_counter() - t:.3f} secs')
 
 
 if __name__ == '__main__':
     print('Start')
 
-    import psutil
     disk_list = [disk.device for disk in psutil.disk_partitions() if 'fixed' in disk.opts]
-    print('  Found ({}): {}'.format(len(disk_list), disk_list))
+    print(f'  Found ({len(disk_list)}): {disk_list}')
     print()
 
-    import time
-    t = time.clock()
+    t = time.perf_counter()
 
     # Number of thread or process
     worker_number = len(disk_list)
 
-    # If need use threads
-    from multiprocessing.dummy import Pool
-    #
-    # Or:
-    # from multiprocessing import Pool
     pool = Pool(worker_number)
-
     results = pool.map(search_empty_folders, disk_list)
 
     # Close the pool and wait for the work to finish
     pool.close()
     pool.join()
 
-    print('Finish! Elapsed time: {:.3f} secs'.format(time.clock() - t))
+    print(f'Finish! Elapsed time: {time.perf_counter() - t:.3f} secs')
