f26bcbaddc16ede60c5930fadef2a4670085717e
ipetrash <ilya.petrash@inbox.ru>
2020-10-21 05:59:39 +0500

    Refactoring

--- github_io_git_repo_url.py
+++ github_io_git_repo_url.py
@@ -4,14 +4,18 @@
 __author__ = 'ipetrash'
 
 
-# Convertor githun pages url to github repo url
+import re
+from typing import Optional
+
+
+# Convertor github pages url to github repo url
 # http://nemilya.github.io/coffeescript-game-life/html/game.html
 # https://github.com/nemilya/coffeescript-game-life
 
-def github_io_git_repo_url(github_io_url):
-    import re
-    match = re.search('https?://(.+)\.github.io/(.+)', github_io_url)
-    if match is not None:
+
+def github_io_git_repo_url(github_io_url: str) -> Optional[str]:
+    match = re.search('https?://(.+)\.github\.io/(.+)', github_io_url)
+    if match:
         user = match.group(1)
         repo = match.group(2).split('/')[0]
 
