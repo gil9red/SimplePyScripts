b3e82efea4812c2a62bf521fe01e6315333005b6
ipetrash <ilya.petrash@inbox.ru>
2020-11-03 15:14:20 +0500

    Added selenium__examples/screenshot_from_local_file_with_fixed_window_size.py

---
+++ selenium__examples/screenshot_from_local_file_with_fixed_window_size.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import time
+from pathlib import Path
+
+# pip install selenium
+from selenium import webdriver
+
+
+file_name = 'file_test.html'
+
+driver = webdriver.Firefox()
+driver.set_window_size(500, 500)
+driver.get('file://' + str(Path(file_name).resolve()))
+
+time.sleep(5)
+
+driver.save_screenshot(file_name + '.png')
+
+driver.quit()
