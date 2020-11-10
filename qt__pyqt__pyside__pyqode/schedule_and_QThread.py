5391958f6debe4ee2192191d67316a72353f9d23
ipetrash <ilya.petrash@inbox.ru>
2020-11-10 18:35:01 +0500

    Added qt__pyqt__pyside__pyqode/schedule_and_QThread.py

---
+++ qt__pyqt__pyside__pyqode/schedule_and_QThread.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://ru.stackoverflow.com/a/1202691/201445
+
+
+import time
+import sys
+from datetime import datetime
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+import schedule
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(449, 208)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(30, 40, 191, 31))
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(250, 50, 171, 21))
+        self.lineEdit.setObjectName("lineEdit")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(340, 130, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 449, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Когда вывести сообщение:"))
+        self.pushButton.setText(_translate("MainWindow", "Запуститть"))
+
+
+class ScheduleThread(QThread):
+    about_time = pyqtSignal(str)
+
+    def add_time(self, time: str):
+        schedule.every().day.at(time).do(
+            lambda: self.about_time.emit(time)
+        )
+
+    def run(self):
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+
+
+class Example(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.go)
+
+        self.thread = ScheduleThread()
+        self.thread.about_time.connect(self.write)
+        self.thread.start()
+
+    def write(self, time: str):
+        print('красафчик', time)
+
+    def go(self):
+        try:
+            t = self.lineEdit.text()
+            datetime_object = datetime.strptime(t, '%H:%M')
+            time_str = str(datetime_object.time())
+
+            self.thread.add_time(time_str)
+
+        except Exception as e:
+            print(e)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec_())
