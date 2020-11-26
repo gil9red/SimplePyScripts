0c7fd8afceac620c3f0f84c631af5482687c3647
gil9red <ilya.petrash@inbox.ru>
2020-11-27 00:03:42 +0500

    Added tx_parse_xml/acl_update_name_props.py

---
+++ tx_parse_xml/acl_update_name_props.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from pathlib import Path
+from bs4 import BeautifulSoup
+
+
+FILE_NAME_ACL = Path(...)
+
+ITEMS = [
+    ...
+]
+
+root_acl = BeautifulSoup(open(FILE_NAME_ACL, 'rb'), 'xml')
+root_acl_str = str(root_acl)
+
+for license_name, license_id, prop_name, prop_id in ITEMS:
+    print(license_name, license_id, prop_name, prop_id)
+
+    prop_el = root_acl.select_one(f'[Id="{prop_id}"]')
+    old_prop_el_str = str(prop_el)
+
+    prop_el['Name'] = f"{prop_name}_{license_id}"
+    new_prop_el_str = str(prop_el)
+
+    root_acl_str = root_acl_str.replace(old_prop_el_str, new_prop_el_str)
+
+with open('new_' + FILE_NAME_ACL.name, 'w', encoding='utf-8') as f:
+    f.write(root_acl_str)
