4465095e57db376297f077c4f6dc079fa1423f0b
ipetrash <ilya.petrash@inbox.ru>
2020-12-04 03:17:15 +0500

    Added zip_file_example/shutil_unpack_archive

---
+++ zip_file_example/shutil_unpack_archive/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import shutil
+
+
+DIR_NAME = '../extract_directory/dir_1.zip'
+OUTPUT = 'dir_1'
+
+shutil.unpack_archive(DIR_NAME, OUTPUT)
