1605eb9e4c2cd9dd019a46b53031a1cadb36e4c9
ipetrash <ilya.petrash@inbox.ru>
2020-12-13 04:06:54 +0500

    Added html_parsing/gifer_com__ru__gifs__loading.py

---
+++ html_parsing/gifer_com__ru__gifs__loading.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from typing import List
+
+# pip install selenium
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+
+def get_urls(driver) -> List[str]:
+    urls = []
+
+    for link in driver.find_elements_by_css_selector('figure.media-thumb.desktop link[itemprop=contentUrl]'):
+        url_gif = link.get_attribute('content')
+        gif_id = url_gif.split('/')[-1]
+        url_download = URL_DOWNLOAD_TEMPLATE + gif_id
+        urls.append(url_download)
+
+    return urls
+
+
+URL_DOWNLOAD_TEMPLATE = 'https://i.gifer.com/embedded/download/'
+
+
+options = Options()
+options.add_argument('--headless')
+
+driver = webdriver.Firefox(options=options)
+driver.get('https://gifer.com/ru/gifs/loading')
+
+print(f'Title: "{driver.title}"')
+
+driver.implicitly_wait(20)
+
+urls = get_urls(driver)
+print(f'{len(urls)}: {urls}')
+# 4: ['https://i.gifer.com/embedded/download/g0R5.gif', 'https://i.gifer.com/embedded/download/VAyR.gif', 'https://i.gifer.com/embedded/download/ZKZx.gif', 'https://i.gifer.com/embedded/download/ZZ5H.gif']
+
+# Small scroll down
+driver.execute_script(f'window.scrollTo(0, 200);')
+
+urls = get_urls(driver)
+print(f'{len(urls)}: {urls}')
+# 8: ['https://i.gifer.com/embedded/download/g0R5.gif', 'https://i.gifer.com/embedded/download/VAyR.gif', 'https://i.gifer.com/embedded/download/ZKZx.gif', 'https://i.gifer.com/embedded/download/ZZ5H.gif', 'https://i.gifer.com/embedded/download/g0R9.gif', 'https://i.gifer.com/embedded/download/ZWdx.gif', 'https://i.gifer.com/embedded/download/7pld.gif', 'https://i.gifer.com/embedded/download/AqCa.gif']
+
+driver.quit()
