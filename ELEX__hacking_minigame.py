0c96572b423906773d518c2b0367761c8e14527e
ipetrash <ilya.petrash@inbox.ru>
2020-12-12 17:59:52 +0500

    Refactoring

--- ELEX__hacking_minigame.py
+++ ELEX__hacking_minigame.py
@@ -12,15 +12,17 @@ keys = [1, 2, 3, 4, 5, 6, 7, 8, 9] * 2
 keys = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 keys = [2, 3, 5, 7] * 2
 
-eval_template = '{a} == {b} and ({a} > {c} < {d})'
-eval_template = '{a} > {b} < {c} > {d}'
+eval_template = 'a == b and (a > c < d)'
+eval_template = 'a > b < c > d'
 
 while True:
     items = keys.copy()
     random.shuffle(items)
 
     a, b, c, d = items.pop(), items.pop(), items.pop(), items.pop()
-    text = eval_template.format(a=a, b=b, c=c, d=d)
+    text = eval_template\
+        .replace('a', str(a)).replace('b', str(b))\
+        .replace('c', str(c)).replace('d', str(d))
 
     ok = eval(text)
     if ok:
