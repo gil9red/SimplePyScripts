4bbfc36f147211ab40f9fb8fb52d5b213c2a3e47
ipetrash <ilya.petrash@inbox.ru>
2020-12-28 16:02:55 +0500

    Added "update_ConEmu_start_task/4. move ConEmu to second desktop.py"

---
+++ update_ConEmu_start_task/4. move ConEmu to second desktop.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install psutil
+import psutil
+
+# pip install pyvda
+import pyvda
+
+# pip install win32gui
+import win32gui
+
+# pip install pywin32
+import win32process
+
+
+NEED_WINDOW_DESKTOP_NUMBER = 2
+
+
+def get_hwnd_for_pid(pid: int):
+    def callback(hwnd: int, hwnds: list):
+        if win32gui.IsWindowVisible(hwnd) and win32gui.IsWindowEnabled(hwnd):
+            _, found_pid = win32process.GetWindowThreadProcessId(hwnd)
+            if found_pid == pid:
+                hwnds.append(hwnd)
+                return True
+        return True
+
+    hwnds = []
+    win32gui.EnumWindows(callback, hwnds)
+    return hwnds[0] if hwnds else -1
+
+
+for proc in psutil.process_iter():
+    if not proc.is_running() or proc.name() != 'ConEmu.exe':
+        continue
+
+    hwnd = get_hwnd_for_pid(proc.pid)
+
+    # If the window is already on the desired desktop
+    if pyvda.GetWindowDesktopNumber(hwnd) == NEED_WINDOW_DESKTOP_NUMBER:
+        continue
+
+    print(f'Moved window (pid={proc.pid}) to window desktop #{NEED_WINDOW_DESKTOP_NUMBER}')
+    pyvda.MoveWindowToDesktopNumber(hwnd, NEED_WINDOW_DESKTOP_NUMBER)
