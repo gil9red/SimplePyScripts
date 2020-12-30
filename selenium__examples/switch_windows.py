48f2b747bda410b9387f1be5175120fc07aec6f3
ipetrash <ilya.petrash@inbox.ru>
2020-12-31 00:07:14 +0500

    Refactoring

--- selenium__examples/switch_windows.py
+++ selenium__examples/switch_windows.py
@@ -19,14 +19,24 @@ try:
 
     # Opens a new tab
     driver.execute_script("window.open()")
+    driver.execute_script("window.open()")
 
     print('Tabs:', len(driver.window_handles), driver.window_handles)
 
     driver.switch_to.window(driver.window_handles[1])
+    driver.get('https://google.com')
+    print(f'Switch to title: "{driver.title}"')
+
+    time.sleep(2)
+
+    driver.switch_to.window(driver.window_handles[2])
+    driver.get('https://ya.ru')
+    print(f'Switch to title: "{driver.title}"')
 
     time.sleep(2)
 
     driver.switch_to.window(driver.window_handles[0])
+    print(f'Switch to title: "{driver.title}"')
 
     time.sleep(2)
 
