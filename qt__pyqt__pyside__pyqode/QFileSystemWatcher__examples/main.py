27e60868ba991037147b6cdc536decf59d1f30c4
ipetrash <ilya.petrash@inbox.ru>
2021-01-15 13:19:20 +0500

    Refactoring

--- qt__pyqt__pyside__pyqode/QFileSystemWatcher__examples/main.py
+++ qt__pyqt__pyside__pyqode/QFileSystemWatcher__examples/main.py
@@ -4,6 +4,7 @@
 __author__ = 'ipetrash'
 
 
+import datetime as DT
 from pathlib import Path
 
 from PyQt5.QtCore import QCoreApplication, QFileSystemWatcher
@@ -11,11 +12,18 @@ from PyQt5.QtCore import QCoreApplication, QFileSystemWatcher
 
 current_dir = str(Path(__file__).resolve().parent)
 
-
 app = QCoreApplication([])
 
 watcher = QFileSystemWatcher([current_dir])
-watcher.directoryChanged.connect(lambda directory: print('Directory:', directory))
-watcher.fileChanged.connect(lambda file: print('File:', file))
+watcher.directoryChanged.connect(
+    lambda directory: print(
+        f'[{DT.datetime.now():%d/%m/%Y %H:%M:%S}] Directory: {directory!r}'
+    )
+)
+watcher.fileChanged.connect(
+    lambda file: print(
+        f'[{DT.datetime.now():%d/%m/%Y %H:%M:%S}] File: {file!r}'
+    )
+)
 
 app.exec_()
