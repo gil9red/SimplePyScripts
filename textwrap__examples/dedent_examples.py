78ccc8f3adb212a67a9197497ba8c1a6bfa50efd
ipetrash <ilya.petrash@inbox.ru>
2021-01-30 22:10:00 +0500

    Added textwrap__examples/dedent_examples.py

---
+++ textwrap__examples/dedent_examples.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from textwrap import dedent
+
+
+text = """\
+    Съешь ещё этих мягких французских 
+    булок, да выпей же чаю
+"""
+print(repr(text))
+print(repr(dedent(text)))
+
+print()
+
+
+def foo():
+    """
+    Съешь ещё этих мягких французских
+    булок, да выпей же чаю
+    """
+
+
+print(repr(foo.__doc__))
+print(repr(dedent(foo.__doc__)))
