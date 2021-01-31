b757aa51b4daa7ea3193ee3b1c37e28d3922096b
ipetrash <ilya.petrash@inbox.ru>
2021-01-31 17:19:42 +0500

    Added html_parsing/anekdot_me__random.py

---
+++ html_parsing/anekdot_me__random.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from typing import Tuple
+
+import requests
+from bs4 import BeautifulSoup
+
+
+URL = 'https://anekdot.me/wiki/Служебная:RandomInCategory/Анекдоты'
+
+
+def get_random() -> Tuple[int, str]:
+    rs = requests.get(URL)
+    root = BeautifulSoup(rs.content, 'html.parser')
+
+    anekdot_id = int(rs.url.split('/')[-1])
+    text = root.select_one('.anekdot-centred-text').get_text(strip=True)
+
+    return anekdot_id, text
+
+
+if __name__ == '__main__':
+    anekdot_id, text = get_random()
+    print(f'#{anekdot_id}:')
+    print(text)
