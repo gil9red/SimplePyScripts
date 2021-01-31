01e3e33676d0829012671a5e66a6c214928aecb8
ipetrash <ilya.petrash@inbox.ru>
2021-01-31 18:53:44 +0500

    Added html_parsing/PVS_Studio__customers.py

---
+++ html_parsing/PVS_Studio__customers.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from typing import List
+
+import requests
+from bs4 import BeautifulSoup
+
+
+URL = 'https://www.viva64.com/ru/customers/'
+
+session = requests.session()
+session.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0'
+
+
+def get_customers() -> List[str]:
+    rs = session.get(URL)
+    root = BeautifulSoup(rs.content, 'html.parser')
+
+    return [
+        a['href']
+        for a in root.select('#tab-all a[href]')
+    ]
+
+
+if __name__ == '__main__':
+    items = get_customers()
+    print(len(items))
+    # 228
