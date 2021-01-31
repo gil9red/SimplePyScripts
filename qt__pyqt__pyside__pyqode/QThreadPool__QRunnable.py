3fffb243c993682f03396c59c8f740ebcc4e7200
ipetrash <ilya.petrash@inbox.ru>
2021-01-31 20:28:00 +0500

    Added qt__pyqt__pyside__pyqode/QThreadPool__QRunnable.py

---
+++ qt__pyqt__pyside__pyqode/QThreadPool__QRunnable.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+import threading
+import time
+import random
+
+from PyQt5.QtCore import QThreadPool, QRunnable
+
+lock = threading.Lock()
+
+
+class HelloWorldTask(QRunnable):
+    def __init__(self, idx: int):
+        super().__init__()
+
+        self.idx = idx
+
+    def run(self):
+        with lock:
+            print(f"[{self.idx}] Hello world from thread: {threading.current_thread()}")
+
+        ms = random.randint(1, 10) / 1000
+        time.sleep(ms)
+
+
+# QThreadPool takes ownership and deletes 'hello' automatically
+pool = QThreadPool.globalInstance()
+
+for i in range(1, 100 + 1):
+    hello = HelloWorldTask(idx=i)
+    pool.start(hello)
+
+pool.waitForDone()
