2c3c69b64162b6b0f18a5076edaaf5ac6f1e981c
ipetrash <ilya.petrash@inbox.ru>
2021-02-03 14:06:41 +0500

    Added plural_days.py

---
+++ plural_days.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+def plural_days(n):
+    days = ['день', 'дня', 'дней']
+
+    if n % 10 == 1 and n % 100 != 11:
+        p = 0
+    elif 2 <= n % 10 <= 4 and (n % 100 < 10 or n % 100 >= 20):
+        p = 1
+    else:
+        p = 2
+
+    return f'{n} {days[p]}'
+
+
+if __name__ == '__main__':
+    for i in range(100 + 1):
+        print(plural_days(i))
