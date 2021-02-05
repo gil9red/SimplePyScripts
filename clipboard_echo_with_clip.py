b83465b55d03f377f1afffb979e742e4aa834f96
ipetrash <ilya.petrash@inbox.ru>
2021-02-05 12:54:53 +0500

    Added clipboard_echo_with_clip.py

---
+++ clipboard_echo_with_clip.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import os
+from tkinter import Tk  # Python 3
+
+os.system('echo 123|clip')
+
+print(repr(Tk().clipboard_get()))
+# '123\n'
