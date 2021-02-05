c3c0172bff03477d6fb93f58e3a9ee31659458a3
ipetrash <ilya.petrash@inbox.ru>
2021-02-05 13:33:31 +0500

    Added prevent_multiple_instances__mutex.py

---
+++ prevent_multiple_instances__mutex.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import sys
+import time
+
+import win32api
+import win32event
+import winerror
+
+
+# Prevent multiple instances
+mutex = win32event.CreateMutex(None, 1, "MY_MUTEX_PYTHON")
+if win32api.GetLastError() == winerror.ERROR_ALREADY_EXISTS:
+    print('Already running!')
+    mutex = None
+    sys.exit(0)
+
+while True:
+    time.sleep(99999)
