1ef7f236f1fa0a1be442bd5ca2036b0007484f6c
ipetrash <ilya.petrash@inbox.ru>
2021-02-05 12:33:33 +0500

    Added pymem__examples/hello_world.py

---
+++ pymem__examples/hello_world.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install pymem
+from pymem import Pymem
+
+
+pm = Pymem('notepad.exe')
+print('Process id: %s' % pm.process_id)
+address = pm.allocate(10)
+print('Allocated address: %s' % address)
+pm.write_int(address, 1337)
+value = pm.read_int(address)
+print('Allocated value: %s' % value)
+pm.free(address)
