49cdd2547d1e147b826c97a6828946087fb64a90
ipetrash <ilya.petrash@inbox.ru>
2021-02-09 02:45:37 +0500

    Added html_parsing/get_customers_of_bink.py

---
+++ html_parsing/get_customers_of_bink.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'http://www.radgametools.com/binkgames.htm'
+rs = requests.get(url)
+root = BeautifulSoup(rs.content, 'html.parser')
+
+items = [x.get_text(strip=True) for x in root.select('.gameslist > dl > dt')]
+print(f'Items ({len(items)}): {items}')
+# Items (403): ['1C', '2015 Inc', '2XL Games', ..., 'Zombie', 'Zoë Mode', 'Zoo Digital Publishing']
