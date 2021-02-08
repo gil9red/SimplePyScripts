605581b0d70ec5b59dfecfa43e6830b5a2b36532
ipetrash <ilya.petrash@inbox.ru>
2021-02-09 00:11:43 +0500

    Added socket__tcp__examples/redirect_stdout

---
+++ socket__tcp__examples/redirect_stdout/client.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import contextlib
+import sys
+import socket
+
+sys.path.append('..')
+
+from common import send_msg, recv_msg
+
+
+HOST, PORT = "localhost", 9090
+
+
+class SocketIO:
+    def __init__(self, socket: socket.socket):
+        self.socket = socket
+
+    def write(self, text: str):
+        data = text.encode('utf-8')
+        print(f'Sending ({len(data)}): {data!r}', file=sys.stderr)
+
+        send_msg(self.socket, data)
+
+        response_data = recv_msg(self.socket)
+        if response_data:
+            print(f'Response ({len(response_data)}): {response_data!r}', file=sys.stderr)
+
+
+sock = socket.socket()
+sock.connect((HOST, PORT))
+
+f = SocketIO(sock)
+with contextlib.redirect_stdout(f):
+    print('123')
+    print('Hello World!')
+    print('Привет мир!')
+
+    for i in range(1, 5 + 1):
+        print(f'{i} * {i} = {i * i}')
---
+++ socket__tcp__examples/redirect_stdout/server.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import socket
+
+import sys
+sys.path.append('..')
+
+from common import send_msg, recv_msg
+
+
+PORT = 9090
+
+
+with socket.socket() as sock:
+    sock.bind(('', PORT))
+    sock.listen()
+
+    print(f'Server: {sock.getsockname()}')
+
+    while True:
+        conn, addr = sock.accept()
+        print('Connected:', addr)
+
+        while True:
+            data = recv_msg(conn)
+            if not data:
+                break
+
+            print(f'Receiving ({len(data)}): {data}')
+
+            print('Sending')
+            data = data.decode('utf-8').upper().encode('utf-8')
+            send_msg(conn, data)
+
+        print('Close\n')
