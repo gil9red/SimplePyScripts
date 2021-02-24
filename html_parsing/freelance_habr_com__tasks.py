bd875d9b055b4db140debcacfbc6e58195bd850f
ipetrash <ilya.petrash@inbox.ru>
2021-02-24 09:27:33 +0500

    Refactoring

--- html_parsing/freelance_habr_com__tasks.py
+++ html_parsing/freelance_habr_com__tasks.py
@@ -13,8 +13,8 @@ from bs4 import BeautifulSoup
 rs = requests.get('https://freelance.habr.com/tasks?q=python')
 root = BeautifulSoup(rs.content, 'html.parser')
 urls = [
-    urljoin(rs.url, a['href'])
+    (urljoin(rs.url, a['href']), a.get_text(strip=True))
     for a in root.select('.task__title > a[href]')
 ]
 print(len(urls), urls)
-# 25 ['https://freelance.habr.com/tasks/349681', ..., 'https://freelance.habr.com/tasks/348667']
+# 25 [('https://freelance.habr.com/tasks/349695', 'Парсер '), ..., 'Доработка бота Telegram на Python')]
