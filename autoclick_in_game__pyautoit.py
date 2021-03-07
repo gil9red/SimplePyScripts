5011ff691c52ea6b27e10483984b35d8fd510d6a
ipetrash <ilya.petrash@inbox.ru>
2021-03-07 13:35:12 +0500

    Added autoclick_in_game__pyautoit.py

---
+++ autoclick_in_game__pyautoit.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import threading
+import time
+
+import keyboard
+
+# pip install PyAutoIt
+import autoit
+
+
+DATA = {
+    'START': False,
+}
+
+
+def change_start():
+    DATA['START'] = not DATA['START']
+    print(DATA['START'])
+
+
+def process_auto_click():
+    while True:
+        if not DATA['START']:
+            time.sleep(0.01)
+            continue
+
+        # Симуляция атаки
+        if DATA['START']:
+            autoit.mouse_click()
+
+        time.sleep(0.01)
+
+
+keyboard.add_hotkey('Ctrl+Alt+1', change_start)
+
+# Запуск потока для автоатаки
+thread_auto_click = threading.Thread(target=process_auto_click)
+thread_auto_click.start()
+
+
+while True:
+    if not DATA['START']:
+        time.sleep(0.01)
+        continue
