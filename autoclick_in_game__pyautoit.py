f14ab668a6e1f5989a2170811a78175deaa853fe
ipetrash <ilya.petrash@inbox.ru>
2021-03-09 05:16:30 +0500

    Changed timeouts

--- autoclick_in_game__pyautoit.py
+++ autoclick_in_game__pyautoit.py
@@ -26,17 +26,17 @@ def change_start():
 def process_auto_click():
     while True:
         if not DATA['START']:
-            time.sleep(0.01)
+            time.sleep(0.100)
             continue
 
         # Симуляция атаки
         if DATA['START']:
             autoit.mouse_click()
 
-        time.sleep(0.01)
+        time.sleep(0.070)
 
 
-keyboard.add_hotkey('Ctrl+Alt+1', change_start)
+keyboard.add_hotkey('Ctrl+Alt+Space', change_start)
 
 # Запуск потока для автоатаки
 thread_auto_click = threading.Thread(target=process_auto_click)
@@ -45,5 +45,5 @@ thread_auto_click.start()
 
 while True:
     if not DATA['START']:
-        time.sleep(0.01)
+        time.sleep(0.100)
         continue
