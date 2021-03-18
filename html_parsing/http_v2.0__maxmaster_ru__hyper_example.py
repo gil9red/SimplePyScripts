44cf2a500f5ee399b45b24b0344e7f8cc5b84834
ipetrash <ilya.petrash@inbox.ru>
2021-03-18 21:04:42 +0500

    Added html_parsing/http_v2.0__maxmaster_ru__hyper_example.py

---
+++ html_parsing/http_v2.0__maxmaster_ru__hyper_example.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from bs4 import BeautifulSoup
+from hyper import HTTPConnection
+
+
+conn = HTTPConnection('maxmaster.ru:443')
+conn.request('GET', '/')
+resp = conn.get_response()
+print(resp.status)
+# 200
+
+root = BeautifulSoup(resp.read(), 'html.parser')
+print(root.select_one('head > title').text)
+# MAXMASTER - интернет-магазин электроинструмента, ...
