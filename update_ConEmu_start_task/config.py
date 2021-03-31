2a9cc02259e555c782cd4882e90e2ba51c1747ff
ipetrash <ilya.petrash@inbox.ru>
2021-03-31 17:00:51 +0500

    Refactoring

--- update_ConEmu_start_task/config.py
+++ update_ConEmu_start_task/config.py
@@ -19,7 +19,7 @@ DIR_LNKS.mkdir(parents=True, exist_ok=True)
 
 FILE_NAME_CONEMU_SETTINGS = DIR / 'conemu_settings.xml'
 
-PATTERN_NAME_TASK = 'My startup python scripts {}'
-RE_PATTERN_CONEMU_TASK = re.compile(r'^{My startup python scripts \d+}$', flags=re.IGNORECASE)
+PATTERN_NAME_TASK = 'My python {}'
+RE_PATTERN_CONEMU_TASK = re.compile(r'^{My python \d+}$', flags=re.IGNORECASE)
 
 RE_PATTERN_FILE_TASK = re.compile(r'^group(\d+)$')
