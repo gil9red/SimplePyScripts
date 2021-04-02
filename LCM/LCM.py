d0ebabb6c9112df9b8017454bf76d22b3288d538
ipetrash <ilya.petrash@inbox.ru>
2021-04-02 13:49:58 +0500

    Added math.gcd

--- LCM/LCM.py
+++ LCM/LCM.py
@@ -18,3 +18,6 @@ def gcd(a, b):
 if __name__ == '__main__':
     a, b = 7006652, 112307574
     print("LCM: %s" % ((a * b) / gcd(a, b)))
+
+    import math
+    print("LCM: %s" % ((a * b) / math.gcd(a, b)))
