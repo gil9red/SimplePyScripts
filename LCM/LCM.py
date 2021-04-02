78d4f817f1054bbc863e170eee9b826101b66733
ipetrash <ilya.petrash@inbox.ru>
2021-04-02 13:30:10 +0500

    Refactoring

--- LCM/LCM.py
+++ LCM/LCM.py
@@ -1,18 +1,20 @@
-# coding=utf-8
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 __author__ = 'ipetrash'
+
 
 # EN: Least Common Multiple.
 # RU: Наименьшее общее кратное (НОК).
 
+
 def gcd(a, b):
-   "Нахождение НОД"
-   while a != 0:
-      a,b = b%a,a # параллельное определение
-   return b
+    """Нахождение НОД"""
+    while a != 0:
+        a, b = b % a, a  # Параллельное определение
+    return b
 
 
 if __name__ == '__main__':
-    a = int(raw_input("a = "))
-    b = int(raw_input("b = "))
+    a, b = 7006652, 112307574
     print("LCM: %s" % ((a * b) / gcd(a, b)))
