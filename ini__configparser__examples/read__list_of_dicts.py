02daba78ff3546da1f0a607c7016376f2f029385
gil9red <ilya.petrash@inbox.ru>
2021-04-15 14:00:23 +0500

    Added ini__configparser__examples/read__list_of_dicts.py

---
+++ ini__configparser__examples/read__list_of_dicts.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://ru.stackoverflow.com/q/1270426/201445
+
+
+import configparser
+
+
+config = configparser.ConfigParser()
+config.read('example.ini')
+
+print(dict(config['DEFAULT']))
+
+for section in config.sections():
+    print(dict(config[section]))
