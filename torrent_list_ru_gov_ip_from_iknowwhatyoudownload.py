5c0b30e0b9b7439b53278b18db105a45b8cd63d8
gil9red <ilya.petrash@inbox.ru>
2021-04-27 13:51:17 +0500

    Refactoring. quit() -> sys.exit()

--- torrent_list_ru_gov_ip_from_iknowwhatyoudownload.py
+++ torrent_list_ru_gov_ip_from_iknowwhatyoudownload.py
@@ -12,21 +12,25 @@ __author__ = 'ipetrash'
 """
 
 
+import ipaddress
+import time
+import sys
+
+import requests
+from bs4 import BeautifulSoup
+
+
 def get_torrents_by_ip(ip, append_torrent_size=False):
     url = 'http://iknowwhatyoudownload.com/ru/peer/?ip=' + ip
 
-    import requests
     rs = requests.get(url, headers={'User-Agent': '-'})
-
-    from bs4 import BeautifulSoup
     root = BeautifulSoup(rs.content, 'lxml')
 
     # Если нужно вместе с названием передавать и размер торрента
     if not append_torrent_size:
         return [item.text.strip() for item in root.select('.torrent_files > a')]
 
-    items = list()
-
+    items = []
     for row in root.select('table > tbody > tr'):
         name = row.select_one('.name-column').text.strip()
         size = row.select_one('.size-column').text.strip()
@@ -37,26 +41,24 @@ def get_torrents_by_ip(ip, append_torrent_size=False):
 
 
 if __name__ == '__main__':
-    import requests
     rs = requests.get('https://jarib.github.io/anon-history/RuGovEdits/ru/latest/ranges.json')
 
     # Проверка удачного запроса и полученных данных
     if not rs or not rs.json() or 'ranges' not in rs.json():
         print('Не получилось получить список ip государственных организаций')
-        quit()
+        sys.exit()
 
     # Получение и сортировка элементов по названию организации
     items = sorted(rs.json()['ranges'].items(), key=lambda x: x[0])
 
     for i, (name, ip_network_list) in enumerate(items, 1):
-        print('{}. {}'.format(i, name))
+        print(f'{i}. {name}')
 
-        # Получени ip с маской подсети
+        # Получение ip с маской подсети
         for ip_network in ip_network_list:
-            print('    {}:'.format(ip_network))
+            print(f'    {ip_network}:')
 
             # Получение ip подсети
-            import ipaddress
             net4 = ipaddress.ip_network(ip_network)
 
             # Перебор ip адресов указанной организации
@@ -65,7 +67,6 @@ if __name__ == '__main__':
 
                 torrents = get_torrents_by_ip(ip, append_torrent_size=True)
                 if torrents:
-                    print('        {}. Найдено {}: {}'.format(ip, len(torrents), torrents))
+                    print(f'        {ip}. Найдено {len(torrents)}: {torrents}')
 
-                import time
                 time.sleep(0.3)
