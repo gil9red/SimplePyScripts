04a98d6ce105c5418fd52d43993ed862c4370513
gil9red <ilya.petrash@inbox.ru>
2021-04-27 13:46:19 +0500

    Refactoring. quit() -> sys.exit()

--- get_ip_list_ru_gov.py
+++ get_ip_list_ru_gov.py
@@ -10,35 +10,38 @@ __author__ = 'ipetrash'
 """
 
 
-if __name__ == '__main__':
-    import requests
-    rs = requests.get('https://jarib.github.io/anon-history/RuGovEdits/ru/latest/ranges.json')
+import ipaddress
+import sys
 
-    # Проверка удачного запроса и полученных данных
-    if not rs or not rs.json() or 'ranges' not in rs.json():
-        print('Не получилось получить список ip государственных организаций')
-        quit()
+import requests
 
-    # Получение и сортировка элементов по названию организации
-    items = sorted(rs.json()['ranges'].items(), key=lambda x: x[0])
 
-    ip_counter = 0
+rs = requests.get('https://jarib.github.io/anon-history/RuGovEdits/ru/latest/ranges.json')
 
-    for i, (name, ip_network_list) in enumerate(items, 1):
-        print('{}. {}'.format(i, name))
+# Проверка удачного запроса и полученных данных
+if not rs or not rs.json() or 'ranges' not in rs.json():
+    print('Не получилось получить список ip государственных организаций')
+    sys.exit()
 
-        # Получени ip с маской подсети
-        for ip_network in ip_network_list:
-            print('    {}:'.format(ip_network))
+# Получение и сортировка элементов по названию организации
+items = sorted(rs.json()['ranges'].items(), key=lambda x: x[0])
 
-            # Получение ip подсети
-            import ipaddress
-            net4 = ipaddress.ip_network(ip_network)
+ip_counter = 0
 
-            # Перебор ip адресов указанной организации
-            for ip in net4.hosts():
-                print('        {}'.format(ip))
-                ip_counter += 1
+for i, (name, ip_network_list) in enumerate(items, 1):
+    print(f'{i}. {name}')
 
-    print()
-    print('Всего ip:', ip_counter)
+    # Получение ip с маской подсети
+    for ip_network in ip_network_list:
+        print(f'    {ip_network}:')
+
+        # Получение ip подсети
+        net4 = ipaddress.ip_network(ip_network)
+
+        # Перебор ip адресов указанной организации
+        for ip in net4.hosts():
+            print(f'        {ip}')
+            ip_counter += 1
+
+print()
+print('Всего ip:', ip_counter)
