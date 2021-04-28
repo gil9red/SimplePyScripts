853d0963ee0b96ab1a1fbc1c7105f0983ac12ec5
gil9red <ilya.petrash@inbox.ru>
2021-04-28 14:54:30 +0500

    Fixed quit() -> sys.exit()

--- exchange_rates/cbr_ru_parse.py
+++ exchange_rates/cbr_ru_parse.py
@@ -7,26 +7,30 @@ __author__ = 'ipetrash'
 """Парсер курса доллара и евро за текущую дату от сайта центробанка России."""
 
 
-if __name__ == '__main__':
-    from datetime import date
-    date_req = date.today().strftime('%d.%m.%Y')
-    url = 'https://www.cbr.ru/currency_base/daily.aspx?date_req=' + date_req
+import sys
+from datetime import date
 
-    # pip install robobrowser
-    from robobrowser import RoboBrowser
-    browser = RoboBrowser(
-        user_agent='Mozilla/5.0 (Windows NT 6.1; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0',
-        parser='lxml'
-    )
-    browser.open(url)
-    if not browser.response.ok:
-        print(browser.response.status_code, browser.response.reason)
-        quit()
+# pip install robobrowser
+from robobrowser import RoboBrowser
 
-    for tr in browser.select('.data tr'):
-        td_list = tr.select('td')
-        if not td_list:
-            continue
 
-        if td_list[1].text in ['USD', 'EUR']:
-            print(td_list[1].text, td_list[4].text)
+date_req = date.today().strftime('%d.%m.%Y')
+url = 'http://www.cbr.ru/scripts/XML_daily.asp?date_req=' + date_req
+
+browser = RoboBrowser(
+    user_agent='Mozilla/5.0 (Windows NT 6.1; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0',
+    parser='html.parser'
+)
+browser.open(url)
+rs = browser.response
+
+if not rs.ok:
+    print(rs.status_code, rs.reason)
+    sys.exit()
+
+for valute_el in browser.select('Valute'):
+    char_code = valute_el.select_one('CharCode').get_text(strip=True)
+    value = valute_el.select_one('Value').get_text(strip=True)
+
+    if char_code in ['USD', 'EUR']:
+        print(char_code, value)
