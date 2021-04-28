e6acfd5b6c922f7c44e375389eb49864f2f6b285
gil9red <ilya.petrash@inbox.ru>
2021-04-28 17:48:03 +0500

    Refactoring. quit() -> sys.exit()

--- Moodle_examples/search_current_user.py
+++ Moodle_examples/search_current_user.py
@@ -4,39 +4,41 @@
 __author__ = 'ipetrash'
 
 
+import sys
+import requests
+from get_token import get_token
+
+
 USERNAME = '<USERNAME>'
 PASSWORD = '<PASSWORD>'
 
 
-if __name__ == '__main__':
-    from get_token import get_token
-    token = get_token(USERNAME, PASSWORD)
+token = get_token(USERNAME, PASSWORD)
 
-    params = {
-        'wstoken': token,
-        'wsfunction': 'core_user_get_users_by_field',
-        'moodlewsrestformat': 'json',
-        'field': 'username',
-        'values[0]': USERNAME,
-    }
+params = {
+    'wstoken': token,
+    'wsfunction': 'core_user_get_users_by_field',
+    'moodlewsrestformat': 'json',
+    'field': 'username',
+    'values[0]': USERNAME,
+}
 
-    import requests
-    rs = requests.get('http://newlms.magtu.ru/webservice/rest/server.php', params=params)
-    print(rs)
+rs = requests.get('http://newlms.magtu.ru/webservice/rest/server.php', params=params)
+print(rs)
 
-    json_data = rs.json()
-    print('Response: {}'.format(json_data))
+json_data = rs.json()
+print('Response: {}'.format(json_data))
 
-    if 'errorcode' in json_data:
-        print(json_data['errorcode'])
-        quit()
+if 'errorcode' in json_data:
+    print(json_data['errorcode'])
+    sys.exit()
 
-    if not json_data:
-        print('Not found user "{}"'.format(USERNAME))
-        quit()
+if not json_data:
+    print('Not found user "{}"'.format(USERNAME))
+    sys.exit()
 
-    info = json_data[0]
-    print(info['username'])
-    print(info['fullname'])
-    print(info['email'])
-    print(info['idnumber'])
+info = json_data[0]
+print(info['username'])
+print(info['fullname'])
+print(info['email'])
+print(info['idnumber'])
