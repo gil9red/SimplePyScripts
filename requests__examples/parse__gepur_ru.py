2a628b09d896fa17425aeb3afa5a84b4dd00428d
gil9red <ilya.petrash@inbox.ru>
2021-04-29 18:40:25 +0500

    Refactoring. quit() -> sys.exit()

--- requests__examples/parse__gepur_ru.py
+++ requests__examples/parse__gepur_ru.py
@@ -4,30 +4,32 @@
 __author__ = 'ipetrash'
 
 
+import json
+import sys
+import re
+
+from urllib.parse import urljoin
+
 import requests
+
+
 session = requests.session()
 rs = session.get('http://gepur.ru/product/plate-6917')
 html = rs.text
 
 # Ищем следующую строку c описанием модели
-re_expr = r'ProductPage\.init\((.+)\)'
-
-import re
-match = re.search(re_expr, html)
+match = re.search(r'ProductPage\.init\((.+)\)', html)
 if not match:
     print('Не получилось вытащить описание модели')
-    quit()
+    sys.exit()
 
 # Вытаскиваем объект js из параметра функции init
 json_text = match.group(1)
 
 # Парсим его как JSON
-import json
 json_data = json.loads(json_text)
 
 # Вытаскиваем список ссылок на картинки
 for url_img_rel in json_data['getImages']['originImg']:
-    from urllib.parse import urljoin
     url_img = urljoin('http://gepur.ru', url_img_rel)
-
     print(url_img)
