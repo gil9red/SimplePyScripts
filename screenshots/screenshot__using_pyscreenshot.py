d64f419fc370e06c0d636b56535f31e0e58ebfb9
gil9red <ilya.petrash@inbox.ru>
2021-04-30 11:38:09 +0500

    Refactoring

--- screenshots/screenshot__using_pyscreenshot.py
+++ screenshots/screenshot__using_pyscreenshot.py
@@ -5,7 +5,24 @@ __author__ = 'ipetrash'
 
 
 # Fullscreen
+
+
+import datetime as DT
+
+# pip install pyscreenshot
 import pyscreenshot as ImageGrab
-im = ImageGrab.grab()
-im.save('screenshot.png')
-im.show()
+
+
+PATTERN = 'pyscreenshot__screenshot'
+
+
+img = ImageGrab.grab()
+img.save(f'{PATTERN}.png')
+img.save(f'{PATTERN}.jpg')
+# img.show()
+
+# Filename with datetime
+file_name = f'{PATTERN}_{DT.datetime.now():%Y-%m-%d_%H%M%S}.jpg'
+print(file_name)
+
+img.save(file_name)
