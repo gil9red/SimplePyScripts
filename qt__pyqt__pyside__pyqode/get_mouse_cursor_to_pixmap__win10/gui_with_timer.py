ff57915b99f617b279cd4d6ce719b81cabc6d09d
ipetrash <ilya.petrash@inbox.ru>
2021-05-04 04:12:38 +0500

    Added qt__pyqt__pyside__pyqode/get_mouse_cursor_to_pixmap__win10/gui_with_timer.py

---
+++ qt__pyqt__pyside__pyqode/get_mouse_cursor_to_pixmap__win10/gui_with_timer.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt5.QtCore import QTimer
+from PyQt5.QtWinExtras import QtWin
+
+import win32gui
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.label = QLabel()
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.label)
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self._on_tick)
+        self.timer.setInterval(100)
+        self.timer.start()
+
+    def _on_tick(self):
+        # flags, hcursor, (x, y) =
+        _, hcursor, _ = win32gui.GetCursorInfo()
+
+        # fIcon, xHotspot, yHotspot, hbmMask, hbmColor =
+        _, _, _, _, hbmColor = win32gui.GetIconInfo(hcursor)
+
+        pixmap = QtWin.fromHBITMAP(hbmColor.handle, QtWin.HBitmapPremultipliedAlpha)
+        self.label.setPixmap(pixmap)
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    mw = MainWindow()
+    mw.show()
+
+    app.exec()
