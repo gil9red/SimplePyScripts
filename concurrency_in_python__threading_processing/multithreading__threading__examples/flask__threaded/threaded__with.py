e1b2ed795db81799e4de582ad3417308b62ef624
gil9red <ilya.petrash@inbox.ru>
2021-05-17 14:08:13 +0500

    Removed the extra threaded=True parameter for flask

--- concurrency_in_python__threading_processing/multithreading__threading__examples/flask__threaded/threaded__with.py
+++ concurrency_in_python__threading_processing/multithreading__threading__examples/flask__threaded/threaded__with.py
@@ -4,20 +4,17 @@
 __author__ = 'ipetrash'
 
 
+import threading
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    import threading
     return "Current thread: {}".format(threading.current_thread())
 
 
 if __name__ == '__main__':
-    app.run(
-        port=6000,
-
-        # Включение поддержки множества подключений
-        threaded=True,
-    )
+    app.run(port=6000)
