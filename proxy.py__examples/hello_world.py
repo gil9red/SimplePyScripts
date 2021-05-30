a1d477f92d29b88ef9efd33cd495967ab82b455b
ipetrash <ilya.petrash@inbox.ru>
2021-05-30 15:10:50 +0500

    Added proxy.py__examples/

---
+++ proxy.py__examples/hello_world.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import ipaddress
+
+from typing import Optional
+
+import proxy
+from proxy.common.utils import bytes_
+from proxy.http.parser import HttpParser
+from proxy.plugin import FilterByUpstreamHostPlugin
+from proxy.http.proxy import HttpProxyBasePlugin
+
+
+# NOTE: Analog https://github.com/gil9red/SimplePyScripts/blob/f0f5e96a2ddf768ec8e02fc41365226a334ee354/http.server__examples/simple_http_proxy_server/main.py#L18
+class AddHeadersPlugin(HttpProxyBasePlugin):
+    def before_upstream_connection(self, request: HttpParser) -> Optional[HttpParser]:
+        return request
+
+    def handle_client_request(self, request: HttpParser) -> Optional[HttpParser]:
+        # NOTE: Add custom header
+        request.add_header(b'x-my-proxy', b'hell yeah!')
+        request.add_header(b'x-client-ip', bytes_(self.client.addr[0]))
+
+        return request
+
+    def handle_upstream_chunk(self, chunk: memoryview) -> memoryview:
+        return chunk
+
+    def on_upstream_connection_close(self) -> None:
+        pass
+
+
+if __name__ == '__main__':
+    proxy.main(
+        hostname=ipaddress.ip_address('127.0.0.1'),
+        port=33333,
+        plugins=[
+            'proxy.plugin.CacheResponsesPlugin',  # Adding plugin v1
+            FilterByUpstreamHostPlugin,           # Adding plugin v2
+            AddHeadersPlugin,                     # Adding custom plugin
+        ]
+    )
---
+++ proxy.py__examples/test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import requests
+
+
+proxies = {
+    "http": 'localhost:33333',
+    "https": 'localhost:33333',
+}
+
+# url = 'https://httpbin.org/headers'
+url = 'http://httpbin.org/headers'
+rs = requests.get(url, proxies=proxies)
+print(rs)
+print(rs.url)
+print(rs.headers)
+print(rs.content)
+for header, value in rs.json()['headers'].items():
+    if header.lower().startswith('x-'):
+        print(f'{header}: {value!r}')
+
+"""
+...
+X-Client-Ip: '127.0.0.1'
+X-My-Proxy: 'hell yeah!'
+"""
