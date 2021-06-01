743dd4641c2815ecd12e3ad5ce733a8bcb6f3843
ipetrash <ilya.petrash@inbox.ru>
2021-06-02 01:48:15 +0500

    Added "Check with notification/Check new season Богиня благословляет этот прекрасный мир"

---
+++ Check with notification/Check new season Богиня благословляет этот прекрасный мир/main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+"""
+Скрипт для уведомления о появлении новых сезонов аниме Богиня благословляет этот прекрасный мир.
+
+"""
+
+
+import re
+import sys
+
+from typing import List
+
+import requests
+
+# Чтобы можно было импортировать all_common.py, находящийся уровнем выше
+sys.path.append('..')
+from all_common import make_backslashreplace_console, run_notification_job
+
+
+def get_items() -> List[str]:
+    rs = requests.get('https://en.wikipedia.org/wiki/List_of_KonoSuba_episodes')
+    items = re.findall(r'Season \d+', rs.text)
+    return sorted(set(items))
+
+
+run_notification_job(
+    'Новый сезон "Богиня благословляет этот прекрасный мир"',
+    'seasons',
+    get_items,
+    notified_by_sms=True,
+    format_current_items='Текущий список сезонов (%s): %s',
+    format_get_items='Запрос сезонов',
+    format_items='Список сезонов (%s): %s',
+    format_new_item='Новый сезон "%s"',
+    format_no_new_items='Изменений нет',
+)
