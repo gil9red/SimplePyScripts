033aa91dd9c955d8e93f64a2d192d3ec39a99bce
ipetrash <ilya.petrash@inbox.ru>
2021-06-02 02:15:39 +0500

    Refactoring

--- telegram_notifications/config.py
+++ telegram_notifications/config.py
@@ -5,12 +5,16 @@ __author__ = 'ipetrash'
 
 
 import os
+import sys
 
 from pathlib import Path
 from typing import Optional
 
 
 DIR = Path(__file__).resolve().parent
+
+# NOTE: хак для помощи импортирования из telegram_notifications
+sys.path.append(str(DIR.parent))
 
 TOKEN_PATH = DIR / 'TOKEN.txt'
 TOKEN = os.environ.get('TOKEN') or TOKEN_PATH.read_text('utf-8').strip()
