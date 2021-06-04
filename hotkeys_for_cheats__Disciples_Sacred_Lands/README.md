36cc4e3e5f42bc2a94dc1dc58deaea8f17600b2f
ipetrash <ilya.petrash@inbox.ru>
2021-06-04 18:45:30 +0500

    Added hotkeys_for_cheats__Disciples_Sacred_Lands

---
+++ hotkeys_for_cheats__Disciples_Sacred_Lands/README.md
@@ -0,0 +1,15 @@
+### Environment
+
+* Win10, game in windowed mode (DXWnd)
+
+### HOTKEYS
+
+|HOTKEY|CHEAT|DESCRIPTION|
+--- | --- | --- |
+|**U**|UPGRADEME|Get exp points|
+|**G**|GIVEMEMONEY|Gold becomes 5000|
+|**I**|IWANTTOBUILDAGAIN|Build again|
+|**N**|NOWICANSEEYOU|Reveal map|
+|**M**|MAKEMESTRONGER|Recover health|
+|**L**|LETMEMOVE|Able to move again|
+|**R**|GIVEMEANOTHERCHANCE|Revives dead units|
---
+++ hotkeys_for_cheats__Disciples_Sacred_Lands/main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install keyboard
+import keyboard
+
+
+def input_command(text: str):
+    keyboard.press_and_release('enter')
+
+    keyboard.write(text)
+
+    keyboard.press_and_release('enter')
+    keyboard.press_and_release('esc')
+
+
+HOTKEY_BY_COMMAND = {
+    'U': 'UPGRADEME',
+    'G': 'GIVEMEMONEY',
+    'I': 'IWANTTOBUILDAGAIN',
+    'N': 'NOWICANSEEYOU',
+    'M': 'MAKEMESTRONGER',
+    'L': 'LETMEMOVE',
+    'R': 'GIVEMEANOTHERCHANCE',
+}
+
+for hotkey, text in HOTKEY_BY_COMMAND.items():
+    print(f'{hotkey}: {text}')
+    keyboard.add_hotkey(hotkey, lambda text=text: input_command(text))
+
+keyboard.wait()
