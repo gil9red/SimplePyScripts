e6233752c50e1e1e341bac69d76497cb82d83876
ipetrash <ilya.petrash@inbox.ru>
2021-06-06 00:11:29 +0500

    Added SNMP__pysnmp__examples/hello_world__fix_imports.py

---
+++ SNMP__pysnmp__examples/hello_world__fix_imports.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: http://www.oidview.com/mibs/0/SNMPv2-MIB.html
+
+
+# pip install pysnmp
+from pysnmp.hlapi import getCmd, SnmpEngine, CommunityData, UdpTransportTarget, ContextData, ObjectType, ObjectIdentity
+
+
+iterator = getCmd(
+    SnmpEngine(),
+    CommunityData('public', mpModel=0),
+    UdpTransportTarget(('localhost', 161)),
+    ContextData(),
+    ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0)),
+    ObjectType(ObjectIdentity('1.3.6.1.2.1.1.1.0')),   # OID, same as above
+    ObjectType(ObjectIdentity('.1.3.6.1.2.1.1.1.0')),  # OID, same as above
+)
+
+errorIndication, errorStatus, errorIndex, varBinds = next(iterator)
+
+if errorIndication:
+    print(errorIndication)
+
+elif errorStatus:
+    print('%s at %s' % (errorStatus.prettyPrint(),
+                        errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+
+else:
+    for varBind in varBinds:
+        var, value = varBind
+        print(varBind)
+        print(f'{var} = {value}')
+        print(f'{var.prettyPrint()} = {value.prettyPrint()}')
+        print()
