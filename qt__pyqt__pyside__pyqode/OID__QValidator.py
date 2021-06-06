31f1307af6c49675a201f3db7f7ca83d41167a16
ipetrash <ilya.petrash@inbox.ru>
2021-06-06 05:05:29 +0500

    Added qt__pyqt__pyside__pyqode/OID__QValidator.py

---
+++ qt__pyqt__pyside__pyqode/OID__QValidator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.QtWidgets import QApplication, QLineEdit
+from PyQt5.QtGui import QRegExpValidator
+from PyQt5.QtCore import QRegExp
+
+
+app = QApplication([])
+
+mw = QLineEdit()
+mw.setText('1.3.6.1.2.1.25.1.1.0')
+mw.setValidator(QRegExpValidator(QRegExp(r'\d+(\.\d+)+')))
+mw.show()
+
+app.exec()
