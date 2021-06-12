ae5543922db83b10670096ac2e765608ab792880
ipetrash <ilya.petrash@inbox.ru>
2021-06-13 01:15:47 +0500

    Refactoring

--- html_parsing/vitaly_zykov_ru_knigi__get_books.py
+++ html_parsing/vitaly_zykov_ru_knigi__get_books.py
@@ -4,19 +4,19 @@
 __author__ = 'ipetrash'
 
 
-import typing
+from typing import List
+
+import requests
+from bs4 import BeautifulSoup
 
 
-def get_books() -> typing.List[str]:
-    import requests
+def get_books() -> List[str]:
     rs = requests.get('http://vitaly-zykov.ru/knigi')
-
-    from bs4 import BeautifulSoup
     root = BeautifulSoup(rs.content, 'html.parser')
 
-    return [x.text.strip().replace('"', '') for x in root.select('.book_tpl > h3')]
+    return [x.get_text(strip=True).replace('"', '') for x in root.select('.book_tpl > h3')]
 
 
 if __name__ == '__main__':
     books = get_books()
-    print('Items ({}): {}'.format(len(books), books))
+    print(f'Items ({len(books)}): {books}')
