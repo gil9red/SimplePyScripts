fe2e2d56b2eac5a23f359898b59c6a17e937aac5
gil9red <ilya.petrash@inbox.ru>
2021-06-16 10:57:11 +0500

    Added html_parsing/get_seasons_anime_Dorohedoro.py

---
+++ html_parsing/get_seasons_anime_Dorohedoro.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import re
+from typing import List
+
+import requests
+
+
+def get_seasons() -> List[str]:
+    rs = requests.get('https://en.wikipedia.org/wiki/List_of_Dorohedoro_episodes')
+    rs.raise_for_status()
+
+    items = re.findall(r'Season \w+', rs.text, flags=re.IGNORECASE)
+    return sorted(set(items))
+
+
+if __name__ == '__main__':
+    print(get_seasons())
+    # ['Season One']
