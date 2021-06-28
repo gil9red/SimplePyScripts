d09e6c23e06ca51e1722c4c8f88f6fdaf521efe4
gil9red <ilya.petrash@inbox.ru>
2021-06-28 11:31:37 +0500

    Updated. Tab 'Current Assigned Open Issues': removed button 'REFRESH', added progress-bar

--- parse_jira_Assigned_Open_Issues_per_Project/gui.py
+++ parse_jira_Assigned_Open_Issues_per_Project/gui.py
@@ -7,17 +7,20 @@ __author__ = 'ipetrash'
 import calendar
 import datetime as DT
 import math
-from pathlib import Path
-from typing import List
+import time
+import traceback
 import sys
+
+from pathlib import Path
+from typing import List, Dict
 
 from PyQt5.QtWidgets import (
     QApplication, QMessageBox, QMainWindow, QSystemTrayIcon, QTabWidget, QTableWidget,
     QTableWidgetItem, QVBoxLayout, QWidget, QPushButton, QSplitter, QLabel, QGridLayout,
-    QHeaderView
+    QHeaderView, QProgressBar
 )
 from PyQt5.QtGui import QIcon, QPainter
-from PyQt5.QtCore import QEvent, QTimer, Qt
+from PyQt5.QtCore import QEvent, QTimer, Qt, QThread, pyqtSignal
 from PyQt5.QtChart import QChart, QLineSeries, QDateTimeAxis, QValueAxis
 
 from common import get_table
@@ -30,7 +33,6 @@ from chart_line__show_tooltip_on_series__QtChart import ChartViewToolTips
 
 def log_uncaught_exceptions(ex_cls, ex, tb):
     text = '{}: {}:\n'.format(ex_cls.__name__, ex)
-    import traceback
     text += ''.join(traceback.format_tb(tb))
 
     print(text)
@@ -55,6 +57,22 @@ def get_table_widget(header_labels: list) -> QTableWidget:
     table.setHorizontalHeaderLabels(header_labels)
     table.horizontalHeader().setStretchLastSection(True)
     return table
+
+
+class GetAssignedOpenIssuesPerProjectThread(QThread):
+    about_items = pyqtSignal(dict)
+    about_error = pyqtSignal(Exception)
+
+    def run(self):
+        try:
+            items = get_assigned_open_issues_per_project()
+            self.about_items.emit(items)
+
+            # Даем время на отображение и анимацию прогресс-бара
+            time.sleep(0.3)
+
+        except Exception as e:
+            self.about_error.emit(e)
 
 
 class TableWidgetRun(QWidget):
@@ -119,8 +137,13 @@ class CurrentAssignedOpenIssues(QWidget):
         self.table = get_table_widget(['PROJECT', 'NUMBER'])
         self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
-        self.pb_refresh = QPushButton('REFRESH')
-        self.pb_refresh.clicked.connect(self.refresh)
+        self.progress_bar = QProgressBar()
+        self.progress_bar.setRange(0, 0)
+        self.progress_bar.setTextVisible(False)
+        sp_progress_bar = self.progress_bar.sizePolicy()
+        sp_progress_bar.setRetainSizeWhenHidden(True)
+        self.progress_bar.setSizePolicy(sp_progress_bar)
+        self.progress_bar.hide()
 
         self.label_total = QLabel()
         font = self.label_total.font()
@@ -128,24 +151,30 @@ class CurrentAssignedOpenIssues(QWidget):
         self.label_total.setFont(font)
 
         self.label_last_refresh_date = QLabel()
+        font = self.label_last_refresh_date.font()
+        font.setPixelSize(18)
+        self.label_last_refresh_date.setFont(font)
 
         main_layout = QGridLayout()
-
-        main_layout.addWidget(self.label_total, 0, 0)
-        main_layout.addWidget(self.pb_refresh, 0, 1)
-        main_layout.addWidget(self.label_last_refresh_date, 0, 2)
+        main_layout.addWidget(self.label_total, 0, 0, Qt.AlignLeft | Qt.AlignCenter)
+        main_layout.addWidget(self.progress_bar, 0, 1)
+        main_layout.addWidget(self.label_last_refresh_date, 0, 2, Qt.AlignRight | Qt.AlignCenter)
         main_layout.addWidget(self.table, 1, 0, 2, 0)
 
         self.setLayout(main_layout)
+
+        self.thread = GetAssignedOpenIssuesPerProjectThread()
+        self.thread.started.connect(self.progress_bar.show)
+        self.thread.finished.connect(self.progress_bar.hide)
+        self.thread.about_items.connect(self._on_set_items)
+        self.thread.about_error.connect(self._on_error)
 
         self._update_total_issues('-')
 
     def _update_total_issues(self, value):
         self.label_total.setText(f'<b>Total issues:</b> {value}')
 
-    def refresh(self):
-        items = get_assigned_open_issues_per_project()
-
+    def _on_set_items(self, items: Dict[str, int]):
         self._update_total_issues(sum(items.values()))
 
         # Удаление строк таблицы
@@ -161,6 +190,17 @@ class CurrentAssignedOpenIssues(QWidget):
         self.label_last_refresh_date.setText(
             "Last refresh date: " + DT.datetime.now().strftime('%d/%m/%Y %H:%M:%S')
         )
+
+    def _on_error(self, e: Exception):
+        tb_str = "".join(traceback.format_tb(e.__traceback__))
+        print(tb_str)
+        QMessageBox.warning(self, 'ERROR', str(e))
+
+    def refresh(self):
+        if self.thread.isRunning():
+            return
+
+        self.thread.start()
 
 
 class MyChartViewToolTips(ChartViewToolTips):
@@ -227,10 +267,12 @@ class MainWindow(QMainWindow):
         self.table_run = TableWidgetRun()
         self.table_run.layout().setContentsMargins(0, 0, 0, 0)
 
+        self.current_assigned_open_issues = CurrentAssignedOpenIssues()
+
         self.tab_widget = QTabWidget()
         self.tab_widget.addTab(self.chart_view, 'CHART')
         self.tab_widget.addTab(self.table_run, 'TABLE RUN')
-        self.tab_widget.addTab(CurrentAssignedOpenIssues(), 'Current Assigned Open Issues')
+        self.tab_widget.addTab(self.current_assigned_open_issues, 'Current Assigned Open Issues')
 
         self.pb_refresh = QPushButton('REFRESH')
         self.pb_refresh.clicked.connect(self.refresh)
@@ -244,6 +286,16 @@ class MainWindow(QMainWindow):
 
         self.setCentralWidget(central_widget)
 
+    @staticmethod
+    def _get_timegm(date: DT.date) -> int:
+        return calendar.timegm(date.timetuple()) * 1000
+
+    def _get_datetime(self, date: DT.date, delta: DT.timedelta = None) -> DT.datetime:
+        dt = DT.datetime.combine(date, DT.datetime.min.time())
+        if delta:
+            dt += delta
+        return dt
+
     def _fill_chart(self, items: List[Run]):
         series = QLineSeries()
         series.setPointsVisible(True)
@@ -254,7 +306,7 @@ class MainWindow(QMainWindow):
         self.timestamp_by_run.clear()
 
         for run in items:
-            date_value = calendar.timegm(run.date.timetuple()) * 1000
+            date_value = self._get_timegm(run.date)
             total_issues = run.get_total_issues()
             series.append(date_value, total_issues)
 
@@ -271,6 +323,11 @@ class MainWindow(QMainWindow):
         chart.setBackgroundRoundness(0)
 
         axisX = QDateTimeAxis()
+        # TODO: Не уверен, что это нужно...
+        # axisX.setRange(
+        #     self._get_datetime(items[0].date, DT.timedelta(weeks=-1)),
+        #     self._get_datetime(items[-1].date, DT.timedelta(weeks=1))
+        # )
         axisX.setFormat("dd/MM/yyyy")
         axisX.setTitleText('Date')
         chart.addAxis(axisX, Qt.AlignBottom)
@@ -286,6 +343,8 @@ class MainWindow(QMainWindow):
         self.chart_view.setChart(chart)
 
     def refresh(self):
+        self.current_assigned_open_issues.refresh()
+
         items = list(Run.select())
         self._fill_chart(items)
 
