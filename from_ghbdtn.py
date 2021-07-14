9f11bc2611b3978e821801da5f52fdf3af26563d
gil9red <ilya.petrash@inbox.ru>
2021-07-14 12:11:32 +0500

    Refactoring

--- from_ghbdtn.py
+++ from_ghbdtn.py
@@ -4,7 +4,7 @@
 __author__ = 'ipetrash'
 
 
-def from_ghbdtn(text):
+def from_ghbdtn(text: str) -> str:
     # SOURCE: https://ru.stackoverflow.com/a/812203/201445
     layout = dict(zip(map(ord, '''qwertyuiop[]asdfghjkl;'zxcvbnm,./`QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>?~'''),
                                '''йцукенгшщзхъфывапролджэячсмитьбю.ёЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,Ё'''))
@@ -16,3 +16,4 @@ if __name__ == '__main__':
     text = 'B ,skb ghj,ktvs c ujcntdjq dhjlt ,s? gjcvjnhb '
     print(text)
     print(from_ghbdtn(text))
+    assert from_ghbdtn(text) == 'И были проблемы с гостевой вроде бы, посмотри '
