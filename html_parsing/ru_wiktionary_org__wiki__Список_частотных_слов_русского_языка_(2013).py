4f1ea541b73becd0aabe51c041eb26a4218d4959
ipetrash <ilya.petrash@inbox.ru>
2021-07-18 16:02:32 +0500

    Refactoring

--- html_parsing/ru_wiktionary_org__wiki__Список_частотных_слов_русского_языка_(2013).py
+++ html_parsing/ru_wiktionary_org__wiki__Список_частотных_слов_русского_языка_(2013).py
@@ -4,17 +4,24 @@
 __author__ = 'ipetrash'
 
 
+from typing import List
+
 import requests
 from bs4 import BeautifulSoup
 
 
-url = 'https://ru.wiktionary.org/wiki/Приложение:Список_частотных_слов_русского_языка_(2013)'
+def get_words() -> List[str]:
+    url = 'https://ru.wiktionary.org/wiki/Приложение:Список_частотных_слов_русского_языка_(2013)'
 
-rs = requests.get(url)
-root = BeautifulSoup(rs.content, 'html.parser')
+    rs = requests.get(url)
+    root = BeautifulSoup(rs.content, 'html.parser')
 
-words = [
-    x.text
-    for x in root.select_one('.wikitable').select('tbody > tr > td:nth-child(2)')
-]
-print(len(words), words)
+    return [
+        x.text
+        for x in root.select_one('.wikitable').select('tbody > tr > td:nth-child(2)')
+    ]
+
+
+if __name__ == '__main__':
+    words = get_words()
+    print(len(words), words)
