4c0a365eda89e411b181e24d5fffd6f4a450f1ac
ipetrash <ilya.petrash@inbox.ru>
2021-07-19 15:44:25 +0500

    Refactoring

--- word_to_emoji/filling_via_emojipedia.py
+++ word_to_emoji/filling_via_emojipedia.py
@@ -10,33 +10,50 @@ import traceback
 # pip install translate
 from translate import Translator
 
+# pip install goslate
+import goslate
+
 import db
 from html_parsing.emojipedia_org__search import get_emoji
 
 
 translator = Translator(from_lang="ru", to_lang='en')
+gs = goslate.Goslate()
+
+
+def ru2en(text: str) -> str:
+    text_en = translator.translate(text)
+
+    # Если translate перестал работать попробуем через другой перевести
+    if 'MYMEMORY WARNING: YOU USED ALL AVAILABLE FREE TRANSLATIONS FOR TODAY' in text_en:
+        text_en = gs.translate(text, 'en')
+
+    return text_en
 
 
 while True:
-    for word_ru in db.Word2Emoji.get_unprocessed_words():
+    for word_ru in reversed(db.Word2Emoji.get_unprocessed_words()):
         # Если эмодзи уже есть
         if db.Word2Emoji.get_emoji(word_ru):
             continue
 
-        try:
-            # На emojipedia поиск нужен на английском
-            word = translator.translate(word_ru)
-            emoji = get_emoji(word)
-            print(f'Add {word_ru!r} ({word!r}) -> {emoji!r}')
-            if not emoji:
-                continue
+        while True:
+            try:
+                # На emojipedia поиск нужен на английском
+                word = ru2en(word_ru)
+                emoji = get_emoji(word)
+                print(f'Add {word_ru!r} ({word!r}) -> {emoji!r}')
+                if not emoji:
+                    continue
 
-            db.Word2Emoji.add(word_ru, emoji)
+                db.Word2Emoji.add(word_ru, emoji)
+                break
 
-        except:
-            print(traceback.format_exc())
+            except:
+                print(traceback.format_exc())
+                time.sleep(5 * 60)
 
-        finally:
-            time.sleep(5)
+            finally:
+                time.sleep(5)
 
     time.sleep(5)
