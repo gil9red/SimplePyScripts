291848808b88503e89e395a7549b45dd46acb50b
ipetrash <ilya.petrash@inbox.ru>
2021-07-19 23:58:57 +0500

    Update. Improving replacement

--- word_to_emoji/text_to_emoji.py
+++ word_to_emoji/text_to_emoji.py
@@ -29,7 +29,7 @@ def text_to_emoji(text: str) -> str:
         word = token.word
         emoji = db.Word2Emoji.get_emoji(word)
         if emoji:
-            text = re.sub(re.escape(word), emoji, text)
+            text = re.sub(rf'\b{re.escape(word)}\b', emoji, text)
 
     return text
 
@@ -38,3 +38,9 @@ if __name__ == '__main__':
     text = text_to_emoji('Смотри в оба глаза')
     print(text)
     print(repr(text))
+
+    print()
+
+    text = 'xxx: у мальчиков есть воображаемые друзья, а у девочек - воображаемый жыр )))'
+    text = text_to_emoji(text)
+    print(text)
