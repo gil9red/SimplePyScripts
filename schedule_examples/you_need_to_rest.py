c7fdadaa0885dd3bad5e4439362a507bd4be0645
gil9red <ilya.petrash@inbox.ru>
2021-07-28 17:13:58 +0500

    Refactoring. Using schedule

--- schedule_examples/you_need_to_rest.py
+++ schedule_examples/you_need_to_rest.py
@@ -4,6 +4,11 @@
 __author__ = 'ipetrash'
 
 
+import time
+
+# pip install schedule
+import schedule
+
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
@@ -11,18 +16,16 @@ from PyQt5.QtCore import *
 
 # 60 * 1000 * 10 -- 10 minutes
 def show_message(text, timeout=60 * 1000 * 10):
-    print('show_message: "{}"'.format(text))
+    print(f'show_message: {text!r}')
 
     msg = QMessageBox()
     msg.setWindowFlags(msg.windowFlags() | Qt.WindowStaysOnTopHint)
     msg.setWindowTitle("Информация")
-    msg.setText("<p align='center'>{}<.p>".format(text))
+    msg.setText(f"<p align='center'>{text}<.p>")
     msg.setIcon(QMessageBox.Information)
     msg.setStandardButtons(QMessageBox.Ok)
 
-    # font = msg.font()
-    # or:
-    font = QFont()
+    font = msg.font()
     font.setFamily('Times')
     font.setPointSize(50)
     msg.setFont(font)
@@ -35,13 +38,11 @@ def show_message(text, timeout=60 * 1000 * 10):
 if __name__ == '__main__':
     app = QApplication([])
 
-    # pip install schedule
-    import schedule
-    schedule.every().day.at("11:00").do(lambda: show_message("Пора в столовку"))
-    schedule.every().day.at("13:00").do(lambda: show_message("Иди прогуляйся"))
-    schedule.every().day.at("15:00").do(lambda: show_message("Иди прогуляйся"))
-    schedule.every().day.at("17:00").do(lambda: show_message("Иди прогуляйся"))
-    schedule.every().day.at("19:00").do(lambda: show_message("Вали домой"))
+    schedule.every().day.at("11:00").do(show_message, "Пора в столовку")
+    schedule.every().day.at("13:00").do(show_message, "Иди прогуляйся")
+    schedule.every().day.at("15:00").do(show_message, "Иди прогуляйся")
+    schedule.every().day.at("17:00").do(show_message, "Иди прогуляйся")
+    schedule.every().day.at("19:00").do(show_message, "Вали домой")
 
     print('Jobs:')
     for job in schedule.jobs:
@@ -51,6 +52,4 @@ if __name__ == '__main__':
 
     while True:
         schedule.run_pending()
-
-        import time
         time.sleep(1)
