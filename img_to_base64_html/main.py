dec39b0ab1da8fc9662786bb37d03d06785dd822
gil9red <ilya.petrash@inbox.ru>
2021-07-29 11:40:08 +0500

    Refactoring

--- img_to_base64_html/main.py
+++ img_to_base64_html/main.py
@@ -4,6 +4,13 @@
 __author__ = 'ipetrash'
 
 
+import base64
+import io
+
+# pip install pillow
+from PIL import Image
+
+
 def img_to_base64_html(file_name__or__bytes__or__file_object):
     arg = file_name__or__bytes__or__file_object
 
@@ -17,23 +24,17 @@ def img_to_base64_html(file_name__or__bytes__or__file_object):
     else:
         img_bytes = arg.read()
 
-    import io
     bytes_io = io.BytesIO(img_bytes)
-
-    from PIL import Image
     img = Image.open(bytes_io)
 
-    import base64
     img_base64 = base64.b64encode(img_bytes).decode('utf-8')
-    # print(img_base64)
-
-    return 'data:image/{};base64,'.format(img.format.lower()) + img_base64
+    return f'data:image/{img.format.lower()};base64,{img_base64}'
 
 
 if __name__ == '__main__':
     file_name = 'img.jpg'
     img_base64 = img_to_base64_html(file_name)
-    print('[len {}]: {}...'.format(len(img_base64), img_base64[:50]))
+    print(f'[len {len(img_base64)}]: {img_base64[:50]}...')
 
     with open(file_name + '_base64.txt', mode='w', encoding='utf-8') as f:
         f.write(img_base64)
