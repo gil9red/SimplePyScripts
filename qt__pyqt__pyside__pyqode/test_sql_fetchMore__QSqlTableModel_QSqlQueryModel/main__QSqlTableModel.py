a62e84d7eaaa5b989cc788b1833cb616c78e986d
gil9red <ilya.petrash@inbox.ru>
2021-07-30 14:40:38 +0500

    Added qt__pyqt__pyside__pyqode/test_sql_fetchMore__QSqlTableModel_QSqlQueryModel

---
+++ qt__pyqt__pyside__pyqode/test_sql_fetchMore__QSqlTableModel_QSqlQueryModel/main__QSqlTableModel.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.QtWidgets import QApplication, QTableView
+from PyQt5.QtSql import QSqlDatabase, QSqlTableModel, QSqlQuery
+
+
+db = QSqlDatabase.addDatabase('QSQLITE')
+db.setDatabaseName('database.sqlite')
+if not db.open():
+    raise Exception(db.lastError().text())
+
+TABLE = 'word2emoji'
+query = QSqlQuery()
+query.exec(f'SELECT COUNT(*) FROM {TABLE}')
+query.next()
+TABLE_ROW_COUNT = query.value(0)
+
+
+def update_window_title():
+    mw.setWindowTitle(f'{model.rowCount()} / {TABLE_ROW_COUNT}')
+
+
+app = QApplication([])
+
+model = QSqlTableModel()
+model.rowsInserted.connect(update_window_title)
+model.setTable(TABLE)
+model.select()
+
+mw = QTableView()
+mw.setEditTriggers(QTableView.NoEditTriggers)
+mw.setModel(model)
+mw.resize(600, 480)
+mw.show()
+
+update_window_title()
+
+app.exec()
