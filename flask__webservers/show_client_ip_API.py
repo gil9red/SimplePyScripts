9b2fa8055d0dab6cf16ac72f2d7c7a6b6b42899d
gil9red <ilya.petrash@inbox.ru>
2021-08-02 14:15:58 +0500

    Added flask__webservers/show_client_ip_API.py

---
+++ flask__webservers/show_client_ip_API.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import logging
+import xml.etree.ElementTree as ET
+
+from flask import Flask, request, Response
+
+
+app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+
+@app.route('/')
+def index():
+    return request.remote_addr
+
+
+@app.route('/json')
+def get_json():
+    return {
+        'ip': request.remote_addr,
+    }
+
+
+@app.route('/xml')
+def get_xml():
+    root = ET.Element('ip')
+    root.text = request.remote_addr
+    xml_bytes = ET.tostring(root, encoding="utf-8", xml_declaration=True)
+    return Response(xml_bytes, mimetype='text/xml')
+
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=5000
+    )
