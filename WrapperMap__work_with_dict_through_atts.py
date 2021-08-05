f9f2107141f519075f739803ac4bdb11bb147ec5
gil9red <ilya.petrash@inbox.ru>
2021-08-05 11:04:13 +0500

    Added WrapperMap__work_with_dict_through_atts.py

---
+++ WrapperMap__work_with_dict_through_atts.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+class WrapperMap:
+    def __init__(self, d: dict):
+        self.d = d
+
+    def get_value(self):
+        return self.d
+
+    def __getattr__(self, item: str):
+        value = self.d.get(item)
+        if isinstance(value, dict):
+            return self.__class__(value)
+
+        return value
+
+    def __repr__(self):
+        return repr(self.d)
+
+
+genMessage = {
+    'from_user': {
+        'id': 123,
+        'username': "username",
+        'full_name': "fullName"
+    }
+}
+x = WrapperMap(genMessage)
+
+print(x.from_user, type(x.from_user))
+# {'id': 123, 'username': 'username', 'full_name': 'fullName'} <class '__main__.WrapperMap'>
+
+print(x.from_user.id, type(x.from_user.id))
+# 123 <class 'int'>
+
+print(x.from_user.username, type(x.from_user.username))
+# username <class 'str'>
