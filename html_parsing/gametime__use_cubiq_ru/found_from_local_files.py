e6eed9d4b1b45137127286fd841b48530730e81c
ipetrash <ilya.petrash@inbox.ru>
2021-09-04 11:16:44 +0500

    Update. Added caching

--- html_parsing/gametime__use_cubiq_ru/found_from_local_files.py
+++ html_parsing/gametime__use_cubiq_ru/found_from_local_files.py
@@ -4,6 +4,7 @@
 __author__ = 'ipetrash'
 
 
+import json
 import os
 import time
 from pathlib import Path
@@ -11,23 +12,50 @@ from pathlib import Path
 from main import find
 
 
-DIR = Path(os.path.expanduser(r'~\Desktop\Пройти'))
-game_names = [p.stem for p in DIR.glob('*.lnk')]
+DIR = Path(__file__).resolve().parent
+DIR_CACHE = DIR / 'cache.json'
 
-items = []
+try:
+    cache = json.load(open(DIR_CACHE, encoding='utf-8'))
+except:
+    cache = dict()
+
+DIR_GAMES = Path(os.path.expanduser(r'~\Desktop\Пройти'))
+game_names = [p.stem for p in DIR_GAMES.glob('*.lnk')]
+
+changed = False
 for game in game_names:
+    if game in cache:
+        continue
+
     data = find(game)
     if data:
-        items.append((game, data['Основной сюжет']))
+        time_obj = data['Основной сюжет']
+        cache[game] = {
+            'text': time_obj.text,
+            'seconds': time_obj.seconds,
+        }
+        changed = True
+    else:
+        cache[game] = None
 
     time.sleep(1)
 
+if changed:
+    json.dump(cache, open(DIR_CACHE, 'w', encoding='utf-8'), indent=4, ensure_ascii=False)
+
+items = [
+    (game, time_obj['text'], time_obj['seconds'])
+    for game, time_obj in cache.items()
+    if time_obj
+]
+
 # Сортировка по возрастанию времени прохождения
-items.sort(key=lambda x: x[1].seconds)
+items.sort(key=lambda x: x[2])
 
 print('Первые 10 игр с минимум времени прохождения:')
-for i, (game, time) in enumerate(items[:10], 1):
-    print(f'{i:2}. {game!r}: {time.text}')
+for i, (game, time_text, _) in enumerate(items[:10], 1):
+    print(f'{i:2}. {game!r}: {time_text}')
 """
 Первые 10 игр с минимум времени прохождения:
  1. 'FOTONICA': 2 ч. 11 мин.
