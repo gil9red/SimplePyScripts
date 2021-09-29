bf2d83ef9d6c1ebd80043be9686c3bc9388f90fd
gil9red <ilya.petrash@inbox.ru>
2021-09-29 14:52:28 +0500

    Refactoring

--- win_create_shortcut_lnk.py
+++ win_create_shortcut_lnk.py
@@ -4,10 +4,11 @@
 __author__ = 'ipetrash'
 
 
+# pip install pywin32
 from win32com.client import Dispatch
 
 
-def create_shortcut(file_name: str, target: str, work_dir: str, arguments: str):
+def create_shortcut(file_name: str, target: str, work_dir: str, arguments: str = ''):
     shell = Dispatch('WScript.Shell')
     shortcut = shell.CreateShortCut(file_name)
     shortcut.TargetPath = target
@@ -17,10 +18,23 @@ def create_shortcut(file_name: str, target: str, work_dir: str, arguments: str):
 
 
 if __name__ == '__main__':
+    from pathlib import Path
+
+    abs_file_name = r'C:\Program Files (x86)\ConEmu\ConEmu.exe'
+    path = Path(abs_file_name)
+
     name = 'My startup python scripts 1'
+
     create_shortcut(
         file_name=f"ConEmu start task '{name}'.lnk",
-        target=r'C:\Program Files (x86)\ConEmu\ConEmu.exe',
-        work_dir=r"C:\Program Files (x86)\ConEmu",
+        target=str(path),
+        work_dir=str(path.parent),
         arguments='/cmd {%s} -new_console' % name,
     )
+    # NOTE: same
+    # create_shortcut(
+    #     file_name=f"ConEmu start task '{name}'.lnk",
+    #     target=r'C:\Program Files (x86)\ConEmu\ConEmu.exe',
+    #     work_dir=r"C:\Program Files (x86)\ConEmu",
+    #     arguments='/cmd {%s} -new_console' % name,
+    # )
