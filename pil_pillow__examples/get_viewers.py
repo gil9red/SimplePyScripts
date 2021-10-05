ef59657518b328cf2ce459a41dbee0a52a0b0766
gil9red <ilya.petrash@inbox.ru>
2021-10-05 11:05:34 +0500

    Added pil_pillow__examples/get_viewers.py

---
+++ pil_pillow__examples/get_viewers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PIL.ImageShow import _viewers
+
+
+print(f'Viewers: {len(_viewers)}')
+for viewer in _viewers:
+    print(viewer.get_command('123.png'))
+
+"""
+Viewers: 1
+start "Pillow" /WAIT "123.png" && ping -n 2 127.0.0.1 >NUL && del /f "123.png"
+"""
