dd43a5ba251ead34159475f7a8581857d8144444
ipetrash <ilya.petrash@inbox.ru>
2021-11-04 01:27:32 +0500

    Added telegram_bot__telethon__examples/restriction_on_frequent_sending_of_messages__using_dict.py

---
+++ telegram_bot__telethon__examples/restriction_on_frequent_sending_of_messages__using_dict.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import datetime as DT
+
+# SOURCE: pip install pyTelegramBotAPI
+import telebot
+
+from config import TOKEN
+
+
+bot = telebot.TeleBot(TOKEN)
+
+
+CHAT_BY_DATETIME = dict()
+
+
+@bot.message_handler(commands=['help', 'start'])
+def on_start(message: telebot.types.Message):
+    bot.send_message(message.chat.id, 'Write something')
+
+
+@bot.message_handler(func=lambda message: True)
+def on_request(message: telebot.types.Message):
+    text = 'Получено!'
+    need_seconds = 50
+    current_time = DT.datetime.now()
+    last_datetime = CHAT_BY_DATETIME.get(message.chat.id)
+
+    # Если первое сообщение (время не задано)
+    if not last_datetime:
+        CHAT_BY_DATETIME[message.chat.id] = current_time
+    else:
+        # Разница в секундах между текущим временем и временем последнего сообщения
+        delta_seconds = (current_time - last_datetime).total_seconds()
+
+        # Осталось ждать секунд перед отправкой
+        seconds_left = int(need_seconds - delta_seconds)
+
+        # Если время ожидания не закончилось
+        if seconds_left > 0:
+            text = f'Подождите {seconds_left} секунд перед выполнение этой команды'
+        else:
+            CHAT_BY_DATETIME[message.chat.id] = current_time
+
+    bot.reply_to(message, text)
+
+
+bot.infinity_polling()
