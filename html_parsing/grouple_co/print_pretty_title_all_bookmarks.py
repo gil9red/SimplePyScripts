e760c19f2557ae8fbe2ae52c15ed3b52387fea89
ipetrash <ilya.petrash@inbox.ru>
2021-11-07 21:41:03 +0500

    Refactoring

--- html_parsing/grouple_co/print_pretty_title_all_bookmarks.py
+++ html_parsing/grouple_co/print_pretty_title_all_bookmarks.py
@@ -4,30 +4,40 @@
 __author__ = 'ipetrash'
 
 
+from typing import List
 from common import get_all_bookmarks
 
 
-status_by_bookmarks = get_all_bookmarks()
-all_bookmarks = []
-for bookmarks in status_by_bookmarks.values():
-    all_bookmarks += bookmarks
-print('Total bookmarks:', len(all_bookmarks))
-# Total bookmarks: 143
+def get_all_pretty_title_of_bookmarks() -> List[str]:
+    items = []
+    for bookmarks in get_all_bookmarks().values():
+        items += [
+            x.get_title_with_tags()
+            for x in bookmarks
+        ]
 
-print()
+    return items
 
-for i, bookmark in enumerate(all_bookmarks, 1):
-    print(f'{i}. {bookmark.get_title_with_tags()}')
 
-"""
-1. Башня Бога
-2. Берсерк
-3. Боруто
-4. Ван Пис [обновлено]
-...
-139. Фейри Тейл [переведено, без глав]
-140. Шаман Кинг - зеро [выпуск завершен]
-141. Шухер! У нас новый студент! [переведено]
-142. Эльфийская песнь [переведено]
-143. Я — герой! [переведено]
-"""
+if __name__ == '__main__':
+    all_bookmarks = get_all_pretty_title_of_bookmarks()
+    print('Total bookmarks:', len(all_bookmarks))
+    # Total bookmarks: 143
+
+    print()
+
+    for i, bookmark_title in enumerate(all_bookmarks, 1):
+        print(f'{i}. {bookmark_title}')
+
+    """
+    1. Башня Бога
+    2. Берсерк
+    3. Боруто
+    4. Ван Пис [обновлено]
+    ...
+    139. Фейри Тейл [переведено, без глав]
+    140. Шаман Кинг - зеро [выпуск завершен]
+    141. Шухер! У нас новый студент! [переведено]
+    142. Эльфийская песнь [переведено]
+    143. Я — герой! [переведено]
+    """
