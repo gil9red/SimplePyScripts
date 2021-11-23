4bba3bee3efd6b8e0842acd6080c8bc579e9d127
gil9red <ilya.petrash@inbox.ru>
2021-11-23 18:29:10 +0500

    Added html_parsing/poocoin_app.py

---
+++ html_parsing/poocoin_app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import traceback
+
+# pip install selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+URL = 'https://poocoin.app/tokens/0x2a69c59b8b493141d9f41b8f4fa724b60cd204e0'
+
+driver = webdriver.Firefox()
+try:
+    driver.implicitly_wait(5)
+    driver.get(URL)
+    print(f'Title: {driver.title!r}')
+
+    token_price = driver.find_element(By.CSS_SELECTOR, '.mb-1.d-flex.flex-column.lh-1 > .text-success')
+    print(token_price.text)
+    # $0.00165060
+
+except:
+    print(traceback.format_exc())
+
+finally:
+    driver.quit()
