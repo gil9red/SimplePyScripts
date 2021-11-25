fc375db9e1c82e72696fcc4da875826dfb1a4d71
gil9red <ilya.petrash@inbox.ru>
2021-11-25 15:06:37 +0500

    Updated. Added delay by 30 FPS

--- qt__pyqt__pyside__pyqode/remote_controller__widget_with_webserver/camera_requests.py
+++ qt__pyqt__pyside__pyqode/remote_controller__widget_with_webserver/camera_requests.py
@@ -16,15 +16,32 @@ URL_SCREENSHOT = f'http://127.0.0.1:{PORT}/command/SCREENSHOT'
 
 
 class Camera(BaseCamera):
+    FPS = 30
+    IS_PRINTING_FPS = False
+
     @staticmethod
     def frames() -> bytes:
-        while True:
-            while True:
-                try:
-                    rs = requests.post(URL_SCREENSHOT)
-                    break
-                except:
-                    time.sleep(0.5)
+        frame_period = 1.0 / Camera.FPS
+        now = time.time()
+        next_frame = now + frame_period
 
+        start_time = time.time()
+        counter = 0
+
+        while True:
+            rs = requests.post(URL_SCREENSHOT)
             yield rs.content
 
+            # Ограничение работы по FPS
+            while now < next_frame:
+                time.sleep(next_frame - now)
+                now = time.time()
+            next_frame += frame_period
+
+            if Camera.IS_PRINTING_FPS:
+                counter += 1
+                if (time.time() - start_time) >= 1:
+                    fps = counter / (time.time() - start_time)
+                    print(f"FPS: {fps:.2f}")
+                    counter = 0
+                    start_time = time.time()
