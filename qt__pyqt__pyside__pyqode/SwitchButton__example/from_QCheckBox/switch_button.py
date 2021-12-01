abdad0cb783fd6e85a5ad63bf7277d6a2e6a02a5
gil9red <ilya.petrash@inbox.ru>
2021-12-01 18:34:26 +0500

    Added qt__pyqt__pyside__pyqode\SwitchButton__example\from_QCheckBox

---
+++ qt__pyqt__pyside__pyqode/SwitchButton__example/from_QCheckBox/switch_button.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://ru.stackoverflow.com/a/1355594/201445
+
+
+import sys
+
+from PyQt5.QtWidgets import QCheckBox
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtCore import Qt, QEasingCurve, QPropertyAnimation, pyqtProperty, QPoint, QRect
+
+
+class ToggleButton(QCheckBox):
+    def __init__(
+            self,
+            width=70,
+            height=40,
+            bg_color="#777",
+            circle_color="#DDD",
+            active_color="#00BCff",
+            animation_curve=QEasingCurve.OutBounce,
+    ):
+        super().__init__()
+
+        self.setFixedSize(width, height)
+        self.setCursor(Qt.PointingHandCursor)
+
+        self._bg_color = bg_color
+        self._circle_color = circle_color
+        self._active_color = active_color
+
+        self._circle_margin = 3
+        self._circle_position = self._circle_margin
+        self._circle_size = self.height() - self._circle_margin
+
+        self.animation = QPropertyAnimation(self, b"circle_position")
+        self.animation.setEasingCurve(animation_curve)
+        self.animation.setDuration(500)
+        self.stateChanged.connect(self.start_transition)
+
+    @pyqtProperty(int)
+    def circle_position(self) -> int:
+        return self._circle_position
+
+    @circle_position.setter
+    def circle_position(self, pos: int):
+        self._circle_position = pos
+        self.update()
+
+    def start_transition(self, value):
+        self.animation.setStartValue(self.circle_position)
+        if value:
+            self.animation.setEndValue(self.width() - self._circle_size)
+        else:
+            self.animation.setEndValue(self._circle_margin)
+        self.animation.start()
+
+    def hitButton(self, pos: QPoint) -> bool:
+        return self.contentsRect().contains(pos)
+
+    def paintEvent(self, e):
+        p = QPainter(self)
+        p.setRenderHint(QPainter.Antialiasing)
+
+        p.setPen(Qt.NoPen)
+
+        rect = QRect(0, 0, self.width(), self.height())
+
+        p.setBrush(QColor(self._active_color if self.isChecked() else self._bg_color))
+        p.drawRoundedRect(
+            0, 0,
+            rect.width(),
+            self.height(),
+            self.height() / 2,
+            self.height() / 2
+        )
+
+        p.setBrush(QColor(self._circle_color))
+
+        x, y = self._circle_position, self._circle_margin
+        p.drawEllipse(x, y, self._circle_size - self._circle_margin, self._circle_size - self._circle_margin)
+
+
+if __name__ == '__main__':
+    from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout
+
+    class MainWindow(QWidget):
+        def __init__(self):
+            super().__init__()
+
+            self.setWindowTitle("Анимация кнопки переключения")
+            self.toggleBtn = ToggleButton()
+
+            self.layout = QVBoxLayout(self)
+            self.layout.addWidget(self.toggleBtn, Qt.AlignCenter, Qt.AlignCenter)
+
+    app = QApplication(sys.argv)
+    w = MainWindow()
+    w.resize(500, 500)
+    w.show()
+    sys.exit(app.exec_())
+
---
+++ qt__pyqt__pyside__pyqode/SwitchButton__example/from_QCheckBox/test.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
+from switch_button import ToggleButton
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        switch_btn1 = ToggleButton()
+        label_switch_btn1 = QLabel(f'Checked: {switch_btn1.isChecked()}')
+        switch_btn1.clicked.connect(lambda checked: label_switch_btn1.setText(
+            f'Checked: {switch_btn1.isChecked()}'
+        ))
+
+        switch_btn2 = ToggleButton(width=200)
+        label_switch_btn2 = QLabel(f'Checked: {switch_btn2.isChecked()}')
+        switch_btn2.clicked.connect(lambda checked: label_switch_btn2.setText(
+            f'Checked: {switch_btn2.isChecked()}'
+        ))
+
+        main_layout = QVBoxLayout(self)
+        main_layout.addWidget(switch_btn1)
+        main_layout.addWidget(label_switch_btn1)
+
+        main_layout.addWidget(switch_btn2)
+        main_layout.addWidget(label_switch_btn2)
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    mw = MainWindow()
+    mw.show()
+
+    app.exec()
