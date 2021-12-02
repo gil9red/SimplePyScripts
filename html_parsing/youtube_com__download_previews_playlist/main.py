59915838eef2f4d2f0bc83bc0f117c896cbdc22b
gil9red <ilya.petrash@inbox.ru>
2021-12-02 15:55:13 +0500

    Refactoring

--- html_parsing/youtube_com__download_previews_playlist/main.py
+++ html_parsing/youtube_com__download_previews_playlist/main.py
@@ -7,49 +7,30 @@ __author__ = 'ipetrash'
 import sys
 from pathlib import Path
 
-# pip install dpath
-import dpath.util
-
 sys.path.append('..')
 sys.path.append('../..')
 from get_valid_filename import get_valid_filename
-from youtube_com__results_search_query import get_ytInitialData, session
+from youtube_com__results_search_query import Playlist
 
 
-PATTERN_URL_PLAYLIST = 'https://www.youtube.com/playlist?list={playlist_id}'
-PATTERN_URL_VIDEO_PREVIEW = 'https://i.ytimg.com/vi/{video_id}/maxresdefault.jpg'
+def download_playlist_video_previews(url_or_id: str):
+    playlist = Playlist.get_from(url_or_id)
+    safe_playlist_title = get_valid_filename(playlist.title)
 
-
-def download_playlist_video_previews(playlist_id: str):
-    url = PATTERN_URL_PLAYLIST.format(playlist_id=playlist_id)
-    rs = session.get(url)
-
-    data = get_ytInitialData(rs.text)
-    if not data:
-        print('Not found "ytInitialData"!')
-        return
-
-    playlist_title = dpath.util.get(data, '**/playlistMetadataRenderer/title')
-    safe_playlist_title = get_valid_filename(playlist_title)
-
-    dir_name = Path(f'{safe_playlist_title}. {playlist_id}')
+    dir_name = Path(f'{safe_playlist_title}. {playlist.id}')
     dir_name.mkdir(parents=True, exist_ok=True)
 
-    videos = dpath.util.values(data, '**/playlistVideoRenderer')
+    for video in playlist.video_list:
+        safe_title = get_valid_filename(video.title)
+        img_data = video.get_thumbnail_for_maxresdefault()
 
-    for video in videos:
-        video_id = video['videoId']
-        seq = video['index']['simpleText']
-        title = dpath.util.get(video, 'title/runs/0/text')
-
-        safe_title = get_valid_filename(title)
-        file_name = dir_name / f'{seq}. {safe_title}. {video_id}.jpg'
-
-        url_img = PATTERN_URL_VIDEO_PREVIEW.format(video_id=video_id)
-        img_data = session.get(url_img).content
+        file_name = dir_name / f'{video.seq}. {safe_title}. {video.id}.jpg'
         file_name.write_bytes(img_data)
 
 
 if __name__ == '__main__':
     download_playlist_video_previews('PLKom48yw6lJrkPuqimEiH3PT1ibALMh0k')
     download_playlist_video_previews('PLejGw9J2xE9XCDw_lFIo9RJCnzpr6P_0Z')
+
+    url = 'https://www.youtube.com/playlist?list=PLndO6DOY2cLyxQYX7pkDspTJ42JWx07AO'
+    download_playlist_video_previews(url)
