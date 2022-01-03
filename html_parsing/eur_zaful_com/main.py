a5efef03cd6022d80e78887324b25910871a7767
ipetrash <ilya.petrash@inbox.ru>
2022-01-04 04:56:25 +0500

    Updated

--- html_parsing/eur_zaful_com/main.py
+++ html_parsing/eur_zaful_com/main.py
@@ -23,8 +23,8 @@ session.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84
 rs = session.get('https://eur.zaful.com/long-sleeve-geometric-lace-dress-puid_2036888.html?kuid=252759')
 root = BeautifulSoup(rs.content, 'html.parser')
 
-for li in root.select('#js-goodsGalleryThumb > ul > li[data-big-img]'):
-    url_img = li['data-big-img']
+for li in root.select('#js-goodsGalleryThumb > ul > li[data-source-img]'):
+    url_img = li['data-source-img']
     print(url_img)
 
     file_name = url_img.split('/')[-1]
@@ -33,9 +33,9 @@ for li in root.select('#js-goodsGalleryThumb > ul > li[data-big-img]'):
     (DIR_IMAGES / file_name).write_bytes(rs.content)
 
 """
-https://gloimg.zafcdn.com/zaful/pdm-product-pic/Clothing/2016/12/13/goods-first-img/1494263969627244199.jpg
-https://gloimg.zafcdn.com/zaful/pdm-product-pic/Clothing/2016/12/13/goods-first-img/1494263969683201775.jpg
-https://gloimg.zafcdn.com/zaful/pdm-product-pic/Clothing/2016/12/13/goods-first-img/1494263969243092472.jpg
-https://gloimg.zafcdn.com/zaful/pdm-product-pic/Clothing/2016/12/13/goods-first-img/1494263969101328839.JPG
-https://gloimg.zafcdn.com/zaful/pdm-product-pic/Clothing/2016/12/13/goods-first-img/1494263969081073908.JPG
+https://gloimg.zafcdn.com/zaful/pdm-product-pic/Clothing/2016/12/13/source-img/20161213190242_28449.jpg
+https://gloimg.zafcdn.com/zaful/pdm-product-pic/Clothing/2016/12/13/source-img/20161213190242_66497.jpg
+https://gloimg.zafcdn.com/zaful/pdm-product-pic/Clothing/2016/12/13/source-img/20161213190243_16127.jpg
+https://gloimg.zafcdn.com/zaful/pdm-product-pic/Clothing/2016/12/13/source-img/20161213190243_17500.JPG
+https://gloimg.zafcdn.com/zaful/pdm-product-pic/Clothing/2016/12/13/source-img/20161213190243_73427.JPG
 """
