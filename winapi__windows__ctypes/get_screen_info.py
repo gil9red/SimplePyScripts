c8bc76b673c03c6ebfe9c694119a6046af477713
ipetrash <ilya.petrash@inbox.ru>
2022-01-04 03:29:35 +0500

    Added winapi__windows__ctypes/get_screen_info.py

---
+++ winapi__windows__ctypes/get_screen_info.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import ctypes
+import tkinter
+
+from typing import Tuple
+
+
+def get_screen_info() -> Tuple[int, int, int]:
+    ctypes.windll.shcore.SetProcessDpiAwareness(1)
+
+    root = tkinter.Tk()
+
+    width = ctypes.windll.user32.GetSystemMetrics(0)
+    height = ctypes.windll.user32.GetSystemMetrics(1)
+
+    dpi = ctypes.windll.user32.GetDpiForWindow(root.winfo_id())
+
+    # Destroy the window
+    root.destroy()
+
+    return width, height, dpi
+
+
+if __name__ == '__main__':
+    width, height, dpi = get_screen_info()
+    print(width, height, dpi)
