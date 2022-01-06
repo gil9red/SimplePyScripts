091fe0c2fe800f9bb2b3896c65a3beee18e7dd76
ipetrash <ilya.petrash@inbox.ru>
2022-01-06 22:16:53 +0500

    Added "winreg__examples/get_known_dlls.py"

---
+++ winreg__examples/get_known_dlls.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from typing import Dict
+from common import get_entries_as_dict
+
+
+PATH = r'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\KnownDLLs'
+
+
+def get_known_dlls() -> Dict[str, str]:
+    return get_entries_as_dict(PATH, raw_value=True)
+
+
+if __name__ == '__main__':
+    print(get_known_dlls())
+
