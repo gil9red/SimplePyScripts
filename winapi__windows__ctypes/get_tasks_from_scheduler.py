b49b6ca92ec6dd1b1003cefadeab0a7bc72f46e8
ipetrash <ilya.petrash@inbox.ru>
2022-01-08 00:21:21 +0500

    Refactoring

--- winapi__windows__ctypes/get_tasks_from_scheduler.py
+++ winapi__windows__ctypes/get_tasks_from_scheduler.py
@@ -4,67 +4,202 @@
 __author__ = 'ipetrash'
 
 
-import win32com.client
+# SOURCE: https://docs.microsoft.com/en-us/windows/win32/taskschd/taskschedulerschema-task-element
+# SOURCE: https://docs.microsoft.com/en-us/windows/win32/taskschd/execaction
+# SOURCE: https://docs.microsoft.com/en-us/windows/win32/taskschd/comhandleraction
+# SOURCE: https://docs.microsoft.com/en-us/windows/win32/taskschd/emailaction
+# SOURCE: https://docs.microsoft.com/en-us/windows/win32/taskschd/showmessageaction
+
+
+import datetime as DT
+import enum
+from dataclasses import dataclass, field
+from typing import List, TypeVar
+
 import pywintypes
+import win32com.client
 
 
+# SOURCE: https://docs.microsoft.com/en-us/windows/win32/api/taskschd/nf-taskschd-itaskfolder-gettasks#parameters
+#         Specifies whether to retrieve hidden tasks. Pass in TASK_ENUM_HIDDEN to retrieve all tasks in the folder
+#         including hidden tasks, and pass in 0 to retrieve all the tasks in the folder excluding the hidden tasks.
 TASK_ENUM_HIDDEN = 1
-TASK_STATE = ['Unknown', 'Disabled', 'Queued', 'Ready', 'Running']
+
+
+class TaskStateEnum(enum.IntEnum):
+    Unknown = 0
+    Disabled = enum.auto()
+    Queued = enum.auto()
+    Ready = enum.auto()
+    Running = enum.auto()
+
 
 # https://docs.microsoft.com/en-us/windows/win32/taskschd/action-type
-TASK_ACTION_EXEC = 0
-TASK_ACTION_COM_HANDLER = 5
-TASK_ACTION_SEND_EMAIL = 6
-TASK_ACTION_SHOW_MESSAGE = 7
+class TaskActionEnum(enum.IntEnum):
+    Exec = 0
+    ComHandler = 5
+    SendEmail = 6
+    ShowMessage = 7
 
 
-scheduler = win32com.client.Dispatch('Schedule.Service')
-scheduler.Connect()
+@dataclass
+class ExecAction:
+    path: str
+    working_directory: str
+    arguments: str
 
-n = 0
-folders = [scheduler.GetFolder('\\')]
-while folders:
-    folder = folders.pop(0)
-    folders += list(folder.GetFolders(0))
-    for task in folder.GetTasks(TASK_ENUM_HIDDEN):
-        n += 1
+    @classmethod
+    def get_from(cls, action: win32com.client.CDispatch) -> 'ExecAction':
+        return cls(
+            path=action.Path,
+            working_directory=action.WorkingDirectory,
+            arguments=action.Arguments,
+        )
 
+
+@dataclass
+class ComHandlerAction:
+    class_id: str
+    data: str
+
+    @classmethod
+    def get_from(cls, action: win32com.client.CDispatch) -> 'ComHandlerAction':
+        return cls(
+            class_id=action.ClassId,
+            data=action.Data,
+        )
+
+
+@dataclass
+class EmailAction:
+    from_: str
+    to: str
+    subject: str
+    body: str
+    server: str
+    # attachments: ???  # TODO: Unknown
+    bcc: str
+    cc: str
+    # header_fields: ???  # TODO: Unknown
+    reply_to: str
+
+    @classmethod
+    def get_from(cls, action: win32com.client.CDispatch) -> 'EmailAction':
+        return cls(
+            from_=action.From,
+            to=action.To,
+            subject=action.Subject,
+            body=action.Body,
+            server=action.Server,
+            # attachments=???  # TODO: Unknown
+            bcc=action.Bcc,
+            cc=action.Cc,
+            # header_fields=xxx,  # TODO: Unknown
+            reply_to=action.ReplyTo,
+        )
+
+
+@dataclass
+class ShowMessageAction:
+    title: str
+    message_body: str
+
+    @classmethod
+    def get_from(cls, action: win32com.client.CDispatch) -> 'ShowMessageAction':
+        return cls(
+            title=action.Title,
+            message_body=action.MessageBody,
+        )
+
+
+ActionType = TypeVar('ActionType', ExecAction, ComHandlerAction, EmailAction, ShowMessageAction)
+
+
+@dataclass
+class Task:
+    name: str
+    path: str
+    hidden: bool
+    state: str
+    enabled: bool
+    last_run_time: DT.datetime
+    last_task_result: int
+    next_run_time: DT.datetime
+    number_of_missed_runs: int
+    actions: List[ActionType] = field(default_factory=list)
+
+    @classmethod
+    def get_from(cls, task: win32com.client.CDispatch) -> 'Task':
         try:
-            settings = task.Definition.Settings
-            hidden = settings.Hidden
-        except:
-            hidden = '<unknown>'
-
-        print('%s.' % n)
-        print('Name          : %s' % task.Name)
-        print('Path          : %s' % task.Path)
-        print('Hidden        : %s' % hidden)
-        print('State         : %s' % TASK_STATE[task.State])
-        print('Enabled       : %s' % task.Enabled)
-        print('Last Run      : %s' % task.LastRunTime)
-        print('Last Result   : %s' % task.LastTaskResult)
-        print('Next Run Time : %s' % task.NextRunTime)
-        print('Number Of Missed Runs : %s' % task.NumberOfMissedRuns)
-
-        actions = []
-        try:
-            for action in task.Definition.Actions:
-                # TODO: Использовать типы на питоне
-                actions.append(action)
-                if action.Type == TASK_ACTION_EXEC:
-                    print('TASK_ACTION_EXEC', action.Path, action.WorkingDirectory, action.Arguments, sep=' | ')
-                elif action.Type == TASK_ACTION_COM_HANDLER:
-                    print('TASK_ACTION_COM_HANDLER', action.ClassId, action.Data)
-                elif action.Type == TASK_ACTION_SEND_EMAIL:
-                    print('TASK_ACTION_SEND_EMAIL', action.To, action.Subject, action.Body)
-                elif action.Type == TASK_ACTION_SHOW_MESSAGE:
-                    print('TASK_ACTION_SHOW_MESSAGE', action.Title, action.MessageBody)
-                else:
-                    raise Exception(f'Unknown type: {action.Type}')
+            hidden = task.Definition.Settings.Hidden
         except pywintypes.com_error:
-            pass
+            hidden = False
 
-        print('Actions : %s' % actions)
-        print()
+        try:
+            def_actions = task.Definition.Actions
+        except pywintypes.com_error:
+            def_actions = []
+        actions = []
 
-print('Listed %d tasks.' % n)
+        for action_com_obj in def_actions:
+            action_type = TaskActionEnum(action_com_obj.Type)
+
+            if action_type == TaskActionEnum.Exec:
+                actions.append(ExecAction.get_from(action_com_obj))
+            elif action_type == TaskActionEnum.ComHandler:
+                actions.append(ComHandlerAction.get_from(action_com_obj))
+            elif action_type == TaskActionEnum.SendEmail:
+                actions.append(EmailAction.get_from(action_com_obj))
+            elif action_type == TaskActionEnum.ShowMessage:
+                actions.append(ShowMessageAction.get_from(action_com_obj))
+
+        return cls(
+            name=task.Name,
+            path=task.Path,
+            hidden=hidden,
+            state=TaskStateEnum(task.State).name,
+            enabled=task.Enabled,
+            last_run_time=task.LastRunTime,
+            last_task_result=task.LastTaskResult,
+            next_run_time=task.NextRunTime,
+            number_of_missed_runs=task.NumberOfMissedRuns,
+            actions=actions,
+        )
+
+
+def get_tasks() -> List[Task]:
+    items = []
+
+    scheduler = win32com.client.Dispatch('Schedule.Service')
+    scheduler.Connect()
+
+    folders = [scheduler.GetFolder('\\')]
+    while folders:
+        folder = folders.pop(0)
+        folders += list(folder.GetFolders(0))
+        for task_com_obj in folder.GetTasks(TASK_ENUM_HIDDEN):
+            items.append(
+                Task.get_from(task_com_obj)
+            )
+
+    return items
+
+
+if __name__ == '__main__':
+    items = get_tasks()
+    print(f'Total task: {len(items)}')
+
+    hidden_tasks = [task for task in items if task.hidden]
+    print(f'Total hidden tasks: {len(hidden_tasks)}')
+
+    enabled_tasks = [task for task in items if task.enabled]
+    print(f'Total enabled tasks: {len(enabled_tasks)}')
+
+    hidden_and_enabled_tasks = [task for task in items if task.hidden and task.enabled]
+    print(f'Total hidden and enabled tasks: {len(hidden_and_enabled_tasks)}')
+
+    print()
+
+    print('First 10 tasks:')
+    for i, task in enumerate(items[:10], 1):
+        print(f'    {i}. {task}')
