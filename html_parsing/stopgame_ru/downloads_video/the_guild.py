d66c1de5a031c46314a631f603984b383d8425de
ipetrash <ilya.petrash@inbox.ru>
2022-01-17 21:13:32 +0500

    Added "html_parsing/stopgame_ru/dragon_age_redemption.py"

---
+++ html_parsing/stopgame_ru/downloads_video/the_guild.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from pathlib import Path
+from common import download
+
+
+DIR = Path(__file__).resolve().parent
+
+DIR_VIDEO = DIR / Path(__file__).stem
+DIR_VIDEO.mkdir(parents=True, exist_ok=True)
+
+URL = 'https://stopgame.ru/gamemovie/genre/guild'
+
+download(DIR_VIDEO, URL)
