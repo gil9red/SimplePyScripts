04b52d669f7c9d8ab89dfb447a57c6065f41e673
gil9red <ilya.petrash@inbox.ru>
2022-01-28 12:32:58 +0500

    Updated. Added functions: get_random_seed, create_world. Renamed: "world_seed" -> "seed"

--- world_seed_in_binary_2D.py
+++ world_seed_in_binary_2D.py
@@ -4,19 +4,33 @@
 __author__ = 'ipetrash'
 
 
+import random
+import string
+
 from itertools import cycle
 
 
-def get_bits_world_seed(world_seed: str) -> str:
-    return ''.join(bin(ord(c))[2:][:8].zfill(8) for c in world_seed)
+def get_random_seed(length: int = 8) -> str:
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+
+def get_bits_seed(seed: str) -> str:
+    return ''.join(bin(ord(c))[2:][:8].zfill(8) for c in seed)
+
+
+def create_world(rows: int, cols: int) -> list[list[int]]:
+    return [
+        [0] * cols
+        for _ in range(rows)
+    ]
 
 
 def print_world(world: list[list[int]]):
     print('\n'.join(' '.join(map(str, row)) for row in world))
 
 
-def fill_world(world: list[list[int]], world_seed: str):
-    bits = get_bits_world_seed(world_seed)
+def fill_world(world: list[list[int]], seed: str):
+    bits = get_bits_seed(seed)
     bits = cycle(bits)
 
     for row in range(len(world)):
@@ -25,18 +39,19 @@ def fill_world(world: list[list[int]], world_seed: str):
 
 
 if __name__ == '__main__':
-    assert get_bits_world_seed('1') == '00110001'
-    assert get_bits_world_seed('123') == '001100010011001000110011'
+    for i in range(8, 64+1):
+        assert len(get_random_seed(length=i)) == i
 
-    N = 10
-    world = [
-        [0] * N
-        for _ in range(N)
-    ]
+    print('Random seed:', get_random_seed())
+    print()
+
+    assert get_bits_seed('1') == '00110001'
+    assert get_bits_seed('123') == '001100010011001000110011'
+
+    world = create_world(rows=5, cols=10)
     print_world(world)
 
     print()
 
-    world_seed = '123'
-    fill_world(world, world_seed)
+    fill_world(world, seed='123')
     print_world(world)
