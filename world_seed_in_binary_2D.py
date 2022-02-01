af691bf5a93cee3aac903fedccfb0f4ba9ab2a0c
ipetrash <ilya.petrash@inbox.ru>
2022-02-01 21:32:53 +0500

    Updated. Changed algorithm of seed: using sha256

--- world_seed_in_binary_2D.py
+++ world_seed_in_binary_2D.py
@@ -4,6 +4,7 @@
 __author__ = 'ipetrash'
 
 
+import hashlib
 import random
 import string
 
@@ -15,7 +16,8 @@ def get_random_seed(length: int = 8) -> str:
 
 
 def get_bits_seed(seed: str) -> str:
-    return ''.join(bin(ord(c))[2:][:8].zfill(8) for c in seed)
+    seed = bytes(seed, encoding='utf-8')
+    return ''.join(bin(c)[2:][:8].zfill(8) for c in hashlib.sha256(seed).digest())
 
 
 def create_world(rows: int, cols: int) -> list[list[int]]:
@@ -45,8 +47,8 @@ if __name__ == '__main__':
     print('Random seed:', get_random_seed())
     print()
 
-    assert get_bits_seed('1') == '00110001'
-    assert get_bits_seed('123') == '001100010011001000110011'
+    assert get_bits_seed('1') == '0110101110000110101100100111001111111111001101001111110011100001100111010110101110000000010011101111111101011010001111110101011101000111101011011010010011101010101000100010111100011101010010011100000000011110010100101101110110110111100001110101101101001011'
+    assert get_bits_seed('123') == '1010011001100101101001000101100100100000010000100010111110011101010000010111111001001000011001111110111111011100010011111011100010100000010010100001111100111111111111110001111110100000011111101001100110001110100001101111011111110111101000100111101011100011'
 
     world = create_world(rows=5, cols=10)
     print_world(world)
