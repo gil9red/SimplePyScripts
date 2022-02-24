7e9706cf11cde1accbc3ecea55921f6450db4746
gil9red <ilya.petrash@inbox.ru>
2022-02-24 13:00:23 +0500

    Updated.

--- cbr_ru/xml_metall/main.py
+++ cbr_ru/xml_metall/main.py
@@ -14,34 +14,39 @@ from parser import get_metal_rates, get_pair_dates
 
 while True:
     log.info("Запуск")
+    try:
+        start_date = db.MetalRate.get_last_date()
+        log.info(f"Поиск от {start_date}\n")
 
-    start_date = db.MetalRate.get_last_date()
-    log.info(f"Поиск от {start_date}\n")
+        for date_req1, date_req2 in get_pair_dates(start_date):
+            log.info(f'Поиск за {date_req1} - {date_req2}')
 
-    for date_req1, date_req2 in get_pair_dates(start_date):
-        log.info(f'Поиск за {date_req1} - {date_req2}')
+            while True:
+                try:
+                    rates = get_metal_rates(date_req1, date_req2)
 
-        while True:
-            try:
-                rates = get_metal_rates(date_req1, date_req2)
+                    log.info(f'Найдено: {len(rates)}\n')
+                    if not rates:
+                        log.info('Ничего не вернулось, похоже на ошибку сервиса. Нужно повторить')
+                        time.sleep(60)
+                        continue
 
-                log.info(f'Найдено: {len(rates)}\n')
-                if not rates:
-                    log.info('Ничего не вернулось, похоже на ошибку сервиса. Нужно повторить')
-                    time.sleep(60)
+                    for metal_rate in rates:
+                        db.MetalRate.add_from(metal_rate)
+
+                except Exception:
+                    log.exception('Ошибка:')
+                    time.sleep(60 * 5)
                     continue
 
-                for metal_rate in rates:
-                    db.MetalRate.add_from(metal_rate)
+                break
 
-            except Exception:
-                log.exception('Ошибка:')
-                time.sleep(60 * 5)
-                continue
+            time.sleep(60)
 
-            break
-
-        time.sleep(60)
+    except Exception:
+        log.exception('Ошибка:')
+        time.sleep(60 * 5)
+        continue
 
     log.info("Завершено.")
 
