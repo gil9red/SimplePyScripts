93ea960979c967d69ae6ba872af768d90e5e2e8b
gil9red <ilya.petrash@inbox.ru>
2022-03-05 17:13:36 +0500

    Updated.

--- Pascal Triangle.py
+++ Pascal Triangle.py
@@ -5,11 +5,12 @@ __author__ = 'ipetrash'
 
 
 # SOURCE: https://www.delftstack.com/howto/python/python-pascal-triangle/#print-pascal-s-triangle-by-computing-the-power-of-11-in-python
+
 """
 Print Pascal’s Triangle by Computing the Power of 11 in Python
 """
 
 
-N = 8
+N = 5  # NOTE: MAXIMUM
 for n in range(N):
     print(list(map(int, str(11 ** n))))
