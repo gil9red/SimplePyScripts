9f773a4f3aae70e338d02bd3a38d5713f788c1ec
gil9red <ilya.petrash@inbox.ru>
2022-03-27 00:21:59 +0500

    Added "pyautogui__keyboard__examples/input_cheats_starcraft2.py"

---
+++ pyautogui__keyboard__examples/input_cheats_starcraft2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# Запустить скрипт от админа. В игре: нажать Enter, ввести комбинацию для чита, нажать Ctrl+V
+
+
+import os
+
+# pip install keyboard
+import keyboard
+
+
+def write(text: str):
+    print(text)
+
+    os.system(f'echo {text}|clip')
+
+
+# Отключение тумана войны
+keyboard.add_hotkey('Ctrl+Shift+Z', write, args=['TookTheRedPill'])
+
+# +5000 к каждому ресурсу
+keyboard.add_hotkey('Ctrl+Shift+X', write, args=['WhoRunBartertown'])
+
+# Активация быстрой постройки и улучшений
+keyboard.add_hotkey('Ctrl+Shift+C', write, args=['CatFoodForPrawnGuns'])
+
+# Режим «бога»
+keyboard.add_hotkey('Ctrl+Shift+V', write, args=['TerribleTerribleDamage'])
+
+# Мгновенная победа
+keyboard.add_hotkey('Ctrl+Shift+B', write, args=['WhatIsBestInLife'])
+
+keyboard.wait()
