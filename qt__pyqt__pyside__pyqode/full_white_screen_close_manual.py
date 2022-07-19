7a6061bef31fd34ef44d1582fdcf99fc4c116c80
gil9red <ilya.petrash@inbox.ru>
2022-07-20 00:38:52 +0500

    Added "qt__pyqt__pyside__pyqode/full_white_screen_close_manual.py"

---
+++ qt__pyqt__pyside__pyqode/full_white_screen_close_manual.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt5.QtWidgets import QApplication
+
+from full_black_screen_close_manual import MainWindow
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    mw = MainWindow(background_color='white', text_color='black')
+    mw.resize(600, 600)
+    mw.showFullScreen()
+
+    app.exec()
