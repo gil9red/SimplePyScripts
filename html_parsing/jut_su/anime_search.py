dc2b0fdb7c85db5d3648f9da4a2d03518083f9d3
gil9red <ilya.petrash@inbox.ru>
2022-08-25 02:30:17 +0500

    Added "html_parsing/jut_su/anime.py"

---
+++ html_parsing/jut_su/anime_search.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from typing import List
+from anime import Anime, send_post, parse_anime_list
+
+
+def search(text: str) -> List[Anime]:
+    rs = send_post(show_search=text)
+    return parse_anime_list(rs)
+
+
+if __name__ == '__main__':
+    for anime in search(text='гора'):
+        print(anime)
+    """
+    Anime(url='https://jut.su/shiki/', title='Усопшие')
+    Anime(url='https://jut.su/slime-taoshite-300-nen/', title='Убивала слизней 300 лет до максимального уровня')
+    Anime(url='https://jut.su/reikenzan/', title='Гора Священного меча')
+    Anime(url='https://jut.su/shakunetsu-kabaddi/', title='Пылающий Кабадди')
+    """
