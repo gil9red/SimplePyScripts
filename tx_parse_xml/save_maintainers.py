2d3b35554b8280152d6c6fff8d5196ff947a1fdb
gil9red <ilya.petrash@inbox.ru>
2022-09-16 13:21:17 +0500

    Added tx_parse_xml/save_maintainers.py

---
+++ tx_parse_xml/save_maintainers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from show_maintainers import get_owner_by_modules
+
+
+project_dir = "C:/DEV__TX/trunk_tx"
+
+with open('maintainers.txt', 'w', encoding='utf-8') as f:
+    owner_by_modules = get_owner_by_modules(project_dir)
+
+    lines = []
+    for email in sorted(owner_by_modules):
+        modules = owner_by_modules[email]
+        modules.sort()
+
+        lines.append(f'{email} ({len(modules)}):')
+        for module in modules:
+            lines.append(f'    {module}')
+        lines.append('')
+
+    text = '\n'.join(lines).strip()
+    f.write(text)
