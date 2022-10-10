2b24d5f7ff94a1cb797ce3441102a78e8cd27fa8
gil9red <ilya.petrash@inbox.ru>
2022-10-10 22:36:26 +0500

    Updated. Added tests for: get_anime_achievement, get_achievements

--- html_parsing/jut_su/test.py
+++ html_parsing/jut_su/test.py
@@ -4,6 +4,7 @@
 __author__ = 'ipetrash'
 
 
+import dataclasses
 import time
 import unittest
 
@@ -14,6 +15,27 @@ from anime_search import search
 
 
 class GetPossibleAchievementsTestCase(unittest.TestCase):
+    EXPECTED_704 = {
+        'category': 'events',
+        'time_start': 704,
+        'time_start_pretty': get_possible_achievements_from_video.seconds_to_str(704),
+        'title': 'Ядовитый бутон',
+        'description': 'Высвобождение Заэльаппоро',
+        'icon': 'js_preres_url + "/uploads/achievements/icons/5703.jpg"',
+        'id': 5703,
+        'hash': '11560c9068571116'
+    }
+    EXPECTED_1286 = {
+        'category': 'events',
+        'time_start': 1286,
+        'time_start_pretty': get_possible_achievements_from_video.seconds_to_str(1286),
+        'title': 'Не изменилась',
+        'description': 'Нелл не думает сдаваться',
+        'icon': 'js_preres_url + "/uploads/achievements/icons/5704.jpg"',
+        'id': 5704,
+        'hash': 'dc3acd1d8af21525'
+    }
+
     def test_parse_raw_anime_achievement(self):
         text = """
         category: "events",
@@ -25,17 +47,7 @@ class GetPossibleAchievementsTestCase(unittest.TestCase):
         hash: "11560c9068571116"
         """
         actual = get_possible_achievements_from_video.parse_raw_anime_achievement(text)
-        expected = {
-            'category': 'events',
-            'time_start': 704,
-            'time_start_pretty': get_possible_achievements_from_video.seconds_to_str(704),
-            'title': 'Ядовитый бутон',
-            'description': 'Высвобождение Заэльаппоро',
-            'icon': 'js_preres_url + "/uploads/achievements/icons/5703.jpg"',
-            'id': 5703,
-            'hash': '11560c9068571116'
-        }
-        self.assertEqual(actual, expected)
+        self.assertEqual(actual, self.EXPECTED_704)
 
     def test_parse_raw_anime_achievements(self):
         text = """
@@ -61,26 +73,8 @@ class GetPossibleAchievementsTestCase(unittest.TestCase):
         """
         actual = get_possible_achievements_from_video.parse_raw_anime_achievements(text)
         expected = [
-            {
-                'category': 'events',
-                'time_start': 704,
-                'time_start_pretty': get_possible_achievements_from_video.seconds_to_str(704),
-                'title': 'Ядовитый бутон',
-                'description': 'Высвобождение Заэльаппоро',
-                'icon': 'js_preres_url + "/uploads/achievements/icons/5703.jpg"',
-                'id': 5703,
-                'hash': '11560c9068571116'
-            },
-            {
-                'category': 'events',
-                'time_start': 1286,
-                'time_start_pretty': get_possible_achievements_from_video.seconds_to_str(1286),
-                'title': 'Не изменилась',
-                'description': 'Нелл не думает сдаваться',
-                'icon': 'js_preres_url + "/uploads/achievements/icons/5704.jpg"',
-                'id': 5704,
-                'hash': 'dc3acd1d8af21525'
-            }
+            self.EXPECTED_704,
+            self.EXPECTED_1286,
         ]
         self.assertEqual(actual, expected)
 
@@ -89,31 +83,41 @@ class GetPossibleAchievementsTestCase(unittest.TestCase):
         self.assertTrue(actual)
 
         expected = [
-            {
-                'category': 'events',
-                'time_start': 704,
-                'time_start_pretty': get_possible_achievements_from_video.seconds_to_str(704),
-                'title': 'Ядовитый бутон',
-                'description': 'Высвобождение Заэльаппоро',
-                'icon': 'js_preres_url + "/uploads/achievements/icons/5703.jpg"',
-                'id': 5703,
-                # 'hash': '11560c9068571116'
-            },
-            {
-                'category': 'events',
-                'time_start': 1286,
-                'time_start_pretty': get_possible_achievements_from_video.seconds_to_str(1286),
-                'title': 'Не изменилась',
-                'description': 'Нелл не думает сдаваться',
-                'icon': 'js_preres_url + "/uploads/achievements/icons/5704.jpg"',
-                'id': 5704,
-                # 'hash': 'dc3acd1d8af21525'
-            }
+            dict(self.EXPECTED_704),
+            dict(self.EXPECTED_1286),
         ]
 
         # Хеш может отличаться (думаю, от пользователя к пользователю), поэтому нужно убрать его
-        for achievement in actual:
-            achievement.pop('hash')
+        for a, e in zip(actual, expected):
+            a.pop('hash')
+            e.pop('hash')
+
+        self.assertEqual(actual, expected)
+
+    def test_get_anime_achievement(self):
+        expected = self.EXPECTED_704
+        actual = get_possible_achievements_from_video.get_anime_achievement(expected)
+        actual = dataclasses.asdict(actual)
+
+        self.assertEqual(actual, expected)
+
+    def test_get_achievements(self):
+        actual = get_possible_achievements_from_video.get_achievements('https://jut.su/bleeach/episode-193.html')
+        actual = [
+            dataclasses.asdict(x)
+            for x in actual
+        ]
+        self.assertTrue(actual)
+
+        expected = [
+            dict(self.EXPECTED_704),
+            dict(self.EXPECTED_1286),
+        ]
+
+        # Хеш может отличаться (думаю, от пользователя к пользователю), поэтому нужно убрать его
+        for a, e in zip(actual, expected):
+            a.pop('hash')
+            e.pop('hash')
 
         self.assertEqual(actual, expected)
 
