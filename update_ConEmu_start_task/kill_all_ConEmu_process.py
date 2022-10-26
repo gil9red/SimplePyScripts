f949501ae297576d7a8029a5c872dff98bd3cad7
gil9red <ilya.petrash@inbox.ru>
2022-10-26 16:18:09 +0500

    Added "update_ConEmu_start_task/kill_all_ConEmu_process.py"

---
+++ update_ConEmu_start_task/kill_all_ConEmu_process.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install psutil
+import psutil
+
+
+if __name__ == '__main__':
+    for process in psutil.process_iter():
+        if 'conemu64' in process.name().lower():
+            children = process.children(recursive=True)
+            print(f'Kill process. Pid={process.pid}, name={process.name()}, children: {len(children)}')
+
+            # Если убивать только процесс, то его дочерние процессы останутся как отдельные окна консоли
+            for child in children:
+                child.kill()
+
+            process.kill()
