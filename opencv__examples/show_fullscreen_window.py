ecd80e087b2f6e4d4a53509e049211aee81620bd
gil9red <ilya.petrash@inbox.ru>
2022-11-03 12:25:47 +0500

    Added "opencv__examples/show_fullscreen_window.py"

---
+++ opencv__examples/show_fullscreen_window.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import sys
+import cv2
+
+
+window_name = "window"
+img = cv2.imread('gaussian_blur/example.jpg')
+
+while True:
+    cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+    cv2.imshow(window_name, img)
+
+    if cv2.waitKey(25) == 27:  # Esc to quit
+        sys.exit()
