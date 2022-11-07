34b4c88bcf91b9b9896b449a77d524d8827dfee0
gil9red <ilya.petrash@inbox.ru>
2022-11-07 22:23:52 +0500

    Refactoring

--- Mass Effect 1 - Feros - Valve Puzzle Solution.py
+++ Mass Effect 1 - Feros - Valve Puzzle Solution.py
@@ -15,9 +15,9 @@ for i in range(len(ITEMS)):
     for items in itertools.combinations(ITEMS, r=i+1):
         total = sum(items)
         if total in NEEDS:
-            print(f'{"+".join(map(str, items))} = {total}')
+            print(f'{" + ".join(map(str, items))} = {total}')
 
 """
-5+17+11 = 33
-7+11+13 = 31
+5 + 17 + 11 = 33
+7 + 11 + 13 = 31
 """
