04eac7bd824cd1789d439c1f0e8bba7471c25409
gil9red <ilya.petrash@inbox.ru>
2022-12-23 01:13:17 +0500

    Moved

--- pikabu_ru/profile_rating__tracking/main.py
+++ pikabu_ru/profile_rating__tracking/main.py
@@ -14,7 +14,7 @@ from pathlib import Path
 DIR = Path(__file__).resolve().parent
 
 # Import https://github.com/gil9red/SimplePyScripts/blob/8fa9b9c23d10b5ee7ff0161da997b463f7a861bf/wait/wait.py
-sys.path.append(str(DIR.parent.parent.parent / 'wait'))
+sys.path.append(str(DIR.parent.parent / 'wait'))
 sys.path.append(str(DIR.parent))
 from wait import wait
 from get_profile_rating import get_profile_rating
--- pikabu_ru/profile_rating__tracking/web.py
+++ pikabu_ru/profile_rating__tracking/web.py
@@ -4,15 +4,11 @@
 __author__ = 'ipetrash'
 
 
-import logging
-
 from flask import Flask, render_template
 from db import ProfileRating
 
 
 app = Flask(__name__)
-
-logging.basicConfig(level=logging.DEBUG)
 
 
 @app.route("/")
@@ -23,11 +19,4 @@ def index():
 
 if __name__ == '__main__':
     app.debug = True
-
-    # Localhost
-    # port=0 -- random free port
-    # app.run(port=0)
     app.run(port=10017)
-
-    # # Public IP
-    # app.run(host='0.0.0.0')
