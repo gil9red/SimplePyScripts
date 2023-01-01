01f34527ff7dab5af03079eee525f38c45d9f094
gil9red <ilya.petrash@inbox.ru>
2023-01-01 22:36:22 +0500

    Bugfix

--- html_parsing/get_game_genres/parsers/stopgame_ru.py
+++ html_parsing/get_game_genres/parsers/stopgame_ru.py
@@ -4,22 +4,26 @@
 __author__ = 'ipetrash'
 
 
+import re
 from base_parser import BaseParser
 
 
 class StopgameRu_Parser(BaseParser):
     def _parse(self) -> list[str]:
-        url = f'https://stopgame.ru/search/?s={self.game_name}&where=games&sort=name'
+        url = f'https://stopgame.ru/search/?s={self.game_name}&where=games&sort=relevance'
         root = self.send_get(url, return_html=True)
     
-        for game_block in root.select('.game-summary'):
-            title = self.get_norm_text(game_block.select_one('.caption'))
+        for game_block in root.select('div[data-key]'):
+            title = self.get_norm_text(game_block.select_one('h2[class*="_title_"]'))
             if not self.is_found_game(title):
                 continue
 
+            genre_title_el = game_block.select_one('*[class*="_info-grid__title_"]:-soup-contains("Жанр")')
+            genre_value_el = genre_title_el.find_next_sibling(attrs={'class': re.compile('info-grid__value_')})
+
             # Сойдет первый, совпадающий по имени, вариант
             return [
-                self.get_norm_text(a) for a in game_block.select('a[href*="?genre[]"]')
+                self.get_norm_text(a) for a in genre_value_el.select('a')
             ]
     
         self.log_info(f'Not found game {self.game_name!r}')
