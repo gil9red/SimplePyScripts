e8dfc1353dfe304c3cbc557a9a52995399eeab4c
gil9red <ilya.petrash@inbox.ru>
2023-01-05 22:17:33 +0500

    Added "html_parsing/robots_txt__get__sitemap_xml.py"

---
+++ html_parsing/robots_txt__get__sitemap_xml.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import requests
+
+
+rs = requests.get('https://ru.stackoverflow.com/robots.txt')
+
+for line in rs.text.splitlines():
+    key, value = map(str.strip, line.split(':', maxsplit=1))
+    if key == 'Sitemap':
+        print(value)
+    # https://ru.stackoverflow.com/sitemap.xml
