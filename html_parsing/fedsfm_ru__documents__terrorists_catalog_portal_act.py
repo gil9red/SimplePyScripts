b2a1af09c4882cae57308f5564486599b8c52ae3
gil9red <ilya.petrash@inbox.ru>
2023-01-05 22:05:58 +0500

    Added "html_parsing/fedsfm_ru__documents__terrorists_catalog_portal_act.py"

---
+++ html_parsing/fedsfm_ru__documents__terrorists_catalog_portal_act.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import requests
+from bs4 import BeautifulSoup
+
+
+rs = requests.get('http://www.fedsfm.ru/documents/terrorists-catalog-portal-act')
+root = BeautifulSoup(rs.content, 'html.parser')
+
+items = [li.text for li in root.select('#russianFL li')]
+print(f'Всего: {len(items)}')
+# Всего: 12675
+
+print(*items[:3], sep='\n')
+print('...')
+print(*items[-3:], sep='\n')
