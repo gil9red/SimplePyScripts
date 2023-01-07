45576bbbeaf003529a0db09ffb80ba0a68eb4265
gil9red <ilya.petrash@inbox.ru>
2023-01-08 01:31:30 +0500

    Added "flask_restful__examples/todo/*"

---
+++ flask_restful__examples/todo/example.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import requests
+
+
+URL_BASE = 'http://127.0.0.1:5000'
+
+
+url = URL_BASE
+print(f'GET: {url}')
+rs = requests.get(url)
+print(rs, rs.json())
+"""
+GET: http://127.0.0.1:5000
+<Response [200]> {'total': 3}
+"""
+
+print()
+
+url = f'{URL_BASE}/todos'
+print(f'GET: {url}')
+rs = requests.get(url)
+rs_json = rs.json()
+print(rs, rs_json)
+"""
+GET: http://127.0.0.1:5000/todos
+<Response [200]> {'todo1': {'task': 'build an API'}, 'todo2': {'task': '?????'}, 'todo3': {'task': 'profit!'}}
+"""
+
+print()
+
+todo_id = list(rs_json)[0]
+
+url = f'{URL_BASE}/todos/{todo_id}'
+print(f'GET: {url}')
+rs = requests.get(url)
+print(rs, rs.json())
+"""
+GET: http://127.0.0.1:5000/todos/todo1
+<Response [200]> {'task': 'build an API'}
+"""
+
+print()
+
+url = f'{URL_BASE}/todos/{todo_id}'
+print(f'PUT: {url}')
+rs = requests.put(url, json=dict(task=f'Update task {todo_id}'))
+print(rs, rs.json())
+"""
+PUT: http://127.0.0.1:5000/todos/todo1
+<Response [201]> {'task': 'Update task todo1'}
+"""
+
+print()
+
+url = f'{URL_BASE}/todos/{todo_id}_new'
+print(f'PUT: {url}')
+rs = requests.put(url, json=dict(task=f'New task!'))
+print(rs, rs.json())
+"""
+PUT: http://127.0.0.1:5000/todos/todo1_new
+<Response [201]> {'task': 'New task!'}
+"""
+
+print()
+
+url = f'{URL_BASE}/todos'
+print(f'POST: {url}')
+rs = requests.post(url, json=dict(task=f'New task!'))
+print(rs, rs.json())
+"""
+POST: http://127.0.0.1:5000/todos
+<Response [201]> {'task': 'New task!'}
+"""
+
+print()
+
+url = f'{URL_BASE}/todos'
+print(f'GET: {url}')
+rs = requests.get(url)
+print(rs, rs.json())
+"""
+GET: http://127.0.0.1:5000/todos
+<Response [200]> {'todo1': {'task': 'Update task todo1'}, 'todo2': {'task': '?????'}, 'todo3': {'task': 'profit!'}, 'todo1_new': {'task': 'New task!'}, 'todo5': {'task': 'New task!'}}
+"""
+
+print()
+
+url = f'{URL_BASE}/todos/{todo_id}'
+print(f'DELETE: {url}')
+rs = requests.delete(url)
+print(rs, rs.json())
+"""
+DELETE: http://127.0.0.1:5000/todos/todo1
+<Response [200]> {'ok': True}
+"""
+
+print()
+
+url = f'{URL_BASE}/todos'
+print(f'GET: {url}')
+rs = requests.get(url)
+print(rs, rs.json())
+"""
+GET: http://127.0.0.1:5000/todos
+<Response [200]> {'todo2': {'task': '?????'}, 'todo3': {'task': 'profit!'}, 'todo1_new': {'task': 'New task!'}, 'todo5': {'task': 'New task!'}}
+"""
+
+print()
+
+url = URL_BASE
+print(f'GET: {url}')
+rs = requests.get(url)
+print(rs, rs.json())
+"""
+GET: http://127.0.0.1:5000
+<Response [200]> {'total': 4}
+"""
---
+++ flask_restful__examples/todo/main.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# SOURCE: https://flask-restful.readthedocs.io/en/latest/quickstart.html#full-example
+
+
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+TODOS = {
+    'todo1': {'task': 'build an API'},
+    'todo2': {'task': '?????'},
+    'todo3': {'task': 'profit!'},
+}
+
+
+def abort_if_todo_doesnt_exist(todo_id):
+    if todo_id not in TODOS:
+        abort(404, message=f"Todo {todo_id} doesn't exist")
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('task')
+
+
+class Index(Resource):
+    def get(self):
+        return {'total': len(TODOS)}
+
+
+class Todo(Resource):
+    """
+    Shows a single todo item and lets you delete a todo item
+    """
+
+    def get(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        return TODOS[todo_id]
+
+    def delete(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        del TODOS[todo_id]
+        return dict(ok=True)
+
+    def put(self, todo_id):
+        args = parser.parse_args()
+        task = {'task': args['task']}
+        TODOS[todo_id] = task
+        return task, 201
+
+
+class TodoList(Resource):
+    """
+    Shows a list of all todos, and lets you POST to add new tasks
+    """
+
+    def get(self):
+        return TODOS
+
+    def post(self):
+        args = parser.parse_args()
+        todo_id = f'todo{len(TODOS.keys()) + 1}'
+        TODOS[todo_id] = {'task': args['task']}
+        return TODOS[todo_id], 201
+
+
+api.add_resource(Index, '/')
+api.add_resource(TodoList, '/todos')
+api.add_resource(Todo, '/todos/<todo_id>')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
