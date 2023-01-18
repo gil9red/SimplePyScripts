ed37b16e23b7abf7c296ea482aea7d2cff2d56b6
gil9red <ilya.petrash@inbox.ru>
2023-01-19 00:26:40 +0500

    Added "cpu_info/hello_world__cpuinfo.py"

---
+++ cpu_info/hello_world__cpuinfo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+# pip install py-cpuinfo
+import cpuinfo
+
+
+info = cpuinfo.get_cpu_info()
+print(info['brand_raw'])
+# AMD Ryzen 7 3700X 8-Core Processor
