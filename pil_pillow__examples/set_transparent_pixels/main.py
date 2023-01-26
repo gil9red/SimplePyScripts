4cf079eb1e97234808a593b006983ecb633e0597
gil9red <ilya.petrash@inbox.ru>
2023-01-26 13:02:39 +0500

    Added "pil_pillow__examples/set_transparent_pixels"

---
+++ pil_pillow__examples/set_transparent_pixels/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PIL import Image
+
+
+file_name = "../for_cycle__pixels__array/input.jpg"
+
+img = Image.open(file_name).convert('RGBA')
+pixels = img.load()
+width, height = img.size
+
+for i in range(width):
+    for j in range(height):
+        if (j + i) % 2 == 0:
+            pixels[i, j] = 0, 0, 0, 0
+
+img.save("new.png")
