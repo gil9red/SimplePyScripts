c4e5acd8588863da922953536719985206035816
gil9red <ilya.petrash@inbox.ru>
2023-02-17 13:42:08 +0500

    Added "qt__pyqt__pyside__pyqode/PyQt6_screens_size.py"

---
+++ qt__pyqt__pyside__pyqode/PyQt6_screens_size.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from PyQt6.QtWidgets import QApplication
+from PyQt6.QtGui import QGuiApplication
+
+
+app = QApplication([])
+for screen in QApplication.screens():
+    print(screen.geometry(), screen.size())
+"""
+PyQt6.QtCore.QRect(0, 0, 1920, 1080) PyQt6.QtCore.QSize(1920, 1080)
+PyQt6.QtCore.QRect(-3840, 0, 1536, 864) PyQt6.QtCore.QSize(1536, 864)
+PyQt6.QtCore.QRect(-1920, 0, 1920, 1080) PyQt6.QtCore.QSize(1920, 1080)
+"""
+
+app = None
+print()
+
+app = QGuiApplication([])
+for screen in QGuiApplication.screens():
+    print(screen.geometry(), screen.size())
+"""
+PyQt6.QtCore.QRect(0, 0, 1920, 1080) PyQt6.QtCore.QSize(1920, 1080)
+PyQt6.QtCore.QRect(-3840, 0, 1536, 864) PyQt6.QtCore.QSize(1536, 864)
+PyQt6.QtCore.QRect(-1920, 0, 1920, 1080) PyQt6.QtCore.QSize(1920, 1080)
+"""
+
