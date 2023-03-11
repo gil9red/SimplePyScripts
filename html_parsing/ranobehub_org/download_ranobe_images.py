ce08bfe8c93c38006429198445e97694f5bcd36b
gil9red <ilya.petrash@inbox.ru>
2023-03-11 23:58:57 +0500

    Added "html_parsing/ranobehub_org/download_ranobe_images.py"

---
+++ html_parsing/ranobehub_org/download_ranobe_images.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import re
+import time
+
+from pathlib import Path
+from urllib.parse import urljoin
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_valid_filename(s: str) -> str:
+    s = s.strip().replace(':', '.')
+    return re.sub(r'(?u)[^-\w. ]', '', s)
+
+
+DIR = Path(__file__).resolve().parent
+
+session = requests.Session()
+session.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0'
+
+
+def parse(start_url: str, download_path: Path = DIR):
+    url = start_url
+
+    while True:
+        rs = session.get(url)
+        soup = BeautifulSoup(rs.content, 'html.parser')
+
+        title = soup.select_one('title').get_text(strip=True)
+
+        # Example: 'Да будет благословен этот прекрасный мир! / Том 666 / Веб-новелла: Короткая история богини'
+        #       -> 'Да будет благословен этот прекрасный мир/Том 666/Веб-новелла Короткая история богини'
+        title = '/'.join(get_valid_filename(el) for el in title.split('/'))
+
+        print(title)
+
+        dir_chapter = download_path / title
+
+        img_urls = []
+        for img_el in soup.select('img[data-media-id]'):
+            media_id = img_el['data-media-id']
+            url = urljoin(rs.url, f'/api/media/{media_id}')
+            if url not in img_urls:
+                img_urls.append(url)
+
+        if img_urls:
+            dir_chapter.mkdir(parents=True, exist_ok=True)
+
+            print(f'    Изображений: {len(img_urls)}')
+
+            for i, url in enumerate(img_urls, 1):
+                rs = session.get(url)
+                time.sleep(0.1)
+
+                img_path = dir_chapter / f'{i}.png'
+                img_path.write_bytes(rs.content)
+
+        next_chapter_link_el = soup.select_one('a[data-next-chapter-link]')
+        if not next_chapter_link_el.get('href'):
+            break
+
+        url = urljoin(rs.url, next_chapter_link_el['href'])
+
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    url = 'https://ranobehub.org/ranobe/131/1/1'
+    parse(url)
