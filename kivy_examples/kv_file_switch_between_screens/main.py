105f027e133679a66976117acf70cf1f6109b18c
gil9red <ilya.petrash@inbox.ru>
2023-03-24 12:31:06 +0500

    Added "kivy_examples/kv_file_switch_between_screens/*"

---
+++ kivy_examples/kv_file_switch_between_screens/main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+from kivy.app import App
+from kivy.uix.screenmanager import Screen
+
+
+class EegScreen(Screen):
+    pass
+
+
+class SettingsScreen(Screen):
+    pass
+
+
+class ExchangeScreen(Screen):
+    pass
+
+
+class ConnectionsScreen(Screen):
+    pass
+
+
+class MyApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    MyApp().run()
