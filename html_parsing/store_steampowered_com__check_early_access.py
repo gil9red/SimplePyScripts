9cfb38bcf1ef590bd867b8965231dd1f02a7b418
gil9red <ilya.petrash@inbox.ru>
2023-03-27 21:08:47 +0500

    Added "html_parsing/store_steampowered_com__check_early_access.py"

---
+++ html_parsing/store_steampowered_com__check_early_access.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+import requests
+
+
+session = requests.Session()
+session.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0'
+
+
+def is_early_access(app_id_or_url: int | str) -> bool:
+    if isinstance(app_id_or_url, str):
+        url = app_id_or_url
+    else:
+        app_id = app_id_or_url
+        url = f'https://store.steampowered.com/app/{app_id}/'
+
+    rs = session.get(url)
+    return 'id="earlyAccess' in rs.text
+
+
+if __name__ == '__main__':
+    # ULTRAKILL
+    print(is_early_access(1229490))
+    # True
+
+    print(is_early_access('https://store.steampowered.com/app/1229490/ULTRAKILL/'))
+    # True
