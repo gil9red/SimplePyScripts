5dd6fb52cf30825bc7a7982d6f23246f39e38cc8
gil9red <ilya.petrash@inbox.ru>
2023-04-17 16:42:33 +0500

    Refactoring

--- AnalysisOfChatLog/AnalysisOfChatLog.py
+++ AnalysisOfChatLog/AnalysisOfChatLog.py
@@ -1,25 +1,27 @@
 # coding=utf-8
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 # Есть лог-файл какого-то чата. Посчитать «разговорчивость» пользователей в нем в виде ник — количество фраз.
 # Посчитать среднее число букв на участника чата.
 
-if __name__ == '__main__':
-    #file_name = raw_input("Chat log file: ")
-    file_name = "C:\Users\ipetrash\Desktop\chat log.txt"
-    user_count = {}
-    for line in file(file_name):
-        line = line.decode("windows-1251").replace("\n", "")
-        user, count = line.split(":")[0], len(line.split(":")[1].split(' ')) - 1
-        if user in user_count:
-            user_count[user] += count
-        else:
-            user_count[user] = count
 
-    sum = 0.0
-    for user, count in user_count.items():
-        sum += count
-        print(user + " = %d" % count)
+file_name = r"chat log.txt"
 
-    print("Average = %s" % (sum / len(user_count)) )
+user_count: dict[str, int] = dict()
+for line in open(file_name, encoding="utf-8"):
+    line = line.strip()
+
+    user, text = line.split(":", maxsplit=1)
+    count = len(text.strip().split(" ")) - 1
+    if user in user_count:
+        user_count[user] += count
+    else:
+        user_count[user] = count
+
+total = 0
+for user, count in user_count.items():
+    total += count
+    print(f"{user} = {count}")
+
+print(f"Average = {total // len(user_count)}")
