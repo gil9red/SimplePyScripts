b3b3f1a4ac49d507903dd6edb98995e01506f422
gil9red <ilya.petrash@inbox.ru>
2023-04-20 18:30:56 +0500

    Refactoring. Using black code style

--- alpha2_to_country/main.py
+++ alpha2_to_country/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # SOURCE: https://github.com/gil9red/SimplePyScripts/blob/f2c11477ae4410e5a9d47272b72cb69354c271b8/html_parsing/ru_wikipedia_org__wiki__ISO_3166_1.py
@@ -14,7 +14,7 @@ from bs4 import BeautifulSoup
 import requests
 
 
-FILE_NAME_COUNTRY = Path(__file__).resolve().parent / 'alpha2_to_country.json'
+FILE_NAME_COUNTRY = Path(__file__).resolve().parent / "alpha2_to_country.json"
 ALPHA2_TO_COUNTRY = None
 
 
@@ -23,17 +23,17 @@ def init():
 
     if FILE_NAME_COUNTRY.exists():
         ALPHA2_TO_COUNTRY = json.loads(
-            FILE_NAME_COUNTRY.read_text('utf-8')
+            FILE_NAME_COUNTRY.read_text("utf-8")
         )
         return
 
-    rs = requests.get('https://ru.wikipedia.org/wiki/ISO_3166-1')
-    root = BeautifulSoup(rs.content, 'html.parser')
+    rs = requests.get("https://ru.wikipedia.org/wiki/ISO_3166-1")
+    root = BeautifulSoup(rs.content, "html.parser")
 
     ALPHA2_TO_COUNTRY = dict()
 
-    for tr in root.select_one('.wikitable').select('tr'):
-        td_list = tr.select('td')
+    for tr in root.select_one(".wikitable").select("tr"):
+        td_list = tr.select("td")
         if not td_list:
             continue
 
@@ -45,9 +45,9 @@ def init():
 
     json.dump(
         ALPHA2_TO_COUNTRY,
-        open(FILE_NAME_COUNTRY, 'w', encoding='utf-8'),
+        open(FILE_NAME_COUNTRY, "w", encoding="utf-8"),
         ensure_ascii=False,
-        indent=4
+        indent=4,
     )
 
 
@@ -58,12 +58,12 @@ def get_country(alpha2: str) -> str:
     return ALPHA2_TO_COUNTRY[alpha2]
 
 
-if __name__ == '__main__':
-    print(get_country('RU'))
+if __name__ == "__main__":
+    print(get_country("RU"))
     # Россия
 
-    print(get_country('US'))
+    print(get_country("US"))
     # США
 
-    print(get_country('DE'))
+    print(get_country("DE"))
     # Германия
