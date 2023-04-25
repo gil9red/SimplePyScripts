ae2d7a453ab3f42f7b98df59fa4571b51f14be3a
gil9red <ilya.petrash@inbox.ru>
2023-04-25 15:45:18 +0500

    Refactoring. Using black code style

--- build_exe/pyinstaller_example/console/hello_world/main.py
+++ build_exe/pyinstaller_example/console/hello_world/main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import os
     os.system("echo Hello World!")
--- build_exe/pyinstaller_example/console/print_current_script_dir/main.py
+++ build_exe/pyinstaller_example/console/print_current_script_dir/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # FROM: https://github.com/gil9red/SimplePyScripts/blob/927af3d3f05a0129338b21d074ca241c8a055118/get_current_script_dir.py
@@ -14,7 +14,7 @@ import sys
 
 def get_current_script_dir(follow_symlinks=True, normcase=False) -> str:
     # py2exe, PyInstaller, cx_Freeze
-    if getattr(sys, 'frozen', False):
+    if getattr(sys, "frozen", False):
         path = os.path.abspath(sys.executable)
     else:
         # Analog inspect.getabsfile without os.path.normcase
@@ -30,5 +30,5 @@ def get_current_script_dir(follow_symlinks=True, normcase=False) -> str:
     return os.path.dirname(path)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(get_current_script_dir())
