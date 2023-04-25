e3dd6278c7e973b9694ee1950a55c2afdf18469c
gil9red <ilya.petrash@inbox.ru>
2023-04-25 12:20:50 +0500

    Refactoring. Using black code style

--- bs4__BeautifulSoup__examples/about_builders.py
+++ bs4__BeautifulSoup__examples/about_builders.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # pip install lxml
@@ -13,20 +13,20 @@ from bs4 import BeautifulSoup
 
 text = "<xml/>"
 
-root = BeautifulSoup(text, 'html.parser')
+root = BeautifulSoup(text, "html.parser")
 print(root.builder)  # bs4.builder._htmlparser.HTMLParserTreeBuilder
 
-root = BeautifulSoup(text, 'html5lib')
+root = BeautifulSoup(text, "html5lib")
 print(root.builder)  # bs4.builder._html5lib.HTML5TreeBuilder
 
-root = BeautifulSoup(text, 'xml')
+root = BeautifulSoup(text, "xml")
 print(root.builder)  # bs4.builder._lxml.LXMLTreeBuilderForXML
 
-root = BeautifulSoup(text, 'lxml-xml')
+root = BeautifulSoup(text, "lxml-xml")
 print(root.builder)  # bs4.builder._lxml.LXMLTreeBuilderForXML
 
-root = BeautifulSoup(text, 'lxml')
+root = BeautifulSoup(text, "lxml")
 print(root.builder)  # bs4.builder._lxml.LXMLTreeBuilder
 
-root = BeautifulSoup(text, 'lxml-html')
+root = BeautifulSoup(text, "lxml-html")
 print(root.builder)  # bs4.builder._lxml.LXMLTreeBuilder
