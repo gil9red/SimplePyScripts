23fd43a6e51428d89bef513695c11515ec9e6517
gil9red <ilya.petrash@inbox.ru>
2023-04-25 16:01:08 +0500

    Refactoring. Using black code style

--- calc_statistic_anime_Naruto__by_fillers/main.py
+++ calc_statistic_anime_Naruto__by_fillers/main.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
-def get_html_by_url__from_cache(url, cache_dir='cache'):
-    import os
+import os
+import re
+
+import requests
+
+
+def get_html_by_url__from_cache(url, cache_dir="cache"):
     os.makedirs(cache_dir, exist_ok=True)
 
     file_name = os.path.basename(url)
+    file_name = re.sub(r"\W", "_", file_name)
 
-    import re
-    file_name = re.sub(r'[^\w\d]', '_', file_name)
-
-    file_name = cache_dir + '/' + file_name + '.html'
+    file_name = cache_dir + "/" + file_name + ".html"
 
     if os.path.exists(file_name):
-        with open(file_name, 'rb') as f:
+        with open(file_name, "rb") as f:
             return f.read()
 
-    with open(file_name, 'wb') as f:
-        import requests
+    with open(file_name, "wb") as f:
         rs = requests.get(url)
         f.write(rs.content)
 
@@ -30,9 +32,9 @@ def get_html_by_url__from_cache(url, cache_dir='cache'):
 def get_ep_chapters_s1():
     def get_urls_of_season():
         return [
-            'https://ru.wikipedia.org/wiki/Список_серий_«Наруто»_(сезоны_1—3)',
-            'https://ru.wikipedia.org/wiki/Список_серий_«Наруто»_(сезоны_4—6)',
-            'https://ru.wikipedia.org/wiki/Список_серий_«Наруто»_(сезоны_7—9)',
+            "https://ru.wikipedia.org/wiki/Список_серий_«Наруто»_(сезоны_1—3)",
+            "https://ru.wikipedia.org/wiki/Список_серий_«Наруто»_(сезоны_4—6)",
+            "https://ru.wikipedia.org/wiki/Список_серий_«Наруто»_(сезоны_7—9)",
         ]
 
     from bs4 import BeautifulSoup
@@ -42,10 +44,10 @@ def get_ep_chapters_s1():
     for url in get_urls_of_season():
         html_content = get_html_by_url__from_cache(url)
 
-        root = BeautifulSoup(html_content, 'html.parser')
+        root = BeautifulSoup(html_content, "html.parser")
 
-        for td in root.select('td'):
-            if td.has_attr('id') and td['id'].startswith('ep'):
+        for td in root.select("td"):
+            if td.has_attr("id") and td["id"].startswith("ep"):
                 episode = td.text.strip()
                 manga_chapters = td.next_sibling.next_sibling.text.strip()
 
@@ -57,13 +59,13 @@ def get_ep_chapters_s1():
 def get_ep_chapters_s2():
     def get_urls_of_season():
         return [
-            'https://ru.wikipedia.org/wiki/Список_серий_«Наруто:_Ураганные_хроники»_(сезоны_1—4)',
-            'https://ru.wikipedia.org/wiki/Список_серий_«Наруто:_Ураганные_хроники»_(сезоны_5—8)',
-            'https://ru.wikipedia.org/wiki/Список_серий_«Наруто:_Ураганные_хроники»_(сезоны_9—12)',
-            'https://ru.wikipedia.org/wiki/Список_серий_«Наруто:_Ураганные_хроники»_(сезоны_13—16)',
-            'https://ru.wikipedia.org/wiki/Список_серий_«Наруто:_Ураганные_хроники»_(сезоны_17—20)',
-            'https://ru.wikipedia.org/wiki/Список_серий_«Наруто:_Ураганные_хроники»_(сезоны_21—24)',
-            'https://ru.wikipedia.org/wiki/Список_серий_«Наруто:_Ураганные_хроники»_(сезоны_25—28)',
+            "https://ru.wikipedia.org/wiki/Список_серий_«Наруто:_Ураганные_хроники»_(сезоны_1—4)",
+            "https://ru.wikipedia.org/wiki/Список_серий_«Наруто:_Ураганные_хроники»_(сезоны_5—8)",
+            "https://ru.wikipedia.org/wiki/Список_серий_«Наруто:_Ураганные_хроники»_(сезоны_9—12)",
+            "https://ru.wikipedia.org/wiki/Список_серий_«Наруто:_Ураганные_хроники»_(сезоны_13—16)",
+            "https://ru.wikipedia.org/wiki/Список_серий_«Наруто:_Ураганные_хроники»_(сезоны_17—20)",
+            "https://ru.wikipedia.org/wiki/Список_серий_«Наруто:_Ураганные_хроники»_(сезоны_21—24)",
+            "https://ru.wikipedia.org/wiki/Список_серий_«Наруто:_Ураганные_хроники»_(сезоны_25—28)",
         ]
 
     from bs4 import BeautifulSoup
@@ -73,10 +75,10 @@ def get_ep_chapters_s2():
     for url in get_urls_of_season():
         html_content = get_html_by_url__from_cache(url)
 
-        root = BeautifulSoup(html_content, 'html.parser')
+        root = BeautifulSoup(html_content, "html.parser")
 
-        for td in root.select('td'):
-            if td.has_attr('id') and td['id'].startswith('ep'):
+        for td in root.select("td"):
+            if td.has_attr("id") and td["id"].startswith("ep"):
                 episode = td.text.strip()
                 manga_chapters = td.next_sibling.next_sibling.next_sibling.text.strip()
 
@@ -87,28 +89,40 @@ def get_ep_chapters_s2():
 
 def calc_statistic(ep_chapters: list) -> (int, int, int, int):
     number_all_ep = len(ep_chapters)
-    number_filler_ep = len([x for x in ep_chapters if 'Нет (филлер)' in x[1]])
+    number_filler_ep = len([x for x in ep_chapters if "Нет (филлер)" in x[1]])
     number_manga_ep = number_all_ep - number_filler_ep
     precent_filler = number_filler_ep / number_all_ep * 100
 
     return number_all_ep, number_manga_ep, number_filler_ep, precent_filler
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     ep_chapters_s1 = get_ep_chapters_s1()
-    number_all_ep_s1, _, number_filler_ep_s1, precent_filler_s1 = calc_statistic(ep_chapters_s1)
-    print(f'Season 1.\n'
-          f'    Total: {number_all_ep_s1}, fillers: {number_filler_ep_s1} ({precent_filler_s1:.1f}%)\n')
+    number_all_ep_s1, _, number_filler_ep_s1, precent_filler_s1 = calc_statistic(
+        ep_chapters_s1
+    )
+    print(
+        f"Season 1.\n"
+        f"    Total: {number_all_ep_s1}, fillers: {number_filler_ep_s1} ({precent_filler_s1:.1f}%)\n"
+    )
 
     ep_chapters_s2 = get_ep_chapters_s2()
-    number_all_ep_s2, _, number_filler_ep_s2, precent_filler_s2 = calc_statistic(ep_chapters_s2)
-    print(f'Season 2.\n'
-          f'    Total: {number_all_ep_s2}, fillers: {number_filler_ep_s2} ({precent_filler_s2:.1f}%)\n')
+    number_all_ep_s2, _, number_filler_ep_s2, precent_filler_s2 = calc_statistic(
+        ep_chapters_s2
+    )
+    print(
+        f"Season 2.\n"
+        f"    Total: {number_all_ep_s2}, fillers: {number_filler_ep_s2} ({precent_filler_s2:.1f}%)\n"
+    )
 
     ep_chapters_s1_2 = ep_chapters_s1 + ep_chapters_s2
-    number_all_ep_s1_2, _, number_filler_ep_s1_2, precent_filler_s1_2 = calc_statistic(ep_chapters_s1_2)
-    print(f'Season 1+2.\n'
-          f'    Total: {number_all_ep_s1_2}, fillers: {number_filler_ep_s1_2} ({precent_filler_s1_2:.1f}%)\n')
+    number_all_ep_s1_2, _, number_filler_ep_s1_2, precent_filler_s1_2 = calc_statistic(
+        ep_chapters_s1_2
+    )
+    print(
+        f"Season 1+2.\n"
+        f"    Total: {number_all_ep_s1_2}, fillers: {number_filler_ep_s1_2} ({precent_filler_s1_2:.1f}%)\n"
+    )
 
 # Season 1.
 #     Total: 220, fillers: 93 (42.3%)
