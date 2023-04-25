dcbb3ea83952771d6047965a3208806aaebbe73d
gil9red <ilya.petrash@inbox.ru>
2023-04-25 16:16:19 +0500

    Refactoring. Using black code style

--- calculator/use__numexpr__module/main.py
+++ calculator/use__numexpr__module/main.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import numexpr
-print(numexpr.evaluate('2 + 2 * 2'))
-print(numexpr.evaluate('10 ** 3'))
-print(numexpr.evaluate('2 ** 10'))
-print(numexpr.evaluate('sin(2 ** 10)'))
-print(numexpr.evaluate('(0xFF + 255) / 0b1010'))
-print(numexpr.evaluate('(0xFF + 255) // 0b1010'))
+
+
+print(numexpr.evaluate("2 + 2 * 2"))
+print(numexpr.evaluate("10 ** 3"))
+print(numexpr.evaluate("2 ** 10"))
+print(numexpr.evaluate("sin(2 ** 10)"))
+print(numexpr.evaluate("(0xFF + 255) / 0b1010"))
+print(numexpr.evaluate("(0xFF + 255) // 0b1010"))
