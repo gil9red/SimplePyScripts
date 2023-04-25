b559e2217496ac188bbdac93ab854d4b7cfb82e8
gil9red <ilya.petrash@inbox.ru>
2023-04-25 12:19:48 +0500

    Refactoring. Using black code style

--- brute_force__examples/multiprocess.py
+++ brute_force__examples/multiprocess.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # SOURCE: https://ru.stackoverflow.com/a/776977/201445
@@ -11,9 +11,11 @@ import binascii
 import hashlib
 import itertools
 import multiprocessing
+import string
+
 from functools import partial
 
-import string
+
 alphabet = (string.ascii_letters + string.digits).encode()
 
 
@@ -35,12 +37,14 @@ def brute_force(mask, target_sha256, n_cutoff=4):
 
     target_sha256 = binascii.unhexlify(target_sha256)
 
-    bytes_format = mask.replace(b'%', b'%%').replace(b'*', b'%c')
-    mp_check = partial(check_sha256,
-                       bytes_format=bytes_format,
-                       n=min(n_cutoff, mask.count(b'*')),
-                       target_sha256=target_sha256)
-    n = max(0, mask.count(b'*') - n_cutoff)
+    bytes_format = mask.replace(b"%", b"%%").replace(b"*", b"%c")
+    mp_check = partial(
+        check_sha256,
+        bytes_format=bytes_format,
+        n=min(n_cutoff, mask.count(b"*")),
+        target_sha256=target_sha256,
+    )
+    n = max(0, mask.count(b"*") - n_cutoff)
     all_repls_parent = itertools.product(alphabet, repeat=n)
     with multiprocessing.Pool() as pool:
         for data in pool.imap_unordered(mp_check, all_repls_parent):
@@ -48,9 +52,9 @@ def brute_force(mask, target_sha256, n_cutoff=4):
                 return data
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Password: qwe12y
-    sha256_hex = b'b8cc184b3f4aeb6adb6601ea3e39ef5ac098791acc2a505a66746f43d0c7ed85'
+    sha256_hex = b"b8cc184b3f4aeb6adb6601ea3e39ef5ac098791acc2a505a66746f43d0c7ed85"
 
-    passw_bytes = brute_force(b'qw****', sha256_hex)
+    passw_bytes = brute_force(b"qw****", sha256_hex)
     print(passw_bytes.decode())
--- brute_force__examples/single.py
+++ brute_force__examples/single.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # SOURCE: https://ru.stackoverflow.com/a/776780/201445
@@ -13,28 +13,28 @@ from itertools import product
 
 def brute_force(mask, hsh, alphabet):
     # заменяем '*' на '{}' для последующей подстановки в 'str.format()'
-    pwd_pat = mask.replace('*', '{}')
+    pwd_pat = mask.replace("*", "{}")
 
     # число звездочек - будем использовать в качестве `product(.., repeat)`
-    N = mask.count('*')
+    N = mask.count("*")
     i = 0
     for chars in product(alphabet, repeat=N):
         i += 1
         if i % 10000 == 0:
-            print('Iterations: {}'.format(i))
+            print(f"Iterations: {i}")
 
         if hsh == hashlib.sha256(pwd_pat.format(*chars).encode()).hexdigest():
             text = pwd_pat.format(*chars)
-            print('Found: ' + text)
+            print(f"Found: {text}")
             return text
 
     return None
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from string import ascii_letters, digits
     alphabet = ascii_letters + digits
 
     # Password: qwe12y
-    hsh = 'b8cc184b3f4aeb6adb6601ea3e39ef5ac098791acc2a505a66746f43d0c7ed85'
-    print(brute_force('qw****', hsh, alphabet))
+    hsh = "b8cc184b3f4aeb6adb6601ea3e39ef5ac098791acc2a505a66746f43d0c7ed85"
+    print(brute_force("qw****", hsh, alphabet))
