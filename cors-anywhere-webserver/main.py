c3bde0014bf8afbcace2206cb0b6a7a77bf79605
gil9red <ilya.petrash@inbox.ru>
2023-04-26 15:11:08 +0500

    Refactoring. Using black code style

--- cors-anywhere-webserver/main.py
+++ cors-anywhere-webserver/main.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
+
+
+import re
+from urllib.request import urlopen, Request
+
+import flask
 
 
 # TODO: append logging
-
-import flask
 app = flask.Flask(__name__)
 app.debug = True
 
-from urllib.request import urlopen, Request
 
-
-@app.route('/')
+@app.route("/")
 def index():
-    url = flask.request.args.get('url')
-    print('URL:', url)
-    if url is None:
-        return "Append url, please: {}?url=&lt;your_url&gt;".format(flask.request.host_url)
+    url = flask.request.args.get("url")
+    print("URL:", url)
+    if not url:
+        return f"Append url, please: {flask.request.host_url}?url=&lt;your_url&gt;"
 
     headers = dict()
-    headers['Origin'] = flask.request.host_url
+    headers["Origin"] = flask.request.host_url
 
     request = Request(url, headers=headers)
 
@@ -31,12 +33,11 @@ def index():
 
         # Нужно узнать encoding, для этого вытаскиваем xml-декларацию, а из нее уже значение encoding
         try:
-            s_index = content.find(b'<?')
-            e_index = content.find(b'?>')
+            s_index = content.find(b"<?")
+            e_index = content.find(b"?>")
             if s_index != -1 and e_index != -1:
-                declaration = content[s_index: e_index + len(b'?>')].decode('utf-8')
+                declaration = content[s_index : e_index + len(b"?>")].decode("utf-8")
 
-                import re
                 match = re.search(r'encoding="(.+)"', declaration)
                 if match:
                     print(match.group(1))
@@ -49,7 +50,7 @@ def index():
 
         rs = flask.Response(content)
         rs.headers.extend(headers)
-        rs.headers['Access-Control-Allow-Origin'] = '*'
+        rs.headers["Access-Control-Allow-Origin"] = "*"
         print(rs.headers)
         return rs
 
