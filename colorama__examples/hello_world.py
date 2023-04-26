ed9796fbaaa48ab982615fe3db6a860318debfb7
gil9red <ilya.petrash@inbox.ru>
2023-04-26 13:09:47 +0500

    Refactoring. Using black code style

--- colorama__examples/hello_world.py
+++ colorama__examples/hello_world.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # pip install colorama
 from colorama import Fore, Back, Style
 
 
-print(Fore.RED + 'some red text')
-print(Back.GREEN + 'and with a green background')
-print(Style.DIM + 'and in dim text')
+print(Fore.RED + "some red text")
+print(Back.GREEN + "and with a green background")
+print(Style.DIM + "and in dim text")
 print(Style.RESET_ALL)
-print('back to normal now')
+print("back to normal now")
