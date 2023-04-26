6ab6ea8754b0e0daf077a80cf72077ab1866a04c
gil9red <ilya.petrash@inbox.ru>
2023-04-26 18:01:10 +0500

    Refactoring. Using black code style

--- create_thumbnail/main.py
+++ create_thumbnail/main.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
-SIZE_THUMBS = (75, 75)
-FROM_DIR = 'images'
-TO_DIR = 'thumbs'
+import glob
+import os
+
+from PIL import Image
 
 
-if __name__ == '__main__':
-    import os
-    if not os.path.exists(TO_DIR):
-        os.mkdir(TO_DIR)
-
-    import glob
-    for filename in glob.glob(FROM_DIR + "/*.jpg"):
-        filename_thumbnail = os.path.join(TO_DIR, os.path.split(filename)[1])
-        print('{} -> {}'.format(filename, filename_thumbnail))
-
-        from PIL import Image
-        im = Image.open(filename)
-        im.thumbnail(SIZE_THUMBS)
-        im.save(filename_thumbnail)
+SIZE_THUMBS = 75, 75
+FROM_DIR = "images"
+TO_DIR = "thumbs"
 
 
+if not os.path.exists(TO_DIR):
+    os.mkdir(TO_DIR)
+
+for filename in glob.glob(FROM_DIR + "/*.jpg"):
+    filename_thumbnail = os.path.join(TO_DIR, os.path.split(filename)[1])
+    print(f"{filename} -> {filename_thumbnail}")
+
+    im = Image.open(filename)
+    im.thumbnail(SIZE_THUMBS)
+    im.save(filename_thumbnail)
