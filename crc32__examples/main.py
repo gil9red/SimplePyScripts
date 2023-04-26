7a9113e70f1e387160af71090bc1d6a356bdf77c
gil9red <ilya.petrash@inbox.ru>
2023-04-26 17:58:29 +0500

    Refactoring. Using black code style

--- crc32__examples/main.py
+++ crc32__examples/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import zlib
@@ -16,15 +16,15 @@ def crc32_hex_from_bytes(data: bytes) -> str:
 
 
 def crc32_hex_from_file(filename):
-    with open(filename, 'rb') as f:
+    with open(filename, "rb") as f:
         buf = f.read()
 
     return crc32_hex_from_bytes(buf)
 
 
-if __name__ == '__main__':
-    print(crc32_from_bytes(b'hello-world'))  # 2983461467
-    print(crc32_hex_from_bytes(b'hello-world'))  # B1D4025B
+if __name__ == "__main__":
+    print(crc32_from_bytes(b"hello-world"))  # 2983461467
+    print(crc32_hex_from_bytes(b"hello-world"))  # B1D4025B
     print()
-    print(crc32_hex_from_file('1.csv'))  # 7EB0F2B2
-    print(crc32_hex_from_file('2.csv'))  # 5D884C77
+    print(crc32_hex_from_file("1.csv"))  # 7EB0F2B2
+    print(crc32_hex_from_file("2.csv"))  # 5D884C77
