5d3afb2f885d025bcca4c5c32cf3625ac36b6e9d
gil9red <ilya.petrash@inbox.ru>
2023-04-26 14:11:36 +0500

    Refactoring. Using black code style

--- convert_image_to_ico/main.py
+++ convert_image_to_ico/main.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
+
+
+from PIL import Image
 
 
 def convert_image_to_ico(file_name, file_name_ico, icon_sizes=None):
-    from PIL import Image
     img = Image.open(file_name)
 
     if icon_sizes:
@@ -14,9 +16,13 @@ def convert_image_to_ico(file_name, file_name_ico, icon_sizes=None):
         img.save(file_name_ico)
 
 
-if __name__ == '__main__':
-    file_name = 'image.jpg'
+if __name__ == "__main__":
+    file_name = "image.jpg"
 
-    convert_image_to_ico(file_name, 'logo.ico')
-    convert_image_to_ico(file_name, 'logo2.ico', icon_sizes=[(16, 16), (32, 32), (48, 48), (64, 64)])
-    convert_image_to_ico(file_name, 'logo3.ico', icon_sizes=[(16, 16), (32, 32)])
+    convert_image_to_ico(file_name, "logo.ico")
+    convert_image_to_ico(
+        file_name,
+        "logo2.ico",
+        icon_sizes=[(16, 16), (32, 32), (48, 48), (64, 64)],
+    )
+    convert_image_to_ico(file_name, "logo3.ico", icon_sizes=[(16, 16), (32, 32)])
