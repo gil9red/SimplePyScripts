67de0a30ae00d26c158660241367cbf2ae4d2331
gil9red <ilya.petrash@inbox.ru>
2023-04-28 12:58:26 +0500

    Refactoring. Using black code style

--- Dark Souls/ru_darksouls_wikia_com__fandom__bosses/main.py
+++ Dark Souls/ru_darksouls_wikia_com__fandom__bosses/main.py
@@ -1,60 +1,84 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import sqlite3
 
 from config import URL_DS1, URL_DS2, URL_DS3
 from utils import (
-    get_bosses, print_bosses, convert_bosses_to_only_name,
-    export_to_json, export_to_sqlite,
+    get_bosses,
+    print_bosses,
+    convert_bosses_to_only_name,
+    export_to_json,
+    export_to_sqlite,
 )
 
 
 bosses_ds1 = get_bosses(URL_DS1)
 print_bosses(URL_DS1, bosses_ds1)
-export_to_json('dumps/ds1/bosses.json', bosses_ds1)
-export_to_json('dumps/ds1/bosses__only_name.json', convert_bosses_to_only_name(bosses_ds1))
+export_to_json("dumps/ds1/bosses.json", bosses_ds1)
+export_to_json(
+    "dumps/ds1/bosses__only_name.json", convert_bosses_to_only_name(bosses_ds1)
+)
 
 bosses_ds2 = get_bosses(URL_DS2)
 print_bosses(URL_DS2, bosses_ds2)
-export_to_json('dumps/ds2/bosses.json', bosses_ds2)
-export_to_json('dumps/ds2/bosses__only_name.json', convert_bosses_to_only_name(bosses_ds2))
+export_to_json("dumps/ds2/bosses.json", bosses_ds2)
+export_to_json(
+    "dumps/ds2/bosses__only_name.json", convert_bosses_to_only_name(bosses_ds2)
+)
 
 bosses_ds3 = get_bosses(URL_DS3)
 print_bosses(URL_DS3, bosses_ds3)
-export_to_json('dumps/ds3/bosses.json', bosses_ds3)
-export_to_json('dumps/ds3/bosses__only_name.json', convert_bosses_to_only_name(bosses_ds3))
+export_to_json("dumps/ds3/bosses.json", bosses_ds3)
+export_to_json(
+    "dumps/ds3/bosses__only_name.json", convert_bosses_to_only_name(bosses_ds3)
+)
 
 # All bosses
 bosses_ds123 = {
-    'Dark Souls':     bosses_ds1,
-    'Dark Souls II':  bosses_ds2,
-    'Dark Souls III': bosses_ds3,
+    "Dark Souls": bosses_ds1,
+    "Dark Souls II": bosses_ds2,
+    "Dark Souls III": bosses_ds3,
 }
-export_to_json('dumps/bosses_ds123.json', bosses_ds123)
+export_to_json("dumps/bosses_ds123.json", bosses_ds123)
 
 bosses_ds123__only_name = {
-    'Dark Souls':     convert_bosses_to_only_name(bosses_ds1),
-    'Dark Souls II':  convert_bosses_to_only_name(bosses_ds2),
-    'Dark Souls III': convert_bosses_to_only_name(bosses_ds3),
+    "Dark Souls": convert_bosses_to_only_name(bosses_ds1),
+    "Dark Souls II": convert_bosses_to_only_name(bosses_ds2),
+    "Dark Souls III": convert_bosses_to_only_name(bosses_ds3),
 }
-export_to_json('dumps/bosses_ds123__only_name.json', bosses_ds123__only_name)
+export_to_json("dumps/bosses_ds123__only_name.json", bosses_ds123__only_name)
 
 #
 # SQLITE
 #
-sql_file_name = 'dumps/bosses_ds123.sqlite'
+sql_file_name = "dumps/bosses_ds123.sqlite"
 export_to_sqlite(sql_file_name, bosses_ds123)
 
 # TEST
 connect = sqlite3.connect(sql_file_name)
-print('Total boss:',     connect.execute('SELECT count(*) FROM BOSS').fetchone()[0])
-print('Total boss DS1:', connect.execute('SELECT count(*) FROM BOSS WHERE game = "Dark Souls"').fetchone()[0])
-print('Total boss DS2:', connect.execute('SELECT count(*) FROM BOSS WHERE game = "Dark Souls II"').fetchone()[0])
-print('Total boss DS3:', connect.execute('SELECT count(*) FROM BOSS WHERE game = "Dark Souls III"').fetchone()[0])
+print("Total boss:", connect.execute("SELECT count(*) FROM BOSS").fetchone()[0])
+print(
+    "Total boss DS1:",
+    connect.execute(
+        'SELECT count(*) FROM BOSS WHERE game = "Dark Souls"'
+    ).fetchone()[0],
+)
+print(
+    "Total boss DS2:",
+    connect.execute(
+        'SELECT count(*) FROM BOSS WHERE game = "Dark Souls II"'
+    ).fetchone()[0],
+)
+print(
+    "Total boss DS3:",
+    connect.execute(
+        'SELECT count(*) FROM BOSS WHERE game = "Dark Souls III"'
+    ).fetchone()[0],
+)
 # Total boss: 92
 # Total boss DS1: 26
 # Total boss DS2: 41
--- Dark Souls/ru_darksouls_wikia_com__fandom__bosses/utils.py
+++ Dark Souls/ru_darksouls_wikia_com__fandom__bosses/utils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import json
@@ -17,7 +17,9 @@ from bs4 import BeautifulSoup
 
 
 session = requests.Session()
-session.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0'
+session.headers[
+    "User-Agent"
+] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0"
 
 
 class Boss(NamedTuple):
@@ -28,15 +30,15 @@ class Boss(NamedTuple):
         _, root = parse(self.url)
 
         key_by_values = dict()
-        for table in root.select('table.pi-horizontal-group'):
-            el_items = table.select_one('tbody td[data-source]')
+        for table in root.select("table.pi-horizontal-group"):
+            el_items = table.select_one("tbody td[data-source]")
             if not el_items:
                 continue
 
-            data_source = el_items['data-source']
+            data_source = el_items["data-source"]
             items = [
                 x.strip()
-                for x in el_items.get_text(strip=True, separator='\n').split('\n')
+                for x in el_items.get_text(strip=True, separator="\n").split("\n")
             ]
 
             key_by_values[data_source] = items
@@ -46,7 +48,7 @@ class Boss(NamedTuple):
 
 def parse(url: str) -> Tuple[requests.Response, BeautifulSoup]:
     rs = session.get(url)
-    return rs, BeautifulSoup(rs.content, 'html.parser')
+    return rs, BeautifulSoup(rs.content, "html.parser")
 
 
 def get_bosses(url: str) -> Dict[str, List[Boss]]:
@@ -56,9 +58,9 @@ def get_bosses(url: str) -> Dict[str, List[Boss]]:
 
     category_name = None
 
-    for tr in root.select('table tr'):
+    for tr in root.select("table tr"):
         # Заголовок первым идет
-        th = tr.select_one('th')
+        th = tr.select_one("th")
         if th:
             category_name = th.text.strip().upper()
             continue
@@ -68,12 +70,12 @@ def get_bosses(url: str) -> Dict[str, List[Boss]]:
 
         td_list = []
 
-        for td in tr.select('td'):
+        for td in tr.select("td"):
             name = td.text.strip()
             if not name:
                 continue
 
-            url = urljoin(rs.url, td.select_one('a')['href'])
+            url = urljoin(rs.url, td.select_one("a")["href"])
             boss = Boss(name, url)
 
             td_list.append(boss)
@@ -84,10 +86,10 @@ def get_bosses(url: str) -> Dict[str, List[Boss]]:
 
 
 def print_bosses(url: str, bosses: Dict[str, List[Boss]]):
-    print('{} ({}):'.format(url, sum(len(i) for i in bosses.values())))
+    print("{} ({}):".format(url, sum(len(i) for i in bosses.values())))
 
     for category, bosses in bosses.items():
-        print(f'{category} ({len(bosses)}):')
+        print(f"{category} ({len(bosses)}):")
 
         for i, boss in enumerate(bosses, 1):
             print(f'    {i}. "{boss.name}": {boss.url}')
@@ -109,7 +111,12 @@ def export_to_json(file_name, bosses):
     dir_name = os.path.dirname(file_name)
     os.makedirs(dir_name, exist_ok=True)
 
-    json.dump(bosses, open(file_name, 'w', encoding='utf-8'), ensure_ascii=False, indent=4)
+    json.dump(
+        bosses,
+        open(file_name, mode="w", encoding="utf-8"),
+        ensure_ascii=False,
+        indent=4,
+    )
 
 
 def export_to_sqlite(file_name: str, bosses_ds123: Dict[str, Dict[str, List[Boss]]]):
@@ -118,7 +125,7 @@ def export_to_sqlite(file_name: str, bosses_ds123: Dict[str, Dict[str, List[Boss
 
     connect = sqlite3.connect(file_name)
 
-    connect.executescript('''
+    connect.executescript("""
         DROP TABLE IF EXISTS Boss;
 
         CREATE TABLE Boss (
@@ -128,14 +135,14 @@ def export_to_sqlite(file_name: str, bosses_ds123: Dict[str, Dict[str, List[Boss
             name TEXT,
             url TEXT
         );
-    ''')
+    """)
 
     for game, categories in bosses_ds123.items():
         for category, bosses in categories.items():
             for boss in bosses:
                 connect.execute(
-                    'INSERT INTO Boss (game, category, name, url) VALUES (?, ?, ?, ?)',
-                    (game, category, boss.name, boss.url)
+                    "INSERT INTO Boss (game, category, name, url) VALUES (?, ?, ?, ?)",
+                    (game, category, boss.name, boss.url),
                 )
 
     connect.commit()
