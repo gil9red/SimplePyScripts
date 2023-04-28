e02811afa1a7a8cb48f74247a1090ecd72074e34
gil9red <ilya.petrash@inbox.ru>
2023-04-28 12:37:10 +0500

    Refactoring. Using black code style

--- Dark Souls/print__stats_from_save_file__using__dsfp/main.py
+++ Dark Souls/print__stats_from_save_file__using__dsfp/main.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # SOURCE: https://github.com/tarvitz/dsfp
+import dsfp
 
 
 # C:\Users\<CURRENT_USER>\Documents\NBGI\DarkSouls\<PLAYER_NAME>\DRAKS0005.sl2
-file_name = '../print__deaths_from_save_file/DRAKS0005.sl2'
+file_name = "../print__deaths_from_save_file/DRAKS0005.sl2"
 
-import dsfp
 ds = dsfp.DSSaveFileParser(file_name)
 
 stats_list = ds.get_stats()
-print(f'Stats list ({len(stats_list)}): {stats_list}')
+print(f"Stats list ({len(stats_list)}): {stats_list}")
 print()
 
-print(f'Character ({len(stats_list)}):')
+print(f"Character ({len(stats_list)}):")
 
 for stats in stats_list:
-    name, deaths = stats['name'], stats['deaths']
-    print('    {}, deaths: {}'.format(name, deaths))
+    name, deaths = stats["name"], stats["deaths"]
+    print(f"    {name}, deaths: {deaths}")
