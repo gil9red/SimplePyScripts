cd0a793f348f660078110a66d757889ab6933e14
gil9red <ilya.petrash@inbox.ru>
2023-04-28 12:37:45 +0500

    Refactoring. Using black code style

--- Dark Souls/print_bosses_of_location/print_bosses_of_location__Dark_Souls_II.py
+++ Dark Souls/print_bosses_of_location/print_bosses_of_location__Dark_Souls_II.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import sys
-sys.path.append('..')
+sys.path.append("..")
 
 from common import Parser, find_bosses_of_location_ds2
 
@@ -13,10 +13,10 @@ from common import Parser, find_bosses_of_location_ds2
 p = Parser.DS2(log=False).parse()
 
 for location in p.get_locations():
-    print(f'{location} -> {p.get_location_by_url(location)}')
+    print(f"{location} -> {p.get_location_by_url(location)}")
 
     for boss, url in p.get_location_by_bosses(location):
-        print(f'   {boss} -> {url}')
+        print(f"   {boss} -> {url}")
 
     print()
 
