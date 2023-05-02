3103f538a9eed4a9ffda33314addc2be04c6fdf7
gil9red <ilya.petrash@inbox.ru>
2023-05-02 16:36:08 +0500

    Refactoring. Using black code style

--- design_patterns__examples/Factory/example_1.py
+++ design_patterns__examples/Factory/example_1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # SOURCE: https://ru.wikipedia.org/wiki/Фабричный_метод_(шаблон_проектирования)
@@ -19,13 +19,13 @@ class Parser(ABC):
 class ParserFactory:
     @staticmethod
     def get_parser(name_parser: str) -> Parser:
-        if name_parser == 'XML':
+        if name_parser == "XML":
             return ParserXML()
 
-        elif name_parser == 'JSON':
+        elif name_parser == "JSON":
             return ParserJSON()
 
-        elif name_parser == 'CSV':
+        elif name_parser == "CSV":
             return ParserCSV()
 
         raise Exception(f'Unsupported parser "{name_parser}"')
@@ -34,34 +34,38 @@ class ParserFactory:
 class ParserXML(Parser):
     def parse(self, data: str) -> object:
         from bs4 import BeautifulSoup
-        root = BeautifulSoup(data, 'xml')
+
+        root = BeautifulSoup(data, "xml")
         return root
 
 
 class ParserJSON(Parser):
     def parse(self, data: str) -> object:
         import json
+
         return json.loads(data)
 
 
 class ParserCSV(Parser):
     def parse(self, data: str) -> object:
         import csv
+
         csv_reader = csv.reader(data.splitlines(), delimiter=";")
         return list(csv_reader)
 
 
-if __name__ == '__main__':
-    result = ParserFactory.get_parser('XML').parse('<a><b>123</b></a>')
+if __name__ == "__main__":
+    result = ParserFactory.get_parser("XML").parse("<a><b>123</b></a>")
     print(result)  # '<?xml version="1.0" encoding="utf-8"?>\n<a><b>123</b></a>'
 
-    json_parser = ParserFactory.get_parser('JSON')
-    result = json_parser.parse('[null, null, null]')
+    json_parser = ParserFactory.get_parser("JSON")
+    result = json_parser.parse("[null, null, null]")
     print(result)  # [None, None, None]
 
     result = json_parser.parse('["a", ["b", "c"], null]')
     print(result)  # ['a', ['b', 'c'], None]
 
-    csv_parser = ParserFactory.get_parser('CSV')
-    result = csv_parser.parse('1;vasya;moscow;11111\n2;oleg;sochi;22222')
-    print(result)  # [['1', 'vasya', 'moscow', '11111'], ['2', 'oleg', 'sochi', '22222']]
+    csv_parser = ParserFactory.get_parser("CSV")
+    result = csv_parser.parse("1;vasya;moscow;11111\n2;oleg;sochi;22222")
+    print(result)
+    # [['1', 'vasya', 'moscow', '11111'], ['2', 'oleg', 'sochi', '22222']]
--- design_patterns__examples/Factory/example_2.py
+++ design_patterns__examples/Factory/example_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # SOURCE: https://ru.wikipedia.org/wiki/Фабричный_метод_(шаблон_проектирования)
@@ -12,10 +12,10 @@ from abc import ABC, abstractmethod
 
 class Animal(ABC):
     @staticmethod
-    def initial(animal: str) -> 'Animal':
-        if animal == 'Lion':
+    def initial(animal: str) -> "Animal":
+        if animal == "Lion":
             return Lion()
-        elif animal == 'Cat':
+        elif animal == "Cat":
             return Cat()
 
         raise Exception(f'Unsupported animal "{animal}"')
@@ -35,9 +35,9 @@ class Cat(Animal):
         print("Meow, meow i'm the kitty")
 
 
-if __name__ == '__main__':
-    animal_1 = Animal.initial('Lion')
-    animal_2 = Animal.initial('Cat')
+if __name__ == "__main__":
+    animal_1 = Animal.initial("Lion")
+    animal_2 = Animal.initial("Cat")
 
     animal_1.voice()
     animal_2.voice()
