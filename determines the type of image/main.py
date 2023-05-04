b7a7b846d6c7bc509b32987f56c800863c138ce6
gil9red <ilya.petrash@inbox.ru>
2023-05-04 18:44:14 +0500

    Refactoring. Using black code style

--- determines the type of image/main.py
+++ determines the type of image/main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import glob
-for file_name in glob.glob('img/*.txt'):
-    import imghdr
+import imghdr
+
+for file_name in glob.glob("img/*.txt"):
     img_type = imghdr.what(file_name)
-    print('{} -> {}'.format(file_name, img_type))
+    print("{} -> {}".format(file_name, img_type))
