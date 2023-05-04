bb44d8eb9afb0a65a657a094dc284f1f25b66486
gil9red <ilya.petrash@inbox.ru>
2023-05-04 18:58:14 +0500

    Refactoring. Using black code style

--- downloads_database_domains/downloads_database_domains.py
+++ downloads_database_domains/downloads_database_domains.py
@@ -1,24 +1,25 @@
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 """Скрипт скачивает базу доменов в зоне ru и выводит ее в лог"""
 
 
-if __name__ == '__main__':
-    from urllib.request import urlretrieve
-    from os.path import basename
-    url = 'https://partner.r01.ru/zones/ru_domains.gz'
-    file_name = basename(url)
+import gzip
 
-    # Скачиваем архив
-    urlretrieve(url, file_name)
+from urllib.request import urlretrieve
+from os.path import basename
 
-    import gzip
 
-    # Открытие архива
-    with gzip.open(file_name, 'rb') as f:
-        c = 1
-        # Построчный вывод архивированного файла
-        for line in f:
-            print("{} {}".format(c, line))
-            c += 1
+url = "https://partner.r01.ru/zones/ru_domains.gz"
+file_name = basename(url)
+
+# Скачиваем архив
+urlretrieve(url, file_name)
+
+# Открытие архива
+with gzip.open(file_name, "rb") as f:
+    c = 1
+    # Построчный вывод архивированного файла
+    for line in f:
+        print(f"{c} {line}")
+        c += 1
