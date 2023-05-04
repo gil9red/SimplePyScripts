7e654a98598bd22f084dd8f37077c0cc433aea84
gil9red <ilya.petrash@inbox.ru>
2023-05-04 18:45:53 +0500

    Refactoring. Using black code style

--- disabling_redirection/requests_example.py
+++ disabling_redirection/requests_example.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import requests
 
 
-rs = requests.get('http://ya.ru/')
+rs = requests.get("http://ya.ru/")
 print(rs.status_code, rs.url)
 # 200 https://ya.ru/
 
-rs = requests.get('http://ya.ru/', allow_redirects=False)
+rs = requests.get("http://ya.ru/", allow_redirects=False)
 print(rs.status_code, rs.url)
 # 302 http://ya.ru/
