587f1288accbe805ff698100535aca16cc251a4a
gil9red <ilya.petrash@inbox.ru>
2023-05-10 17:42:45 +0500

    Refactoring. Using black code style

--- EscapeString/main.py
+++ EscapeString/main.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import time
@@ -24,11 +24,11 @@ except:
 
 
 def log_uncaught_exceptions(ex_cls, ex, tb):
-    text = f'{ex_cls.__name__}: {ex}:\n'
-    text += ''.join(traceback.format_tb(tb))
+    text = f"{ex_cls.__name__}: {ex}:\n"
+    text += "".join(traceback.format_tb(tb))
 
     print(text)
-    QMessageBox.critical(None, 'Error', text)
+    QMessageBox.critical(None, "Error", text)
     sys.exit(1)
 
 
@@ -37,15 +37,15 @@ sys.excepthook = log_uncaught_exceptions
 
 class MainWindow(QWidget):
     ESCAPE_RULES = [
-        ('"', '"', r'\"', True),
+        ('"', '"', r"\"", True),
         ("'", "'", r"\'", False),
-        (r'\n', "\n", r'\n', True),
-        (r'\t', "\t", r'\t', False),
-        (r'\b', "\b", r'\b', False),
-        (r'\f', "\f", r'\f', False),
-        ('\\', "\\", '\\\\', False),
+        (r"\n", "\n", r"\n", True),
+        (r"\t", "\t", r"\t", False),
+        (r"\b", "\b", r"\b", False),
+        (r"\f", "\f", r"\f", False),
+        ("\\", "\\", "\\\\", False),
     ]
-    TITLE = 'EscapeString'
+    TITLE = "EscapeString"
 
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -72,12 +72,12 @@ class MainWindow(QWidget):
 
         button_layout.addStretch()
 
-        self.cb_save_multiline = QCheckBox('Save multiline')
+        self.cb_save_multiline = QCheckBox("Save multiline")
         self.cb_save_multiline.setChecked(False)
         self.cb_save_multiline.clicked.connect(self.input_text_changed)
         button_layout.addWidget(self.cb_save_multiline)
 
-        self.cb_string_literal = QCheckBox('String literal')
+        self.cb_string_literal = QCheckBox("String literal")
         self.cb_string_literal.setChecked(True)
         self.cb_string_literal.clicked.connect(self.input_text_changed)
         button_layout.addWidget(self.cb_string_literal)
@@ -95,9 +95,9 @@ class MainWindow(QWidget):
         self.label_error.setTextInteractionFlags(Qt.TextSelectableByMouse)
         self.label_error.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
 
-        self.button_detail_error = QPushButton('...')
+        self.button_detail_error = QPushButton("...")
         self.button_detail_error.setFixedSize(20, 20)
-        self.button_detail_error.setToolTip('Detail error')
+        self.button_detail_error.setToolTip("Detail error")
         self.button_detail_error.clicked.connect(self.show_detail_error_massage)
         self.button_detail_error.hide()
 
@@ -128,10 +128,10 @@ class MainWindow(QWidget):
         if not self.last_error_message or not self.last_detail_error_message:
             return
 
-        message = self.last_error_message + '\n\n' + self.last_detail_error_message
+        message = self.last_error_message + "\n\n" + self.last_detail_error_message
 
         mb = QErrorMessage()
-        mb.setWindowTitle('Error')
+        mb.setWindowTitle("Error")
         # Сообщение ошибки содержит отступы, символы-переходы на следующую строку,
         # которые поломаются при вставке через QErrorMessage.showMessage, и нет возможности
         # выбрать тип текста, то делаем такой хак.
@@ -139,20 +139,22 @@ class MainWindow(QWidget):
 
         mb.exec_()
 
-    def _escape(self, in_text: str, ignored='') -> str:
+    def _escape(self, in_text: str, ignored="") -> str:
         out_text = []
         for char in in_text:
             # Если char нет в словаре экранирования, или флаг экранирования для char выключен,
             # или char среди игнорируемых
-            if char not in self.escape_char_by_checkbox \
-                    or not self.escape_char_by_checkbox[char].isChecked()\
-                    or char in ignored:
+            if (
+                char not in self.escape_char_by_checkbox
+                or not self.escape_char_by_checkbox[char].isChecked()
+                or char in ignored
+            ):
                 out_text.append(char)
                 continue
 
             out_text.append(self.char_by_escape[char])
 
-        return ''.join(out_text)
+        return "".join(out_text)
 
     def input_text_changed(self):
         self.label_error.clear()
@@ -172,7 +174,7 @@ class MainWindow(QWidget):
                     line = self._escape(line)
                     lines.append(f'"{line}"')
 
-                out_text = ' +\n'.join(lines)
+                out_text = " +\n".join(lines)
 
             else:
                 out_text = self._escape(in_text)
@@ -182,16 +184,16 @@ class MainWindow(QWidget):
                     out_text = '"' + out_text
                 if not out_text.endswith('"'):
                     out_text += '"'
-                out_text += ';'
+                out_text += ";"
 
             self.text_edit_output.setPlainText(out_text)
 
             self.setWindowTitle(
-                f'{self.TITLE} (number of characters: '
-                f'{len(self.text_edit_input.toPlainText())} -> '
-                f'{len(self.text_edit_output.toPlainText())})'
+                f"{self.TITLE} (number of characters: "
+                f"{len(self.text_edit_input.toPlainText())} -> "
+                f"{len(self.text_edit_output.toPlainText())})"
             )
-            print('Escape for {:.6f} secs'.format(time.perf_counter() - t))
+            print("Escape for {:.6f} secs".format(time.perf_counter() - t))
 
         except Exception as e:
             # Выводим ошибку в консоль
@@ -204,10 +206,10 @@ class MainWindow(QWidget):
             self.last_detail_error_message = str(tb)
             self.button_detail_error.show()
 
-            self.label_error.setText('Error: ' + self.last_error_message)
+            self.label_error.setText("Error: " + self.last_error_message)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication([])
 
     mw = MainWindow()
