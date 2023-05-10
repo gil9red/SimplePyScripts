7d777155b74950ed0e0150844579559d184d5fcb
gil9red <ilya.petrash@inbox.ru>
2023-05-10 12:34:43 +0500

    Refactoring. Using black code style

--- EncodeStringExample/EncodeStringExample.py
+++ EncodeStringExample/EncodeStringExample.py
@@ -1,20 +1,27 @@
 # coding=utf-8
 import argparse
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 def create_parser():
-    parse = argparse.ArgumentParser(description="Output a user-defined string in different encodings.")
+    parse = argparse.ArgumentParser(
+        description="Output a user-defined string in different encodings."
+    )
     return parse
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     create_parser().parse_args()
 
-    string = raw_input("text = ")
-    uni = unicode(string)
-
-    list_encoding = {"windows-1251", "UTF-16", "UTF-16LE", "UTF-16BE", "ASCII", "Latin-1"}
+    string = input("text = ")
+    list_encoding = {
+        "windows-1251",
+        "UTF-16",
+        "UTF-16LE",
+        "UTF-16BE",
+        "ASCII",
+        "Latin-1",
+    }
     for encoding in list_encoding:
-         print(encoding + ": " + uni.encode(encoding))
+        print(f"{encoding}: {string.encode(encoding)}")
