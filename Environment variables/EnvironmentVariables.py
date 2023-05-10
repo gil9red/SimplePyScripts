efbb22ba4fea1cf774422f5e739293010079754e
gil9red <ilya.petrash@inbox.ru>
2023-05-10 15:04:07 +0500

    Refactoring. Using black code style

--- Environment variables/EnvironmentVariables.py
+++ Environment variables/EnvironmentVariables.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import os
@@ -12,22 +12,22 @@ print("Environment variables:\n{}".format(os.environ))
 
 print("\nEnvironment variables:")
 for var, value in os.environ.items():
-    print("'{}': '{}'".format(var, value))
+    print(f"'{var}': '{value}'")
 
 #
 # Получение значения переменной окружения Path
 env_path = os.environ["Path"]  # или os.environ.get("Path")
 
 # Вывести значение переменной окружения Path
-print("\nPath: {}".format(env_path))
+print(f"\nPath: {env_path}")
 
 # Разделение строки с путями на список
 values_env_path = env_path.split(";")
-print("Path: {}".format(values_env_path))  # Вывод списка
+print(f"Path: {values_env_path}")  # Вывод списка
 
 # Вывод списка путей
 print("Path:")
 for i, val in enumerate(values_env_path, 1):
     # Если элемент не пустой
     if val:
-        print("{}. '{}'".format(i, val) + (": not exist" if not os.path.exists(val) else ""))
+        print(f"{i}. '{val}'" + (": not exist" if not os.path.exists(val) else ""))
--- Environment variables/expand_env__APPDATA.py
+++ Environment variables/expand_env__APPDATA.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # https://ru.wikipedia.org/wiki/Переменная_среды_Windows
 
 import os
-app_data = os.path.expandvars('%APPDATA%')
-print(app_data)
 
+
+app_data = os.path.expandvars("%APPDATA%")
+print(app_data)
 print()
-print(os.path.expandvars('%OS%'))
-print(os.path.expandvars('%COMPUTERNAME%'))
-print(os.path.expandvars('%WINDIR%'))
+
+print(os.path.expandvars("%OS%"))
+print(os.path.expandvars("%COMPUTERNAME%"))
+print(os.path.expandvars("%WINDIR%"))
 print()
-print(os.path.expandvars('%NUMBER_OF_PROCESSORS%'))
-print(os.path.expandvars('%PROCESSOR_ARCHITECTURE%'))
+
+print(os.path.expandvars("%NUMBER_OF_PROCESSORS%"))
+print(os.path.expandvars("%PROCESSOR_ARCHITECTURE%"))
