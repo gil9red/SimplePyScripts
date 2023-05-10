dc7b22e4f3fba4be975d232b7c8103ec1abc190e
gil9red <ilya.petrash@inbox.ru>
2023-05-10 19:34:31 +0500

    Refactoring. Using black code style

--- exchange_rates/banki_ru.py
+++ exchange_rates/banki_ru.py
@@ -1,32 +1,39 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
+
+
+import requests
 
 
 def exchange_rate(currency_id, timestamp=None):
     if timestamp is None:
         from datetime import datetime
+
         timestamp = int(datetime.today().timestamp())
 
     data = {
-        'currency_id': currency_id,
-        'date': timestamp
+        "currency_id": currency_id,
+        "date": timestampб
     }
 
     headers = {
-        'X-Requested-With': 'XMLHttpRequest',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0'
+        "X-Requested-With": "XMLHttpRequest",
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0",
     }
 
-    import requests
-    rs = requests.post('http://www.banki.ru/products/currency/ajax/quotations/value/cbr/', json=data, headers=headers)
-    return rs.json()['value']
+    rs = requests.post(
+        "http://www.banki.ru/products/currency/ajax/quotations/value/cbr/",
+        json=data,
+        headers=headers,
+    )
+    return rs.json()["value"]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # 840 -- USD
-    print('USD:', exchange_rate(840))
+    print("USD:", exchange_rate(840))
 
     # 978 -- EUR
-    print('EUR:', exchange_rate(978))
+    print("EUR:", exchange_rate(978))
