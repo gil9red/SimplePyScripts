e941f5d666673defef0de2a0f3ae8aad650a83ef
gil9red <ilya.petrash@inbox.ru>
2023-05-11 17:22:50 +0500

    Refactoring. Using black code style

--- extract_from_doc.qt.io/main.py
+++ extract_from_doc.qt.io/main.py
@@ -1,47 +1,45 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 """Скрипт для скачивания примеров как проекты: сохранение в папки, в файлы"""
 
 
-def get_path_and_content(url):
-    import requests
+from urllib.parse import urljoin
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_path_and_content(url: str) -> tuple[str, str]:
     rs = requests.get(url)
+    root = BeautifulSoup(rs.content, "lxml")
 
-    from bs4 import BeautifulSoup
-    root = BeautifulSoup(rs.content, 'lxml')
-
-    path = root.select_one('.subtitle').text.strip()
-    content = root.select_one('.descr').text.strip()
+    path = root.select_one(".subtitle").text.strip()
+    content = root.select_one(".descr").text.strip()
     return path, content
 
 
-def get_content(url):
+def get_content(url: str) -> str:
     _, content = get_path_and_content(url)
     return content
 
 
-def get_list_urls_files(url):
-    import requests
+def get_list_urls_files(url: str) -> list[tuple[str, str]]:
     rs = requests.get(url)
+    root = BeautifulSoup(rs.content, "lxml")
 
-    from bs4 import BeautifulSoup
-    root = BeautifulSoup(rs.content, 'lxml')
+    urls_files_list = []
 
-    urls_files_list = list()
-
-    for p in root.select('p'):
-        if p.text.strip() != 'Files:':
+    for p in root.select("p"):
+        if p.text.strip() != "Files:":
             continue
 
-        ul = p.find_next_sibling('ul')
-        for a in ul.select('li > a'):
-            from urllib.parse import urljoin
-            abs_url = urljoin(url, a['href'])
-
+        ul = p.find_next_sibling("ul")
+        for a in ul.select("li > a"):
+            abs_url = urljoin(url, a["href"])
             file_name = a.text.strip()
 
             urls_files_list.append((abs_url, file_name))
@@ -49,26 +47,29 @@ def get_list_urls_files(url):
     return urls_files_list
 
 
-if __name__ == '__main__':
-    url = 'http://doc.qt.io/qt-5/qtwebengine-webenginewidgets-simplebrowser-simplebrowser-pro.html'
+if __name__ == "__main__":
+    import os
+
+    url = "http://doc.qt.io/qt-5/qtwebengine-webenginewidgets-simplebrowser-simplebrowser-pro.html"
     path, content = get_path_and_content(url)
-    print(path, content.encode('utf-8'))
+    print(path, content.encode("utf-8"))
 
     content = get_content(url)
-    print(content.encode('utf-8'))
+    print(content.encode("utf-8"))
 
     print()
-    url = 'http://doc.qt.io/qt-5/qtwebengine-webenginewidgets-simplebrowser-example.html'
+    url = (
+        "http://doc.qt.io/qt-5/qtwebengine-webenginewidgets-simplebrowser-example.html"
+    )
     urls_files_list = get_list_urls_files(url)
-    print('Files: {}'.format(len(urls_files_list)))
+    print(f"Files: {len(urls_files_list)}")
 
     for i, (url, path) in enumerate(urls_files_list, 1):
-        print('{}. "{}": {}'.format(i, path, url))
+        print(f'{i}. "{path}": {url}')
 
-        import os
         dirs = os.path.dirname(path)
         os.makedirs(dirs, exist_ok=True)
 
-        with open(path, 'w', encoding='utf-8') as f:
+        with open(path, "w", encoding="utf-8") as f:
             content = get_content(url)
             f.write(content)
