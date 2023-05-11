c1c8b98199f5f8026476151b704db7860f99541f
gil9red <ilya.petrash@inbox.ru>
2023-05-11 18:19:24 +0500

    Added "fb2__parsing/extract_pictures_from_fb2/fb2_pictures__using_str_find.py"

---
+++ fb2__parsing/extract_pictures_from_fb2/fb2_pictures__using_str_find.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+"""
+Скрипт парсит файл формата fb2, вытаскивает из него картинки и сохраняет их в папке с таким же названием, как файл fb2.
+"""
+
+
+import base64
+import io
+import os
+import traceback
+
+from typing import Iterator
+from PIL import Image
+
+import sys
+
+sys.path.append("..")
+
+from common import sizeof_fmt, get_file_name_from_binary
+
+
+def find_inner(text: str, start_str: str, end_str: str) -> tuple[str | None, int]:
+    idx_start = text.find(start_str)
+    if idx_start == -1:
+        return None, -1
+
+    idx_end = text.find(end_str, idx_start + len(start_str))
+    if idx_end == -1:
+        return None, -1
+
+    return (
+        text[idx_start + len(start_str) : idx_end],
+        idx_end + len(end_str)
+    )
+
+
+def iter_blocks(text: str, start_str: str, end_str: str) -> Iterator[str]:
+    block, idx_next_start = find_inner(text, start_str, end_str)
+    if not block:
+        return
+    yield block
+
+    while block:
+        text = text[idx_next_start:]
+        block, idx_next_start = find_inner(text, start_str, end_str)
+        if not block:
+            break
+
+        yield block
+
+
+def do(file_name, output_dir="output", debug=True):
+    dir_fb2 = os.path.basename(file_name)
+    dir_im = os.path.join(output_dir, dir_fb2)
+    os.makedirs(dir_im, exist_ok=True)
+    debug and print(dir_im + ":")
+
+    total_image_size = 0
+
+    with open(file_name, encoding="utf-8") as fb2:
+        for i, binary in enumerate(
+            iter_blocks(fb2.read(), "<binary ", "</binary>"),
+            start=1,
+        ):
+            try:
+                binary_header, idx_next_start = find_inner(binary, "", ">")
+
+                im_id, _ = find_inner(binary_header, 'id="', '"')
+                content_type, _ = find_inner(binary_header, 'content-type="', '"')
+
+                im_base64 = binary[idx_next_start:]
+
+                im_file_name = get_file_name_from_binary(im_id, content_type)
+                im_file_name = os.path.join(dir_im, im_file_name)
+
+                im_data = base64.b64decode(im_base64.encode())
+
+                count_bytes = len(im_data)
+                total_image_size += count_bytes
+
+                with open(im_file_name, mode="wb") as f:
+                    f.write(im_data)
+
+                im = Image.open(io.BytesIO(im_data))
+                debug and print(
+                    "    {}. {} {} format={} size={}".format(
+                        i, im_id, sizeof_fmt(count_bytes), im.format, im.size
+                    )
+                )
+            except:
+                traceback.print_exc()
+
+        file_size = os.path.getsize(file_name)
+        debug and print()
+        debug and print("fb2 file size =", sizeof_fmt(file_size))
+        debug and print(
+            "total image size = {} ({:.2f}%)".format(
+                sizeof_fmt(total_image_size), total_image_size / file_size * 100
+            )
+        )
+
+
+if __name__ == "__main__":
+    fb2_file_name = "../input/Непутевый ученик в школе магии 1. Зачисление в школу (Часть 1).fb2"
+    do(fb2_file_name)
--- fb2__parsing/extract_pictures_from_fb2/time_test.py
+++ fb2__parsing/extract_pictures_from_fb2/time_test.py
@@ -12,6 +12,7 @@ from fb2_pictures__using_xml_expat import do as do_xml_expat
 from fb2_pictures__using_xml_etree import do as do_xml_etree
 from fb2_pictures__using_xml_sax import do as do_xml_sax
 from fb2_pictures__using_re import do as do_using_re
+from fb2_pictures__using_str_find import do as do_using_str_find
 
 
 file_name = "../input/Непутевый ученик в школе магии 1. Зачисление в школу (Часть 1).fb2"
@@ -23,6 +24,7 @@ runs = [
     ("XML.ETREE", "do_xml_etree"),
     ("XML SAX", "do_xml_sax"),
     ("REGEXP", "do_using_re"),
+    ("STR FIND", "do_using_str_find"),
     ("BS4", "do_bs4"),
 ]
 runs_format = "{:%s} | {:.3f} secs" % (max(len(x[0]) for x in runs),)
@@ -30,3 +32,12 @@ runs_format = "{:%s} | {:.3f} secs" % (max(len(x[0]) for x in runs),)
 for name, stmt in runs:
     timing = timeit(stmt + "(file_name, debug=False)", globals=globals(), number=count)
     print(runs_format.format(name, timing))
+"""
+LXML      | 0.733 secs
+XML EXPAT | 0.867 secs
+XML.ETREE | 0.755 secs
+XML SAX   | 1.014 secs
+REGEXP    | 2.466 secs
+STR FIND  | 1.210 secs
+BS4       | 2.166 secs
+"""
