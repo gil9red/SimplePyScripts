34f3c91a1dc934b87316953115e0fdf093107233
gil9red <ilya.petrash@inbox.ru>
2023-05-12 13:33:15 +0500

    Refactoring. Using black code style

--- FindWordInString/FindWordInString.py
+++ FindWordInString/FindWordInString.py
@@ -1,17 +1,22 @@
 # coding=utf-8
-import argparse
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
+
+
+import argparse
 
 
 def create_parser():
-    parser = argparse.ArgumentParser(description="Find the longest word in a string, separated by spaces.")
+    parser = argparse.ArgumentParser(
+        description="Find the longest word in a string, separated by spaces."
+    )
     return parser
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     create_parser().parse_args()
 
-    string = str(raw_input("Input string: "))
+    string = input("Input string: ")
     max_len = -1
     max_len_word = ""
     for word in string.split(" "):
--- FindWordInString/regexp.py
+++ FindWordInString/regexp.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
-
-text = 'in comparison to dogs, cats have not undergone major changes during the domestication process.'
 
 import re
-words = re.findall(r'\b(\w+)\b', text)
+
+
+text = "in comparison to dogs, cats have not undergone major changes during the domestication process."
+
+words = re.findall(r"\b(\w+)\b", text)
 print(words)
