6043e8b93a59d5ad466b26eb7f421323d031a617
gil9red <ilya.petrash@inbox.ru>
2023-05-12 19:56:09 +0500

    Refactoring. Using black code style

--- flask_http_auth/http_digest_auth.py
+++ flask_http_auth/http_digest_auth.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import json
 
-from typing import Optional
 from pathlib import Path
 
 # pip install flask
@@ -17,27 +16,27 @@ from flask_httpauth import HTTPDigestAuth
 
 
 DIR = Path(__file__).resolve().parent
-PATH_USERS = DIR / 'users.json'
+PATH_USERS = DIR / "users.json"
 
-users = json.loads(PATH_USERS.read_text('utf-8'))
+users = json.loads(PATH_USERS.read_text("utf-8"))
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '<SECRET_KEY_HERE>'
+app.config["SECRET_KEY"] = "<SECRET_KEY_HERE>"
 
 auth = HTTPDigestAuth()
 
 
 @auth.get_password
-def get_password(username: str) -> Optional[str]:
+def get_password(username: str) -> str | None:
     return users.get(username)
 
 
-@app.route('/')
+@app.route("/")
 @auth.login_required
 def index():
     return f"Hello, {auth.username()}!"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
