f219c184f7fd302cd9cfe214ebe288d6f9ab2c4d
gil9red <ilya.petrash@inbox.ru>
2023-05-12 18:44:15 +0500

    Refactoring. Using black code style

--- flask__websocket/ajax_vs_websocket/ajax/main.py
+++ flask__websocket/ajax_vs_websocket/ajax/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 from timeit import default_timer
@@ -9,50 +9,48 @@ from threading import Lock
 
 from flask import Flask, render_template, request, jsonify
 
-app = Flask(__name__, static_folder='../_static')
-app.config['SECRET_KEY'] = 'secret!'
+
+app = Flask(__name__, static_folder="../_static")
+app.config["SECRET_KEY"] = "secret!"
 
 lock = Lock()
 DATA = {
-    'count': 0,
-    'start_time': 0.0
+    "count": 0,
+    "start_time": 0.0,
 }
 
 
-@app.route('/')
+@app.route("/")
 def index():
     with lock:
-        DATA['count'] = 0
+        DATA["count"] = 0
 
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route("/post_method", methods=['POST'])
+@app.route("/post_method", methods=["POST"])
 def post_method():
     data = request.get_json()
     # print(data)
 
     with lock:
-        if DATA['count'] == 0:
-            DATA['start_time'] = default_timer()
+        if DATA["count"] == 0:
+            DATA["start_time"] = default_timer()
 
-        DATA['count'] += 1
+        DATA["count"] += 1
 
     return jsonify({
-        'number': data['number'],
-        'count': DATA['count'],
-        'elapsed': round(default_timer() - DATA['start_time'], 3)
+        "number": data["number"],
+        "count": DATA["count"],
+        "elapsed": round(default_timer() - DATA["start_time"], 3),
     })
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Localhost
-    HOST = '127.0.0.1'
+    HOST = "127.0.0.1"
     PORT = 12000
-    print(f'http://{HOST}:{PORT}')
+    print(f"http://{HOST}:{PORT}")
 
     app.debug = True
-    app.run(
-        host=HOST,
-        port=PORT
-    )
+    app.run(host=HOST, port=PORT)
--- flask__websocket/ajax_vs_websocket/websocket/main.py
+++ flask__websocket/ajax_vs_websocket/websocket/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 from timeit import default_timer
@@ -11,6 +11,7 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from engineio.payload import Payload
 
+
 Payload.max_decode_packets = 1000
 
 
@@ -19,50 +20,50 @@ Payload.max_decode_packets = 1000
 # the best option based on installed packages.
 async_mode = None
 
-app = Flask(__name__, static_folder='../_static')
-app.config['SECRET_KEY'] = 'secret!'
+app = Flask(__name__, static_folder="../_static")
+app.config["SECRET_KEY"] = "secret!"
 socketio = SocketIO(app, async_mode=async_mode)
 
 lock = Lock()
 DATA = {
-    'count': 0,
-    'start_time': 0.0
+    "count": 0,
+    "start_time": 0.0,
 }
 
 
-@app.route('/')
+@app.route("/")
 def index():
     with lock:
-        DATA['count'] = 0
+        DATA["count"] = 0
 
-    return render_template('index.html', async_mode=socketio.async_mode)
+    return render_template("index.html", async_mode=socketio.async_mode)
 
 
-@socketio.on('post_method', namespace='/test')
+@socketio.on("post_method", namespace="/test")
 def post_method(message):
     # print(message)
 
     with lock:
-        if DATA['count'] == 0:
-            DATA['start_time'] = default_timer()
+        if DATA["count"] == 0:
+            DATA["start_time"] = default_timer()
 
-        DATA['count'] += 1
+        DATA["count"] += 1
 
     emit(
-        'my_response',
+        "my_response",
         {
-            'number': message['number'],
-            'count': DATA['count'],
-            'elapsed': round(default_timer() - DATA['start_time'], 3)
-        }
+            "number": message["number"],
+            "count": DATA["count"],
+            "elapsed": round(default_timer() - DATA["start_time"], 3),
+        },
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Localhost
-    HOST = '127.0.0.1'
+    HOST = "127.0.0.1"
     PORT = 12001
-    print(f'http://{HOST}:{PORT}')
+    print(f"http://{HOST}:{PORT}")
 
     app.debug = True
     socketio.run(
