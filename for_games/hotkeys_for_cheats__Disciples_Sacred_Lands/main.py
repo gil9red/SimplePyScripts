d74c4df6f8b7267577a3dd3b1eb944f81d5f0721
gil9red <ilya.petrash@inbox.ru>
2023-05-16 11:28:52 +0500

    Refactoring. Using black code style

--- for_games/hotkeys_for_cheats__Disciples_Sacred_Lands/main.py
+++ for_games/hotkeys_for_cheats__Disciples_Sacred_Lands/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # pip install keyboard
@@ -9,27 +9,27 @@ import keyboard
 
 
 def input_command(text: str):
-    keyboard.press_and_release('enter')
+    keyboard.press_and_release("enter")
 
     keyboard.write(text)
 
-    keyboard.press_and_release('enter')
-    keyboard.press_and_release('esc')
+    keyboard.press_and_release("enter")
+    keyboard.press_and_release("esc")
 
 
 HOTKEY_BY_COMMAND = {
-    'U': 'UPGRADEME',
-    'G': 'GIVEMEMONEY',
-    'I': 'IWANTTOBUILDAGAIN',
-    'N': 'NOWICANSEEYOU',
-    'M': 'MAKEMESTRONGER',
-    'L': 'LETMEMOVE',
-    'R': 'GIVEMEANOTHERCHANCE',
-    'W': 'NOBODYCANBEATME',
+    "U": "UPGRADEME",
+    "G": "GIVEMEMONEY",
+    "I": "IWANTTOBUILDAGAIN",
+    "N": "NOWICANSEEYOU",
+    "M": "MAKEMESTRONGER",
+    "L": "LETMEMOVE",
+    "R": "GIVEMEANOTHERCHANCE",
+    "W": "NOBODYCANBEATME",
 }
 
 for hotkey, text in HOTKEY_BY_COMMAND.items():
-    print(f'{hotkey}: {text}')
+    print(f"{hotkey}: {text}")
     keyboard.add_hotkey(hotkey, lambda text=text: input_command(text))
 
 keyboard.wait()
