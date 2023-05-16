f4ad70fdd8d645b97ddb8068c66bb79ecda370eb
gil9red <ilya.petrash@inbox.ru>
2023-05-16 11:18:46 +0500

    Refactoring. Using black code style

--- for_games/fable3_backup_save/fable3_backup_save.py
+++ for_games/fable3_backup_save/fable3_backup_save.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
-# Backups
+import os.path
+import zipfile
+
+from datetime import datetime
 
 
-if __name__ == '__main__':
-    dir_path = r'C:\Users\ipetrash\Saved Games\Lionhead Studios\Fable 3\1000100010001000'
+dir_path = os.path.expanduser(r'~\Saved Games\Lionhead Studios\Fable 3\1000100010001000')
 
-    from datetime import datetime as dt
-    import os
+save_name = os.path.basename(dir_path)
 
-    save_name = os.path.basename(dir_path)
+backup_file_name = "{}_{}.backup.zip".format(
+    save_name, datetime.today().strftime("%Y%m%d_%H%M%S")
+)
+backup_full_file_name = os.path.join(os.path.dirname(dir_path), backup_file_name)
 
-    backup_file_name = '{}_{}.backup.zip'.format(save_name, dt.today().strftime('%Y%m%d_%H%M%S'))
-    backup_full_file_name = os.path.join(os.path.dirname(dir_path), backup_file_name)
-
-    import zipfile
-
-    with zipfile.ZipFile(backup_full_file_name, mode="w") as f:
-        for file in os.listdir(dir_path):
-            f.write(os.path.join(dir_path, file), file)
+with zipfile.ZipFile(backup_full_file_name, mode="w") as f:
+    for file in os.listdir(dir_path):
+        f.write(os.path.join(dir_path, file), file)
--- for_games/fable3_backup_save/run.py
+++ for_games/fable3_backup_save/run.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import time
 import os
 
+
 while True:
-    os.system('fable3_backup_save.py')
+    os.system("fable3_backup_save.py")
 
     # Каждые 12 часов
     time.sleep(60 * 60 * 12)
