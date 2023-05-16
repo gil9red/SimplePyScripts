c82f2077ee611f4ab6a65269279592da6b5884b8
gil9red <ilya.petrash@inbox.ru>
2023-05-16 13:15:34 +0500

    Refactoring. Using black code style

--- function_example/function_example.py
+++ function_example/function_example.py
@@ -1,4 +1,4 @@
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 def my_sum(*args):
@@ -17,7 +17,11 @@ def sql_insert(table, **kwargs):
     # Example: INSERT INTO users (login, pass) values('TestUser', '123456')
     columns = kwargs.keys()
     values = ["'%s'" % x for x in kwargs.values()]
-    return "INSERT INTO %s (%s) VALUES (%s)" % (table, ', '.join(columns), ', '.join(values))
+    return "INSERT INTO %s (%s) VALUES (%s)" % (
+        table,
+        ", ".join(columns),
+        ", ".join(values),
+    )
 
 
 def say_hello(word="World", spl="!"):
@@ -38,7 +42,7 @@ def binary_operation(a, b, func):
 
 # http://pythonworld.ru/tipy-dannyx-v-python/vse-o-funkciyax-i-ix-argumentax.html
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(my_sum(1, 2, 3, 4))
     my_print(a=2, b=3, c="Foo")
     print(sql_insert("Users", login="TestUser", pas=123))
@@ -57,17 +61,17 @@ if __name__ == '__main__':
     print("3: %d" % binary_operation(a, b, min))
     print("4: %d" % binary_operation(a, b, max))
     print("5: %d" % binary_operation(a, b, lambda a, b: a * b))
-    print("6: %d" % binary_operation(a, b, lambda a, b: a ** b))
+    print("6: %d" % binary_operation(a, b, lambda a, b: a**b))
 
     print()
 
     lambda_sum = lambda a, b: a + b
     print(lambda_sum(1, 1))
     print(lambda_sum(1.1, 1.9))
-    print(lambda_sum('a', 'bc'))
+    print(lambda_sum("a", "bc"))
     print(lambda_sum([1, 2], [3, 4]))
 
     print()
 
-    print((lambda a, b: a ** b)(2, 3))
+    print((lambda a, b: a**b)(2, 3))
     print((lambda a, b: a / b)(4, 2))
