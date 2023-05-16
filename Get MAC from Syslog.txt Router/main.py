d830746460b489ee91e1b3fbcd93674d41e82321
gil9red <ilya.petrash@inbox.ru>
2023-05-16 16:31:44 +0500

    Refactoring

--- Get MAC from Syslog.txt Router/main.py
+++ Get MAC from Syslog.txt Router/main.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+__author__ = "ipetrash"
 
-__author__ = 'ipetrash'
-
-
-text = open('Syslog.txt').read()
 
 import re
-for mac in set(re.findall(r'[0-9a-fA-F]{2}(?::[0-9a-fA-F]{2}){5}', text)):
+
+
+with open("Syslog.txt") as f:
+    text = f.read()
+
+for mac in set(re.findall(r"[0-9a-fA-F]{2}(?::[0-9a-fA-F]{2}){5}", text)):
     print(mac)
+"""
+51:FD:DE:2B:B6:DE
+61:61:6D:3F:99:1F
+03:B4:1D:40:9B:CD
+AC:38:72:82:FF:C4
+"""
