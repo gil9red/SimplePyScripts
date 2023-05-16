b3689f539d97222de75b6a235a000f90b6300639
gil9red <ilya.petrash@inbox.ru>
2023-05-16 12:50:33 +0500

    Refactoring

--- for_games/solutions of games/ELEX__hacking_minigame.py
+++ for_games/solutions of games/ELEX__hacking_minigame.py
@@ -9,59 +9,84 @@ import random
 
 # Этот шаблон допускает любые числа
 _ = "*"
-keys = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-fixed_values = [_, _, _, _]
-need_values = []
 
-keys = [1, 2, 4, 5, 7, 8, 9]
-keys = [0, 1, 6, 8]
-keys = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-eval_template = "a == b and (a > c < d)"
-eval_template = "a > b > c < d"
-eval_template = "a > b < c > d"
+def run(
+    eval_template: str,
+    keys: list[int],
+    fixed_values: list[str | int],
+    need_values: list[str | int],
+):
+    while True:
+        items = keys.copy()
+        fixed_items = fixed_values.copy()
+        need_items = need_values.copy()
 
-fixed_values = [_, _, _, _]
-fixed_values = [_, 1, 8, _]
+        # Удаляем дублирующие значения
+        for fixed in fixed_items:
+            if fixed in items:
+                items.remove(fixed)
 
-need_values = [4]
+            if fixed in need_items:
+                need_items.remove(fixed)
 
-while True:
-    items = keys.copy()
-    fixed_items = fixed_values.copy()
-    need_items = need_values.copy()
+        random.shuffle(items)
+        random.shuffle(need_items)
 
-    # Удаляем дублирующие значения
-    for fixed in fixed_items:
-        if fixed in items:
-            items.remove(fixed)
+        free_idx = [i for i, x in enumerate(fixed_items) if x == _]
+        random.shuffle(free_idx)
 
-        if fixed in need_items:
-            need_items.remove(fixed)
+        for i in free_idx:
+            # Если есть обязательные значения, то берем из них
+            if need_items:
+                fixed_items[i] = need_items.pop()
+            else:
+                fixed_items[i] = items.pop()
 
-    random.shuffle(items)
-    random.shuffle(need_items)
+        a, b, c, d = fixed_items
 
-    free_idx = [i for i, x in enumerate(fixed_items) if x == _]
-    random.shuffle(free_idx)
+        text = (
+            eval_template.replace("$1", str(a))
+            .replace("$2", str(b))
+            .replace("$3", str(c))
+            .replace("$4", str(d))
+        )
 
-    for i in free_idx:
-        # Если есть обязательные значения, то берем из них
-        if need_items:
-            fixed_items[i] = need_items.pop()
-        else:
-            fixed_items[i] = items.pop()
+        ok = eval(text)
+        if ok:
+            print(text)
+            break
 
-    a, b, c, d = fixed_items
 
-    text = (
-        eval_template.replace("a", str(a))
-        .replace("b", str(b))
-        .replace("c", str(c))
-        .replace("d", str(d))
+if __name__ == "__main__":
+    run(
+        eval_template="$1 == $2 and ($1 > $3 < $4)",
+        keys=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        fixed_values=[_, _, _, _],
+        need_values=[4],
     )
+    # 4 == 4 and (4 > 3 < 8)
 
-    ok = eval(text)
-    if ok:
-        print(text)
-        break
+    run(
+        eval_template="$1 == $2 and ($1 > $3 < $4)",
+        keys=[1, 2, 4, 5, 7, 8, 9],
+        fixed_values=[_, _, _, _],
+        need_values=[4],
+    )
+    # 4 == 4 and (4 > 2 < 5)
+
+    run(
+        eval_template="$1 > $2 > $3 < $4",
+        keys=[0, 1, 6, 8],
+        fixed_values=[_, _, _, _],
+        need_values=[4],
+    )
+    # 8 > 4 > 1 < 6
+
+    run(
+        eval_template="$1 > $2 < $3 > $4",
+        keys=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        fixed_values=[_, 1, 8, _],
+        need_values=[4],
+    )
+    # 4 > 1 < 8 > 5
