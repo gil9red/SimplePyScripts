73b7eb142d49f46134f7e2fc328b2dc96006d549
gil9red <ilya.petrash@inbox.ru>
2023-05-17 11:34:46 +0500

    Added "get_dir_total_size__examples/get_dir_total_size__using__pathlib_Path_rglob.py"

---
+++ get_dir_total_size__examples/get_dir_total_size__using__pathlib_Path_rglob.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+import pathlib
+
+from common import sizeof_fmt
+
+
+def get_dir_total_size(dir_name: str) -> tuple[int, str]:
+    total_size = 0
+
+    for file_name in pathlib.Path(dir_name).rglob('*'):
+        if file_name.is_file():
+            total_size += file_name.stat().st_size
+
+    return total_size, sizeof_fmt(total_size)
+
+
+if __name__ == "__main__":
+    import os
+
+    # paths = [r"C:\Users\Default", r"C:\Program Files (x86)", os.path.expanduser(r'~\Desktop')]
+    paths = [".."]
+
+    for path in paths:
+        path = os.path.abspath(path)
+
+        size, size_str = get_dir_total_size(path)
+        print(f'"{path}": {size} bytes / {size_str}')
