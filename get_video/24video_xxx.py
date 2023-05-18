3eff5ae1cf8b6be2037475aac66ad34b511ecc7b
gil9red <ilya.petrash@inbox.ru>
2023-05-18 14:15:38 +0500

    Refactoring. Using black code style

--- get_video/24video_xxx.py
+++ get_video/24video_xxx.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 from urllib.request import urlopen
@@ -10,19 +10,19 @@ from lxml import etree
 
 def get_video_url_from_24video_xxx(url):
     # url выглядят так: http://www.24video.xxx/video/view/2249418
-    video_id = url.split('/')[-1]
-    xml_info_url = 'http://www.24video.xxx/video/xml/{}?mode=play'.format(video_id)
+    video_id = url.split("/")[-1]
+    xml_info_url = f"http://www.24video.xxx/video/xml/{video_id}?mode=play"
 
     with urlopen(xml_info_url) as rs:
         root = etree.XML(rs.read())
-        match = root.xpath('//video/@url')
+        match = root.xpath("//video/@url")
         if match:
             return match[0]
 
 
-if __name__ == '__main__':
-    url = 'http://www.24video.xxx/video/view/2249418'
+if __name__ == "__main__":
+    url = "http://www.24video.xxx/video/view/2249418"
     print(get_video_url_from_24video_xxx(url))
 
-    url = 'http://www.24video.xxx/video/view/1724382'
+    url = "http://www.24video.xxx/video/view/1724382"
     print(get_video_url_from_24video_xxx(url))
--- get_video/kinokong_net.py
+++ get_video/kinokong_net.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
+
+
+import re
+import requests
 
 
 def get_video_url(url):
-    import requests
     rs = requests.get(url)
 
-    import re
     match = re.search(r'new.+?Uppod\(.*?file:.*?"(.+?mp4)"', rs.text)
     if match:
         return match.group(1)
 
 
-if __name__ == '__main__':
-    url = 'http://kinokong.net/29053-vse-o-muzhchinah-2016-smotret-online.html'
+if __name__ == "__main__":
+    url = "http://kinokong.net/29053-vse-o-muzhchinah-2016-smotret-online.html"
     print(get_video_url(url))
