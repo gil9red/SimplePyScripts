8690f8d75a4240e115c994baa4b42b2ec939292a
gil9red <ilya.petrash@inbox.ru>
2023-05-18 13:05:11 +0500

    Refactoring. Using black code style

--- get_video/kinogo_club/get_file_video_url.py
+++ get_video/kinogo_club/get_file_video_url.py
@@ -1,20 +1,42 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import base64
+import re
+
+import requests
 
 
 # Алгоритм: https://hms.lostcut.net/viewtopic.php?id=80
-def DecodeUppodTextHash( data ):
-    hash = "0123456789WGXMHRUZID=NQVBLihbzaclmepsJxdftioYkngryTwuvihv7ec41D6GpBtXx3QJRiN5WwMf=ihngU08IuldVHosTmZz9kYL2bayE"
-    data = DecodeUppod_tr(data, "r", "A")
-    data = data.replace("\n", "")
-    hash = hash.split('ih')
 
-    if data.endswith('!'):
+
+def decode_uppod_tr(data, ch1, ch2):
+    if data[:-1].endswith(ch1) and data[2] == ch2:
+        srev = data[::-1]  # revers string
+        try:
+            loc3 = int(float(srev[-2:]) / 2)  # get number at end of string
+        except ValueError:
+            return data
+        srev = srev[2:-3]  # get string between ch1 and ch2
+        if loc3 < len(srev):
+            i = loc3
+            while i < len(srev):
+                srev = srev[:i] + srev[i + 1 :]  # remove char at index i
+                i += loc3
+        data = srev + "!"
+    return data
+
+
+def decode_uppod_text_hash(data):
+    hash = "0123456789WGXMHRUZID=NQVBLihbzaclmepsJxdftioYkngryTwuvihv7ec41D6GpBtXx3QJRiN5WwMf=ihngU08IuldVHosTmZz9kYL2bayE"
+    data = decode_uppod_tr(data, "r", "A")
+    data = data.replace("\n", "")
+    hash = hash.split("ih")
+
+    if data.endswith("!"):
         data = data[:-1]
         taba = hash[3]
         tabb = hash[2]
@@ -26,48 +48,28 @@ def DecodeUppodTextHash( data ):
     while i < len(taba):
         data = data.replace(tabb[i], "__")
         data = data.replace(taba[i], tabb[i])
-        data = data.replace("__",    taba[i])
+        data = data.replace("__", taba[i])
         i += 1
 
     result = base64.b64decode(data)
     return result
 
 
-def DecodeUppod_tr(data, ch1, ch2):
-    if data[:-1].endswith(ch1) and data[2]==ch2:
-        srev = data[::-1]  # revers string
-        try:
-            loc3 = int(float(srev[-2:]) / 2) # get number at end of string
-        except ValueError:
-            return data
-        srev = srev[2:-3] # get string between ch1 and ch2
-        if loc3 < len(srev):
-            i = loc3
-            while i < len(srev):
-                srev = srev[:i] + srev[i+1:] # remove char at index i
-                i += loc3
-        data = srev + "!"
-    return data
-
-
 def decode_file_url(base64_data):
     # secret_word случайно вставляется base64_data, портя его
-    secret_word = 'tQ3N'
-    file_url = DecodeUppodTextHash(base64_data.replace(secret_word, ''))
-    return file_url.decode('utf-8')
+    secret_word = "tQ3N"
+    file_url = decode_uppod_text_hash(base64_data.replace(secret_word, ""))
+    return file_url.decode("utf-8")
 
-
-import re
 
 # Вытаскивание закодированного в base64 html кода, описывающего плеер
 base64_uppod_data_pattern = re.compile(r"document\.write\(Base64.decode\('(.+?)'\)")
 
 # Вытаскивание закодированного в измененный base64 ссылки на файл видео
-base64_data_url_pattern = re.compile(r'&amp;file=(.+?)&amp;')
+base64_data_url_pattern = re.compile(r"&amp;file=(.+?)&amp;")
 
 
 def get_file_video_url(url):
-    import requests
     rs = requests.get(url)
     if not rs.ok:
         return
@@ -77,7 +79,7 @@ def get_file_video_url(url):
         return
 
     uppod_data_base64 = match.group(1)
-    uppod_data = base64.standard_b64decode(uppod_data_base64).decode('utf-8')
+    uppod_data = base64.standard_b64decode(uppod_data_base64).decode("utf-8")
     match = base64_data_url_pattern.search(uppod_data)
     if match is None:
         return
@@ -85,12 +87,12 @@ def get_file_video_url(url):
     return decode_file_url(match.group(1))
 
 
-if __name__ == '__main__':
-    url = 'http://kinogo.club/6246-pervyy-mstitel-3-protivostoyanie-2016.html'
+if __name__ == "__main__":
+    url = "http://kinogo.club/6246-pervyy-mstitel-3-protivostoyanie-2016.html"
     print(get_file_video_url(url))
 
-    url = 'http://kinogo.club/6253-sudnaya-noch-3-2016.html'
+    url = "http://kinogo.club/6253-sudnaya-noch-3-2016.html"
     print(get_file_video_url(url))
 
-    url = 'http://kinogo.club/2963-poceluy-mamochku-na-noch-2013.html'
+    url = "http://kinogo.club/2963-poceluy-mamochku-na-noch-2013.html"
     print(get_file_video_url(url))
--- get_video/kinogo_club/get_serial_video_list.py
+++ get_video/kinogo_club/get_serial_video_list.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
+import json
+import random
 import re
-url_serial_name_from_url = re.compile(r'http://kinogo\.club/\d+?-(.+?)\.html')
+
+import requests
+
+
+url_serial_name_from_url = re.compile(r"http://kinogo\.club/\d+?-(.+?)\.html")
 
 
 def get_video_list_url_from_seasonvar_ru(url):
@@ -15,32 +21,30 @@ def get_video_list_url_from_seasonvar_ru(url):
 
     serial_url_name = match.group(1)
 
-    import random
-
     # Пример: http://kinogo.club/playlist/sklifosovskiy-3-sezon.txt?rand=0.24304015329107642
-    get_playlist_url = 'http://kinogo.club/playlist/{}.txt?rand={}'.format(serial_url_name, random.random())
+    get_playlist_url = f"http://kinogo.club/playlist/{serial_url_name}.txt?rand={random.random()}"
 
-    import requests
     rs = requests.get(get_playlist_url)
     if not rs.ok:
         return
 
     # Небольшое ухищрение. В начале json приходит набор символов "п»ї" и они мешают парсеру JSON.
-    import json
     json_data = json.loads(rs.text[3:])
 
     list_of_series = list()
 
-    for row in json_data['playlist']:
-        if 'file' in row:
-            list_of_series.append((row['comment'].encode('cp1251').decode('utf-8'), row['file']))
+    for row in json_data["playlist"]:
+        if "file" in row:
+            list_of_series.append(
+                (row["comment"].encode("cp1251").decode("utf-8"), row["file"])
+            )
 
     return list_of_series
 
 
-if __name__ == '__main__':
-    url = 'http://kinogo.club/6583-simpsony-28-sezon.html'
+if __name__ == "__main__":
+    url = "http://kinogo.club/6583-simpsony-28-sezon.html"
     print(get_video_list_url_from_seasonvar_ru(url))
 
-    url = 'http://kinogo.club/4733-sklifosovskiy-3-sezon.html'
+    url = "http://kinogo.club/4733-sklifosovskiy-3-sezon.html"
     print(get_video_list_url_from_seasonvar_ru(url))
