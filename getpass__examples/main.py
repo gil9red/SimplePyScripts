be0a742b947e5e2549a9b023f3b8d22fffc8a330
gil9red <ilya.petrash@inbox.ru>
2023-05-18 19:05:11 +0500

    Refactoring. Using black code style

--- getpass__examples/main.py
+++ getpass__examples/main.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import getpass
 
+
 # Return the “login name” of the user.
-print('Hello,', getpass.getuser())
+print("Hello,", getpass.getuser())
 
 # NOTE: not work in PyCharm console
 password = getpass.getpass()
 print(password)
 
-password = getpass.getpass('Input pass: ')
+password = getpass.getpass("Input pass: ")
 print(password)
