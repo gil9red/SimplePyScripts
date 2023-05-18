75f7eb1227a80494798983c92551fccad805d61e
gil9red <ilya.petrash@inbox.ru>
2023-05-18 19:05:44 +0500

    Refactoring. Using black code style

--- github3py__examples/config.py
+++ github3py__examples/config.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import os
@@ -9,6 +9,6 @@ from pathlib import Path
 
 
 DIR = Path(__file__).resolve().parent
-TOKEN_FILE_NAME = DIR / 'TOKEN.txt'
+TOKEN_FILE_NAME = DIR / "TOKEN.txt"
 
-TOKEN = os.environ.get('TOKEN') or TOKEN_FILE_NAME.read_text('utf-8').strip()
+TOKEN = os.environ.get("TOKEN") or TOKEN_FILE_NAME.read_text("utf-8").strip()
--- github3py__examples/get_gist_history.py
+++ github3py__examples/get_gist_history.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # SOURCE: https://github.com/sigmavirus24/github3.py
@@ -15,8 +15,12 @@ from config import TOKEN
 
 gh = GitHub(token=TOKEN)
 
-gist = gh.gist('2f80a34fb601cd685353')
+gist = gh.gist("2f80a34fb601cd685353")
 
 for commit in gist.commits():
     # TODO: KeyError: 'gistfile1.txt'
-    print(commit.committed_at, commit.version, len(commit.gist().files['gistfile1.txt'].content()))
+    print(
+        commit.committed_at,
+        commit.version,
+        len(commit.gist().files["gistfile1.txt"].content()),
+    )
