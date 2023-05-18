65b5e12fe6eea046bbf29a50668d18a22197a7a4
gil9red <ilya.petrash@inbox.ru>
2023-05-18 14:14:20 +0500

    Refactoring. Using black code style

--- get_video/www_youtube_com/get_file_video_url.py
+++ get_video/www_youtube_com/get_file_video_url.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
-def get_video_file_urls(url):
-    # http://np1.github.io/pafy/
-    # https://github.com/mps-youtube/pafy
-    import pafy
+# http://np1.github.io/pafy/
+# https://github.com/mps-youtube/pafy
+import pafy
+
+
+def get_video_file_urls(url: str) -> list[dict]:
     video = pafy.new(url)
 
-    file_url_list = list()
+    file_url_list = []
 
     for s in video.streams:
         file_url_list.append({
-            'url': s.url,
-            'resolution': s.resolution,
-            'extension': s.extension,
+            "url": s.url,
+            "resolution": s.resolution,
+            "extension": s.extension,
         })
 
     return file_url_list
 
 
-if __name__ == '__main__':
-    url = 'https://www.youtube.com/watch?v=lz4r-TtIOjA'
+if __name__ == "__main__":
+    url = "https://www.youtube.com/watch?v=lz4r-TtIOjA"
     print(get_video_file_urls(url))
 
-    url = 'https://www.youtube.com/watch?v=bMt47wvK6u0'
+    url = "https://www.youtube.com/watch?v=bMt47wvK6u0"
     print(get_video_file_urls(url))
