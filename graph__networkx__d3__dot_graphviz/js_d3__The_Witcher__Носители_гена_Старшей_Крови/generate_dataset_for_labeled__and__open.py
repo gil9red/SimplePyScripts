d3f84c4af2ac300993fb719cccd4de28893c2521
gil9red <ilya.petrash@inbox.ru>
2023-05-20 14:12:11 +0500

    Refactoring. Using black code style

--- graph__networkx__d3__dot_graphviz/js_d3__The_Witcher__Носители_гена_Старшей_Крови/generate_dataset_for_labeled__and__open.py
+++ graph__networkx__d3__dot_graphviz/js_d3__The_Witcher__Носители_гена_Старшей_Крови/generate_dataset_for_labeled__and__open.py
@@ -1,41 +1,36 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 """Граф носителей гена Старшей Крови (ген Лары)"""
 
 
-COLOR_1 = '#8b00ff'
-COLOR_2 = 'blue'
-COLOR_3 = 'red'
+import os
+
+
+COLOR_1 = "#8b00ff"
+COLOR_2 = "blue"
+COLOR_3 = "red"
 
 GLOBAL_TRANSITIONS = {
-    (('Лара Доррен', COLOR_1), ('Рианнон', COLOR_1)),
-
-    (('Рианнон', COLOR_1), ('Фиона', COLOR_2)),
-    (('Рианнон', COLOR_1), ('Амавет', COLOR_3)),
-
-    (('Фиона', COLOR_2), ('Корбетт', COLOR_2)),
-
-    (('Корбетт', COLOR_2), ('Дагорад', COLOR_2)),
-
-    (('Адалия', COLOR_3), ('Калантэ', COLOR_1)),
-    (('Дагорад', COLOR_2), ('Калантэ', COLOR_1)),
-
-    (('Калантэ', COLOR_1), ('Паветта', COLOR_1)),
-
-    (('Паветта', COLOR_1), ('Цири', COLOR_1)),
-
-    (('Амавет', COLOR_3), ('Мюриель', COLOR_3)),
-
-    (('Мюриель', COLOR_3), ('Адалия', COLOR_3)),
-    (('Мюриель', COLOR_3), ('Роберт II', COLOR_3)),
+    (("Лара Доррен", COLOR_1), ("Рианнон", COLOR_1)),
+    (("Рианнон", COLOR_1), ("Фиона", COLOR_2)),
+    (("Рианнон", COLOR_1), ("Амавет", COLOR_3)),
+    (("Фиона", COLOR_2), ("Корбетт", COLOR_2)),
+    (("Корбетт", COLOR_2), ("Дагорад", COLOR_2)),
+    (("Адалия", COLOR_3), ("Калантэ", COLOR_1)),
+    (("Дагорад", COLOR_2), ("Калантэ", COLOR_1)),
+    (("Калантэ", COLOR_1), ("Паветта", COLOR_1)),
+    (("Паветта", COLOR_1), ("Цири", COLOR_1)),
+    (("Амавет", COLOR_3), ("Мюриель", COLOR_3)),
+    (("Мюриель", COLOR_3), ("Адалия", COLOR_3)),
+    (("Мюриель", COLOR_3), ("Роберт II", COLOR_3)),
 }
 
-edges = list()
-nodes = list()
+edges = []
+nodes = []
 
 for _from, _to in GLOBAL_TRANSITIONS:
     if _from not in nodes:
@@ -44,25 +39,26 @@ for _from, _to in GLOBAL_TRANSITIONS:
     if _to not in nodes:
         nodes.append(_to)
 
-    edges.append({'source': nodes.index(_from), 'target': nodes.index(_to)})
+    edges.append({"source": nodes.index(_from), "target": nodes.index(_to)})
 
-nodes_dict = [{"name": title, 'color': color} for title, color in nodes]
+nodes_dict = [{"name": title, "color": color} for title, color in nodes]
 
 dataset_text = """\
     var dataset = {{
         nodes: {},
         edges: {}
     }};
-""".format(nodes_dict, edges)
+""".format(
+    nodes_dict, edges
+)
 print(dataset_text)
 
-with open('template__graph_with_labeled_edges.html', 'r', encoding='utf-8') as f:
+with open("template__graph_with_labeled_edges.html", "r", encoding="utf-8") as f:
     text = f.read()
-    text = text.replace('{{dataset}}', dataset_text)
+    text = text.replace("{{dataset}}", dataset_text)
 
-with open('graph_with_labeled_edges.html', 'w', encoding='utf-8') as f:
+with open("graph_with_labeled_edges.html", "w", encoding="utf-8") as f:
     f.write(text)
 
 # Open file
-import os
-os.startfile('graph_with_labeled_edges.html')
+os.startfile("graph_with_labeled_edges.html")
