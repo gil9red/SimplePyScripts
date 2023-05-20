df2ccec4029444b267ffe4a33c21c6df5f147046
gil9red <ilya.petrash@inbox.ru>
2023-05-20 14:16:16 +0500

    Refactoring. Using black code style

--- Guess the number/GuessTheNumber.py
+++ Guess the number/GuessTheNumber.py
@@ -1,32 +1,35 @@
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
+
 
 # Guess the number / Угадай число
+
+
 import random
 
-if __name__ == '__main__':
-    max = int(input("Input max: "))
-    print("Random number (x): from %d to %d" % (1, max))
-    number = random.randrange(1, max + 1)
-    user_choice = -1
-    range_min = range_max = "?"
 
-    while True:
-        print("\n%s < x < %s" % (range_min, range_max))
-        user_choice = int(input("Input number: "))
-        if number > user_choice:
-            if range_min == "?":
-                range_min = user_choice
-            if user_choice > range_min:
-                range_min = user_choice
-            print("x > %d" % user_choice)
+max = int(input("Input max: "))
+print(f"Random number (x): from 1 to {max}")
+number = random.randrange(1, max + 1)
+user_choice = -1
+range_min = range_max = "?"
 
-        elif number < user_choice:
-            if range_max == "?":
-                range_max = user_choice
-            if user_choice < range_max:
-                range_max = user_choice
-            print("x < %d" % user_choice)
+while True:
+    print("\n%s < x < %s" % (range_min, range_max))
+    user_choice = int(input("Input number: "))
+    if number > user_choice:
+        if range_min == "?":
+            range_min = user_choice
+        if user_choice > range_min:
+            range_min = user_choice
+        print("x > %d" % user_choice)
 
-        elif number == user_choice:
-            print("Congratulations! You guessed it!")
-            break
+    elif number < user_choice:
+        if range_max == "?":
+            range_max = user_choice
+        if user_choice < range_max:
+            range_max = user_choice
+        print("x < %d" % user_choice)
+
+    elif number == user_choice:
+        print("Congratulations! You guessed it!")
+        break
