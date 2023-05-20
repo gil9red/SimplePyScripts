8588b7836ed716c74cddce7a3952a092d0d35c01
gil9red <ilya.petrash@inbox.ru>
2023-05-20 19:32:59 +0500

    Refactoring. Using black code style

--- hmac__examples/hello_world.py
+++ hmac__examples/hello_world.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # SOURCE: https://docs.python.org/3.7/library/hmac.html
 
 
-import hmac
 import hashlib
+import hmac
 
-key = b'<SECRET_KEY>'
-msg = 'Hello World!'.encode('utf-8')
+
+key = b"<SECRET_KEY>"
+msg = "Hello World!".encode("utf-8")
 
 digest = hmac.new(key, msg, digestmod=hashlib.sha256).hexdigest()
-text = 'Now: ' + digest
-print(text)  # Now: 9a9e1262844d237c5fa17b839867ec3d49006a3639e4bd402008726474b12a76
+text = "Now: " + digest
+print(text)
+# Now: 9a9e1262844d237c5fa17b839867ec3d49006a3639e4bd402008726474b12a76
