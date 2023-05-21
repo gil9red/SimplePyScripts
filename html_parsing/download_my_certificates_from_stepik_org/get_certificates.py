c05fb8b5cbc8466c01fae833656426c9eed28264
gil9red <ilya.petrash@inbox.ru>
2023-05-21 16:42:41 +0500

    Refactoring. Using black code style

--- html_parsing/download_my_certificates_from_stepik_org/get_certificates.py
+++ html_parsing/download_my_certificates_from_stepik_org/get_certificates.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
-from typing import List, Dict
 import requests
 
 
-def get_certificates(user: int) -> List[Dict]:
+def get_certificates(user: int) -> list[dict]:
     items = []
 
     s = requests.Session()
     page = 1
     while True:
-        url = f'https://stepik.org/api/certificates?order=-id&page={page}&user={user}'
+        url = f"https://stepik.org/api/certificates?order=-id&page={page}&user={user}"
 
         rs = s.get(url)
         rs.raise_for_status()
 
         data = rs.json()
-        items += data['certificates']
+        items += data["certificates"]
 
-        if not data['meta']['has_next']:
+        if not data["meta"]["has_next"]:
             break
 
         page += 1
@@ -30,15 +29,16 @@ def get_certificates(user: int) -> List[Dict]:
     return items
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     USER = 1381287  # https://stepik.org/users/1381287/certificates
 
     certificates = get_certificates(USER)
-    print(f'Certificates ({len(certificates)}):')
+    print(f"Certificates ({len(certificates)}):")
 
     for cert in certificates:
-        print('   ', cert['course_title'], 'https://stepik.org/course/' + str(cert['course']))
-        print('   ', cert['issue_date'])
-        print('   ', cert['url'])
-        print('   ', cert['preview_url'])
+        url = "https://stepik.org/course/" + str(cert["course"])
+        print("   ", cert["course_title"], url)
+        print("   ", cert["issue_date"])
+        print("   ", cert["url"])
+        print("   ", cert["preview_url"])
         print()
--- html_parsing/download_my_certificates_from_stepik_org/main.py
+++ html_parsing/download_my_certificates_from_stepik_org/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 from pathlib import Path
@@ -14,21 +14,23 @@ from get_certificates import get_certificates
 
 DIR = Path(__file__).resolve().parent
 
-DIR_CERTS = DIR / 'certs'
+DIR_CERTS = DIR / "certs"
 DIR_CERTS.mkdir(parents=True, exist_ok=True)
 
 USER = 1381287  # https://stepik.org/users/1381287/certificates
 
 certificates = get_certificates(USER)
-print(f'Certificates: {len(certificates)}')
+print(f"Certificates: {len(certificates)}")
 
 for cert in certificates:
-    course_title = cert['course_title']
+    course_title = cert["course_title"]
 
-    file_name = sanitize_filename(course_title) + '.pdf'
+    file_name = sanitize_filename(course_title) + ".pdf"
     file_name = DIR_CERTS / file_name
     if file_name.exists():
         continue
 
-    print(f'Downloading certificate file for {course_title!r} course: {file_name.relative_to(DIR)}')
-    urlretrieve(cert['url'], file_name)
+    print(
+        f"Downloading certificate file for {course_title!r} course: {file_name.relative_to(DIR)}"
+    )
+    urlretrieve(cert["url"], file_name)
