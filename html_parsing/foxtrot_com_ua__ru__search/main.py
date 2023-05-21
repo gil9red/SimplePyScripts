e4561c4feeb5ad114c37041806332413ae249b08
gil9red <ilya.petrash@inbox.ru>
2023-05-21 18:42:11 +0500

    Refactoring. Using black code style

--- html_parsing/foxtrot_com_ua__ru__search/main.py
+++ html_parsing/foxtrot_com_ua__ru__search/main.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import datetime as DT
-import time
 
-from typing import List, Tuple, Union
 from pathlib import Path
 
 # pip install pandas
@@ -30,9 +28,9 @@ def get_text_by_css(parent, css_selector: str, default: str) -> str:
         return default
 
 
-def parse(url_search: str) -> List[Tuple[str, str, str]]:
+def parse(url_search: str) -> list[tuple[str, str, str]]:
     options = Options()
-    options.add_argument('--headless')
+    options.add_argument("--headless")
 
     items = []
 
@@ -44,15 +42,15 @@ def parse(url_search: str) -> List[Tuple[str, str, str]]:
         while page <= last_page:
             url = url_search
             if page > 1:
-                url = f'{url_search}&page={page}'
+                url = f"{url_search}&page={page}"
 
-            print(f'Load: {url}')
+            print(f"Load: {url}")
             driver.get(url)
 
             for item_el in driver.find_elements_by_css_selector(".card[data-url]"):
-                name = get_text_by_css(item_el, '.card__title', 'Null')
-                price = get_text_by_css(item_el, '.card-price', '-')
-                nal = get_text_by_css(item_el, '.card__buttons', '-')
+                name = get_text_by_css(item_el, ".card__title", "Null")
+                price = get_text_by_css(item_el, ".card-price", "-")
+                nal = get_text_by_css(item_el, ".card__buttons", "-")
 
                 row = name, price, nal
                 print(row)
@@ -61,8 +59,10 @@ def parse(url_search: str) -> List[Tuple[str, str, str]]:
 
             # Обновление номера последней страницы
             try:
-                pages_count_el = driver.find_element_by_css_selector('*[data-pages-count]')
-                last_page = int(pages_count_el.get_attribute('data-pages-count'))
+                pages_count_el = driver.find_element_by_css_selector(
+                    "*[data-pages-count]"
+                )
+                last_page = int(pages_count_el.get_attribute("data-pages-count"))
 
             except NoSuchElementException:
                 break
@@ -75,21 +75,20 @@ def parse(url_search: str) -> List[Tuple[str, str, str]]:
     return items
 
 
-
 def save_goods(
-        file_name: Union[str, Path],
-        items: List[Tuple[str, str, str]],
-        encoding='utf-8'
+    file_name: str | Path,
+    items: list[tuple[str, str, str]],
+    encoding="utf-8",
 ):
-    df = pd.DataFrame(items, columns=['Name', 'Price', 'Nal'])
+    df = pd.DataFrame(items, columns=["Name", "Price", "Nal"])
     df.to_csv(file_name, encoding=encoding)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     url = "https://www.foxtrot.com.ua/ru/search?query=gazer&filter=_195_588"
     items = parse(url)
-    print(f'Total goods: {len(items)}')
+    print(f"Total goods: {len(items)}")
 
-    file_name = f'foxtrot_parser_{DT.datetime.now():%Y-%m-%d}.csv'
-    print(f'Saved to {file_name}')
+    file_name = f"foxtrot_parser_{DT.datetime.now():%Y-%m-%d}.csv"
+    print(f"Saved to {file_name}")
     save_goods(file_name, items)
