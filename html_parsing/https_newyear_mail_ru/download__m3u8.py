14c1995018e6c09310fd11c75c04dd488471322f
gil9red <ilya.petrash@inbox.ru>
2023-05-22 14:36:12 +0500

    Refactoring. Using black code style

--- html_parsing/https_newyear_mail_ru/download__m3u8.py
+++ html_parsing/https_newyear_mail_ru/download__m3u8.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import requests
-rs = requests.get('https://newyear.mail.ru/video/man/kolya/sweets/manifest.m3u8')
+
+
+rs = requests.get("https://newyear.mail.ru/video/man/kolya/sweets/manifest.m3u8")
 print(rs.content)
 
-rs = requests.get('https://newyear.mail.ru/video/man/kolya/games/manifest.m3u8')
+rs = requests.get("https://newyear.mail.ru/video/man/kolya/games/manifest.m3u8")
 print(rs.content)
--- html_parsing/https_newyear_mail_ru/download__ts__check_variations_age.py
+++ html_parsing/https_newyear_mail_ru/download__ts__check_variations_age.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import requests
 
 
 for i in range(20):
-    url = f'https://nypromo2019.hb.bizmrg.com/video/girl-age{i}/ny2019__020.ts'
+    url = f"https://nypromo2019.hb.bizmrg.com/video/girl-age{i}/ny2019__020.ts"
     rs = requests.get(url)
     print(i, rs)
