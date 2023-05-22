e2e683dc1c8c1a355773cf2f63cfd54d9a5d4874
gil9red <ilya.petrash@inbox.ru>
2023-05-22 14:16:27 +0500

    Refactoring. Using black code style

--- html_parsing/https___lavkagsm_ru__example/gui.py
+++ html_parsing/https___lavkagsm_ru__example/gui.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 from PyQt5.Qt import *
@@ -14,7 +14,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        headers = ['NAME', 'PRICE', 'ARTICLE', 'URL', 'PHOTO_URL']
+        headers = ["NAME", "PRICE", "ARTICLE", "URL", "PHOTO_URL"]
 
         self.table_widget = QTableWidget()
         self.table_widget.setColumnCount(len(headers))
@@ -27,19 +27,19 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.table_widget)
 
     def fill(self):
-        url = 'https://lavkagsm.ru/catalog/mikroskhemy/?view=blocks&page_count=48&sort=name&by=asc'
+        url = "https://lavkagsm.ru/catalog/mikroskhemy/?view=blocks&page_count=48&sort=name&by=asc"
         html_content = get_html_by_url__from_cache(url)
-        root = BeautifulSoup(html_content, 'html.parser')
+        root = BeautifulSoup(html_content, "html.parser")
 
         for item_data in get_parsed_items(url, root):
             row = self.table_widget.rowCount()
             self.table_widget.setRowCount(row + 1)
 
-            self.table_widget.setItem(row, 0, QTableWidgetItem(item_data['name']))
-            self.table_widget.setItem(row, 1, QTableWidgetItem(item_data['price']))
-            self.table_widget.setItem(row, 2, QTableWidgetItem(item_data['art']))
-            self.table_widget.setItem(row, 3, QTableWidgetItem(item_data['url']))
-            self.table_widget.setItem(row, 4, QTableWidgetItem(item_data['photo_url']))
+            self.table_widget.setItem(row, 0, QTableWidgetItem(item_data["name"]))
+            self.table_widget.setItem(row, 1, QTableWidgetItem(item_data["price"]))
+            self.table_widget.setItem(row, 2, QTableWidgetItem(item_data["art"]))
+            self.table_widget.setItem(row, 3, QTableWidgetItem(item_data["url"]))
+            self.table_widget.setItem(row, 4, QTableWidgetItem(item_data["photo_url"]))
 
         self.table_widget.resizeColumnToContents(0)
         self.table_widget.resizeColumnToContents(1)
@@ -53,7 +53,7 @@ class MainWindow(QMainWindow):
         QDesktopServices.openUrl(QUrl(url))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication([])
 
     mw = MainWindow()
--- html_parsing/https___lavkagsm_ru__example/main.py
+++ html_parsing/https___lavkagsm_ru__example/main.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
+
+import re
+import os
 
 from urllib.parse import urljoin
 
+import requests
 
-def get_html_by_url__from_cache(url, cache_dir='cache'):
-    import os
+
+def get_html_by_url__from_cache(url, cache_dir="cache"):
     os.makedirs(cache_dir, exist_ok=True)
 
     file_name = os.path.basename(url)
-
-    import re
-    file_name = re.sub(r'[^a-zA-Z\d]', '_', file_name)
-
-    file_name = cache_dir + '/' + file_name + '.html'
+    file_name = re.sub(r"[^a-zA-Z\d]", "_", file_name)
+    file_name = cache_dir + "/" + file_name + ".html"
 
     if os.path.exists(file_name):
-        with open(file_name, 'rb') as f:
+        with open(file_name, "rb") as f:
             return f.read()
 
-    with open(file_name, 'wb') as f:
-        import requests
+    with open(file_name, "wb") as f:
         rs = requests.get(url)
         f.write(rs.content)
 
@@ -32,26 +32,30 @@ def get_html_by_url__from_cache(url, cache_dir='cache'):
 
 def parse_item(url, node_item) -> dict:
     data = {
-        'name': node_item.select_one('.link').text.strip(),
-        'url': urljoin(url, node_item.select_one('.link')['href']),
-        'photo_url': urljoin(url, node_item.select_one('.photo img')['data-original']),
-        'art': node_item.select_one('.art > strong').text.strip(),
-        'price': node_item.select_one('.price > .value').text.strip(),
+        "name": node_item.select_one(".link").text.strip(),
+        "url": urljoin(url, node_item.select_one(".link")["href"]),
+        "photo_url": urljoin(url, node_item.select_one(".photo img")["data-original"]),
+        "art": node_item.select_one(".art > strong").text.strip(),
+        "price": node_item.select_one(".price > .value").text.strip(),
     }
 
     return data
 
 
 def get_parsed_items(url, root) -> [dict]:
-    return [parse_item(url, item) for item in root.select('#catalog_section .catalog-item')]
+    return [
+        parse_item(url, item)
+        for item in root.select("#catalog_section .catalog-item")
+    ]
 
 
-if __name__ == '__main__':
-    url = 'https://lavkagsm.ru/catalog/mikroskhemy/?view=blocks&page_count=48&sort=name&by=asc'
-
+if __name__ == "__main__":
     from bs4 import BeautifulSoup
+
+    url = "https://lavkagsm.ru/catalog/mikroskhemy/?view=blocks&page_count=48&sort=name&by=asc"
+
     html_content = get_html_by_url__from_cache(url)
-    root = BeautifulSoup(html_content, 'html.parser')
+    root = BeautifulSoup(html_content, "html.parser")
 
     items = get_parsed_items(url, root)
 
