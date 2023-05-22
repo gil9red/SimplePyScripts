c62af7a479631e941280f1615ba216fbbc4f4086
gil9red <ilya.petrash@inbox.ru>
2023-05-22 14:35:51 +0500

    Refactoring. Using black code style

--- html_parsing/https__www.klinikapawlikowski.pl_cennik/main.py
+++ html_parsing/https__www.klinikapawlikowski.pl_cennik/main.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
-from bs4 import BeautifulSoup as bs
-import requests
 import csv
+
+import requests
+from bs4 import BeautifulSoup as bs
 
 
 HEADERS = {
-    'accept': '*/*',
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
-                  '(KHTML, like Gecko) Ubuntu Chromium/77.0.3865.90 Chrome/77.0.3865.90 Safari/537.36'
+    "accept": "*/*",
+    "user-agent": (
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
+        "(KHTML, like Gecko) Ubuntu Chromium/77.0.3865.90 Chrome/77.0.3865.90 Safari/537.36"
+    ),
 }
-BASE_URL = 'https://www.klinikapawlikowski.pl/cennik/'
+BASE_URL = "https://www.klinikapawlikowski.pl/cennik/"
 
 
 def parser(base_url, headers) -> dict:
@@ -23,25 +26,33 @@ def parser(base_url, headers) -> dict:
     session = requests.Session()
     request = session.get(base_url, headers=headers)
     if request.status_code != 200:
-        print('ERROR')
+        print("ERROR")
         return data
 
-    soup = bs(request.content, 'lxml')
+    soup = bs(request.content, "lxml")
 
-    for header in soup.select('.dropdown-list__header'):
+    for header in soup.select(".dropdown-list__header"):
         # "Konsultacje", "Dermatologia kliniczna" и т.п.
-        header_title = header.select_one('.dropdown-list__header__title').getText(strip=True)
+        header_title = header.select_one(".dropdown-list__header__title").getText(
+            strip=True
+        )
 
         data[header_title] = dict()
 
-        for block in soup.select('.dropdown-list__body'):
+        for block in soup.select(".dropdown-list__body"):
             # "Dermatolog", "Badanie histopatologiczne", и т.п.
-            procedure = block.select_one('.dropdown-list__body__block > .dropdown-list__body__block__title').getText(strip=True)
+            procedure = block.select_one(
+                ".dropdown-list__body__block > .dropdown-list__body__block__title"
+            ).getText(strip=True)
             items = []
 
-            for item in block.select('.dropdown-list__body__block__item'):
-                title = item.select_one('.dropdown-list__body__block__item__title').getText(strip=True)
-                price = item.select_one('.dropdown-list__body__block__item__price').getText(strip=True)
+            for item in block.select(".dropdown-list__body__block__item"):
+                title = item.select_one(
+                    ".dropdown-list__body__block__item__title"
+                ).getText(strip=True)
+                price = item.select_one(
+                    ".dropdown-list__body__block__item__price"
+                ).getText(strip=True)
                 items.append((title, price))
 
             data[header_title][procedure] = items
@@ -50,9 +61,9 @@ def parser(base_url, headers) -> dict:
 
 
 def files_writer(data: dict):
-    with open('parsed_cennik.csv', 'a', encoding='utf-8', newline='') as file:
+    with open("parsed_cennik.csv", "a", encoding="utf-8", newline="") as file:
         a_pen = csv.writer(file)
-        a_pen.writerow(['Title', 'Title_proedure', 'Opisanie_procedur', 'Ceny'])
+        a_pen.writerow(["Title", "Title_proedure", "Opisanie_procedur", "Ceny"])
 
         # "Konsultacje", "Dermatologia kliniczna" и т.п.
         for header, procedures in data.items():
@@ -62,11 +73,12 @@ def files_writer(data: dict):
                     a_pen.writerow([header, procedure, title, price])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    import json
+
     data = parser(BASE_URL, HEADERS)
 
-    import json
-    with open('result.json', 'w', encoding='utf-8') as f:
+    with open("result.json", "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
 
     files_writer(data)
