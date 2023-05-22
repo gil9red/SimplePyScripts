f52be1ebda99e91f744f2d9ffb9e7eb0f18915c2
gil9red <ilya.petrash@inbox.ru>
2023-05-22 14:13:39 +0500

    Refactoring. Using black code style

--- html_parsing/horoscope/horo.mail.ru_pisces_today.py
+++ html_parsing/horoscope/horo.mail.ru_pisces_today.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import requests
-rs = requests.get('https://horo.mail.ru/prediction/pisces/today/')
-
 from bs4 import BeautifulSoup
-root = BeautifulSoup(rs.content, 'lxml')
-text = root.select_one('.article__text').text.strip()
+
+
+rs = requests.get("https://horo.mail.ru/prediction/pisces/today/")
+root = BeautifulSoup(rs.content, "lxml")
+text = root.select_one(".article__text").text.strip()
 print(repr(text))
 print(text)
