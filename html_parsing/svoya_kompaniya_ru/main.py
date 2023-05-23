6f98bbb548a22721eb38990a484cb0ca8c6d4a79
gil9red <ilya.petrash@inbox.ru>
2023-05-23 13:17:37 +0500

    Refactoring. Using black code style

--- html_parsing/svoya_kompaniya_ru/main.py
+++ html_parsing/svoya_kompaniya_ru/main.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 from common import get_menu
 
 
-url = 'https://magnitogorsk.svoya-kompaniya.ru/'
+url = "https://magnitogorsk.svoya-kompaniya.ru/"
 
 # TODO: Логирование перенести из get_menu
 result = get_menu(url, is_business_lunch=True)
-print('\n' + '-' * 100 + '\n')
+print("\n" + "-" * 100 + "\n")
 
 for full_menu_name, dishes in result.items():
     for dish in dishes:
-        print(f'{full_menu_name}: {dish}')
+        print(f"{full_menu_name}: {dish}")
