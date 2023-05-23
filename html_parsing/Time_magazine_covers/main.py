1bb245fd45a7f734a2fe07e63d69e3cad8ef3baa
gil9red <ilya.petrash@inbox.ru>
2023-05-23 13:18:27 +0500

    Refactoring. Using black code style

--- html_parsing/Time_magazine_covers/main.py
+++ html_parsing/Time_magazine_covers/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import datetime as DT
@@ -13,7 +13,7 @@ import traceback
 from os import makedirs
 from os.path import abspath, normpath
 from urllib.request import urlretrieve
-from typing import List, Union, NamedTuple
+from typing import NamedTuple
 
 import requests
 
@@ -25,8 +25,8 @@ class Cover(NamedTuple):
 
 def parse_date(date_str: str) -> DT.date:
     fmts = (
-        '%B %d, %Y',
-        '%b %d, %Y',
+        "%B %d, %Y",
+        "%b %d, %Y",
     )
     for fmt in fmts:
         try:
@@ -37,14 +37,14 @@ def parse_date(date_str: str) -> DT.date:
     raise ValueError(f'Unknown date format for "{date_str}"')
 
 
-def get_covers(year: Union[int, str]) -> List[Cover]:
+def get_covers(year: int | str) -> list[Cover]:
     items = []
 
     try:
-        url = f'https://time.com/vault/year/{year}/'
+        url = f"https://time.com/vault/year/{year}/"
 
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0',
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0",
         }
 
         session = requests.session()
@@ -52,63 +52,74 @@ def get_covers(year: Union[int, str]) -> List[Cover]:
 
         rs = session.get(url)
         if not rs.ok:
-            raise Exception(f'Something went wrong...: status_code: {rs.status_code}'
-                            f'\nUrl: {url}\nRs.Url: {rs.url}\n{rs.text}')
+            raise Exception(
+                f"Something went wrong...: status_code: {rs.status_code}"
+                f"\nUrl: {url}\nRs.Url: {rs.url}\n{rs.text}"
+            )
 
-        match = re.search(r'<script>Time\.bootstrap = ({.+})</script>', rs.text)
+        match = re.search(r"<script>Time\.bootstrap = ({.+})</script>", rs.text)
         if not match:
-            raise Exception(f'Not found "Time.bootstrap = ...): status_code: {rs.status_code}'
-                            f'\nUrl: {url}\nRs.Url: {rs.url}\n{rs.text}"')
+            raise Exception(
+                f'Not found "Time.bootstrap = ...): status_code: {rs.status_code}'
+                f'\nUrl: {url}\nRs.Url: {rs.url}\n{rs.text}"'
+            )
 
         result = json.loads(match.group(1), encoding=rs.encoding)
-        if 'vault' not in result:
-            raise Exception(f'Field "vault" not found in result: status_code: {rs.status_code}'
-                            f'\nUrl: {url}\nRs.Url: {rs.url}'
-                            f'\nResult: {result}\nHtml:\n{rs.text}')
+        if "vault" not in result:
+            raise Exception(
+                f'Field "vault" not found in result: status_code: {rs.status_code}'
+                f"\nUrl: {url}\nRs.Url: {rs.url}"
+                f"\nResult: {result}\nHtml:\n{rs.text}"
+            )
 
-        for vault in result['vault']:
-            cover_date = parse_date(vault['date'])
+        for vault in result["vault"]:
+            cover_date = parse_date(vault["date"])
             items.append(
                 Cover(
                     title=f"Cover_{cover_date:%Y-%m-%d}",
-                    url=vault['hero']['src']['large']
+                    url=vault["hero"]["src"]["large"],
                 )
             )
 
     except Exception:
-        print(f'[-] {traceback.format_exc()}')
+        print(f"[-] {traceback.format_exc()}")
 
     return items
 
 
-def dump_covers(year: Union[str, int], out_dir='out', need_zip=False, remove_out_covers=False):
+def dump_covers(
+    year: str | int,
+    out_dir="out",
+    need_zip=False,
+    remove_out_covers=False,
+):
     covers = get_covers(year)
     out_dir = normpath(abspath(out_dir + f"/{year}"))
 
-    print(f'Dump {len(covers)} covers to {out_dir!r}')
+    print(f"Dump {len(covers)} covers to {out_dir!r}")
     makedirs(out_dir, exist_ok=True)
 
     for x in covers:
-        urlretrieve(x.url, f'{out_dir}/{x.title}.jpg')
+        urlretrieve(x.url, f"{out_dir}/{x.title}.jpg")
 
     if need_zip:
-        print(f'Make archive from {out_dir!r}')
-        zip_name = shutil.make_archive(out_dir, 'zip', root_dir=out_dir)
-        print(f'Finish make archive to {zip_name!r}')
+        print(f"Make archive from {out_dir!r}")
+        zip_name = shutil.make_archive(out_dir, "zip", root_dir=out_dir)
+        print(f"Finish make archive to {zip_name!r}")
 
         if remove_out_covers:
-            print(f'Remove {out_dir!r}')
+            print(f"Remove {out_dir!r}")
             shutil.rmtree(out_dir)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     YEAR = DT.datetime.now().year
 
     items = get_covers(YEAR)
-    print(f'Covers for {YEAR} year ({len(items)}):')
+    print(f"Covers for {YEAR} year ({len(items)}):")
 
     for x in items:
-        print(f'    {x.title}: {x.url}')
+        print(f"    {x.title}: {x.url}")
 
     # Covers (39):
     #     Cover_140119: https://api.time.com/wp-content/uploads/2019/01/TIM190114v1.jpg?quality=85&w=550
