addf0ae5c4f7bc2a14d3a2567c99300a8850544b
gil9red <ilya.petrash@inbox.ru>
2023-05-23 18:38:03 +0500

    Refactoring. Using black code style

--- html_parsing/https_www_stoloto_ru_4x20_archive.py
+++ html_parsing/https_www_stoloto_ru_4x20_archive.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
+
+import csv
+from urllib.request import urlopen
 
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
-import csv
 
 
-def parse_page(page_number):
-    url = 'https://www.stoloto.ru/4x20/archive/{}'.format(page_number)
-    root = BeautifulSoup(urlopen(url), 'html.parser')
+def parse_page(page_number: int) -> tuple[str, list[str]]:
+    url = f"https://www.stoloto.ru/4x20/archive/{page_number}"
+    root = BeautifulSoup(urlopen(url), "html.parser")
 
     # Название, пример: "Результаты тиража № 1, 31 декабря 2016 в 15:10"
-    title = root.select_one('#content > h2').text.strip()
+    title = root.select_one("#content > h2").text.strip()
 
     # Вытаскиваем дату, пример: "31 декабря 2016 в 15:10"
-    date_time_str = title.split(', ')[1]
+    date_time_str = title.split(", ")[1]
 
     # Вытаскиваем номера, пример: ['20', '2', '10', '4', '2', '16', '9', '17']
-    numbers = [x.text.strip() for x in root.select('.winning_numbers > ul > li')]
+    numbers = [x.text.strip() for x in root.select(".winning_numbers > ul > li")]
 
     return date_time_str, numbers
 
@@ -34,11 +35,11 @@ for page_number in range(1, max_page_number + 1):
 
     # Список чисел преобразуем в строку:
     # ['20', '2', '10', '4', '2', '16', '9', '17'] -> '20 2 10 4 2 16 9 17'
-    numbers = ' '.join(numbers)
+    numbers = " ".join(numbers)
     result.append((page_number, date_time_str, numbers))
 
 print(result)
 
-with open('lotto.csv', 'w', encoding='utf-8', newline='') as f:
+with open("lotto.csv", "w", encoding="utf-8", newline="") as f:
     file = csv.writer(f)
     file.writerows(result)
--- html_parsing/https_www_stoloto_ru_4x20_archive__parse_all_loto.py
+++ html_parsing/https_www_stoloto_ru_4x20_archive__parse_all_loto.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
+import csv
 from urllib.parse import urljoin
+
 import requests
 from bs4 import BeautifulSoup
-import csv
 
 
 def get_number(text: str) -> int:
-    return int(''.join(c for c in text.strip() if c.isdigit()))
+    return int("".join(c for c in text.strip() if c.isdigit()))
 
 
 first = 1
@@ -21,13 +22,13 @@ result = []
 
 
 while True:
-    url = f'https://www.stoloto.ru/4x20/archive?firstDraw={first}&lastDraw={last}&mode=draw'
-    print(f'first={first}, last={last}: {url}')
+    url = f"https://www.stoloto.ru/4x20/archive?firstDraw={first}&lastDraw={last}&mode=draw"
+    print(f"first={first}, last={last}: {url}")
 
     rs = requests.get(url)
-    root = BeautifulSoup(rs.content, 'html.parser')
+    root = BeautifulSoup(rs.content, "html.parser")
 
-    rows = root.select('.drawings_data .elem > .main')
+    rows = root.select(".drawings_data .elem > .main")
 
     # Если пустое, значит достигли конца
     if not rows:
@@ -37,13 +38,15 @@ while True:
     rows.reverse()
 
     for row in rows:
-        date_time_str = row.select_one('.draw_date').text.strip()
-        a = row.select_one('.draw > a')
-        abs_url = urljoin(url, a['href'])
+        date_time_str = row.select_one(".draw_date").text.strip()
+        a = row.select_one(".draw > a")
+        abs_url = urljoin(url, a["href"])
         number = get_number(a.text)
 
-        numbers = ' '.join(x.text.strip() for x in row.select('.numbers .numbers_wrapper b'))
-        prize = get_number(row.select_one('.prize').text)
+        numbers = " ".join(
+            x.text.strip() for x in row.select(".numbers .numbers_wrapper b")
+        )
+        prize = get_number(row.select_one(".prize").text)
 
         item = [number, date_time_str, numbers, prize, abs_url]
         result.append(item)
@@ -62,6 +65,6 @@ print(max(result, key=lambda x: x[3]))
 print(min(result, key=lambda x: x[3]))
 print()
 
-with open('all_lotto.csv', 'w', encoding='utf-8', newline='') as f:
+with open("all_lotto.csv", "w", encoding="utf-8", newline="") as f:
     file = csv.writer(f)
     file.writerows(result)
