fc492aeea8dc23d03ce57e470fc820cfeaeba6ac
gil9red <ilya.petrash@inbox.ru>
2023-05-23 14:57:29 +0500

    Refactoring. Using black code style

--- html_parsing/fitgirl_RE_Village_with_win10_notify.py
+++ html_parsing/fitgirl_RE_Village_with_win10_notify.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import datetime as DT
@@ -17,43 +17,45 @@ from selenium.webdriver.firefox.options import Options
 
 ROOT_DIR = Path(__file__).resolve().parent.parent
 
-sys.path.append(str(ROOT_DIR / 'wait'))
+sys.path.append(str(ROOT_DIR / "wait"))
 from wait import wait
 
-sys.path.append(str(ROOT_DIR / 'winapi__windows__ctypes/windows__toast_balloontip_notifications'))
+sys.path.append(
+    str(ROOT_DIR / "winapi__windows__ctypes/windows__toast_balloontip_notifications")
+)
 from run_notify import run_in_thread
 
 
 def show_notify(name: str):
     title = f"Уведомление за {DT.datetime.now():%Y/%m/%d %H:%M:%S}"
-    print(f'{title}\n{name}\n')
+    print(f"{title}\n{name}\n")
 
     run_in_thread(title, name, duration=10 * 3600)
 
 
-name = 'Resident Evil Village'
-URL = f'https://fitgirl-repacks.site/?s={name}'
+name = "Resident Evil Village"
+URL = f"https://fitgirl-repacks.site/?s={name}"
 
 
 options = Options()
-options.add_argument('--headless')
+options.add_argument("--headless")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     while True:
         driver = webdriver.Firefox(options=options)
         driver.implicitly_wait(5)
 
         try:
             driver.get(URL)
-            if 'DDOS-GUARD' in driver.title.upper():
+            if "DDOS-GUARD" in driver.title.upper():
                 driver.refresh()
-                print(f'Title: {driver.title!r}')
+                print(f"Title: {driver.title!r}")
 
-            for title_el in driver.find_elements_by_css_selector('.entry-title'):
+            for title_el in driver.find_elements_by_css_selector(".entry-title"):
                 title = title_el.text.strip()
 
-                if 'VILLAGE' in title_el.text.upper():
+                if "VILLAGE" in title_el.text.upper():
                     show_notify(title)
 
             wait(hours=8)
