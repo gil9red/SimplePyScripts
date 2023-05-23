3438881046a3cd6d56078d7e0f35f097c1e2fd06
gil9red <ilya.petrash@inbox.ru>
2023-05-23 18:13:36 +0500

    Refactoring. Using black code style

--- html_parsing/get_stackoverflow_people_reached.py
+++ html_parsing/get_stackoverflow_people_reached.py
@@ -1,51 +1,50 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
-from typing import Optional
 import re
 
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
 
-def get_stackoverflow_people_reached(url: str) -> Optional[str]:
+def get_stackoverflow_people_reached(url: str) -> str | None:
     rs = requests.get(url)
     rs.raise_for_status()
 
-    root = BeautifulSoup(rs.content, 'html.parser')
-    profile_avatar_el = root.select_one('#main-content .s-card')
+    root = BeautifulSoup(rs.content, "html.parser")
+    profile_avatar_el = root.select_one("#main-content .s-card")
     if not profile_avatar_el:
         return
 
-    text = profile_avatar_el.get_text(strip=True, separator='\n')
-    m = re.search(r'(\d+\.?\d*[km]?)\s*(затронуто|reached)', text)
+    text = profile_avatar_el.get_text(strip=True, separator="\n")
+    m = re.search(r"(\d+\.?\d*[km]?)\s*(затронуто|reached)", text)
     if not m:
-        raise Exception('Reached not found!')
+        raise Exception("Reached not found!")
 
     return m.group(1)
 
 
-if __name__ == '__main__':
-    url = 'https://ru.stackoverflow.com/users/201445/gil9red'
+if __name__ == "__main__":
+    url = "https://ru.stackoverflow.com/users/201445/gil9red"
     print(get_stackoverflow_people_reached(url))
     # 1.5m
 
-    url = 'https://ru.stackoverflow.com'
+    url = "https://ru.stackoverflow.com"
     print(get_stackoverflow_people_reached(url))
     # None
 
     print()
 
     urls = [
-        'https://ru.stackoverflow.com/users/213987/a-k',
-        'https://ru.stackoverflow.com/users/17609/%d0%ae%d1%80%d0%b8%d0%b9%d0%a1%d0%9f%d0%b1',
-        'https://ru.stackoverflow.com/users/1984/nofate',
-        'https://stackoverflow.com/users/541136/aaron-hall',
-        'https://stackoverflow.com/users/106224/boltclock',
-        'https://stackoverflow.com/users/168175/flexo',
+        "https://ru.stackoverflow.com/users/213987/a-k",
+        "https://ru.stackoverflow.com/users/17609/%d0%ae%d1%80%d0%b8%d0%b9%d0%a1%d0%9f%d0%b1",
+        "https://ru.stackoverflow.com/users/1984/nofate",
+        "https://stackoverflow.com/users/541136/aaron-hall",
+        "https://stackoverflow.com/users/106224/boltclock",
+        "https://stackoverflow.com/users/168175/flexo",
     ]
     for url in urls:
         print(get_stackoverflow_people_reached(url))
--- html_parsing/get_video_list__xcadr.com.py
+++ html_parsing/get_video_list__xcadr.com.py
@@ -1,71 +1,77 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
-def get_video_list(url):
-    import requests
+import re
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_video_list(url) -> list[dict]:
     rs = requests.get(url)
+    root = BeautifulSoup(rs.content, "lxml")
 
-    from bs4 import BeautifulSoup
-    root = BeautifulSoup(rs.content, 'lxml')
-
-    import re
-    pattern_get_duration = re.compile('(\d+).:(\d+).')
+    pattern_get_duration = re.compile(r"(\d+).:(\d+).")
 
     items = []
 
-    for item in root.select('#playlist_view_playlist_view_items > .item'):
-        url = item.a['href']
-        title = item.a['title']
-        url_thumb = item.select_one('img')['src']
+    for item in root.select("#playlist_view_playlist_view_items > .item"):
+        url = item.a["href"]
+        title = item.a["title"]
+        url_thumb = item.select_one("img")["src"]
 
         # Example: "60%", "80%"
-        rating = item.select_one('.rating').text
-        rating = int(rating.replace('%', ''))
+        rating = item.select_one(".rating").text
+        rating = int(rating.replace("%", ""))
 
         # Example: "0м:31с", "6м:32с"
-        duration = item.select_one('.duration').text
+        duration = item.select_one(".duration").text
         minutes, seconds = map(int, pattern_get_duration.findall(duration)[0])
         duration = minutes * 60 + seconds
 
         items.append({
-            'title': title,
-            'url': url,
-            'duration': duration,
-            'rating': rating,
-            'url_thumb': url_thumb
+            "title": title,
+            "url": url,
+            "duration": duration,
+            "rating": rating,
+            "url_thumb": url_thumb,
         })
 
     return items
 
 
-if __name__ == '__main__':
-    items = get_video_list('http://xcadr.com/collection/seks-v-poze-naezdnicy/')
-    print('Total:', len(items))
+if __name__ == "__main__":
+    items = get_video_list("http://xcadr.com/collection/seks-v-poze-naezdnicy/")
+    print("Total:", len(items))
 
-    items = get_video_list('http://xcadr.com/collection/sceny-bdsm-v-filmah/')
-    print('Total:', len(items))
+    items = get_video_list("http://xcadr.com/collection/sceny-bdsm-v-filmah/")
+    print("Total:", len(items))
     print()
 
-    items = get_video_list('http://xcadr.com/collection/luchshie-sceny-v-bane/')
-    print('Total:', len(items))
+    items = get_video_list("http://xcadr.com/collection/luchshie-sceny-v-bane/")
+    print("Total:", len(items))
 
     def print_items(items):
         for i, item in enumerate(items, 1):
-            print('{0:2}. "{title}" ({duration} secs, rating: {rating}): {url} [{url_thumb}]'.format(i, **item))
+            print(
+                '{0:2}. "{title}" ({duration} secs, rating: {rating}): {url} [{url_thumb}]'.format(
+                    i, **item
+                )
+            )
 
-    print('Sorted by duration (top 5):')
-    new_items = sorted(items, key=lambda x: x['duration'], reverse=True)[:5]
+    print("Sorted by duration (top 5):")
+    new_items = sorted(items, key=lambda x: x["duration"], reverse=True)[:5]
     print_items(new_items)
 
     print()
-    print('Sorted by duration:')
-    new_items = sorted(items, key=lambda x: x['duration'], reverse=True)
+    print("Sorted by duration:")
+    new_items = sorted(items, key=lambda x: x["duration"], reverse=True)
     print_items(new_items)
 
     print()
-    print('Sorted by rating:')
-    new_items = sorted(items, key=lambda x: x['rating'], reverse=True)
+    print("Sorted by rating:")
+    new_items = sorted(items, key=lambda x: x["rating"], reverse=True)
     print_items(new_items)
