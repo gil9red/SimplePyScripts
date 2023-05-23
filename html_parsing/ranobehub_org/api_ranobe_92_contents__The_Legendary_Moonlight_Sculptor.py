4231935ba1deba7f41cd2472ffe68d35ddf424e4
gil9red <ilya.petrash@inbox.ru>
2023-05-23 11:43:58 +0500

    Refactoring. Using black code style

--- html_parsing/ranobehub_org/api_ranobe_92_contents__The_Legendary_Moonlight_Sculptor.py
+++ html_parsing/ranobehub_org/api_ranobe_92_contents__The_Legendary_Moonlight_Sculptor.py
@@ -1,30 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
-
-from typing import List
 
 import requests
 
 
-def get_chapters() -> List[str]:
-    rs = requests.get('https://ranobehub.org/api/ranobe/92/contents')
+def get_chapters() -> list[str]:
+    rs = requests.get("https://ranobehub.org/api/ranobe/92/contents")
     rs.raise_for_status()
 
     items = []
 
-    for volume in rs.json()['volumes']:
-        for chapter in volume['chapters']:
+    for volume in rs.json()["volumes"]:
+        for chapter in volume["chapters"]:
             items.append(f'{volume["name"]} - {chapter["name"]}')
 
     return items
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     items = get_chapters()
 
-    print(f'Items ({len(items)}):')
+    print(f"Items ({len(items)}):")
     for x in items:
-        print(f'    {x}')
+        print(f"    {x}")
--- html_parsing/ranobehub_org/download_ranobe_images.py
+++ html_parsing/ranobehub_org/download_ranobe_images.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import re
@@ -15,14 +15,16 @@ from bs4 import BeautifulSoup
 
 
 def get_valid_filename(s: str) -> str:
-    s = s.strip().replace(':', '.')
-    return re.sub(r'(?u)[^-\w. ]', '', s)
+    s = s.strip().replace(":", ".")
+    return re.sub(r"(?u)[^-\w. ]", "", s)
 
 
 DIR = Path(__file__).resolve().parent
 
 session = requests.Session()
-session.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0'
+session.headers[
+    "User-Agent"
+] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0"
 
 
 def parse(start_url: str, download_path: Path = DIR):
@@ -30,26 +32,26 @@ def parse(start_url: str, download_path: Path = DIR):
 
     while True:
         rs = session.get(url)
-        soup = BeautifulSoup(rs.content, 'html.parser')
+        soup = BeautifulSoup(rs.content, "html.parser")
 
-        title = soup.select_one('title').get_text(strip=True)
+        title = soup.select_one("title").get_text(strip=True)
         print(title)
 
         img_urls = []
-        for img_el in soup.select('img[data-media-id]'):
-            media_id = img_el['data-media-id']
-            url = urljoin(rs.url, f'/api/media/{media_id}')
+        for img_el in soup.select("img[data-media-id]"):
+            media_id = img_el["data-media-id"]
+            url = urljoin(rs.url, f"/api/media/{media_id}")
             if url not in img_urls:
                 img_urls.append(url)
 
         if img_urls:
-            print(f'    Изображений: {len(img_urls)}')
+            print(f"    Изображений: {len(img_urls)}")
 
             # Example: 'Да будет благословен этот прекрасный мир! / Том 666 / Веб-новелла: Короткая история богини'
             #       -> ['Да будет благословен этот прекрасный мир', 'Том 666', 'Веб-новелла Короткая история богини']
-            parts: list[str] = [get_valid_filename(el) for el in title.split('/')]
+            parts: list[str] = [get_valid_filename(el) for el in title.split("/")]
             ranobe_title: str = parts[0]
-            chapter_title: str = '. '.join(parts[1:])
+            chapter_title: str = ". ".join(parts[1:])
 
             dir_ranobe = download_path / ranobe_title
             dir_ranobe.mkdir(parents=True, exist_ok=True)
@@ -58,18 +60,18 @@ def parse(start_url: str, download_path: Path = DIR):
                 rs = session.get(url)
                 time.sleep(0.1)
 
-                img_path = dir_ranobe / f'{chapter_title}. {i}.png'
+                img_path = dir_ranobe / f"{chapter_title}. {i}.png"
                 img_path.write_bytes(rs.content)
 
-        next_chapter_link_el = soup.select_one('a[data-next-chapter-link]')
-        if not next_chapter_link_el.get('href'):
+        next_chapter_link_el = soup.select_one("a[data-next-chapter-link]")
+        if not next_chapter_link_el.get("href"):
             break
 
-        url = urljoin(rs.url, next_chapter_link_el['href'])
+        url = urljoin(rs.url, next_chapter_link_el["href"])
 
         time.sleep(1)
 
 
-if __name__ == '__main__':
-    url = 'https://ranobehub.org/ranobe/131/1/1'
+if __name__ == "__main__":
+    url = "https://ranobehub.org/ranobe/131/1/1"
     parse(url)
