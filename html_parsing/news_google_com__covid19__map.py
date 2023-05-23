62a8253d64c05a61d25118414c8e6eaf52fb6c26
gil9red <ilya.petrash@inbox.ru>
2023-05-23 19:00:53 +0500

    Refactoring. Using black code style

--- html_parsing/news_google_com__covid19__map.py
+++ html_parsing/news_google_com__covid19__map.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import re
@@ -11,20 +11,21 @@ import requests
 
 
 def parse():
-    url = 'https://news.google.com/covid19/map?hl=ru&gl=RU&ceid=RU:ru'
+    url = "https://news.google.com/covid19/map?hl=ru&gl=RU&ceid=RU:ru"
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0'
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0"
     }
+
     def _get_num(item) -> str:
-        return re.sub(r'\s+', '.', item.get_text(strip=True))
+        return re.sub(r"\s+", ".", item.get_text(strip=True))
 
     rs = requests.get(url, headers=headers)
-    root = BeautifulSoup(rs.content, 'html.parser')
+    root = BeautifulSoup(rs.content, "html.parser")
 
-    item = root.select_one('div.tZjT9b')
-    confirmed = _get_num(item.select_one('div.fNm5wd.qs41qe > div.UvMayb'))
-    deaths    = _get_num(item.select_one('div.fNm5wd.ckqIZ > div.UvMayb'))
-    recovered = _get_num(item.select_one('div.fNm5wd.gZvxhb > div.UvMayb'))
+    item = root.select_one("div.tZjT9b")
+    confirmed = _get_num(item.select_one("div.fNm5wd.qs41qe > div.UvMayb"))
+    deaths = _get_num(item.select_one("div.fNm5wd.ckqIZ > div.UvMayb"))
+    recovered = _get_num(item.select_one("div.fNm5wd.gZvxhb > div.UvMayb"))
 
     print("Заболевших:   ", confirmed)
     print("Выздоровевших:", recovered)
@@ -34,5 +35,5 @@ def parse():
     # Умерших:       233.998
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parse()
