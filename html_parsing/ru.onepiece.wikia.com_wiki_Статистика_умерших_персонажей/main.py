3e7f21432581a4c3bd169b09eab9bd5b6a5b8cd8
gil9red <ilya.petrash@inbox.ru>
2023-05-23 13:15:50 +0500

    Refactoring. Using black code style

--- html_parsing/ru.onepiece.wikia.com_wiki_Статистика_умерших_персонажей/main.py
+++ html_parsing/ru.onepiece.wikia.com_wiki_Статистика_умерших_персонажей/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 from typing import List
@@ -11,48 +11,52 @@ from bs4 import BeautifulSoup
 
 
 session = requests.session()
-session.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0'
+session.headers[
+    "User-Agent"
+] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0"
 
 
 def get_canonical_characters() -> List[str]:
-    url = 'https://onepiece.fandom.com/ru/wiki/Список_канонических_персонажей'
+    url = "https://onepiece.fandom.com/ru/wiki/Список_канонических_персонажей"
 
     rs = session.get(url)
-    root = BeautifulSoup(rs.content, 'html.parser')
+    root = BeautifulSoup(rs.content, "html.parser")
 
     return [
         td.get_text(strip=True)
-        for td in root.select_one('.wikitable').select('tbody > tr > td:nth-of-type(2)')
+        for td in root.select_one(".wikitable").select("tbody > tr > td:nth-of-type(2)")
     ]
 
 
 def get_dead_characters() -> List[str]:
-    url = 'https://onepiece.fandom.com/ru/wiki/Категория:Умершие_персонажи'
+    url = "https://onepiece.fandom.com/ru/wiki/Категория:Умершие_персонажи"
 
     rs = session.get(url)
-    root = BeautifulSoup(rs.content, 'html.parser')
+    root = BeautifulSoup(rs.content, "html.parser")
 
     return [
         x.get_text(strip=True)
-        for x in root.select('.category-page__member-link')
+        for x in root.select(".category-page__member-link")
     ]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     canonical_characters = get_canonical_characters()
     total_canonical_characters = len(canonical_characters)
 
-    dead_canonical_characters = sorted(set(canonical_characters) & set(get_dead_characters()))
+    dead_canonical_characters = sorted(
+        set(canonical_characters) & set(get_dead_characters())
+    )
     total_dead_canonical_characters = len(dead_canonical_characters)
 
-    print('Total canonical characters:', total_canonical_characters)
+    print("Total canonical characters:", total_canonical_characters)
     print(
-        f'Total dead canonical characters: {total_dead_canonical_characters} '
-        f'({total_dead_canonical_characters * 100 / total_canonical_characters:.2f}%)'
+        f"Total dead canonical characters: {total_dead_canonical_characters} "
+        f"({total_dead_canonical_characters * 100 / total_canonical_characters:.2f}%)"
     )
     print(
         f'[{", ".join(map(repr, dead_canonical_characters[:3]))},',
-        '...,',
+        "...,",
         f'{", ".join(map(repr, dead_canonical_characters[-3:]))}]',
     )
     # Total canonical characters: 876
