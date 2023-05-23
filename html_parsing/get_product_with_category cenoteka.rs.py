6fa2a09ae3ccc09fa7930517ef00b59550fbc6fc
gil9red <ilya.petrash@inbox.ru>
2023-05-23 18:12:08 +0500

    Refactoring. Using black code style

--- html_parsing/get_product_with_category cenoteka.rs.py
+++ html_parsing/get_product_with_category cenoteka.rs.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # SOURCE: https://ru.stackoverflow.com/questions/894302/
 
 
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
 
-url = 'https://cenoteka.rs/proizvodi/mlecni-proizvodi/jogurt?page=3'
+url = "https://cenoteka.rs/proizvodi/mlecni-proizvodi/jogurt?page=3"
 rs = requests.get(url)
-root = BeautifulSoup(rs.content, 'lxml')
+root = BeautifulSoup(rs.content, "lxml")
 
-for category in root.select('#products > .row.section'):
-    category_title = category.select_one('.section-title').get_text(strip=True)
+for category in root.select("#products > .row.section"):
+    category_title = category.select_one(".section-title").get_text(strip=True)
     print(category_title)
 
-    for product in category.select('[data-product-id]'):
+    for product in category.select("[data-product-id]"):
         try:
-            title = product.select_one('.article-name').get_text(strip=True)
-            price_list = [price.get_text(strip=True) for price in product.select('.article-price')]
-            print('    {}: {}'.format(title, price_list))
+            title = product.select_one(".article-name").get_text(strip=True)
+            price_list = [
+                price.get_text(strip=True)
+                for price in product.select(".article-price")
+            ]
+            print(f"    {title}: {price_list}")
 
         except:
             pass
--- html_parsing/get_set_sushi_list__sushivkusno.com.py
+++ html_parsing/get_set_sushi_list__sushivkusno.com.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import re
-from bs4 import BeautifulSoup
+
 import requests
+from bs4 import BeautifulSoup
 
 
-rs = requests.get('http://sushivkusno.com/category/nabory-siety')
+rs = requests.get("http://sushivkusno.com/category/nabory-siety")
 print(rs)
 
-root = BeautifulSoup(rs.content, 'lxml')
-items = root.select('.CardContent')
+root = BeautifulSoup(rs.content, "lxml")
+items = root.select(".CardContent")
 
 for i, product in enumerate(items, 1):
-    title = product.select_one('.CardText__title').text
-    weight = product.select_one('.CardText__subtitle > span > b').text
+    title = product.select_one(".CardText__title").text
+    weight = product.select_one(".CardText__subtitle > span > b").text
 
-    price = product.select_one('.ProductParams__price').text
-    price = int(re.sub('\D', '', price))
+    price = product.select_one(".ProductParams__price").text
+    price = int(re.sub(r"\D", "", price))
 
-    print('{}. "{}": {}, {}'.format(i, title, weight, price))
+    print(f'{i}. "{title}": {weight}, {price}')
