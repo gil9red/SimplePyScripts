e7d3e9867f551239913a629b7e90369b7ce53760
gil9red <ilya.petrash@inbox.ru>
2023-05-23 17:50:26 +0500

    Refactoring. Using black code style

--- html_parsing/get_comments__russianfood_com.py
+++ html_parsing/get_comments__russianfood_com.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 from urllib.parse import urljoin
@@ -11,7 +11,7 @@ from bs4 import BeautifulSoup
 import requests
 
 
-PATTERN_NEXT = re.compile('Следующая →')
+PATTERN_NEXT = re.compile("Следующая →")
 
 
 def get_comments(url: str, no_quote=False) -> list:
@@ -19,43 +19,43 @@ def get_comments(url: str, no_quote=False) -> list:
 
     while True:
         rs = requests.get(url)
-        root = BeautifulSoup(rs.content, 'html.parser')
+        root = BeautifulSoup(rs.content, "html.parser")
 
         # Перебор комментариев
-        for comment_text_el in root.select('.comment > div'):
+        for comment_text_el in root.select(".comment > div"):
             # Перебор цитат
-            for blockquote_el in comment_text_el.select('blockquote'):
+            for blockquote_el in comment_text_el.select("blockquote"):
                 if no_quote:
                     blockquote_el.decompose()
                     continue
 
                 blockquote_el.replace_with(
-                    "«" + blockquote_el.get_text(separator='\n', strip=True) + "»"
+                    "«" + blockquote_el.get_text(separator="\n", strip=True) + "»"
                 )
 
-            for br in comment_text_el.select('br'):
-                br.replace_with('\n')
+            for br in comment_text_el.select("br"):
+                br.replace_with("\n")
 
-            comment_text = comment_text_el.get_text(separator='\n', strip=True)
+            comment_text = comment_text_el.get_text(separator="\n", strip=True)
             items.append(comment_text)
 
-        a_next = root.find(name='a', text=PATTERN_NEXT)
+        a_next = root.find(name="a", text=PATTERN_NEXT)
         if not a_next:
             break
 
-        url = urljoin(rs.url, a_next.get('href'))
+        url = urljoin(rs.url, a_next.get("href"))
 
     return items
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     for url in [
-        'https://www.russianfood.com/recipes/recipe.php?rid=107392',
-        'https://www.russianfood.com/recipes/recipe.php?rid=121323',
+        "https://www.russianfood.com/recipes/recipe.php?rid=107392",
+        "https://www.russianfood.com/recipes/recipe.php?rid=121323",
     ]:
         print(url)
 
         for i, comment_text in enumerate(get_comments(url), 1):
             print(i, repr(comment_text))
 
-        print('\n' + '-' * 100 + '\n')
+        print("\n" + "-" * 100 + "\n")
--- html_parsing/get_comments__stopgame_ru.py
+++ html_parsing/get_comments__stopgame_ru.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 from bs4 import BeautifulSoup
@@ -10,40 +10,40 @@ import requests
 
 def get_comments(url: str, no_quote=False) -> list:
     rs = requests.get(url)
-    root = BeautifulSoup(rs.content, 'html.parser')
+    root = BeautifulSoup(rs.content, "html.parser")
 
     items = []
 
     # Перебор комментариев
-    for comment_text_el in root.select('.comment-text'):
+    for comment_text_el in root.select(".comment-text"):
         # Перебор цитат
-        for blockquote_el in comment_text_el.select('blockquote'):
+        for blockquote_el in comment_text_el.select("blockquote"):
             if no_quote:
                 blockquote_el.decompose()
                 continue
 
             blockquote_el.replace_with(
-                "«" + blockquote_el.get_text(separator='\n', strip=True) + "»"
+                "«" + blockquote_el.get_text(separator="\n", strip=True) + "»"
             )
 
-        for br in comment_text_el.select('br'):
-            br.replace_with('\n')
+        for br in comment_text_el.select("br"):
+            br.replace_with("\n")
 
-        comment_text = comment_text_el.get_text(separator='\n', strip=True)
+        comment_text = comment_text_el.get_text(separator="\n", strip=True)
         items.append(comment_text)
 
     return items
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     for url in [
-        'https://stopgame.ru/show/102770/atom_rpg_review',
-        'https://stopgame.ru/show/43787/project_dark_review',
-        'https://stopgame.ru/show/82379/dark_souls_iii_videoreview',
+        "https://stopgame.ru/show/102770/atom_rpg_review",
+        "https://stopgame.ru/show/43787/project_dark_review",
+        "https://stopgame.ru/show/82379/dark_souls_iii_videoreview",
     ]:
         print(url)
 
         for i, comment_text in enumerate(get_comments(url), 1):
             print(i, repr(comment_text))
 
-        print('\n' + '-' * 100 + '\n')
+        print("\n" + "-" * 100 + "\n")
