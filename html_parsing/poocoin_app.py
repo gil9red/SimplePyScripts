5a9495668881eab4f0dcf971fa864374b1a9ff2f
gil9red <ilya.petrash@inbox.ru>
2023-05-23 19:24:24 +0500

    Refactoring. Using black code style

--- html_parsing/poocoin_app.py
+++ html_parsing/poocoin_app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import traceback
@@ -11,15 +11,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-URL = 'https://poocoin.app/tokens/0x2a69c59b8b493141d9f41b8f4fa724b60cd204e0'
+URL = "https://poocoin.app/tokens/0x2a69c59b8b493141d9f41b8f4fa724b60cd204e0"
 
 driver = webdriver.Firefox()
 try:
     driver.implicitly_wait(5)
     driver.get(URL)
-    print(f'Title: {driver.title!r}')
+    print(f"Title: {driver.title!r}")
 
-    token_price = driver.find_element(By.CSS_SELECTOR, '.mb-1.d-flex.flex-column.lh-1 > .text-success')
+    token_price = driver.find_element(
+        By.CSS_SELECTOR, ".mb-1.d-flex.flex-column.lh-1 > .text-success"
+    )
     print(token_price.text)
     # $0.00165060
 
