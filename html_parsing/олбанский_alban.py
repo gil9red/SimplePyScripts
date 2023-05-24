359b00986a7fc5750a6df531c7feb21e95439dea
gil9red <ilya.petrash@inbox.ru>
2023-05-24 19:56:29 +0500

    Refactoring. Using black code style

--- html_parsing/олбанский_alban.py
+++ html_parsing/олбанский_alban.py
@@ -1,46 +1,49 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
-
-from bs4 import BeautifulSoup
-import requests
 
 from urllib.parse import quote
+
+import requests
+from bs4 import BeautifulSoup
 
 
 def get_alban(text: str) -> str:
     headers = {
-        'Host': 'javer.kiev.ua',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0',
+        "Host": "javer.kiev.ua",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0",
     }
 
     params = f"?input={quote(text, encoding='cp1251')}&s={quote('Пиривеcти', encoding='cp1251')}"
-    rs = requests.get('http://javer.kiev.ua/alban.php' + params, headers=headers)
+    rs = requests.get("http://javer.kiev.ua/alban.php" + params, headers=headers)
 
-    root = BeautifulSoup(rs.text, 'html.parser')
-    return root.select_one('textarea:nth-of-type(2)').get_text(strip=True)
+    root = BeautifulSoup(rs.text, "html.parser")
+    return root.select_one("textarea:nth-of-type(2)").get_text(strip=True)
 
 
-if __name__ == '__main__':
-    text = get_alban('привет')
+if __name__ == "__main__":
+    text = get_alban("привет")
     print(text)
-    assert text == 'превед'
+    assert text == "превед"
 
     print()
 
-    text = '''
+    text = """
 Я памятник себе воздвиг нерукотворный,
 К нему не зарастет народная тропа,
 Вознесся выше он главою непокорной
 Александрийского столпа.
-    '''.strip()
+    """.strip()
     text = get_alban(text)
     print(text)
-    assert text == '''
+    assert (
+        text
+        == """
 Я памятнег себе воздвиг нирукотворный,
 К ниму ни зарастед народнайа тропа,
 Вознессо выше ан главайу нипакорной
 Александрийсково столпа.
-    '''.strip()
+    """.strip()
+    )
