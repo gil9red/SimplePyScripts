78a55ac1be8758da6145cdca4eb4c16eb488419e
gil9red <ilya.petrash@inbox.ru>
2023-05-24 19:56:48 +0500

    Refactoring. Using black code style

--- html_parsing/стопкоронавирус_рф__information__bs4.py
+++ html_parsing/стопкоронавирус_рф__information__bs4.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import json
 import sys
 
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
 
-rs = requests.get('https://стопкоронавирус.рф/information/')
-root = BeautifulSoup(rs.content, 'html.parser')
+rs = requests.get("https://стопкоронавирус.рф/information/")
+root = BeautifulSoup(rs.content, "html.parser")
 
-stats = root.select_one('cv-stats-virus')
+stats = root.select_one("cv-stats-virus")
 if not stats:
     print('Not found "cv-stats-virus"!')
     sys.exit()
 
-data = stats[':charts-data']
+data = stats[":charts-data"]
 data = json.loads(data)
 print(data)
 # [{'date': '01.05.2020', 'sick': 114431, 'healed': 13220, 'died': 1169}, ...
 
 for x in data:
-    print(x['date'], x['sick'], x['healed'], x['died'])
+    print(x["date"], x["sick"], x["healed"], x["died"])
     # 01.05.2020 114431 13220 1169
     # 30.04.2020 106498 11619 1073
     # 29.04.2020 99399 10286 972
--- html_parsing/стопкоронавирус_рф__information__regexp.py
+++ html_parsing/стопкоронавирус_рф__information__regexp.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import json
@@ -11,7 +11,7 @@ import sys
 import requests
 
 
-rs = requests.get('https://стопкоронавирус.рф/information/')
+rs = requests.get("https://стопкоронавирус.рф/information/")
 m = re.search(":charts-data='(.+?)'", rs.text)
 if not m:
     print('Not found "charts-data"!')
@@ -22,7 +22,7 @@ print(data)
 # [{'date': '01.05.2020', 'sick': 114431, 'healed': 13220, 'died': 1169}, ...
 
 for x in data:
-    print(x['date'], x['sick'], x['healed'], x['died'])
+    print(x["date"], x["sick"], x["healed"], x["died"])
     # 01.05.2020 114431 13220 1169
     # 30.04.2020 106498 11619 1073
     # 29.04.2020 99399 10286 972
