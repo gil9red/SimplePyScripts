f3c94ad784983e38b5f5615d10b1a65a75e8675b
gil9red <ilya.petrash@inbox.ru>
2023-05-24 11:29:35 +0500

    Refactoring. Using black code style

--- html_parsing/sslproxies_org.py
+++ html_parsing/sslproxies_org.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
-
-from typing import List, Tuple
 
 import requests
 from bs4 import BeautifulSoup
 
 
-URL = 'https://www.sslproxies.org/'
+URL = "https://www.sslproxies.org/"
 
 session = requests.Session()
-session.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0'
+session.headers[
+    "User-Agent"
+] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0"
 
 
-def get_proxies(by_code: str = None) -> List[Tuple[str, int, str]]:
+def get_proxies(by_code: str = None) -> list[tuple[str, int, str]]:
     items = []
 
     rs = session.get(URL)
-    root = BeautifulSoup(rs.content, 'html.parser')
+    root = BeautifulSoup(rs.content, "html.parser")
 
-    for tr in root.select('#proxylisttable > tbody > tr'):
-        td_host, td_port, td_code = tr.select('td')[:3]
+    for tr in root.select("#proxylisttable > tbody > tr"):
+        td_host, td_port, td_code = tr.select("td")[:3]
         host = td_host.get_text(strip=True)
         port = int(td_port.get_text(strip=True))
         code = td_code.get_text(strip=True)
@@ -35,12 +35,12 @@ def get_proxies(by_code: str = None) -> List[Tuple[str, int, str]]:
     return items
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     items = get_proxies()
     print(len(items), items)
     # 100 [('191.100.20.14', 8080, 'EC'), ..., ('185.198.188.55', 8080, 'GB')]
 
-    items_ru = get_proxies(by_code='RU')
+    items_ru = get_proxies(by_code="RU")
     print(len(items_ru), items_ru)
     # 9 [('95.165.163.188', 60103, 'RU'), ..., ('94.141.117.1', 8080, 'RU')]
 
@@ -48,16 +48,17 @@ if __name__ == '__main__':
 
     # Test
     import random
+
     for host, port, _ in random.choices(items, k=5):
         proxies = {
-            'http': f'http://{host}:{port}',
-            'https': f'https://{host}:{port}',
+            "http": f"http://{host}:{port}",
+            "https": f"https://{host}:{port}",
         }
         try:
-            rs = requests.get('https://api.ipify.org/', proxies=proxies, timeout=5)
-            print(f'{host}:{port} -> {rs.text}')
+            rs = requests.get("https://api.ipify.org/", proxies=proxies, timeout=5)
+            print(f"{host}:{port} -> {rs.text}")
         except:
-            print(f'[#] {host}:{port} -> proxy error!')
+            print(f"[#] {host}:{port} -> proxy error!")
     """
     [#] 104.218.240.70:999 -> proxy error!
     [#] 88.99.134.61:8080 -> proxy error!
