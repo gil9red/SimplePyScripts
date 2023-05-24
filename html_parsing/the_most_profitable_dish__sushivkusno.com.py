4391be1600b624885896f53757588ea42fbcde56
gil9red <ilya.petrash@inbox.ru>
2023-05-24 18:04:01 +0500

    Refactoring. Using black code style

--- html_parsing/the_most_profitable_dish__sushivkusno.com.py
+++ html_parsing/the_most_profitable_dish__sushivkusno.com.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import sys
@@ -17,7 +17,7 @@ from selenium.common.exceptions import MoveTargetOutOfBoundsException
 from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
 from selenium.webdriver.remote.webdriver import WebElement
 
-sys.path.append('..')
+sys.path.append("..")
 from ascii_table import ascii_table
 
 
@@ -28,13 +28,13 @@ def do_page_down(driver: RemoteWebDriver, footer: WebElement):
     y_position = 0
 
     while True:
-        DEBUG_LOG and print('y_position:', y_position)
+        DEBUG_LOG and print("y_position:", y_position)
         try:
             ActionChains(driver).move_to_element(footer).perform()
             break
         except MoveTargetOutOfBoundsException:
             y_position += 250
-            driver.execute_script(f'window.scrollTo(0, {y_position});')
+            driver.execute_script(f"window.scrollTo(0, {y_position});")
             time.sleep(1)
 
 
@@ -42,7 +42,7 @@ def print_the_most_profitable_dish(url: str):
     print(url)
 
     options = Options()
-    options.add_argument('--headless')
+    options.add_argument("--headless")
 
     unknown_metrics_items = []
     items = []
@@ -54,17 +54,21 @@ def print_the_most_profitable_dish(url: str):
         driver.get(url)
 
         # Пролистывание страницы до низа
-        footer_el = driver.find_element(By.CSS_SELECTOR, 'footer')
+        footer_el = driver.find_element(By.CSS_SELECTOR, "footer")
         do_page_down(driver, footer_el)
 
-        for i, product_el in enumerate(driver.find_elements(By.CSS_SELECTOR, '.product-card'), 1):
-            title = product_el.find_element(By.CSS_SELECTOR, '.card-title').text.strip()
+        for i, product_el in enumerate(
+            driver.find_elements(By.CSS_SELECTOR, ".product-card"), 1
+        ):
+            title = product_el.find_element(By.CSS_SELECTOR, ".card-title").text.strip()
 
-            price = product_el.find_element(By.CSS_SELECTOR, '.price-value').text
-            price = int(re.sub(r'\D', '', price))
+            price = product_el.find_element(By.CSS_SELECTOR, ".price-value").text
+            price = int(re.sub(r"\D", "", price))
 
             try:
-                tag_subtitle = product_el.find_element(By.CSS_SELECTOR, '.parameters > .param-size').text.strip()
+                tag_subtitle = product_el.find_element(
+                    By.CSS_SELECTOR, ".parameters > .param-size"
+                ).text.strip()
                 weight, metrics = tag_subtitle.split()
 
             except Exception:
@@ -72,46 +76,48 @@ def print_the_most_profitable_dish(url: str):
                 continue
 
             # Определение метрики
-            if metrics not in ['кг.', 'гр.', 'г.']:
+            if metrics not in ["кг.", "гр.", "г."]:
                 unknown_metrics_items.append((title, tag_subtitle, price))
                 continue
 
             weight = float(weight)
-            if metrics == 'кг.':
+            if metrics == "кг.":
                 weight *= 1000
 
-            DEBUG_LOG and print(f'{i}. "{title}": {weight} г., {price} -> {weight / price:.3f}')
+            DEBUG_LOG and print(
+                f'{i}. "{title}": {weight} г., {price} -> {weight / price:.3f}'
+            )
             items.append((title, weight, price, weight / price))
 
-        print('Самые выгодные по количеству грамм за единицу цены:\n')
+        print("Самые выгодные по количеству грамм за единицу цены:\n")
 
         # Сортировка по коэффициенту
         items.sort(key=lambda x: x[3], reverse=True)
 
         items = [
-            [title, weight, price, f'{rate:.3f}']
+            [title, weight, price, f"{rate:.3f}"]
             for title, weight, price, rate in items
         ]
 
-        columns = ['Название', 'Вес (г.)', 'Цена', 'Коэффициент']
+        columns = ["Название", "Вес (г.)", "Цена", "Коэффициент"]
         items.insert(0, columns)
         print(ascii_table(items))
 
         if unknown_metrics_items:
-            print('\nНе удалось обработать:')
+            print("\nНе удалось обработать:")
             for i, item in enumerate(unknown_metrics_items, 1):
                 print(f'  {i}. {", ".join(map(str, item))}')
 
-        print('\n')
+        print("\n")
 
     finally:
         if driver:
             driver.quit()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     urls = [
-        'https://sushivkusno.com/nabory-sety',
+        "https://sushivkusno.com/nabory-sety",
         # 'https://sushivkusno.com/goryachie-zakuski',
         # 'https://sushivkusno.com/salaty',
     ]
