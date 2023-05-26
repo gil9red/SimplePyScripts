5024ed7307f447fe078330a716950e532626f612
gil9red <ilya.petrash@inbox.ru>
2023-05-26 16:55:46 +0500

    Refactoring. Using black code style

--- HypotenuseOfTriangle/HypotenuseOfTriangle.py
+++ HypotenuseOfTriangle/HypotenuseOfTriangle.py
@@ -1,34 +1,40 @@
 # coding=utf-8
-import math
-import argparse
 
-__author__ = 'ipetrash'
+
+__author__ = "ipetrash"
+
+
+import argparse
+import math
+
 
 def createParser():
-    parse = argparse.ArgumentParser(description=u"Программа высчитывает гипотенузу прямоугольного треугольника")
-    parse.add_argument("-a", type=int, help=u"Первый катет")
-    parse.add_argument("-b", type=int, help=u"второй катет")
+    parse = argparse.ArgumentParser(
+        description="Программа высчитывает гипотенузу прямоугольного треугольника"
+    )
+    parse.add_argument("-a", type=int, help="Первый катет")
+    parse.add_argument("-b", type=int, help="второй катет")
     return parse
 
 
 def hypotenuse(a, b):
-    u"""
+    """
     Функция высчитывает гипотенузу прямоугольного треугольника.
     :param a: Первый катет
     :param b: Второй катет
     :return: Гипотенуза
     """
-    return math.sqrt(a ** 2 + b ** 2)
+    return math.sqrt(a**2 + b**2)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parse = createParser()
     args = parse.parse_args()
     if args.a is not None and args.b is not None:
         a = args.a
         b = args.b
-        print(u"Катет а=%s, катет b=%s, гипотенуза с=%s" % (a, b, hypotenuse(a, b)))
+        print(f"Катет а={a}, катет b={b}, гипотенуза с={hypotenuse(a, b)}")
     else:
-        a = int(raw_input(u"Введи катет a: "))
-        b = int(raw_input(u"Введи катет b: "))
-        print(u"Гипотенуза с=%s" % hypotenuse(a, b))
+        a = int(input("Введи катет a: "))
+        b = int(input("Введи катет b: "))
+        print(f"Гипотенуза с={hypotenuse(a, b)}")
