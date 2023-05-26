bf20363c384dc0f5d9f09831d1f4c95d58b73acb
gil9red <ilya.petrash@inbox.ru>
2023-05-26 16:59:54 +0500

    Refactoring. Using black code style

--- img_to_base64_html/main.py
+++ img_to_base64_html/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import base64
@@ -11,14 +11,14 @@ import io
 from PIL import Image
 
 
-def img_to_base64_html(file_name__or__bytes__or__file_object):
+def img_to_base64_html(file_name__or__bytes__or__file_object: str | bytes | io.IOBase):
     arg = file_name__or__bytes__or__file_object
 
-    if type(arg) == str:
-        with open(arg, mode='rb') as f:
+    if isinstance(arg, str):
+        with open(arg, mode="rb") as f:
             img_bytes = f.read()
 
-    elif type(arg) == bytes:
+    elif isinstance(arg, bytes):
         img_bytes = arg
 
     else:
@@ -27,14 +27,14 @@ def img_to_base64_html(file_name__or__bytes__or__file_object):
     bytes_io = io.BytesIO(img_bytes)
     img = Image.open(bytes_io)
 
-    img_base64 = base64.b64encode(img_bytes).decode('utf-8')
-    return f'data:image/{img.format.lower()};base64,{img_base64}'
+    img_base64 = base64.b64encode(img_bytes).decode("utf-8")
+    return f"data:image/{img.format.lower()};base64,{img_base64}"
 
 
-if __name__ == '__main__':
-    file_name = 'img.jpg'
+if __name__ == "__main__":
+    file_name = "img.jpg"
     img_base64 = img_to_base64_html(file_name)
-    print(f'[len {len(img_base64)}]: {img_base64[:50]}...')
+    print(f"[len {len(img_base64)}]: {img_base64[:50]}...")
 
-    with open(file_name + '_base64.txt', mode='w', encoding='utf-8') as f:
+    with open(file_name + "_base64.txt", mode="w", encoding="utf-8") as f:
         f.write(img_base64)
