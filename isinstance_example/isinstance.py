69d0afbdce3f45284cd782624aa95b611c6e47b2
gil9red <ilya.petrash@inbox.ru>
2023-05-26 17:58:17 +0500

    Refactoring. Using black code style

--- isinstance_example/isinstance.py
+++ isinstance_example/isinstance.py
@@ -1,9 +1,8 @@
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
-if __name__ == '__main__':
-    print(isinstance(5, str))
-    print(isinstance(5, int))
-    print(isinstance(5, bool))
-    print(isinstance(False, bool))
-    print(isinstance("Hello", str))
+print(isinstance(5, str))
+print(isinstance(5, int))
+print(isinstance(5, bool))
+print(isinstance(False, bool))
+print(isinstance("Hello", str))
