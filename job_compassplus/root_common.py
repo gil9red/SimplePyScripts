6c8271ee4988d99e0e8f2dab7f7aec41377f5942
gil9red <ilya.petrash@inbox.ru>
2023-05-28 16:21:10 +0500

    Refactoring. Using black code style

--- job_compassplus/root_common.py
+++ job_compassplus/root_common.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import ssl
@@ -14,21 +14,23 @@ from root_config import PATH_CERT
 class TLSAdapter(requests.adapters.HTTPAdapter):
     def init_poolmanager(self, *args, **kwargs):
         ctx = ssl.create_default_context()
-        ctx.set_ciphers('DEFAULT@SECLEVEL=1')
-        kwargs['ssl_context'] = ctx
+        ctx.set_ciphers("DEFAULT@SECLEVEL=1")
+        kwargs["ssl_context"] = ctx
         return super(TLSAdapter, self).init_poolmanager(*args, **kwargs)
 
 
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0'
+USER_AGENT = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0"
+)
 
 
 session = requests.session()
 session.cert = str(PATH_CERT)
-session.mount('https://', TLSAdapter())
-session.headers['User-Agent'] = USER_AGENT
+session.mount("https://", TLSAdapter())
+session.headers["User-Agent"] = USER_AGENT
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Check
-    rs = session.get('https://helpdesk.compassluxe.com/pa-reports/')
+    rs = session.get("https://helpdesk.compassluxe.com/pa-reports/")
     print(rs)
