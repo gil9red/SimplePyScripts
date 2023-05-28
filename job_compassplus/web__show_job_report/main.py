fe0b0405c818f7d4663f462d08ee6b417653b86d
gil9red <ilya.petrash@inbox.ru>
2023-05-28 16:20:36 +0500

    Refactoring. Using black code style

--- job_compassplus/web__show_job_report/main.py
+++ job_compassplus/web__show_job_report/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 """Вывод переработки текущего (или конкретного) пользователя"""
@@ -13,15 +13,14 @@ from pathlib import Path
 
 from flask import Flask
 
+DIR = Path(__file__).resolve().parent
+sys.path.append(str(DIR.parent / "job_report"))
+from utils import get_report_persons_info, get_person_info
+
 
 app = Flask(__name__)
 
 logging.basicConfig(level=logging.DEBUG)
-
-
-DIR = Path(__file__).resolve().parent
-sys.path.append(str(DIR.parent / 'job_report'))
-from utils import get_report_persons_info, get_person_info
 
 
 @app.route("/")
@@ -29,20 +28,20 @@ def index():
     report_dict = get_report_persons_info()
 
     try:
-        person = report_dict['Текущий пользователь'][0]
+        person = report_dict["Текущий пользователь"][0]
     except:
         person = None
 
     if person is None:
-        person = get_person_info(second_name='Петраш', report_dict=report_dict)
+        person = get_person_info(second_name="Петраш", report_dict=report_dict)
 
     if person:
-        return '{} {}'.format(person.full_name, person.deviation_of_time)
+        return f"{person.full_name} {person.deviation_of_time}"
 
-    return 'Not found specific user'
+    return "Not found specific user"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Localhost
     app.run(port=5001)
 
