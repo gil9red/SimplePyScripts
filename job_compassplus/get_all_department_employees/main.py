e71e9e5eefaae33087e685304a974aee62c5811b
gil9red <ilya.petrash@inbox.ru>
2023-05-28 15:09:10 +0500

    Refactoring. Using black code style

--- job_compassplus/get_all_department_employees/main.py
+++ job_compassplus/get_all_department_employees/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import sys
@@ -33,7 +33,7 @@ class Employee:
 
 session.auth = HttpNtlmAuth(USERNAME, PASSWORD)
 
-URL = 'https://mysite.compassplus.com/Person.aspx?accountname={}'
+URL = "https://mysite.compassplus.com/Person.aspx?accountname={}"
 
 
 def get_employees(boss_username: str) -> list[Employee]:
@@ -41,35 +41,35 @@ def get_employees(boss_username: str) -> list[Employee]:
     rs = session.get(url)
     rs.raise_for_status()
 
-    root = BeautifulSoup(rs.content, 'html.parser')
-    root_table = root.select_one('#ReportingHierarchy')
+    root = BeautifulSoup(rs.content, "html.parser")
+    root_table = root.select_one("#ReportingHierarchy")
 
     items = []
 
-    for td in root_table.select('.ms-orgname'):
+    for td in root_table.select(".ms-orgname"):
         a = td.a
-        url = a['href']
+        url = a["href"]
         items.append(
             Employee(
                 full_name=a.get_text(strip=True),
                 # Example: "https://.../Person.aspx?accountname=CP%5Cipetrash" -> "ipetrash"
-                user_name=url.split('%5C')[-1],
+                user_name=url.split("%5C")[-1],
                 url=url,
-                position=td.select_one('.ms_metadata').get_text(strip=True),
+                position=td.select_one(".ms_metadata").get_text(strip=True),
             )
         )
 
     return items
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from base64 import b64decode
-    boss_username = b64decode('Q1BcbnZheW5lcg==').decode('utf-8')
+    boss_username = b64decode("Q1BcbnZheW5lcg==").decode("utf-8")
 
     employees = get_employees(boss_username)
-    print(f'Employees ({len(employees)}):')
+    print(f"Employees ({len(employees)}):")
     for i, employee in enumerate(employees, 1):
-        print(f'{i}. {employee}')
+        print(f"{i}. {employee}")
     """
     ...
     9. Employee(full_name='Ilya A. Petrash', user_name='ipetrash', position='Senior Software Engineer', url='https://mysite.compassplus.com:443/Person.aspx?accountname=CP%5Cipetrash')
@@ -79,4 +79,4 @@ if __name__ == '__main__':
 
     usernames = sorted(x.user_name for x in employees)
     print(usernames)
-    print(', '.join(usernames))
+    print(", ".join(usernames))
