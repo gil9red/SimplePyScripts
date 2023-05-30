798fc417d5dbfb7f8a26e7be511d7a1816ced6fb
gil9red <ilya.petrash@inbox.ru>
2023-05-30 13:07:43 +0500

    Refactoring

--- pymorphy2__examples/cогласование_слов_с_числительными.py
+++ pymorphy2__examples/cогласование_слов_с_числительными.py
@@ -4,14 +4,11 @@
 __author__ = 'ipetrash'
 
 
-# Для импортирования ascii_table__simple_pretty__format.py
-import sys
-sys.path.append('..')
-
-from ascii_table__simple_pretty__format import print_pretty_table
-
 # pip install pymorphy2
 import pymorphy2
+
+# pip install tabulate
+from tabulate import tabulate
 
 
 morph = pymorphy2.MorphAnalyzer()
@@ -26,7 +23,7 @@ words = [
 ]
 parsed_words = [morph.parse(word)[0] for word in words]
 
-rows = [words]
+rows = []
 for i in range(5 + 1):
     row = []
     for parsed_word in parsed_words:
@@ -34,12 +31,14 @@ for i in range(5 + 1):
         row.append(f'{i} {word}')
     rows.append(row)
 
-print_pretty_table(rows, align='<')
-# программист     | ёлка   | фрукт     | игр    | барсук
-# ----------------+--------+-----------+--------+-----------
-# 0 программистов | 0 ёлок | 0 фруктов | 0 игр  | 0 барсуков
-# 1 программист   | 1 ёлка | 1 фрукт   | 1 игры | 1 барсук
-# 2 программиста  | 2 ёлки | 2 фрукта  | 2 игр  | 2 барсука
-# 3 программиста  | 3 ёлки | 3 фрукта  | 3 игр  | 3 барсука
-# 4 программиста  | 4 ёлки | 4 фрукта  | 4 игр  | 4 барсука
-# 5 программистов | 5 ёлок | 5 фруктов | 5 игр  | 5 барсуков
+print(tabulate(rows, headers=words, tablefmt="orgtbl"))
+"""
+| программист     | ёлка   | фрукт     | игр    | барсук     |
+|-----------------+--------+-----------+--------+------------|
+| 0 программистов | 0 ёлок | 0 фруктов | 0 игр  | 0 барсуков |
+| 1 программист   | 1 ёлка | 1 фрукт   | 1 игры | 1 барсук   |
+| 2 программиста  | 2 ёлки | 2 фрукта  | 2 игр  | 2 барсука  |
+| 3 программиста  | 3 ёлки | 3 фрукта  | 3 игр  | 3 барсука  |
+| 4 программиста  | 4 ёлки | 4 фрукта  | 4 игр  | 4 барсука  |
+| 5 программистов | 5 ёлок | 5 фруктов | 5 игр  | 5 барсуков |
+"""
