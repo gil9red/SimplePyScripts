815cd27fca657f05f5f42ddc55b16ac053738a9c
gil9red <ilya.petrash@inbox.ru>
2023-05-30 16:33:44 +0500

    Refactoring. Using black code style

--- LuhnAlgorithm/LuhnAlgorithm.py
+++ LuhnAlgorithm/LuhnAlgorithm.py
@@ -1,13 +1,13 @@
 # coding=utf-8
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
-if __name__ == '__main__':
-    number_card = raw_input("Number card: ")
+if __name__ == "__main__":
+    number_card = input("Number card: ")
     sum = 0
     digits = [int(x) for x in number_card if x.isdigit()]
     digits.reverse()
-    for i in xrange(len(digits)):
+    for i in range(len(digits)):
         p = digits[i]
         if i % 2 is not 0:
             p *= 2
@@ -15,9 +15,9 @@ if __name__ == '__main__':
                 p -= 9
         sum += p
 
-    print("sum: %d" % sum)
+    print(f"sum: {sum}")
     sum = 10 - (sum % 10)
     sum = 0 if sum is 10 else sum
 
-    print("checksum: %s" % sum)
+    print(f"checksum: {sum}")
     print("card number is correct" if (sum % 10 is 0) else "card number is incorrect")
