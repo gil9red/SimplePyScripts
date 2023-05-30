f361eda6a1b837c0e13f9ff277141e16b90713d7
gil9red <ilya.petrash@inbox.ru>
2023-05-30 17:03:19 +0500

    Refactoring. Using black code style

--- matplotlib__examples/animate.py
+++ matplotlib__examples/animate.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
-import psutil
-from datetime import datetime as dt
 from collections import deque
+from datetime import datetime as dt
+
+import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 from matplotlib.dates import date2num
-import matplotlib.animation as animation
+
+import psutil
+
 
 N = 600
 x = deque([date2num(dt.now())], maxlen=N)
 y = deque([0], maxlen=N)
 
 fig, ax = plt.subplots(figsize=(8, 3))
-line, = ax.plot_date(x, y, marker="")
+(line,) = ax.plot_date(x, y, marker="")
 
-ax.spines['left'].set_visible(False)
-ax.spines['bottom'].set_visible(False)
-ax.spines['top'].set_visible(False)
-ax.spines['right'].set_visible(False)
+ax.spines["left"].set_visible(False)
+ax.spines["bottom"].set_visible(False)
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
 
 
 def get_data():
-    return psutil.cpu_percent(.15)
+    return psutil.cpu_percent(0.15)
 
 
 def animate(i):
@@ -34,8 +37,8 @@ def animate(i):
     ax.relim(visible_only=True)
     ax.autoscale_view(True)
     line.set_data(x, y)
-    ax.fill_between(x, -0.5, y, color='lightgrey')
-    return line,
+    ax.fill_between(x, -0.5, y, color="lightgrey")
+    return (line,)
 
 
 ani = animation.FuncAnimation(fig, animate, interval=300)
