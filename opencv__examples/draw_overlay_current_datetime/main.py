c8109510bcf2b3d9be6c5c34ea089a96938262ab
gil9red <ilya.petrash@inbox.ru>
2023-05-31 14:55:34 +0500

    Refactoring. Using black code style

--- opencv__examples/draw_overlay_current_datetime/main.py
+++ opencv__examples/draw_overlay_current_datetime/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import datetime as DT
@@ -9,10 +9,13 @@ import sys
 
 # pip install opencv-python
 import cv2
+
 import numpy
 
 
-def draw_overlay(img: numpy.ndarray, text: str, color_text=(255, 255, 255), max_text_height_percent=5):
+def draw_overlay(
+    img: numpy.ndarray, text: str, color_text=(255, 255, 255), max_text_height_percent=5
+):
     h, w, _ = img.shape
 
     max_text_height = (h / 100) * max_text_height_percent
@@ -33,25 +36,29 @@ def draw_overlay(img: numpy.ndarray, text: str, color_text=(255, 255, 255), max_
     text_y = h - text_rect_h // 2
 
     # cv2.LINE_AA -- Anti aliased line
-    cv2.putText(img, text, (0, text_y), font, scale, color_text, thickness, lineType=cv2.LINE_AA)
+    cv2.putText(
+        img, text, (0, text_y), font, scale, color_text, thickness, lineType=cv2.LINE_AA
+    )
 
 
 def draw_overlay_current_datetime(
-        img: numpy.ndarray, datetime_fmt='%d/%m/%y %H:%M:%S',
-        color_text=(255, 255, 255), max_text_height_percent=5
+    img: numpy.ndarray,
+    datetime_fmt="%d/%m/%y %H:%M:%S",
+    color_text=(255, 255, 255),
+    max_text_height_percent=5,
 ):
     text = DT.datetime.now().strftime(datetime_fmt)
     draw_overlay(img, text, color_text, max_text_height_percent)
 
 
-if __name__ == '__main__':
-    img = cv2.imread('../gaussian_blur/example.jpg')
+if __name__ == "__main__":
+    img = cv2.imread("../gaussian_blur/example.jpg")
 
     while True:
         img_overlay = img.copy()
         draw_overlay_current_datetime(img_overlay)
 
-        cv2.imshow('With overlay', img_overlay)
+        cv2.imshow("With overlay", img_overlay)
 
         if cv2.waitKey(25) == 27:  # Esc to quit
             sys.exit()
