bf6008feceeef78314978f53ea85ef5aaa12dcb8
gil9red <ilya.petrash@inbox.ru>
2023-05-31 11:37:02 +0500

    Refactoring. Using black code style

--- MergeListOfLists/MergeListOfLists.py
+++ MergeListOfLists/MergeListOfLists.py
@@ -1,9 +1,16 @@
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
-## EN: Ways to merge a list of lists
-## RU: Cпособы слияния списка списков
-# source: http://habrahabr.ru/post/63539/
-def listmerge1(lstlst):
+
+# EN: Ways to merge a list of lists
+# RU: Cпособы слияния списка списков
+
+
+import operator
+from functools import reduce
+
+
+# Source: http://habrahabr.ru/post/63539/
+def list_merge_1(lstlst):
     all = []
     for lst in lstlst:
         for el in lst:
@@ -11,43 +18,37 @@ def listmerge1(lstlst):
     return all
 
 
-def listmerge2(lstlst):
+def list_merge_2(lstlst):
     all = []
     for lst in lstlst:
         all = all + lst
     return all
 
 
-def listmerge3(lstlst):
+def list_merge_3(lstlst):
     all = []
     for lst in lstlst:
         all.extend(lst)
     return all
 
 
-from functools import reduce
+list_merge_4_a = lambda ll: reduce(lambda a, b: a + b, ll, [])
+list_merge_4_b = lambda ll: sum(ll, [])
 
-listmerge4a = lambda ll: reduce(lambda a, b: a + b, ll, [])
-listmerge4b = lambda ll: sum(ll, [])
+list_merge_5 = lambda ll: [el for lst in ll for el in lst]
 
-
-listmerge5 = lambda ll: [el for lst in ll for el in lst]
-
-
-listmerge6a = lambda s: reduce(lambda d, el: d.extend(el) or d, s, [])
-
-import operator
-listmerge6b = lambda s: reduce(operator.iadd, s, [])
+list_merge_6_a = lambda s: reduce(lambda d, el: d.extend(el) or d, s, [])
+list_merge_6_b = lambda s: reduce(operator.iadd, s, [])
 
 
-lstlst = ([6, 6], [1, 2, 3], [4, 5], [6], [7, 8], [9])
-print("List: ", lstlst)
+lst_lst = ([6, 6], [1, 2, 3], [4, 5], [6], [7, 8], [9])
+print("List: ", lst_lst)
 print("Result:")
-print("1.  ", listmerge1(lstlst))
-print("2.  ", listmerge2(lstlst))
-print("3.  ", listmerge3(lstlst))
-print("4a. ", listmerge4a(lstlst))
-print("4b. ", listmerge4b(lstlst))
-print("5.  ", listmerge5(lstlst))
-print("6a. ", listmerge6a(lstlst))
-print("6b. ", listmerge6b(lstlst))
+print("1.  ", list_merge_1(lst_lst))
+print("2.  ", list_merge_2(lst_lst))
+print("3.  ", list_merge_3(lst_lst))
+print("4a. ", list_merge_4_a(lst_lst))
+print("4b. ", list_merge_4_b(lst_lst))
+print("5.  ", list_merge_5(lst_lst))
+print("6a. ", list_merge_6_a(lst_lst))
+print("6b. ", list_merge_6_b(lst_lst))
