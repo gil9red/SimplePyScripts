363ea2f66b6e28394c94a27a99ab394596ccab7b
gil9red <ilya.petrash@inbox.ru>
2023-05-31 14:50:03 +0500

    Refactoring. Using black code style

--- opencv (cv2) vs pil (pillow)/main.py
+++ opencv (cv2) vs pil (pillow)/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # pip install opencv-python
@@ -9,93 +9,148 @@ __author__ = 'ipetrash'
 
 import timeit
 
-FILE_NAME = 'input.jpg'
+FILE_NAME = "input.jpg"
 NUMBER = 100
-GLOBALS = {
-    'FILE_NAME': FILE_NAME
-}
+GLOBALS = {"FILE_NAME": FILE_NAME}
 
-print('Read image from file:')
-print('PIL',
-    timeit.timeit("img = Image.open(FILE_NAME)", setup="from PIL import Image", number=NUMBER, globals=GLOBALS)
+print("Read image from file:")
+print(
+    "PIL",
+    timeit.timeit(
+        "img = Image.open(FILE_NAME)",
+        setup="from PIL import Image",
+        number=NUMBER,
+        globals=GLOBALS,
+    ),
 )
-print('CV2',
-    timeit.timeit("img = cv2.imread(FILE_NAME)", setup="import cv2", number=NUMBER, globals=GLOBALS)
+print(
+    "CV2",
+    timeit.timeit(
+        "img = cv2.imread(FILE_NAME)",
+        setup="import cv2",
+        number=NUMBER,
+        globals=GLOBALS,
+    ),
 )
 print()
 
-print('Read image from file and get image bytes:')
-print('PIL', timeit.timeit(
-    """\
+print("Read image from file and get image bytes:")
+print(
+    "PIL",
+    timeit.timeit(
+        """\
 img = Image.open(FILE_NAME)
 bytes_io = io.BytesIO()
 img.save(bytes_io, format='JPEG')
 img_data = bytes_io.getvalue()
     """,
-    setup="from PIL import Image\nimport io", number=NUMBER, globals=GLOBALS
-))
-print('CV2', timeit.timeit(
-    """\
+        setup="from PIL import Image\nimport io",
+        number=NUMBER,
+        globals=GLOBALS,
+    ),
+)
+print(
+    "CV2",
+    timeit.timeit(
+        """\
 img = cv2.imread(FILE_NAME)
 img_data = cv2.imencode('.jpg', img)[1].tostring()
     """,
-    setup="import cv2\nimport io", number=NUMBER, globals=GLOBALS
-))
+        setup="import cv2\nimport io",
+        number=NUMBER,
+        globals=GLOBALS,
+    ),
+)
 print()
 
-print('Invert:')
-print('PIL', timeit.timeit(
-    """\
+print("Invert:")
+print(
+    "PIL",
+    timeit.timeit(
+        """\
 img = Image.open(FILE_NAME)
 img_invert = ImageOps.invert(img)
     """,
-    setup="from PIL import Image, ImageOps", number=NUMBER, globals=GLOBALS
-))
-print('CV2', timeit.timeit(
-    """\
+        setup="from PIL import Image, ImageOps",
+        number=NUMBER,
+        globals=GLOBALS,
+    ),
+)
+print(
+    "CV2",
+    timeit.timeit(
+        """\
 img = cv2.imread(FILE_NAME)
 img_invert = cv2.bitwise_not(img)
     """,
-    setup="import cv2", number=NUMBER, globals=GLOBALS
-))
+        setup="import cv2",
+        number=NUMBER,
+        globals=GLOBALS,
+    ),
+)
 print()
 
-print('Grayscale:')
-print('PIL  ', timeit.timeit(
-    """\
+print("Grayscale:")
+print(
+    "PIL  ",
+    timeit.timeit(
+        """\
 img = Image.open(FILE_NAME)
 img_gray = ImageOps.grayscale(img)
     """,
-    setup="from PIL import Image, ImageOps", number=NUMBER, globals=GLOBALS
-))
-print('CV2_1', timeit.timeit(
-    """\
+        setup="from PIL import Image, ImageOps",
+        number=NUMBER,
+        globals=GLOBALS,
+    ),
+)
+print(
+    "CV2_1",
+    timeit.timeit(
+        """\
 img = cv2.imread(FILE_NAME)
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     """,
-    setup="import cv2", number=NUMBER, globals=GLOBALS
-))
-print('CV2_2', timeit.timeit(
-    """\
+        setup="import cv2",
+        number=NUMBER,
+        globals=GLOBALS,
+    ),
+)
+print(
+    "CV2_2",
+    timeit.timeit(
+        """\
 img_gray = cv2.imread(FILE_NAME, cv2.IMREAD_GRAYSCALE)
     """,
-    setup="import cv2", number=NUMBER, globals=GLOBALS
-))
+        setup="import cv2",
+        number=NUMBER,
+        globals=GLOBALS,
+    ),
+)
 print()
 
-print('GaussianBlur:')
-print('PIL', timeit.timeit(
-    """\
+print("GaussianBlur:")
+print(
+    "PIL",
+    timeit.timeit(
+        """\
 img = Image.open(FILE_NAME)
 img_blur = img.filter(ImageFilter.GaussianBlur(2))
     """,
-    setup="from PIL import Image, ImageFilter", number=NUMBER, globals=GLOBALS
-))
-print('CV2', timeit.timeit(
-    """\
+        setup="from PIL import Image, ImageFilter",
+        number=NUMBER,
+        globals=GLOBALS,
+    ),
+)
+print(
+    "CV2",
+    timeit.timeit(
+        """\
 img = cv2.imread(FILE_NAME)
 img_blur = cv2.GaussianBlur(img, (15, 15), 0)
     """,
-    setup="import cv2", number=NUMBER, globals=GLOBALS
-))
+        setup="import cv2",
+        number=NUMBER,
+        globals=GLOBALS,
+    ),
+)
 print()
