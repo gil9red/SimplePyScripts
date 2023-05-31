5d0959c4e7de5e077770bafad936857e3cf06765
gil9red <ilya.petrash@inbox.ru>
2023-05-31 12:43:32 +0500

    Refactoring. Using black code style

--- office__word__doc_docx/read_docx/read_docx.py
+++ office__word__doc_docx/read_docx/read_docx.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
+
+import re
 
 # pip install python-docx
 from docx import Document
+
+
 document = Document("Обеденное меню 777.docx")
 
 # Регулярка для поиска последовательностей пробелов: от двух подряд и более
-import re
-multi_space_pattern = re.compile(r'[ ]{2,}')
+multi_space_pattern = re.compile(r" {2,}")
 
 for table in document.tables:
     for row in table.rows:
-        name, weight, price = [multi_space_pattern.sub(' ', i.text.strip()) for i in row.cells]
+        name, weight, price = [
+            multi_space_pattern.sub(" ", i.text.strip()) for i in row.cells
+        ]
 
         if name == weight == price or (not weight or not price):
             print()
@@ -22,7 +27,7 @@ for table in document.tables:
             print(name)
             continue
 
-        print('{} {} {}'.format(name, weight, price))
+        print(f"{name} {weight} {price}")
 
     # Таблицы в меню дублируются
     break
