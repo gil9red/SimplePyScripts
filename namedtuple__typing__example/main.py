d86ab88826259b1f8c70a828b730638a90cbfe09
gil9red <ilya.petrash@inbox.ru>
2023-05-31 12:17:56 +0500

    Refactoring. Using black code style

--- namedtuple__typing__example/main.py
+++ namedtuple__typing__example/main.py
@@ -1,28 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 from collections import namedtuple
+from typing import NamedTuple
 
 
-Point = namedtuple('Point', ['x', 'y'])
+Point = namedtuple("Point", ["x", "y"])
 
 # # TypeError: __new__() missing 2 required positional arguments: 'x' and 'y'
 # p = Point()
 # print(p)
 
 p = Point(1, 2)
-print(p)            # Point(x=1, y=2)
+print(p)  # Point(x=1, y=2)
 print(p._asdict())  # OrderedDict([('x', 1), ('y', 2)])
 print()
 
 
 # SOURCE: https://docs.python.org/3/library/typing.html#typing.NamedTuple
-from typing import NamedTuple
-
-
 class Point(NamedTuple):
     x: int = 0
     y: int = 0
@@ -32,5 +30,5 @@ p = Point()
 print(p)  # Point(x=0, y=0)
 
 p = Point(1, 2)
-print(p)            # Point(x=1, y=2)
+print(p)  # Point(x=1, y=2)
 print(p._asdict())  # OrderedDict([('x', 1), ('y', 2)])
