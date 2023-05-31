c5460af67700e6be4c86fa085578b76ab784bcdc
gil9red <ilya.petrash@inbox.ru>
2023-05-31 13:49:03 +0500

    Refactoring. Using black code style

--- online_anidub_com/get_video_list.py
+++ online_anidub_com/get_video_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import sys
 import re
 
-from typing import List
 from pathlib import Path
 
 from bs4 import BeautifulSoup
@@ -16,8 +15,9 @@ from bs4 import BeautifulSoup
 ROOT_DIR = Path(__file__).resolve().parent
 
 
-sys.path.append(str(ROOT_DIR.parent / 'using_proxy'))
+sys.path.append(str(ROOT_DIR.parent / "using_proxy"))
 import proxy_requests__upgraded
+
 ProxyRequests = proxy_requests__upgraded.ProxyRequests
 
 
@@ -27,28 +27,28 @@ proxy_requests__upgraded.DEBUG_LOG = DEBUG_LOG
 
 def _get_title(el) -> str:
     title = el.get_text(strip=True)
-    return re.sub(r'\s{2,}', ' ', title)
+    return re.sub(r"\s{2,}", " ", title)
 
 
-def search_video_list(text: str) -> List[str]:
-    url = 'https://online.anidub.com/index.php?do=search'
+def search_video_list(text: str) -> list[str]:
+    url = "https://online.anidub.com/index.php?do=search"
 
     data = {
-        'do': 'search',
-        'subaction': 'search',
-        'search_start': '0',
-        'full_search': '0',
-        'result_from': '1',
-        'story': text,
+        "do": "search",
+        "subaction": "search",
+        "search_start": "0",
+        "full_search": "0",
+        "result_from": "1",
+        "story": text,
     }
     headers = {
-        'Host': 'online.anidub.com',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
-        'Accept-Encoding': 'gzip, deflate',
-        'Origin': 'https://online.anidub.com',
-        'Referer': 'https://online.anidub.com/index.php?do=search',
+        "Host": "online.anidub.com",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
+        "Accept-Encoding": "gzip, deflate",
+        "Origin": "https://online.anidub.com",
+        "Referer": "https://online.anidub.com/index.php?do=search",
     }
 
     while True:
@@ -57,24 +57,33 @@ def search_video_list(text: str) -> List[str]:
 
         content = rs.content
 
-        if b'Attention Required! | Cloudflare' in content \
-                or b'Access denied | online.anidub.com used Cloudflare to restrict access' in content:
-            DEBUG_LOG and print('Fail! Cloudflare!')
+        if (
+            b"Attention Required! | Cloudflare" in content
+            or b"Access denied | online.anidub.com used Cloudflare to restrict access"
+            in content
+        ):
+            DEBUG_LOG and print("Fail! Cloudflare!")
             continue
 
         break
 
-    root = BeautifulSoup(content, 'html.parser')
-    return [_get_title(a) for a in root.select('.th-title')]
+    root = BeautifulSoup(content, "html.parser")
+    return [_get_title(a) for a in root.select(".th-title")]
 
 
-if __name__ == '__main__':
-    text = 'Моя геройская академия'
+if __name__ == "__main__":
+    text = "Моя геройская академия"
 
     items = search_video_list(text)
-    print(f'Items ({len(items)}):')
+    print(f"Items ({len(items)}):")
     for x in items:
-        print(f'    {x}')
+        print(f"    {x}")
 
     import json
-    json.dump(items, open('video_list.json', 'w', encoding='utf-8'), ensure_ascii=False, indent=4)
+
+    json.dump(
+        items,
+        open("video_list.json", "w", encoding="utf-8"),
+        ensure_ascii=False,
+        indent=4,
+    )
