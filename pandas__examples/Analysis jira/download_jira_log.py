ee0a2d750758c994818f38b5295fdf8a43288862
gil9red <ilya.petrash@inbox.ru>
2023-06-02 16:39:46 +0500

    Refactoring. Using black code style

--- pandas__examples/Analysis jira/download_jira_log.py
+++ pandas__examples/Analysis jira/download_jira_log.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import pathlib
-from urllib.parse import urlparse
+
 from datetime import datetime
+from urllib.parse import urlparse
+
+from bs4 import BeautifulSoup
 
 
-URL = 'https://helpdesk.compassluxe.com/sr/jira.issueviews:searchrequest-xml/24381/SearchRequest-24381.xml?tempMax=1000'
+URL = "https://helpdesk.compassluxe.com/sr/jira.issueviews:searchrequest-xml/24381/SearchRequest-24381.xml?tempMax=1000"
 HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:44.0) Gecko/20100101 Firefox/44.0',
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:44.0) Gecko/20100101 Firefox/44.0",
 }
 
 # NOTE. Get <PEM_FILE_NAME>: openssl pkcs12 -nodes -out key.pem -in file.p12
-PEM_FILE_NAME = 'ipetrash.pem'
+PEM_FILE_NAME = "ipetrash.pem"
 
-FILE_NAME = 'jira_log__clear.xml'
-FILE_NAME_FULL = 'jira_log.xml'
+FILE_NAME = "jira_log__clear.xml"
+FILE_NAME_FULL = "jira_log.xml"
 
 
 def generate_file_name(url: str) -> str:
@@ -32,63 +35,64 @@ def generate_file_name(url: str) -> str:
 
     result = urlparse(url)
     path = pathlib.Path(result.path)
-    current_date_str = datetime.now().date().strftime('%Y-%m-%d')
+    current_date_str = datetime.now().date().strftime("%Y-%m-%d")
 
-    return f'{path.name}__{result.query}__{current_date_str}.xml'
+    return f"{path.name}__{result.query}__{current_date_str}.xml"
 
 
 def clear_jira_xml(xml_content: bytes) -> bytes:
-    xml_content = xml_content\
-        .replace(b'TranzAxis', b'FooBar')\
-        .replace(b'compassplus', b'foobar')\
-        .replace(b'Compass Plus', b'Foo Bar')
+    xml_content = (
+        xml_content.replace(b"TranzAxis", b"FooBar")
+        .replace(b"compassplus", b"foobar")
+        .replace(b"Compass Plus", b"Foo Bar")
+    )
 
-    from bs4 import BeautifulSoup
-    root = BeautifulSoup(xml_content, 'html.parser')
+    root = BeautifulSoup(xml_content, "html.parser")
 
     def mask_tag_content(css_selector, attr=None):
         for x in root.select(css_selector):
-            x.string = '...'
+            x.string = "..."
 
             if attr:
-                x[attr] = '...'
+                x[attr] = "..."
 
     def delete_tag(css_selector):
         for x in root.select(css_selector):
             x.decompose()
 
-    delete_tag('comments > comment')
-    delete_tag('attachments > attachment')
-    delete_tag('customfields')
-    delete_tag('issuelinks')
-    delete_tag('subtasks')
+    delete_tag("comments > comment")
+    delete_tag("attachments > attachment")
+    delete_tag("customfields")
+    delete_tag("issuelinks")
+    delete_tag("subtasks")
 
-    mask_tag_content('[username]', 'username')
-    mask_tag_content('item > title')
-    mask_tag_content('item > summary')
-    mask_tag_content('item > description')
-    mask_tag_content('item > link')
-    mask_tag_content('item > key')
-    mask_tag_content('item > version')
-    mask_tag_content('item > fixversion')
-    mask_tag_content('item > component')
-    mask_tag_content('item > environment')
-    mask_tag_content('item > project', 'key')
+    mask_tag_content("[username]", "username")
+    mask_tag_content("item > title")
+    mask_tag_content("item > summary")
+    mask_tag_content("item > description")
+    mask_tag_content("item > link")
+    mask_tag_content("item > key")
+    mask_tag_content("item > version")
+    mask_tag_content("item > fixversion")
+    mask_tag_content("item > component")
+    mask_tag_content("item > environment")
+    mask_tag_content("item > project", "key")
 
     # return root.prettify(encoding='utf-8')
-    return str(root).encode('utf-8')
+    return str(root).encode("utf-8")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import requests
+
     rs = requests.get(URL, headers=HEADERS, cert=PEM_FILE_NAME)
     print(rs)
     print(len(rs.text), repr(rs.text[:50]))
 
     # file_name = generate_file_name(URL)
 
-    with open(FILE_NAME_FULL, mode='wb') as f:
+    with open(FILE_NAME_FULL, mode="wb") as f:
         f.write(rs.content)
 
-    with open(FILE_NAME, mode='wb') as f:
+    with open(FILE_NAME, mode="wb") as f:
         f.write(clear_jira_xml(rs.content))
--- pandas__examples/Analysis jira/main.py
+++ pandas__examples/Analysis jira/main.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 from datetime import datetime
 
-from download_jira_log import FILE_NAME
-data_file = FILE_NAME
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import FormatStrFormatter
+
+import pandas as pd
 
 from bs4 import BeautifulSoup
-root = BeautifulSoup(open(data_file, mode='rb'), 'lxml')
+
+from download_jira_log import FILE_NAME
+
+
+data_file = FILE_NAME
+root = BeautifulSoup(open(data_file, mode="rb"), "lxml")
 
 records = []
 for resolved in root.select("item > resolved"):
     # date local is en_US
-    resolved_date_time = datetime.strptime(resolved.text.strip(), '%a, %d %b %Y %H:%M:%S %z')
+    resolved_date_time = datetime.strptime(
+        resolved.text.strip(), "%a, %d %b %Y %H:%M:%S %z"
+    )
     resolved_year = resolved_date_time.year
     resolved_year_month = datetime(resolved_date_time.year, resolved_date_time.month, 1)
 
@@ -23,39 +32,39 @@ for resolved in root.select("item > resolved"):
 
 records.sort(key=lambda x: x[0])
 
-import pandas as pd
-df = pd.DataFrame(data=records, columns=['resolved_date_time', 'resolved_year', 'resolved_year_month'])
+df = pd.DataFrame(
+    data=records, columns=["resolved_date_time", "resolved_year", "resolved_year_month"]
+)
 print(df)
-print('Total rows:', len(df))
+print("Total rows:", len(df))
 
-df_month = pd.DataFrame({'count': df.groupby("resolved_year_month").size()}).reset_index()
+df_month = pd.DataFrame(
+    {"count": df.groupby("resolved_year_month").size()}
+).reset_index()
 print(df_month)
 print()
 
-df_year = pd.DataFrame({'count': df.groupby("resolved_year").size()}).reset_index()
+df_year = pd.DataFrame({"count": df.groupby("resolved_year").size()}).reset_index()
 print(df_year)
 
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import FormatStrFormatter
-
 fig = plt.figure(1)
-fig.suptitle('Analysis jira', fontsize=14, fontweight='bold')
+fig.suptitle("Analysis jira", fontsize=14, fontweight="bold")
 
 ax1 = fig.add_subplot(121)
-ax1.plot(df_month['resolved_year_month'], df_month['count'])
+ax1.plot(df_month["resolved_year_month"], df_month["count"])
 ax1.grid()
-ax1.set_title('Jira by month')
-ax1.set_xlabel('Date')
-ax1.set_ylabel('Count')
+ax1.set_title("Jira by month")
+ax1.set_xlabel("Date")
+ax1.set_ylabel("Count")
 plt.gcf().autofmt_xdate()
 
 ax2 = fig.add_subplot(122)
-ax2.plot(df_year['resolved_year'], df_year['count'])
+ax2.plot(df_year["resolved_year"], df_year["count"])
 ax2.ticklabel_format(useOffset=False)
-ax2.xaxis.set_major_formatter(FormatStrFormatter('%d'))
-ax2.set_title('Jira by year')
-ax2.set_xlabel('Date')
-ax2.set_ylabel('Count')
+ax2.xaxis.set_major_formatter(FormatStrFormatter("%d"))
+ax2.set_title("Jira by year")
+ax2.set_xlabel("Date")
+ax2.set_ylabel("Count")
 ax2.grid()
 plt.gcf().autofmt_xdate()
 
