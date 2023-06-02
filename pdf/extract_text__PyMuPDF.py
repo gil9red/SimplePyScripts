5365bdcb479a2478ffd69f91271bf736368f40bc
gil9red <ilya.petrash@inbox.ru>
2023-06-02 17:16:45 +0500

    Refactoring. Using black code style

--- pdf/extract_text__PyMuPDF.py
+++ pdf/extract_text__PyMuPDF.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # pip install PyMuPDF
@@ -12,7 +12,6 @@ with fitz.open("merger__PyPDF2/1.pdf") as doc:
     items = []
     for page in doc:
         items.append(page.get_text())
-    text = '\n'.join(items)
+    text = "\n".join(items)
 
 print(text)
-
--- pdf/merger__PyPDF2/main.py
+++ pdf/merger__PyPDF2/main.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # pip install pypdf2
 from PyPDF2 import PdfFileMerger
 
 
-pdfs = ['1.pdf', '2.pdf', '3.pdf']
+pdfs = ["1.pdf", "2.pdf", "3.pdf"]
 
 merger = PdfFileMerger()
 for pdf in pdfs:
