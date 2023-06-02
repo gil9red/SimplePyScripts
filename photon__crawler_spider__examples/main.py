4b229f9d375c3f129ea1ff0cc4922c5f3251d120
gil9red <ilya.petrash@inbox.ru>
2023-06-02 17:27:51 +0500

    Refactoring. Using black code style

--- photon__crawler_spider__examples/main.py
+++ photon__crawler_spider__examples/main.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # SOURCE: https://github.com/s0md3v/Photon/wiki/Photon-Library
+
+
+import json
+
 # pip install photon
 import photon
-photon.crawl('https://github.com/s0md3v/Photon/wiki/Photon-Library')
+
+
+photon.crawl("https://github.com/s0md3v/Photon/wiki/Photon-Library")
 result = photon.result()
 print(result)
 
-with open('result.json', 'w', encoding='utf-8') as f:
-    import json
+with open("result.json", "w", encoding="utf-8") as f:
     json.dump(result, f, indent=4, ensure_ascii=False)
