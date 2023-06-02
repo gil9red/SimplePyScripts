562af54099e494aa145c95b7eee47c216b2d3edf
gil9red <ilya.petrash@inbox.ru>
2023-06-02 19:03:05 +0500

    Refactoring. Using black code style

--- print__win__wmi/print__baseboard__info.py
+++ print__win__wmi/print__baseboard__info.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
-from subprocess import check_output
 import xml.etree.ElementTree as ET
+from subprocess import check_output
 
 
 def get_baseboard_info() -> dict:
     # Motherboard model
-    cmd = 'wmic baseboard get product, Manufacturer, version, serialnumber /format:RAWXML'
+    cmd = "wmic baseboard get product, Manufacturer, version, serialnumber /format:RAWXML"
     text = check_output(cmd)
 
     root = ET.fromstring(text)
 
     result = dict()
-    for x in root.iter('PROPERTY'):
-        key = x.attrib['NAME']
-        result[key] = x.find('VALUE').text
+    for x in root.iter("PROPERTY"):
+        key = x.attrib["NAME"]
+        result[key] = x.find("VALUE").text
 
     return result
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     info = get_baseboard_info()
     print(info)
-    print(info['Product'])
+    print(info["Product"])
