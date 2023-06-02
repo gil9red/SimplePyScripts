e6c2501368eab2cbd277d0e45485cbf5610e4527
gil9red <ilya.petrash@inbox.ru>
2023-06-02 18:43:59 +0500

    Refactoring. Using black code style

--- play_mp3_with_mp3play_module/play.py
+++ play_mp3_with_mp3play_module/play.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
+
+import time
 
 # https://pypi.python.org/pypi/mp3play/
+import mp3play
 
 
 def play(filename):
-    import mp3play
     clip = mp3play.load(filename)
     clip.play()
 
-    import time
     time.sleep(min(30, clip.seconds()))
     clip.stop()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # TODO: не работает:     print 'Error %s for "%s": %s' % (str(err), txt, buf)
     #                             ^SyntaxError: invalid syntax
-    play(r'speak.mp3')
+    play(r"speak.mp3")
