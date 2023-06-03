b996a62c44ce7ff4fa2c3023c78d998c867960cb
gil9red <ilya.petrash@inbox.ru>
2023-06-03 15:11:09 +0500

    Refactoring. Using black code style

--- pyautogui__keyboard__examples/keyboard.send__ctrl+v.py
+++ pyautogui__keyboard__examples/keyboard.send__ctrl+v.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # SOURCE: https://github.com/boppreh/keyboard#keyboard.send
 
 # pip install keyboard
 import keyboard
+
 
 keyboard.send("Ctrl+V")
