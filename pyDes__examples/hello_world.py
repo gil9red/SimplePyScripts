cf16e20034c71bd9b0b6e25a3d228ef67228549a
gil9red <ilya.petrash@inbox.ru>
2023-06-03 16:30:14 +0500

    Refactoring. Using black code style

--- pyDes__examples/hello_world.py
+++ pyDes__examples/hello_world.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # SOURCE: https://github.com/toddw-as/pyDes
@@ -10,11 +10,13 @@ __author__ = 'ipetrash'
 # pip install pyDes
 import pyDes
 
+
 data = b"Hello World!"
 
-
 # def __init__(self, key, mode=ECB, IV=None, pad=None, padmode=PAD_NORMAL):
-k = pyDes.des(b"DESCRYPT", pyDes.CBC, b"\0\0\0\0\0\0\0\0", pad=None, padmode=pyDes.PAD_PKCS5)
+k = pyDes.des(
+    b"DESCRYPT", pyDes.CBC, b"\0\0\0\0\0\0\0\0", pad=None, padmode=pyDes.PAD_PKCS5
+)
 
 d = k.encrypt(data)
 print("Encrypted: %r" % d)
