6b8bb280a6c912bb3a3840ed6caec4be1f8f650a
gil9red <ilya.petrash@inbox.ru>
2023-06-04 15:42:19 +0500

    Refactoring. Using black code style

--- PySvn_examples/commits__draw_plot__by_all_authors.py
+++ PySvn_examples/commits__draw_plot__by_all_authors.py
@@ -1,41 +1,50 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
-import pandas as pd
+import os
 from datetime import datetime
 
-# Название текущего файла без '.py'
-import os
-plot_title = os.path.basename(__file__)[:-3]
-
+import pandas as pd
 import matplotlib.pyplot as plt
-fig = plt.figure(1)
-fig.suptitle(plot_title, fontsize=14, fontweight='bold')
-ax = fig.add_subplot(111)
 
 import config
 from common import get_log_list_by_author
+
+
+# Название текущего файла без '.py'
+plot_title = os.path.basename(__file__)[:-3]
+
+fig = plt.figure(1)
+fig.suptitle(plot_title, fontsize=14, fontweight="bold")
+ax = fig.add_subplot(111)
+
 author_by_log = get_log_list_by_author(config.SVN_FILE_NAME)
 
-list_top_author_by_log = sorted(author_by_log.items(), key=lambda item: len(item[1]), reverse=True)
+list_top_author_by_log = sorted(
+    author_by_log.items(), key=lambda item: len(item[1]), reverse=True
+)
 
 # Добавление на график информации о авторах
 for author, logs in list_top_author_by_log:
     # Сбор коммитов за месяц/год
     records = [datetime(log.date.year, log.date.month, 1) for log in logs]
 
-    df = pd.DataFrame(data=records, columns=['year_month'])
-    df_month = pd.DataFrame({'count': df.groupby("year_month").size()}).reset_index()
-    ax.plot(df_month['year_month'], df_month['count'], label='{} ({})'.format(author, len(logs)))
+    df = pd.DataFrame(data=records, columns=["year_month"])
+    df_month = pd.DataFrame({"count": df.groupby("year_month").size()}).reset_index()
+    ax.plot(
+        df_month["year_month"],
+        df_month["count"],
+        label="{} ({})".format(author, len(logs)),
+    )
 
 ax.legend()
 ax.grid()
-ax.set_title('Commits')
-ax.set_xlabel('Date')
-ax.set_ylabel('Count')
+ax.set_title("Commits")
+ax.set_xlabel("Date")
+ax.set_ylabel("Count")
 
 plt.gcf().autofmt_xdate()
 
--- PySvn_examples/commits__draw_plot__by_single_author.py
+++ PySvn_examples/commits__draw_plot__by_single_author.py
@@ -1,43 +1,51 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
+
+
+import os
+from datetime import datetime
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import config
+from common import get_log_list_by_author
 
 
 AUTHOR = 'ipetrash'
 
-import config
-from common import get_log_list_by_author
 author_by_log = get_log_list_by_author(config.SVN_FILE_NAME)
 
 # Сбор коммитов за месяц/год
-from datetime import datetime
 records = [datetime(log.date.year, log.date.month, 1) for log in author_by_log[AUTHOR]]
 
-import pandas as pd
-df = pd.DataFrame(data=records, columns=['year_month'])
+df = pd.DataFrame(data=records, columns=["year_month"])
 print(df)
-print('Total rows:', len(df))
+print("Total rows:", len(df))
 
-df_month = pd.DataFrame({'count': df.groupby("year_month").size()}).reset_index()
+df_month = pd.DataFrame({"count": df.groupby("year_month").size()}).reset_index()
 print(df_month)
 print()
 
 # Название текущего файла без '.py'
-import os
 plot_title = os.path.basename(__file__)[:-3]
 
-import matplotlib.pyplot as plt
 fig = plt.figure(1)
-fig.suptitle(plot_title, fontsize=14, fontweight='bold')
+fig.suptitle(plot_title, fontsize=14, fontweight="bold")
 
 ax = fig.add_subplot(111)
-ax.plot(df_month['year_month'], df_month['count'], label='{} ({})'.format(AUTHOR, len(author_by_log[AUTHOR])))
+ax.plot(
+    df_month["year_month"],
+    df_month["count"],
+    label="{} ({})".format(AUTHOR, len(author_by_log[AUTHOR])),
+)
 ax.legend()
 ax.grid()
-ax.set_title('Commits')
-ax.set_xlabel('Date')
-ax.set_ylabel('Count')
+ax.set_title("Commits")
+ax.set_xlabel("Date")
+ax.set_ylabel("Count")
 
 plt.gcf().autofmt_xdate()
 
