82f761ea5b493d16e68abf2359f82026b4fa3cb3
gil9red <ilya.petrash@inbox.ru>
2023-06-04 15:44:32 +0500

    Refactoring. Using black code style

--- PySvn_examples/common.py
+++ PySvn_examples/common.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # SOURCE: https://github.com/dsoprea/PySvn
 
+
+import os
+from collections import defaultdict
+
+import svn.local
+import svn.remote
+
+
 def get_log_list(url__or__file_name: str, revision_from=None, limit=None) -> list:
-    import os
     if os.path.exists(url__or__file_name):
-        import svn.local
         repo = svn.local.LocalClient(url__or__file_name)
     else:
-        import svn.remote
         repo = svn.remote.RemoteClient(url__or__file_name)
 
     return list(repo.log_default(revision_from=revision_from, limit=limit))
@@ -22,9 +27,7 @@ def get_log_list_by_author(url__or__file_name: str, log_list: list = None) -> di
     if not log_list:
         log_list = get_log_list(url__or__file_name)
 
-    from collections import defaultdict
     author_by_log = defaultdict(list)
-
     for log in log_list:
         author_by_log[log.author].append(log)
 
--- PySvn_examples/config.py
+++ PySvn_examples/config.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
-URL_SVN = 'svn+cplus://svn2.compassplus.ru/twrbs/csm/optt/dev/trunk'
-SVN_FILE_NAME = 'E:/OPTT/optt_trunk'
+URL_SVN = "svn+cplus://svn2.compassplus.ru/twrbs/csm/optt/dev/trunk"
+SVN_FILE_NAME = "E:/OPTT/optt_trunk"
