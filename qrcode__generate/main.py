44c086187d3946b0bd522020ed303a9e2a700275
gil9red <ilya.petrash@inbox.ru>
2023-06-04 15:56:20 +0500

    Refactoring. Using black code style

--- qrcode__generate/main.py
+++ qrcode__generate/main.py
@@ -1,28 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # SOURCE: https://github.com/lincolnloop/python-qrcode
 
 
-text = 'Hello World!'
+# In memory
+import io
 
 # pip install qrcode
 import qrcode
-img = qrcode.make(text)
 
-# In memory
-import io
+
+text = "Hello World!"
+
+img = qrcode.make(text)
 io_data = io.BytesIO()
-img.save(io_data, 'png')
+img.save(io_data, "png")
 data = io_data.getvalue()
 print(data)
 
 # In file
-img.save('qr_code_1.png')
+img.save("qr_code_1.png")
 #
 # In file, version 2:
-with open('qr_code_2.png', 'wb') as f:
+with open("qr_code_2.png", "wb") as f:
     f.write(data)
--- qrcode__reader/main.py
+++ qrcode__reader/main.py
@@ -1,38 +1,40 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
-# pip install git+https://github.com/ewino/qreader.git
+import io
+from urllib.request import urlopen
+
+import requests
 
 # SOURCE: https://github.com/tomerfiliba/reedsolomon/blob/master/reedsolo.py
 # pip install reedsolo
+#
+# pip install git+https://github.com/ewino/qreader.git
 import qreader
 
 
 # FROM FILE
-data = qreader.read('../qrcode__generate/qr_code_1.png')
+data = qreader.read("../qrcode__generate/qr_code_1.png")
 print(data)  # Hello World!
 
 
 # FROM URL
-url = 'https://upload.wikimedia.org/wikipedia/commons/8/8f/Qr-2.png'
-from urllib.request import urlopen
+url = "https://upload.wikimedia.org/wikipedia/commons/8/8f/Qr-2.png"
 data = qreader.read(urlopen(url))
 print(data)  # "Version 2"
 
-url = 'https://upload.wikimedia.org/wikipedia/commons/e/eb/QR-%D0%BA%D0%BE%D0%B4.png'
+url = "https://upload.wikimedia.org/wikipedia/commons/e/eb/QR-%D0%BA%D0%BE%D0%B4.png"
 data = qreader.read(urlopen(url))
 print(data)  # http://ru.wikipedia.org/wiki/QR_Code
 
 
 # FROM URL as bytes in file-like object
-import requests
 rs = requests.get(url)
 image_data = rs.content
 
-import io
 bytes_io = io.BytesIO(image_data)
 
 data = qreader.read(bytes_io)
