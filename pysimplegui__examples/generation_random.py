9a908fa26ecb1ac80590af14166ed5649db8a287
gil9red <ilya.petrash@inbox.ru>
2023-06-04 15:29:23 +0500

    Refactoring. Using black code style

--- pysimplegui__examples/generation_random.py
+++ pysimplegui__examples/generation_random.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 from random import randint
@@ -10,20 +10,20 @@ from random import randint
 import PySimpleGUI as sg
 
 
-sg.theme('SystemDefault')
+sg.theme("SystemDefault")
 layout = [
-    [sg.Text('Number of digits'), sg.Input(default_text='5', key='ND')],
-    [sg.Text('Minimum'), sg.Input(default_text='1', key='Min')],
-    [sg.Text('Maximum'), sg.Input(default_text='9', key='Max')],
-    [sg.Text(' ', key='OUT')],
-    [sg.Button('Generate', key='button_generate'), sg.Button('Clear')]
+    [sg.Text("Number of digits"), sg.Input(default_text="5", key="ND")],
+    [sg.Text("Minimum"), sg.Input(default_text="1", key="Min")],
+    [sg.Text("Maximum"), sg.Input(default_text="9", key="Max")],
+    [sg.Text(" ", key="OUT")],
+    [sg.Button("Generate", key="button_generate"), sg.Button("Clear")],
 ]
 
-window = sg.Window('Generator', layout)
+window = sg.Window("Generator", layout)
 
 
 def generate(number: int, min_number: int, max_number: int) -> str:
-    return ''.join(str(randint(min_number, max_number)) for _ in range(number))
+    return "".join(str(randint(min_number, max_number)) for _ in range(number))
 
 
 while True:
@@ -31,14 +31,18 @@ while True:
     if event is None or event == sg.WIN_CLOSED:
         break
 
-    if event == 'button_generate':
-        number, min_number, max_number = int(values['ND']), int(values['Min']), int(values['Max'])
+    if event == "button_generate":
+        number, min_number, max_number = (
+            int(values["ND"]),
+            int(values["Min"]),
+            int(values["Max"]),
+        )
         text = generate(number, min_number, max_number)
         print(text)
-        window['OUT'].update(f'Result: {text}')
+        window["OUT"].update(f"Result: {text}")
 
-    if event == 'Clear':
-        window.find_element('ND').update('')
-        window.find_element('Min').update('')
-        window.find_element('Max').update('')
-        window.find_element('OUT').update('')
+    if event == "Clear":
+        window.find_element("ND").update("")
+        window.find_element("Min").update("")
+        window.find_element("Max").update("")
+        window.find_element("OUT").update("")
