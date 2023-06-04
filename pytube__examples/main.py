653bc0c2a5937f926aababb49a4040ac1bc3e0bc
gil9red <ilya.petrash@inbox.ru>
2023-06-04 16:55:49 +0500

    Refactoring. Using black code style

--- pytube__examples/main.py
+++ pytube__examples/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import sys
@@ -10,20 +10,20 @@ import sys
 from pytube import YouTube
 
 
-url = 'https://www.youtube.com/watch?v=90KZnwrVMgY'
+url = "https://www.youtube.com/watch?v=90KZnwrVMgY"
 
 yt = YouTube(url)
-print(f'Download video {yt.title!r}: {url}')
+print(f"Download video {yt.title!r}: {url}")
 
 # Атрибут progressive=True нужен, чтобы получить поток с видео и аудио в одном файле
-streams = yt.streams\
-    .filter(progressive=True, file_extension='mp4', resolution='720p')\
-    .order_by('resolution')
+streams = yt.streams.filter(
+    progressive=True, file_extension="mp4", resolution="720p"
+).order_by("resolution")
 
 if not streams:
-    print('Not found mp4 with 720p!')
+    print("Not found mp4 with 720p!")
     sys.exit()
 
 video = streams[-1]
-print('Stream url:', video.url)
+print("Stream url:", video.url)
 video.download()
