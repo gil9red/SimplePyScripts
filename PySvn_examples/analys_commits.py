d243895ce5b55a60ac7f7d1dc4a6af66f8a04bca
gil9red <ilya.petrash@inbox.ru>
2023-06-04 15:32:32 +0500

    Refactoring. Using black code style

--- PySvn_examples/analys_commits.py
+++ PySvn_examples/analys_commits.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
-
-from common import get_log_list, get_log_list_by_author
 
 import config
+from common import get_log_list, get_log_list_by_author
+
+
 url = config.SVN_FILE_NAME
 
 log_list = get_log_list(url)
-print('Total commits ({}):'.format(len(log_list)))
+print("Total commits ({}):".format(len(log_list)))
 
 author_by_log = get_log_list_by_author(url, log_list)
 
-for author, logs in sorted(author_by_log.items(), key=lambda item: len(item[1]), reverse=True):
-    print('    {}: {}'.format(author, len(logs)))
+for author, logs in sorted(
+    author_by_log.items(), key=lambda item: len(item[1]), reverse=True
+):
+    print("    {}: {}".format(author, len(logs)))
--- PySvn_examples/commit_watcher.py
+++ PySvn_examples/commit_watcher.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
+
+import time
 
 import config
+from common import get_log_list
+
+
 url = config.URL_SVN
 
-from common import get_log_list
 last_log = get_log_list(url, limit=1)[0]
 last_revision = last_log.revision
-print('Start from commit rev{} by {}: "{}" in {}'.format(
-    last_revision,
-    last_log.author,
-    repr(last_log.msg),
-    last_log.date
-))
+print(
+    'Start from commit rev{} by {}: "{}" in {}'.format(
+        last_revision, last_log.author, repr(last_log.msg), last_log.date
+    )
+)
 
 while True:
     log_list = get_log_list(url, revision_from=last_revision)
@@ -26,17 +29,18 @@ while True:
         last_log = log_list[-1]
 
         if last_revision != last_log.revision:
-            print('commits +{} from: rev{} by {}: "{}" in {}....{}\n'.format(
-                len(log_list),
-                last_revision,
-                last_log.author,
-                repr(last_log.msg),
-                last_log.date,
-                log_list
-            ))
+            print(
+                'commits +{} from: rev{} by {}: "{}" in {}....{}\n'.format(
+                    len(log_list),
+                    last_revision,
+                    last_log.author,
+                    repr(last_log.msg),
+                    last_log.date,
+                    log_list,
+                )
+            )
 
             last_revision = last_log.revision
 
     # Every 10 minutes
-    import time
     time.sleep(60 * 10)
