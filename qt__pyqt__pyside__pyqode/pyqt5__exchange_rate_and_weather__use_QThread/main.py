623f0ef5d4f11e1e376b3aac04379adcad1248f4
gil9red <ilya.petrash@inbox.ru>
2023-06-07 15:20:42 +0500

    Refactoring. Using black code style

--- qt__pyqt__pyside__pyqode/pyqt5__exchange_rate_and_weather__use_QThread/main.py
+++ qt__pyqt__pyside__pyqode/pyqt5__exchange_rate_and_weather__use_QThread/main.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
-
-
-from PyQt5 import Qt
-import time
-
-
-def log_uncaught_exceptions(ex_cls, ex, tb):
-    text = '{}: {}:\n'.format(ex_cls.__name__, ex)
-    import traceback
-    text += ''.join(traceback.format_tb(tb))
-
-    print(text)
-    Qt.QMessageBox.critical(None, 'Error', text)
-    sys.exit(1)
+__author__ = "ipetrash"
 
 
 import sys
-sys.excepthook = log_uncaught_exceptions
+import traceback
+import time
 
+from PyQt5 import Qt
 
 from utils import exchange_rate, get_weather
+
+
+def log_uncaught_exceptions(ex_cls, ex, tb):
+    text = "{}: {}:\n".format(ex_cls.__name__, ex)
+    text += "".join(traceback.format_tb(tb))
+
+    print(text)
+    Qt.QMessageBox.critical(None, "Error", text)
+    sys.exit(1)
+
+
+sys.excepthook = log_uncaught_exceptions
 
 
 class ThreadExchangeRate(Qt.QThread):
@@ -35,10 +35,10 @@ class ThreadExchangeRate(Qt.QThread):
 
     def run(self):
         while True:
-            print('Start ThreadExchangeRate.currency: ' + self.currency)
+            print("Start ThreadExchangeRate.currency: " + self.currency)
 
             value = exchange_rate(self.currency)
-            print('  ThreadExchangeRate.{} value: {}'.format(self.currency, value))
+            print("  ThreadExchangeRate.{} value: {}".format(self.currency, value))
 
             self.about_exchange_rate.emit(value)
 
@@ -55,10 +55,10 @@ class ThreadGetWeather(Qt.QThread):
 
     def run(self):
         while True:
-            print('Start ThreadGetWeather.city: ' + self.city)
+            print("Start ThreadGetWeather.city: " + self.city)
 
             weather = get_weather(self.city)
-            print('  ThreadGetWeather.{} weather: {}'.format(self.city, weather))
+            print("  ThreadGetWeather.{} weather: {}".format(self.city, weather))
 
             self.about_weather.emit(weather)
 
@@ -69,7 +69,7 @@ class Window(Qt.QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle('Widget-Info: exchange rate and weather')
+        self.setWindowTitle("Widget-Info: exchange rate and weather")
 
         self.exchange_rate_USD = Qt.QLabel()
         self.exchange_rate_EUR = Qt.QLabel()
@@ -79,8 +79,8 @@ class Window(Qt.QWidget):
         self.weather_3 = Qt.QLabel()
 
         layout = Qt.QFormLayout()
-        layout.addRow('USD:', self.exchange_rate_USD)
-        layout.addRow('EUR:', self.exchange_rate_EUR)
+        layout.addRow("USD:", self.exchange_rate_USD)
+        layout.addRow("EUR:", self.exchange_rate_EUR)
 
         line = Qt.QFrame()
         line.setFrameShape(Qt.QFrame.HLine)
@@ -89,34 +89,38 @@ class Window(Qt.QWidget):
 
         layout.addRow(line)
 
-        layout.addRow('Магнитогорск:', self.weather_1)
-        layout.addRow('Челябинск:', self.weather_2)
-        layout.addRow('Екатеринбург:', self.weather_3)
+        layout.addRow("Магнитогорск:", self.weather_1)
+        layout.addRow("Челябинск:", self.weather_2)
+        layout.addRow("Екатеринбург:", self.weather_3)
 
         self.setLayout(layout)
 
-        self.thread_exchange_rate_USD = ThreadExchangeRate(self, 'USD')
-        self.thread_exchange_rate_USD.about_exchange_rate.connect(self.exchange_rate_USD.setText)
+        self.thread_exchange_rate_USD = ThreadExchangeRate(self, "USD")
+        self.thread_exchange_rate_USD.about_exchange_rate.connect(
+            self.exchange_rate_USD.setText
+        )
         self.thread_exchange_rate_USD.start()
 
-        self.thread_exchange_rate_EUR = ThreadExchangeRate(self, 'EUR')
-        self.thread_exchange_rate_EUR.about_exchange_rate.connect(self.exchange_rate_EUR.setText)
+        self.thread_exchange_rate_EUR = ThreadExchangeRate(self, "EUR")
+        self.thread_exchange_rate_EUR.about_exchange_rate.connect(
+            self.exchange_rate_EUR.setText
+        )
         self.thread_exchange_rate_EUR.start()
 
-        self.thread_weather_1 = ThreadGetWeather(self, 'Магнитогорск')
+        self.thread_weather_1 = ThreadGetWeather(self, "Магнитогорск")
         self.thread_weather_1.about_weather.connect(self.weather_1.setText)
         self.thread_weather_1.start()
 
-        self.thread_weather_2 = ThreadGetWeather(self, 'Челябинск')
+        self.thread_weather_2 = ThreadGetWeather(self, "Челябинск")
         self.thread_weather_2.about_weather.connect(self.weather_2.setText)
         self.thread_weather_2.start()
 
-        self.thread_weather_3 = ThreadGetWeather(self, 'Екатеринбург')
+        self.thread_weather_3 = ThreadGetWeather(self, "Екатеринбург")
         self.thread_weather_3.about_weather.connect(self.weather_3.setText)
         self.thread_weather_3.start()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = Qt.QApplication([])
 
     mw = Window()
--- qt__pyqt__pyside__pyqode/pyqt5__exchange_rate_and_weather__use_QThread/utils.py
+++ qt__pyqt__pyside__pyqode/pyqt5__exchange_rate_and_weather__use_QThread/utils.py
@@ -1,86 +1,88 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
+
+
+from datetime import date
+
+import requests
+from bs4 import BeautifulSoup
 
 
 def exchange_rate(currency, date_req=None):
     # Example:
     # http://www.cbr.ru/scripts/XML_daily.asp
     # http://www.cbr.ru/scripts/XML_daily.asp?date_req=21.10.2016
-    from datetime import date
     if date_req is None:
         date_req = date.today()
 
     if isinstance(date_req, date):
-        date_req = date_req.strftime('%d.%m.%Y')
+        date_req = date_req.strftime("%d.%m.%Y")
 
-    url = 'http://www.cbr.ru/scripts/XML_daily.asp?date_req=' + date_req
+    url = "http://www.cbr.ru/scripts/XML_daily.asp?date_req=" + date_req
     # url = 'http://www.cbr.ru/scripts/XML_daily.asp'
 
-    import requests
     rs = requests.get(url)
+    root = BeautifulSoup(rs.content, "html.parser")
 
-    from bs4 import BeautifulSoup
-    root = BeautifulSoup(rs.content, 'html.parser')
-
-    for valute in root.select('valute'):
+    for valute in root.select("valute"):
         if valute.charcode.text == currency:
             return valute.value.text
 
-    return '<NONE>'
+    return "<NONE>"
 
 
 CODE_WEATHER_BY_DESCRIPTION = {
-    '0': 'Торнадо',
-    '1': 'Тропический шторм',
-    '2': 'Ураган',
-    '3': 'Сильные грозы',
-    '4': 'Грозы',
-    '5': 'Дождь со снегом',
-    '6': 'Дождь с мокрым снегом',
-    '7': 'Cнег и мокрый снег',
-    '8': 'Изморозь',
-    '9': 'Изморось',
-    '10': 'Ледяной дождь',
-    '11': 'Ливневые дожди',
-    '12': 'Ливни',
-    '13': 'Порывы снега',
-    '14': 'Легкий снежный дождь',
-    '15': 'Низовая метель',
-    '16': 'Снег',
-    '17': 'Град',
-    '18': 'Мокрый снег',
-    '19': 'Вихрь',
-    '20': 'Туманно',
-    '21': 'Легкий туман',
-    '22': 'Дымка',
-    '23': 'Бушующий ветер',
-    '24': 'Ветренно',
-    '25': 'Холодно',
-    '26': 'Облачно',
-    '27': 'В основном облачно (ночь)',
-    '28': 'В основном облачно (день)',
-    '29': 'Местами облачно (ночь)',
-    '30': 'Местами облачно (день)',
-    '31': 'Безоблачно (ночь)',
-    '32': 'Солнечно',
-    '33': 'Ясно (ночь)',
-    '34': 'Ясно (день)',
-    '35': 'Дождь с градом',
-    '36': 'Жарко',
-    '37': 'Изолированные грозы',
-    '38': 'Рассеяные грозы',
-    '39': 'Местами грозы',
-    '40': 'Местами ливни',
-    '41': 'Снегопад',
-    '42': 'Местами снегпад',
-    '43': 'Снегопад',
-    '44': 'Местами облачно',
-    '45': 'Грозовые ливни',
-    '46': 'Снежные ливни',
-    '47': 'Изолированные грозовые ливни',
-    '3200': 'Недоступно'
+    "0": "Торнадо",
+    "1": "Тропический шторм",
+    "2": "Ураган",
+    "3": "Сильные грозы",
+    "4": "Грозы",
+    "5": "Дождь со снегом",
+    "6": "Дождь с мокрым снегом",
+    "7": "Cнег и мокрый снег",
+    "8": "Изморозь",
+    "9": "Изморось",
+    "10": "Ледяной дождь",
+    "11": "Ливневые дожди",
+    "12": "Ливни",
+    "13": "Порывы снега",
+    "14": "Легкий снежный дождь",
+    "15": "Низовая метель",
+    "16": "Снег",
+    "17": "Град",
+    "18": "Мокрый снег",
+    "19": "Вихрь",
+    "20": "Туманно",
+    "21": "Легкий туман",
+    "22": "Дымка",
+    "23": "Бушующий ветер",
+    "24": "Ветренно",
+    "25": "Холодно",
+    "26": "Облачно",
+    "27": "В основном облачно (ночь)",
+    "28": "В основном облачно (день)",
+    "29": "Местами облачно (ночь)",
+    "30": "Местами облачно (день)",
+    "31": "Безоблачно (ночь)",
+    "32": "Солнечно",
+    "33": "Ясно (ночь)",
+    "34": "Ясно (день)",
+    "35": "Дождь с градом",
+    "36": "Жарко",
+    "37": "Изолированные грозы",
+    "38": "Рассеяные грозы",
+    "39": "Местами грозы",
+    "40": "Местами ливни",
+    "41": "Снегопад",
+    "42": "Местами снегпад",
+    "43": "Снегопад",
+    "44": "Местами облачно",
+    "45": "Грозовые ливни",
+    "46": "Снежные ливни",
+    "47": "Изолированные грозовые ливни",
+    "3200": "Недоступно",
 }
 
 
@@ -90,24 +92,24 @@ def get_weather(city):
 
     """
 
-    url = "https://query.yahooapis.com/v1/public/yql?q=select item from weather.forecast where woeid in " \
-          "(select woeid from geo.places(1) where text='{city}') and u='c'" \
-          "&format=json&diagnostics=true".format(city=city)
-
-    import requests
+    url = (
+        "https://query.yahooapis.com/v1/public/yql?q=select item from weather.forecast where woeid in "
+        "(select woeid from geo.places(1) where text='{city}') and u='c'"
+        "&format=json&diagnostics=true".format(city=city)
+    )
     rs = requests.get(url)
 
-    query = rs.json().get('query')
+    query = rs.json().get("query")
 
     # Если query = None или query['results'] = None
-    if not query or not query['results']:
+    if not query or not query["results"]:
         return "Не найден населенный пункт"
 
-    item = query['results']['channel']['item']
-    condition = item['condition']
+    item = query["results"]["channel"]["item"]
+    condition = item["condition"]
 
-    temp = condition['temp']
-    code = condition['code']
+    temp = condition["temp"]
+    code = condition["code"]
     text = CODE_WEATHER_BY_DESCRIPTION[code]
 
-    return '{} °C, {}'.format(temp, text)
+    return "{} °C, {}".format(temp, text)
