f3875d44e36d3a846452c71d922dc41e7ae24b66
gil9red <ilya.petrash@inbox.ru>
2023-06-13 15:19:02 +0500

    Updated.

--- job_compassplus/kill.py
+++ job_compassplus/kill.py
@@ -4,30 +4,49 @@
 __author__ = "ipetrash"
 
 
-from psutil import process_iter, Process
+from pathlib import Path
+from psutil import process_iter, Process, AccessDenied
 
 
-def is_found(p: Process) -> bool:
-    return "java" in p.name()
+def is_found(p: Process, cwd: str | Path = None) -> bool:
+    if isinstance(cwd, str):
+        cwd = Path(cwd)
+    if cwd:
+        is_equal_path = cwd.exists() and cwd == Path(p.cwd())
+    else:
+        is_equal_path = True
+
+    return "java" in p.name() and is_equal_path
+
+
+def get_processes(cwd: str | Path = None) -> list[Process]:
+    items = []
+    for p in process_iter():
+        try:
+            if is_found(p, cwd):
+                items.append(p)
+        except AccessDenied:
+            pass
+    return items
 
 
 def is_server(p: Process) -> bool:
-    return is_found(p) and "org.radixware.kernel.server.Server" in p.cmdline()
+    return "org.radixware.kernel.server.Server" in p.cmdline()
 
 
 def is_explorer(p: Process) -> bool:
-    return is_found(p) and "org.radixware.kernel.explorer.Explorer" in p.cmdline()
+    return "org.radixware.kernel.explorer.Explorer" in p.cmdline()
 
 
-def kill_servers():
-    for p in process_iter():
+def kill_servers(cwd: str | Path = None):
+    for p in get_processes(cwd):
         if is_server(p):
             print(f"Kill server #{p.pid}")
             p.kill()
 
 
-def kill_explorers():
-    for p in process_iter():
+def kill_explorers(cwd: str | Path = None):
+    for p in get_processes(cwd):
         if is_explorer(p):
             print(f"Kill explorer #{p.pid}")
             p.kill()
