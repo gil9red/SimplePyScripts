ccbcc243aca494aa54857c7e8120da68caef9f14
gil9red <ilya.petrash@inbox.ru>
2023-06-14 13:42:56 +0500

    Refactoring. Using black code style

--- QuadraticEquation/QuadraticEquation.py
+++ QuadraticEquation/QuadraticEquation.py
@@ -1,33 +1,37 @@
 # coding=utf-8
 
+
+__author__ = "ipetrash"
+
+
 import argparse
 import math
 
-__author__ = 'ipetrash'
-
 
 def create_parser():
-    return argparse.ArgumentParser(description="Finding the roots of a quadratic equation.")
+    return argparse.ArgumentParser(
+        description="Finding the roots of a quadratic equation."
+    )
 
 
 def calculate_D(a, b, c):
-    return (b ** 2) - 4 * a * c
+    return (b**2) - 4 * a * c
 
-	
+
 def calculate_Roots(a, b, D):
     sqrt_D = math.sqrt(D)
     x1 = (-b - sqrt_D) / (2 * a)
     x2 = (-b + sqrt_D) / (2 * a)
     return x1, x2
 
-	
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parse = create_parser()
     parse.parse_args()
 
-    a = int(raw_input("a="))
-    b = int(raw_input("b="))
-    c = int(raw_input("c="))
+    a = int(input("a="))
+    b = int(input("b="))
+    c = int(input("c="))
     D = calculate_D(a, b, c)
     print("D=%s" % D)
     if D > 0:
