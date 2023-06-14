f64a0371b9a1fbc75ec73b1690d92bfb729daf3e
gil9red <ilya.petrash@inbox.ru>
2023-06-14 13:51:15 +0500

    Refactoring. Using black code style

--- ReadURLsFromFile/ReadURLsFromFile.py
+++ ReadURLsFromFile/ReadURLsFromFile.py
@@ -1,18 +1,19 @@
 # coding=utf-8
 
-import argparse
+__author__ = "ipetrash"
 
-__author__ = 'ipetrash'
+
+import argparse
 
 
 def create_parser():
     parser = argparse.ArgumentParser(description="Read URLs from file with URL.")
     return parser
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     create_parser().parse_args()
-    file_path = raw_input("Input file path: ")
-    file = open(file_path, "r")
-    for line in file.readlines():
-        print(line)
-    file.close()
+    file_path = input("Input file path: ")
+    with open(file_path) as f:
+        for line in f.readlines():
+            print(line)
