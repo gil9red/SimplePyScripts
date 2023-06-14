7a7aaff0e83cfe59d3853b7a72409b82cf9bc555
gil9red <ilya.petrash@inbox.ru>
2023-06-14 13:48:11 +0500

    Refactoring. Using black code style

--- read_pair_lines_file_and_write_to_other_file__using_next_StopIteration/main.py
+++ read_pair_lines_file_and_write_to_other_file__using_next_StopIteration/main.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
-with open("short_100.txt", encoding='utf-8') as in_f, \
-        open("long_100.txt", "w", encoding='utf-8') as out_f:
+with (
+    open("short_100.txt", encoding="utf-8") as in_f,
+    open("long_100.txt", "w", encoding="utf-8") as out_f,
+):
     while True:
         try:
             line = f"{next(in_f).rstrip()} {next(in_f).rstrip()}\n"
