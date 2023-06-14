7c53755b9904e2cf819f03004e8dcd4169b46cb3
gil9red <ilya.petrash@inbox.ru>
2023-06-14 13:52:30 +0500

    Refactoring. Using black code style

--- Recursive search exe/via glob.py
+++ Recursive search exe/via glob.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import glob
-for file_name in glob.iglob('C://**/*.exe', recursive=True):
+
+
+for file_name in glob.iglob("C://**/*.exe", recursive=True):
     print(file_name)
--- Recursive search exe/via os.walk.py
+++ Recursive search exe/via os.walk.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import os
-for root, dirs, files in os.walk('C://'):
+
+
+for root, dirs, files in os.walk("C://"):
     for file in files:
         base_name, ext = os.path.splitext(file)
-        if ext.lower() == '.exe':
+        if ext.lower() == ".exe":
             file_name = os.path.join(root, file)
             file_name = os.path.normpath(file_name)
             print(file_name)
