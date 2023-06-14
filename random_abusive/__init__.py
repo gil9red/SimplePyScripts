595386741d962aea885ca9b59aa972d48268dcc3
gil9red <ilya.petrash@inbox.ru>
2023-06-14 13:45:13 +0500

    Refactoring. Using black code style

--- random_abusive/__init__.py
+++ random_abusive/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 from random_abusive.random_abusive import get_words
--- random_abusive/random_abusive.py
+++ random_abusive/random_abusive.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
-import os.path
+import random
+from pathlib import Path
 
-file_name = os.path.join(os.path.dirname(__file__), 'abusive words.txt')
-WORDS = open(file_name, encoding='utf-8').readlines()
+
+file_name = Path(__file__).parent / "abusive words.txt"
+with open(file_name, encoding="utf-8") as f:
+    WORDS = f.readlines()
 
 
 def get_words(number, chain_length=2):
-    import random
     random.shuffle(WORDS)
+    random_words = list(map(str.strip, WORDS[: number * chain_length]))
 
-    random_words = list(map(str.strip, WORDS[:number * chain_length]))
-
-    words = list()
-
+    words = []
     for i in range(0, len(random_words), chain_length):
-        chain = random_words[i: i + chain_length]
+        chain = random_words[i : i + chain_length]
         chain[0] = chain[0].title()
-        words.append(' '.join(chain))
+        words.append(" ".join(chain))
 
     return words
 
 
-if __name__ == '__main__':
-    print(*get_words(10), sep='\n')
+if __name__ == "__main__":
+    print(*get_words(10), sep="\n")
