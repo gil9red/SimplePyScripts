5fe3784a677c7a3c482bb90790da5de8745deb55
gil9red <ilya.petrash@inbox.ru>
2023-06-14 13:46:02 +0500

    Refactoring. Using black code style

--- Random_example/main.py
+++ Random_example/main.py
@@ -1,19 +1,22 @@
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
+
 
 import random
 
-if __name__ == '__main__':
-    direction = ["up", 'down', 'left', 'right']
-    for i in range(5):
-        print("Directions: %s, random direction = %s" % (direction, random.choice(direction)))
 
-    print("random: %s" % random.random())  # rand float number
-    print("randrange(5): %s" % random.randrange(5))  # rand int number
-    print("randrange(0, 10): %s" % random.randrange(0, 10))  # rand int number
-    print("uniform(1, 10): %s" % random.uniform(1, 10))  # rand float number
+direction = ["up", "down", "left", "right"]
+for i in range(5):
+    print(
+        f"Directions: {direction}, random direction = {random.choice(direction)}"
+    )
 
-    print("Directions: %s" % direction)
-    random.shuffle(direction)  # shuffle list
-    print("Shuffle directions: %s" % direction)
+print(f"random: {random.random()}")  # rand float number
+print(f"randrange(5): {random.randrange(5)}")  # rand int number
+print(f"randrange(0, 10): {random.randrange(0, 10)}")  # rand int number
+print(f"uniform(1, 10): {random.uniform(1, 10)}")  # rand float number
 
-    print(random.sample(direction, 2))  # select two element
+print(f"Directions: {direction}")
+random.shuffle(direction)  # shuffle list
+print(f"Shuffle directions: {direction}")
+
+print(random.sample(direction, 2))  # select two element
