5b5738664275f2bd653babe0d8ed5efb29060091
gil9red <ilya.petrash@inbox.ru>
2023-06-14 13:35:27 +0500

    Refactoring. Using black code style

--- qt__pyqt__pyside__pyqode/show_hide_tabs__QTabWidget.py
+++ qt__pyqt__pyside__pyqode/show_hide_tabs__QTabWidget.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # SOURCE: https://forum.qt.io/topic/23803/solved-hiding-tab-from-qtabwidget-made-in-qtcreator/2
@@ -15,18 +15,22 @@ class MainWindow(QWidget):
         super().__init__()
 
         self.tab_widget = QTabWidget()
-        self.tab_widget.tabBar().setStyleSheet("""
+        self.tab_widget.tabBar().setStyleSheet(
+            """
             ::tab:disabled { width: 0; height: 0; margin: 0; padding: 0; border: none; }
-        """)
+            """
+        )
 
-        self.tab_widget.addTab(QPushButton('Tab 1'), 'Tab 1')
-        self.tab_widget.addTab(QPushButton('Tab 2'), 'Tab 2')
-        self.tab_widget.addTab(QPushButton('Tab 3'), 'Tab 3')
+        self.tab_widget.addTab(QPushButton("Tab 1"), "Tab 1")
+        self.tab_widget.addTab(QPushButton("Tab 2"), "Tab 2")
+        self.tab_widget.addTab(QPushButton("Tab 3"), "Tab 3")
 
-        self.button = QPushButton('Show/Hide tab #2')
+        self.button = QPushButton("Show/Hide tab #2")
         self.button.setCheckable(True)
         self.button.setChecked(True)
-        self.button.clicked.connect(lambda checked: self.tab_widget.setTabEnabled(1, checked))
+        self.button.clicked.connect(
+            lambda checked: self.tab_widget.setTabEnabled(1, checked)
+        )
 
         main_layout = QVBoxLayout()
         main_layout.addWidget(self.button)
@@ -35,7 +39,7 @@ class MainWindow(QWidget):
         self.setLayout(main_layout)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication([])
 
     mw = MainWindow()
--- qt__pyqt__pyside__pyqode/show_user_grouple_rss__qt.py
+++ qt__pyqt__pyside__pyqode/show_user_grouple_rss__qt.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import traceback
 import sys
+import webbrowser
 
 import feedparser
 import requests
@@ -15,11 +16,11 @@ from PyQt5.QtCore import *
 
 
 def log_uncaught_exceptions(ex_cls, ex, tb):
-    text = f'{ex_cls.__name__}: {ex}:\n'
-    text += ''.join(traceback.format_tb(tb))
+    text = f"{ex_cls.__name__}: {ex}:\n"
+    text += "".join(traceback.format_tb(tb))
 
     print(text)
-    QMessageBox.critical(None, 'Error', text)
+    QMessageBox.critical(None, "Error", text)
     sys.exit(1)
 
 
@@ -28,7 +29,7 @@ sys.excepthook = log_uncaught_exceptions
 
 def get_feeds_by_manga_chapters(url_rss: str) -> list:
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0',
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0",
     }
 
     rss_text = requests.get(url_rss, headers=headers).text
@@ -41,29 +42,33 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle('show_user_grouple_rss__qt')
+        self.setWindowTitle("show_user_grouple_rss__qt")
 
         self.line_edit_url_user = QLineEdit()
         self.line_edit_id_user = QLineEdit()
         self.line_edit_rss_user = QLineEdit()
 
-        self.line_edit_url_user.textChanged.connect(self._on_line_edit_url_user_text_changed)
-        self.line_edit_id_user.textChanged.connect(self._on_line_edit_id_user_text_changed)
+        self.line_edit_url_user.textChanged.connect(
+            self._on_line_edit_url_user_text_changed
+        )
+        self.line_edit_id_user.textChanged.connect(
+            self._on_line_edit_id_user_text_changed
+        )
 
-        self.push_button_start = QPushButton('Start')
+        self.push_button_start = QPushButton("Start")
         self.push_button_start.clicked.connect(self._start)
 
         self.list_widget_feeds = QListWidget()
         self.list_widget_feeds.itemDoubleClicked.connect(self._on_item_double_clicked)
 
         layout_url_user = QHBoxLayout()
-        layout_url_user.addWidget(QLabel('Url user:'))
+        layout_url_user.addWidget(QLabel("Url user:"))
         layout_url_user.addWidget(self.line_edit_url_user)
-        layout_url_user.addWidget(QLabel('ID user:'))
+        layout_url_user.addWidget(QLabel("ID user:"))
         layout_url_user.addWidget(self.line_edit_id_user)
 
         layout_rss_user = QHBoxLayout()
-        layout_rss_user.addWidget(QLabel('Rss user:'))
+        layout_rss_user.addWidget(QLabel("Rss user:"))
         layout_rss_user.addWidget(self.line_edit_rss_user)
 
         main_layout = QVBoxLayout()
@@ -78,15 +83,17 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(central_widget)
 
         # Set default url
-        self.line_edit_url_user.setText('https://grouple.co/user/315828')
+        self.line_edit_url_user.setText("https://grouple.co/user/315828")
 
     def _on_line_edit_url_user_text_changed(self, text):
-        id_user = text.replace(' ', '').split('/')[-1]
+        id_user = text.replace(" ", "").split("/")[-1]
         self.line_edit_id_user.setText(id_user)
 
     def _on_line_edit_id_user_text_changed(self, text):
         id_user = text.strip()
-        self.line_edit_rss_user.setText(f'https://grouple.co/user/rss/{id_user}?filter=')
+        self.line_edit_rss_user.setText(
+            f"https://grouple.co/user/rss/{id_user}?filter="
+        )
 
     def _start(self):
         self.list_widget_feeds.clear()
@@ -103,12 +110,10 @@ class MainWindow(QMainWindow):
     @staticmethod
     def _on_item_double_clicked(item):
         url = item.data(Qt.UserRole)
-
-        import webbrowser
         webbrowser.open_new_tab(url)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication([])
 
     mw = MainWindow()
