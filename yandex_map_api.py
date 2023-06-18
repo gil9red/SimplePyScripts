9d515df4668b9483033c0ef4a85104b7321f9f02
gil9red <ilya.petrash@inbox.ru>
2023-06-18 18:13:14 +0500

    Refactoring. Using black code style

--- yandex_map_api.py
+++ yandex_map_api.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
+
+
+import requests
 
 
 api_key = None
-url = 'https://search-maps.yandex.ru/v1/?text=Магнитогорск, бизнец-центра&type=biz&lang=ru_RU&apikey={}'.format(api_key)
-import requests
+url = "https://search-maps.yandex.ru/v1/?text=Магнитогорск, бизнец-центра&type=biz&lang=ru_RU&apikey={}".format(
+    api_key
+)
 print(requests.get(url).json())
--- yandex_search_img.py
+++ yandex_search_img.py
@@ -1,31 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 from urllib.parse import urljoin
-from typing import List
 
 import requests
 from bs4 import BeautifulSoup
 
 
-def get_images(text: str) -> List[str]:
-    url = 'https://yandex.ru/images/search?text=' + text
+def get_images(text: str) -> list[str]:
+    url = "https://yandex.ru/images/search?text=" + text
 
     rs = requests.get(url)
-    root = BeautifulSoup(rs.content, 'html.parser')
+    root = BeautifulSoup(rs.content, "html.parser")
 
     return [
-        urljoin(rs.url, img['src'])
-        for img in root.select('img.serp-item__thumb')
+        urljoin(rs.url, img["src"])
+        for img in root.select("img.serp-item__thumb")
     ]
 
 
-if __name__ == '__main__':
-    text = 'Котята'
+if __name__ == "__main__":
+    text = "Котята"
     urls = get_images(text)
-    print(f'Urls ({len(urls)})')
+    print(f"Urls ({len(urls)})")
     for i, url in enumerate(urls, 1):
-        print(f'{i}. {url}')
+        print(f"{i}. {url}")
