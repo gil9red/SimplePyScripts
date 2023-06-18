06fa13f827c8e2a3a7fa51fe6627320e210f9b64
gil9red <ilya.petrash@inbox.ru>
2023-06-18 18:21:02 +0500

    Refactoring. Using black code style

--- winapi__screenshot_window.py
+++ winapi__screenshot_window.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # SOURCE: https://stackoverflow.com/a/24352388/5909792
@@ -9,20 +9,23 @@ __author__ = 'ipetrash'
 
 import win32gui
 import win32ui
+
 from ctypes import windll
+
 from PIL import Image
 
-hwnd = win32gui.FindWindow(None, 'Telegram')
+
+hwnd = win32gui.FindWindow(None, "Telegram")
 
 # Change the line below depending on whether you want the whole window
 # or just the client area.
-#left, top, right, bot = win32gui.GetClientRect(hwnd)
+# left, top, right, bot = win32gui.GetClientRect(hwnd)
 left, top, right, bot = win32gui.GetWindowRect(hwnd)
 w = right - left
 h = bot - top
 
 hwndDC = win32gui.GetWindowDC(hwnd)
-mfcDC  = win32ui.CreateDCFromHandle(hwndDC)
+mfcDC = win32ui.CreateDCFromHandle(hwndDC)
 saveDC = mfcDC.CreateCompatibleDC()
 
 saveBitMap = win32ui.CreateBitmap()
@@ -40,9 +43,8 @@ bmpinfo = saveBitMap.GetInfo()
 bmpstr = saveBitMap.GetBitmapBits(True)
 
 im = Image.frombuffer(
-    'RGB',
-    (bmpinfo['bmWidth'], bmpinfo['bmHeight']),
-    bmpstr, 'raw', 'BGRX', 0, 1)
+    "RGB", (bmpinfo["bmWidth"], bmpinfo["bmHeight"]), bmpstr, "raw", "BGRX", 0, 1
+)
 
 win32gui.DeleteObject(saveBitMap.GetHandle())
 saveDC.DeleteDC()
@@ -50,5 +52,5 @@ mfcDC.DeleteDC()
 win32gui.ReleaseDC(hwnd, hwndDC)
 
 if result == 1:
-    #PrintWindow Succeeded
+    # PrintWindow Succeeded
     im.save("test.png")
--- world_seed_in_binary_2D.py
+++ world_seed_in_binary_2D.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import hashlib
@@ -12,23 +12,20 @@ from itertools import cycle
 
 
 def get_random_seed(length: int = 8) -> str:
-    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+    return "".join(random.choices(string.ascii_letters + string.digits, k=length))
 
 
 def get_bits_seed(seed: str) -> str:
-    seed = bytes(seed, encoding='utf-8')
-    return ''.join(bin(c)[2:][:8].zfill(8) for c in hashlib.sha256(seed).digest())
+    seed = bytes(seed, encoding="utf-8")
+    return "".join(bin(c)[2:][:8].zfill(8) for c in hashlib.sha256(seed).digest())
 
 
 def create_world(rows: int, cols: int) -> list[list[int]]:
-    return [
-        [0] * cols
-        for _ in range(rows)
-    ]
+    return [[0] * cols for _ in range(rows)]
 
 
 def print_world(world: list[list[int]]):
-    print('\n'.join(' '.join(map(str, row)) for row in world))
+    print("\n".join(" ".join(map(str, row)) for row in world))
 
 
 def fill_world(world: list[list[int]], seed: str):
@@ -40,20 +37,26 @@ def fill_world(world: list[list[int]], seed: str):
             world[row][col] = int(next(bits))
 
 
-if __name__ == '__main__':
-    for i in range(8, 64+1):
+if __name__ == "__main__":
+    for i in range(8, 64 + 1):
         assert len(get_random_seed(length=i)) == i
 
-    print('Random seed:', get_random_seed())
+    print("Random seed:", get_random_seed())
     print()
 
-    assert get_bits_seed('1') == '0110101110000110101100100111001111111111001101001111110011100001100111010110101110000000010011101111111101011010001111110101011101000111101011011010010011101010101000100010111100011101010010011100000000011110010100101101110110110111100001110101101101001011'
-    assert get_bits_seed('123') == '1010011001100101101001000101100100100000010000100010111110011101010000010111111001001000011001111110111111011100010011111011100010100000010010100001111100111111111111110001111110100000011111101001100110001110100001101111011111110111101000100111101011100011'
+    assert (
+        get_bits_seed("1")
+        == "0110101110000110101100100111001111111111001101001111110011100001100111010110101110000000010011101111111101011010001111110101011101000111101011011010010011101010101000100010111100011101010010011100000000011110010100101101110110110111100001110101101101001011"
+    )
+    assert (
+        get_bits_seed("123")
+        == "1010011001100101101001000101100100100000010000100010111110011101010000010111111001001000011001111110111111011100010011111011100010100000010010100001111100111111111111110001111110100000011111101001100110001110100001101111011111110111101000100111101011100011"
+    )
 
     world = create_world(rows=5, cols=10)
     print_world(world)
 
     print()
 
-    fill_world(world, seed='123')
+    fill_world(world, seed="123")
     print_world(world)
