5a7557e8654e1ed9afb874306adf01f54d2ad3e6
gil9red <ilya.petrash@inbox.ru>
2023-06-18 18:49:22 +0500

    Refactoring. Using black code style

--- search duplicate files py.py
+++ search duplicate files py.py
@@ -1,34 +1,36 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import glob
-all_file_name = glob.glob('**/*.py', recursive=True)
-print('Total files:', len(all_file_name))
-
-from collections import defaultdict
-hash_by_files = defaultdict(list)
-
 import hashlib
 
+from collections import defaultdict
+
+
+all_file_name = glob.glob("**/*.py", recursive=True)
+print("Total files:", len(all_file_name))
+
+hash_by_files = defaultdict(list)
+
 for file_name in all_file_name:
-    with open(file_name, 'rb') as f:
+    with open(file_name, "rb") as f:
         hash_hex = hashlib.sha1(f.read()).hexdigest()
 
         hash_by_files[hash_hex].append(file_name)
 
-print('Unique files:', len(hash_by_files))
+print("Unique files:", len(hash_by_files))
 print()
 
-print('Duplicates:')
+print("Duplicates:")
 for hash_hex, files in hash_by_files.items():
     if len(files) == 1:
         continue
 
-    print('    {} ({}):'.format(hash_hex, len(files)))
+    print("    {} ({}):".format(hash_hex, len(files)))
     for file_name in files:
-        print('        ' + file_name)
+        print("        " + file_name)
 
     print()
--- search_files_with_Zone.Identifier.py
+++ search_files_with_Zone.Identifier.py
@@ -1,46 +1,49 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # SOURCE: https://blogs.technet.microsoft.com/askcore/2013/03/24/alternate-data-streams-in-ntfs/
 
 
-from glob import glob
-from os.path import isfile, expanduser, normpath
 import subprocess
 
+from glob import glob
+from os.path import isfile, expanduser, normpath
+
+
 # Example: 'C:/Users/<user_name>/Downloads'
-DIR_NAME = normpath(expanduser('~/Downloads'))
+DIR_NAME = normpath(expanduser("~/Downloads"))
 
 print(f'Search in "{DIR_NAME}"\n')
 
 
 # NOTE: For test
-f = open('log.txt', 'w', encoding='utf-8')
+f = open("log.txt", "w", encoding="utf-8")
 
 # For statistic
-files = [file_name for file_name in glob(DIR_NAME + '/**/*', recursive=True) if isfile(file_name)]
+files = [
+    file_name
+    for file_name in glob(DIR_NAME + "/**/*", recursive=True)
+    if isfile(file_name)
+]
 found = 0
 
 for i, file_name in enumerate(files, 1):
-    print(f'{i}/{len(files)} ({int(i / len(files) * 100)}%). found: {found}')
+    print(f"{i}/{len(files)} ({int(i / len(files) * 100)}%). found: {found}")
 
-    escape_file_name = file_name.replace('[', '`[').replace(']', '`]')
+    escape_file_name = file_name.replace("[", "`[").replace("]", "`]")
     try:
         cmd = f'''powershell -Command "get-content '{escape_file_name}' -stream Zone.Identifier"'''
         # print(cmd)
 
         text = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
-        print('  [+]', file_name, text)
-        f.write(file_name + " " + repr(text) + '\n')
+        print("  [+]", file_name, text)
+        f.write(file_name + " " + repr(text) + "\n")
         found += 1
-
-    # except subprocess.CalledProcessError:
-    #     pass
 
     # NOTE: for test
     except subprocess.CalledProcessError as e:
         data = e.output
-        print('  [-]', file_name, e, repr(data.decode('cp1251')))
+        print("  [-]", file_name, e, repr(data.decode("cp1251")))
