a0e09bb80f0a781d7b43b592e3f351e6585db429
gil9red <ilya.petrash@inbox.ru>
2023-06-18 19:32:15 +0500

    Refactoring. Using black code style

--- max_seqs_zeros__itertools.groupby.py
+++ max_seqs_zeros__itertools.groupby.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import itertools
@@ -11,13 +11,13 @@ def end_zeros(num: int) -> int:
     size = 0
 
     for x, items in itertools.groupby(str(num)):
-        if x == '0':
+        if x == "0":
             size = max(size, len(list(items)))
 
     return size
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     assert end_zeros(10979000000) == 6
     assert end_zeros(1000000) == 6
     assert end_zeros(10009) == 3
--- mediawiki__wikipedia.py
+++ mediawiki__wikipedia.py
@@ -1,38 +1,39 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
+
+from urllib.parse import unquote
 
 # pip install pymediawiki
 # https://github.com/barrust/mediawiki
 # http://pymediawiki.readthedocs.io/en/latest/index.html
+from mediawiki import MediaWiki
 
 
-def wiki_search(query: str, lang='ru', unquote_percent_encoded=False) -> str:
+def wiki_search(query: str, lang="ru", unquote_percent_encoded=False) -> str:
     # Default using wikipedia
-    from mediawiki import MediaWiki
     wikipedia = MediaWiki(lang=lang)
     result = wikipedia.opensearch(query, results=1)
     if not result:
-        return ''
+        return ""
 
     _, text, url = result[0]
 
     if unquote_percent_encoded:
-        from urllib.parse import unquote
         url = unquote(url)
 
-    return '{} ({})'.format(text, url)
+    return "{} ({})".format(text, url)
 
 
-if __name__ == '__main__':
-    query = 'GitHub'
+if __name__ == "__main__":
+    query = "GitHub"
     print(wiki_search(query))
 
-    query = 'python'
+    query = "python"
     print(wiki_search(query))
 
-    query = 'магнитогорск'
+    query = "магнитогорск"
     print(wiki_search(query))
     print(wiki_search(query, unquote_percent_encoded=True))
