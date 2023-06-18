301acc1f2daa3cfbf30c0d0a31d0e03a04c7058b
gil9red <ilya.petrash@inbox.ru>
2023-06-18 18:22:47 +0500

    Refactoring. Using black code style

--- weather__openweathermap__pyowm.py
+++ weather__openweathermap__pyowm.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
-
-API_KEY = '87c7712a9b72646a269102230858837b'
-place = 'Магнитогорск'
 
 # pip install pyowm
 import pyowm
+
+
+API_KEY = "87c7712a9b72646a269102230858837b"
+place = "Магнитогорск"
+
 owm = pyowm.OWM(API_KEY)
 observation = owm.weather_at_place(place)
 w = observation.get_weather()
-temperature = w.get_temperature('celsius')['temp']
+temperature = w.get_temperature("celsius")["temp"]
 status = w.get_status()
 
-print('Температура: {} °C'.format(temperature))
-print('Небо: {}'.format(status))
+print("Температура: {} °C".format(temperature))
+print("Небо: {}".format(status))
--- weather__yahoo.py
+++ weather__yahoo.py
@@ -1,32 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
-if __name__ == '__main__':
-    city = "Магнитогорск"
-    # # OR:
-    # city = 'Magnitogorsk'
-    url = "https://query.yahooapis.com/v1/public/yql?q=select item from weather.forecast where woeid in " \
-          "(select woeid from geo.places(1) where text='{city}') and u='c'" \
-          "&format=json&diagnostics=true".format(city=city)
+import requests
 
-    import requests
-    rs = requests.get(url)
-    item = rs.json()['query']['results']['channel']['item']
 
-    # Если нужна иконка для погоды:
-    # https://developer.yahoo.com/weather/documentation.html in Condition Codes
-    # code = condition['code']
-    #
-    # Weather image: http://l.yimg.com/a/i/us/we/52/' + code + '.gif
-    # Example: http://l.yimg.com/a/i/us/we/52/26.gif
-    #
-    condition = item['condition']
-    print('Current: {temp} °C, {text}'.format(**condition))
-    print()
+city = "Магнитогорск"
+# # OR:
+# city = 'Magnitogorsk'
+url = (
+    "https://query.yahooapis.com/v1/public/yql?q=select item from weather.forecast where woeid in "
+    "(select woeid from geo.places(1) where text='{city}') and u='c'"
+    "&format=json&diagnostics=true".format(city=city)
+)
 
-    print('Forecast:')
-    for forecast in item['forecast']:
-        print('{date}: {low} - {high} °C. {day}: {text}'.format(**forecast))
+rs = requests.get(url)
+item = rs.json()["query"]["results"]["channel"]["item"]
+
+# Если нужна иконка для погоды:
+# https://developer.yahoo.com/weather/documentation.html in Condition Codes
+# code = condition['code']
+#
+# Weather image: http://l.yimg.com/a/i/us/we/52/' + code + '.gif
+# Example: http://l.yimg.com/a/i/us/we/52/26.gif
+#
+condition = item["condition"]
+print("Current: {temp} °C, {text}".format(**condition))
+print()
+
+print("Forecast:")
+for forecast in item["forecast"]:
+    print("{date}: {low} - {high} °C. {day}: {text}".format(**forecast))
