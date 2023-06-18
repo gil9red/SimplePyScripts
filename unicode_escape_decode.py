564549cc121b3bf0fb08ddb64b0d75517a2de9ce
gil9red <ilya.petrash@inbox.ru>
2023-06-18 18:27:25 +0500

    Refactoring. Using black code style

--- unicode_escape_decode.py
+++ unicode_escape_decode.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
-
-text = '\u041e\u0442\u0441\u0443\u0442\u0441\u0442\u0432\u0443\u0435\u0442 \u043e\u0431\u044f\u0437\u0430\u0442\u0435\u043b\u044c\u043d\u044b\u0439 \u043f\u0430\u0440\u0430\u043c\u0435\u0442\u0440 (username)'
 
 import codecs
-print(codecs.unicode_internal_decode(text)[0])  # Отсутствует обязательный параметр (username)
+
+
+text = "\u041e\u0442\u0441\u0443\u0442\u0441\u0442\u0432\u0443\u0435\u0442 \u043e\u0431\u044f\u0437\u0430\u0442\u0435\u043b\u044c\u043d\u044b\u0439 \u043f\u0430\u0440\u0430\u043c\u0435\u0442\u0440 (username)"
+
+print(codecs.unicode_internal_decode(text)[0])
+# Отсутствует обязательный параметр (username)
--- unshorten_url.py
+++ unshorten_url.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # SOURCE: https://ru.stackoverflow.com/a/847070/201445
@@ -14,9 +14,9 @@ def unshorten_url(url):
     return requests.head(url, allow_redirects=True).url
 
 
-if __name__ == '__main__':
-    url = 'https://goo.gl/7RvxZh'
+if __name__ == "__main__":
+    url = "https://goo.gl/7RvxZh"
     print(unshorten_url(url))  # https://ru.stackoverflow.com/questions/847061/
 
-    url = 'https://bit.ly/2tHp17X'
+    url = "https://bit.ly/2tHp17X"
     print(unshorten_url(url))  # https://ru.stackoverflow.com/questions/847061/
