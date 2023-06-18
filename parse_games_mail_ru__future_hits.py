ae2dbe8df0c187b15c7946955542b95d0a309812
gil9red <ilya.petrash@inbox.ru>
2023-06-18 19:20:34 +0500

    Refactoring. Using black code style

--- parse_games_mail_ru__future_hits.py
+++ parse_games_mail_ru__future_hits.py
@@ -1,42 +1,47 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
+
+
+from urllib.parse import urljoin
+
+import requests
+from bs4 import BeautifulSoup
 
 
 def get_game_list():
-    from urllib.parse import urljoin
-
-    import requests
-    rs = requests.get('https://games.mail.ru/pc/games/future_hits/')
-
-    from bs4 import BeautifulSoup
-    root = BeautifulSoup(rs.content, 'lxml')
+    rs = requests.get("https://games.mail.ru/pc/games/future_hits/")
+    root = BeautifulSoup(rs.content, "lxml")
 
     items = []
 
     # Перебор табличек с играми
-    for item in root.select('.b-pc__entities-item'):
-        a = item.select_one('.b-pc__entities-title > a')
+    for item in root.select(".b-pc__entities-item"):
+        a = item.select_one(".b-pc__entities-title > a")
         title = a.text.strip()
 
-        url = urljoin(rs.url, a['href'])
+        url = urljoin(rs.url, a["href"])
 
-        description = item.select_one('.b-pc__entities-descr').text.strip()
-        img_url = item.select_one('.b-pc__entities-img')['src']
-        release_date = item.select_one('.b-pc__entities-author').text.strip()
+        description = item.select_one(".b-pc__entities-descr").text.strip()
+        img_url = item.select_one(".b-pc__entities-img")["src"]
+        release_date = item.select_one(".b-pc__entities-author").text.strip()
 
         items.append((title, description, release_date, url, img_url))
 
     return items
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     game_list = get_game_list()
 
     def print_list(items):
         for i, (title, description, release_date, url, img_url) in enumerate(items, 1):
-            print('{:2}. "{}" ({}): {} [{}]\n{}\n'.format(i, title, release_date, url, img_url, description))
+            print(
+                '{:2}. "{}" ({}): {} [{}]\n{}\n'.format(
+                    i, title, release_date, url, img_url, description
+                )
+            )
 
     # Full
     print_list(game_list)
--- parse_magnet_links.py
+++ parse_magnet_links.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # https://en.wikipedia.org/wiki/Magnet_URI_scheme
 # https://ru.wikipedia.org/wiki/Magnet-ссылка
 
+
+from urllib.parse import urlparse, parse_qs
+
+
 def parse(url: str) -> dict:
-    from urllib.parse import urlparse, parse_qs
     result = urlparse(url)
     return parse_qs(result.query)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    import json
+
     links = [
-        'magnet:?xt=urn:btih:19270c27523f428d6b9d23c264e4a0ff57869275&dn=rutor.info_%D0%94%D0%BE%D0%BA%D1%82%D0%BE%D1%80+%D0%A8%D0%B0%D0%BD%D1%81+%2F+Chance+%5B02x01-04+%D0%B8%D0%B7+10%5D+%282017%29+WEB-DLRip+1080p+%7C+Profix+Media&tr=udp://opentor.org:2710&tr=udp://opentor.org:2710&tr=http://retracker.local/announce',
-        'magnet:?xt=urn:btih:1ff5a51d2f65e7c2fa200ada0896075543398a73&dn=rutor.info_Heroes+%26+Generals+%5B12.10.17%5D+%282016%29+PC+%7C+Online-only&tr=udp://opentor.org:2710&tr=udp://opentor.org:2710&tr=http://retracker.local/announce',
-        'magnet:?xt=urn:btih:08c3a30e88b38ccf6926c073ecd41f983cbf2b20&dn=rutor.info_%D0%A4%D0%B8%D0%B7%D1%80%D1%83%D0%BA+%5B04%D1%8501-10+%D0%B8%D0%B7+21%5D+%282017%29+HDTVRip-AVC+%D0%BE%D1%82+GeneralFilm&tr=udp://opentor.org:2710&tr=udp://opentor.org:2710&tr=http://retracker.local/announce',
-        'magnet:?xl=10826029&dn=mediawiki-1.15.1.tar.gz&xt=urn:sha1:XRX2PEFXOOEJFRVUCX6HMZMKS5TWG4K5&as=https%3A%2F%2Freleases.wikimedia.org%2Fmediawiki%2F1.15%2Fmediawiki-1.15.1.tar.gz',
+        "magnet:?xt=urn:btih:19270c27523f428d6b9d23c264e4a0ff57869275&dn=rutor.info_%D0%94%D0%BE%D0%BA%D1%82%D0%BE%D1%80+%D0%A8%D0%B0%D0%BD%D1%81+%2F+Chance+%5B02x01-04+%D0%B8%D0%B7+10%5D+%282017%29+WEB-DLRip+1080p+%7C+Profix+Media&tr=udp://opentor.org:2710&tr=udp://opentor.org:2710&tr=http://retracker.local/announce",
+        "magnet:?xt=urn:btih:1ff5a51d2f65e7c2fa200ada0896075543398a73&dn=rutor.info_Heroes+%26+Generals+%5B12.10.17%5D+%282016%29+PC+%7C+Online-only&tr=udp://opentor.org:2710&tr=udp://opentor.org:2710&tr=http://retracker.local/announce",
+        "magnet:?xt=urn:btih:08c3a30e88b38ccf6926c073ecd41f983cbf2b20&dn=rutor.info_%D0%A4%D0%B8%D0%B7%D1%80%D1%83%D0%BA+%5B04%D1%8501-10+%D0%B8%D0%B7+21%5D+%282017%29+HDTVRip-AVC+%D0%BE%D1%82+GeneralFilm&tr=udp://opentor.org:2710&tr=udp://opentor.org:2710&tr=http://retracker.local/announce",
+        "magnet:?xl=10826029&dn=mediawiki-1.15.1.tar.gz&xt=urn:sha1:XRX2PEFXOOEJFRVUCX6HMZMKS5TWG4K5&as=https%3A%2F%2Freleases.wikimedia.org%2Fmediawiki%2F1.15%2Fmediawiki-1.15.1.tar.gz",
     ]
 
-    import json
     pretty = lambda data: json.dumps(data, indent=4, ensure_ascii=False)
 
     for i, link in enumerate(links, 1):
         data = parse(link)
-        print('{}.\n{}\n'.format(i, pretty(data)))
+        print("{}.\n{}\n".format(i, pretty(data)))
