9710d6acae85d95a396017ee3d029d5e44935346
gil9red <ilya.petrash@inbox.ru>
2023-06-18 18:57:29 +0500

    Refactoring. Using black code style

--- regexp_pattern_to_format_string.py
+++ regexp_pattern_to_format_string.py
@@ -1,38 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import re
-from typing import Union
 
 
-def get_format_string(pattern: Union[re.Pattern, str]) -> str:
+def get_format_string(pattern: re.Pattern | str) -> str:
     if isinstance(pattern, re.Pattern):
         pattern = pattern.pattern
 
-    pattern = pattern.strip('^$')
-    return re.sub(r'\(.+?\)', '{}', pattern)
+    pattern = pattern.strip("^$")
+    return re.sub(r"\(.+?\)", "{}", pattern)
 
 
-if __name__ == '__main__':
-    PATTERN_BOOK = re.compile(r'^book_([a-fA-F\d]+)$')
+if __name__ == "__main__":
+    PATTERN_BOOK = re.compile(r"^book_([a-fA-F\d]+)$")
     fmt = get_format_string(PATTERN_BOOK)
     print(fmt)
-    assert fmt == 'book_{}'
+    assert fmt == "book_{}"
 
-    PATTERN_BOOK_IMAGE = re.compile(r'^([a-fA-F\d]+)_image_(.+)$')
+    PATTERN_BOOK_IMAGE = re.compile(r"^([a-fA-F\d]+)_image_(.+)$")
     fmt = get_format_string(PATTERN_BOOK_IMAGE)
     print(fmt)
-    assert fmt == '{}_image_{}'
+    assert fmt == "{}_image_{}"
 
-    PATTERN_BOOK_ANNOTATION = re.compile(r'^([a-fA-F\d]+)_annotation$')
+    PATTERN_BOOK_ANNOTATION = re.compile(r"^([a-fA-F\d]+)_annotation$")
     fmt = get_format_string(PATTERN_BOOK_ANNOTATION)
     print(fmt)
-    assert fmt == '{}_annotation'
+    assert fmt == "{}_annotation"
 
-    PATTERN_HIDE_IMAGE = 'hide_image'
+    PATTERN_HIDE_IMAGE = "hide_image"
     fmt = get_format_string(PATTERN_HIDE_IMAGE)
     print(fmt)
-    assert fmt == 'hide_image'
+    assert fmt == "hide_image"
--- remove_all_py__author__.py
+++ remove_all_py__author__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import re
@@ -10,24 +10,24 @@ import shutil
 from pathlib import Path
 
 
-TEMPLATE_DIR = 'No __author__ in {path}'
+TEMPLATE_DIR = "No __author__ in {path}"
 
 
 def process_file(file_name: Path):
-    text = file_name.read_text('utf-8')
-    text = re.sub(r'__author__ = .+', '', text)
-    text = re.sub(r'\n{4,}', '\n\n\n', text)
+    text = file_name.read_text("utf-8")
+    text = re.sub(r"__author__ = .+", "", text)
+    text = re.sub(r"\n{4,}", "\n\n\n", text)
 
-    file_name.write_text(text, 'utf-8')
+    file_name.write_text(text, "utf-8")
 
 
 def run(path: str):
     path = Path(path)
     if not path.exists():
-        raise Exception(f'Not found {path}!')
+        raise Exception(f"Not found {path}!")
 
     if not path.is_dir():
-        raise Exception(f'Must be directory: {path}!')
+        raise Exception(f"Must be directory: {path}!")
 
     dir_path = path.parent
     copy_dir_path = dir_path / (TEMPLATE_DIR.format(path=path.name))
@@ -36,9 +36,9 @@ def run(path: str):
     shutil.copytree(path, copy_dir_path)
 
     # Удалить из всех ее файлов указание автора
-    for file_name in copy_dir_path.rglob('*.py'):
+    for file_name in copy_dir_path.rglob("*.py"):
         process_file(file_name)
 
 
-if __name__ == '__main__':
-    run(r'C:\Users\ipetrash\PycharmProjects\SNMP_monitor')
+if __name__ == "__main__":
+    run(r"C:\Users\ipetrash\PycharmProjects\SNMP_monitor")
