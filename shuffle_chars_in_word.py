db4d03ba1b65b1b8034001f15d1a93396e796022
gil9red <ilya.petrash@inbox.ru>
2023-06-18 18:44:21 +0500

    Refactoring. Using black code style

--- shuffle_chars_in_word.py
+++ shuffle_chars_in_word.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
+
+
+import random
 
 
 def shuffle_chars_in_word(text):
-    import random
-
     words = [list(word) for word in text.split()]
     new_words = list()
     for word in words:
         random.shuffle(word)
+        new_words.append("".join(word))
 
-        new_words.append(''.join(word))
-
-    text = ' '.join(new_words)
+    text = " ".join(new_words)
     return text.capitalize()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     text = "Иди нахуй!"
     print(shuffle_chars_in_word(text))
     print(shuffle_chars_in_word(text))
--- shutdown.py
+++ shutdown.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import time
@@ -13,25 +13,33 @@ def shutdown(off_pc=19):
     while time.localtime().tm_hour != off_pc:
         time.sleep(60)  # Ожидание 1 минута
 
-    if sys.platform == 'win32':
+    if sys.platform == "win32":
         import ctypes
         import win32security
         import win32api
         import winerror
 
         # Try to enable the required privileges to change the power state
-        privilege_flags = win32security.TOKEN_ADJUST_PRIVILEGES | win32security.TOKEN_QUERY
-        token_handle = win32security.OpenProcessToken(win32api.GetCurrentProcess(), privilege_flags)
-        privilege_id = win32security.LookupPrivilegeValue(None, win32security.SE_SHUTDOWN_NAME)
+        privilege_flags = (
+            win32security.TOKEN_ADJUST_PRIVILEGES | win32security.TOKEN_QUERY
+        )
+        token_handle = win32security.OpenProcessToken(
+            win32api.GetCurrentProcess(), privilege_flags
+        )
+        privilege_id = win32security.LookupPrivilegeValue(
+            None, win32security.SE_SHUTDOWN_NAME
+        )
 
         # Does the user have permission to change the power state?
-        win32security.AdjustTokenPrivileges(token_handle, 0, [(privilege_id, win32security.SE_PRIVILEGE_ENABLED)])
+        win32security.AdjustTokenPrivileges(
+            token_handle, 0, [(privilege_id, win32security.SE_PRIVILEGE_ENABLED)]
+        )
 
         if ctypes.GetLastError() != winerror.ERROR_SUCCESS:
             print(ctypes.WinError(), file=sys.stderr)
             sys.exit()
 
-        user32 = ctypes.WinDLL('user32')
+        user32 = ctypes.WinDLL("user32")
 
         # https://docs.microsoft.com/en-us/windows/desktop/api/winuser/nf-winuser-exitwindowsex
         user32.ExitWindowsEx(0x00000001, 0x00000000)
@@ -41,8 +49,8 @@ def shutdown(off_pc=19):
             sys.exit()
 
     else:
-        os.system('sudo shutdown now')
+        os.system("sudo shutdown now")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     shutdown()
