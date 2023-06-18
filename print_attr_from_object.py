ea1a6a79509b08994d843aebdb972a3e1d601936
gil9red <ilya.petrash@inbox.ru>
2023-06-18 19:08:48 +0500

    Refactoring. Using black code style

--- print_attr_from_object.py
+++ print_attr_from_object.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 obj = 1
 
-print(list(filter(lambda x: not x.startswith('_'), dir(obj))))
-for i in list(filter(lambda x: not x.startswith('_'), dir(obj))):
-    print(i, ':', getattr(obj, i))
+print(list(filter(lambda x: not x.startswith("_"), dir(obj))))
+for i in list(filter(lambda x: not x.startswith("_"), dir(obj))):
+    print(i, ":", getattr(obj, i))
 print()
--- print_class_name.py
+++ print_class_name.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 class A:
     pass
+
 
 a = A()
 print(A.__name__)  # A
@@ -15,5 +16,6 @@ print(a.__class__.__name__)  # A
 class B:
     def __init__(self):
         print(self.__class__.__name__)  # B
+
 
 B()
