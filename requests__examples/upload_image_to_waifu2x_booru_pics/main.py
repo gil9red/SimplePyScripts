93b08040e43beb88cfc6a0a7704e77448526f0a4
gil9red <ilya.petrash@inbox.ru>
2023-06-19 15:08:28 +0500

    Refactoring. Using black code style

--- requests__examples/upload_image_to_waifu2x_booru_pics/main.py
+++ requests__examples/upload_image_to_waifu2x_booru_pics/main.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import time
 import requests
 
+
 URL = "https://waifu2x.booru.pics/Home/upload"
 HEADERS = {
-    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65",
 }
 DATA = {
-    'denoise': '1',
-    'scale': '1',
+    "denoise": "1",
+    "scale": "1",
 }
-FILES = {
-    'img': open('image.jpg', 'rb')
-}
+FILES = {"img": open("image.jpg", "rb")}
 
 with requests.session() as s:
     s.headers.update(HEADERS)
@@ -27,7 +26,7 @@ with requests.session() as s:
     # https://waifu2x.booru.pics/Home/status?handle=H%3Awaifu2x.slayerduck.com%3A2949305&hash=c8a984fc10b416869ca04c8f8629d429d4b28461_s1_n1
 
     # Ждем пока файл обработается на сервере
-    while '/Home/status?' in rs.url:
+    while "/Home/status?" in rs.url:
         rs = s.get(rs.url)
         time.sleep(0.5)
 
