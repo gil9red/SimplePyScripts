b7aa699024c61b5497743d42f17e106600c73795
gil9red <ilya.petrash@inbox.ru>
2023-06-19 17:18:22 +0500

    Refactoring. Using black code style

--- requests__examples/vk_search_video.py
+++ requests__examples/vk_search_video.py
@@ -1,48 +1,53 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 """Скрипт для эмуляции запроса поиска видео в vk"""
 
 
-LOGIN = '<LOGIN>'
-PASSWORD = '<PASSWORD>'
+import json
+import re
 
-if __name__ == '__main__':
-    import requests
+import requests
+
+
+LOGIN = "<LOGIN>"
+PASSWORD = "<PASSWORD>"
+
+if __name__ == "__main__":
     session = requests.Session()
-    session.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0'
+    session.headers[
+        "User-Agent"
+    ] = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0"
 
-    rs = session.get('https://m.vk.com')
-
-    import re
+    rs = session.get("https://m.vk.com")
     match = re.search(r'<form method="post" action="(.+?)"', rs.text)
 
     # Авторизация. По полученной ссылке делаем post запрос с данными формы -- логин и пароль
     url = match.group(1)
-    url = url + '&email=' + LOGIN + '&pass=' + PASSWORD
+    url = url + "&email=" + LOGIN + "&pass=" + PASSWORD  # TODO: using params
     rs = session.post(url)
 
-    rs = session.post('https://vk.com/al_video.php?act=search_video&al=1&offset=0', data={'q': 'Варкрафт 2016'})
+    rs = session.post(
+        "https://vk.com/al_video.php?act=search_video&al=1&offset=0",
+        data={"q": "Варкрафт 2016"},
+    )
     print(rs)
     json_text = rs.text
     print(json_text)
 
-    start = json_text.index('{')
-    end = json_text.rindex('}')
-    json_text = json_text[start:end + 1]
+    start = json_text.index("{")
+    end = json_text.rindex("}")
+    json_text = json_text[start : end + 1]
     print(json_text)
-    import json
+
     data = json.loads(json_text)
     print(data)
 
     print()
-    print('Result:')
-
-    import requests
-    import re
+    print("Result:")
 
     def get_video_file_urls(url):
         video_urls = list()
@@ -52,8 +57,8 @@ if __name__ == '__main__':
             print(rs)
             return video_urls
 
-        for source in re.findall(r'<source.+?>', rs.text):
-            source = source.replace('\\', '')
+        for source in re.findall(r"<source.+?>", rs.text):
+            source = source.replace("\\", "")
             match = re.search('src="(http.+?\.mp4).*?"', source)
             if match:
                 url_video = match.group(1)
@@ -61,8 +66,7 @@ if __name__ == '__main__':
 
         return video_urls
 
-
-    for video_data in data['list']:
+    for video_data in data["list"]:
         print(video_data)
         owner = video_data[0]
         video_id = video_data[1]
@@ -71,8 +75,8 @@ if __name__ == '__main__':
 
         # TODO: для мобильной ссылки работает поиск, однако качество всегдла -- 240
         # нужно научить парсер искать ссылки в другом формате, текущий -- через тег source работает
-        video_url = 'https://m.vk.com/video{}_{}'.format(owner, video_id)
+        video_url = "https://m.vk.com/video{}_{}".format(owner, video_id)
         print(title, video_url)
         for file_url in get_video_file_urls(video_url):
-            print('    {}'.format(file_url))
+            print("    {}".format(file_url))
         print()
--- requests__examples/yahoo_api__rate_currency.py
+++ requests__examples/yahoo_api__rate_currency.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # TODO: использовать http://www.cbr.ru/scripts/Root.asp?PrtId=SXML или разобраться с данными от query.yahooapis.com
@@ -11,8 +11,11 @@ __author__ = 'ipetrash'
 # TODO: сделать сервер
 import requests
 
-rs = requests.get('https://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+%22USDRUB,EURRUB%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=')
+
+rs = requests.get(
+    "https://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+%22USDRUB,EURRUB%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
+)
 print(rs.json())
 
-for rate in rs.json()['query']['results']['rate']:
-    print(rate['Name'], rate['Rate'])
+for rate in rs.json()["query"]["results"]["rate"]:
+    print(rate["Name"], rate["Rate"])
