fc3d5e25d7311bf26798006a8955edde8e453bf6
gil9red <ilya.petrash@inbox.ru>
2023-06-20 12:42:16 +0500

    Refactoring. Using black code style

--- roman_numbers/main.py
+++ roman_numbers/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # SOURCE: https://github.com/zopefoundation/roman
@@ -9,25 +9,26 @@ __author__ = 'ipetrash'
 
 import roman
 
-print(roman.toRoman(5))     # V
-print(roman.toRoman(9))     # IX
-print(roman.toRoman(10))    # X
-print(roman.toRoman(13))    # XIII
-print(roman.toRoman(255))   # CCLV
+
+print(roman.toRoman(5))  # V
+print(roman.toRoman(9))  # IX
+print(roman.toRoman(10))  # X
+print(roman.toRoman(13))  # XIII
+print(roman.toRoman(255))  # CCLV
 print(roman.toRoman(1024))  # MXXIV
 print()
 
-print(roman.fromRoman('V'))      # 5
-print(roman.fromRoman('IX'))     # 9
-print(roman.fromRoman('X'))      # 10
-print(roman.fromRoman('XIII'))   # 13
-print(roman.fromRoman('CCLV'))   # 255
-print(roman.fromRoman('MXXIV'))  # 1024
+print(roman.fromRoman("V"))  # 5
+print(roman.fromRoman("IX"))  # 9
+print(roman.fromRoman("X"))  # 10
+print(roman.fromRoman("XIII"))  # 13
+print(roman.fromRoman("CCLV"))  # 255
+print(roman.fromRoman("MXXIV"))  # 1024
 
 assert roman.fromRoman(roman.toRoman(5)) == 5
 assert roman.fromRoman(roman.toRoman(1024)) == 1024
 assert roman.fromRoman(roman.toRoman(4048)) == 4048
 
-assert roman.toRoman(roman.fromRoman('MXXIV')) == 'MXXIV'
-assert roman.toRoman(roman.fromRoman('XIII')) == 'XIII'
-assert roman.toRoman(roman.fromRoman('IX')) == 'IX'
+assert roman.toRoman(roman.fromRoman("MXXIV")) == "MXXIV"
+assert roman.toRoman(roman.fromRoman("XIII")) == "XIII"
+assert roman.toRoman(roman.fromRoman("IX")) == "IX"
