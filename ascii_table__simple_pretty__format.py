2f8c1db98f03a9c57df9dc99a28defac6791fdac
gil9red <ilya.petrash@inbox.ru>
2023-06-24 22:58:32 +0500

    Refactoring. Using black code style

--- ascii_table__simple_pretty__format.py
+++ ascii_table__simple_pretty__format.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
-def pretty_table(data, cell_sep=' | ', header_separator=True, align='>') -> str:
+def pretty_table(data, cell_sep=" | ", header_separator=True, align=">") -> str:
     rows = len(data)
     cols = len(data[0])
 
@@ -13,8 +13,8 @@ def pretty_table(data, cell_sep=' | ', header_separator=True, align='>') -> str:
         columns = [str(data[row][col]) for row in range(rows)]
         col_width.append(len(max(columns, key=len)))
 
-    templates = ['{:' + align + '%d}' % width for width in col_width]
-    separator = "-+-".join('-' * n for n in col_width)
+    templates = ["{:" + align + "%d}" % width for width in col_width]
+    separator = "-+-".join("-" * n for n in col_width)
 
     lines = []
     for i, row in enumerate(range(rows)):
@@ -29,20 +29,20 @@ def pretty_table(data, cell_sep=' | ', header_separator=True, align='>') -> str:
         if i == 0 and header_separator:
             lines.append(separator)
 
-    return '\n'.join(lines)
+    return "\n".join(lines)
 
 
-def print_pretty_table(data, cell_sep=' | ', header_separator=True, align='>'):
+def print_pretty_table(data, cell_sep=" | ", header_separator=True, align=">"):
     print(pretty_table(data, cell_sep, header_separator, align))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     table_data = [
-        ['FRUIT', 'PERSON', 'ANIMAL'],
-        ['apples', 'Alice', 'dogs'],
-        ['oranges', 'Bob', 'cats'],
-        ['cherries', 'Carol', 'moose'],
-        ['banana', 'David', 'goose'],
+        ["FRUIT", "PERSON", "ANIMAL"],
+        ["apples", "Alice", "dogs"],
+        ["oranges", "Bob", "cats"],
+        ["cherries", "Carol", "moose"],
+        ["banana", "David", "goose"],
     ]
 
     print_pretty_table(table_data, header_separator=False)
@@ -64,4 +64,4 @@ if __name__ == '__main__':
 
     print()
 
-    print_pretty_table([['FRUIT', 'PERSON', 'ANIMAL']])
+    print_pretty_table([["FRUIT", "PERSON", "ANIMAL"]])
--- ascii_table__simple_pretty__rjust.py
+++ ascii_table__simple_pretty__rjust.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
-def pretty_table(data, cell_sep=' | ', header_separator=True) -> str:
+def pretty_table(data, cell_sep=" | ", header_separator=True) -> str:
     rows = len(data)
     cols = len(data[0])
 
@@ -13,7 +13,7 @@ def pretty_table(data, cell_sep=' | ', header_separator=True) -> str:
         columns = [str(data[row][col]) for row in range(rows)]
         col_width.append(len(max(columns, key=len)))
 
-    separator = "-+-".join('-' * n for n in col_width)
+    separator = "-+-".join("-" * n for n in col_width)
 
     lines = []
 
@@ -28,20 +28,20 @@ def pretty_table(data, cell_sep=' | ', header_separator=True) -> str:
         if i == 0 and header_separator:
             lines.append(separator)
 
-    return '\n'.join(lines)
+    return "\n".join(lines)
 
 
-def print_pretty_table(data, cell_sep=' | ', header_separator=True):
+def print_pretty_table(data, cell_sep=" | ", header_separator=True):
     print(pretty_table(data, cell_sep, header_separator))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     table_data = [
-        ['FRUIT', 'PERSON', 'ANIMAL'],
-        ['apples', 'Alice', 'dogs'],
-        ['oranges', 'Bob', 'cats'],
-        ['cherries', 'Carol', 'moose'],
-        ['banana', 'David', 'goose'],
+        ["FRUIT", "PERSON", "ANIMAL"],
+        ["apples", "Alice", "dogs"],
+        ["oranges", "Bob", "cats"],
+        ["cherries", "Carol", "moose"],
+        ["banana", "David", "goose"],
     ]
 
     print_pretty_table(table_data, header_separator=False)
@@ -63,4 +63,4 @@ if __name__ == '__main__':
 
     print()
 
-    print_pretty_table([['FRUIT', 'PERSON', 'ANIMAL']])
+    print_pretty_table([["FRUIT", "PERSON", "ANIMAL"]])
