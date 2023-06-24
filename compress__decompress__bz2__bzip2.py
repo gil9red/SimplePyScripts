6cdc5d51e33b1d9bca4312bfca49f6c41d4078d7
gil9red <ilya.petrash@inbox.ru>
2023-06-24 22:45:22 +0500

    Refactoring. Using black code style

--- compress__decompress__bz2__bzip2.py
+++ compress__decompress__bz2__bzip2.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # SOURCE: https://docs.python.org/3/library/bz2.html#one-shot-de-compression
 
 
+import bz2
+
+
 s_in = "HelloWorld!" * 1000000
-s_in = bytes(s_in, encoding='utf-8')
+s_in = bytes(s_in, encoding="utf-8")
 print(len(s_in))  # 11000000
 
-import bz2
 s_out = bz2.compress(s_in)
 assert bz2.decompress(s_out) == s_in
 print(len(s_out))  # 927
--- compress__decompress__zlib.py
+++ compress__decompress__zlib.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
+
+
+import zlib
 
 
 s_in = "HelloWorld!" * 1000000
-s_in = bytes(s_in, encoding='utf-8')
+s_in = bytes(s_in, encoding="utf-8")
 print(len(s_in))  # 11000000
 
-import zlib
 s_out = zlib.compress(s_in)
 assert zlib.decompress(s_out) == s_in
 print(len(s_out))  # 21384
