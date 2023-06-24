9ab80a1b6cf8817926c912595ea5c03b738e0b2a
gil9red <ilya.petrash@inbox.ru>
2023-06-24 19:58:34 +0500

    Refactoring. Using black code style

--- confucius_quotes.py
+++ confucius_quotes.py
@@ -1,25 +1,29 @@
-from urllib.parse import quote
-from grab import Grab
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+__author__ = "ipetrash"
 
-__author__ = 'ipetrash'
 
 # Скрипт качает цитаты Конфуция из викицитатника.
 
 
+from urllib.parse import quote
+from grab import Grab
+
+
 def get_confucius_quotes():
-    url = quote('http://ru.wikiquote.org/wiki/Конфуций', safe='/:')
+    url = quote("http://ru.wikiquote.org/wiki/Конфуций", safe="/:")
 
     g = Grab()
     g.go(url)
 
     quotes = list()
-    for el in g.doc.select('//h2/following-sibling::ul/li'):
+    for el in g.doc.select("//h2/following-sibling::ul/li"):
         quotes.append(el.text())
 
     return quotes
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     for q in get_confucius_quotes():
         print("'{}'".format(q))
