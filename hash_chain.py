806df21381fccf19e0a744d16de8129dc1349e6d
gil9red <ilya.petrash@inbox.ru>
2023-06-24 15:01:21 +0500

    Refactoring. Using black code style

--- hash_chain.py
+++ hash_chain.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # https://en.wikipedia.org/wiki/Hash_chain
 
 
+import hashlib
+
+
 def get_hash(text):
-    import hashlib
-    sha256 = hashlib.sha256(text.encode('utf-8'))
+    sha256 = hashlib.sha256(text.encode("utf-8"))
     return sha256.hexdigest()
 
 
@@ -20,28 +22,28 @@ def hash_chain(text, number=1):
     return text
 
 
-if __name__ == '__main__':
-    text = 'Hello World!'
-    print('Text:', text)
+if __name__ == "__main__":
+    text = "Hello World!"
+    print("Text:", text)
 
     # A hash chain is a successive application of a cryptographic hash function h to a string x x.
     # For example, h(h(h(h(x)))) gives a hash chain of length 4, often denoted h^{4}(x)
     h = get_hash
-    print('hash chain (5):', h(h(h(h(h(text))))))
-    print('\n')
+    print("hash chain (5):", h(h(h(h(h(text))))))
+    print("\n")
 
-    print('hash_chain')
+    print("hash_chain")
 
     hash_text = hash_chain(text)
-    print('hash_chain(1):', hash_text)
+    print("hash_chain(1):", hash_text)
     print()
 
     hash_text = hash_chain(text, number=4)
-    print('hash_chain(4):', hash_text)
+    print("hash_chain(4):", hash_text)
 
     hash_text = hash_chain(hash_text)
-    print('hash_chain(4+1):', hash_text)
+    print("hash_chain(4+1):", hash_text)
     print()
 
     hash_text = hash_chain(text, number=5)
-    print('hash_chain(5):', hash_text)
+    print("hash_chain(5):", hash_text)
--- how_create_С„РѕС‚РєРё.py
+++ how_create_С„РѕС‚РєРё.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
-with open('1.txt', 'a', encoding='cp1251') as f:
-    f.write('фотки\n')
+with open("1.txt", "a", encoding="cp1251") as f:
+    f.write("фотки\n")
 
-with open('1.txt', 'a', encoding='utf-8') as f:
-    f.write('фотки\n')
+with open("1.txt", "a", encoding="utf-8") as f:
+    f.write("фотки\n")
 
 # OR...
 
-with open('2.txt', 'ab') as f:
-    f.write('фотки\n'.encode('cp1251'))
+with open("2.txt", "ab") as f:
+    f.write("фотки\n".encode("cp1251"))
 
-with open('2.txt', 'ab') as f:
-    f.write('фотки\n'.encode('utf-8'))
+with open("2.txt", "ab") as f:
+    f.write("фотки\n".encode("utf-8"))
