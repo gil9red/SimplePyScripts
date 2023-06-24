2bff66a3e92aef337b44ef60fc9a912ca4ce6fbd
gil9red <ilya.petrash@inbox.ru>
2023-06-24 19:36:52 +0500

    Refactoring. Using black code style

--- find_groups_of_repeating_sequences__using_itertools_groupby.py
+++ find_groups_of_repeating_sequences__using_itertools_groupby.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 from itertools import groupby
 
 
 def get_groups_seqs(text):
-    return [''.join(v) for _, v in groupby(text)]
+    return ["".join(v) for _, v in groupby(text)]
 
 
-if __name__ == '__main__':
-    text = 'hhhrrrraaavvvvvvv'
+if __name__ == "__main__":
+    text = "hhhrrrraaavvvvvvv"
     items = get_groups_seqs(text)
     print(items)  # ['hhh', 'rrrr', 'aaa', 'vvvvvvv']
--- find_groups_of_repeating_sequences__using_re.py
+++ find_groups_of_repeating_sequences__using_re.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import re
 
 
 def get_groups_seqs(text):
-    return [m.group() for m in re.finditer(r'(.)\1+', text)]
+    return [m.group() for m in re.finditer(r"(.)\1+", text)]
 
 
-if __name__ == '__main__':
-    text = 'hhhrrrraaavvvvvvv'
+if __name__ == "__main__":
+    text = "hhhrrrraaavvvvvvv"
     items = get_groups_seqs(text)
     print(items)  # ['hhh', 'rrrr', 'aaa', 'vvvvvvv']
