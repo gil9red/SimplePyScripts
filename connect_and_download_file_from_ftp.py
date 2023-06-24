2c7ab9d744ce5f7757623c31b9dffbc835500e0d
gil9red <ilya.petrash@inbox.ru>
2023-06-24 19:57:54 +0500

    Refactoring. Using black code style

--- connect_and_download_file_from_ftp.py
+++ connect_and_download_file_from_ftp.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 from ftplib import FTP
 
+
 # Connect to host, default port
-ftp = FTP('ftp.debian.org')
+ftp = FTP("ftp.debian.org")
 print(ftp.host)
 
 # User anonymous, passwd anonymous
 print(ftp.login())
 
 # Change into "debian" directory
-print(ftp.cwd('debian'))
+print(ftp.cwd("debian"))
 
 # List directory contents
-print(ftp.retrlines('LIST'))
+print(ftp.retrlines("LIST"))
 
 # Show file content
-print(ftp.retrbinary('RETR README', lambda data: print(repr(data))))
+print(ftp.retrbinary("RETR README", lambda data: print(repr(data))))
 
 # Download file
-print(ftp.retrbinary('RETR README', open('README', 'wb').write))
+print(ftp.retrbinary("RETR README", open("README", "wb").write))
 
 # Send a QUIT command to the server and close the connection
 print(ftp.quit())
