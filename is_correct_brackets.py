aa7a6aaa679b9d4052685443f20cc83f54121f21
gil9red <ilya.petrash@inbox.ru>
2023-06-24 14:54:32 +0500

    Refactoring. Using black code style

--- is_correct_brackets.py
+++ is_correct_brackets.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 def is_correct_brackets(text):
-    while '()' in text or '[]' in text or '{}' in text:
-        text = text.replace('()', '')
-        text = text.replace('[]', '')
-        text = text.replace('{}', '')
+    while "()" in text or "[]" in text or "{}" in text:
+        text = text.replace("()", "")
+        text = text.replace("[]", "")
+        text = text.replace("{}", "")
 
     # Возвращаем True, если text с пустой строкой
     return not text
 
 
-if __name__ == '__main__':
-    assert is_correct_brackets('(((())))')
-    assert is_correct_brackets('(((())') is False
-    assert is_correct_brackets('())))') is False
-    assert is_correct_brackets('((((){}[]{}[])))')
-    assert is_correct_brackets('(){}[]{}[])))') is False
-    assert is_correct_brackets('(){}[]{}[]')
+if __name__ == "__main__":
+    assert is_correct_brackets("(((())))")
+    assert is_correct_brackets("(((())") is False
+    assert is_correct_brackets("())))") is False
+    assert is_correct_brackets("((((){}[]{}[])))")
+    assert is_correct_brackets("(){}[]{}[])))") is False
+    assert is_correct_brackets("(){}[]{}[]")
--- is_even__is_odd.py
+++ is_even__is_odd.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 def is_even(num):
@@ -16,8 +16,8 @@ def is_even_2(num):
     return num & 1 == 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     for i in range(10):
-        print('{} is even: {}, {}'.format(i, is_even(i), is_even_2(i)))
-        print('{} is odd: {}'.format(i, is_odd(i)))
+        print("{} is even: {}, {}".format(i, is_even(i), is_even_2(i)))
+        print("{} is odd: {}".format(i, is_odd(i)))
         print()
