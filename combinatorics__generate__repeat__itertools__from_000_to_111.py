bf4d478b00f02be4fbcf30cf83693224c8cb46c0
gil9red <ilya.petrash@inbox.ru>
2023-06-24 22:46:51 +0500

    Refactoring. Using black code style

--- combinatorics__generate__repeat__itertools__from_000_to_111.py
+++ combinatorics__generate__repeat__itertools__from_000_to_111.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import itertools
-items = [''.join(i) for i in itertools.product('01', repeat=3)]
+
+
+items = ["".join(i) for i in itertools.product("01", repeat=3)]
 print(items)  # ['000', '001', '010', '011', '100', '101', '110', '111']
 
-items = [i for i in items if '11' not in i]
+items = [i for i in items if "11" not in i]
 print(items)  # ['000', '001', '010', '100', '101']
