7ae6a4a51fd0853d2bd1e9efe85260ddc9e5eb5f
gil9red <ilya.petrash@inbox.ru>
2023-06-24 23:04:57 +0500

    Refactoring. Using black code style

--- zlib_example/zlib_example.py
+++ zlib_example/zlib_example.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # Data Compression (модуль zlib_example)
 import zlib
 
-text = b'witch which has which witches wrist watch'
-print('[{}]: {} (crc32: {})'.format(len(text), text, zlib.crc32(text)))
+
+text = b"witch which has which witches wrist watch"
+print(f"[{len(text)}]: {text} (crc32: {zlib.crc32(text)})")
 
 print()
-print('Compress...')
+print("Compress...")
 compress_text = zlib.compress(text)
-print('[{}]: {} (crc32: {})'.format(len(compress_text), compress_text, zlib.crc32(compress_text)))
+print(f"[{len(compress_text)}]: {compress_text} (crc32: {zlib.crc32(compress_text)})")
 
 print()
-print('Decompress...')
+print("Decompress...")
 
 decompress_text = zlib.decompress(compress_text)
-print('[{}]: {} (crc32: {})'.format(len(decompress_text), decompress_text, zlib.crc32(decompress_text)))
+print(
+    f"[{len(decompress_text)}]: {decompress_text} (crc32: {zlib.crc32(decompress_text)})"
+)
