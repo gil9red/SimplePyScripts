dcf6e14ed1f69e69b2cba7afbdea87f0bf969f74
gil9red <ilya.petrash@inbox.ru>
2023-06-24 15:06:17 +0500

    Refactoring. Using black code style

--- get_timestamp_datetime_from_UUID_version_1.py
+++ get_timestamp_datetime_from_UUID_version_1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import uuid
@@ -12,7 +12,7 @@ from datetime import datetime
 
 
 def get_timestamp_from_UUIDv1(uuid_obj: uuid.UUID) -> float:
-    return (uuid_obj.time - 0x01b21dd213814000) * 100 / 1e9
+    return (uuid_obj.time - 0x01B21DD213814000) * 100 / 1e9
 
 
 def get_datetime_from_UUIDv1(uuid_obj: uuid.UUID) -> datetime:
@@ -20,17 +20,23 @@ def get_datetime_from_UUIDv1(uuid_obj: uuid.UUID) -> datetime:
     return datetime.fromtimestamp(timestamp)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     uuid_obj = uuid.UUID("a860729c-c7c0-11e8-94e9-f079598c1eec")
     print(get_datetime_from_UUIDv1(uuid_obj))  # 2018-10-04 15:31:30.734147
     print()
 
     uuid_variant1_list = [
-        "a860729c-c7c0-11e8-94e9-f079598c1eec", "a8f9c0dc-c7c0-11e8-84fc-f079598c1eec",
-        "a9925f02-c7c0-11e8-8a15-f079598c1eec", "aa2b2152-c7c0-11e8-90a0-f079598c1eec"
+        "a860729c-c7c0-11e8-94e9-f079598c1eec",
+        "a8f9c0dc-c7c0-11e8-84fc-f079598c1eec",
+        "a9925f02-c7c0-11e8-8a15-f079598c1eec",
+        "aa2b2152-c7c0-11e8-90a0-f079598c1eec",
     ]
 
     for uuid1_value in uuid_variant1_list:
         uuid_obj = uuid.UUID(uuid1_value)
         timestamp = get_timestamp_from_UUIDv1(uuid_obj)
-        print("'{}': {} -> {}".format(uuid_obj, timestamp, datetime.fromtimestamp(timestamp)))
+        print(
+            "'{}': {} -> {}".format(
+                uuid_obj, timestamp, datetime.fromtimestamp(timestamp)
+            )
+        )
--- get_unique_items__with_ordered.py
+++ get_unique_items__with_ordered.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
+
+
+from collections import OrderedDict
+
+# pip install more_itertools
+from more_itertools import unique_everseen
 
 
 items = [1, 2, 0, 1, 3, 2]
 
-
 seen = set()
 print([x for x in items if x not in seen and not seen.add(x)])  # [1, 2, 0, 3]
 
-
-from collections import OrderedDict
 print(list(OrderedDict.fromkeys(items)))  # [1, 2, 0, 3]
 
-
-# pip install more_itertools
-from more_itertools import unique_everseen
 print(list(unique_everseen(items)))  # [1, 2, 0, 3]
