eeece088793546dc93454ea58799be55fbe02653
gil9red <ilya.petrash@inbox.ru>
2023-06-24 19:41:50 +0500

    Refactoring. Using black code style

--- dynamic_methods_link_call.py
+++ dynamic_methods_link_call.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 class CallBuilder:
-    def __init__(self, part=None, sep=''):
+    def __init__(self, part=None, sep=""):
         self._part = part
         self._sep = sep
 
     def __getattr__(self, part):
         return CallBuilder(
-            part=((self._part + self._sep) if self._part else '') + part,
-            sep=self._sep
+            part=((self._part + self._sep) if self._part else "") + part,
+            sep=self._sep,
         )
 
     def __call__(self, **kwargs):
         return self._part
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     builder = CallBuilder()
     result = builder.H.e.l.l.o._.World()
     print(result)  # Hello_World
 
-    builder = CallBuilder(sep='.')
+    builder = CallBuilder(sep=".")
     result = builder.H.e.l.l.o._.World()
     print(result)  # H.e.l.l.o._.World
 
-    builder = CallBuilder(sep='.')
+    builder = CallBuilder(sep=".")
     result = builder.H.e.l.l.o._.W.o.r.l.d()
     print(result)  # H.e.l.l.o._.W.o.r.l.d
--- earth_console_animations.py
+++ earth_console_animations.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # SOURCE: https://github.com/Dark-Princ3/X-tra-Telegram/blob/aa628465dddaf06383c84914df70c89a4d4fbf15/userbot/plugins/Earth.py#L13
 
+
 import time
+
 
 items = "🌏🌍🌎🌎🌍🌏🌍🌎"
 for i in range(48):
-    print('\r' + ' ' * 100 + '\r', end='')
-    print(items[i % len(items)], end='')
+    print("\r" + " " * 100 + "\r", end="")
+    print(items[i % len(items)], end="")
     time.sleep(0.1)
