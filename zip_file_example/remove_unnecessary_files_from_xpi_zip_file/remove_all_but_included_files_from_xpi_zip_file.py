ae29a8094f15905fe3e53eaf3802b0042d8d7772
gil9red <ilya.petrash@inbox.ru>
2023-06-25 17:46:53 +0500

    Refactoring. Using black code style

--- zip_file_example/remove_unnecessary_files_from_xpi_zip_file/remove_all_but_included_files_from_xpi_zip_file.py
+++ zip_file_example/remove_unnecessary_files_from_xpi_zip_file/remove_all_but_included_files_from_xpi_zip_file.py
@@ -1,47 +1,47 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
-"""Плагин удаляет из архива все файлы кроме указанных.
-xpi файл -- плагин для FireFox, является zip архивом."""
-
-
-import os.path
-import fnmatch
-from zipfile import ZipFile
-
-
-INCLUDE = ['data/*', 'index.js', 'bootstrap.js', 'package.json', 'install.rdf']
+"""
+Плагин удаляет из архива все файлы кроме указанных.
+xpi файл -- плагин для FireFox, является zip архивом.
+"""
 
 
 import argparse
+import fnmatch
+import os.path
+
+from zipfile import ZipFile
+
+
+INCLUDE = ["data/*", "index.js", "bootstrap.js", "package.json", "install.rdf"]
 
 
 def create_parser():
-    parser = argparse.ArgumentParser(description='Remove all but included files from zip.py.',
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('zip_file_name', type=str)
-    parser.add_argument('--include', nargs='*', default=INCLUDE)
-    parser.add_argument('--add_include', action='store_true')
+    parser = argparse.ArgumentParser(
+        description="Remove all but included files from zip.py.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("zip_file_name", type=str)
+    parser.add_argument("--include", nargs="*", default=INCLUDE)
+    parser.add_argument("--add_include", action="store_true")
 
     return parser.parse_args()
 
 
 def do(zip_file_name, include):
-    print('zip_file_name:', zip_file_name)
-    print('Include files:', include)
+    print("zip_file_name:", zip_file_name)
+    print("Include files:", include)
 
     # Измененный zip
-    out_zip_file_name = '_' + zip_file_name
+    out_zip_file_name = "_" + zip_file_name
 
-    try:
-        print('open {} and {} zip arhives'.format(zip_file_name, out_zip_file_name))
-        zin = ZipFile(zip_file_name, 'r')
-        zout = ZipFile(out_zip_file_name, 'w')
-
-        print('start fill {} zip arhive'.format(out_zip_file_name))
+    print(f"open {zip_file_name} and {out_zip_file_name} zip arhives")
+    with ZipFile(zip_file_name, "r") as zin, ZipFile(out_zip_file_name, "w") as zout:
+        print(f"start fill {out_zip_file_name} zip arhive")
 
         for item in zin.infolist():
             buffer = zin.read(item.filename)
@@ -49,24 +49,20 @@ def do(zip_file_name, include):
             if any((fnmatch.fnmatch(item.filename, pattern) for pattern in include)):
                 zout.writestr(item, buffer)
             else:
-                print('Delete', item.filename)
+                print("Delete", item.filename)
 
-        print('finish fill {} zip arhive'.format(out_zip_file_name))
+        print(f"finish fill {out_zip_file_name} zip arhive")
 
-    finally:
-        zout.close()
-        zin.close()
+    # Удаляем оригинальный
+    print(f"remove original {zip_file_name} zip file")
+    os.remove(zip_file_name)
 
-        # Удаляем оригинальный
-        print('remove original {} zip file'.format(zip_file_name))
-        os.remove(zip_file_name)
-
-        # Переименновываем измененный zip в оригинальный
-        print('rename {} zip file as original {}'.format(out_zip_file_name, zip_file_name))
-        os.rename(out_zip_file_name, zip_file_name)
+    # Переименовываем измененный zip в оригинальный
+    print(f"rename {out_zip_file_name} zip file as original {zip_file_name}")
+    os.rename(out_zip_file_name, zip_file_name)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = create_parser()
 
     include = set(INCLUDE + args.include if args.add_include else args.include)
--- zip_file_example/remove_unnecessary_files_from_xpi_zip_file/remove_exclude_files_from_xpi_zip_file.py
+++ zip_file_example/remove_unnecessary_files_from_xpi_zip_file/remove_exclude_files_from_xpi_zip_file.py
@@ -1,69 +1,65 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
-"""Плагин удаляет из архива указанные в exclude файлы.
-xpi файл -- плагин для FireFox, является zip архивом."""
-
-
-import os.path
-from zipfile import ZipFile
-
-
-EXCLUDE = ['README.md', 'run.bat', 'xpi.bat']
+"""
+Плагин удаляет из архива указанные в exclude файлы.
+xpi файл -- плагин для FireFox, является zip архивом.
+"""
 
 
 import argparse
+import os.path
+
+from zipfile import ZipFile
+
+
+EXCLUDE = ["README.md", "run.bat", "xpi.bat"]
 
 
 def create_parser():
-    parser = argparse.ArgumentParser(description='Remove unnecessary files from zip.',
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('zip_file_name', type=str)
-    parser.add_argument('--exclude', nargs='*', default=EXCLUDE)
-    parser.add_argument('--add_exclude', action='store_true')
+    parser = argparse.ArgumentParser(
+        description="Remove unnecessary files from zip.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("zip_file_name", type=str)
+    parser.add_argument("--exclude", nargs="*", default=EXCLUDE)
+    parser.add_argument("--add_exclude", action="store_true")
 
     return parser.parse_args()
 
 
 def do(zip_file_name, exclude):
-    print('zip_file_name:', zip_file_name)
+    print("zip_file_name:", zip_file_name)
 
-    print('Delete files:', EXCLUDE)
+    print("Delete files:", EXCLUDE)
 
     # Измененный zip
-    out_zip_file_name = '_' + zip_file_name
+    out_zip_file_name = "_" + zip_file_name
 
-    try:
-        print('open {} and {} zip arhives'.format(zip_file_name, out_zip_file_name))
-        zin = ZipFile(zip_file_name, 'r')
-        zout = ZipFile(out_zip_file_name, 'w')
-
-        print('start fill {} zip arhive'.format(out_zip_file_name))
+    print(f"open {zip_file_name} and {out_zip_file_name} zip arhives")
+    with ZipFile(zip_file_name, "r") as zin, ZipFile(out_zip_file_name, "w") as zout:
+        print(f"start fill {out_zip_file_name} zip arhive")
 
         for item in zin.infolist():
             buffer = zin.read(item.filename)
             if os.path.basename(item.filename) not in exclude:
                 zout.writestr(item, buffer)
 
-        print('finish fill {} zip arhive'.format(out_zip_file_name))
+        print(f"finish fill {out_zip_file_name} zip arhive")
 
-    finally:
-        zout.close()
-        zin.close()
+    # Удаляем оригинальный
+    print(f"remove original {zip_file_name} zip file")
+    os.remove(zip_file_name)
 
-        # Удаляем оригинальный
-        print('remove original {} zip file'.format(zip_file_name))
-        os.remove(zip_file_name)
-
-        # Переименновываем измененный zip в оригинальный
-        print('rename {} zip file as original {}'.format(out_zip_file_name, zip_file_name))
-        os.rename(out_zip_file_name, zip_file_name)
+    # Переименовываем измененный zip в оригинальный
+    print(f"rename {out_zip_file_name} zip file as original {zip_file_name}")
+    os.rename(out_zip_file_name, zip_file_name)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = create_parser()
 
     exclude = EXCLUDE + args.exclude if args.add_exclude else args.exclude
