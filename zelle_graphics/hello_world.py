d4d7725e6226882958dd21f7f985ac78d7c1394b
gil9red <ilya.petrash@inbox.ru>
2023-06-25 17:51:46 +0500

    Refactoring. Using black code style

--- zelle_graphics/hello_world.py
+++ zelle_graphics/hello_world.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 from graphics import *
+
+
 win = GraphWin("My Circle", 100, 100)
 c = Circle(Point(50, 50), 10)
 c.draw(win)
