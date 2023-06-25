ed28dbfb4d486ff97b353ef78ecda657fe008ead
gil9red <ilya.petrash@inbox.ru>
2023-06-25 17:52:28 +0500

    Refactoring. Using black code style

--- zeep_examples__WSDL/hello_world.py
+++ zeep_examples__WSDL/hello_world.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # SOURCE: https://github.com/mvantellingen/python-zeep
@@ -9,6 +9,7 @@ __author__ = 'ipetrash'
 
 # pip install zeep
 from zeep import Client
+
 
 # client = Client('http://www.webservicex.net/ConvertSpeed.asmx?WSDL')
 # result = client.service.ConvertSpeed(100, 'kilometersPerhour', 'milesPerhour')
