d6a36d352bfb20728cd9a63fa2ebec5822b7a4f5
gil9red <ilya.petrash@inbox.ru>
2023-06-25 17:47:31 +0500

    Refactoring. Using black code style

--- zip_file_example/shutil_make_archive/main.py
+++ zip_file_example/shutil_make_archive/main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import shutil
 
 
-DIR_NAME = 'dir_1'
-shutil.make_archive(DIR_NAME, 'zip', DIR_NAME)
+DIR_NAME = "dir_1"
+shutil.make_archive(DIR_NAME, "zip", DIR_NAME)
