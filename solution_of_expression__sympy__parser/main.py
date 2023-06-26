04260cbb294b99d2b457877fb46f4c9990e1ee1e
gil9red <ilya.petrash@inbox.ru>
2023-06-26 15:25:19 +0500

    Refactoring. Using black code style

--- solution_of_expression__sympy__parser/main.py
+++ solution_of_expression__sympy__parser/main.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
-from sympy.parsing.sympy_parser import parse_expr, standard_transformations, implicit_multiplication_application
+from sympy.parsing.sympy_parser import (
+    parse_expr,
+    standard_transformations,
+    implicit_multiplication_application,
+)
 from sympy import *
+
 
 input_str = "(x+1)(y+1)+(x+1)y+xy"
 transformations = standard_transformations + (implicit_multiplication_application,)
-print(expand(parse_expr(input_str, transformations=transformations)))  # 3*x*y + x + 2*y + 1
+print(
+    expand(parse_expr(input_str, transformations=transformations))
+)
+# 3*x*y + x + 2*y + 1
