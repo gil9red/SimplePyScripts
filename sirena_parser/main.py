333301926ce33fc748070bfe1ab6481cb2c4ccb4
gil9red <ilya.petrash@inbox.ru>
2023-06-26 14:50:45 +0500

    Refactoring. Using black code style

--- sirena_parser/main.py
+++ sirena_parser/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import re
@@ -13,11 +13,11 @@ class SirenaRecord:
         self.type = line[0]
 
         data = line[1:]
-        raw = re.split(r'\s+', data)
+        raw = re.split(r"\s+", data)
 
         for i, part in enumerate(raw):
             match self.type:
-                case '1' if i == 0:
+                case "1" if i == 0:
                     self.values.append(part[0:6])
                     self.values.append(part[6:10])
                     self.values.append(part[10:13])
@@ -25,22 +25,22 @@ class SirenaRecord:
                     self.values.append(part[17:27])
                     self.values.append(part[27:])
 
-                case '2' if i == 5:
+                case "2" if i == 5:
                     self.values.append(part[0:2])
                     self.values.append(part[2:])
 
-                case '4' if i == 0:
+                case "4" if i == 0:
                     self.values += part.split("/")
 
-                case '6' if i == 0:
+                case "6" if i == 0:
                     self.values.append(part[0:6])
                     self.values.append(part[6:])
 
-                case '7' if i == 0:
+                case "7" if i == 0:
                     self.values.append(part[0:6])
                     self.values.append(part[6:])
 
-                case '9' if i in [0, 1]:
+                case "9" if i in [0, 1]:
                     self.values += part.split("/")
 
                 case _:
@@ -55,13 +55,13 @@ class SirenaMessage:
         while queue_lines:
             line = queue_lines[0]
             type = line[0]
-            if type == 'Z' and self.lines:
+            if type == "Z" and self.lines:
                 break
 
             queue_lines.pop(0)
             self.lines.append(line)
 
-            if type in ['9', '1', 'Z']:
+            if type in ["9", "1", "Z"]:
                 break
 
         for l in self.lines:
@@ -76,7 +76,7 @@ class SirenaFile:
         self.queue_lines: list[str] = []
         self.messages: list[SirenaMessage] = []
 
-        with open(file_name, encoding='cp1251') as f:
+        with open(file_name, encoding="cp1251") as f:
             for line in f:
                 line = line.strip()
                 if not line:
@@ -86,23 +86,21 @@ class SirenaFile:
                 self.queue_lines.append(line)
 
         while self.queue_lines:
-            self.messages.append(
-                SirenaMessage(self.queue_lines)
-            )
+            self.messages.append(SirenaMessage(self.queue_lines))
 
 
-if __name__ == '__main__':
-    in_file_name = 'RET.TCH.STRA.S.20170510-20170510'
-    out_file_name = 'parse_RET.TCH.STRA.S.20170510-20170510'
+if __name__ == "__main__":
+    in_file_name = "RET.TCH.STRA.S.20170510-20170510"
+    out_file_name = "parse_RET.TCH.STRA.S.20170510-20170510"
 
     sirena_file = SirenaFile(in_file_name)
 
-    with open(out_file_name, 'w', encoding='cp1251', newline='\n') as f:
+    with open(out_file_name, "w", encoding="cp1251", newline="\n") as f:
         for message in sirena_file.messages:
             for record in message.records:
                 for value in record.values:
                     f.write(value + ";")
-                    print(value + ";", end='')
+                    print(value + ";", end="")
 
-                f.write('\n')
+                f.write("\n")
                 print()
