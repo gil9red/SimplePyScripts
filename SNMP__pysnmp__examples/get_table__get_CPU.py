d788174b971303ac8dd6e6f780a398ac7306105d
gil9red <ilya.petrash@inbox.ru>
2023-06-26 14:53:35 +0500

    Refactoring. Using black code style

--- SNMP__pysnmp__examples/get_table__get_CPU.py
+++ SNMP__pysnmp__examples/get_table__get_CPU.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # SOURCE: http://www.oidview.com/mibs/0/SNMPv2-MIB.html
@@ -10,11 +10,19 @@ __author__ = 'ipetrash'
 from typing import Iterator
 
 # pip install pysnmp
-from pysnmp.hlapi import nextCmd, SnmpEngine, CommunityData, UdpTransportTarget, ContextData, ObjectType, ObjectIdentity
+from pysnmp.hlapi import (
+    nextCmd,
+    SnmpEngine,
+    CommunityData,
+    UdpTransportTarget,
+    ContextData,
+    ObjectType,
+    ObjectIdentity,
+)
 
 
-def get_iterator(host: str, port: int = 161, community: str = '') -> Iterator:
-    hrProcessorLoad = '1.3.6.1.2.1.25.3.3.1.2'
+def get_iterator(host: str, port: int = 161, community: str = "") -> Iterator:
+    hrProcessorLoad = "1.3.6.1.2.1.25.3.3.1.2"
 
     return nextCmd(
         SnmpEngine(),
@@ -26,23 +34,23 @@ def get_iterator(host: str, port: int = 161, community: str = '') -> Iterator:
     )
 
 
-iterator = get_iterator('localhost', 161, 'public')
+iterator = get_iterator("localhost", 161, "public")
 items = list(iterator)
 print(f"Number CPU cores: {len(items)}\n")
 
 for i, (error_indication, error_status, error_index, var_binds) in enumerate(items, 1):
     if error_indication:
-        print(f'Error indication: {error_indication}')
+        print(f"Error indication: {error_indication}")
 
     elif error_status:
-        at = '?'
+        at = "?"
         if error_index:
             at = var_binds[int(error_index) - 1][0]
-        print(f'Error status {error_status.prettyPrint()!r} at {at}')
+        print(f"Error status {error_status.prettyPrint()!r} at {at}")
 
     else:
         for var, value in var_binds:
-            print(f'{var} = {value}')
-            print(f'{var.getOid()} = {value}')
-            print(f'{var.prettyPrint()} = {value.prettyPrint()}')
+            print(f"{var} = {value}")
+            print(f"{var.getOid()} = {value}")
+            print(f"{var.prettyPrint()} = {value.prettyPrint()}")
             print()
--- SNMP__pysnmp__examples/hello_world__fix_imports.py
+++ SNMP__pysnmp__examples/hello_world__fix_imports.py
@@ -1,40 +1,48 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # SOURCE: http://www.oidview.com/mibs/0/SNMPv2-MIB.html
 
 
 # pip install pysnmp
-from pysnmp.hlapi import getCmd, SnmpEngine, CommunityData, UdpTransportTarget, ContextData, ObjectType, ObjectIdentity
+from pysnmp.hlapi import (
+    getCmd,
+    SnmpEngine,
+    CommunityData,
+    UdpTransportTarget,
+    ContextData,
+    ObjectType,
+    ObjectIdentity,
+)
 
 
 iterator = getCmd(
     SnmpEngine(),
-    CommunityData('public', mpModel=0),
-    UdpTransportTarget(('localhost', 161)),
+    CommunityData("public", mpModel=0),
+    UdpTransportTarget(("localhost", 161)),
     ContextData(),
-    ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0)),
-    ObjectType(ObjectIdentity('1.3.6.1.2.1.1.1.0')),   # OID, same as above
-    ObjectType(ObjectIdentity('.1.3.6.1.2.1.1.1.0')),  # OID, same as above
+    ObjectType(ObjectIdentity("SNMPv2-MIB", "sysDescr", 0)),
+    ObjectType(ObjectIdentity("1.3.6.1.2.1.1.1.0")),  # OID, same as above
+    ObjectType(ObjectIdentity(".1.3.6.1.2.1.1.1.0")),  # OID, same as above
 )
 
 error_indication, error_status, error_index, var_binds = next(iterator)
 
 if error_indication:
-    print(f'Error indication: {error_indication}')
+    print(f"Error indication: {error_indication}")
 
 elif error_status:
-    at = '?'
+    at = "?"
     if error_index:
         at = var_binds[int(error_index) - 1][0]
-    print(f'Error status {error_status.prettyPrint()!r} at {at}')
+    print(f"Error status {error_status.prettyPrint()!r} at {at}")
 
 else:
     for var, value in var_binds:
-        print(f'{var} = {value}')
-        print(f'{var.getOid()} = {value}')
-        print(f'{var.prettyPrint()} = {value.prettyPrint()}')
+        print(f"{var} = {value}")
+        print(f"{var.getOid()} = {value}")
+        print(f"{var.prettyPrint()} = {value.prettyPrint()}")
         print()
