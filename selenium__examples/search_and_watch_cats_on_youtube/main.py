eef1755331b60c5f06cf0cd2f6d8952fb9a97687
gil9red <ilya.petrash@inbox.ru>
2023-06-26 12:12:05 +0500

    Refactoring. Using black code style

--- selenium__examples/search_and_watch_cats_on_youtube/main.py
+++ selenium__examples/search_and_watch_cats_on_youtube/main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
+
+import random
 
 # pip install selenium
 from selenium import webdriver
@@ -11,38 +13,40 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
+
 driver = webdriver.Firefox()
 driver.implicitly_wait(10)  # seconds
-driver.get('https://www.youtube.com/')
+driver.get("https://www.youtube.com/")
 print('Title: "{}"'.format(driver.title))
 
 # Делаем скриншот результата
-driver.save_screenshot('before_search.png')
+driver.save_screenshot("before_search.png")
 
-driver.find_element_by_css_selector('input#search').send_keys('Funny cats' + Keys.RETURN)
+driver.find_element_by_css_selector("input#search").send_keys(
+    "Funny cats" + Keys.RETURN
+)
 
-result_count = driver.find_element_by_id('result-count')
+result_count = driver.find_element_by_id("result-count")
 print(result_count.text)
 
 print('Title: "{}"'.format(driver.title))
 
 # Делаем скриншот результата
-driver.save_screenshot('after_search.png')
+driver.save_screenshot("after_search.png")
 
-video_list = driver.find_elements_by_id('dismissable')
+video_list = driver.find_elements_by_id("dismissable")
 
 # Click on random video
-import random
 random.choice(video_list).click()
 
 video = WebDriverWait(driver, timeout=10).until(
-    EC.visibility_of_element_located((By.TAG_NAME, 'video'))
+    EC.visibility_of_element_located((By.TAG_NAME, "video"))
 )
 
-video_title = driver.find_element_by_class_name('title')
+video_title = driver.find_element_by_class_name("title")
 print('Title: "{}"'.format(driver.title))
 print('Video Title: "{}"'.format(video_title.text))
 
-driver.save_screenshot('final.png')
+driver.save_screenshot("final.png")
 
 # driver.quit()
--- selenium__examples/send_message__vk.com/main.py
+++ selenium__examples/send_message__vk.com/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import time
@@ -14,45 +14,43 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-LOGIN = '<LOGIN>'
-PASSWORD = '<PASSWORD>'
+
+LOGIN = "<LOGIN>"
+PASSWORD = "<PASSWORD>"
+
 
 driver = webdriver.Firefox()
-driver.get('https://vk.com/')
+driver.get("https://vk.com/")
 print('Title: "{}"'.format(driver.title))
 
-driver.find_element_by_id('index_email').send_keys(LOGIN)
-driver.find_element_by_id('index_pass').send_keys(PASSWORD)
+driver.find_element_by_id("index_email").send_keys(LOGIN)
+driver.find_element_by_id("index_pass").send_keys(PASSWORD)
 
 # Делаем скриншот результата
-driver.save_screenshot('before_auth.png')
+driver.save_screenshot("before_auth.png")
 
 driver.find_element_by_id("index_login_button").click()
 
 wait = WebDriverWait(driver, timeout=10)
 
-l_msg = wait.until(
-    EC.presence_of_element_located((By.ID, 'l_msg'))
-)
+l_msg = wait.until(EC.presence_of_element_located((By.ID, "l_msg")))
 
 print('Title: "{}"'.format(driver.title))
 
 # Делаем скриншот результата
-driver.save_screenshot('after_auth.png')
+driver.save_screenshot("after_auth.png")
 
 l_msg.click()
 
-im_dialogs = wait.until(
-    EC.presence_of_element_located((By.ID, 'im_dialogs'))
-)
-im_dialogs.screenshot('dialogs_page.png')
+im_dialogs = wait.until(EC.presence_of_element_located((By.ID, "im_dialogs")))
+im_dialogs.screenshot("dialogs_page.png")
 print('Title: "{}"'.format(driver.title))
 
-dialog_items = driver.find_elements_by_class_name('_im_dialog_link')
+dialog_items = driver.find_elements_by_class_name("_im_dialog_link")
 
 for dialog in dialog_items:
-    if dialog.text == 'Майя Вернер':
-        print('Click dialog')
+    if dialog.text == "Майя Вернер":
+        print("Click dialog")
         while True:
             try:
                 dialog.click()
@@ -63,20 +61,18 @@ for dialog in dialog_items:
 
         break
 
-im_editable0 = wait.until(
-    EC.presence_of_element_located((By.ID, 'im_editable0'))
-)
+im_editable0 = wait.until(EC.presence_of_element_located((By.ID, "im_editable0")))
 
 print('Title: "{}"'.format(driver.title))
-driver.save_screenshot('current_dialog_page.png')
+driver.save_screenshot("current_dialog_page.png")
 
-im_editable0.send_keys('Привет!' + Keys.LEFT_CONTROL + Keys.RETURN)
+im_editable0.send_keys("Привет!" + Keys.LEFT_CONTROL + Keys.RETURN)
 # OR:
 # im_editable0.send_keys('Привет!' + Keys.RETURN)
 
 time.sleep(3)
 
 # Делаем скриншот результата
-driver.save_screenshot('last_page.png')
+driver.save_screenshot("last_page.png")
 
 driver.quit()
