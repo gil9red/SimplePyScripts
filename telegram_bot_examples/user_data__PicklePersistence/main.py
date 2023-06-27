6c71f0c48d110bc5c84bddd54ad34a923f3e3763
gil9red <ilya.petrash@inbox.ru>
2023-06-27 17:51:52 +0500

    Refactoring. Using black code style

--- telegram_bot_examples/user_data__PicklePersistence/main.py
+++ telegram_bot_examples/user_data__PicklePersistence/main.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import sys
 
 # pip install python-telegram-bot
 from telegram import Update
-from telegram.ext import MessageHandler, CommandHandler, Filters, CallbackContext, PicklePersistence
+from telegram.ext import (
+    MessageHandler,
+    CommandHandler,
+    Filters,
+    CallbackContext,
+    PicklePersistence,
+)
 
-sys.path.append('..')
+sys.path.append("..")
 from common import get_logger, log_func, reply_error, start_bot, run_main
 
 
@@ -18,10 +24,8 @@ log = get_logger(__file__)
 
 
 @log_func(log)
-def on_start(update: Update, context: CallbackContext):
-    update.effective_message.reply_text(
-        'Commands: set and get.'
-    )
+def on_start(update: Update, _: CallbackContext):
+    update.effective_message.reply_text("Commands: set and get.")
 
 
 @log_func(log)
@@ -31,45 +35,37 @@ def on_set(update: Update, context: CallbackContext):
     end = context.match.span()[1]
     text = message.text[end:].strip()
 
-    context.user_data['text'] = text
+    context.user_data["text"] = text
 
-    message.reply_text('Saving!')
+    message.reply_text("Saving!")
 
 
 @log_func(log)
 def on_get(update: Update, context: CallbackContext):
     message = update.effective_message
 
-    text = context.user_data.get('text', '<Not data, using command set>')
+    text = context.user_data.get("text", "<Not data, using command set>")
 
-    message.reply_text(
-        "get: " + text
-    )
+    message.reply_text("get: " + text)
 
 
 @log_func(log)
-def on_request(update: Update, context: CallbackContext):
+def on_request(update: Update, _: CallbackContext):
     message = update.effective_message
 
-    message.reply_text(
-        'Echo: ' + message.text
-    )
+    message.reply_text("Echo: " + message.text)
 
 
 def main():
     handlers = [
-        CommandHandler('start', on_start),
-        MessageHandler(Filters.regex(r'(?i)^(set)\b'), on_set),
-        MessageHandler(Filters.regex(r'(?i)^get'), on_get),
+        CommandHandler("start", on_start),
+        MessageHandler(Filters.regex(r"(?i)^(set)\b"), on_set),
+        MessageHandler(Filters.regex(r"(?i)^get"), on_get),
         MessageHandler(Filters.text, on_request),
     ]
 
-    start_bot(
-        log,
-        handlers,
-        persistence=PicklePersistence(filename='data.pickle')
-    )
+    start_bot(log, handlers, persistence=PicklePersistence(filename="data.pickle"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_main(main, log)
