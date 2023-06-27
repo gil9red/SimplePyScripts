b6f82ccef9266d1d4c70341fbd084ce6ed3673d6
gil9red <ilya.petrash@inbox.ru>
2023-06-27 15:55:13 +0500

    Refactoring. Using black code style

--- string_formatting/string_formatting.py
+++ string_formatting/string_formatting.py
@@ -1,9 +1,9 @@
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # TODO: побольше примеров форматирования строк.
     # https://docs.python.org/3/library/string.html#string-formatting
     # http://pythonworld.ru/osnovy/formatirovanie-strok-metod-format.html
     # http://pythonworld.ru/osnovy/formatirovanie-strok-operator.html
-    print('{one} * {one} {two}'.format(one="45", two="Bugaga"))
+    print("{one} * {one} {two}".format(one="45", two="Bugaga"))
