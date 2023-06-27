7f0add7eea4f5946af30089c794ff3729c7af3eb
gil9red <ilya.petrash@inbox.ru>
2023-06-27 15:48:02 +0500

    Refactoring. Using black code style

--- Start a file with its associated application/main.py
+++ Start a file with its associated application/main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import os
-os.startfile('result.txt')
-os.startfile('img.png')
-os.startfile('cert.cer')
+
+
+os.startfile("result.txt")
+os.startfile("img.png")
+os.startfile("cert.cer")
