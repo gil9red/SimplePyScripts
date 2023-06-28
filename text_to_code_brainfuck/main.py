f66d62db936f21a9182ed79885412eebbac4e573
gil9red <ilya.petrash@inbox.ru>
2023-06-28 13:54:05 +0500

    Refactoring. Using black code style

--- text_to_code_brainfuck/main.py
+++ text_to_code_brainfuck/main.py
@@ -1,109 +1,49 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
+
+
+import base64
+import zlib
+
+import simple_brainfuck
 
 
 def text_to_code_brainfuck(text):
     commands_brainfuck = []
 
     for c in text:
-        commands_brainfuck.append('+' * ord(c) + '.')
+        commands_brainfuck.append("+" * ord(c) + ".")
 
-    return '>'.join(commands_brainfuck)
+    return ">".join(commands_brainfuck)
 
 
-if __name__ == '__main__':
-    text = 'Hello World!'
-#     text = """\
-# #include <iostream>
-# #include <fstream>
-# #include <vector>
-#
-# using namespace std;
-#
-# static char cpu[30000];
-#
-# int main(int argc, char **argv) {
-#     vector<char> acc;
-#     char ch;
-#     ifstream infile(argv[1]);
-#     while (infile) {
-#         infile.get(ch);
-#         acc.push_back(ch);
-#     }
-#     infile.close();
-#     unsigned int j = 0;
-#     int brc = 0;
-#     for (int i = 0; i < acc.size(); ++i) {
-#         if (acc[i] == '>')
-#             j++;
-#         if (acc[i] == '<')
-#             j--;
-#         if (acc[i] == '+')
-#             cpu[j]++;
-#         if (acc[i] == '-')
-#             cpu[j]--;
-#         if (acc[i] == '.')
-#             cout << cpu[j];
-#         if (acc[i] == ',')
-#             cin >> cpu[j];
-#         if (acc[i] == '[') {
-#             if (!cpu[j]) {
-#                 ++brc;
-#                 while (brc) {
-#                     ++i;
-#                     if (acc[i] == '[')
-#                         ++brc;
-#                     if (acc[i] == ']')
-#                         --brc;
-#                 }
-#             } else
-#                 continue;
-#         } else if (acc[i] == ']') {
-#             if (!cpu[j])
-#                 continue;
-#             else {
-#                 if (acc[i] == ']')
-#                     brc++;
-#                 while (brc) {
-#                     --i;
-#                     if (acc[i] == '[')
-#                         brc--;
-#                     if (acc[i] == ']')
-#                         brc++;
-#                 }
-#                 --i;
-#             }
-#         }
-#     }
-# }
-#     """
+if __name__ == "__main__":
+    text = "Hello World!"
 
     code_brainfuck = text_to_code_brainfuck(text)
-    print('code_brainfuck:', len(code_brainfuck))
+    print("code_brainfuck:", len(code_brainfuck))
     # print(code_brainfuck)
     print()
 
     # Test generated brainfuck code
-    import simple_brainfuck
     result = simple_brainfuck.execute(code_brainfuck)
-    print('result:', len(result))
+    print("result:", len(result))
     print(result)
     assert result == text
 
     # TODO: Compress variant
-    import zlib
-    code_brainfuck_compress = zlib.compress(code_brainfuck.encode('utf-8'))
+    code_brainfuck_compress = zlib.compress(code_brainfuck.encode("utf-8"))
     print()
-    print('code_brainfuck_compress:', len(code_brainfuck_compress))
+    print("code_brainfuck_compress:", len(code_brainfuck_compress))
     print(code_brainfuck_compress)
 
-    import base64
-    code_brainfuck_compress_base64 = base64.b64encode(code_brainfuck_compress).decode('utf-8')
+    data = base64.b64encode(code_brainfuck_compress)
+    code_brainfuck_compress_base64 = data.decode("utf-8")
     print()
-    print('code_brainfuck_compress_base64:', len(code_brainfuck_compress_base64))
+    print("code_brainfuck_compress_base64:", len(code_brainfuck_compress_base64))
     print(code_brainfuck_compress_base64)
 
-    with open('code.bf', mode='w', encoding='utf-8') as f:
+    with open("code.bf", mode="w", encoding="utf-8") as f:
         f.write(code_brainfuck)
--- text_to_code_brainfuck/simple_brainfuck.py
+++ text_to_code_brainfuck/simple_brainfuck.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # SOURCE: https://github.com/gil9red/SimplePyScripts/blob/master/simple_brainfuck.py
+
 
 def get_loops_block(source):
     begin_block = []
     blocks = dict()
 
     for i, s in enumerate(source):
-        if s is '[':
+        if s is "[":
             begin_block.append(i)
 
-        elif s is ']':
+        elif s is "]":
             b_i = begin_block.pop()  # b_i -- begin index
             blocks[i] = b_i
             blocks[b_i] = i
@@ -38,7 +39,10 @@ def execute(source, silent=True):
     x = 0  # Cell index
 
     from collections import defaultdict
-    bf = defaultdict(int)  # Dictionary, which is stored in the key index of the cell, and in the value - its value
+
+    bf = defaultdict(
+        int
+    )  # Dictionary, which is stored in the key index of the cell, and in the value - its value
     l = len(source)  # Number of code symbols
     loops_block = get_loops_block(source)
 
@@ -47,56 +51,60 @@ def execute(source, silent=True):
     while i < l:
         s = source[i]
 
-        if s is '>':  # Go to the next cell
+        if s is ">":  # Go to the next cell
             x += 1
 
-        elif s is '<':  # Go to the previous cell
+        elif s is "<":  # Go to the previous cell
             x -= 1
 
-        elif s is '+':  # Increasing the value of the current cell on 1
+        elif s is "+":  # Increasing the value of the current cell on 1
             bf[x] += 1
 
-        elif s is '-':  # Decrease the value of the current cell on 1
+        elif s is "-":  # Decrease the value of the current cell on 1
             bf[x] -= 1
 
-        elif s is '.':  # Printing the value of the current cell
+        elif s is ".":  # Printing the value of the current cell
             value = chr(bf[x])
             result_list.append(value)
 
             if not silent:
-                print(value, end='')
+                print(value, end="")
 
-        elif s is ',':  # Enter a value in the current cell
+        elif s is ",":  # Enter a value in the current cell
             bf[x] = int(input("Input = "))
 
-        elif s is '[':  # Begin loop
-            if not bf[x]:  # If bf[x] == 0, then gets the index of the closing parenthesis
+        elif s is "[":  # Begin loop
+            if not bf[
+                x
+            ]:  # If bf[x] == 0, then gets the index of the closing parenthesis
                 i = loops_block[i]
 
-        elif s is ']':  # End loop
+        elif s is "]":  # End loop
             if bf[x]:  # Если bf[x] != 0, then gets the index of the opening parenthesis
                 i = loops_block[i]
 
         i += 1
 
-    return ''.join(result_list)
+    return "".join(result_list)
 
 
-if __name__ == '__main__':
-    text = '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.>+++++' \
-           '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++' \
-           '+++++++++++++++++.>++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++' \
-           '++++++++++++++++++++++++++++++++++++++++++++++++.>+++++++++++++++++++++++++++++' \
-           '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++' \
-           '.>+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++' \
-           '++++++++++++++++++++++++++++++++++.>++++++++++++++++++++++++++++++++.>+++++++++' \
-           '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.' \
-           '>++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++' \
-           '+++++++++++++++++++++++++++++++++.>++++++++++++++++++++++++++++++++++++++++++++' \
-           '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.>+++++++' \
-           '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++' \
-           '++++++++++++++++++++++.>+++++++++++++++++++++++++++++++++++++++++++++++++++++++' \
-           '+++++++++++++++++++++++++++++++++++++++++++++.>+++++++++++++++++++++++++++++++++.'
+if __name__ == "__main__":
+    text = (
+        "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.>+++++"
+        "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+        "+++++++++++++++++.>++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+        "++++++++++++++++++++++++++++++++++++++++++++++++.>+++++++++++++++++++++++++++++"
+        "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+        ".>+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+        "++++++++++++++++++++++++++++++++++.>++++++++++++++++++++++++++++++++.>+++++++++"
+        "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++."
+        ">++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+        "+++++++++++++++++++++++++++++++++.>++++++++++++++++++++++++++++++++++++++++++++"
+        "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.>+++++++"
+        "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+        "++++++++++++++++++++++.>+++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+        "+++++++++++++++++++++++++++++++++++++++++++++.>+++++++++++++++++++++++++++++++++."
+    )
     result = execute(text)
     print("result: " + repr(result))
 
