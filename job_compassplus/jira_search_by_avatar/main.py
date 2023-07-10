25d63f7d0812dfd09b91eb023272b2ad0363ca33
gil9red <ilya.petrash@inbox.ru>
2023-07-10 13:22:34 +0500

    Added "job_compassplus/jira_search_by_avatar/main.py"

---
+++ job_compassplus/jira_search_by_avatar/main.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+import sys
+import time
+from pathlib import Path
+
+DIR = Path(__file__).resolve().parent
+ROOT_DIR = DIR.parent
+
+sys.path.append(str(ROOT_DIR))
+from root_common import session
+
+
+URL_PROFILE_FORMAT = "https://helpdesk.compassluxe.com/secure/ViewProfile.jspa?name={name}"
+
+
+def search_by_avatar(avatar_id: int) -> list[dict]:
+    url = "https://helpdesk.compassluxe.com/rest/api/latest/user/search"
+
+    start_at = 0
+    max_results = 500
+
+    avatar_id = str(avatar_id)
+
+    items = []
+    while True:
+        params = {
+            "username": ".",
+            "startAt": start_at,
+            "maxResults": max_results,
+        }
+
+        rs = session.get(url, params=params)
+        rs.raise_for_status()
+
+        if not rs.json():
+            break
+
+        for user in rs.json():
+            if avatar_id in user["avatarUrls"]["48x48"]:
+                items.append(user)
+
+        time.sleep(5)
+
+        start_at += max_results
+
+    return items
+
+
+if __name__ == '__main__':
+    avatar_id = 17250  # pirate
+    users = search_by_avatar(avatar_id)
+    print(f"Users ({len(users)}):")
+    for i, user in enumerate(users, 1):
+        display_name = user["displayName"]
+        user_url = URL_PROFILE_FORMAT.format(name=user["key"])
+        print(f"{i}. '{display_name}': {user_url}")
