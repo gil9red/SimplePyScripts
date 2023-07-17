8820b199f08f48f7f979d8c366ca91783ba109cb
gil9red <ilya.petrash@inbox.ru>
2023-07-17 21:38:36 +0500

    Added "grouple_co/get_updates_from_api.py"

---
+++ html_parsing/grouple_co/get_updates_from_api.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+from common import session, load
+
+
+def get_feeds_by_manga_chapters() -> list[str]:
+    # Auth and load
+    load("https://grouple.co/private/bookmarks/index#")
+
+    data = {
+        "bookmarkSort": "NAME",
+        "elementFilter": [],
+        "statusFilter": ["ANOTHER_UPDATES"],
+        "includeUpdates": True,
+        "limit": 50,
+        "offset": 0
+    }
+    session.headers["Authorization"] = f'Bearer {session.cookies["gwt"]}'
+
+    rs = session.post("https://grouple.co/api/bookmark/activitiesList", json=data)
+    rs.raise_for_status()
+
+    return [
+        f'{item["element"]["name"]} {item["title"]}'
+        for item in rs.json()["list"]
+    ]
+
+
+if __name__ == "__main__":
+    items = get_feeds_by_manga_chapters()
+    print(f"Items ({len(items)}):", items)
