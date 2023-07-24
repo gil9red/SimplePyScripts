2339786eb0092c71e53f36b54ca6ac1067591004
gil9red <ilya.petrash@inbox.ru>
2023-07-24 15:14:02 +0500

    Added "socket__tcp__examples/send_and_receive_at_the_same_time/*"

---
+++ socket__tcp__examples/send_and_receive_at_the_same_time/client.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+import socket
+import sys
+import time
+
+from threading import Thread
+
+sys.path.append("..")
+from common import send_msg, recv_msg
+
+
+HOST = socket.gethostname()
+PORT = 9090
+
+sock = socket.socket()
+sock.connect((HOST, PORT))
+
+
+def read_from():
+    while True:
+        response_data = recv_msg(sock)
+        print(f"Response ({len(response_data)}): {response_data}")
+
+
+Thread(target=read_from).start()
+
+
+stan = 0
+while True:
+    stan += 1
+    if stan <= 20:
+        data = f"#{stan} Hello World!".encode('utf-8')
+        print(f"Sending: {data}")
+
+        send_msg(sock, data)
+
+    time.sleep(0.1)
---
+++ socket__tcp__examples/send_and_receive_at_the_same_time/server.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+import random
+import socket
+import time
+from threading import Thread
+
+import sys
+sys.path.append("..")
+from common import send_msg, recv_msg
+
+
+PORT = 9090
+
+
+rqs: list[bytes] = []
+
+
+def write_to():
+    while True:
+        if rqs:
+            idx = random.randrange(len(rqs))
+            data = rqs.pop().upper()
+            print(f"Sending #{idx}: {data}")
+            send_msg(conn, data)
+
+        time.sleep(0.3)
+
+
+Thread(target=write_to).start()
+
+
+while True:
+    with socket.socket() as sock:
+        sock.bind(("", PORT))
+        sock.listen()
+
+        print(f"Server: {sock.getsockname()}")
+
+        while True:
+            conn, addr = sock.accept()
+            print("Connected:", addr)
+
+            while True:
+                try:
+                    data = recv_msg(conn)
+                    if not data:
+                        break
+
+                    print(f"Receiving ({len(data)}): {data}")
+                    rqs.append(data)
+
+                except ConnectionResetError:
+                    break
+
+            print("Close\n")
