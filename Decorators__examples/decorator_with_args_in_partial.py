0656eccd95289dc8f1ad6b71fdf0dd5a26663bca
gil9red <ilya.petrash@inbox.ru>
2023-08-03 14:22:55 +0500

    Added "Decorators__examples/decorator_with_args_in_partial.py"

---
+++ Decorators__examples/decorator_with_args_in_partial.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+from functools import partial
+from combine_decorators__with_args import custom_tag
+
+
+@custom_tag(
+    name="a",
+    href="https://example.com",
+    title="Hint!",
+)
+def hello(text):
+    return text
+
+
+custom_tag_a = partial(
+    custom_tag,
+    name="a",
+    href="https://example.com",
+    title="Hint!",
+)
+
+@custom_tag_a()
+def hello2(text):
+    return text
+
+
+@custom_tag_a(
+    title="Other hint!",
+)
+def hello3(text):
+    return text
+
+
+print(hello("Foo"))
+# <a href="https://example.com" title="Hint!">Foo</a>
+
+print(hello2("Foo"))
+# <a href="https://example.com" title="Hint!">Foo</a>
+
+print(hello3("Foo"))
+# <a href="https://example.com" title="Other hint!">Foo</a>
