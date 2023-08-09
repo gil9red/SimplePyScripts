d9691eaecf1278846af306a4bcf3582ed30b76be
gil9red <ilya.petrash@inbox.ru>
2023-08-09 19:10:31 +0500

    Updated. Added characters and characters_number fields

--- html_parsing/ranobehub_org/get_ranobe_info.py
+++ html_parsing/ranobehub_org/get_ranobe_info.py
@@ -4,11 +4,29 @@
 __author__ = "ipetrash"
 
 
+import re
 from dataclasses import dataclass, field
 
 from bs4 import BeautifulSoup
 
 from common import session
+
+
+def parse_characters(text: str) -> tuple[str, int]:
+    title = ""
+    number = 0
+
+    if m := re.search(r"(\d+)([KM]?)\b", text.upper()):
+        title = m.group()
+        number, unit = m.groups()
+        number = int(number)
+        match unit:
+            case "K":
+                number *= 1_000
+            case "M":
+                number *= 1_000_000
+
+    return title, number
 
 
 @dataclass
@@ -17,6 +35,8 @@ class Ranobe:
     release_year: int
     country: str
     status: str
+    characters: str
+    characters_number: int = field(repr=False)
     genres: list[str] = field(default_factory=list, repr=False)
     tags: list[str] = field(default_factory=list, repr=False)
 
@@ -32,9 +52,14 @@ def get_ranobe_info(url: str) -> Ranobe:
     release_year = 0
     country = ""
     status = ""
+    characters_text = ""
+    characters_number = 0
+
     for row in soup.select(".book-meta-row"):
         key = row.select_one(".book-meta-key").get_text(strip=True)
-        value = row.select_one(".book-meta-value").get_text(strip=True)
+
+        value_el = row.select_one(".book-meta-value")
+        value = value_el.get_text(strip=True)
 
         match key:
             case "Год выпуска":
@@ -43,6 +68,9 @@ def get_ranobe_info(url: str) -> Ranobe:
                 country = value
             case "Статус перевода":
                 status = value
+            case "Главы":
+                chapters_info = value_el.select_one("i[data-tippy-content]")["data-tippy-content"]
+                characters_text, characters_number = parse_characters(chapters_info)
 
     genres = [
         el.get_text(strip=True)
@@ -61,6 +89,8 @@ def get_ranobe_info(url: str) -> Ranobe:
         release_year=release_year,
         country=country,
         status=status,
+        characters=characters_text,
+        characters_number=characters_number,
         genres=genres,
         tags=tags,
     )
@@ -70,7 +100,10 @@ if __name__ == "__main__":
     url = "https://ranobehub.org/ranobe/72-god-and-devil-world"
     ranobe = get_ranobe_info(url)
     print(ranobe)
-    # Ranobe(title='Мир Бога и Дьявола', release_year=2013, country='Китай', status='Завершено')
+    # Ranobe(title='Мир Бога и Дьявола', release_year=2013, country='Китай', status='Завершено', characters='11M')
+
+    print(f"Characters: {ranobe.characters}, number: {ranobe.characters_number}")
+    # Characters: 11M, number: 11000000
 
     print(f"Genres: ({len(ranobe.genres)}): {ranobe.genres}")
     # Genres: (13): ['Боевые искусства', 'Гарем', 'Для взрослых', ..., 'Фэнтези', 'Экшн', 'Эччи']
