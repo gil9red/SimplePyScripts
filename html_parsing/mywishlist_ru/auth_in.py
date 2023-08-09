614fa803e9f611626ab2f37b10a734805856679a
gil9red <ilya.petrash@inbox.ru>
2023-08-10 03:28:07 +0500

    Refactoring

--- html_parsing/mywishlist_ru/auth_in.py
+++ html_parsing/mywishlist_ru/auth_in.py
@@ -4,14 +4,8 @@
 __author__ = "ipetrash"
 
 
-import requests
 from bs4 import BeautifulSoup
-
-
-session = requests.session()
-session.headers[
-    "User-Agent"
-] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0"
+from common import session
 
 
 URL_LOGIN = "http://mywishlist.ru/login/login"
