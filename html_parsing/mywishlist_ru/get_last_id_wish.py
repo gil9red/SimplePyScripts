539677315917a3ccff3a57e8b1d044b959edf08c
gil9red <ilya.petrash@inbox.ru>
2023-08-10 04:03:15 +0500

    Added "html_parsing/mywishlist_ru/get_last_id_wish.py"

---
+++ html_parsing/mywishlist_ru/get_last_id_wish.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+from common import BASE_URL, do_get
+
+
+def get_last_id_wish() -> int:
+    _, soup = do_get(BASE_URL)
+    wish_url = soup.select_one(".pWishList .pWishLite a[href]")["href"]
+    return int(wish_url.split("/")[-1])
+
+
+if __name__ == "__main__":
+    last_id = get_last_id_wish()
+    print(last_id)
+    # 10320355
+
+    from get_wish_info import Wish
+    print(Wish.parse_from(last_id))
+    # Wish(id=10320355, user='Wicked Red Fish', title='PREDUBEZHDAI butter body', created_at='2023-08-08 23:50', img_url='/pic/i/wish/300x300/010/320/355.jpeg')
