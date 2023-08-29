5f494849343ca972ede4f7eea2964ce8382b240a
gil9red <ilya.petrash@inbox.ru>
2023-08-29 13:37:46 +0500

    Updated UI. Moved HTML to file. Added new chart

--- https_гибдд_рф__crash_statistics/web.py
+++ https_гибдд_рф__crash_statistics/web.py
@@ -6,7 +6,7 @@ __author__ = "ipetrash"
 
 import datetime as dt
 
-from flask import Flask, render_template_string
+from flask import Flask, render_template
 from common import get_crash_statistics_list_db
 
 
@@ -19,11 +19,14 @@ def index():
     rows = get_crash_statistics_list_db()
     rows.reverse()
 
-    data = []
-    for date, dtp, died, children_died, wounded, wounded_children in rows:
+    data: list[dict] = []
+    year_by_number: dict[int, int] = dict()
+    for date_str, dtp, died, children_died, wounded, wounded_children in rows:
+        date = dt.datetime.strptime(date_str, "%d.%m.%Y").date()
+
         data.append({
-            "date": date,
-            "date_iso": dt.datetime.strptime(date, "%d.%m.%Y").date().isoformat(),
+            "date": date_str,
+            "date_iso": date.isoformat(),
             "dtp": int(dtp),
             "died": int(died),
             "children_died": int(children_died),
@@ -31,157 +34,16 @@ def index():
             "wounded_children": int(wounded_children),
         })
 
-    return render_template_string(
-        """
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta content='text/html; charset=UTF-8' http-equiv='Content-Type'/>
-    <title>{{ title }}</title>
+        if date.year not in year_by_number:
+            year_by_number[date.year] = 0
+        year_by_number[date.year] += 1
 
-    <link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='bootstrap-4.3.1/bootstrap.min.css') }}">
-
-    <script src="{{ url_for('static', filename='js/jquery-3.1.1.min.js') }}"></script>
-    <script src="{{ url_for('static', filename='bootstrap-4.3.1/bootstrap.min.js') }}"></script>
-    <script src="{{ url_for('static', filename='chart_js_2.9.3/Chart.bundle.min.js') }}"></script>
-    <style>
-        .table-wrap {
-            height: 90vh;
-            overflow-y: auto;
-        }
-    </style>
-</head>
-<body>
-<div class="container-fluid">
-    <h2 class="text-center"><a href="https://гибдд.рф/">{{ title }}</a></h2>
-
-    <div class="row">
-        <div class="col-5 table-wrap">
-            <table class="table table-hover">
-                <thead class="thead-dark">
-                    <tr>
-                    {% for header in headers %}
-                        <th>{{ header }}</th>
-                    {% endfor %}
-                    </tr>
-                </thead>
-                <tbody></tbody>
-            </table>
-        </div>
-        <div class="col-7">
-            <canvas id="lineChart"></canvas>
-        </div>
-    </div>
-</div>
-<script>
-    let data = {{ data | safe }};
-
-    $(document).ready(function() {
-        let labels = [];
-        let data_dtp = [];
-        let data_died = [];
-        let data_children_died = [];
-        let data_wounded = [];
-        let data_wounded_children = [];
-    
-        let table = $('table > tbody');
-        
-        for (let x of data) {
-            let tr = $('<tr>')
-                .append($(`<td>${x.date}</td>`))
-                .append($(`<td>${x.dtp}</td>`))
-                .append($(`<td>${x.died}</td>`))
-                .append($(`<td>${x.children_died}</td>`))
-                .append($(`<td>${x.wounded}</td>`))
-                .append($(`<td>${x.wounded_children}</td>`))
-            ;
-            table.append(tr);
-            
-            labels.push(x.date_iso);
-            data_dtp.push({
-                x: x.date_iso,
-                y: x.dtp,
-            });
-            data_died.push({
-                x: x.date_iso,
-                y: x.died,
-            });
-            data_children_died.push({
-                x: x.date_iso,
-                y: x.children_died,
-            });
-            data_wounded.push({
-                x: x.date_iso,
-                y: x.wounded,
-            });
-            data_wounded_children.push({
-                x: x.date_iso,
-                y: x.wounded_children,
-            });
-        }
-    
-        var ctx = document.getElementById("lineChart").getContext("2d");
-        var lineChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: labels,
-                datasets: [
-                    {
-                        label: 'ДТП',
-                        lineTension: 0,
-                        borderColor: "red",
-                        data: data_dtp,
-                    },
-                    {
-                        label: 'Погибли',
-                        lineTension: 0,
-                        borderColor: "rgb(246, 139, 31)",
-                        data: data_died,
-                    },
-                    {
-                        label: 'Погибло детей',
-                        lineTension: 0,
-                        borderColor: "rgb(68, 44, 110)",
-                        data: data_children_died,
-                    },
-                    {
-                        label: 'Ранены',
-                        lineTension: 0,
-                        borderColor: "blue",
-                        data: data_wounded,
-                    },
-                    {
-                        label: 'Ранено детей',
-                        lineTension: 0,
-                        borderColor: "green",
-                        data: data_wounded_children,
-                    },
-                ],
-            },
-            options: {
-                scales: {
-                    xAxes: [{
-                        type: 'time',
-                        time: {
-                            unit: 'month',
-                            tooltipFormat: 'DD/MM/YYYY',
-                            displayFormats: {
-                               day: 'DD/MM/YY'
-                            }
-                        },
-                        distribution: 'linear'
-                    }]
-                }
-            }
-        });
-    });
-</script>
-</body>
-</html>
-    """,
+    return render_template(
+        "index.html",
         title="АВАРИЙНОСТЬ НА ДОРОГАХ РОССИИ",
         headers=headers,
         data=data,
+        year_by_number=year_by_number,
     )
 
 
