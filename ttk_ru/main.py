725adb9ad97f33f6ba1962103b4b7e6af1aa1a01
gil9red <ilya.petrash@inbox.ru>
2023-09-06 23:57:47 +0500

    Bug fix.

--- ttk_ru/main.py
+++ ttk_ru/main.py
@@ -8,6 +8,7 @@ import time
 
 # pip install selenium
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support import expected_conditions as EC
@@ -16,7 +17,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from config import LOGIN, PASSWORD
 
 
-URL_LOGIN = "https://lk.ttk.ru/po/login.jsf#/"
+URL = "https://lk.ttk.ru/services"
 
 
 def get_price() -> str:
@@ -27,43 +28,54 @@ def get_price() -> str:
     wait = WebDriverWait(driver, timeout=30)
 
     try:
-        driver.get(URL_LOGIN)
+        driver.get(URL)
         print(f'Title: "{driver.title}", url: {driver.current_url}')
 
-        username_el = wait.until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="username"]'))
-        )
-        username_el.send_keys(LOGIN)
+        if "/auth" in driver.current_url:
+            print("Нужно авторизоваться")
 
-        password_el = wait.until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="password"]'))
-        )
-        password_el.send_keys(PASSWORD)
+            username_el = wait.until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="username"]'))
+            )
+            username_el.send_keys(LOGIN)
 
-        while "login" in driver.current_url:
-            driver.find_element(By.ID, "submit").click()
-            time.sleep(1)
+            # Кнопка "Продолжить"
+            driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
+
+            # На текущий момент поле пароля показывается после клика на кнопку после ввода логина
+            password_el = wait.until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="password"]'))
+            )
+            password_el.send_keys(PASSWORD)
+
+            # Кнопка "Войти"
+            driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
+
+            while "/auth" in driver.current_url:
+                time.sleep(1)
+
+            # Попытаемся пройти дальше
+            driver.get(URL)
 
         print(f'Title: "{driver.title}", url: {driver.current_url}')
 
-        css_path = '[services="services.internet"][account="selectedAccount"] [ng-show*="tariffPrice"]'
-        price_internet_el = wait.until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, css_path))
-        )
+        attempts = 30
+        while "/services" not in driver.current_url:
+            if attempts == 0:
+                raise Exception("Не удалось открыть страницу /services")
 
-        # Waiting for the price to be shown
-        attempts = 60
-        price_str = None
-        for _ in range(attempts):
-            # Scrolling to an element so it generates text
-            driver.execute_script("arguments[0].scrollIntoView();", price_internet_el)
-
-            price_str = "".join(c for c in price_internet_el.text if c.isdigit())
-            if price_str:
-                break
-
+            attempts -= 1
             time.sleep(1)
 
+        # Попробуем найти тег, у которого будет тег с текстом "Стоимость"
+        try:
+            div_wrapper_price_el = wait.until(
+                EC.presence_of_element_located((By.XPATH, '//*[*[text()="Стоимость"]]'))
+            )
+        except TimeoutException:
+            raise Exception(f"Не удалось стоимость! Текущая страница: {driver.current_url}")
+
+        price_str = "".join(c for c in div_wrapper_price_el.text if c.isdigit())
         if not price_str:
             raise Exception("Не удалось получить цену!")
 
