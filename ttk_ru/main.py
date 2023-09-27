8e3e44e8ee20ef13b1d4586abd35d6f2f6e73eab
gil9red <ilya.petrash@inbox.ru>
2023-09-28 00:43:48 +0500

    Bug fix. Supported both versions of site

--- ttk_ru/main.py
+++ ttk_ru/main.py
@@ -17,10 +17,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from config import LOGIN, PASSWORD
 
 
-URL = "https://lk.ttk.ru/services"
+URL_V1_LOGIN = "https://lk.ttk.ru/po/login.jsf#/"
+URL_V2 = "https://lk.ttk.ru/services"
 
 
-def get_price() -> str:
+def get_price_v1() -> str:
     options = Options()
     options.add_argument("--headless")
 
@@ -28,7 +29,61 @@ def get_price() -> str:
     wait = WebDriverWait(driver, timeout=30)
 
     try:
-        driver.get(URL)
+        driver.get(URL_V1_LOGIN)
+        print(f'Title: "{driver.title}", url: {driver.current_url}')
+
+        username_el = wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="username"]'))
+        )
+        username_el.send_keys(LOGIN)
+
+        password_el = wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="password"]'))
+        )
+        password_el.send_keys(PASSWORD)
+
+        while "login" in driver.current_url:
+            driver.find_element(By.ID, "submit").click()
+            time.sleep(1)
+
+        print(f'Title: "{driver.title}", url: {driver.current_url}')
+
+        css_path = '[services="services.internet"][account="selectedAccount"] [ng-show*="tariffPrice"]'
+        price_internet_el = wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, css_path))
+        )
+
+        # Waiting for the price to be shown
+        attempts = 60
+        price_str = None
+        for _ in range(attempts):
+            # Scrolling to an element so it generates text
+            driver.execute_script("arguments[0].scrollIntoView();", price_internet_el)
+
+            price_str = "".join(c for c in price_internet_el.text if c.isdigit())
+            if price_str:
+                break
+
+            time.sleep(1)
+
+        if not price_str:
+            raise Exception("Не удалось получить цену!")
+
+        return price_str
+
+    finally:
+        driver.quit()
+
+
+def get_price_v2() -> str:
+    options = Options()
+    options.add_argument("--headless")
+
+    driver = webdriver.Firefox(options=options)
+    wait = WebDriverWait(driver, timeout=30)
+
+    try:
+        driver.get(URL_V2)
         print(f'Title: "{driver.title}", url: {driver.current_url}')
 
         if "/auth" in driver.current_url:
@@ -60,7 +115,7 @@ def get_price() -> str:
                 time.sleep(1)
 
             # Попытаемся пройти дальше
-            driver.get(URL)
+            driver.get(URL_V2)
 
         print(f'Title: "{driver.title}", url: {driver.current_url}')
 
@@ -88,6 +143,14 @@ def get_price() -> str:
 
     finally:
         driver.quit()
+
+
+def get_price() -> str:
+    try:
+        return get_price_v2()
+    except Exception:
+        print("Не удалось получить цену из новой версии сайта, пробую на старой")
+        return get_price_v1()
 
 
 if __name__ == "__main__":
