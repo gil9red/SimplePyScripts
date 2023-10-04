822325110e7150e8423113869288d0504d1579c7
gil9red <ilya.petrash@inbox.ru>
2023-10-04 14:00:15 +0500

    Added "job_compassplus/mysite_compassplus_com/get_profile_image_from_web.py"

---
+++ job_compassplus/mysite_compassplus_com/get_profile_image_from_web.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+from base64 import b64encode
+
+# pip install flask==2.3.3
+from flask import Flask, Response, abort
+
+# pip install flask-caching=2.0.2
+from flask_caching import Cache
+
+# pip install flask-cors==4.0.0
+from flask_cors import CORS
+
+from requests.exceptions import RequestException
+
+from get_profile_image import get_profile_image
+
+
+config = {
+    "DEBUG": True,  # some Flask specific configs
+    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300,
+}
+
+app = Flask(__name__)
+
+# tell Flask to use the above defined config
+app.config.from_mapping(config)
+
+cache = Cache(app)
+CORS(app)
+
+
+@app.route("/api/get_profile_image/<username>")
+@cache.cached()
+def api_get_profile_image(username: str):
+    try:
+        img_data: bytes = get_profile_image(username)
+        if not img_data:
+            abort(404)
+
+        img_base64 = b64encode(img_data).decode("utf-8")
+        return f"data:image/jpg;base64, {img_base64}"
+
+    except RequestException as e:
+        return Response(
+            response=str(e),
+            status=e.response.status_code,
+        )
+
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=50000,
+        ssl_context="adhoc",
+    )
