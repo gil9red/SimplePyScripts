49fe6d67a4b158146f48f29faa7c12d902f268d0
gil9red <ilya.petrash@inbox.ru>
2023-10-05 01:21:41 +0500

    Updated.

--- html_parsing/ranobehub_org/get_notes.py
+++ html_parsing/ranobehub_org/get_notes.py
@@ -4,8 +4,8 @@
 __author__ = "ipetrash"
 
 
+import re
 from bs4 import BeautifulSoup
-
 from common import session
 
 
@@ -23,7 +23,7 @@ def get_notes(url: str) -> list[str]:
     #         <a href="#190cite_ref-34548627613">↑</a> Автор тут допустил небольшую ошибку. Циклотетраметилентетранитрамин ещё называют октогеном, но не СL-20. СL-20 будет «гексанитрогексаазаизовюрцитан», он более эффективен и стоит 1300 долларов за килограмм, октоген же — 100 долларов за килограмм.
     #     </li>
     # </ol>
-    for li_el in soup.select("ol.references > li"):
+    for li_el in soup.find_all("li", id=re.compile(".*cite_note.*")):
         li_el.a.decompose()
         items.append(
             li_el.get_text(strip=True)
@@ -44,3 +44,10 @@ if __name__ == "__main__":
     'В предыдущих томах её фамилия была Баранс.'
     'Канопус: звезда южного полушария, вторая по яркости после Сириуса.'
     """
+    print()
+
+    url = "https://ranobehub.org/ranobe/19/21/2"
+    notes = get_notes(url)
+    print(f"Notes ({len(notes)}):")
+    for note in notes:
+        print(repr(note))
