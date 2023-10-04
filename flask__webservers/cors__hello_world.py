7f5bfc9a7d745eec19b2e0dfcd3b69464cc6f934
gil9red <ilya.petrash@inbox.ru>
2023-10-04 12:27:31 +0500

    Added "flask__webservers/cors__hello_world.py"

---
+++ flask__webservers/cors__hello_world.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+# pip install flask==2.3.3
+from flask import Flask
+
+# pip install flask-cors==4.0.0
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.debug = True
+
+    # Localhost
+    # port=0 -- random free port
+    # app.run(port=0)
+    app.run(port=50000)
+
+    # # Public IP
+    # app.run(host='0.0.0.0')
