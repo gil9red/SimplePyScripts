2b33d01614d50cbd1535077bd14b254c0e293a38
gil9red <ilya.petrash@inbox.ru>
2023-10-09 05:47:09 +0500

    Added "html_parsing/howlongtobeat_com/download_reaction_images.py"

---
+++ html_parsing/ranobehub_org/download_reaction_images.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+from pathlib import Path
+
+from common import session
+from get_reactions import get_reactions_raw
+
+
+PATH = Path(__file__)
+DIR = PATH.parent.resolve()
+
+DIR_IMAGES = DIR / f"{PATH.name}_images"
+DIR_IMAGES.mkdir(exist_ok=True, parents=True)
+
+
+def download_reaction_images(url: str, dir_path: Path = DIR_IMAGES):
+    for x in get_reactions_raw(url)["reactions"]:
+        img_url = x["reaction"]["image"]
+
+        rs = session.get(img_url)
+        rs.raise_for_status()
+
+        name = x["reaction"]["name"]
+        img_suffix = Path(img_url).suffix
+
+        img_path = dir_path / f"{name}{img_suffix}"
+        img_path.write_bytes(rs.content)
+
+
+if __name__ == "__main__":
+    download_reaction_images("https://ranobehub.org/ranobe/72/1/1")
