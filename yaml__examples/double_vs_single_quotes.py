220716e11329f047101a3b645057886af3cab901
gil9red <ilya.petrash@inbox.ru>
2023-10-31 11:35:07 +0500

    Added "yaml__examples/double_vs_single_quotes.py"

---
+++ yaml__examples/double_vs_single_quotes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+# pip install PyYAML
+import yaml
+
+
+data = yaml.safe_load(r"""
+double: "123\n456"
+single: '123\n456'
+""")
+
+import json
+print(json.dumps(data, indent=4))
+r"""
+{
+    "double": "123\n456",
+    "single": "123\\n456"
+}
+"""
