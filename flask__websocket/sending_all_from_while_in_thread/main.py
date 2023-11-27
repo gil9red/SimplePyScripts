14633ce41caf44127ea34cfba60f02a34b32bc00
gil9red <ilya.petrash@inbox.ru>
2023-11-27 15:02:46 +0500

    Added "flask__websocket/sending_all_from_while_in_thread/"

---
+++ flask__websocket/sending_all_from_while_in_thread/main.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+import time
+
+from datetime import datetime
+from threading import Thread
+
+from flask import Flask, render_template, session, request
+
+# pip install flask-socketio==5.3.6
+from flask_socketio import SocketIO, emit
+
+
+# Set this variable to "threading", "eventlet" or "gevent" to test the
+# different async modes, or leave it set to None for the application to choose
+# the best option based on installed packages.
+async_mode = None
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret!"
+socketio = SocketIO(app, async_mode=async_mode)
+
+
+def send_all_cycled():
+    with app.app_context():
+        i = 0
+        while True:
+            i += 1
+            emit(
+                "my_response_all",
+                {"data": f"#{i}. {datetime.now().isoformat()}"},
+                broadcast=True,  # Send all clients
+                namespace="/",
+            )
+            time.sleep(1)
+
+
+thread = Thread(target=send_all_cycled)
+thread.daemon = True
+thread.start()
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@socketio.on("my_event")
+def test_message(message):
+    session["receive_count"] = session.get("receive_count", 0) + 1
+    print(message)
+
+    response = message["data"]
+    emit(
+        "my_response",
+        {"data": response, "count": session["receive_count"], "sid": request.sid},
+        broadcast=True,  # Send all clients
+    )
+
+
+@socketio.on("my_ping")
+def ping_pong():
+    emit("my_pong")
+
+
+@socketio.on("connect")
+def test_connect():
+    print("Client connected", request.sid)
+    emit("my_response", {"data": f"Connected!", "count": 0, "sid": request.sid})
+
+
+@socketio.on("disconnect")
+def test_disconnect():
+    print("Client disconnected", request.sid)
+
+
+if __name__ == "__main__":
+    # HOST = '127.0.0.1'
+    # HOST = "0.0.0.0"
+    PORT = 12000
+    # print(f"http://{HOST}:{PORT}")
+
+    socketio.run(
+        app,
+        # host=HOST,
+        port=PORT,
+        allow_unsafe_werkzeug=True,
+    )
---
+++ flask__websocket/sending_all_from_while_in_thread/static/index.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+    // Connect to the Socket.IO server.
+    var socket = io();
+
+    // Event handler for new connections.
+    // The callback function is invoked when a connection with the
+    // server is established.
+    socket.on('connect', function() {
+        socket.emit('my_event', {data: "I'm connected!"});
+    });
+
+    // Event handler for server sent data.
+    // The callback function is invoked whenever the server emits data
+    // to the client. The data is then displayed in the "Received"
+    // section of the page.
+    socket.on('my_response', function(msg, cb) {
+        console.log(msg);
+        $('#log').prepend(
+            '<br>' + $('<div/>').text(`Received #${msg.count}: ${msg.data} from #${msg.sid}`).html()
+        );
+        if (cb)
+            cb();
+    });
+
+    socket.on('my_response_all', function(msg, cb) {
+        console.log(msg);
+        $('#server_data').text(msg.data);
+        if (cb)
+            cb();
+    });
+
+    // Interval function that tests message latency by sending a "ping"
+    // message. The server then responds with a "pong" message and the
+    // round trip time is measured.
+    var ping_pong_times = [];
+    var start_time;
+    window.setInterval(function() {
+        start_time = (new Date).getTime();
+        socket.emit('my_ping');
+    }, 1000);
+
+    // Handler for the "pong" message. When the pong is received, the
+    // time from the ping is stored, and the average of the last 30
+    // samples is average and displayed.
+    socket.on('my_pong', function() {
+        var latency = (new Date).getTime() - start_time;
+        ping_pong_times.push(latency);
+        ping_pong_times = ping_pong_times.slice(-30); // keep last 30 samples
+        var sum = 0;
+        for (var i = 0; i < ping_pong_times.length; i++)
+            sum += ping_pong_times[i];
+        $('#ping-pong').text(Math.round(10 * sum / ping_pong_times.length) / 10);
+    });
+
+    // Handlers for the different forms in the page.
+    // These accept data from the user and send it to the server in a
+    // variety of ways
+    $('form#emit').submit(function(event) {
+        socket.emit('my_event', {data: $('#emit_data').val()});
+        return false;
+    });
+});
