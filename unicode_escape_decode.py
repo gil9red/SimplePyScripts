b2cf0d26580ddcb25139addcadce40e51d3c5179
gil9red <ilya.petrash@inbox.ru>
2023-12-11 14:10:59 +0500

    Bugfix

--- unicode_escape_decode.py
+++ unicode_escape_decode.py
@@ -7,7 +7,8 @@ __author__ = "ipetrash"
 import codecs
 
 
-text = "\u041e\u0442\u0441\u0443\u0442\u0441\u0442\u0432\u0443\u0435\u0442 \u043e\u0431\u044f\u0437\u0430\u0442\u0435\u043b\u044c\u043d\u044b\u0439 \u043f\u0430\u0440\u0430\u043c\u0435\u0442\u0440 (username)"
+text = r"\u041e\u0442\u0441\u0443\u0442\u0441\u0442\u0432\u0443\u0435\u0442 \u043e\u0431\u044f\u0437\u0430\u0442\u0435\u043b\u044c\u043d\u044b\u0439 \u043f\u0430\u0440\u0430\u043c\u0435\u0442\u0440 (username)"
 
-print(codecs.unicode_internal_decode(text)[0])
+data = text.encode("utf-8")
+print(codecs.decode(data, "unicode-escape"))
 # Отсутствует обязательный параметр (username)
