fca77c73551539a3dbaad52e81cb5d5105149791
gil9red <ilya.petrash@inbox.ru>
2024-01-25 16:58:14 +0500

    Updated.

--- run_processes_with_output_in_web/common.py
+++ run_processes_with_output_in_web/common.py
@@ -20,6 +20,7 @@ class TaskStatusEnum(enum.Enum):
     Pending = enum.auto()
     Running = enum.auto()
     Finished = enum.auto()
+    Stopped = enum.auto()
 
 
 @dataclass
@@ -32,6 +33,7 @@ class Task:
     status: TaskStatusEnum = TaskStatusEnum.Pending
     process: Popen = field(init=False, repr=False, default=None)
     process_return_code: int = None
+    __need_stop: bool = field(init=False, repr=False, default=False)
 
     @property
     def process_id(self) -> int | None:
@@ -44,8 +46,11 @@ class Task:
             self.process = process
 
         def on_exit():
-            self.status = TaskStatusEnum.Finished
+            self.status = TaskStatusEnum.Stopped if self.__need_stop else TaskStatusEnum.Finished
             self.process_return_code = self.process.returncode
+
+        def stop_on() -> bool:
+            return self.__need_stop
 
         runner = command_runner_threaded if threaded else command_runner
         runner(
@@ -56,7 +61,11 @@ class Task:
             stderr=self.stderr,
             process_callback=process_callback,
             on_exit=on_exit,
+            stop_on=stop_on,
         )
+
+    def stop(self):
+        self.__need_stop = True
 
 
 if __name__ == "__main__":
@@ -112,3 +121,17 @@ if __name__ == "__main__":
 
         if task_threaded.status == TaskStatusEnum.Finished and not stdout_line and not stderr_line:
             break
+
+    print()
+
+    import threading
+    threading.Timer(2, lambda: task_max_2_secs.stop()).start()
+    task_max_2_secs = Task(
+        command="ping 127.0.0.1",
+        stdout=lambda text: process_stdout(f"[{task.command}]: {text}"),
+        stderr=lambda text: process_stderr(f"[{task.command}]: {text}"),
+        encoding="cp866",
+    )
+    print(task_max_2_secs)
+    task_max_2_secs.run(threaded=False)
+    print(task_max_2_secs)
