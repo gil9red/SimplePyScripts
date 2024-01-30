c92708fe4c56a3e7bc91f6fe71ea68e0f262dbc2
gil9red <ilya.petrash@inbox.ru>
2024-01-30 13:56:02 +0500

    Updated.

--- shorten.py
+++ shorten.py
@@ -4,19 +4,19 @@
 __author__ = "ipetrash"
 
 
-def shorten(text: str, length=30) -> str:
+def shorten(text: str, length: int = 30, placeholder: str = "...") -> str:
     if not text:
         return text
 
     if len(text) > length:
-        text = text[:length] + "..."
+        text = text[:length - len(placeholder)] + placeholder
     return text
 
 
 if __name__ == "__main__":
     text = "1234" * 20
     new_text = shorten(text)
-    assert new_text == "123412341234123412341234123412..."
+    assert new_text == "123412341234123412341234123..."
 
     text = "12345"
     new_text = shorten(text, length=5)
@@ -26,6 +26,10 @@ if __name__ == "__main__":
     new_text = shorten(text, length=5)
     assert new_text == "123"
 
+    text = "1234567890"
+    new_text = shorten(text, length=7)
+    assert new_text == "1234..."
+
     text = "12356"
     new_text = shorten(text, length=3)
-    assert new_text == "123..."
+    assert new_text == "..."
