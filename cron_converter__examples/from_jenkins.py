b93dda2899e1f4ce42dcbdb1fc4aa534a3cf9833
gil9red <ilya.petrash@inbox.ru>
2024-02-12 15:41:06 +0500

    Added "cron_converter__examples/"

---
+++ cron_converter__examples/from_jenkins.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+import re
+
+
+def do_convert(cron: str) -> str:
+    match cron:
+        case "@hourly":
+            cron = "H * * * *"
+
+        case "@daily":
+            cron = "H H * * *"
+
+        case "@midnight":
+            cron = "H 0 * * *"
+
+        case "@weekly":
+            cron = "H H * * H"
+
+        case "@monthly":
+            cron = "H H H * *"
+
+        case "@yearly" | "@annually":
+            cron = "0 0 1 1 *"
+
+    # NOTE: "H(0-29)/10 * * * *" -> "0-29/10 * * * *"
+    cron = re.sub(r"H\((.+?)\)", r"\1", cron)
+
+    parts: list[str] = cron.split()
+
+    def _process(value: str, default_value: str = "0") -> str:
+        return value.replace("H/", "*/").replace("H", default_value)
+
+    # Minute
+    parts[0] = _process(parts[0])
+
+    # Hour
+    parts[1] = _process(parts[1])
+
+    # Day (month). Тут диапазон начинается с 1
+    parts[2] = _process(parts[2], default_value="1")
+
+    # Month. Тут диапазон начинается с 1
+    parts[3] = _process(parts[3], default_value="1")
+
+    # Day (week)
+    parts[4] = _process(parts[4])
+
+    cron = " ".join(parts)
+
+    return cron
+
+
+if __name__ == "__main__":
+    from datetime import datetime
+
+    # pip install cron-converter
+    from cron_converter import Cron
+
+    cron = "H */8 * * *"
+    cron = do_convert(cron)
+    cron_instance = Cron(cron)
+
+    print(f"Cron: {cron_instance}")
+
+    start_date = datetime.now()
+    print(f"Start date: {start_date}")
+
+    schedule = cron_instance.schedule(start_date)
+    print(f"Next: {schedule.next().isoformat()}")
+    print(f"Next: {schedule.next().isoformat()}")
+    print(f"Next: {schedule.next().isoformat()}")
+    print(f"Next: {schedule.next().isoformat()}")
+    print(f"Next: {schedule.next().isoformat()}")
---
+++ cron_converter__examples/test_from_jenkins.py
@@ -0,0 +1,400 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+from datetime import datetime
+from unittest import TestCase
+
+from cron_converter import Cron
+
+from from_jenkins import do_convert
+
+
+class Test(TestCase):
+    def test_do_convert_every_15_minutes(self):
+        # Every fifteen minutes
+        cron = "H/15 * * * *"
+
+        cron = do_convert(cron)
+
+        cron_instance = Cron(cron)
+
+        start_date_str = "2024-01-01T12:00:00"
+        schedule = cron_instance.schedule(datetime.fromisoformat(start_date_str))
+
+        actual: list[str] = [
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+        ]
+        expected = ["2024-01-01T12:00:00", "2024-01-01T12:15:00", "2024-01-01T12:30:00"]
+
+        self.assertEqual(actual, expected)
+
+    def test_do_convert_every_1_hours(self):
+        cron = "H * * * *"
+
+        cron = do_convert(cron)
+
+        cron_instance = Cron(cron)
+
+        start_date_str = "2024-01-01T12:00:00"
+        schedule = cron_instance.schedule(datetime.fromisoformat(start_date_str))
+
+        actual: list[str] = [
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+        ]
+        expected = ["2024-01-01T12:00:00", "2024-01-01T13:00:00", "2024-01-01T14:00:00"]
+
+        self.assertEqual(actual, expected)
+
+    def test_do_convert_every_8_hours(self):
+        cron = "H */8 * * *"
+
+        cron = do_convert(cron)
+
+        cron_instance = Cron(cron)
+
+        start_date_str = "2024-01-01T12:00:00"
+        schedule = cron_instance.schedule(datetime.fromisoformat(start_date_str))
+
+        actual: list[str] = [
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+        ]
+        expected = [
+            "2024-01-01T16:00:00",
+            "2024-01-02T00:00:00",
+            "2024-01-02T08:00:00",
+            "2024-01-02T16:00:00",
+            "2024-01-03T00:00:00",
+        ]
+
+        self.assertEqual(actual, expected)
+
+    def test_do_convert_every_24_hours(self):
+        cron = "H 0 * * *"
+
+        cron = do_convert(cron)
+
+        cron_instance = Cron(cron)
+
+        start_date_str = "2024-01-01T12:00:00"
+        schedule = cron_instance.schedule(datetime.fromisoformat(start_date_str))
+
+        actual: list[str] = [
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+        ]
+        expected = ["2024-01-02T00:00:00", "2024-01-03T00:00:00", "2024-01-04T00:00:00"]
+
+        self.assertEqual(actual, expected)
+
+    def test_do_convert_hourly(self):
+        cron = "@hourly"
+
+        cron = do_convert(cron)
+
+        cron_instance = Cron(cron)
+
+        start_date_str = "2024-01-01T12:00:00"
+        schedule = cron_instance.schedule(datetime.fromisoformat(start_date_str))
+
+        actual: list[str] = [
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+        ]
+        expected = ["2024-01-01T12:00:00", "2024-01-01T13:00:00", "2024-01-01T14:00:00"]
+
+        self.assertEqual(actual, expected)
+
+    def test_do_convert_daily(self):
+        cron = "@daily"
+
+        cron = do_convert(cron)
+
+        cron_instance = Cron(cron)
+
+        start_date_str = "2024-01-01T12:00:00"
+        schedule = cron_instance.schedule(datetime.fromisoformat(start_date_str))
+
+        actual: list[str] = [
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+        ]
+        expected = ["2024-01-02T00:00:00", "2024-01-03T00:00:00", "2024-01-04T00:00:00"]
+
+        self.assertEqual(actual, expected)
+
+    def test_do_convert_midnight(self):
+        cron = "@midnight"
+
+        cron = do_convert(cron)
+
+        cron_instance = Cron(cron)
+
+        start_date_str = "2024-01-01T12:00:00"
+        schedule = cron_instance.schedule(datetime.fromisoformat(start_date_str))
+
+        actual: list[str] = [
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+        ]
+        expected = [
+            "2024-01-02T00:00:00",
+            "2024-01-03T00:00:00",
+            "2024-01-04T00:00:00",
+            "2024-01-05T00:00:00",
+            "2024-01-06T00:00:00",
+            "2024-01-07T00:00:00",
+        ]
+
+        self.assertEqual(actual, expected)
+
+    def test_do_convert_weekly(self):
+        cron = "@weekly"
+
+        cron = do_convert(cron)
+
+        cron_instance = Cron(cron)
+
+        start_date_str = "2024-01-01T12:00:00"
+        schedule = cron_instance.schedule(datetime.fromisoformat(start_date_str))
+
+        actual: list[str] = [
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+        ]
+        expected = [
+            "2024-01-07T00:00:00",
+            "2024-01-14T00:00:00",
+            "2024-01-21T00:00:00",
+            "2024-01-28T00:00:00",
+            "2024-02-04T00:00:00",
+            "2024-02-11T00:00:00",
+        ]
+
+        self.assertEqual(actual, expected)
+
+    def test_do_convert_monthly(self):
+        cron = "@monthly"
+
+        cron = do_convert(cron)
+
+        cron_instance = Cron(cron)
+
+        start_date_str = "2024-01-01T12:00:00"
+        schedule = cron_instance.schedule(datetime.fromisoformat(start_date_str))
+
+        actual: list[str] = [
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+        ]
+        expected = [
+            "2024-02-01T00:00:00",
+            "2024-03-01T00:00:00",
+            "2024-04-01T00:00:00",
+            "2024-05-01T00:00:00",
+            "2024-06-01T00:00:00",
+            "2024-07-01T00:00:00",
+        ]
+
+        self.assertEqual(actual, expected)
+
+    def test_do_convert_yearly(self):
+        cron = "@yearly"
+
+        cron = do_convert(cron)
+
+        cron_instance = Cron(cron)
+
+        start_date_str = "2024-01-01T12:00:00"
+        schedule = cron_instance.schedule(datetime.fromisoformat(start_date_str))
+
+        actual: list[str] = [
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+        ]
+        expected = ["2025-01-01T00:00:00", "2026-01-01T00:00:00", "2027-01-01T00:00:00"]
+
+        self.assertEqual(actual, expected)
+
+    def test_do_convert_annually(self):
+        cron = "@annually"
+
+        cron = do_convert(cron)
+
+        cron_instance = Cron(cron)
+
+        start_date_str = "2024-01-01T12:00:00"
+        schedule = cron_instance.schedule(datetime.fromisoformat(start_date_str))
+
+        actual: list[str] = [
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+        ]
+        expected = ["2025-01-01T00:00:00", "2026-01-01T00:00:00", "2027-01-01T00:00:00"]
+
+        self.assertEqual(actual, expected)
+
+    def test_do_convert_complex_1(self):
+        # Every ten minutes in the first half of every hour
+        cron = "H(0-29)/10 * * * *"
+
+        cron = do_convert(cron)
+
+        cron_instance = Cron(cron)
+
+        start_date_str = "2024-01-01T12:00:00"
+        schedule = cron_instance.schedule(datetime.fromisoformat(start_date_str))
+
+        actual: list[str] = [
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+        ]
+        expected = [
+            "2024-01-01T12:00:00",
+            "2024-01-01T12:10:00",
+            "2024-01-01T12:20:00",
+            "2024-01-01T13:00:00",
+            "2024-01-01T13:10:00",
+            "2024-01-01T13:20:00",
+            "2024-01-01T14:00:00",
+            "2024-01-01T14:10:00",
+            "2024-01-01T14:20:00",
+        ]
+
+        self.assertEqual(actual, expected)
+
+    def test_do_convert_complex_2(self):
+        # Once every two hours at 45 minutes past the hour starting at 9:45 AM and finishing at 3:45 PM every weekday
+        cron = "45 9-16/2 * * 1-5"
+
+        cron = do_convert(cron)
+
+        cron_instance = Cron(cron)
+
+        start_date_str = "2024-01-04T12:00:00"
+        schedule = cron_instance.schedule(datetime.fromisoformat(start_date_str))
+
+        actual: list[str] = [
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+        ]
+        expected = [
+            "2024-01-04T13:45:00",
+            "2024-01-04T15:45:00",
+            "2024-01-05T09:45:00",
+            "2024-01-05T11:45:00",
+            "2024-01-05T13:45:00",
+            "2024-01-05T15:45:00",
+            "2024-01-08T09:45:00",
+            "2024-01-08T11:45:00",
+        ]
+
+        self.assertEqual(actual, expected)
+
+    def test_do_convert_complex_3(self):
+        # Once in every two hour slot between 8 AM and 4 PM every weekday
+        cron = "H H(8-15)/2 * * 1-5"
+
+        cron = do_convert(cron)
+
+        cron_instance = Cron(cron)
+
+        start_date_str = "2024-01-04T12:00:00"
+        schedule = cron_instance.schedule(datetime.fromisoformat(start_date_str))
+
+        actual: list[str] = [
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+        ]
+        expected = [
+            "2024-01-04T12:00:00",
+            "2024-01-04T14:00:00",
+            "2024-01-05T08:00:00",
+            "2024-01-05T10:00:00",
+            "2024-01-05T12:00:00",
+            "2024-01-05T14:00:00",
+            "2024-01-08T08:00:00",
+            "2024-01-08T10:00:00",
+        ]
+
+        self.assertEqual(actual, expected)
+
+    def test_do_convert_complex_4(self):
+        # Once a day on the 1st and 15th of every month except December
+        cron = "H H 1,15 1-11 *"
+
+        cron = do_convert(cron)
+
+        cron_instance = Cron(cron)
+
+        start_date_str = "2023-10-01T12:00:00"
+        schedule = cron_instance.schedule(datetime.fromisoformat(start_date_str))
+
+        actual: list[str] = [
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+            schedule.next().isoformat(),
+        ]
+        expected = [
+            "2023-10-15T00:00:00",
+            "2023-11-01T00:00:00",
+            "2023-11-15T00:00:00",
+            "2024-01-01T00:00:00",
+            "2024-01-15T00:00:00",
+            "2024-02-01T00:00:00",
+            "2024-02-15T00:00:00",
+            "2024-03-01T00:00:00",
+        ]
+
+        self.assertEqual(actual, expected)
