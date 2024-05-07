c03d84870cbee26345e469a31c3675051b5c5eaf
gil9red <ilya.petrash@inbox.ru>
2024-05-07 18:01:28 +0500

    Updated.

--- update_task_and_jobs_from_gist/root_common.py
+++ update_task_and_jobs_from_gist/root_common.py
@@ -16,6 +16,9 @@ def get_gist_file(gist_url: str, file_name: str) -> str:
     soup = BeautifulSoup(rs.content, "html.parser")
 
     a_all = soup.find("a", {"href": re.compile(rf"/raw/.+/{re.escape(file_name)}")})
+    if not a_all:
+        raise Exception(f"Не удалось найти файл {file_name!r} в {gist_url!r}")
+
     url_all = urljoin(rs.url, a_all["href"])
 
     rs = requests.get(url_all)
