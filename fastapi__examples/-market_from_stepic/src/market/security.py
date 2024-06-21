f107c6ceb59f4772f7253bb63d10be5e61a2379b
gil9red <ilya.petrash@inbox.ru>
2024-06-21 15:14:54 +0500

    Refactoring

--- fastapi__examples/-market_from_stepic/src/market/security.py
+++ fastapi__examples/-market_from_stepic/src/market/security.py
@@ -4,15 +4,10 @@
 __author__ = "ipetrash"
 
 
-# TODO:
-# from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-# TODO:
-# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 def verify_password(plain_password, hashed_password):
