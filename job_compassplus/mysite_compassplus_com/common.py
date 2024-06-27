2e6e19aeca5ad82280d891c17a42fd3f974c9870
gil9red <ilya.petrash@inbox.ru>
2024-06-27 18:09:52 +0500

    Refactoring

--- job_compassplus/mysite_compassplus_com/common.py
+++ job_compassplus/mysite_compassplus_com/common.py
@@ -5,52 +5,18 @@ __author__ = "ipetrash"
 
 
 import sys
-import time
-
 from pathlib import Path
-
-# pip install requests_ntlm2
-from requests_ntlm2 import HttpNtlmAuth
-
-from requests import Response
-from requests.exceptions import RequestException
 
 DIR = Path(__file__).resolve().parent
 ROOT_DIR = DIR.parent
 
 sys.path.append(str(ROOT_DIR))
-from root_common import session
-
-from config_token import USERNAME, PASSWORD
+from site_common import do_get
 
 
 URL = "https://mysite.compassplus.com/Person.aspx?accountname={}"
 
 
-def do_get(url: str, *args, **kwargs) -> Response:
-    attempts = 0
-    max_attempts = 5
-
-    while True:
-        attempts += 1
-        try:
-            rs = session.get(
-                url,
-                auth=HttpNtlmAuth(USERNAME, PASSWORD),
-                *args,
-                **kwargs
-            )
-
-            # Через какое-то отваливается доступ, почистим куки и заново авторизуемся
-            if rs.status_code == 401:
-                session.cookies.clear()
-
-            rs.raise_for_status()
-
-            return rs
-
-        except RequestException as e:
-            if attempts >= max_attempts:
-                raise e
-
-            time.sleep(5)  # 5 seconds
+if __name__ == "__main__":
+    rs = do_get("https://mysite.compassplus.com:443/Person.aspx?accountname=CP%5Cipetrash")
+    print(rs)
---
+++ job_compassplus/site_common.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+import time
+
+# pip install requests_ntlm2
+from requests_ntlm2 import HttpNtlmAuth
+
+from requests import Response
+from requests.exceptions import RequestException
+
+from root_common import session
+from site_config_token import USERNAME, PASSWORD
+
+
+def do_get(url: str, *args, **kwargs) -> Response:
+    attempts = 0
+    max_attempts = 5
+
+    while True:
+        attempts += 1
+        try:
+            rs = session.get(
+                url,
+                auth=HttpNtlmAuth(USERNAME, PASSWORD),
+                *args,
+                **kwargs
+            )
+
+            # Через какое-то отваливается доступ, почистим куки и заново авторизуемся
+            if rs.status_code == 401:
+                session.cookies.clear()
+
+            rs.raise_for_status()
+
+            return rs
+
+        except RequestException as e:
+            if attempts >= max_attempts:
+                raise e
+
+            time.sleep(5)  # 5 seconds
