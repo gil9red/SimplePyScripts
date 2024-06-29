c9a7b0efee02d64c2ce68dd1316689c91e0fabd6
gil9red <ilya.petrash@inbox.ru>
2024-06-30 02:53:47 +0500

    Updated.

--- flask__webservers/logging__remove_date_from_werkzeug_logs/utils.py
+++ flask__webservers/logging__remove_date_from_werkzeug_logs/utils.py
@@ -10,7 +10,9 @@ import re
 
 # NOTE: Fix https://github.com/pallets/werkzeug/blob/72b2e48e7d44927b1b7d6b2f940d0691230de893/src/werkzeug/serving.py#L425C38-L425C44
 class FilterRemoveDateFromWerkzeugLogs(logging.Filter):
+    # '192.168.0.102 - - [30/Jun/2024 01:14:03] "%s" %s %s' -> '192.168.0.102 - "%s" %s %s'
+    pattern: re.Pattern = re.compile(r' - - \[.+?] "')
+
     def filter(self, record: logging.LogRecord) -> bool:
-        # '192.168.0.102 - - [30/Jun/2024 01:14:03] "%s" %s %s' -> '192.168.0.102 - "%s" %s %s'
-        record.msg = re.sub(r' - - \[.+?] "', ' - "', record.msg)
+        record.msg = self.pattern.sub(' - "', record.msg)
         return True
