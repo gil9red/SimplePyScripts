433356543496947c4cd66cd74ae8ceb4654f4ac8
gil9red <ilya.petrash@inbox.ru>
2024-06-30 02:21:47 +0500

    Refactoring

--- flask__webservers/logging__remove_date_from_werkzeug_logs/utils.py
+++ flask__webservers/logging__remove_date_from_werkzeug_logs/utils.py
@@ -8,6 +8,7 @@ import logging
 import re
 
 
+# NOTE: Fix https://github.com/pallets/werkzeug/blob/72b2e48e7d44927b1b7d6b2f940d0691230de893/src/werkzeug/serving.py#L425C38-L425C44
 class FilterRemoveDateFromWerkzeugLogs(logging.Filter):
     def filter(self, record: logging.LogRecord) -> bool:
         # "192.168.0.102 - - [30/Jun/2024 01:14:03] "%s" %s %s" -> "192.168.0.102 - "%s" %s %s"
