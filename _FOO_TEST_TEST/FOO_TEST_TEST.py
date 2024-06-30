a176ea57fbc11c1ac9c85e297247412b7cd31935
gil9red <ilya.petrash@inbox.ru>
2024-07-01 00:20:22 +0500

    Updated.

--- _FOO_TEST_TEST/FOO_TEST_TEST.py
+++ _FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -4,3 +4,19 @@
 __author__ = "ipetrash"
 
 
+import time
+from pathlib import Path
+
+from filelock import FileLock, Timeout
+
+
+FILE_NAME = Path(__file__).resolve()
+FILE_NAME_LOCK = str(FILE_NAME) + ".lock"
+
+
+try:
+    with FileLock(FILE_NAME_LOCK, timeout=0):
+        print("Lock")
+        time.sleep(20)
+except Timeout:
+    print("Обнаружен запуск второго приложения. Завершение работы")
