04b183d355906e539df7c7e9045e7453ceceb4da
gil9red <ilya.petrash@inbox.ru>
2024-07-01 12:56:59 +0500

    Refactoring

--- sqlite3__examples/e1.py
+++ sqlite3__examples/e1.py
@@ -8,44 +8,39 @@ __author__ = "ipetrash"
 import sqlite3
 
 
-if __name__ == "__main__":
-    conn = sqlite3.connect("example.db")
-    c = conn.cursor()
+conn = sqlite3.connect("example.db")
+c = conn.cursor()
 
-    # Create table
-    try:
-        c.execute(
-            """CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)"""
-        )
-    except Exception as e:
-        if " already exists" not in str(e):
-            raise e
+# Create table
+c.execute(
+    """CREATE TABLE IF NOT EXISTS stocks (date text, trans text, symbol text, qty real, price real)"""
+)
 
-    # Insert a row of data
-    c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+# Insert a row of data
+c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
 
-    # Save (commit) the changes
-    conn.commit()
+# Save (commit) the changes
+conn.commit()
 
-    # Do this instead
-    t = ("RHAT",)
-    c.execute("SELECT * FROM stocks WHERE symbol=?", t)
-    print(c.fetchone())
+# Do this instead
+t = ("RHAT",)
+c.execute("SELECT * FROM stocks WHERE symbol=?", t)
+print(c.fetchone())
 
-    # Larger example that inserts many records at a time
-    purchases = [
-        ("2006-03-28", "BUY", "IBM", 1000, 45.00),
-        ("2006-04-05", "BUY", "MSFT", 1000, 72.00),
-        ("2006-04-06", "SELL", "IBM", 500, 53.00),
-    ]
-    c.executemany("INSERT INTO stocks VALUES (?,?,?,?,?)", purchases)
+# Larger example that inserts many records at a time
+purchases = [
+    ("2006-03-28", "BUY", "IBM", 1000, 45.00),
+    ("2006-04-05", "BUY", "MSFT", 1000, 72.00),
+    ("2006-04-06", "SELL", "IBM", 500, 53.00),
+]
+c.executemany("INSERT INTO stocks VALUES (?,?,?,?,?)", purchases)
 
-    # Save (commit) the changes
-    conn.commit()
+# Save (commit) the changes
+conn.commit()
 
-    for row in c.execute("SELECT * FROM stocks ORDER BY price"):
-        print(row)
+for row in c.execute("SELECT * FROM stocks ORDER BY price"):
+    print(row)
 
-    # We can also close the connection if we are done with it.
-    # Just be sure any changes have been committed or they will be lost.
-    conn.close()
+# We can also close the connection if we are done with it.
+# Just be sure any changes have been committed or they will be lost.
+conn.close()
