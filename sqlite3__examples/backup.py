95e2b159e0db0125ba65017a53be2976feafe665
gil9red <ilya.petrash@inbox.ru>
2024-07-01 17:01:05 +0500

    Updated.

--- sqlite3__examples/backup.py
+++ sqlite3__examples/backup.py
@@ -19,7 +19,11 @@ DIR_DB_BACKUP.mkdir(parents=True, exist_ok=True)
 FILE_NAME = Path(__file__).resolve()
 
 
-def create_zip_for_file(file_name_zip: str | Path, file_name: Path, delete_file_name: bool = True):
+def create_zip_for_file(
+        file_name_zip: str | Path,
+        file_name: Path,
+        delete_file_name: bool = True,
+):
     with zipfile.ZipFile(file_name_zip, mode="w", compression=zipfile.ZIP_DEFLATED) as f:
         f.write(file_name, arcname=file_name.name)
 
@@ -46,10 +50,10 @@ def backup(
     return file_name
 
 
-with sqlite3.connect(":memory:") as connect:
+def _process_test(connect: sqlite3.Connection):
     connect.executescript(
         """
-        create table stocks (
+        create table if not exists stocks (
             date text, 
             trans text, 
             symbol text,
@@ -62,9 +66,27 @@ with sqlite3.connect(":memory:") as connect:
     """
     )
 
+    print()
     print(connect.execute("select * from stocks").fetchall())
     print()
 
-    file_name_backup = DIR_DB_BACKUP / f"{FILE_NAME.stem}_{date.today()}.db"
+
+with sqlite3.connect(":memory:") as connect:
+    _process_test(connect)
+
+    file_name_backup = DIR_DB_BACKUP / f"{FILE_NAME.stem}_memory_{date.today()}.db"
+    file_name_backup_zip = backup(connect, file_name_backup)
+    print(f"Создан бэкап базы данных в: {file_name_backup_zip}")
+
+
+with sqlite3.connect(
+        str(DIR_DB / FILE_NAME.stem) + ".db",
+        isolation_level=None,
+) as connect:
+    connect.execute('pragma journal_mode=wal')
+
+    _process_test(connect)
+
+    file_name_backup = DIR_DB_BACKUP / f"{FILE_NAME.stem}_wal_{date.today()}.db"
     file_name_backup_zip = backup(connect, file_name_backup)
     print(f"Создан бэкап базы данных в: {file_name_backup_zip}")
