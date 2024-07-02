e8b5f516ce9108985e2d1e4a4f45d448fc5829e8
gil9red <ilya.petrash@inbox.ru>
2024-07-02 15:10:20 +0500

    Updated. Added "pragma_pragma_list" example

--- sqlite3__examples/print_pragma_list.py
+++ sqlite3__examples/print_pragma_list.py
@@ -24,3 +24,22 @@ with sqlite3.connect(":memory:") as connect:
     65. 'wal_checkpoint'
     66. 'writable_schema'
     """
+
+    print("\n" + "-" * 100 + "\n")
+
+    cursor = connect.execute("SELECT * FROM pragma_pragma_list")
+    for i, (name, ) in enumerate(cursor, 1):
+        print(f"{i}. {name!r}")
+    """
+    1. 'analysis_limit'
+    2. 'application_id'
+    3. 'auto_vacuum'
+    4. 'automatic_index'
+    5. 'busy_timeout'
+    ...
+    62. 'trusted_schema'
+    63. 'user_version'
+    64. 'wal_autocheckpoint'
+    65. 'wal_checkpoint'
+    66. 'writable_schema'
+    """
