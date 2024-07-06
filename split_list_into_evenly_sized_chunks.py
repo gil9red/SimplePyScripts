2cf402b71b89a0fe5ad1081f779060e8759c3836
gil9red <ilya.petrash@inbox.ru>
2024-07-06 17:58:06 +0500

    Refactoring

--- split_list_into_evenly_sized_chunks.py
+++ split_list_into_evenly_sized_chunks.py
@@ -7,7 +7,10 @@ __author__ = "ipetrash"
 # SOURCE: https://stackoverflow.com/a/312464/5909792
 
 
-def chunks(l, n):
+from typing import Any, Generator, Sized
+
+
+def chunks(l: Sized, n: int) -> Generator[Any, None, None]:
     """Yield successive n-sized chunks from l."""
     for i in range(0, len(l), n):
         yield l[i : i + n]
@@ -20,3 +23,6 @@ if __name__ == "__main__":
 
     print(list(chunks(list(range(9)), n=3)))  # [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
     print(list(chunks(list(range(9)), n=5)))  # [[0, 1, 2, 3, 4], [5, 6, 7, 8]]
+    print()
+
+    print(list(chunks(range(9), n=3)))  # [range(0, 3), range(3, 6), range(6, 9)]
