0f9470e57b965f6046c9145c61a3a2539321b803
gil9red <ilya.petrash@inbox.ru>
2024-08-11 01:20:37 +0500

    Updated.

--- html_parsing/youtube_com/results_search_query.py
+++ html_parsing/youtube_com/results_search_query.py
@@ -49,7 +49,7 @@ def time_to_seconds(time_str: str) -> int:
 
 
 USER_AGENT = (
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0"
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0"
 )
 
 BASE_URL = "https://www.youtube.com"
