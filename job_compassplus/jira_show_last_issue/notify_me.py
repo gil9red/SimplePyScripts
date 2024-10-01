3a301d2029e9b239f64f2f892dfed678cfebab0a
gil9red <ilya.petrash@inbox.ru>
2024-10-01 21:37:21 +0500

    Updated.

--- job_compassplus/jira_show_last_issue/notify_me.py
+++ job_compassplus/jira_show_last_issue/notify_me.py
@@ -12,15 +12,17 @@ from tkinter.messagebox import showinfo
 from main import get_last_issue_key
 
 
-NEED_PROJECT = "OPTT"
-NEED_ISSUE_KEY = f"{NEED_PROJECT}-999"
+NEED_PROJECT: str = "OPTT"
+NEED_ISSUE_ID: int = 999
 
 
 while True:
-    last_issue_key = get_last_issue_key(NEED_PROJECT)
+    last_issue_key: str = get_last_issue_key(NEED_PROJECT)
     print(f'{datetime.now():%d/%m/%Y %H:%M:%S} last_issue_key: {last_issue_key}')
 
-    if last_issue_key == NEED_ISSUE_KEY:
-        showinfo(title="Информация", message=f"Появилась задача {NEED_ISSUE_KEY}!")
+    # NOTE: "OPTT-1234" -> 1234
+    issue_id: int = int(last_issue_key.split("-")[-1])
+    if issue_id >= NEED_ISSUE_ID:
+        showinfo(title="Информация", message=f"Появилась задача {last_issue_key}!")
 
     time.sleep(30)
