0e15160e8a67edcb0b363954afbc501c3e0b3f26
gil9red <ilya.petrash@inbox.ru>
2024-10-05 00:26:05 +0500

    Updated UI.

--- job_compassplus/parse_jira_logged_time/gui.py
+++ job_compassplus/parse_jira_logged_time/gui.py
@@ -9,6 +9,7 @@ import sys
 import traceback
 import webbrowser
 
+from collections import defaultdict
 from contextlib import redirect_stdout
 from datetime import datetime, date
 from typing import Any
@@ -31,6 +32,7 @@ from PyQt5.QtWidgets import (
     QProgressBar,
     QSizePolicy,
     QToolTip,
+    QTabWidget,
 )
 from PyQt5.QtCore import (
     QThread,
@@ -112,10 +114,289 @@ def create_table_item(
     return item
 
 
+def add_table_row(table: QTableWidget, items: list[QTableWidgetItem]):
+    row = table.rowCount()
+    table.setRowCount(row + 1)
+    for j, item in enumerate(items):
+        table.setItem(row, j, item)
+
+
 def clear_table(table_widget: QTableWidget):
     # Удаление строк таблицы
     while table_widget.rowCount():
         table_widget.removeRow(0)
+
+
+def open_jira(jira_id: str):
+    url = f"{JIRA_HOST}/browse/{jira_id}"
+    webbrowser.open(url)
+
+
+class LoggedWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.table_logged = create_table(
+            header_labels=["DATE", "TOTAL LOGGED"],
+        )
+        self.table_logged.itemSelectionChanged.connect(
+            lambda: self._on_table_logged_item_clicked(self.table_logged.currentItem())
+        )
+
+        self.table_logged_info = create_table(
+            header_labels=["TIME", "LOGGED", "JIRA", "TITLE"],
+        )
+
+        # Первые 3 колонки (кроме названия) имеют размер под содержимое
+        for j in range(3):
+            self.table_logged_info.horizontalHeader().setSectionResizeMode(
+                j, QHeaderView.ResizeToContents
+            )
+        self.table_logged_info.itemDoubleClicked.connect(
+            self._on_table_logged_info_item_double_clicked
+        )
+
+        splitter_main = QSplitter(Qt.Horizontal)
+        splitter_main.addWidget(self.table_logged)
+        splitter_main.addWidget(self.table_logged_info)
+        splitter_main.setSizes([300, 600])
+
+        layout = QVBoxLayout(self)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(splitter_main)
+
+        self.setLayout(layout)
+
+    def set_date_by_activities(
+        self,
+        date_by_activities: dict[date, list[Activity]]
+    ):
+        clear_table(self.table_logged)
+
+        for entry_date, activities in sorted(
+            date_by_activities.items(), key=lambda x: x[0], reverse=True
+        ):
+            activities: list[Activity] = [
+                obj for obj in reversed(activities) if obj.is_logged()
+            ]
+
+            total_seconds: int = get_logged_total_seconds(activities)
+            total_seconds_str: str = seconds_to_str(total_seconds)
+
+            date_str: str = entry_date.strftime("%d/%m/%Y")
+            is_odd_week: int = entry_date.isocalendar().week % 2 == 1
+
+            # Не показывать даты, в которых не было залогировано
+            if not total_seconds:
+                continue
+
+            items = [
+                create_table_item(date_str, data=activities),
+                create_table_item(
+                    total_seconds_str,
+                    tool_tip=f"Total seconds: {total_seconds}",
+                ),
+            ]
+            for item in items:
+                if is_odd_week:
+                    item.setBackground(Qt.lightGray)
+
+            add_table_row(self.table_logged, items)
+
+        self.table_logged.setCurrentCell(0, 0)
+        self.table_logged.setFocus()
+        self._on_table_logged_item_clicked(self.table_logged.currentItem())
+
+    def _on_table_logged_item_clicked(self, item: QTableWidgetItem | None):
+        clear_table(self.table_logged_info)
+
+        if not item:
+            return
+
+        row = item.row()
+        item1 = item.tableWidget().item(row, 0)
+
+        activities: list[Activity] = item1.data(Qt.UserRole)
+        if not activities:
+            return
+
+        for activity in activities:
+            items = [
+                create_table_item(activity.entry_dt.strftime("%H:%M:%S")),
+                create_table_item(activity.logged_human_time),
+                create_table_item(activity.jira_id),
+                create_table_item(activity.jira_title, tool_tip=activity.jira_title),
+            ]
+            add_table_row(self.table_logged_info, items)
+
+    def _on_table_logged_info_item_double_clicked(self, item: QTableWidgetItem):
+        row = item.row()
+        jira_id = item.tableWidget().item(row, 2).text()
+        open_jira(jira_id)
+
+
+class ActivitiesWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.table_date = create_table(
+            header_labels=["DATE", "TOTAL LOGGED", "ACTIVITIES"],
+        )
+        self.table_date.itemSelectionChanged.connect(
+            lambda: self._on_table_date_item_clicked(self.table_date.currentItem())
+        )
+
+        self.table_date_by_jira = create_table(
+            header_labels=["LOGGED", "ACTIVITIES", "JIRA", "TITLE"],
+        )
+        self.table_date_by_jira.itemSelectionChanged.connect(
+            lambda: self._on_table_date_by_jira_item_clicked(self.table_date_by_jira.currentItem())
+        )
+        self.table_date_by_jira.itemDoubleClicked.connect(
+            self._on_table_date_by_jira_item_double_clicked
+        )
+        for j in [0, 1, 2]:
+            self.table_date_by_jira.horizontalHeader().setSectionResizeMode(
+                j, QHeaderView.ResizeToContents
+            )
+
+        self.table_jira_by_activities = create_table(
+            header_labels=["TIME", "LOGGED", "ACTION", "TEXT"],
+        )
+        for j in [0, 1, 2]:
+            self.table_jira_by_activities.horizontalHeader().setSectionResizeMode(
+                j, QHeaderView.ResizeToContents
+            )
+
+        splitter_table_activities = QSplitter(Qt.Vertical)
+        splitter_table_activities.addWidget(self.table_date_by_jira)
+        splitter_table_activities.addWidget(self.table_jira_by_activities)
+
+        splitter_main = QSplitter(Qt.Horizontal)
+        splitter_main.addWidget(self.table_date)
+        splitter_main.addWidget(splitter_table_activities)
+        splitter_main.setSizes([400, 600])
+
+        layout = QVBoxLayout(self)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(splitter_main)
+
+        self.setLayout(layout)
+
+    def set_date_by_activities(
+        self,
+        date_by_activities: dict[date, list[Activity]]
+    ):
+        clear_table(self.table_date)
+
+        for entry_date, activities in sorted(
+            date_by_activities.items(), key=lambda x: x[0], reverse=True
+        ):
+            activities_number = len(activities)
+
+            total_seconds: int = get_logged_total_seconds(activities)
+            total_seconds_str: str = seconds_to_str(total_seconds)
+
+            date_str: str = entry_date.strftime("%d/%m/%Y")
+            is_odd_week: int = entry_date.isocalendar().week % 2 == 1
+
+            # Не показывать даты, в которых не было залогировано
+            if not total_seconds:
+                continue
+
+            items = [
+                create_table_item(date_str, data=activities),
+                create_table_item(
+                    total_seconds_str,
+                    tool_tip=f"Total seconds: {total_seconds}",
+                ),
+                create_table_item(
+                    f"{activities_number}",
+                ),
+            ]
+            for item in items:
+                if is_odd_week:
+                    item.setBackground(Qt.lightGray)
+
+            add_table_row(self.table_date, items)
+
+        self.table_date.setCurrentCell(0, 0)
+        self.table_date.setFocus()
+        self._on_table_date_item_clicked(self.table_date.currentItem())
+
+    def _on_table_date_item_clicked(self, item: QTableWidgetItem | None):
+        clear_table(self.table_date_by_jira)
+
+        if not item:
+            return
+
+        row = item.row()
+        item1 = item.tableWidget().item(row, 0)
+
+        activities: list[Activity] = item1.data(Qt.UserRole)
+        if not activities:
+            return
+
+        jira_by_activity: dict[str, list[Activity]] = defaultdict(list)
+        for activity in activities:
+            jira_by_activity[activity.jira_id].append(activity)
+
+        for activities in sorted(jira_by_activity.values(), key=get_logged_total_seconds, reverse=True):
+            # Группировка была по джире
+            activity = activities[0]
+            jira_id = activity.jira_id
+            jira_title = activity.jira_title
+
+            total_logged_seconds = get_logged_total_seconds(activities)
+            total_logged_human = seconds_to_str(total_logged_seconds) if total_logged_seconds else ""
+
+            items = [
+                create_table_item(total_logged_human),
+                create_table_item(f"{len(activities)}"),
+                create_table_item(jira_id),
+                create_table_item(jira_title, tool_tip=jira_title),
+            ]
+            items[0].setData(Qt.UserRole, activities)
+
+            add_table_row(self.table_date_by_jira, items)
+
+        self.table_date_by_jira.setCurrentCell(0, 0)
+        self._on_table_date_by_jira_item_clicked(self.table_date_by_jira.currentItem())
+
+    def _on_table_date_by_jira_item_clicked(self, item: QTableWidgetItem | None):
+        clear_table(self.table_jira_by_activities)
+
+        if not item:
+            return
+
+        row = item.row()
+        item1 = item.tableWidget().item(row, 0)
+
+        activities: list[Activity] = item1.data(Qt.UserRole)
+        if not activities:
+            return
+
+        activities.sort(key=lambda x: x.entry_dt)
+
+        for activity in activities:
+            items = [
+                create_table_item(activity.entry_dt.strftime("%H:%M:%S")),
+                create_table_item(activity.logged_human_time),
+                create_table_item(activity.action.name),
+                create_table_item(activity.action_text, tool_tip=activity.action_text),
+            ]
+            add_table_row(self.table_jira_by_activities, items)
+
+    def _on_table_date_by_jira_item_double_clicked(self, item: QTableWidgetItem):
+        row = item.row()
+        item1 = item.tableWidget().item(row, 0)
+
+        activities: list[Activity] = item1.data(Qt.UserRole)
+        if not activities:
+            return
+
+        activity: Activity = activities[0]
+        open_jira(activity.jira_id)
 
 
 class MainWindow(QMainWindow):
@@ -175,37 +456,19 @@ class MainWindow(QMainWindow):
         self.thread_get_data.run_finished.connect(self._fill_tables)
         self.thread_get_data.finished.connect(self._after_refresh)
 
-        self.table_logged = create_table(
-            header_labels=["DATE", "TOTAL LOGGED TIME"],
-        )
-        self.table_logged.itemSelectionChanged.connect(
-            lambda: self._on_table_logged_item_clicked(self.table_logged.currentItem())
-        )
+        self.logged_widget = LoggedWidget()
+        self.activities_widget = ActivitiesWidget()
 
-        self.table_logged_info = create_table(
-            header_labels=["TIME", "LOGGED", "JIRA", "TITLE"],
-        )
-
-        # Первые 3 колонки (кроме названия) имеют размер под содержимое
-        for j in range(3):
-            self.table_logged_info.horizontalHeader().setSectionResizeMode(
-                j, QHeaderView.ResizeToContents
-            )
-        self.table_logged_info.itemDoubleClicked.connect(
-            self._on_table_logged_info_item_double_clicked
-        )
-
-        splitter_table = QSplitter(Qt.Horizontal)
-        splitter_table.addWidget(self.table_logged)
-        splitter_table.addWidget(self.table_logged_info)
-        splitter_table.setSizes([300, 600])
+        tab_widget = QTabWidget()
+        tab_widget.addTab(self.logged_widget, "LOGGED")
+        tab_widget.addTab(self.activities_widget, "ACTIVITIES")
 
         layout_log = QVBoxLayout()
         layout_log.addWidget(self.log)
         layout_log.addWidget(self.cb_show_log)
 
         layout_content = QVBoxLayout()
-        layout_content.addWidget(splitter_table)
+        layout_content.addWidget(tab_widget)
         layout_content.addLayout(layout_log)
 
         layout_refresh = QHBoxLayout()
@@ -274,11 +537,12 @@ class MainWindow(QMainWindow):
                 if not date_by_activities:
                     return
 
+                self.logged_widget.set_date_by_activities(date_by_activities)
+                self.activities_widget.set_date_by_activities(date_by_activities)
+
                 # Для красоты выводим результат в табличном виде
                 table_header: tuple = ("DATE", "LOGGED", "SECONDS", "ACTIVITIES")
                 table_lines: list[tuple[str, str, int, int]] = []
-
-                clear_table(self.table_logged)
 
                 for entry_date, activities in sorted(
                     date_by_activities.items(), key=lambda x: x[0], reverse=True
@@ -296,32 +560,6 @@ class MainWindow(QMainWindow):
                     table_lines.append(
                         (date_str, total_seconds_str, total_seconds, activities_number)
                     )
-
-                    is_odd_week: int = entry_date.isocalendar().week % 2 == 1
-
-                    # Не показывать даты, в которых не было залогировано
-                    if not total_seconds:
-                        continue
-
-                    items = [
-                        create_table_item(date_str, data=activities),
-                        create_table_item(
-                            total_seconds_str,
-                            tool_tip=f"Total seconds: {total_seconds}",
-                        ),
-                    ]
-
-                    i = self.table_logged.rowCount()
-                    self.table_logged.setRowCount(i + 1)
-                    for j, item in enumerate(items):
-                        if is_odd_week:
-                            item.setBackground(Qt.lightGray)
-
-                        self.table_logged.setItem(i, j, item)
-
-                self.table_logged.setCurrentCell(0, 0)
-                self.table_logged.setFocus()
-                self._on_table_logged_item_clicked(self.table_logged.currentItem())
 
                 print()
 
@@ -361,39 +599,6 @@ class MainWindow(QMainWindow):
             return
 
         self.thread_get_data.start()
-
-    def _on_table_logged_item_clicked(self, item: QTableWidgetItem | None):
-        clear_table(self.table_logged_info)
-
-        if not item:
-            return
-
-        row = item.row()
-        item1 = self.table_logged.item(row, 0)
-
-        activities: list[Activity] = item1.data(Qt.UserRole)
-        if not activities:
-            return
-
-        for activity in activities:
-            items = [
-                create_table_item(activity.entry_dt.strftime("%H:%M:%S")),
-                create_table_item(activity.logged_human_time),
-                create_table_item(activity.jira_id),
-                create_table_item(activity.jira_title, tool_tip=activity.jira_title),
-            ]
-
-            i = self.table_logged_info.rowCount()
-            self.table_logged_info.setRowCount(i + 1)
-            for j, item in enumerate(items):
-                self.table_logged_info.setItem(i, j, item)
-
-    def _on_table_logged_info_item_double_clicked(self, item: QTableWidgetItem):
-        row = item.row()
-        jira_id = self.table_logged_info.item(row, 2).text()
-
-        url = f"{JIRA_HOST}/browse/{jira_id}"
-        webbrowser.open(url)
 
     def _on_tray_activated(self, reason):
         self.setVisible(not self.isVisible())
