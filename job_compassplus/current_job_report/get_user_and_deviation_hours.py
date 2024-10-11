3a0689d0616cc776f0c64cec0c12844bdc93b418
gil9red <ilya.petrash@inbox.ru>
2024-10-12 01:27:58 +0500

    Updated. Replacing lxml on bs4

--- job_compassplus/current_job_report/get_user_and_deviation_hours.py
+++ job_compassplus/current_job_report/get_user_and_deviation_hours.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 __author__ = "ipetrash"
 
 
-from lxml import etree
-
+import re
+from bs4 import BeautifulSoup, Tag
 from utils import (
     NotFoundReport,
     get_report_context,
@@ -15,84 +14,44 @@ from utils import (
 )
 
 
-def get_text_children(el: etree._Element, idx: int) -> str:
+def get_text_children(el: Tag, idx: int) -> str:
     try:
-        return el.getchildren()[idx].text.strip()
-    except IndexError:
+        return list(el.children)[idx].text.strip()
+    except Exception:
         return ""
 
 
-def get_tr_for_current_user(html: str) -> etree._Element:
-    root = etree.HTML(html)
+def get_tr_for_current_user(html: str) -> Tag:
+    root = BeautifulSoup(html, "html.parser")
 
-    xpath_1 = (
-        '//table[@id="report"]/tbody/tr[th[contains(text(),"Текущий пользователь")]]'
-    )
-    xpath_2 = (
-        '//table[@class="report"]/tbody/tr[th[contains(text(),"Текущий пользователь")]]'
-    )
+    tbody_el = root.select_one("#report > tbody")
+    if not tbody_el:
+        tbody_el = root.select_one(".report > tbody")
+        if not tbody_el:
+            raise NotFoundReport()
 
-    # Вытаскивание tr, у которого есть вложенный th, имеющий в содержимом текст "Текущий пользователь"
-    try:
-        items = root.xpath(xpath_1)
-        if not items:
-            items = root.xpath(xpath_2)
-
-        return items[0]
-
-    except IndexError:
+    tr_el = tbody_el.find("tr", text=re.compile(".*Текущий пользователь.*"))
+    if not tr_el:
         raise NotFoundReport()
+
+    return tr_el
 
 
 def parse_current_user_deviation_hours(html: str) -> tuple[str, str]:
     current_user_tr = get_tr_for_current_user(html)
 
     # Получение следующего элемента после текущего, у него получение первого ребенка, у которого вытаскивается текст
-    name = get_text_children(current_user_tr.getnext(), idx=0)
+    name = get_text_children(current_user_tr.find_next_sibling(), idx=0)
 
     # Получение следующего элемента после текущего, у него получение последнего
     # ребенка, у которого вытаскивается текст
     # Ищем последнюю строку текущего пользователя -- в ней и находится время работы
     # Ее легко найти -- ее первая ячейка пустая
-    deviation_tr = current_user_tr.getnext()
+    deviation_tr = current_user_tr.find_next_sibling()
 
     # Ищем строку с пустой ячейкой
     while get_text_children(deviation_tr, idx=0):
-        deviation_tr = deviation_tr.getnext()
-
-    deviation_hours = get_text_children(deviation_tr, idx=-1)
-    return name, clear_hours(deviation_hours)
-
-
-def parse_user_deviation_hours(html: str, user_name: str = "Петраш") -> tuple[str, str]:
-    root = etree.HTML(html)
-
-    XPATH_1 = f'//table[@id="report"]/tbody/tr[td[contains(text(),"{user_name}")]]'
-    XPATH_2 = f'//table[@class="report"]/tbody/tr[td[contains(text(),"{user_name}")]]'
-
-    # Вытаскивание tr, у которого есть вложенный th, имеющий в содержимом текст "Текущий пользователь"
-    try:
-        items = root.xpath(XPATH_1)
-        if not items:
-            items = root.xpath(XPATH_2)
-
-        current_user_tr = items[0]
-
-    except IndexError:
-        raise NotFoundReport()
-
-    # Получение текста текущего элемента
-    name = get_text_children(current_user_tr, idx=0)
-
-    # Получение следующего элемента после текущего, у него получение последнего
-    # ребенка, у которого вытаскивается текст
-    # Ищем последнюю строку текущего пользователя -- в ней и находится время работы
-    # Ее легко найти -- ее первая ячейка пустая
-    deviation_tr = current_user_tr.getnext()
-
-    # Ищем строку с пустой ячейкой
-    while get_text_children(deviation_tr, idx=0):
-        deviation_tr = deviation_tr.getnext()
+        deviation_tr = deviation_tr.find_next_sibling()
 
     deviation_hours = get_text_children(deviation_tr, idx=-1)
     return name, clear_hours(deviation_hours)
@@ -109,6 +68,16 @@ def get_quarter_user_and_deviation_hours() -> tuple[str, str]:
 
 
 if __name__ == "__main__":
+    """
+    Петраш Илья Андреевич
+    Переработка 03:27:13
+
+    Петраш Илья Андреевич
+    Переработка за квартал 03:27
+
+    Worklog(actually='59:57:13', logged='47:35', logged_percent=79)
+    """
+
     name, deviation_hours = get_user_and_deviation_hours()
     print(name)
     print(
