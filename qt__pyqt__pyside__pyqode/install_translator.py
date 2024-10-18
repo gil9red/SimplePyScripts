7b0c5fafb7e75035504cf4c6e6c2f6699ce19ab0
gil9red <ilya.petrash@inbox.ru>
2024-10-18 18:01:57 +0500

    Added "qt__pyqt__pyside__pyqode/install_translator.py"

---
+++ qt__pyqt__pyside__pyqode/install_translator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+from PyQt5.QtWidgets import QApplication, QMessageBox
+from PyQt5.QtCore import QTranslator, QLibraryInfo
+
+
+def question():
+    QMessageBox.question(
+        None,
+        "TITLE",
+        "TEXT",
+        QMessageBox.Yes | QMessageBox.No | QMessageBox.Abort,
+    )
+
+
+app = QApplication([])
+
+# NOTE: EN
+question()
+
+translator = QTranslator()
+if translator.load("qtbase_ru", directory=QLibraryInfo.location(QLibraryInfo.TranslationsPath)):
+    app.installTranslator(translator)
+
+# NOTE: RU
+question()
