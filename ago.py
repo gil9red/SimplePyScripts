4ed61cc33e0b99eef67bd95e599686f82445d06a
gil9red <ilya.petrash@inbox.ru>
2024-10-19 00:45:22 +0500

    Updated.

--- ago.py
+++ ago.py
@@ -4,10 +4,8 @@
 __author__ = "ipetrash"
 
 
-from dataclasses import dataclass
 from datetime import timedelta
 from enum import IntEnum
-from typing import Callable
 
 
 class UnitSeconds(IntEnum):
@@ -20,43 +18,67 @@ class UnitSeconds(IntEnum):
     YEAR = 12 * MONTH
 
 
-@dataclass
 class L10N:
-    singular: dict[UnitSeconds, str]
-    plural: dict[UnitSeconds, str]
+    def get_template(self) -> str:
+        return "{value} {unit} ago"
 
-    choice: Callable[[int], dict[UnitSeconds, str]] = (
-        lambda self, value: self.singular if value == 1 else self.plural
-    )
+    def get_unit(self, value: int, unit: UnitSeconds) -> str:
+        unit = unit.name.lower()
+        if value != 1:
+            unit = f"{unit}s"
+        return unit
 
     def get_value(self, value: int, unit: UnitSeconds) -> str:
-        template = self.choice(self, value)[unit]
-        return template.format(value=value)
+        unit = self.get_unit(value, unit)
+        return self.get_template().format(value=value, unit=unit)
 
 
-L10N_EN = L10N(
-    singular={
-        UnitSeconds.SECOND: "{value} second ago",
-        UnitSeconds.MINUTE: "{value} minute ago",
-        UnitSeconds.HOUR: "{value} hour ago",
-        UnitSeconds.DAY: "{value} day ago",
-        UnitSeconds.WEEK: "{value} week ago",
-        UnitSeconds.MONTH: "{value} month ago",
-        UnitSeconds.YEAR: "{value} year ago",
-    },
-    plural={
-        UnitSeconds.SECOND: "{value} seconds ago",
-        UnitSeconds.MINUTE: "{value} minutes ago",
-        UnitSeconds.HOUR: "{value} hours ago",
-        UnitSeconds.DAY: "{value} days ago",
-        UnitSeconds.WEEK: "{value} weeks ago",
-        UnitSeconds.MONTH: "{value} months ago",
-        UnitSeconds.YEAR: "{value} years ago",
-    },
-)
+class L10N_RU(L10N):
+    def get_template(self) -> str:
+        return "{value} {unit} назад"
+
+    # SOURCE: https://ru.stackoverflow.com/a/1380460/201445
+    @staticmethod
+    def declension(n: int, form_0: str, form_1: str, form_2: str) -> str:
+        units = n % 10
+        tens = (n // 10) % 10
+        if tens == 1:
+            return form_0
+        if units in [0, 5, 6, 7, 8, 9]:
+            return form_0
+        if units == 1:
+            return form_1
+        if units in [2, 3, 4]:
+            return form_2
+
+    def get_unit(self, value: int, unit: UnitSeconds) -> str:
+        match unit:
+            case UnitSeconds.SECOND:
+                return self.declension(value, "секунд", "секунда", "секунды")
+
+            case UnitSeconds.MINUTE:
+                return self.declension(value, "минут", "минута", "минуты")
+
+            case UnitSeconds.HOUR:
+                return self.declension(value, "часов", "час", "часа")
+
+            case UnitSeconds.DAY:
+                return self.declension(value, "дней", "день", "дня")
+
+            case UnitSeconds.WEEK:
+                return self.declension(value, "недель", "неделя", "недели")
+
+            case UnitSeconds.MONTH:
+                return self.declension(value, "месяцев", "месяц", "месяца")
+
+            case UnitSeconds.YEAR:
+                return self.declension(value, "лет", "год", "года")
+
+            case _:
+                raise NotImplemented()
 
 
-def ago(seconds: timedelta, l10n: L10N = L10N_EN) -> str:
+def ago(seconds: timedelta, l10n: L10N = L10N()) -> str:
     seconds = int(seconds.total_seconds())
     if seconds < 0:
         seconds = -seconds
@@ -106,3 +128,38 @@ if __name__ == "__main__":
 
     dt2 = datetime(year=2024, month=12, day=10)
     assert ago(dt2 - dt) == "2 days ago"
+
+    print("\n" + "-" * 10 + "\n")
+
+    items = [
+        (timedelta(seconds=0), "0 секунд назад"),
+        (timedelta(seconds=1), "1 секунда назад"),
+        (timedelta(seconds=59), "59 секунд назад"),
+        (timedelta(seconds=60), "1 минута назад"),
+        (timedelta(seconds=90), "1 минута назад"),
+        (timedelta(minutes=1), "1 минута назад"),
+        (timedelta(minutes=5), "5 минут назад"),
+        (timedelta(minutes=45), "45 минут назад"),
+        (timedelta(hours=1, minutes=45), "1 час назад"),
+        (timedelta(hours=4, minutes=50), "4 часа назад"),
+        (timedelta(hours=23, minutes=50), "23 часа назад"),
+        (timedelta(hours=40, minutes=50), "1 день назад"),
+        (timedelta(days=1), "1 день назад"),
+        (timedelta(hours=48), "2 дня назад"),
+        (timedelta(days=2), "2 дня назад"),
+        (timedelta(days=7), "1 неделя назад"),
+        (timedelta(days=8), "1 неделя назад"),
+        (timedelta(weeks=1), "1 неделя назад"),
+        (timedelta(weeks=2), "2 недели назад"),
+        (timedelta(weeks=4), "1 месяц назад"),
+        (timedelta(weeks=8), "2 месяца назад"),
+        (timedelta(weeks=12 * 4), "1 год назад"),
+        (timedelta(weeks=5 * 12 * 4), "5 лет назад"),
+    ]
+    for value, expected in items:
+        actual = ago(dt - (dt - value), l10n=L10N_RU())
+        print(f"{value!r} -> {actual!r}")
+        assert expected == actual, f"{expected!r} != {actual!r}"
+
+    dt2 = datetime(year=2024, month=12, day=10)
+    assert ago(dt2 - dt, l10n=L10N_RU()) == "2 дня назад"
