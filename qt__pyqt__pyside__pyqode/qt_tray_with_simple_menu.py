68102d708c050c3415f153a6550913415d638b96
gil9red <ilya.petrash@inbox.ru>
2024-10-31 18:48:24 +0500

    Updated.

--- qt__pyqt__pyside__pyqode/qt_tray_with_simple_menu.py
+++ qt__pyqt__pyside__pyqode/qt_tray_with_simple_menu.py
@@ -14,19 +14,37 @@ mw = QWidget()
 mw.show()
 
 icon = app.style().standardIcon(QStyle.SP_DirOpenIcon)
+# OR
 # icon = QIcon('favicon.ico')
 
 tray = QSystemTrayIcon(icon)
 
 menu = QMenu()
-action_settings = menu.addAction("About Qt")
-action_settings.triggered.connect(lambda: QMessageBox.aboutQt(None))
 
+menu_about = menu.addMenu("About")
+action_about = menu_about.addAction("About")
+action_about.triggered.connect(
+    lambda: QMessageBox.information(None, "Info", "Hello World!")
+)
+
+action_about_qt = menu_about.addAction("About Qt")
+action_about_qt.triggered.connect(lambda: QMessageBox.aboutQt(None))
+
+menu.addSeparator()
 action_quit = menu.addAction("Quit")
 action_quit.triggered.connect(app.quit)
 
 tray.setContextMenu(menu)
-tray.activated.connect(lambda x: menu.exec(tray.geometry().center()))
+
+
+def on_tray_activated(reason: QSystemTrayIcon.ActivationReason):
+    if reason == QSystemTrayIcon.ActivationReason.Context:
+        return
+
+    mw.setVisible(not mw.isVisible())
+
+
+tray.activated.connect(on_tray_activated)
 
 tray.show()
 
