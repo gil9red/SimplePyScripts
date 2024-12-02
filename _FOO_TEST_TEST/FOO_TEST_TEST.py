ea3123ce684e944c853b96f78c7e4966729475f5
gil9red <ilya.petrash@inbox.ru>
2024-12-03 01:11:33 +0500

    Updated.

--- _FOO_TEST_TEST/FOO_TEST_TEST.py
+++ _FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -4,4 +4,21 @@
 __author__ = "ipetrash"
 
 
+from datetime import datetime, timedelta
 
+
+def get_zero_time(dt: datetime) -> datetime:
+    return dt.replace(hour=0, minute=0, second=0, microsecond=0)
+
+
+def to_ms(dt: datetime) -> int:
+    return int(dt.timestamp() * 1000)
+
+
+dt = get_zero_time(datetime.utcnow())
+dt1 = get_zero_time(dt - timedelta(days=1))
+dt2 = get_zero_time(dt + timedelta(days=1))
+
+print(to_ms(dt1), dt1)
+print(to_ms(dt2), dt2)
+print(f"{to_ms(dt1)}+{to_ms(dt2)}")
