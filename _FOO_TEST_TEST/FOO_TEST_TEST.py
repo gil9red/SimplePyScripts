854960f7ba7a9cc111ba50c96dd44828aa74dd02
gil9red <ilya.petrash@inbox.ru>
2024-12-04 01:45:53 +0500

    Updated.

--- _FOO_TEST_TEST/FOO_TEST_TEST.py
+++ _FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -11,14 +11,38 @@ def get_zero_time(dt: datetime) -> datetime:
     return dt.replace(hour=0, minute=0, second=0, microsecond=0)
 
 
+def get_items(
+    start_dt: datetime,
+    end_dt: datetime,
+    delta: timedelta,
+) -> list[tuple[datetime, datetime]]:
+    items = []
+
+    dt = start_dt
+    while True:
+        if dt >= end_dt:
+            break
+
+        dt1 = dt
+        dt += delta
+
+        if dt > end_dt:
+            dt = end_dt
+
+        items.append((dt1, dt))
+
+    return items
+
+
 def to_ms(dt: datetime) -> int:
     return int(dt.timestamp() * 1000)
 
 
 dt = get_zero_time(datetime.utcnow())
-dt1 = get_zero_time(dt - timedelta(days=1))
-dt2 = get_zero_time(dt + timedelta(days=1))
 
-print(to_ms(dt1), dt1)
-print(to_ms(dt2), dt2)
-print(f"{to_ms(dt1)}+{to_ms(dt2)}")
+for dt1, dt2 in get_items(
+    start_dt=get_zero_time(dt - timedelta(weeks=6)),
+    end_dt=get_zero_time(dt + timedelta(days=1)),
+    delta=timedelta(weeks=1),
+):
+    print(f"{dt1} - {dt2}. {to_ms(dt1)}+{to_ms(dt2)}")
