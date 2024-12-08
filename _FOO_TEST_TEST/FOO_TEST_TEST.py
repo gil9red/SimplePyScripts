10a22b25ced15817187eecd78e1d088968051c39
gil9red <ilya.petrash@inbox.ru>
2024-12-08 21:32:09 +0500

    Updated.

--- _FOO_TEST_TEST/FOO_TEST_TEST.py
+++ _FOO_TEST_TEST/FOO_TEST_TEST.py
@@ -4,13 +4,82 @@
 __author__ = "ipetrash"
 
 
-from datetime import datetime, timedelta
+from dataclasses import dataclass
+from datetime import datetime, timedelta, date
+
+
+def change_month(d: date, inc: bool = True, number: int = 1) -> date:
+    d = d.replace(day=1)
+
+    year = d.year
+    month = d.month
+
+    for _ in range(number):
+        month += 1 if inc else -1
+        if month > 12:
+            month = 1
+            year += 1
+        elif month < 1:
+            month = 12
+            year -= 1
+
+        d = date(year=year, month=month, day=1)
+
+    return d
+
+
+"""
+Date: 06 мая 2024 г. 21:11:42 | Release version 3.2.40.10 (release based on revision 324264)
+Date: 03 июля 2024 г. 19:11:35 | Release version 3.2.41.10 (release based on revision 327113)
+Date: 04 сентября 2024 г. 15:08:49 | Release version 3.2.42.10 (release based on revision 330920)
+Date: 05 ноября 2024 г. 19:40:28 | Release version 3.2.43.10 (release based on revision 335027)
+"""
+
+
+@dataclass
+class Release:
+    date: date
+    version: int
+    # TODO: Дата + месяц, когда можно коммитить без согласования
+    # TODO: Дата окончания поддержки
+    # TODO: post_init
+
+    def get_next_release(self) -> "Release":
+        return Release(
+            date=change_month(self.date, number=2),
+            version=self.version + 1,
+        )
+
+
+releases: list[Release] = [
+    Release(
+        date=date.today().replace(month=5, day=1),
+        version=40,
+    ),
+]
+
+for _ in range(10):
+    release = releases[-1]
+    releases.append(release.get_next_release())
+    print(releases[-1])
+
+# d = date.today().replace(day=1)
+# print(d)
+# print()
+#
+# for _ in range(20):
+#     d = change_month(d)
+#     print(d)
+
+
+quit()
 
 
 def get_zero_time(dt: datetime) -> datetime:
     return dt.replace(hour=0, minute=0, second=0, microsecond=0)
 
 
+# TODO: date
 def get_items(
     start_dt: datetime,
     end_dt: datetime,
@@ -34,10 +103,12 @@ def get_items(
     return items
 
 
+# TODO: date
 def to_ms(dt: datetime) -> int:
     return int(dt.timestamp() * 1000)
 
 
+# TODO: date
 dt = get_zero_time(datetime.utcnow())
 print(dt)
 # 2024-12-04 00:00:00
