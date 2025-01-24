cb5aaa91a9af0d63c8cc4fa189991ed2c3fd4eb1
gil9red <ilya.petrash@inbox.ru>
2025-01-24 22:44:54 +0500

    Refactoring. cydoomgeneric__examples/pyqt5.py

--- cydoomgeneric__examples/pyqt5.py
+++ cydoomgeneric__examples/pyqt5.py
@@ -7,15 +7,12 @@ __author__ = "ipetrash"
 import sys
 import traceback
 from queue import Queue
-from typing import Optional
-
-import numpy as np
-import pygame
 
 import cydoomgeneric as cdg
+import numpy as np
 
 from PyQt5.QtCore import Qt, QThread, pyqtSignal
-from PyQt5.QtGui import QImage, qRgb, QPainter
+from PyQt5.QtGui import QImage, qRgb, QPainter, QKeyEvent, QPaintEvent
 from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox
 
 
@@ -43,7 +40,7 @@ sys.excepthook = log_uncaught_exceptions
 
 
 # SOURCE: https://github.com/gil9red/SimplePyScripts/blob/01558c1d6e01c88e93c0f1f82e3da0cd3a654a71/opencv__Color_Detection_Tool/main.py#L50-L89
-GRAY_COLOR_TABLE = [qRgb(i, i, i) for i in range(256)]
+GRAY_COLOR_TABLE: list[int] = [qRgb(i, i, i) for i in range(256)]
 
 
 def numpy_array_to_QImage(numpy_array: np.ndarray) -> QImage | None:
@@ -58,7 +55,6 @@ def numpy_array_to_QImage(numpy_array: np.ndarray) -> QImage | None:
             data,
             width,
             height,
-            # numpy_array.strides[0],
             QImage.Format_Indexed8,
         )
         img.setColorTable(GRAY_COLOR_TABLE)
@@ -70,7 +66,6 @@ def numpy_array_to_QImage(numpy_array: np.ndarray) -> QImage | None:
                 data,
                 width,
                 height,
-                # numpy_array.strides[0],
                 QImage.Format_RGB888,
             )
             return img
@@ -80,35 +75,25 @@ def numpy_array_to_QImage(numpy_array: np.ndarray) -> QImage | None:
                 data,
                 width,
                 height,
-                # numpy_array.strides[0],
                 QImage.Format_ARGB32,
             )
             return img
-
-
-# TODO:
-keymap = {
-    Qt.Key_Left: cdg.Keys.LEFTARROW,
-    Qt.Key_Right: cdg.Keys.RIGHTARROW,
-    Qt.Key_Up: cdg.Keys.UPARROW,
-    Qt.Key_Down: cdg.Keys.DOWNARROW,
-    pygame.K_COMMA: cdg.Keys.STRAFE_L,# TODO:
-    pygame.K_PERIOD: cdg.Keys.STRAFE_R,# TODO:
-    pygame.K_LCTRL: cdg.Keys.FIRE,# TODO:
-    Qt.Key_Space: cdg.Keys.USE,
-    pygame.K_RSHIFT: cdg.Keys.RSHIFT,# TODO:
-    Qt.Key_Return: cdg.Keys.ENTER,
-    # Qt.Key_Escape: cdg.Keys.ESCAPE,
-}
 
 
 class CyDoomGenericThread(QThread):
     about_draw_frame = pyqtSignal(np.ndarray)
     about_set_window_title = pyqtSignal(str)
 
-    def __init__(self, width: int, height: int, queue: Queue):
+    def __init__(
+        self,
+        path_wad: str,
+        width: int,
+        height: int,
+        queue: Queue[tuple[int, int]],
+    ):
         super().__init__()
 
+        self.path_wad = path_wad
         self.width = width
         self.height = height
         self.queue = queue
@@ -131,26 +116,27 @@ class CyDoomGenericThread(QThread):
             get_key=self.get_key,
             set_window_title=self.about_set_window_title.emit,
         )
-        cdg.main(argv=["cydoomgeneric", "-iwad", "DOOM1.WAD"])
+        cdg.main(argv=["cydoomgeneric", "-iwad", self.path_wad])
 
 
 class WidgetDoom(QWidget):
-    def __init__(self) -> None:
+    def __init__(self, path_wad: str):
         super().__init__()
 
         self._resx = 640
         self._resy = 400
         self.q = Queue()
 
-        self.thread_engine = CyDoomGenericThread(self._resx, self._resy, self.q)
+        self.thread_engine = CyDoomGenericThread(
+            path_wad, self._resx, self._resy, self.q
+        )
         self.thread_engine.about_draw_frame.connect(self.draw_frame)
         self.thread_engine.about_set_window_title.connect(self.setWindowTitle)
         self.thread_engine.start()
 
         self.img: QImage | None = None
 
-        # self.setFixedSize(self._resx, self._resy)
-        self.resize(self._resx, self._resy)
+        self.setFixedSize(self._resx, self._resy)
 
     def draw_frame(self, pixels: np.ndarray):
         self.img = numpy_array_to_QImage(pixels)
@@ -159,25 +145,8 @@ class WidgetDoom(QWidget):
         self.img = QImage("frame.jpg")
 
         self.update()
-        # QApplication.processEvents()
 
-    # def get_key(self) -> tuple[int, int] | None:
-    #     # for event in pygame.event.get():
-    #     #     if event.type == pygame.QUIT:
-    #     #         sys.exit()
-    #     #
-    #     #     if event.type == pygame.KEYDOWN:
-    #     #         if event.key in keymap:
-    #     #             return 1, keymap[event.key]
-    #     #
-    #     #     if event.type == pygame.KEYUP:
-    #     #         if event.key in keymap:
-    #     #             return 0, keymap[event.key]
-    #     #
-    #     return None
-
-    # TODO:
-    def _get_key(self, event):
+    def _get_key(self, event: QKeyEvent):
         if event.modifiers() & Qt.ControlModifier:
             return cdg.Keys.RCTRL
 
@@ -186,55 +155,52 @@ class WidgetDoom(QWidget):
 
         if event.modifiers() & Qt.AltModifier:
             return cdg.Keys.LALT
-            # TODO:
-            # return cdg.Keys.RALT
 
         match event.key():
-            case Qt.Key_W: return cdg.Keys.UPARROW
-            case Qt.Key_A: return cdg.Keys.LEFTARROW
-            case Qt.Key_D: return cdg.Keys.RIGHTARROW
-            case Qt.Key_S: return cdg.Keys.DOWNARROW
-            case Qt.Key_E: return cdg.Keys.USE
-            case Qt.Key_Space: return cdg.Keys.FIRE
-            case Qt.Key_Return: return cdg.Keys.ENTER
-        """
-        pygame.K_COMMA: cdg.Keys.STRAFE_L,  # TODO:
-        pygame.K_PERIOD: cdg.Keys.STRAFE_R,  # TODO:
-        """
+            case Qt.Key_W | Qt.Key_Up:
+                return cdg.Keys.UPARROW
+            case Qt.Key_A | Qt.Key_Left:
+                return cdg.Keys.LEFTARROW
+            case Qt.Key_D | Qt.Key_Right:
+                return cdg.Keys.RIGHTARROW
+            case Qt.Key_S | Qt.Key_Down:
+                return cdg.Keys.DOWNARROW
+            case Qt.Key_Q:
+                return cdg.Keys.STRAFE_L
+            case Qt.Key_E:
+                return cdg.Keys.STRAFE_R
+            case Qt.Key_F:
+                return cdg.Keys.USE
+            case Qt.Key_Space:
+                return cdg.Keys.FIRE
+            case Qt.Key_Return:
+                return cdg.Keys.ENTER
 
-    # TODO:
-    def keyPressEvent(self, event):
+    def keyPressEvent(self, event: QKeyEvent):
         if event.key() == Qt.Key_Escape:
             self.close()
 
         self.q.put((1, self._get_key(event)))
 
-        # super().keyPressEvent(event)
-
-    # TODO:
-    def keyReleaseEvent(self, event):
+    def keyReleaseEvent(self, event: QKeyEvent):
         self.q.put((0, self._get_key(event)))
 
-        # super().keyReleaseEvent(event)
-
-    # def set_window_title(self, t: str) -> None:
-    #     self.setWindowTitle(t)
-
-    # TODO:
-    def paintEvent(self, event):
+    def paintEvent(self, event: QPaintEvent):
         if not self.img:
             return
 
         p = QPainter(self)
         p.drawImage(0, 0, self.img)
-        # print(self.img)
-        # self.img.save("frame.jpg")
 
 
 if __name__ == "__main__":
+    from pathlib import Path
+
+    path_wad = str(Path(__file__).parent.resolve() / "DOOM1.WAD")
+
     app = QApplication([])
 
-    g = WidgetDoom()
+    g = WidgetDoom(path_wad=path_wad)
     g.show()
 
     app.exec()
