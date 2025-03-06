90dff98d0cc59599f14fc1a1df25fdf3b173d84b
gil9red <ilya.petrash@inbox.ru>
2025-03-06 22:58:18 +0500

    Updated. eval_expr_total_time.py

--- eval_expr_total_time.py
+++ eval_expr_total_time.py
@@ -12,6 +12,8 @@ PATTERN_TIME: re.Pattern = re.compile(r"(\d\d:\d\d:\d\d)")
 PATTERN_EXPR_WITH_TIME: re.Pattern = re.compile(
     f"^{PATTERN_TIME.pattern}(?: *[+-] *{PATTERN_TIME.pattern})*$"
 )
+PATTERN_COMMENT: re.Pattern = re.compile("#.+$")
+PATTERN_IGNORE_CHARS: re.Pattern = re.compile(r"[^\d+-:]+")
 
 
 def get_seconds(hh_mm_ss: str) -> int:
@@ -23,14 +25,23 @@ def preprocess_expr_with_time(text: str) -> str:
     return PATTERN_TIME.sub(lambda m: str(get_seconds(m[1])), text)
 
 
-def eval_expr_with_time(text: str) -> str:
-    text: str = re.sub(r"[^\d+-:]", "", text)
+def preprocess_text(text: str) -> str:
+    lines: list[str] = []
+    for line in text.splitlines():
+        line = PATTERN_COMMENT.sub("", line)
+        line = PATTERN_IGNORE_CHARS.sub("", line)
+        lines.append(line)
 
+    return "".join(lines)
+
+
+def eval_expr_with_time(text: str) -> str:
+    text: str = preprocess_text(text)
     if not PATTERN_EXPR_WITH_TIME.match(text):
         raise Exception(f"Expression {text!r} not valid!")
 
-    text = preprocess_expr_with_time(text)
-    total_seconds = eval(text)
+    text: str = preprocess_expr_with_time(text)
+    total_seconds: int = eval(text)
     return seconds_to_str(total_seconds)
 
 
@@ -38,6 +49,16 @@ if __name__ == "__main__":
     text = """
     08:53:11 - 07:15:00
     + 08:56:12
+    """
+    result: str = eval_expr_with_time(text)
+    print(result)
+    assert result == "10:34:23"
+
+    text = """
+    # This is comment
+    08:53:11 - 07:15:00
+    # Day 2
+    + 08:56:12  # Comment 2
     """
     result: str = eval_expr_with_time(text)
     print(result)
