e7091398fa4374bf84546e417fddfd261de8b018
gil9red <ilya.petrash@inbox.ru>
2025-03-15 12:44:41 +0500

    Bugfix. html_parsing/pillow__handbook__image_file_formats.py

--- html_parsing/pillow__handbook__image_file_formats.py
+++ html_parsing/pillow__handbook__image_file_formats.py
@@ -4,17 +4,12 @@
 __author__ = "ipetrash"
 
 
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
 
-def get_formats(css_selector: str) -> list:
-    formats_els = (
-        root.select_one(css_selector)
-        .find_next_sibling("ul")
-        .find_all("li", recursive=False)
-    )
-    return [li.a.get_text(strip=True) for li in formats_els]
+def get_formats(css_selector: str) -> list[str]:
+    return [el["id"] for el in root.select(f"{css_selector} > section[id]")]
 
 
 rs = requests.get(
@@ -22,17 +17,18 @@ rs = requests.get(
 )
 root = BeautifulSoup(rs.content, "html.parser")
 
-fully_supported_formats = get_formats('#contents a[href="#fully-supported-formats"]')
+fully_supported_formats = get_formats("#fully-supported-formats")
 print(fully_supported_formats)
-# ['BMP', 'DIB', 'EPS', 'GIF', 'ICNS', 'ICO', 'IM', 'JPEG', 'JPEG 2000',
-# 'MSP', 'PCX', 'PNG', 'PPM', 'SGI', 'SPIDER', 'TGA', 'TIFF', 'WebP', 'XBM']
+# ['blp', 'bmp', 'dds', 'dib', 'eps', 'gif', 'icns', 'ico', 'im', 'jpeg',
+# 'jpeg-2000', 'mpo', 'msp', 'pcx', 'pfm', 'png', 'ppm', 'sgi', 'spider',
+# 'tga', 'tiff', 'webp', 'xbm']
 
-read_only_formats = get_formats('#contents a[href="#read-only-formats"]')
+read_only_formats = get_formats("#read-only-formats")
 print(read_only_formats)
-# ['BLP', 'CUR', 'DCX', 'DDS', 'FLI, FLC', 'FPX', 'FTEX', 'GBR', 'GD',
-# 'IMT', 'IPTC/NAA', 'MCIDAS', 'MIC', 'MPO', 'PCD', 'PIXAR', 'PSD', 'WAL',
-# 'WMF', 'XPM']
+# ['cur', 'dcx', 'fits', 'fli-flc', 'fpx', 'ftex', 'gbr', 'gd', 'imt',
+# 'iptc-naa', 'mcidas', 'mic', 'pcd', 'pixar', 'psd', 'qoi', 'sun', 'wal',
+# 'wmf-emf', 'xpm']
 
-write_only_formats = get_formats('#contents a[href="#write-only-formats"]')
+write_only_formats = get_formats("#write-only-formats")
 print(write_only_formats)
-# ['PALM', 'PDF', 'XV Thumbnails']
+# ['palm', 'pdf', 'xv-thumbnails']
