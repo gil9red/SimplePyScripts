959054d66a0002276e291ac34278971960a80247
gil9red <ilya.petrash@inbox.ru>
2025-03-15 12:45:42 +0500

    Refactoring. html_parsing/pillow__handbook__image_file_formats.py

--- html_parsing/pillow__handbook__image_file_formats.py
+++ html_parsing/pillow__handbook__image_file_formats.py
@@ -17,18 +17,18 @@ rs = requests.get(
 )
 root = BeautifulSoup(rs.content, "html.parser")
 
-fully_supported_formats = get_formats("#fully-supported-formats")
+fully_supported_formats: list[str] = get_formats("#fully-supported-formats")
 print(fully_supported_formats)
 # ['blp', 'bmp', 'dds', 'dib', 'eps', 'gif', 'icns', 'ico', 'im', 'jpeg',
 # 'jpeg-2000', 'mpo', 'msp', 'pcx', 'pfm', 'png', 'ppm', 'sgi', 'spider',
 # 'tga', 'tiff', 'webp', 'xbm']
 
-read_only_formats = get_formats("#read-only-formats")
+read_only_formats: list[str] = get_formats("#read-only-formats")
 print(read_only_formats)
 # ['cur', 'dcx', 'fits', 'fli-flc', 'fpx', 'ftex', 'gbr', 'gd', 'imt',
 # 'iptc-naa', 'mcidas', 'mic', 'pcd', 'pixar', 'psd', 'qoi', 'sun', 'wal',
 # 'wmf-emf', 'xpm']
 
-write_only_formats = get_formats("#write-only-formats")
+write_only_formats: list[str] = get_formats("#write-only-formats")
 print(write_only_formats)
 # ['palm', 'pdf', 'xv-thumbnails']
