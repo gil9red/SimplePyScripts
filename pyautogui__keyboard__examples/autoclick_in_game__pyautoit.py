cb1fed6716b2dfb836288c5d57e28ffec674d901
gil9red <ilya.petrash@inbox.ru>
2025-03-23 19:03:35 +0500

    Updated. pyautogui__keyboard__examples/autoclick_in_game__pyautoit.py

--- pyautogui__keyboard__examples/autoclick_in_game__pyautoit.py
+++ pyautogui__keyboard__examples/autoclick_in_game__pyautoit.py
@@ -4,12 +4,14 @@
 __author__ = "ipetrash"
 
 
-import threading
 import time
+import os
+from threading import Thread
 
-# pip install PyAutoIt
+# pip install PyAutoIt==0.6.5
 import autoit
 
+# pip install keyboard==0.13.5
 import keyboard
 
 
@@ -17,10 +19,12 @@ DATA = {
     "START": False,
 }
 
+HOTKEY: str = os.environ.get("HOTKEY", "Ctrl+Alt+Space")
+
 
 def change_start():
     DATA["START"] = not DATA["START"]
-    print(DATA["START"])
+    print("START:", DATA["START"])
 
 
 def process_auto_click():
@@ -36,12 +40,14 @@ def process_auto_click():
         time.sleep(0.070)
 
 
-keyboard.add_hotkey("Ctrl+Alt+Space", change_start)
+keyboard.add_hotkey(HOTKEY, change_start)
 
 # Запуск потока для автоатаки
-thread_auto_click = threading.Thread(target=process_auto_click)
+thread_auto_click = Thread(target=process_auto_click)
 thread_auto_click.start()
 
+
+print(f"Press {HOTKEY} for starting/stopping")
 
 while True:
     if not DATA["START"]:
