d82739bde291a358e6ac05f0827f26d1c0d55ad2
gil9red <ilya.petrash@inbox.ru>
2025-04-13 01:10:50 +0500

    Refactoring. pil_pillow__examples/rounded_rectangle.py

--- pil_pillow__examples/rounded_rectangle.py
+++ pil_pillow__examples/rounded_rectangle.py
@@ -13,17 +13,23 @@ __author__ = "ipetrash"
 from PIL import Image, ImageDraw
 
 
-def round_corner(radius, fill):
+Color = float | tuple[int, ...] | str
+
+
+def round_corner(radius: int, fill: Color) -> Image:
     """Draw a round corner"""
-    corner = Image.new("RGB", (radius, radius), (0, 0, 0, 0))
+    corner = Image.new("RGBA", (radius, radius), (0, 0, 0, 0))
+
     draw = ImageDraw.Draw(corner)
     draw.pieslice((0, 0, radius * 2, radius * 2), 180, 270, fill=fill)
+
     return corner
 
 
-def round_rectangle(size, radius, fill):
+def round_rectangle(size: tuple[int, int], radius: int, fill: Color) -> Image:
     """Draw a rounded rectangle"""
     width, height = size
+
     rectangle = Image.new("RGBA", size, fill)
     corner = round_corner(radius, fill)
 
@@ -34,9 +40,11 @@ def round_rectangle(size, radius, fill):
 
     rectangle.paste(corner.rotate(180), (width - radius, height - radius))
     rectangle.paste(corner.rotate(270), (width - radius, 0))
+
     return rectangle
 
 
-img = round_rectangle((500, 500), 50, "yellow")
-img.show()
-img.save("img.png")
+if __name__ == "__main__":
+    img = round_rectangle((500, 500), 50, "yellow")
+    img.show()
+    img.save("img.png")
