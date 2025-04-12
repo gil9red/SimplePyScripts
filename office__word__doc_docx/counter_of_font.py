52760e9b6823385f108fb21c209dc4dbaacc7059
gil9red <ilya.petrash@inbox.ru>
2025-04-13 01:02:41 +0500

    Refactoring. office__word__doc_docx/counter_of_font.py

--- office__word__doc_docx/counter_of_font.py
+++ office__word__doc_docx/counter_of_font.py
@@ -4,14 +4,20 @@
 __author__ = "ipetrash"
 
 
+from collections import Counter
 from pathlib import Path
 
+# pip install python-docx==1.1.2
 import docx
 from docx.text.run import Run
 from docx.text.paragraph import Paragraph
 
 
-def get_font_name_and_size(doc: docx.Document, p: Paragraph, r: Run) -> tuple[str | None, int | None]:
+def get_font_name_and_size(
+    doc: docx.Document,
+    p: Paragraph,
+    r: Run,
+) -> tuple[str | None, int | None]:
     def _get_normal_size(size: int | None) -> int | None:
         return size // 12700 if size else None
 
@@ -32,7 +38,6 @@ def get_font_name_and_size(doc: docx.Document, p: Paragraph, r: Run) -> tuple[st
         except:
             pass
 
-    # TODO:
     if not font_name:
         try:
             font_name = doc.styles["Normal"].font.name
@@ -40,56 +45,58 @@ def get_font_name_and_size(doc: docx.Document, p: Paragraph, r: Run) -> tuple[st
             pass
 
     try:
-        font_size = _get_normal_size(r.font.size)
+        font_size = r.font.size
     except:
         font_size = None
 
     if not font_size:
         try:
-            font_size = _get_normal_size(r.style.font.size)
+            font_size = r.style.font.size
         except:
             pass
 
     if not font_size:
         try:
-            font_size = _get_normal_size(p.style.font.size)
+            font_size = p.style.font.size
         except:
             pass
 
-    # TODO:
     if not font_size:
         try:
-            font_size = _get_normal_size(doc.styles["Normal"].font.size)
+            font_size = doc.styles["Normal"].font.size
         except:
             pass
 
-    return font_name, font_size
+    return font_name, _get_normal_size(font_size)
 
 
-dir = Path(__file__).parent / "fill_template"
-for file_name in dir.glob("*.docx"):
-    print(file_name)
+def process(path: Path):
+    for file_name in path.glob("*.docx"):
+        print(file_name)
 
-    try:
-        doc_file = docx.Document(file_name)
+        try:
+            doc_file = docx.Document(file_name)
 
-        name_and_size = []
-        names = []
-        sizes = []
+            name_and_size = []
+            names = []
+            sizes = []
 
-        for p in doc_file.paragraphs:
-            for r in p.runs:
-                name, size = get_font_name_and_size(doc_file, p, r)
-                name_and_size.append((name, size))
-                names.append(name)
-                sizes.append(size)
+            for p in doc_file.paragraphs:
+                for r in p.runs:
+                    name, size = get_font_name_and_size(doc_file, p, r)
+                    name_and_size.append((name, size))
+                    names.append(name)
+                    sizes.append(size)
 
-        from collections import Counter
+            for (name, size), number in Counter(name_and_size).most_common():
+                print((name, size), number)
 
-        for (name, size), number in Counter(name_and_size).most_common():
-            print((name, size), number)
+        except Exception as e:
+            print(e)
 
-    except Exception as e:
-        print(e)
+        print()
 
-    print()
+
+if __name__ == "__main__":
+    path = Path(__file__).parent / "fill_template"
+    process(path)
