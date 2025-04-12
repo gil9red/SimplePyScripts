6adac82055553e9f0343977952d859086860138c
gil9red <ilya.petrash@inbox.ru>
2025-04-13 02:04:52 +0500

    Refactoring. string_formatting/string_formatting.py

--- string_formatting/string_formatting.py
+++ string_formatting/string_formatting.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 __author__ = "ipetrash"
 
 
-if __name__ == "__main__":
-    # TODO: побольше примеров форматирования строк.
-    # https://docs.python.org/3/library/string.html#string-formatting
-    # http://pythonworld.ru/osnovy/formatirovanie-strok-metod-format.html
-    # http://pythonworld.ru/osnovy/formatirovanie-strok-operator.html
-    print("{one} * {one} {two}".format(one="45", two="Bugaga"))
+# TODO: побольше примеров форматирования строк.
+# https://docs.python.org/3/library/string.html#string-formatting
+# http://pythonworld.ru/osnovy/formatirovanie-strok-metod-format.html
+# http://pythonworld.ru/osnovy/formatirovanie-strok-operator.html
+print("{one} * {one} {two}".format(one="45", two="Bugaga"))
