eff64261f0b1b90f85e512730483cb42b73f0da9
gil9red <ilya.petrash@inbox.ru>
2025-04-13 01:24:13 +0500

    Refactoring. pretty_xml/pretty_xml.py

--- pretty_xml/pretty_xml.py
+++ pretty_xml/pretty_xml.py
@@ -8,7 +8,7 @@ from xml.dom.minidom import parseString
 from lxml import etree
 
 
-def process_xml_string(xml_string):
+def process_xml_string(xml_string: str) -> str:
     """
     Функция из текста выдирает строку с xml --
     она должна начинаться на < и заканчиваться >
@@ -20,20 +20,27 @@ def process_xml_string(xml_string):
     return xml_string[start : end + 1]
 
 
-def pretty_xml_minidom(xml_string, ind=2):
+def pretty_xml_minidom(xml_string: str, indent: int = 4) -> str:
     """Функция принимает строку xml и выводит xml с отступами."""
 
     xml_string = process_xml_string(xml_string)
-    xml_utf8 = parseString(xml_string).toprettyxml(indent=" " * ind, encoding="utf-8")
+    xml_utf8 = parseString(xml_string).toprettyxml(
+        indent=" " * indent,
+        encoding="utf-8",
+    )
     return xml_utf8.decode("utf-8")
 
 
-def pretty_xml_lxml(xml_string):
+def pretty_xml_lxml(xml_string: str) -> str:
     """Функция принимает строку xml и выводит xml с отступами."""
 
     xml_string = process_xml_string(xml_string)
     root = etree.fromstring(xml_string)
-    return etree.tostring(root, pretty_print=True, encoding="utf-8").decode("utf-8")
+    return etree.tostring(
+        root,
+        pretty_print=True,
+        encoding="utf-8",
+    ).decode("utf-8")
 
 
 if __name__ == "__main__":
