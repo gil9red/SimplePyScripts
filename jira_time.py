173729b2ef3167e1eeefeffa176a3272028a9411
gil9red <ilya.petrash@inbox.ru>
2025-04-12 23:43:32 +0500

    Refactoring. jira_time.py

--- jira_time.py
+++ jira_time.py
@@ -4,80 +4,78 @@
 __author__ = "ipetrash"
 
 
-import time
 import re
 
 from collections import defaultdict
-from datetime import datetime as dt
+from datetime import datetime
 
 
-def parser_my_jira_time_logs(log):
-    """
-    Функция принимает список строк вида:
-    7417 10:00-12:00
-    7417 12:19-14:00
-    7417 14:37-15:30
-    7417 15:58-17:50
+def seconds_to_jira_str(seconds: int) -> str:
+    hours, minutes = divmod(seconds, 3600)
+    minutes //= 60
+    days, hours = divmod(hours, 8)
+    weeks, days = divmod(days, 5)
 
-    7415 15:58-15:59
+    items = [
+        f"{weeks}w" if weeks else "",
+        f"{days}d" if days else "",
+        f"{hours}h" if hours else "",
+        f"{minutes}m" if minutes else "",
+    ]
+    return " ".join(filter(None, items))
 
-    7456 14:28-15:59
 
-    То, что перед ' ' -- уникальный номер задания
-    Диапазон после ' ' -- отрезок времени вида: начало - конец
+def str_to_time(s: str) -> datetime:
+    return datetime.strptime(s, "%H:%M")
 
-    Далее функция подсчитает количество часов и минут для каждого задания
-    и выведет их
-    """
 
-    # TODO: Защита от копипаста: строки могут повторяться и время подсчитается неправильно
-    # TODO: Если часы перевалят за 24, то начнется отсчет заного
-    # TODO: Для джиры дни и недели не астрономические: 1d = 8h и 1w = 5d
-
+def parser_my_jira_time_logs(log: str) -> str:
     pattern = re.compile(r"(.+) (\d\d:\d\d)-(\d\d:\d\d)")
 
-    jira_time = defaultdict(int)
+    jira_by_seconds: dict[str, int] = defaultdict(int)
 
-    for line in log.split("\n"):
+    for line in log.splitlines():
         line = line.strip()
-        if not line:
+        if not line or line.startswith("#"):
             continue
 
         m = pattern.match(line)
 
-        jira = m.group(1)
-        t1 = m.group(2)
-        t2 = m.group(3)
-        delta = dt.strptime(t2, "%H:%M") - dt.strptime(t1, "%H:%M")
-        seconds = delta.seconds
+        jira: str = m.group(1)
+        t1: str = m.group(2)
+        t2: str = m.group(3)
+        seconds: int = (str_to_time(t2) - str_to_time(t1)).seconds
 
-        jira_time[jira] += seconds
+        jira_by_seconds[jira] += seconds
 
-    for jira, secs in jira_time.items():
-        t = time.gmtime(secs)
-        h = t.tm_hour
-        m = t.tm_min
-        jira_time = None
-        if h:
-            jira_time = str(h) + "h"
-        if m:
-            if jira_time:
-                jira_time += " " + str(m) + "m"
-            else:
-                jira_time = str(m) + "m"
-
-        print("%s: %s" % (jira, jira_time))
+    return "\n".join(
+        f"{jira}: {seconds_to_jira_str(seconds)}"
+        for jira, seconds in jira_by_seconds.items()
+    )
 
 
-parser_my_jira_time_logs(
-    """
-    7417 10:00-12:00
-    7417 12:19-14:00
-    7417 14:37-15:30
-    7417 15:58-17:50
+if __name__ == "__main__":
+    print(
+        parser_my_jira_time_logs(
+            """
+            # Foo
+            7417 10:00-18:00
     
-    7415 15:58-15:59
+            # Bar
+            7417 10:00-12:00
+            7417 12:19-14:00
+            7417 14:37-15:30
+            7417 15:58-17:50
     
-    7456 14:28-15:59
+            7415 15:58-15:59
+            7456 14:28-15:59
+            7425 10:00-18:10
+            """
+        )
+    )
     """
-)
+    7417: 1d 6h 26m
+    7415: 1m
+    7456: 1h 31m
+    77425: 1d 10m
+    """
