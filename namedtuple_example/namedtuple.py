1a6eb8ad37b993713cb6b4c1ac11279522a32aa9
gil9red <ilya.petrash@inbox.ru>
2025-04-13 00:29:39 +0500

    Refactoring. namedtuple_example/namedtuple.py

--- namedtuple_example/namedtuple.py
+++ namedtuple_example/namedtuple.py
@@ -4,20 +4,16 @@
 __author__ = "ipetrash"
 
 
-# TODO: https://docs.python.org/3/library/collections.html#collections.namedtuple_example
-# Basic example
-
-
 from collections import namedtuple
 
 
 Point = namedtuple("Point", ["x", "y"])
 p = Point(11, y=22)  # instantiate with positional or keyword arguments
-print(p[0] + p[1])   # indexable like the plain tuple (11, 22)
-x, y = p             # unpack like a regular tuple
+print(p[0] + p[1])  # indexable like the plain tuple (11, 22)
+x, y = p  # unpack like a regular tuple
 print(x, y)
-print(p.x + p.y)     # fields also accessible by name
-print(p)             # readable __repr__ with a name=value style
+print(p.x + p.y)  # fields also accessible by name
+print(p)  # readable __repr__ with a name=value style
 print()
 
 XYZPoint = namedtuple("XYZPoint", ["x", "y", "z"])
