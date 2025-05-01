1081c5d57de6098802bd13126fddf8d264f3e2b6
gil9red <ilya.petrash@inbox.ru>
2025-05-01 14:01:32 +0500

    Refactoring. for_games/Dark Souls/backup__save_files

--- for_games/Dark Souls/backup__save_files/by_autosave/common.py
+++ for_games/Dark Souls/backup__save_files/by_autosave/common.py
@@ -21,7 +21,7 @@ from utils import get_logger, backup
 log = get_logger(__file__)
 
 
-def backup_saves(path_ds_save: str, forced: bool = False, modified_secs: int = 600):
+def backup_saves(path_ds_save: str, forced: bool = False, modified_minutes: int = 5):
     try:
         for path_file_name in glob(path_ds_save):
             log.debug(f"Check: {path_file_name}")
@@ -30,12 +30,12 @@ def backup_saves(path_ds_save: str, forced: bool = False, modified_secs: int = 6
             save_timestamp = os.path.getmtime(path_file_name)
             now_timestamp = time.time()
 
-            # Save backup. If less than modified_secs secs have passed since the last modification of the file
-            is_modified = (now_timestamp - save_timestamp) < modified_secs
+            # Save backup. If less than modified_minutes have passed since the last modification of the file
+            is_modified = (now_timestamp - save_timestamp) < (modified_minutes * 60)
             ok = forced or is_modified
             log.debug(
                 f"{'Need backup' if ok else 'Not need backup'}. "
-                f"Reason: Forced={forced}, Is modified file save={is_modified}"
+                f"Reason: Forced={forced}, is modified file save={is_modified}"
             )
             if not ok:
                 continue
@@ -60,4 +60,4 @@ def run(path_ds_save: str, timeout_minutes: int = 5):
         print(f"\nSleeping for {timeout_minutes} minutes\n")
         time.sleep(timeout_minutes * 60)
 
-        backup_saves(path_ds_save, forced=False, modified_secs=timeout_minutes * 60)
+        backup_saves(path_ds_save, forced=False, modified_minutes=timeout_minutes)
