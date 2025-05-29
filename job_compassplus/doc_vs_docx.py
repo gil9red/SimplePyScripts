846bcd399f6e07571396699511e63bb9645b690b
gil9red <ilya.petrash@inbox.ru>
2025-05-29 12:49:48 +0500

    Added "job_compassplus/doc_vs_docx.py"

---
+++ job_compassplus/doc_vs_docx.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+from pathlib import Path
+
+# pip install python-docx
+from docx import Document
+
+
+PATH_DIR: str = r"C:\DOC\Specifications\Interfaces"
+
+
+docx_num = 0
+doc_num = 0
+for f in Path(PATH_DIR).glob("*.doc*"):
+    if f.name.startswith("~"):
+        continue
+
+    try:
+        Document(f)
+        ok = True
+        docx_num += 1
+    except:
+        ok = False
+        doc_num += 1
+
+    print(f"[{'+' if ok else '-'}]", f)
+
+print()
+print("Total:", docx_num + doc_num)
+print("Docx:", docx_num)
+print("Doc:", doc_num)
