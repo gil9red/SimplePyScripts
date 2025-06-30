910873cc64b42f31f23e2148e30d2010abdf97c5
gil9red <ilya.petrash@inbox.ru>
2025-06-30 23:36:09 +0500

    Added. pytz__examples/utc_plus_tz.py

---
+++ pytz__examples/utc_plus_tz.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+from datetime import datetime
+
+# pip install pytz==2025.2
+import pytz
+
+
+tz_moscow = pytz.timezone("Europe/Moscow")
+tz_etc_0300 = pytz.timezone("Etc/GMT-3")
+tz_offset_0300 = pytz.FixedOffset(offset=3 * 60)
+tz_utc = pytz.timezone("UTC")
+
+now = datetime(year=2025, month=1, day=1, hour=12, minute=0, second=0)
+print(now)
+# 2025-01-01 12:00:00
+
+print()
+
+print(tz_moscow.fromutc(now))
+# 2025-01-01 15:00:00+03:00
+
+print(tz_etc_0300.fromutc(now))
+# 2025-01-01 15:00:00+03:00
+
+print(tz_offset_0300.fromutc(tz_offset_0300.localize(now)))
+# 2025-01-01 15:00:00+03:00
+
+print(tz_utc.fromutc(tz_utc.localize(now)))
+# 2025-01-01 12:00:00+00:00
