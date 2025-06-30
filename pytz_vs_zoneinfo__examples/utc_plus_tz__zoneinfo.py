9d621dadc945311a87fb931342779aaf5b08c7d4
gil9red <ilya.petrash@inbox.ru>
2025-07-01 01:02:40 +0500

    Refactoring. pytz_vs_zoneinfo__examples/utc_plus_tz__zoneinfo.py

--- pytz_vs_zoneinfo__examples/utc_plus_tz__zoneinfo.py
+++ pytz_vs_zoneinfo__examples/utc_plus_tz__zoneinfo.py
@@ -13,23 +13,6 @@ tz_etc_0300 = zoneinfo.ZoneInfo("Etc/GMT-3")
 tz_offset_0300 = timezone(offset=timedelta(minutes=3 * 60))
 tz_utc = zoneinfo.ZoneInfo("UTC")
 
-dt = datetime(year=2025, month=1, day=1, hour=12, minute=0, second=0)
-print(dt)
-# 2025-01-01 12:00:00
-
-print()
-
-print(tz_moscow.fromutc(dt.replace(tzinfo=tz_moscow)))
-print(tz_etc_0300.fromutc(dt.replace(tzinfo=tz_etc_0300)))
-print(tz_offset_0300.fromutc(dt.replace(tzinfo=tz_offset_0300)))
-print(tz_utc.fromutc(dt.replace(tzinfo=tz_utc)))
-# 2025-01-01 15:00:00+03:00
-# 2025-01-01 15:00:00+03:00
-# 2025-01-01 15:00:00+03:00
-# 2025-01-01 12:00:00+00:00
-
-print("\n" + "-" * 10 + "\n")
-
 dt_utc = datetime(year=2025, month=1, day=1, hour=12, minute=0, second=0, tzinfo=tz_utc)
 print(dt_utc)
 # 2025-01-01 12:00:00+00:00
@@ -42,6 +25,23 @@ print(dt_utc.astimezone(tz_moscow))
 print(dt_utc.astimezone(tz_etc_0300))
 print(dt_utc.astimezone(tz_offset_0300))
 print(dt_utc.astimezone(tz_utc))
+# 2025-01-01 15:00:00+03:00
+# 2025-01-01 15:00:00+03:00
+# 2025-01-01 15:00:00+03:00
+# 2025-01-01 12:00:00+00:00
+
+print("\n" + "-" * 10 + "\n")
+
+dt = datetime(year=2025, month=1, day=1, hour=12, minute=0, second=0)
+print(dt)
+# 2025-01-01 12:00:00
+
+print()
+
+print(tz_moscow.fromutc(dt.replace(tzinfo=tz_moscow)))
+print(tz_etc_0300.fromutc(dt.replace(tzinfo=tz_etc_0300)))
+print(tz_offset_0300.fromutc(dt.replace(tzinfo=tz_offset_0300)))
+print(tz_utc.fromutc(dt.replace(tzinfo=tz_utc)))
 # 2025-01-01 15:00:00+03:00
 # 2025-01-01 15:00:00+03:00
 # 2025-01-01 15:00:00+03:00
