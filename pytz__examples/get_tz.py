4554977c81f5a5d21cc5148f5c2ebca4b75cdc15
gil9red <ilya.petrash@inbox.ru>
2025-06-30 23:12:46 +0500

    Added. pytz__examples/get_tz.py

---
+++ pytz__examples/get_tz.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+# pip install pytz==2025.2
+import pytz
+
+
+tz_moscow = pytz.timezone("Europe/Moscow")
+print(tz_moscow)
+# Europe/Moscow
+
+tz_utc = pytz.timezone("UTC")
+print(tz_utc)
+# UTC
+
+try:
+    tz = pytz.timezone("INVALID")
+    raise Exception()
+except pytz.UnknownTimeZoneError as e:
+    print(e)
+# 'INVALID'
