821525bd2f77367f15e96f3425656533331fc19a
gil9red <ilya.petrash@inbox.ru>
2025-07-01 21:47:44 +0500

    Updated. "pytz_vs_zoneinfo__examples/"

--- pytz_vs_zoneinfo__examples/dt_with_tz_to_utc__pytz.py
+++ pytz_vs_zoneinfo__examples/dt_with_tz_to_utc__pytz.py
@@ -13,6 +13,7 @@ import pytz
 tz_moscow = pytz.timezone("Europe/Moscow")
 tz_etc_0300 = pytz.timezone("Etc/GMT-3")
 tz_offset_0300 = pytz.FixedOffset(offset=3 * 60)
+tz_offset_neg0300 = pytz.FixedOffset(offset=-3 * 60)
 tz_utc = pytz.timezone("UTC")
 
 dt = datetime(year=2025, month=1, day=1, hour=12, minute=0, second=0)
@@ -42,6 +43,14 @@ print(dt_offset_0300)
 print(dt_offset_0300.astimezone(tz_utc).replace(tzinfo=None))
 # 2025-01-01 12:00:00+03:00
 # 2025-01-01 09:00:00
+
+print()
+
+dt_offset_neg0300 = dt.replace(tzinfo=tz_offset_neg0300)
+print(dt_offset_neg0300)
+print(dt_offset_neg0300.astimezone(tz_utc).replace(tzinfo=None))
+# 2025-01-01 12:00:00-03:00
+# 2025-01-01 15:00:00
 
 print()
 
--- pytz_vs_zoneinfo__examples/dt_with_tz_to_utc__zoneinfo.py
+++ pytz_vs_zoneinfo__examples/dt_with_tz_to_utc__zoneinfo.py
@@ -11,6 +11,7 @@ from datetime import datetime, timedelta, timezone
 tz_moscow = zoneinfo.ZoneInfo("Europe/Moscow")
 tz_etc_0300 = zoneinfo.ZoneInfo("Etc/GMT-3")
 tz_offset_0300 = timezone(offset=timedelta(minutes=3 * 60))
+tz_offset_neg0300 = timezone(offset=timedelta(minutes=-3 * 60))
 tz_utc = zoneinfo.ZoneInfo("UTC")
 
 dt = datetime(year=2025, month=1, day=1, hour=12, minute=0, second=0)
@@ -40,6 +41,14 @@ print(dt_offset_0300)
 print(dt_offset_0300.astimezone(tz_utc).replace(tzinfo=None))
 # 2025-01-01 12:00:00+03:00
 # 2025-01-01 09:00:00
+
+print()
+
+dt_offset_neg0300 = dt.replace(tzinfo=tz_offset_neg0300)
+print(dt_offset_neg0300)
+print(dt_offset_neg0300.astimezone(tz_utc).replace(tzinfo=None))
+# 2025-01-01 12:00:00-03:00
+# 2025-01-01 15:00:00
 
 print()
 
