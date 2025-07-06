5a0d89b39d0787f4e1127fb1522b10306ef6ddad
gil9red <ilya.petrash@inbox.ru>
2025-07-06 23:16:31 +0500

    Refactoring. mock__examples/

--- mock__examples/datetime_now_utcnow.py
+++ mock__examples/datetime_now_utcnow.py
@@ -22,11 +22,18 @@ datetime_mock.utcnow.return_value = datetime.datetime(2000, 1, 1)
 
 test()
 
-with patch("_test_datetime.datetime", new=datetime_mock):
-    with patch("datetime.datetime", new=datetime_mock):
-        test()
-        # 1999-01-01 00:00:00 2000-01-01 00:00:00
+with (
+    patch("datetime.datetime", new=datetime_mock),
+    patch("_test_datetime.datetime", new=datetime_mock)
+):
+    test()
+    # 1999-01-01 00:00:00 2000-01-01 00:00:00
 
-        assert datetime.datetime.now() == datetime.datetime(1999, 1, 1)
+    assert datetime.datetime.now() == datetime.datetime(1999, 1, 1)
+    assert datetime.datetime.utcnow() == datetime.datetime(2000, 1, 1)
+
+    now_2, utcnow_2 = _test_datetime.get()
+    assert datetime.datetime.now() == now_2
+    assert datetime.datetime.utcnow() == utcnow_2
 
 test()
