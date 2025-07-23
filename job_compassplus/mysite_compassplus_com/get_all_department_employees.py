8b2505bca3b1df5fa8f184d1b4271eadadc868d9
gil9red <ilya.petrash@inbox.ru>
2025-07-23 14:09:56 +0500

    Refactoring. job_compassplus/mysite_compassplus_com/get_all_department_employees.py

--- job_compassplus/mysite_compassplus_com/get_all_department_employees.py
+++ job_compassplus/mysite_compassplus_com/get_all_department_employees.py
@@ -44,19 +44,26 @@ def get_employees(boss_username: str) -> list[Employee]:
 
 if __name__ == "__main__":
     from base64 import b64decode
-    boss_username = b64decode("Q1BcbnZheW5lcg==").decode("utf-8")
 
-    employees = get_employees(boss_username)
-    print(f"Employees ({len(employees)}):")
-    for i, employee in enumerate(employees, 1):
-        print(f"{i}. {employee}")
-    """
-    ...
-    9. Employee(full_name='Ilya A. Petrash', user_name='ipetrash', position='Senior Software Engineer', url='https://mysite.compassplus.com:443/Person.aspx?accountname=CP%5Cipetrash')
-    ...
-    """
-    print()
+    for boss_username in [
+        b64decode("Q1BcbnZheW5lcg==").decode("utf-8"),
+        b64decode("Q1BcYXZvc3RyaWtvdg==").decode("utf-8"),
+    ]:
+        print(f"Boss: {boss_username!r}")
 
-    usernames = sorted(x.user_name for x in employees)
-    print(usernames)
-    print(", ".join(usernames))
+        employees = get_employees(boss_username)
+        print(f"Employees ({len(employees)}):")
+        for i, employee in enumerate(employees, 1):
+            print(f"{i}. {employee}")
+        """
+        ...
+        9. Employee(full_name='Ilya A. Petrash', user_name='ipetrash', position='Senior Software Engineer', url='https://mysite.compassplus.com:443/Person.aspx?accountname=CP%5Cipetrash')
+        ...
+        """
+        print()
+
+        usernames = sorted(x.user_name for x in employees)
+        print(usernames)
+        print(", ".join(usernames))
+
+        print("\n" + "-" * 10 + "\n")
