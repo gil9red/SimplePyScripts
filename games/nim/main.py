ecade9d819380537282f7a3b4e53b4be127fc75f
gil9red <ilya.petrash@inbox.ru>
2025-09-03 11:31:37 +0500

    Refactoring. games/nim/main.py

--- games/nim/main.py
+++ games/nim/main.py
@@ -5,63 +5,107 @@ __author__ = "ipetrash"
 
 
 import random
+from abc import ABC, abstractmethod
+from typing import Optional
 
 
+# TODO:
 class FinishGameException(Exception):
     pass
 
 
-MIN_STONES: int = 1
-MAX_STONES: int = 3
-MAX_NUMBER: int = 21
+class Player(ABC):
+    def __init__(self, game: Optional["Game"] = None):  # TODO: Выглядит неуклюже
+        self.game: Game | None = game
 
-gamer_is_first: bool = (input("Ты первый? (y/n): ").lower() or "y") == "y"
-number: int = MAX_NUMBER
+    @abstractmethod
+    def make_move(self):
+        pass
 
 
-def player_move():
-    global number
+class UserPlayer(Player):
+    def make_move(self):
+        while True:
+            try:
+                stones = int(
+                    input(f"Камни от {self.game.MIN_STONES} до {self.game.MAX_STONES} (камней {self.game.number}): ")
+                )
+                assert self.game.MIN_STONES <= stones <= self.game.MAX_STONES
+                break
+            except (ValueError, AssertionError):
+                print("Неправильное значение!")
+                continue
 
-    while True:
+        self.game.number -= stones  # TODO: Мб пусть через игру будет логика - игрок будет говорить сколько камней возьмет, а игра сама решит кто победил
+        if self.game.number <= 0:
+            # TODO:
+            raise FinishGameException("Компьютер выиграл")
+
+
+class BotPlayer(Player):
+    def make_move(self):
+        # TODO: Когда остается минимум камней нужно вручную выбрать правильное, а не рандомно
+        #       А то ИИ выглядит как искусственный идиот
+        stones = min(self.game.number, random.randint(self.game.MIN_STONES, self.game.MAX_STONES))
+        print(f"Компьютер выбрал: {stones}")
+        self.game.number -= stones  # TODO: Мб пусть через игру будет логика - игрок будет говорить сколько камней возьмет, а игра сама решит кто победил
+        if self.game.number <= 0:
+            # TODO:
+            raise FinishGameException("Ты выиграл")
+
+
+class Game:
+    MIN_STONES: int = 1
+    MAX_STONES: int = 3
+    MAX_NUMBER: int = 21
+
+    def __init__(self, player1: Player, player2: Player):
+        self.player1: Player = player1
+        self.player1.game = self
+
+        self.player2: Player = player2
+        self.player2.game = self
+
+        self.number: int = self.MAX_NUMBER
+
+        self.is_finished: bool = False
+        self.player1_is_first: bool = True
+
+    # TODO: Методы начала игры
+    def start(self):
+        self.is_finished = False
+
+        # TODO:
+        self.player1_is_first = (input("Ты первый? (y/n): ").lower() or "y") == "y"
+
+    # TODO: Мб счетчик раундов вести и явно писать?
+    # TODO: Немного ASCII для удобного просмотра
+    def move(self) -> bool:
+        print(f"Камней: {self.number}")
+
         try:
-            stones = int(
-                input(f"Камни от {MIN_STONES} до {MAX_STONES} (камней {number}): ")
-            )
-            assert MIN_STONES <= stones <= MAX_STONES
-            break
-        except:
-            print("Неправильное значение!")
-            continue
+            if self.player1_is_first:
+                self.player1.make_move()
+                self.player2.make_move()
+            else:
+                self.player2.make_move()
+                self.player1.make_move()
 
-    number -= stones
-    if number <= 0:
-        raise FinishGameException("Компьютер выиграл")
+        except FinishGameException as e:
+            print(e)
+            self.is_finished = True
 
+        print()
 
-def computer_move():
-    global number
-
-    # TODO: Когда остается минимум камней нужно вручную выбрать правильное, а не рандомно
-    #       А то ИИ выглядит как искусственный идиот
-    stones = min(number, random.randint(MIN_STONES, MAX_STONES))
-    print(f"Компьютер выбрал: {stones}")
-    number -= stones
-    if number <= 0:
-        raise FinishGameException("Ты выиграл")
+        return not self.is_finished
 
 
-while number > 0:
-    print(f"Камней: {number}")
+if __name__ == '__main__':
+    game = Game(
+        player1=UserPlayer(),
+        player2=BotPlayer(),
+    )
+    game.start()
 
-    try:
-        if gamer_is_first:
-            player_move()
-            computer_move()
-        else:
-            computer_move()
-            player_move()
-
-    except FinishGameException as e:
-        print(e)
-
-    print()
+    while game.move():
+        pass
