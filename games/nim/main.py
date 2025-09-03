22614e0381b5bc93823bc0078c646de91a9d84d5
gil9red <ilya.petrash@inbox.ru>
2025-09-03 18:12:07 +0500

    Refactoring. games/nim/main.py

--- games/nim/main.py
+++ games/nim/main.py
@@ -5,7 +5,9 @@ __author__ = "ipetrash"
 
 
 import random
+
 from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
 from typing import Optional
 
 
@@ -14,21 +16,38 @@ class FinishGameException(Exception):
     pass
 
 
+@dataclass
 class Player(ABC):
-    def __init__(self, game: Optional["Game"] = None):  # TODO: Выглядит неуклюже
-        self.game: Game | None = game
+    name: str
+    game: Optional["Game"] = field(repr=False, default=None)
+
+    @abstractmethod
+    def is_bot(self) -> bool:
+        pass
 
     @abstractmethod
     def make_move(self):
         pass
 
+    def do_choice(self, stones: int):
+        if not self.game:
+            return
+
+        print(f"Игрок {self.name!r} выбрал: {stones}")
+        self.game.do_choice(self, stones)
+
 
 class UserPlayer(Player):
+    def is_bot(self) -> bool:
+        return False
+
     def make_move(self):
         while True:
             try:
                 stones = int(
-                    input(f"Камни от {self.game.MIN_STONES} до {self.game.MAX_STONES} (камней {self.game.number}): ")
+                    input(
+                        f"Камни от {self.game.MIN_STONES} до {self.game.MAX_STONES} (камней {self.game.number}): "
+                    )
                 )
                 assert self.game.MIN_STONES <= stones <= self.game.MAX_STONES
                 break
@@ -36,47 +55,50 @@ class UserPlayer(Player):
                 print("Неправильное значение!")
                 continue
 
-        self.game.number -= stones  # TODO: Мб пусть через игру будет логика - игрок будет говорить сколько камней возьмет, а игра сама решит кто победил
-        if self.game.number <= 0:
-            # TODO:
-            raise FinishGameException("Компьютер выиграл")
+        self.do_choice(stones)
 
 
 class BotPlayer(Player):
+    def is_bot(self) -> bool:
+        return True
+
     def make_move(self):
         # TODO: Когда остается минимум камней нужно вручную выбрать правильное, а не рандомно
         #       А то ИИ выглядит как искусственный идиот
-        stones = min(self.game.number, random.randint(self.game.MIN_STONES, self.game.MAX_STONES))
-        print(f"Компьютер выбрал: {stones}")
-        self.game.number -= stones  # TODO: Мб пусть через игру будет логика - игрок будет говорить сколько камней возьмет, а игра сама решит кто победил
-        if self.game.number <= 0:
-            # TODO:
-            raise FinishGameException("Ты выиграл")
+        stones = min(
+            self.game.number, random.randint(self.game.MIN_STONES, self.game.MAX_STONES)
+        )
+        self.do_choice(stones)
 
 
+@dataclass
 class Game:
+    player1: Player
+    player2: Player
+
+    number: int = 0
+    is_finished: bool = False
+    player1_is_first: bool = True
+
     MIN_STONES: int = 1
     MAX_STONES: int = 3
     MAX_NUMBER: int = 21
 
-    def __init__(self, player1: Player, player2: Player):
-        self.player1: Player = player1
+    def start(self):
         self.player1.game = self
-
-        self.player2: Player = player2
         self.player2.game = self
 
-        self.number: int = self.MAX_NUMBER
-
-        self.is_finished: bool = False
-        self.player1_is_first: bool = True
-
-    # TODO: Методы начала игры
-    def start(self):
+        self.number = self.MAX_NUMBER
         self.is_finished = False
 
         # TODO:
         self.player1_is_first = (input("Ты первый? (y/n): ").lower() or "y") == "y"
+
+    def do_choice(self, player: Player, stones: int):
+        self.number -= stones
+        if self.number <= 0:
+            # TODO:
+            raise FinishGameException(f"Игрок {player.name!r} проиграл!")
 
     # TODO: Мб счетчик раундов вести и явно писать?
     # TODO: Немного ASCII для удобного просмотра
@@ -100,10 +122,10 @@ class Game:
         return not self.is_finished
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     game = Game(
-        player1=UserPlayer(),
-        player2=BotPlayer(),
+        player1=UserPlayer("Человек"),
+        player2=BotPlayer("Бот"),
     )
     game.start()
 
