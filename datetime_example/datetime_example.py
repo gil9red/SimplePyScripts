763e50503fed521e4ab4b281e33e8f3a2ceda743
gil9red <ilya.petrash@inbox.ru>
2025-09-26 17:47:44 +0500

    Refactoring. datetime_example/datetime_example.py

--- datetime_example/datetime_example.py
+++ datetime_example/datetime_example.py
@@ -6,16 +6,26 @@ __author__ = "ipetrash"
 # http://pythonworld.ru/moduli/modul-datetime.html
 
 
-from datetime import date
+from datetime import datetime, date, timedelta
 
 
-if __name__ == "__main__":
-    # Dates are easily constructed and formatted
-    now = date.today()
-    print(now)
-    print(now.strftime("%m-%d-%y. %d %b %Y is a %A on the %d day of %B."))
+# Dates are easily constructed and formatted
+now = datetime.today()
+print(now)
+print(now.strftime("%m-%d-%y. %d %b %Y is a %A on the %d day of %B."))
 
-    # Dates support calendar arithmetic
-    birthday = date(1964, 7, 31)
-    age = now - birthday
-    print(age.days)
+# Dates support calendar arithmetic
+birthday: date = datetime(year=1964, month=7, day=31)
+age: timedelta = now - birthday
+print(age.days)
+
+print()
+
+now = date.today()
+print(now)
+print(now.strftime("%m-%d-%y. %d %b %Y is a %A on the %d day of %B."))
+
+# Dates support calendar arithmetic
+birthday: date = date(year=1964, month=7, day=31)
+age: timedelta = now - birthday
+print(age.days)
