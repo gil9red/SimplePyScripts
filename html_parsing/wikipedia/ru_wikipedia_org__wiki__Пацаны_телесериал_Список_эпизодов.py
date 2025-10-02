65a8b65c14a0e1672413396438c40e34f974faad
gil9red <ilya.petrash@inbox.ru>
2025-10-02 22:03:36 +0500

    Refactoring. wikipedia/ru_wikipedia_org__wiki__Пацаны_телесериал_Список_эпизодов.py

--- html_parsing/wikipedia/ru_wikipedia_org__wiki__Пацаны_телесериал_Список_эпизодов.py
+++ html_parsing/wikipedia/ru_wikipedia_org__wiki__Пацаны_телесериал_Список_эпизодов.py
@@ -4,7 +4,7 @@
 __author__ = "ipetrash"
 
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 from common import session
 
 
@@ -21,7 +21,7 @@ def get_seasons() -> dict[str, list[str]]:
     for season_title_el in soup.select('div:has(> h3[id *= "_сезон_"])'):
         season_title: str = season_title_el.h3.get_text(strip=True)
 
-        table_series = season_title_el.find_next(
+        table_series: Tag | None = season_title_el.find_next(
             "table", attrs={"class": "wikiepisodetable"}
         )
         if not table_series:
@@ -33,7 +33,7 @@ def get_seasons() -> dict[str, list[str]]:
             if sup_el := title_el.find("sup"):
                 sup_el.decompose()
 
-            series_title = title_el.get_text(strip=True)
+            series_title: str = title_el.get_text(strip=True)
             series_list.append(series_title)
 
         season_by_series[season_title] = series_list
@@ -103,5 +103,8 @@ if __name__ == "__main__":
 
     print("\n" + "-" * 10 + "\n")
 
-    all_series = get_all_series()
+    all_series: list[str] = get_all_series()
     print(f"All series ({len(all_series)}): {all_series}")
+    """
+    All series (32): ['1 сезон (2019). Такая игра', '1 сезон (2019). Вишенка', '1 сезон (2019). Получи!', '1 сезон (2019). Самка человека', '1 сезон (2019). Полезно для души', '1 сезон (2019). Невинные', '1 сезон (2019). Общество самосохранения', '1 сезон (2019). Ты меня нашёл', '2 сезон (2020). Большая поездка', '2 сезон (2020). Правильная подготовка и планирование', '2 сезон (2020). За холмом тысячи людей с мечами', '2 сезон (2020). Ничего подобного в мире нет', '2 сезон (2020). Нам пора идти', '2 сезон (2020). Прочь чёртовы двери', '2 сезон (2020). Мясник, пекарь, свечник', '2 сезон (2020). Что я знаю', '3 сезон (2022). Расплата', '3 сезон (2022). Единственный в небе', '3 сезон (2022). Берег варваров', '3 сезон (2022). Славная пятилетка', '3 сезон (2022). Последний взор на сей мир лжи', '3 сезон (2022). Героргазм', '3 сезон (2022). Вот зажгу я пару свеч — ты в постельку можешь лечь', '3 сезон (2022). До белого каления', '4 сезон (2024). Отдел грязных трюков', '4 сезон (2024). Жизнь среди заразы', '4 сезон (2024). Вейся, красный наш флаг', '4 сезон (2024). Мудрость пожилых', '4 сезон (2024). Остерегайся бармаглота, сын мой', '4 сезон (2024). Грязный бизнес', '4 сезон (2024). Инсайдер', '4 сезон (2024). Финал четвёртого сезона»«Убийственный забег']
+    """
