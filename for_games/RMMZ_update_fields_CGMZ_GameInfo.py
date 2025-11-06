f75b050a201b435fee7650f2d0fbfe070084f6cd
gil9red <ilya.petrash@inbox.ru>
2025-11-06 21:58:13 +0500

    Added. for_games/RMMZ_update_fields_CGMZ_GameInfo.py

---
+++ for_games/RMMZ_update_fields_CGMZ_GameInfo.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+import json
+import subprocess
+import re
+
+from pathlib import Path
+
+
+PATTERN_FIELD_VERSION: re.Pattern = re.compile(r'(?P<left>"Left Text"\s*:\s*)".+?"')
+
+
+def process(path_dir: Path):
+    path_js_plugins: Path = path_dir / "js" / "plugins.js"
+
+    result: str = subprocess.check_output(
+        args=[
+            "git",
+            "log",
+            "-1",
+            '--pretty=format:{"hash":"%h","datetime":"%ad"}',
+            "--date=format:%Y-%m-%d %H:%M:%S",
+        ],
+        cwd=path_dir,
+        encoding="utf-8",
+    )
+    print("Git log:", result)
+
+    data: dict[str, str] = json.loads(result)
+    print("Git log (parsed):", data)
+
+    version: str = f'v{data["hash"]} ({data["datetime"]})'
+    print(f"Version: {version}")
+
+    js_plugins: str = path_js_plugins.read_text(encoding="utf-8")
+    lines: list[str] = js_plugins.splitlines()
+    for i, line in enumerate(lines):
+        if "CGMZ_GameInfo" not in line:
+            continue
+
+        print(f"Found line {line!r}")
+
+        m = PATTERN_FIELD_VERSION.search(line)
+        if not m:
+            continue
+
+        current_field_version: str = m.group()
+
+        new_field_version: str = PATTERN_FIELD_VERSION.sub(
+            rf'\g<left>"{version}"', current_field_version
+        )
+        print(f"Replace: {current_field_version!r} -> {new_field_version!r}")
+
+        lines[i] = line.replace(current_field_version, new_field_version)
+
+    print(f"Write to {path_js_plugins}")
+    path_js_plugins.write_text("\n".join(lines) + "\n", encoding="utf-8")
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Writes information about the last git commit to the CGMZ_GameInfo plugin parameter."
+    )
+    parser.add_argument(
+        "path_project",
+        help="Path to project (the directory containing game.rmmzproject)",
+        type=Path,
+    )
+    args = parser.parse_args()
+
+    process(args.path_project)
