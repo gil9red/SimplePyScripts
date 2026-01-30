1dca8458e0323e790ee0a12d1bb98bdf926f3619
gil9red <ilya.petrash@inbox.ru>
2026-01-30 20:17:54 +0500

    Updated. html_parsing/ranobehub_org/common.py. Default timeout 60

--- html_parsing/ranobehub_org/common.py
+++ html_parsing/ranobehub_org/common.py
@@ -5,9 +5,25 @@ __author__ = "ipetrash"
 
 
 import requests
+from requests.adapters import HTTPAdapter
 
+
+class TimeoutHTTPAdapter(HTTPAdapter):
+    def __init__(self, timeout, *args, **kwargs):
+        self._timeout = timeout
+        super().__init__(*args, **kwargs)
+
+    def send(self, request, timeout=None, **kwargs):
+        if timeout is None:
+            timeout = self._timeout
+        return super().send(request, timeout=timeout, **kwargs)
+
+
+adapter = TimeoutHTTPAdapter(timeout=60)
 
 session = requests.session()
+session.mount('http://', adapter)
+session.mount('https://', adapter)
 session.headers["User-Agent"] = (
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) "
     "Gecko/20100101 Firefox/113.0"
