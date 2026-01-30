052221615911af39bd6d7014af72d85ddb9828c6
gil9red <ilya.petrash@inbox.ru>
2026-01-30 14:04:04 +0500

    Updated. html_parsing/python_org/get_release_versions.py. Added get_release_specific_versions function

--- html_parsing/python_org/get_release_versions.py
+++ html_parsing/python_org/get_release_versions.py
@@ -14,11 +14,12 @@ import requests
 from bs4 import BeautifulSoup, Tag
 
 
-HOST: str = "https://www.python.org/"
-HOST_DOCS: str = "https://docs.python.org/"
+HOST: str = "https://www.python.org"
+HOST_DOCS: str = "https://docs.python.org"
+URL_DOWNLOAD: str = f"{HOST}/downloads/"
 
 
-def parse_date(date_str: str) -> date:
+def _parse_date(date_str: str) -> date:
     # EXAMPLES: "2026-10-01 (planned)", "2025-10-07", "2031-10", "Dec. 5, 2025", "June 3, 2025"
 
     if m := re.search(r"\d{4}-\d{2}-\d{2}", date_str):
@@ -60,19 +61,33 @@ def parse_date(date_str: str) -> date:
         raise Exception(f"Unknown date format {date_str!r}")
 
 
-def get_tag(parent_el: Tag, selector: str) -> Tag:
+def _get_tag(parent_el: Tag, selector: str) -> Tag:
     el: Tag | None = parent_el.select_one(selector)
     if not el:
         raise Exception(f"Not found element by selector {selector!r}")
     return el
 
 
-def get_tag_text(parent_el: Tag, selector: str) -> str:
-    return get_tag(parent_el, selector).get_text(strip=True)
+def _get_tag_text(parent_el: Tag, selector: str) -> str:
+    return _get_tag(parent_el, selector).get_text(strip=True)
 
 
-def get_tag_attr(parent_el: Tag, selector: str, attr: str) -> str:
-    return get_tag(parent_el, selector)[attr]
+def _get_tag_attr(parent_el: Tag, selector: str, attr: str) -> str:
+    return _get_tag(parent_el, selector)[attr]
+
+
+def _parse_version_from_tag(parent_el: Tag, selector: str) -> str:
+    version: str = _get_tag_text(parent_el, selector)
+    m = re.search(r"\d+\.\d+\.\d+|\d+\.\d+", version)
+    if not m:
+        raise Exception(f"Error in determining the version in {version!r}")
+
+    return m.group()
+
+
+def _parse_date_from_tag(parent_el: Tag, selector: str) -> date:
+    date_str: str = _get_tag_text(parent_el, selector)
+    return _parse_date(date_str)
 
 
 @dataclass
@@ -87,23 +102,49 @@ class ReleaseVersion:
 
     @classmethod
     def parse_from_soup(cls, el: Tag) -> "ReleaseVersion":
-        version: str = get_tag_text(el, ".release-version")
+        version: str = _parse_version_from_tag(el, ".release-version")
 
         return cls(
             version=version,
-            status=get_tag_text(el, ".release-status"),
-            release_start=parse_date(get_tag_text(el, ".release-start")),
-            release_end=parse_date(get_tag_text(el, ".release-end")),
-            url_download=urljoin(HOST, get_tag_attr(el, ".release-dl a[href]", "href")),
+            status=_get_tag_text(el, ".release-status"),
+            release_start=_parse_date_from_tag(el, ".release-start"),
+            release_end=_parse_date_from_tag(el, ".release-end"),
+            url_download=urljoin(
+                HOST, _get_tag_attr(el, ".release-dl a[href]", "href")
+            ),
             url_whatsnew=urljoin(HOST_DOCS, f"{version}/whatsnew/{version}.html"),
             url_release_pep=urljoin(
-                HOST, get_tag_attr(el, ".release-pep a[href]", "href")
+                HOST, _get_tag_attr(el, ".release-pep a[href]", "href")
+            ),
+        )
+
+
+# TODO: Тесты
+@dataclass
+class ReleaseSpecificVersion:
+    version: str
+    release_date: date
+    url_download: str
+    url_changelog: str
+
+    @classmethod
+    def parse_from_soup(cls, el: Tag) -> "ReleaseSpecificVersion":
+        version: str = _parse_version_from_tag(el, ".release-number")
+
+        return cls(
+            version=version,
+            release_date=_parse_date_from_tag(el, ".release-date"),
+            url_download=urljoin(
+                HOST, _get_tag_attr(el, ".release-download a[href]", "href")
+            ),
+            url_changelog=urljoin(
+                HOST_DOCS, _get_tag_attr(el, ".release-enhancements a[href]", "href")
             ),
         )
 
 
 def get_release_versions() -> list[ReleaseVersion]:
-    rs = requests.get(urljoin(HOST, "downloads/"))
+    rs = requests.get(URL_DOWNLOAD)
     rs.raise_for_status()
 
     soup = BeautifulSoup(rs.content, "html.parser")
@@ -112,6 +153,17 @@ def get_release_versions() -> list[ReleaseVersion]:
         for el in soup.select(
             ".active-release-list-widget > ol.list-row-container > li"
         )
+    ]
+
+
+def get_release_specific_versions() -> list[ReleaseSpecificVersion]:
+    rs = requests.get(URL_DOWNLOAD)
+    rs.raise_for_status()
+
+    soup = BeautifulSoup(rs.content, "html.parser")
+    return [
+        ReleaseSpecificVersion.parse_from_soup(el)
+        for el in soup.select(".download-list-widget > ol.list-row-container > li")
     ]
 
 
@@ -128,17 +180,15 @@ if __name__ == "__main__":
         ("Dec. 5, 2025", date(2025, 12, 5)),
         ("June 3, 2025", date(2025, 6, 3)),
     ]:
-        tc.assertEqual(parse_date(date_str), expected)
+        tc.assertEqual(_parse_date(date_str), expected)
 
     versions: list[ReleaseVersion] = get_release_versions()
-
     print(f"Versions ({len(versions)}): {', '.join(v.version for v in versions)}")
-    # Versions (7): 3.15, 3.14, 3.13, 3.12, 3.11, 3.10, 3.9
-
     print(f"Details ({len(versions)}):")
     for v in versions:
         print(f"    {v}")
     """
+    Versions (7): 3.15, 3.14, 3.13, 3.12, 3.11, 3.10, 3.9
     Details (7):
         ReleaseVersion(version='3.15', status='pre-release', release_start=datetime.date(2026, 10, 1), release_end=datetime.date(2031, 10, 1), url_download='https://www.python.org/downloads/latest/python3.15/', url_whatsnew='https://docs.python.org/3.15/whatsnew/3.15.html', url_release_pep='https://peps.python.org/pep-0790/')
         ReleaseVersion(version='3.14', status='bugfix', release_start=datetime.date(2025, 10, 7), release_end=datetime.date(2030, 10, 1), url_download='https://www.python.org/downloads/latest/python3.14/', url_whatsnew='https://docs.python.org/3.14/whatsnew/3.14.html', url_release_pep='https://peps.python.org/pep-0745/')
@@ -147,4 +197,35 @@ if __name__ == "__main__":
         ReleaseVersion(version='3.11', status='security', release_start=datetime.date(2022, 10, 24), release_end=datetime.date(2027, 10, 1), url_download='https://www.python.org/downloads/latest/python3.11/', url_whatsnew='https://docs.python.org/3.11/whatsnew/3.11.html', url_release_pep='https://peps.python.org/pep-0664/')
         ReleaseVersion(version='3.10', status='security', release_start=datetime.date(2021, 10, 4), release_end=datetime.date(2026, 10, 1), url_download='https://www.python.org/downloads/latest/python3.10/', url_whatsnew='https://docs.python.org/3.10/whatsnew/3.10.html', url_release_pep='https://peps.python.org/pep-0619/')
         ReleaseVersion(version='3.9', status='end-of-life, last release was3.9.25', release_start=datetime.date(2020, 10, 5), release_end=datetime.date(2025, 10, 31), url_download='https://www.python.org/downloads/latest/python3.9/', url_whatsnew='https://docs.python.org/3.9/whatsnew/3.9.html', url_release_pep='https://peps.python.org/pep-0596/')
+    """
+
+    print("\n" + "-" * 100 + "\n")
+
+    specific_versions: list[ReleaseSpecificVersion] = get_release_specific_versions()
+    print(
+        f"Specific versions ({len(specific_versions)}):",
+        ", ".join(v.version for v in specific_versions[:5]),
+        "...",
+        ", ".join(v.version for v in specific_versions[-5:]),
+    )
+    print(f"Details ({len(specific_versions)}):")
+    for v in specific_versions[:5]:
+        print(f"    {v}")
+    print("    ...")
+    for v in specific_versions[-5:]:
+        print(f"    {v}")
+    """
+    Specific versions (245): 3.14.2, 3.14.1, 3.14.0, 3.13.11, 3.13.10 ... 2.2.2, 2.2.1, 2.2.0, 2.1.3, 2.0.1
+    Details (245):
+        ReleaseSpecificVersion(version='3.14.2', release_date=datetime.date(2025, 12, 5), url_download='https://www.python.org/downloads/release/python-3142/', url_changelog='https://docs.python.org/release/3.14.2/whatsnew/changelog.html')
+        ReleaseSpecificVersion(version='3.14.1', release_date=datetime.date(2025, 12, 2), url_download='https://www.python.org/downloads/release/python-3141/', url_changelog='https://docs.python.org/release/3.14.1/whatsnew/changelog.html')
+        ReleaseSpecificVersion(version='3.14.0', release_date=datetime.date(2025, 10, 7), url_download='https://www.python.org/downloads/release/python-3140/', url_changelog='https://docs.python.org/3.14/whatsnew/3.14.html')
+        ReleaseSpecificVersion(version='3.13.11', release_date=datetime.date(2025, 12, 5), url_download='https://www.python.org/downloads/release/python-31311/', url_changelog='https://docs.python.org/release/3.13.11/whatsnew/changelog.html')
+        ReleaseSpecificVersion(version='3.13.10', release_date=datetime.date(2025, 12, 2), url_download='https://www.python.org/downloads/release/python-31310/', url_changelog='https://docs.python.org/release/3.13.10/whatsnew/changelog.html')
+        ...
+        ReleaseSpecificVersion(version='2.2.2', release_date=datetime.date(2002, 10, 14), url_download='https://www.python.org/downloads/release/python-222/', url_changelog='http://hg.python.org/cpython/raw-file/v2.2.2/Misc/NEWS')
+        ReleaseSpecificVersion(version='2.2.1', release_date=datetime.date(2002, 4, 10), url_download='https://www.python.org/downloads/release/python-221/', url_changelog='http://hg.python.org/cpython/raw-file/v2.2.1/Misc/NEWS')
+        ReleaseSpecificVersion(version='2.2.0', release_date=datetime.date(2001, 12, 21), url_download='https://www.python.org/downloads/release/python-220/', url_changelog='http://hg.python.org/cpython/raw-file/v2.2/Misc/NEWS')
+        ReleaseSpecificVersion(version='2.1.3', release_date=datetime.date(2002, 4, 9), url_download='https://www.python.org/downloads/release/python-213/', url_changelog='http://hg.python.org/cpython/raw-file/v2.1.3/Misc/NEWS')
+        ReleaseSpecificVersion(version='2.0.1', release_date=datetime.date(2001, 6, 22), url_download='https://www.python.org/downloads/release/python-201/', url_changelog='http://hg.python.org/cpython/raw-file/v2.0.1/Misc/NEWS')
     """
