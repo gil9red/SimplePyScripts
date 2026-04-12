c99594106719002769f81b01934f810f87945a1e
gil9red <ilya.petrash@inbox.ru>
2026-04-12 07:05:04 +0500

    Updated. for_games/grid_clicker/grid_clicker_gui_select_rect.py

--- for_games/grid_clicker/grid_clicker_gui_select_rect.py
+++ for_games/grid_clicker/grid_clicker_gui_select_rect.py
@@ -4,24 +4,37 @@
 __author__ = "ipetrash"
 
 
+import json
 import sys
+
+from pathlib import Path
+from typing import Any
 
 from PyQt5.QtWidgets import (
     QApplication,
     QDialog,
     QFrame,
     QPushButton,
-    QCheckBox,
     QFormLayout,
     QSpinBox,
 )
 from PyQt5.QtCore import Qt, QRect, QPoint
-from PyQt5.QtGui import QPainter, QColor, QPen, QMouseEvent, QKeyEvent, QPaintEvent
+from PyQt5.QtGui import (
+    QPainter,
+    QColor,
+    QPen,
+    QMouseEvent,
+    QKeyEvent,
+    QPaintEvent,
+    QCloseEvent,
+)
 
 from grid_clicker import click_all_on_screen
 
+DIR: Path = Path(__file__).resolve().parent
+PATH_SETTINGS: Path = DIR / "settings.json"
 
-# TODO: Поддержка сохранения и восстановления области
+
 class AreaSelectorDialog(QDialog):
     def __init__(self) -> None:
         super().__init__()
@@ -40,12 +53,15 @@ class AreaSelectorDialog(QDialog):
         self._end_global: QPoint = QPoint()  # Глобальная точка конца
         self._is_selecting: bool = False
 
-        self.selected_rect: QRect | None = None
+        self.selected_rect: QRect = QRect()
+        self.do_click: bool = True
         self.need_run_clicker: bool = False
 
-        self.checkbox_do_click = QCheckBox(checked=True)
         self.spin_box_step = QSpinBox(value=50)
         self.button_run = QPushButton("Run click", clicked=self.on_run_clicked)
+        self.button_run_test = QPushButton(
+            "Run TEST click", clicked=self.on_run_test_clicked
+        )
         self.button_close = QPushButton("Close", clicked=self.close)
 
         self.control_widget = QFrame(self)
@@ -60,22 +76,41 @@ class AreaSelectorDialog(QDialog):
                 color: white;
             }
         """)
-        self.control_widget.hide()
         control_layout = QFormLayout(self.control_widget)
-        control_layout.addRow("Do &Click?:", self.checkbox_do_click)
         control_layout.addRow("&Step:", self.spin_box_step)
+        control_layout.addRow(self.button_run_test)
         control_layout.addRow(self.button_run)
         control_layout.addRow(self.button_close)
+
+        if PATH_SETTINGS.exists():
+            data: dict[str, Any] = json.loads(PATH_SETTINGS.read_text("UTF-8"))
+            coords: list[int] | None = data.get("coords")
+            if coords:
+                self.selected_rect.setCoords(*coords)
+                self.update()
+
+        self._move_control_widget()
+
+    def _move_control_widget(self):
+        if not self.selected_rect:
+            self.control_widget.hide()
+            return
+
+        local_begin = self.mapFromGlobal(self.selected_rect.topLeft())
+        self.control_widget.move(local_begin)
+        self.control_widget.show()
 
     @property
     def step(self) -> int:
         return self.spin_box_step.value()
 
-    @property
-    def do_click(self) -> bool:
-        return self.checkbox_do_click.isChecked()
+    def on_run_test_clicked(self) -> None:
+        self.do_click = False
+        self.need_run_clicker = True
+        self.close()
 
     def on_run_clicked(self) -> None:
+        self.do_click = True
         self.need_run_clicker = True
         self.close()
 
@@ -83,24 +118,24 @@ class AreaSelectorDialog(QDialog):
         if event.button() == Qt.LeftButton:
             self._begin_global = event.globalPos()
             self._end_global = self._begin_global
+
             self._is_selecting = True
+            self._move_control_widget()
             self.update()
 
     def mouseMoveEvent(self, event: QMouseEvent) -> None:
         if self._is_selecting:
             self._end_global = event.globalPos()
+
+            rect = QRect(self._begin_global, self._end_global)
+            self.selected_rect = rect.normalized()
+
+            self._move_control_widget()
             self.update()
 
-            local_begin = self.mapFromGlobal(self._begin_global)
-            self.control_widget.move(local_begin)
-            self.control_widget.show()
-
     def mouseReleaseEvent(self, event: QMouseEvent) -> None:
-        if event.button() == Qt.LeftButton:
+        if self._is_selecting and event.button() == Qt.LeftButton:
             self._is_selecting = False
-            self.selected_rect = QRect(
-                self._begin_global, self._end_global
-            ).normalized()
 
     def keyPressEvent(self, event: QKeyEvent) -> None:
         if event.key() == Qt.Key_Escape:
@@ -110,14 +145,27 @@ class AreaSelectorDialog(QDialog):
         painter = QPainter(self)
         painter.fillRect(self.rect(), QColor(0, 0, 0, 100))
 
-        if self._is_selecting:
-            local_begin = self.mapFromGlobal(self._begin_global)
-            local_end = self.mapFromGlobal(self._end_global)
-            rect = QRect(local_begin, local_end).normalized()
+        if self.selected_rect:
+            local_rect: QRect = QRect(
+                self.mapFromGlobal(self.selected_rect.topLeft()),
+                self.mapFromGlobal(self.selected_rect.bottomRight()),
+            )
 
             painter.setPen(QPen(Qt.red, 2, Qt.SolidLine))
-            painter.fillRect(rect, QColor(255, 255, 255, 1))
-            painter.drawRect(rect)
+            painter.fillRect(local_rect, QColor(255, 255, 255, 1))
+            painter.drawRect(local_rect)
+
+    def closeEvent(self, event: QCloseEvent) -> None:
+        if self.selected_rect:
+            data: dict[str, Any] = {
+                "coords": self.selected_rect.getCoords(),
+            }
+            PATH_SETTINGS.write_text(
+                json.dumps(data),
+                encoding="UTF-8",
+            )
+
+        super().closeEvent(event)
 
 
 if __name__ == "__main__":
