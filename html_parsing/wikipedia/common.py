0df4b9435558abf61f43efa94df172d5c0300cc6
gil9red <ilya.petrash@inbox.ru>
2026-06-11 17:26:43 +0500

    fix(parser): update wikipedia headers in html_parsing\wikipedia\common.py

--- html_parsing/wikipedia/common.py
+++ html_parsing/wikipedia/common.py
@@ -6,18 +6,23 @@ __author__ = "ipetrash"
 
 import requests
 
-
-HTTP_TIMEOUT = 60
+HTTP_TIMEOUT: int = 60
 
 
 # SOURCE: https://stackoverflow.com/a/47384155/5909792
 class TimeoutRequestsSession(requests.Session):
     def request(self, *args, **kwargs):
-        kwargs.setdefault('timeout', HTTP_TIMEOUT)
+        kwargs.setdefault("timeout", HTTP_TIMEOUT)
         return super().request(*args, **kwargs)
 
 
 session = TimeoutRequestsSession()
-session.headers[
-    "User-Agent"
-] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0"
+session.headers.update(
+    {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+    }
+)
