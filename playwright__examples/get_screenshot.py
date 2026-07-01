3bc5d8a091227d96c74f6cd96100520022a4cead
gil9red <ilya.petrash@inbox.ru>
2026-07-01 22:24:18 +0500

    feat(playwright): add playwright__examples/get_screenshot.py

---
+++ playwright__examples/get_screenshot.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+from pathlib import Path
+from playwright.sync_api import sync_playwright
+
+DIR: Path = Path(__file__).resolve().parent
+DIR_SCREENSHOTS: Path = DIR / "screenshots"
+DIR_SCREENSHOTS.mkdir(parents=True, exist_ok=True)
+
+
+with sync_playwright() as p:
+    for browser_type in [p.chromium, p.firefox, p.webkit]:
+        browser = browser_type.launch()
+        page = browser.new_page()
+
+        page.goto("https://github.com/gil9red")
+
+        print(browser_type.name)
+
+        path_page: Path = DIR_SCREENSHOTS / f"{browser_type.name}_page.png"
+        page.screenshot(path=path_page)
+
+        path_full_page: Path = DIR_SCREENSHOTS / f"{browser_type.name}_full_page.png"
+        page.screenshot(path=path_full_page, full_page=True)
