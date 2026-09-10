ddd361e3f991f6235f5e57a0615c4df1282c9439
gil9red <ilya.petrash@inbox.ru>
2026-09-10 18:34:00 +0500

    refactor(base64-examples): clean up code example_encode_and_decode

--- Base64_examples/example_encode_and_decode.py
+++ Base64_examples/example_encode_and_decode.py
@@ -6,27 +6,35 @@ __author__ = "ipetrash"
 
 import base64
 
-text = "Hello py! Привет py!"
-print(f"Text: {text}")
 
-b16 = base64.b16encode(text.encode())
-b32 = base64.b32encode(text.encode())
-b64 = base64.b64encode(text.encode())
-b85 = base64.b85encode(text.encode())
-
-print("encode(base16): '%s'" % b16)
-print("encode(base32): '%s'" % b32)
-print("encode(base64): '%s'" % b64)
-print("encode(base85): '%s'" % b85)
+text: str = "Hello py! Привет py!"
+data: bytes = text.encode()
+print(f"Text ({len(text)}): {text!r}")
+print(f"Data ({len(data)}): {data!r}")
+"""
+Text (20): 'Hello py! Привет py!'
+Data (26): b'Hello py! \xd0\x9f\xd1\x80\xd0\xb8\xd0\xb2\xd0\xb5\xd1\x82 py!'
+"""
 
 print()
-print("decode(base16): '%s'" % base64.b16decode(b16))
-print("decode(base32): '%s'" % base64.b32decode(b32))
-print("decode(base64): '%s'" % base64.b64decode(b64))
-print("decode(base85): '%s'" % base64.b85decode(b85))
 
-print()
-print("decode(base16) utf-8: '%s'" % base64.b16decode(b16).decode())
-print("decode(base32) utf-8: '%s'" % base64.b32decode(b32).decode())
-print("decode(base64) utf-8: '%s'" % base64.b64decode(b64).decode())
-print("decode(base85) utf-8: '%s'" % base64.b85decode(b85).decode())
+b16: bytes = base64.b16encode(data)
+b32: bytes = base64.b32encode(data)
+b64: bytes = base64.b64encode(data)
+b85: bytes = base64.b85encode(data)
+
+print(f"encode base16 ({len(b16)}): {b16!r}")
+print(f"encode base32 ({len(b32)}): {b32!r}")
+print(f"encode base64 ({len(b64)}): {b64!r}")
+print(f"encode base85 ({len(b85)}): {b85!r}")
+"""
+encode base16 (52): b'48656C6C6F2070792120D09FD180D0B8D0B2D0B5D18220707921'
+encode base32 (48): b'JBSWY3DPEBYHSIJA2CP5DAGQXDILFUFV2GBCA4DZEE======'
+encode base64 (36): b'SGVsbG8gcHkhINCf0YDQuNCy0LXRgiBweSE='
+encode base85 (33): b'NM&qnZy<1aAt2D7(SXpn(6Z3A(Sjgwc_9'
+"""
+
+assert base64.b16decode(b16) == data
+assert base64.b16decode(b16) == data
+assert base64.b16decode(b16) == data
+assert base64.b16decode(b16) == data
