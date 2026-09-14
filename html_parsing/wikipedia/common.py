ed8016d3f3eaabdae8aaffeafc104fa73c36e839
gil9red <ilya.petrash@inbox.ru>
2026-09-14 19:38:23 +0500

    fix(parser-wikipedia): add valid User-Agent to respect robot policy

--- html_parsing/wikipedia/common.py
+++ html_parsing/wikipedia/common.py
@@ -19,9 +19,10 @@ class TimeoutRequestsSession(requests.Session):
 session = TimeoutRequestsSession()
 session.headers.update(
     {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0",
+        "User-Agent": "WikiEpisodesParser/1.0 (https://github.com/gil9red/SimplePyScripts; Owner: gil9red)",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
+        "Accept-Encoding": "gzip",
         "Connection": "keep-alive",
         "Upgrade-Insecure-Requests": "1",
     }
