a148d7f8c95cd10443a41623a6a749248c3dc7a2
gil9red <ilya.petrash@inbox.ru>
2026-09-14 19:57:04 +0500

    feat(qt-examples): add QFile_moveToTrash_pyqt5.py

---
+++ qt__pyqt__pyside__pyqode/QFile_moveToTrash_pyqt5.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+from pathlib import Path
+
+from PyQt5.QtCore import QFile
+
+PATH: Path = Path(__file__).resolve()
+DIR: Path = PATH.parent
+
+PATH_EXAMPLE: Path = DIR / f"{PATH.name}.txt"
+PATH_EXAMPLE.write_text("Hello World!")
+
+print(f"Writing to file '{PATH_EXAMPLE.name}'")
+
+file_path: str = str(PATH_EXAMPLE)
+file_obj: QFile = QFile(file_path)
+if file_obj.moveToTrash():
+    print(f"File '{file_path}' was successfully moved to trash.")
+else:
+    print(f"Failed to move file '{file_path}' to trash.")
